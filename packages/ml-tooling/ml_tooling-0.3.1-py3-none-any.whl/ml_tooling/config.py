ac4778061d3e8793ce5b0604d34912146675f945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package ml-tooling/0.3.1/ml_tooling-0.3.1-py3-none-any.whl

---
+++ packages/ml-tooling/ml_tooling-0.3.1-py3-none-any.whl/ml_tooling/config.py
@@ -0,0 +1,25 @@
+import pathlib
+
+BASE_PATH = pathlib.Path(__file__).parent
+MPL_STYLESHEET = str(BASE_PATH.joinpath('almbrand.mplstyle'))
+
+
+class DefaultConfig:
+    """
+    Configuration for a given Baseclass. Configs propagate through each instance
+    """
+    def __init__(self):
+        self.VERBOSITY = 0
+        self.CLASSIFIER_METRIC = 'accuracy'
+        self.REGRESSION_METRIC = 'r2'
+        self.CROSS_VALIDATION = 10
+        self.STYLE_SHEET = MPL_STYLESHEET
+        self.N_JOBS = -1
+        self.TRAIN_SIZE = 0.25
+        self.RANDOM_STATE = 42
+
+    def __repr__(self):
+        attrs = '\n'.join([f"{attr}: {value}"
+                           for attr, value in self.__dict__.items()
+                           if '__' not in attr])
+        return f'<Config: \n{attrs}\n>'
