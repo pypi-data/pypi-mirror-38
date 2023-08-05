78a8591ca8ac7d3c8877ec255bb335376d2184a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:26 +0000

    Add package arctic/1.72.0/arctic-1.72.0.tar.gz

---
+++ packages/arctic/arctic-1.72.0.tar.gz/arctic-1.72.0/tests/unit/test_fixtures.py
@@ -0,0 +1,20 @@
+import arctic.fixtures.arctic as fix
+from mock import Mock
+from arctic.arctic import Arctic
+
+
+def test_overlay_library_name(overlay_library_name):
+    assert(overlay_library_name == 'test.OVERLAY')
+
+
+def test_overlay_library():
+    a = Mock(Arctic, autospec=True)
+    fix._overlay_library(a, 'test')
+    a.initialize_library.assert_called_with("test_RAW", "VersionStore", segment='year')
+
+
+def test_tickstore_lib():
+    a = Mock(Arctic, autospec=True)
+    fix._tickstore_lib(a, "test")
+    a.initialize_library.assert_called_with('test', 'TickStoreV3')
+    a.get_library.assert_called_with('test')
