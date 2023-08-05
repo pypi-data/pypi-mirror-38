ec4d3c6687c2d2b1b8ba0c63c449e5f5316625da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:36 +0000

    Add package gggnester/1.3.0/gggnester-1.3.0.tar.gz

---
+++ packages/gggnester/gggnester-1.3.0.tar.gz/gggnester-1.3.0/gggnester.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-11-04 18:37:13
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+"""This is the "nester.py" module and it provides one function called print_lol()
+   which prints lists that may or may not include nested lists."""
+def print_lol(the_list, intent=False, level=0):
+	"""This function takes one positional argument called "the_list", which is 
+	   any Python list (of - possibly - nested lists). Each data item in the 
+	   provided list is (recursively) printed to the screen on it's own lines.
+	   A Second argument called "intent" is used as a switch to turn on/off tab-stops.
+	   A Third argument called "level" is used to insert tab-stops when a 
+	   nested list is encountered."""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item,intent,level+1)
+		else:
+			if intent:
+				for num in range(level):
+					print("\t",end='')
+			print(each_item)
