470d6db2cd9e550a12abf760fb14d899a64f8d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:23 +0000

    Add package djangoql/0.11.0/djangoql-0.11.0.tar.gz

---
+++ packages/djangoql/djangoql-0.11.0.tar.gz/djangoql-0.11.0/test_project/core/models.py
@@ -0,0 +1,37 @@
+from __future__ import unicode_literals
+
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.utils.timezone import now
+
+from djangoql.queryset import DjangoQLQuerySet
+
+
+class Book(models.Model):
+    GENRES = {
+        1: 'Drama',
+        2: 'Comics',
+        3: 'Other',
+    }
+
+    name = models.CharField(max_length=10)  # lol, we're minimalists
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    genre = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        choices=GENRES.items(),
+    )
+    written = models.DateTimeField(default=now)
+    is_published = models.BooleanField(default=False)
+    rating = models.FloatField(null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    content_type = models.ForeignKey(
+        ContentType,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    objects = DjangoQLQuerySet.as_manager()
