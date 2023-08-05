aaecfea3d8ad1c5168a4bdb683b5834076fa52d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:42 +0000

    Add package settings-loader/0.1.2/settings_loader-0.1.2-py3-none-any.whl

---
+++ packages/settings-loader/settings_loader-0.1.2-py3-none-any.whl/tests/example.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+
+from configuration_loader.loaders.basic_loader import BasicLoader
+from configuration_loader.loaders.environment import EnvironmentLoader
+from configuration_loader.loaders.json import JsonLoader
+from configuration_loader.parsers.default import DefaultParser
+from configuration_loader.mergers.default import DefaultMerger
+
+
+def main():
+    json_loader = JsonLoader(["secrets/configs.json"])
+    env_loader = EnvironmentLoader()
+
+    merger = DefaultMerger()
+
+    parsers = [DefaultParser()]
+    loader = BasicLoader(parsers, merger, [env_loader, json_loader])
+    loader.load()
+    pprint(loader.configs)
+
+
+if __name__ == '__main__':
+    main()
