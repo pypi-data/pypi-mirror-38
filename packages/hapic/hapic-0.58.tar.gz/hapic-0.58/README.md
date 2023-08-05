aa01950ae945f56e30fff9e14136a0c37887da47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:52 +0000

    Add package hapic/0.58/hapic-0.58.tar.gz

---
+++ packages/hapic/hapic-0.58.tar.gz/hapic-0.58/README.md
@@ -0,0 +1,173 @@
+[![Build Status](https://travis-ci.org/algoo/hapic.svg?branch=master)](https://travis-ci.org/algoo/hapic)
+[![Coverage Status](https://coveralls.io/repos/github/algoo/hapic/badge.svg?branch=master)](https://coveralls.io/github/algoo/hapic?branch=master)
+
+# Hapic in a nutshell
+
+Hapic is a framework-agnostic library for implementation of professionnal REST APIs.
+
+Why you should use Hapic :
+
+- Hapic adapt naturally to your existing libraries
+- Hapic map exceptions to HTTP errors without effort
+- Hapic really auto-documents your APIs according to marshmallow,  apispec and web framework routing. You do not "write your doc in docstrings": the documentation is really auto-generated
+- Hapic can be used out-of-the-box with Bottle, Flask or Pyramid
+
+Hapic works with JSON, but it can be used for XML or virtually any data structure format.
+
+Auto-generated documentation can be visualised with swagger.
+
+## Philosophy
+
+Hapic as been developed by algoo in the context of a large client project. The lack of a tool allowing real auto-documentation of Rest API has decided us to develop Hapic.
+
+Target projects of Hapic are not "quick and dirty" but professionnally architectured projects.
+
+The separation of concerns between REST APIs layer and Business Stuff layer is in the DNA of Hapic. Hapic is *just* the HTTP Layer over your business code.
+
+## Licence
+
+Hapic is licenced under the MIT licence. You can use it in your projects, closed or open sourced.
+
+## Status, contributions
+
+Hapic source code is ready for production. Some refactoring are identified and required for maintainability, but public APIs are stable so you can rely on Hapic for your developments.
+
+Hapic is under active development, based on Algoo projects. We will answer your questions and accept merge requests if you find bugs or want to include functionnalities.
+
+Hapic is currently tested on python 3.5 and 3.6.
+
+## TODO references
+
+TODO can make reference to #X, this is github issues references.
+
+## Installation
+
+```
+virtualenv -p /usr/bin/python3 venv
+source venv/bin/activate
+python setup.py develop
+```
+ 
+## Give it a try
+
+### Fast Flask example
+
+```
+from datetime import datetime
+import flask
+import marshmallow
+import hapic
+from hapic.ext.flask import FlaskContext
+import json
+
+hapic = hapic.Hapic()
+app = flask.Flask(__name__)
+
+
+class UriPathSchema(marshmallow.Schema):  # schema describing the URI and allowed values
+    name = marshmallow.fields.String(required=True)
+    age = marshmallow.fields.Integer(required=False)
+
+
+class HelloResponseSchema(marshmallow.Schema): # schema of the API response
+    name = marshmallow.fields.String(required=True)
+    now = marshmallow.fields.DateTime(required=False)
+    greetings = marshmallow.fields.String(required=False)
+
+
+@app.route('/hello/<name>')  # flask route. must always be before hapic decorators
+@hapic.with_api_doc()  # the first hapic decorator. Register the method for auto-documentation
+@hapic.input_path(UriPathSchema())  # validate the URI structure
+@hapic.output_body(HelloResponseSchema())  # define output structure
+def hello(name='<No name>', hapic_data=None):
+    return {
+        'name': name,
+        'now': datetime.now(),
+        'dummy': { 'some': 'dummy' }  # will be ignored
+    }
+
+class UriPathSchemaWithAge(marshmallow.Schema):  # schema describing the URI and allowed values
+    name = marshmallow.fields.String(required=True)
+    age = marshmallow.fields.Integer(required=False)
+
+
+@app.route('/hello/<name>/age/<age>')
+@hapic.with_api_doc()
+@hapic.input_path(UriPathSchemaWithAge())
+@hapic.output_body(HelloResponseSchema())
+def hello2(name='<No name>', age=42, hapic_data=None):
+    return {
+        'name': name,
+        'age': age,
+        'greetings': 'Hello {name}, it looks like you are {age}'.format(
+            name=name,
+            age=age
+        ),
+        'now': datetime.now(),
+        'dummy': { 'some': 'dummy' }  # will be ignored
+    }
+
+
+hapic.set_context(FlaskContext(app))
+print(json.dumps(hapic.generate_doc(title='API Doc', description='doc desc.')))  # Generate the documentation
+app.run('127.0.0.1', 8080, debug=True)
+```
+
+How to use it:
+
+Nominal cases:
+
+```
+$ curl "http://127.0.0.1:8080/hello/michel"
+# {"now": "2017-12-18T12:37:10.751623+00:00", "name": "michel"}
+```
+
+```
+$ curl "http://127.0.0.1:8080/hello/michel/age/17"
+# {"name": "damien", "greetings": "Hello damien, it looks like you are 17", "now": "2017-12-18T12:41:58.229679+00:00"}
+```
+
+Error case (returns a 400):
+
+```
+$ curl "http://127.0.0.1:8080/hello/michel/age/mistaken"
+# {"details": {"age": ["Not a valid integer."]}, "message": "Validation error of input data"}
+```
+
+
+### A complete user API
+
+In the `example/usermanagement` directory you can find a complete example of an API allowing to manage users.
+
+Features are: 
+
+- get list of all users
+- get detail of a given user
+- create a user
+- delete a user
+
+In order to test it :
+
+Install the required dependencies:
+
+```
+pip install bottle flask pyramid`
+```
+
+Run the instance you wan to test (one of the three following lines):
+
+```
+python example/usermanagement/serve_bottle.py
+python example/usermanagement/serve_flask.py
+python example/usermanagement/serve_pyramid.py
+```
+
+Features shown :
+
+- auto-generation of the documentation
+- managing parameters in the uri path
+- managing input schemas
+- managing output schema
+- management of error cases (404, 500, etc)
+- nice exception handling
+- automatic dict/object serialization
---
+++ packages/hapic/hapic-0.58.tar.gz/hapic-0.58/hapic/doc.py
@@ -0,0 +1,429 @@
+# -*- coding: utf-8 -*-
+import json
+
+import typing
+import marshmallow
+import yaml
+
+from apispec import APISpec
+from apispec import Path
+from apispec.ext.marshmallow.swagger import schema2jsonschema
+
+from hapic.context import ContextInterface
+from hapic.context import RouteRepresentation
+from hapic.decorator import DecoratedController
+from hapic.description import ControllerDescription
+
+
+FIELDS_PARAMS_GENERIC_ACCEPTED = [
+    'type',
+    'format',
+    'required',
+    'description',
+    'enum',
+]
+
+FIELDS_TYPE_ARRAY = [
+    'array',
+]
+FIELDS_PARAMS_ARRAY_ACCEPTED = [
+    'items',
+    'collectionFormat',
+    'pattern',
+    'maxitems',
+    'minitems',
+    'uniqueitems',
+]
+
+FIELDS_TYPE_STRING = [
+    'string',
+]
+FIELDS_PARAMS_STRING_ACCEPTED = [
+    'maxLength',
+    'minLength',
+    'pattern',
+]
+
+FIELDS_TYPE_NUMERIC = [
+    'number', 
+    'integer',
+]
+FIELDS_PARAMS_NUMERIC_ACCEPTED = [
+    'maximum',
+    'exclusiveMaximum',
+    'minimum',
+    'exclusiveMinimum',
+    'multipleOf',
+]
+
+
+def generate_schema_ref(spec:APISpec, schema: marshmallow.Schema) -> str:
+    schema_class = spec.schema_class_resolver(
+        spec,
+        schema
+    )
+    ref = {
+        '$ref': '#/definitions/{}'.format(
+            spec.schema_name_resolver(schema_class)
+        )
+    }
+    if schema.many:
+        ref = {
+            'type': 'array',
+            'items': ref
+        }
+    return ref
+
+
+def field_accepted_param(type: str, param_name:str) -> bool:
+    return (
+        param_name in FIELDS_PARAMS_GENERIC_ACCEPTED
+        or (type in FIELDS_TYPE_STRING
+            and param_name in FIELDS_PARAMS_STRING_ACCEPTED)
+        or (type in FIELDS_TYPE_ARRAY
+            and param_name in FIELDS_PARAMS_ARRAY_ACCEPTED)
+        or (type in FIELDS_TYPE_NUMERIC
+            and param_name in FIELDS_PARAMS_NUMERIC_ACCEPTED)
+    )
+
+
+def generate_fields_description(
+    schema,
+    in_: str,
+    name: str,
+    required: bool,
+    type: str=None,
+) -> dict:
+    """
+    Generate field OpenApiDescription for
+    both query and path params
+    :param schema: field schema
+    :param in_: in field
+    :param name: field name
+    :param required: required field
+    :param type: type field
+    :return: File description for OpenApi
+    """
+    description = {}
+    # INFO - G.M - 01-06-2018 - get field
+    # type to know which params are accepted
+    if not type and 'type' in schema:
+        type = schema['type']
+    assert type
+
+    for param_name, value in schema.items():
+        if field_accepted_param(type, param_name):
+            description[param_name] = value
+    description['type'] = type
+    description['in'] = in_
+    description['name'] = name
+    description['required'] = required
+
+
+    # INFO - G.M - 01-06-2018 - example is not allowed in query/path params,
+    # in OpenApi2, remove it and set it as string in field description.
+    if 'example' in schema:
+        if 'description' not in description:
+            description['description'] = ""
+        description['description'] = '{description}\n\n*example value: {example}*'.format(  # nopep8
+            description=description['description'],
+            example=schema['example']
+        )
+    return description
+
+
+def bottle_generate_operations(
+    spec,
+    route: RouteRepresentation,
+    description: ControllerDescription,
+):
+    method_operations = dict()
+    if description.input_body:
+        method_operations.setdefault('parameters', []).append({
+            'in': 'body',
+            'name': 'body',
+            'schema': generate_schema_ref(
+                spec,
+                description.input_body.wrapper.processor.schema,
+            )
+        })
+
+    if description.output_body:
+        method_operations.setdefault('responses', {})\
+            [int(description.output_body.wrapper.default_http_code)] = {
+                'description': str(int(description.output_body.wrapper.default_http_code)),  # nopep8
+                'schema': generate_schema_ref(
+                    spec,
+                    description.output_body.wrapper.processor.schema,
+                )
+            }
+
+    if description.output_stream:
+        # TODO BS 2018-07-31: Is that a correct way to re
+        # instanciate with .__class__ ... ?
+        method_operations.setdefault('responses', {})\
+            [int(description.output_stream.wrapper.default_http_code)] = {
+                'description': str(int(description.output_stream.wrapper.default_http_code)),  # nopep8
+                'schema': generate_schema_ref(
+                    spec,
+                    description
+                        .output_stream
+                        .wrapper
+                        .processor
+                        .schema
+                        .__class__(many=True),
+                )
+            }
+
+    if description.output_file:
+        method_operations.setdefault('produces', []).extend(
+            description.output_file.wrapper.output_types
+        )
+        method_operations.setdefault('responses', {})\
+            [int(description.output_file.wrapper.default_http_code)] = {
+            'description': str(int(description.output_file.wrapper.default_http_code)),  # nopep8
+        }
+
+    if description.errors:
+        for error in description.errors:
+            schema_class = type(error.wrapper.error_builder)
+            method_operations.setdefault('responses', {})\
+                [int(error.wrapper.http_code)] = {
+                    'description': str(int(error.wrapper.http_code)),
+                    'schema': {
+                        '$ref': '#/definitions/{}'.format(
+                            spec.schema_name_resolver(schema_class)
+                        )
+                    }
+                }
+
+    # jsonschema based
+    if description.input_path:
+        schema_class = spec.schema_class_resolver(
+            spec,
+            description.input_path.wrapper.processor.schema
+        )
+        # TODO: look schema2parameters ?
+        jsonschema = schema2jsonschema(schema_class, spec=spec)
+        for name, schema in jsonschema.get('properties', {}).items():
+            method_operations.setdefault('parameters', []).append(
+                generate_fields_description(
+                    schema=schema,
+                    in_='path',
+                    name=name,
+                    required=name in jsonschema.get('required', []),
+                )
+            )
+
+    if description.input_query:
+        schema_class = spec.schema_class_resolver(
+            spec,
+            description.input_query.wrapper.processor.schema
+        )
+        jsonschema = schema2jsonschema(schema_class, spec=spec)
+        for name, schema in jsonschema.get('properties', {}).items():
+            method_operations.setdefault('parameters', []).append(
+                generate_fields_description(
+                    schema=schema,
+                    in_='query',
+                    name=name,
+                    required=name in jsonschema.get('required', []),
+                )
+            )
+
+    if description.input_files or description.input_forms:
+        method_operations.setdefault('consumes', []).append('multipart/form-data')  # nopep8
+
+    if description.input_files:
+        schema_class = spec.schema_class_resolver(
+            spec,
+            description.input_files.wrapper.processor.schema
+        )
+        jsonschema = schema2jsonschema(schema_class, spec=spec)
+        for name, schema in jsonschema.get('properties', {}).items():
+            method_operations.setdefault('parameters', []).append(
+                generate_fields_description(
+                    schema=schema,
+                    in_='formData',
+                    name=name,
+                    required=name in jsonschema.get('required', []),
+                    type='file',
+                )
+            )
+    if description.input_forms:
+        schema_class = spec.schema_class_resolver(
+            spec,
+            description.input_forms.wrapper.processor.schema
+        )
+        jsonschema = schema2jsonschema(schema_class, spec=spec)
+        for name, schema in jsonschema.get('properties', {}).items():
+            method_operations.setdefault('parameters', []).append(
+                generate_fields_description(
+                    schema=schema,
+                    in_='formData',
+                    name=name,
+                    required=name in jsonschema.get('required', []),
+                )
+            )
+
+    if description.tags:
+        method_operations['tags'] = description.tags
+
+    operations = {
+        route.method.lower(): method_operations,
+    }
+
+    return operations
+
+
+class DocGenerator(object):
+    def get_doc(
+        self,
+        controllers: typing.List[DecoratedController],
+        context: ContextInterface,
+        title: str='',
+        description: str='',
+    ) -> dict:
+        """
+        Generate an OpenApi 2.0 documentation. Th given context will be used
+        to found controllers matching with given DecoratedController.
+        :param controllers: List of DecoratedController to match with context
+        controllers
+        :param context: a context instance
+        :param title: The generated doc title
+        :param description: The generated doc description
+        :return: a apispec documentation dict
+        """
+        spec = APISpec(
+            title=title,
+            info=dict(description=description),
+            version='1.0.0',
+            plugins=(
+                'apispec.ext.marshmallow',
+            ),
+            auto_referencing=True,
+            schema_name_resolver=generate_schema_name
+        )
+
+        schemas = []
+        # parse schemas
+        for controller in controllers:
+            description = controller.description
+
+            if description.input_body:
+                schemas.append(spec.schema_class_resolver(
+                    spec,
+                    description.input_body.wrapper.processor.schema
+                ))
+
+            if description.input_forms:
+                schemas.append(spec.schema_class_resolver(
+                    spec,
+                    description.input_forms.wrapper.processor.schema
+                ))
+
+            if description.output_body:
+                schemas.append(spec.schema_class_resolver(
+                    spec,
+                    description.output_body.wrapper.processor.schema
+                ))
+
+            if description.errors:
+                for error in description.errors:
+                    schemas.append(type(error.wrapper.error_builder))
+
+        for schema in set(schemas):
+            spec.definition(
+                spec.schema_name_resolver(schema),
+                schema=schema
+            )
+
+        # add views
+        # with app.test_request_context():
+        paths = {}
+        for controller in controllers:
+            route = context.find_route(controller)
+            swagger_path = context.get_swagger_path(route.rule)
+
+            operations = bottle_generate_operations(
+                spec,
+                route,
+                controller.description,
+            )
+
+            # TODO BS 20171114: TMP code waiting refact of doc
+            doc_string = controller.reference.get_doc_string()
+            if doc_string:
+                for method in operations.keys():
+                    operations[method]['description'] = doc_string
+
+            path = Path(path=swagger_path, operations=operations)
+
+            if swagger_path in paths:
+                paths[swagger_path].update(path)
+            else:
+                paths[swagger_path] = path
+
+            spec.add_path(path)
+
+        return spec.to_dict()
+
+    def get_doc_yaml(
+        self,
+        controllers: typing.List[DecoratedController],
+        context: ContextInterface,
+        title: str = '',
+        description: str = '',
+    ) -> str:
+        dict_doc = self.get_doc(
+            controllers=controllers,
+            context=context,
+            title=title,
+            description=description,
+        )
+        json_doc = json.dumps(dict_doc)
+
+        # We dump then load with json to use real scalar dict.
+        # If not, yaml dump dict-like objects
+        clean_dict_doc = json.loads(json_doc)
+        return yaml.dump(clean_dict_doc, default_flow_style=False)
+
+    def save_in_file(
+        self,
+        doc_file_path: str,
+        controllers: typing.List[DecoratedController],
+        context: ContextInterface,
+        title: str='',
+        description: str='',
+    ) -> None:
+        doc_yaml = self.get_doc_yaml(
+            controllers=controllers,
+            context=context,
+            title=title,
+            description=description,
+        )
+        with open(doc_file_path, 'w+') as doc_file:
+            doc_file.write(doc_yaml)
+
+
+# TODO BS 20171109: Must take care of already existing definition names
+def generate_schema_name(schema: marshmallow.Schema):
+    """
+    Return best candidate name for one schema cls or instance.
+    :param schema: instance or cls schema
+    :return: best schema name
+    """
+    if not isinstance(schema, type):
+        schema = type(schema)
+
+    if getattr(schema, '_schema_name', None):
+        if schema.opts.exclude:
+            schema_name = "{}_without".format(schema.__name__)
+            for elem in sorted(schema.opts.exclude):
+                schema_name="{}_{}".format(schema_name, elem)
+        else:
+            schema_name = schema._schema_name
+    else:
+        schema_name = schema.__name__
+
+    return schema_name
