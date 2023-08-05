0bf303581339011da17d4baeb540893810387ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:27 +0000

    Add package pyskyq/0.6.1/pyskyq-0.6.1-py2.py3-none-any.whl

---
+++ packages/pyskyq/pyskyq-0.6.1-py2.py3-none-any.whl/pyskyq/epg.py
@@ -0,0 +1,229 @@
+"""This module implements the EPG class."""
+
+import json
+import logging
+from typing import Any
+
+import asks
+import trio
+
+from .channel import (Channel, _ChannelJSONEncoder, channel_from_json,
+                      channel_from_skyq_service, merge_channels)
+from .constants import (REST_PORT, REST_SERVICE_DETAIL_URL_PREFIX,
+                        REST_SERVICES_URL)
+from .programme import Programme
+from .xmltvlisting import XMLTVListing
+
+LOGGER = logging.getLogger(__name__)
+
+asks.init('trio')
+
+class EPG:
+    """The top-level class for all EPG data and functions.
+
+    EPG implements access to any data and operations related to the SkyQ box's
+    Electronic Programme Guide through a REST endpoint exposed on the box.
+    Specifically, it fetches both summary and detail channel information from
+    two different endpoints from the box and aggregates this data into a list of
+    :class:`~.channel.Channel` objects.
+
+    This channel data can then be *augmented* with data from an XMLTV feed site
+    like http://www.xmltv.co.uk/ which provides an XML file which follows the ``xmltv``
+    DTD.
+
+    It also uses the ``xmltv`` file to load **listings** data to enable the
+    querying of programmes that are scheduled on the channels.
+
+    Args:
+        host (str): String with resolvable hostname or IPv4 address to SkyQ box.
+        rest_port (int): Defaults to the SkyQ REST port which is 9006.
+
+    Attributes:
+        host (str): Hostname or IPv4 address of SkyQ Box.
+        rest_port (int): Port number to use to connect to the REST HTTP server.
+            Defaults to the standard port used by SkyQ boxes which is 9006.
+
+
+    """
+
+    def __init__(self,
+                 host: str,
+                 *,
+                 rest_port: int = REST_PORT,
+                 ) -> None:
+        """Initialise Sky EPG Object."""
+        self.host: str = host
+        self.rest_port: int = rest_port
+        self._channels: list = []
+        LOGGER.debug(f"Initialised EPG object using SkyQ box={self.host}")
+
+    def __repr__(self):
+        """Print a human-friendly representation of this object."""
+        return f"<EPG: host={self.host}, rest_port={self.rest_port}, " + \
+            f"len(_channels)={len(self._channels)}, len(_xmltv_urls)={len(self._channels)}>"
+
+    def channels_loaded(self) -> bool:
+        """Return whether channels have been loaded.
+
+        Returns:
+            bool: ``True`` if channels have been loaded.
+
+        """
+        return bool(self._channels)
+
+    async def load_skyq_channel_data(self) -> None:
+        """Load channel data into channel property.
+
+        This method fetches the channel list from ``/as/services`` endpoint and
+        loads the fetched channels into this ``EPG`` object. It then
+        concurrently fetches each channel's detail info from
+        ``/as/services/details/<sid>`` and adds that to the channels.
+
+        Returns:
+            None
+
+        """
+        async def _load_chan_detail(detail_url: str, channel: Channel) -> None:
+            """Async function to fetch the details of each channel."""
+            LOGGER.debug(f'Fetching channel details from {detail_url}')
+            detail_payload_json = await sess.get(detail_url)
+            LOGGER.debug(f'Got channel details from {detail_url}')
+            detail_payload = detail_payload_json.json()
+            LOGGER.debug(f'Parsed JSON from {detail_url}')
+            channel.load_skyq_detail_data(detail_payload)
+            LOGGER.debug(f'Merged {detail_url} into {channel}...')
+            self._channels.append(channel)
+            LOGGER.debug(f'Added {channel} to EPG')
+
+        url = f'http://{self.host}:{self.rest_port}{REST_SERVICES_URL}'
+        LOGGER.debug(f'Fetching channel list from {url}')
+        sess = asks.Session(connections=50)
+        LOGGER.debug(f'About to fetch channel list from sky box.')
+        chan_payload_json = await sess.get(url)
+        LOGGER.debug(f'Got channel list from sky box.')
+        chan_payload = chan_payload_json.json()
+        LOGGER.debug(f'Parsed channel list JSON payload.')
+
+        with trio.move_on_after(90): # 90 sec timeout  #TODO parametrise this.
+            async with trio.open_nursery() as nursery:
+                LOGGER.debug('Started nursery to collect channel details.')
+                for single_channel_payload in chan_payload['services']:
+                    LOGGER.debug(f'Processing {single_channel_payload}')
+                    channel = channel_from_skyq_service(single_channel_payload)
+                    sid = channel.sid
+                    detail_url = f'http://{self.host}:{self.rest_port}' + \
+                        f'{REST_SERVICE_DETAIL_URL_PREFIX}{sid}'
+                    nursery.start_soon(_load_chan_detail, detail_url, channel)
+
+
+    def as_json(self) -> str:
+        """Return the channel and programme data as JSON.
+
+        Returns:
+            str: A JSON representation of this EPG.
+
+        """
+        return json.dumps(self._channels, cls=_ChannelJSONEncoder, indent=4)
+
+    def from_json(self, json_: str) -> None:
+        """Load channel and programme data from JSON.
+
+        Args:
+            json_(str): A string of JSON text.
+
+        """
+        payload = json.loads(json_)
+        new_chans: list = []
+
+        for item in payload:
+            if item['__type__'] == "__channel__":
+                new_chans.append(channel_from_json(json.dumps(item)))
+            # TODO: add jobs, programmes, etc
+        self._channels = new_chans
+
+
+
+    def get_channel_by_sid(self,
+                           sid: Any
+                           ) -> Channel:
+        """Get channel data by SkyQ service id.
+
+        This method returns a :class:`~.channel.Channel` object when
+        passed in a channel ``sid``.
+
+        Args:
+            sid: The SkyQ service id of the channel
+        Returns:
+            :class:`~.channel.Channel`: The channel if found.
+
+        Raises:
+            ValueError: If the channel is not found or the channel
+                list is empty.
+
+        """
+        sid = str(sid)
+        if not self.channels_loaded():
+            raise ValueError("No channels loaded.")
+
+        for chan in self._channels:
+            if chan.sid == sid:
+                return chan
+        raise ValueError(f"Sid:{sid} not found.")
+
+
+    def apply_XMLTVListing(self, listing: XMLTVListing) -> None:
+        """Merge listing data into the EPG channels data structure by channel name.
+
+        This method takes an XMLTV listing object and, for every channel already loaded
+        in the EPG it augments that channel's data with the data obtained from the XMLTV
+        listing. This could provide additional descriptive channel data, the URL to the
+        channel's logo and similar extra stuff.
+
+        It then adds Programme objects to the Channel's Programme data structure.
+
+        Args:
+            listing(XMLTVListing): a :class:`~.xmltvlisting.XMLTVListing` object to
+                merge to the EPG.
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: Raised if the XMLTVListing file cannot be found.
+
+        """
+        if not listing.downloaded:
+            raise ValueError("No XMLTVListing file found.")
+
+        # There is no guarantee that the XMLTV file will serve elements in a particular order.
+        # We want to load and create _all_ the channels before we load the programmes so we use
+        # a temp var to hold the programme items which we then add to the Channels _afterwards_.
+
+        prog_temp = []
+        for xmltv_item in listing.parse():
+            if isinstance(xmltv_item, Channel):
+                sky_channel_names = [chan.name.lower() for chan in self._channels]
+                if xmltv_item.xmltv_display_name.lower() in sky_channel_names:
+                    idx = sky_channel_names.index(xmltv_item.xmltv_display_name.lower())
+                    new_chan = merge_channels(self._channels[idx], xmltv_item)
+                    LOGGER.debug(f'New channel:{new_chan}.')
+                    self._channels.append(new_chan)
+                    LOGGER.debug(f'Replaced {self._channels[idx]} with {new_chan} in EPG.')
+                    del self._channels[idx]
+                    continue
+            if isinstance(xmltv_item, Programme):
+                prog_temp.append(xmltv_item)
+                continue
+
+        # We rely on the Programme's hash being the same as any previous
+        # instance of this programme that is being added to the programmes
+        # SortedSet as the __hash__() function relies _only_ on the programm's
+        # Title, StartTime and XMLChannelID data. In this way, if we apply a
+        # newer, richer XMLTV listing to an existing one, by adding this item to
+        # the SortedSet we will _replace_ any previously present Programme if
+        # the hash of the newer one is the same as an existing one, even if the
+        # data is newer/better. This allows us to keep applying XMLTV Listing
+        # over and over without risk of duplication.
+        for chan in self._channels:
+            chan_progs = [prog for prog in prog_temp if prog.channel_xml_id == chan.xmltv_id]
+            chan.programmes.update(chan_progs)
---
+++ packages/pyskyq/pyskyq-0.6.1-py2.py3-none-any.whl/pyskyq/xmltvlisting.py
@@ -0,0 +1,285 @@
+"""This module implements the XMLTVListing listing class."""
+#pylint: disable=line-too-long
+import hashlib
+import logging
+import shutil
+import zlib
+from collections.abc import Hashable
+from datetime import datetime
+from functools import partial
+from os import PathLike
+from pathlib import Path
+from typing import IO, Any, Iterator, Optional, Tuple, Union
+from xml.etree.ElementTree import Element, iterparse
+
+import asks
+import trio
+from yarl import URL
+
+from .channel import channel_from_xmltv_list, Channel
+from .programme import programme_from_xmltv_list, Programme
+from .utils import parse_http_date
+
+LOGGER = logging.getLogger(__name__)
+asks.init('trio')
+
+
+class XMLTVListing(Hashable):  # pylint: disable=too-many-instance-attributes
+    """Hold the data and functions required to obtain an XMLTVListing.
+
+    Currently it only supports the XML TV Listings format which holds
+    Channel and Programme scheduling information. See the dtd_ for details.
+
+    .. _dtd: https://github.com/AlekSi/xmltv/blob/master/xmltv.dtd
+
+    Note:
+
+        The :class:`~.channel.Channel` class provides the **primary**
+        interface to channel data through the :class:`~.epg.EPG` class.
+
+        This class provides the means to download and parse XML data to do with
+        channels, but more importantly, programming schedules. While it can be used
+        stand-alone, it is most effective when injected into the
+        :class:`~.epg.EPG` object using
+        :meth:`.epg.EPG.apply_XMLTVListing()`.
+
+    """
+
+    def __init__(self,
+                 url: URL,
+                 path: PathLike = Path('.epg_data'),
+                 ) -> None:
+        """Instantiate the object with the URL to fetch."""
+        self._url: URL
+        self._path: Path
+
+        if not isinstance(url, URL):
+            self._url = URL(url)
+        else:
+            self._url = url
+
+        # Validate path
+        if not isinstance(path, Path) and not isinstance(path, str):
+            raise TypeError('path must be a string or Path object.')
+
+        # coerce it to a Path if it's a string
+        if isinstance(path, str):
+            self._path = Path(path)
+        else:
+            self._path = path
+
+        if not self._path.is_dir():
+            self._path.mkdir()
+
+        str_binary = str(self._url.human_repr()).encode('utf8')
+        hashobj = hashlib.sha256(str_binary)
+        self._hash = int.from_bytes(hashobj.digest(), 'big')
+        self._filename = f'{hashobj.hexdigest()}.xml'
+        self._full_path = self._path.joinpath(self._filename)
+        self._last_modified: Optional[datetime] = None
+        self._downloaded: bool = False
+        self._downloading: bool = False
+
+        LOGGER.debug(f'XMLTVListing initialised: {self}')
+
+    def __hash__(self) -> int:
+        """Define hash function for a Hashable object."""
+        return self._hash
+
+    def __eq__(self, other) -> bool:
+        """Define equality test for a Hashable object."""
+        # relying on lazy boolean evaluation here.
+        return isinstance(other, XMLTVListing) and hash(other._url) == hash(self._url)  # pylint: disable=protected-access
+
+    def __repr__(self):
+        """Print a human-friendly representation of this object."""
+        return f"<XMLTVListing: url='{self._url}', path='{self._path}', filename='{self._filename}'>"
+
+    @property
+    def last_modified(self):
+        """Return the last modified date from the HTTP header of the last download.
+
+        Returns the date and time when the data was last modified. Taken directly
+        from the ``HTTP-Header``.
+
+        If the header was not provided, it returns ``None``.
+
+        Returns:
+            datetime: A :py:class:`datetime.datetime` object or ``None``.
+
+        """
+        return self._last_modified
+
+    @property
+    def url(self) -> URL:
+        """Return the url of this XMLTVListing.
+
+        Returns:
+            :py:class:`yarl.URL`
+
+        """
+        return self._url
+
+    @property
+    def downloaded(self) -> bool:
+        """Return the status of XMLTV file download.
+
+        Returns:
+            bool: ``True`` if the file was downloaded successfully
+
+        """
+        return self._downloaded
+
+    @property
+    def downloading(self) -> bool:
+        """Return the status of XMLTV file download.
+
+        Returns:
+            bool: ``True`` if the file is currently being downlaoded
+
+        """
+        return self._downloading
+
+    @property
+    def file_path(self) -> Path:
+        """Return the full file path of this listing's XML file.
+
+        Returns:
+            :py:class:`pathlib.Path`: A `Path` to the location of the
+                XML file (whether it has yet been :meth:`fetch`'ed or not).
+
+        """
+        return self._full_path
+
+    # TODO -- add error handling for
+    # -- HTTP headers missing
+    # -- timeouts
+    # -- badURL, etc, etc.
+    # TODO -- add retry support
+
+    async def fetch(self) -> None:
+        """Fetch the XMLTVListing file.
+
+        This async method will download the (XML) file specified by the URL passed
+        at instantiation.
+
+        If the server does not support streaming downloads the method will will
+        fall back to a more memory-intensive, vanilla HTTP download.
+
+        Args:
+            timeout (int): timeout in seconds for the HTTP session. Defaults to
+                60 seconds.
+            range_size (int): the size, in bytes, of each chunk. Defaults to
+                256k (256*1024 bytes).
+
+        Returns:
+            None
+
+        """
+        LOGGER.debug(f'Fetch({self}) call started.')
+        self._downloading = True
+        newfile = self._full_path.with_suffix('.tmp')
+
+
+        # We support gzip encoding as www.xmltv.co.uk sends stuff gzipped.
+        # the zlib decompressiion object supports streaming decompression which
+        # why we use it.
+
+        # We should probably support more encoding algorithms at some point.
+
+        decompression_obj = zlib.decompressobj(wbits=16 + zlib.MAX_WBITS)
+        compressed_payload = True  # assume payload is compressed first.
+
+        async def chunk_processor(decomp_obj, bytechunk):
+            nonlocal compressed_payload
+            async with await trio.open_file(newfile, 'ab') as output_file:
+                if compressed_payload:
+                    try:
+                        LOGGER.debug(f'Got zipped chunk. size = {len(bytechunk)}. Unzipping...')
+                        await output_file.write(decomp_obj.decompress(bytechunk))
+                        LOGGER.debug(f'Wrote unzipped chunk. size = {len(bytechunk)}')
+                    except zlib.error:
+                        await output_file.write(bytechunk)
+                        LOGGER.debug(f'Wrote chunk. size = {len(bytechunk)}')
+                        compressed_payload = False  # it wasn't compressed, so fall back
+                else:
+                    await output_file.write(bytechunk)
+                    LOGGER.debug(f'Wrote chunk. size = {len(bytechunk)}')
+
+
+        resp = await asks.get(str(self._url),
+                              headers={'Accept-Encoding': 'gzip'},
+                              # callback takes bytes only
+                              callback=partial(chunk_processor, decompression_obj)
+                              )
+
+        LOGGER.debug(f'Got headers = {resp.headers}')
+
+        # h11 makes header keys lowercase so we can reply on this
+        if resp.headers.get("last-modified"):
+            self._last_modified = parse_http_date(resp.headers.get("last-modified"))
+            LOGGER.debug(f'Content last modified on: {resp.headers.get("last-modified")}.')
+
+        shutil.move(newfile, self._full_path)
+        self._downloading = False
+        self._downloaded = True
+        LOGGER.debug(f'Fetch finished on {self}')
+
+
+    def parse(self) -> Iterator[Union[Channel, Programme]]:
+        """Parse the XMLTVListing XML file and create an iterator over the data in it.
+
+        Yields:
+            (Any): Either a  :class:`~pyskyq.channel.Channel` or a
+                :class:`~pyskyq.programme.Programme` object is yielded.
+
+        """
+        if not self.downloaded and not self.downloading:
+            raise OSError('File not downloaded, or download is currently in flight.')
+        else:
+            LOGGER.debug(f'in parse_channels. file = {self.file_path}')
+            for xml_type, xml_item in _xml_parse_and_remove(self._full_path): # type: ignore
+                if xml_type == 'channel':
+                    LOGGER.debug('yielding channel...')
+                    yield channel_from_xmltv_list(xml_item)
+                if xml_type == 'programme':
+                    LOGGER.debug('yielding programme...')
+                    yield programme_from_xmltv_list(xml_item)
+
+# TODO: add error checking for XML errors
+# TODO: see if this can be made async.
+def _xml_parse_and_remove(filename: Union[str, bytes, int, IO[Any]],
+                          ) -> Iterator[Tuple[str, Element]]:
+    """Incrementally load and parse an XML file.
+
+    Stolen from Python Cookbook 3rd edition, section 6.4 with credit to the book's authors.
+
+    Args:
+        filename(Union[str, bytes, int, IO[Any]]): a file-like object.
+        path(str): The XML element to parse
+
+    Yields:
+        Element: A Parsed XML element.
+
+    """
+    doc = iterparse(filename, ('start', 'end'))
+    # skip the root element
+    next(doc)  # pylint: disable=stop-iteration-return
+    tag_stack = []
+    elem_stack = []
+    for event, elem in doc:
+        if event == 'start':
+            LOGGER.debug(f'Parsing XML element: {elem.tag}')
+            tag_stack.append(elem.tag)
+            elem_stack.append(elem)
+        else:  # event == 'end'
+            if tag_stack == ['channel']:
+                yield 'channel', elem
+                elem.clear()
+            elif tag_stack == ['programme']:
+                yield 'programme', elem
+            try:
+                tag_stack.pop()
+                elem_stack.pop()
+            except IndexError:
+                pass
