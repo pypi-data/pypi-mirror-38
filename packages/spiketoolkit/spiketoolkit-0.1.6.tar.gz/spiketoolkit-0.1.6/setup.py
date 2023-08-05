c4c9da71774165dca10a92d2fd1b75bc2047f84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:38 +0000

    Add package spiketoolkit/0.1.6/spiketoolkit-0.1.6.tar.gz

---
+++ packages/spiketoolkit/spiketoolkit-0.1.6.tar.gz/spiketoolkit-0.1.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+pkg_name="spiketoolkit"
+
+setup(
+    name=pkg_name,
+    version="0.1.6",
+    author="Cole Hurwitz, Jeremy Magland, Alessio Paolo Buccino, Matthias Hennig",
+    author_email="alessiop.buccino@gmail.com",
+    description="Python toolkit for analysis, visualization, and comparison of spike sorting output",
+    url="https://github.com/alejoe91/spiketoolkit",
+    packages=find_packages(),
+    package_data={},
+    install_requires=[
+        'numpy',
+        'spikeinterface',
+        'scikit-learn'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    )
+)
