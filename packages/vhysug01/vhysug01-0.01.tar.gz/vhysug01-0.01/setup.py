bfe369f901685aae1212379072abed5569f051e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:20 +0000

    Add package vhysug01/0.01/vhysug01-0.01.tar.gz

---
+++ packages/vhysug01/vhysug01-0.01.tar.gz/vhysug01-0.01/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name="vhysug01",
+    version=0.01,
+    author="vhysug01",
+    author_email="vhysug@163.com",
+    maintainer="vhysug",
+    maintainer_email="vhysug@qq.com",
+    license='Apache License v2',
+    package_data={'': ['*.*']},
+    url="https://vhysug.github.io/",
+    platforms=["all"],
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
