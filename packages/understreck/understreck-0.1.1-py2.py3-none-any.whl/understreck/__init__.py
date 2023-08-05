b59fec023916c50dcf5f5412372093c5d6bdd6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:19 +0000

    Add package understreck/0.1.1/understreck-0.1.1-py2.py3-none-any.whl

---
+++ packages/understreck/understreck-0.1.1-py2.py3-none-any.whl/understreck/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Understreck."""
+
+__author__ = """Carl-Fredrik Arvidson"""
+__email__ = "carl-fredrik@arvidson.io"
+__version__ = "__version__ = '0.1.1'"
+
+from .nested_get import nested_get
