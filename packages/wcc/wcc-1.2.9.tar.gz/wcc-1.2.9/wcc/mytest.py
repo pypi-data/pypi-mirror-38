1e1931fda54fe8a347f4a78c9728327ae2ec461f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:53 +0000

    Add package wcc/1.2.9/wcc-1.2.9.tar.gz

---
+++ packages/wcc/wcc-1.2.9.tar.gz/wcc-1.2.9/wcc/mytest.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+import req 
+
+
+
+def main():
+    # url = 'http://www.baidu.com'
+    url = 'http://stockpage.10jqka.com.cn/000063/funds'
+    url2 = 'https://zhuanlan.zhihu.com/p/47777088'
+    url3 = 'http://www.ip138.com/'
+    url4 = 'https://ip.cn/'
+    resp_text = req.getpage(url, use_proxy=True, use_browser=True, wait=3)
+    print (resp_text)
+
+
+if __name__ == "__main__":
+    main()
