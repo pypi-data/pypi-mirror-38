57e77ec3e95141ee5611092aa774fe5d458ca0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package typegql/0.1.2/typegql-0.1.2.tar.gz

---
+++ packages/typegql/typegql-0.1.2.tar.gz/typegql-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['typegql', 'typegql.core']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graphql-core-next>=1.0,<2.0', 'uvloop>=0.11.2,<0.12.0']
+
+setup_kwargs = {
+    'name': 'typegql',
+    'version': '0.1.2',
+    'description': 'A Python GraphQL library that makes use of type hinting and concurrency support with the new async/await syntax.',
+    'long_description': None,
+    'author': 'Ciprian Tarta',
+    'author_email': 'ciprian@cipriantarta.ro',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
