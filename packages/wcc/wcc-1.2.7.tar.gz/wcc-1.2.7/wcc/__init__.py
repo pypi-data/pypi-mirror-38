9650f371a96c9a23accf555b7e6b35b40b6cc31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:53 +0000

    Add package wcc/1.2.7/wcc-1.2.7.tar.gz

---
+++ packages/wcc/wcc-1.2.7.tar.gz/wcc-1.2.7/wcc/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.2.7'
+#from . import models, exceptions
+from . import osskey
+from . import wcc
+from .wcc import Wcc
+from . import ArtDB
+#从本目录下api.py里import Service和Bucket这个类
+#from .api import Service, Bucket
+from .osskey import Osskey
+from .utils import *
+from .ArtDB import * 
+from .messages import getpage
---
+++ packages/wcc/wcc-1.2.7.tar.gz/wcc-1.2.7/wcc/proxy.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+import requests
+
+def get_proxy():
+    """
+    从虎头代理获取一个代理ip和端口
+    :return:一个元组(ip, port), 其中ip是个字符串，port是个整型数字
+    """
+    url = 'http://257656416044421866.standard.hutoudaili.com/?num=1&area_type=1&scheme=1'
+    response = requests.get(url).text
+    iport= response.split(":")
+    ip = str(iport[0])
+    port = int(iport[1])
+    return ip, port
+
+def get_proxy_other():
+    pro = requests.get('http://dynamic.goubanjia.com/dynamic/get/2bd6fa04ff7a708cdbcbaab207602c6e.html?sep=3')
+    ip = str(pro.text.split(":")[0])
+    port = int(pro.text.split(":")[1])
+    return ip, port
+
+
+def main():
+    ip, port = get_proxy()
+    print ("ip:" + ip + ", port:" + str(port))
+    
+
+if __name__ == "__main__":
+    main()
+
