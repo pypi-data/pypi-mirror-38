63cc8e950fb3043536b4a2ea998c6ef6e8c6d6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:42 +0000

    Add package dogma/0.0.8/dogma-0.0.8.tar.gz

---
+++ packages/dogma/dogma-0.0.8.tar.gz/dogma-0.0.8/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from codecs import open
+import io
+from os import path
+
+__version__ = '0.0.8'
+
+here = path.abspath(path.dirname(__file__))
+
+
+DESCRIPTION = 'Collection of object-oriented entities related by the central dogma of biology.',
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+with io.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '')
+                    for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='dogma',
+    version=__version__,
+    description=DESCRIPTION,
+    long_description=readme,
+    url='https://github.com/griffinclausen/dogma',
+    license='MIT',
+    author='Griffin Clausen',
+    author_email='dogma.maintainer@gmail.com',
+    keywords='',
+    entry_points='''
+        [console_scripts]
+        genetic_code=dogma.genetic_codes:test
+    ''',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    install_requires=[
+        'pandas'
+        ],
+    dependency_links=dependency_links
+)
