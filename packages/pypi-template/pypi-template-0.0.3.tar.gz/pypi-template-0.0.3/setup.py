1867fd880de7d6571223de9475ba0d3973036ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:39 +0000

    Add package pypi-template/0.0.3/pypi-template-0.0.3.tar.gz

---
+++ packages/pypi-template/pypi-template-0.0.3.tar.gz/pypi-template-0.0.3/setup.py
@@ -0,0 +1,63 @@
+import os
+import re
+import setuptools
+
+NAME             = "pypi-template"
+AUTHOR           = "Christophe VG"
+AUTHOR_EMAIL     = "contact@christophe.vg"
+DESCRIPTION      = "PyPi template repository."
+LICENSE          = "MIT"
+KEYWORDS         = "pypi template"
+URL              = "https://github.com/christophevg/" + NAME
+README           = ".github/README.md"
+CLASSIFIERS      = [
+  "Environment :: Console",
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Developers",
+  "Intended Audience :: System Administrators",
+  "Topic :: Software Development",
+  "License :: OSI Approved :: MIT License",
+  "Programming Language :: Python",
+  "Programming Language :: Python :: 2",
+  "Programming Language :: Python :: 2.6",
+  "Programming Language :: Python :: 2.7",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+]
+INSTALL_REQUIRES = []
+ENTRY_POINTS     = {}
+SCRIPTS = [
+  'bin/pypi-template'
+]
+
+HERE = os.path.dirname(__file__)
+
+def read(file):
+  with open(os.path.join(HERE, file), "r") as fh:
+    return fh.read()
+
+VERSION = re.search(
+  r'^__version__ = [\'"]([^\'"]*)[\'"]',
+  read(NAME.replace("-", "_") + "/__init__.py")
+).group(1)
+
+LONG_DESCRIPTION = read(README)
+
+if __name__ == "__main__":
+  setuptools.setup(name=NAME,
+        version=VERSION,
+        packages=setuptools.find_packages(),
+        author=AUTHOR,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        license=LICENSE,
+        keywords=KEYWORDS,
+        url=URL,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        entry_points=ENTRY_POINTS,
+        scripts=SCRIPTS)
