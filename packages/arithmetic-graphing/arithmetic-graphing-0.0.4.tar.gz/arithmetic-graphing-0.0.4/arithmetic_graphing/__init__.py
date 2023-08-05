c9cb43531ed6830177bde3c00a7662ca13601106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:45 +0000

    Add package arithmetic-graphing/0.0.4/arithmetic-graphing-0.0.4.tar.gz

---
+++ packages/arithmetic-graphing/arithmetic-graphing-0.0.4.tar.gz/arithmetic-graphing-0.0.4/arithmetic_graphing/__init__.py
@@ -0,0 +1,4 @@
+def Slope(pos, pos2):
+    y = (pos2[1] - pos[1])
+    x = (pos2[0] - pos2[1])
+    print(y/x)
---
+++ packages/arithmetic-graphing/arithmetic-graphing-0.0.4.tar.gz/arithmetic-graphing-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import *
+
+setup(name='arithmetic-graphing',
+    version='0.0.4',
+    description='A small arithmetic module for functions of graphing',
+    author='TheOnlyWalrus',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+)
