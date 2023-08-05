dbf2928420fdf830424d6b8c0257791cec1dfdca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:10 +0000

    Add package xacc/0.1.11/xacc-0.1.11-cp34-cp34m-manylinux1_x86_64.whl

---
+++ packages/xacc/xacc-0.1.11-cp34-cp34m-manylinux1_x86_64.whl/xacc/include/quantum/gate/X.hpp
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2017 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompanies this
+ * distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ *License is available at https://eclipse.org/org/documents/edl-v10.php
+ *
+ * Contributors:
+ *   Alexander J. McCaskey - initial API and implementation
+ *******************************************************************************/
+#ifndef QUANTUM_GATE_IR_X_HPP_
+#define QUANTUM_GATE_IR_X_HPP_
+
+#include "GateInstruction.hpp"
+
+namespace xacc {
+namespace quantum {
+
+/**
+ *
+ */
+class X : public virtual GateInstruction {
+public:
+  X() : GateInstruction("X") {}
+
+  X(std::vector<int> qbit) : GateInstruction("X", qbit) {}
+
+  X(int qbit) : X(std::vector<int>{qbit}) {}
+
+  virtual std::shared_ptr<GateInstruction> clone() {
+    return std::make_shared<X>();
+  }
+  /**
+   * Return the description of this instance
+   * @return description The description of this object.
+   */
+  virtual const std::string description() const { return ""; }
+
+  DEFINE_VISITABLE()
+};
+
+} // namespace quantum
+} // namespace xacc
+
+#endif
