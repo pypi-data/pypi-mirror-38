f1b2abca9e2b814bd32a738499db041b6f94afd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:23 +0000

    Add package calyxhealth/0.1/calyxhealth-0.1.tar.gz

---
+++ packages/calyxhealth/calyxhealth-0.1.tar.gz/calyxhealth-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='calyxhealth',
+      version='0.1',
+      description='library for CalyxHealth.',
+      author='calyx',
+      author_email='admin@calyxhealth.com',
+      packages=['calyxhealth'],
+      license='',
+      )
