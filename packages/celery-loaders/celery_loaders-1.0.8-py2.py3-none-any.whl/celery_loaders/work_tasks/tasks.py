fdcac59bb4c4dcec47bf8627d5d5422811f2feb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:59 +0000

    Add package celery-loaders/1.0.8/celery_loaders-1.0.8-py2.py3-none-any.whl

---
+++ packages/celery-loaders/celery_loaders-1.0.8-py2.py3-none-any.whl/celery_loaders/work_tasks/tasks.py
@@ -0,0 +1,41 @@
+import uuid
+from celery.task import task
+from spylunking.log.setup_logging import build_colorized_logger
+from celery_loaders.work_tasks.custom_task import CustomTask
+
+
+log = build_colorized_logger(
+    name='do_some_work')
+
+
+@task(
+    bind=True,
+    base=CustomTask,
+    queue="do_some_work")
+def do_some_work(
+        self,
+        work_dict):
+    """do_some_work
+
+    :param work_dict: dictionary for key/values
+    """
+
+    label = "do_some_work"
+
+    log.info(("task - {} - start "
+              "work_dict={}")
+             .format(label,
+                     work_dict))
+
+    ret_data = {
+        "job_results": ("some response key={}").format(
+                            str(uuid.uuid4()))
+    }
+
+    log.info(("task - {} - result={} done")
+             .format(
+                 ret_data,
+                 label))
+
+    return ret_data
+# end of do_some_work
