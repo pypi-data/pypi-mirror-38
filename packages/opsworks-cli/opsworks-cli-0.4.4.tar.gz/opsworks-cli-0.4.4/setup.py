e1ff8d407db1803580027bac30dd2d36c0e64708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package opsworks-cli/0.4.4/opsworks-cli-0.4.4.tar.gz

---
+++ packages/opsworks-cli/opsworks-cli-0.4.4.tar.gz/opsworks-cli-0.4.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+from io import open
+import modules
+
+setup(
+    name='opsworks-cli',
+    description='A simple python module to work with aws opsworks',
+    url='https://github.com/chaturanga50/opsworks-cli',
+    author='Chathuranga Abeyrathna',
+    author_email='chaturanga50@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    version='0.4.4',
+    install_requires=[
+        'boto3'
+    ],
+    scripts=['opsworks-cli']
+)
