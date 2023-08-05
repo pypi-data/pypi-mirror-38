bf0e9b5a3aaf6cda43867206eb0f3acf2ce91f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package raiden/0.16.0/raiden-0.16.0.tar.gz

---
+++ packages/raiden/raiden-0.16.0.tar.gz/raiden-0.16.0/.travis/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ -z ${BLOCKCHAIN_TYPE} ] && [[ ${TRAVIS_EVENT_TYPE} == "cron" ]]; then
+    BLOCKCHAIN_TYPE="geth"
+elif [ -z ${BLOCKCHAIN_TYPE} ]; then
+    # FIXME: change to "tester" once the test failures are fixed
+    BLOCKCHAIN_TYPE="geth"
+fi
+
+py.test \
+    -Wd \
+    --travis-fold=always \
+    --log-config='raiden:DEBUG' \
+    --random \
+    -v \
+    --showlocals \
+    --blockchain-type=${BLOCKCHAIN_TYPE} \
+    ${TRANSPORT_OPTIONS} \
+    ${TEST}
