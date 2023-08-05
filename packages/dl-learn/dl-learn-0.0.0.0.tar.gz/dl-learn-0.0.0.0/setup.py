5035cf0a01b2b4147fbe3dae7e09e80eda350522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:43 +0000

    Add package dl-learn/0.0.0.0/dl-learn-0.0.0.0.tar.gz

---
+++ packages/dl-learn/dl-learn-0.0.0.0.tar.gz/dl-learn-0.0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup
+
+setup(
+        name="dl-learn",
+        version="0.0.0.0",
+        author="Smirk",
+        author_email="smirk.cao@gmail.com",
+        url="http://www.smirk.info",
+        description="",
+        packages=["dl-learn"],
+        install_requires=[],
+        )
