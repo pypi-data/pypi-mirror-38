b5cd4199d17c651c4232ffc7a625bcc136bf4cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:01 +0000

    Add package statuscheck/0.6.0/statuscheck-0.6.0-py2.py3-none-any.whl

---
+++ packages/statuscheck/statuscheck-0.6.0-py2.py3-none-any.whl/statuscheck/services/__init__.py
@@ -0,0 +1,24 @@
+SERVICES = [
+    'atlassian',
+    'closeio',
+    'cloudamqp',
+    'discord',
+    'docker',
+    'github',
+    'gitlab',
+    'heroku',
+    'npm',
+    'pagerduty',
+    'pypi',
+    'redislabs',
+    'salesforce',
+    'sentry',
+    'servicemax',
+    'signal',
+    'slack',
+    'sparkpost',
+    'travis',
+    'twilio',
+]
+
+__all__ = SERVICES
