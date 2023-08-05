5a505ea81d2ce6bd43596391f3f18367b7158236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package meltano/0.1.1/meltano-0.1.1.tar.gz

---
+++ packages/meltano/meltano-0.1.1.tar.gz/meltano-0.1.1/src/meltano/core/project.py
@@ -0,0 +1,86 @@
+import os
+import yaml
+import logging
+from pathlib import Path
+from typing import Union, Dict
+from contextlib import contextmanager
+
+from .plugin import Plugin
+
+
+class Project:
+    """
+    Represent the current Meltano project from a file-system
+    perspective.
+    """
+
+    _meltano = {}
+
+    def __init__(self, root: Union[Path, str]):
+        self.root = Path(root)
+
+    def chdir(self):
+        os.chdir(self.root)
+        self.root = Path(".")
+
+    @classmethod
+    def find(self, from_dir: Union[Path, str] = ".", chdir=True):
+        """
+        Recursively search for a `meltano.yml` file. Once found,
+        return a `Project` correct dir.
+        """
+        cwd = os.getcwd()
+
+        while not Project(".").meltanofile.exists():
+            if os.getcwd() == "/":
+                raise Exception("Cannot go any further.")
+
+            os.chdir("..")
+
+        if not chdir:
+            os.chdir(cwd)
+
+        return Project(os.getcwd())
+
+    @property
+    def meltano(self) -> Dict:
+        """Return a copy of the current meltano config"""
+        if not self._meltano:
+            self._meltano = yaml.load(self.meltanofile.open()) or {}
+        return self._meltano.copy()
+
+    @contextmanager
+    def meltano_update(self):
+        """
+        Yield the current meltano configuration and update the meltanofile
+        if the context ends gracefully.
+        """
+        try:
+            meltano_update = self.meltano
+            yield meltano_update
+
+            # save it
+            with self.meltanofile.open("w") as meltanofile:
+                meltanofile.write(yaml.dump(meltano_update, default_flow_style=False))
+
+            # update the cache
+            self._meltano = meltano_update
+        except Exception as err:
+            logging.error(f"Could not update meltano.yml: {err}")
+            raise
+
+    @property
+    def meltanofile(self):
+        return self.root.joinpath("meltano.yml")
+
+    def meltano_dir(self, *joinpaths):
+        return self.root.joinpath(".meltano", *joinpaths)
+
+    def venvs_dir(self, *prefixes):
+        return self.meltano_dir(*prefixes, "venv")
+
+    def run_dir(self, *joinpaths):
+        return self.meltano_dir("run", *joinpaths)
+
+    def plugin_dir(self, plugin: Plugin, *joinpaths):
+        return self.meltano_dir(plugin.type, plugin.name, *joinpaths)
---
+++ packages/meltano/meltano-0.1.1.tar.gz/meltano-0.1.1/src/meltano/core/runner/singer.py
@@ -0,0 +1,132 @@
+import subprocess
+import json
+import logging
+from pathlib import Path
+
+from . import Runner
+from meltano.core.job import Job, JobFinder
+from meltano.core.project import Project
+from meltano.core.plugin_invoker import PluginInvoker
+from meltano.core.config_service import ConfigService
+from meltano.core.plugin.singer import SingerTap, SingerTarget, PluginType
+from meltano.core.utils import file_has_data
+
+
+class SingerRunner(Runner):
+    def __init__(
+        self, project: Project, job_id, config_service: ConfigService = None, **config
+    ):
+        self.project = project
+        self.job_id = job_id
+        self.config_service = config_service or ConfigService(project)
+        self.config = config
+
+        self.job = Job(elt_uri=self.job_id)
+        self.run_dir = Path(config.get("run_dir", "/run/singer"))
+        self.tap_config_dir = Path(config.get("tap_config_dir", "/etc/singer/tap"))
+        self.target_config_dir = Path(
+            config.get("target_config_dir", "/etc/singer/target")
+        )
+        self.tap_output = config.get("tap_output", False)
+
+    @property
+    def database(self):
+        return self.config.get("database", "default")
+
+    def stop(self, process, **wait_args):
+        if process.stdin:
+            process.stdin.close()
+
+        while True:
+            try:
+                code = process.wait(**wait_args)
+                logging.debug(f"{process} exited with {code}")
+                return code
+            except subprocess.TimeoutExpired:
+                process.kill()
+                logging.error(f"{process} was killed.")
+
+    def prepare(self, tap: PluginInvoker, target: PluginInvoker):
+        tap.prepare()
+        target.prepare()
+
+    def invoke(self, tap: PluginInvoker, target: PluginInvoker):
+        tee_args = ["tee"]
+
+        if self.tap_output:
+            tee_args += [tap.files["output"]]
+
+        try:
+            p_target, p_tee, p_tap = None, None, None
+            p_target = target.invoke(
+                stdin=subprocess.PIPE, stdout=target.files["state"].open("w")
+            )
+
+            p_tee = subprocess.Popen(
+                map(str, tee_args), stdin=subprocess.PIPE, stdout=p_target.stdin
+            )
+
+            p_tap = tap.invoke(stdout=p_tee.stdin)
+        except Exception as err:
+            for p in (p_target, p_tee, p_tap):
+                self.stop(p, timeout=0)
+            raise Exception(f"Cannot start tap or target: {err}")
+
+        tap_code = self.stop(p_tap)
+        tee_code = self.stop(p_tee)
+        target_code = self.stop(p_target)
+
+        if any((tap_code, tee_code, target_code)):
+            raise Exception(
+                f"Subprocesses didn't exit cleanly: tap({tap_code}), target({target_code}), tee({tee_code})"
+            )
+
+    def restore_bookmark(self, tap: PluginInvoker):
+        # the `state.json` is stored in the database
+        finder = JobFinder(self.job_id)
+        state_job = finder.latest_success()
+
+        if state_job and "singer_state" in state_job.payload:
+            logging.info(f"Found state from {state_job.started_at}.")
+            with tap.files["state"].open("w+") as state:
+                json.dump(state_job.payload["singer_state"], state)
+        else:
+            logging.warn("No state was found, complete import.")
+
+    def bookmark(self, target: PluginInvoker):
+        state_file = target.files["state"]
+        if not file_has_data(state_file):
+            logging.warn(
+                "State file is empty, this run will not update the incremental state."
+            )
+            return
+
+        with state_file.open() as state:
+            # as per the Singer specification, only the _last_ state
+            # should be persisted
+            *_, last_state = state.readlines()
+            self.job.payload["singer_state"] = json.loads(last_state)
+
+    def dry_run(self, extractor: SingerTap, loader: SingerTarget):
+        tap_exec = extractor.exec_path()
+        target_exec = loader.exec_path()
+
+        logging.info("Dry run:")
+        logging.info(f"\textractor: {extractor.name} at '{tap_exec}'")
+        logging.info(f"\tloader: {extractor.name} at '{target_exec}'")
+
+    def run(self, extractor: str, loader: str, dry_run=False):
+        tap = self.config_service.get_plugin(PluginType.EXTRACTORS, extractor)
+        target = self.config_service.get_plugin(PluginType.LOADERS, loader)
+
+        extractor = PluginInvoker(self.project, tap)
+        loader = PluginInvoker(self.project, target)
+        self.prepare(extractor, loader)
+
+        if dry_run:
+            return self.dry_run(tap, target)
+
+        with self.job.run():
+            self.restore_bookmark(extractor)
+            self.invoke(extractor, loader)
+            self.bookmark(loader)
