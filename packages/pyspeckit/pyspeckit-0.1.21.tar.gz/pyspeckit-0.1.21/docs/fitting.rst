e4d8e42a9bc8ccd158605e18fdcf05e378f41cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package pyspeckit/0.1.21/pyspeckit-0.1.21.tar.gz

---
+++ packages/pyspeckit/pyspeckit-0.1.21.tar.gz/pyspeckit-0.1.21/docs/fitting.rst
@@ -0,0 +1,12 @@
+Model Fitting
+-------------
+.. automodule:: pyspeckit.spectrum.fitters
+.. autoclass:: Specfit
+    :show-inheritance:
+    :members:
+    :inherited-members:
+    :undoc-members:
+
+
+.. toctree::
+   fit_custom_model
---
+++ packages/pyspeckit/pyspeckit-0.1.21.tar.gz/pyspeckit-0.1.21/pyspeckit/tests/ipython_log_2018-01-29.py
@@ -0,0 +1,14 @@
+########################################################
+# Started Logging At: 2018-01-29 16:39:17
+########################################################
+
+########################################################
+# # Started Logging At: 2018-01-29 16:39:17
+########################################################
+get_ipython().system('ag --python linspace')
+get_ipython().magic('cd ../../examples/')
+get_ipython().system('ag --python linspace')
+get_ipython().system('ag --python gaussian')
+get_ipython().system('ag --python np.exp')
+get_ipython().magic('cd -')
+get_ipython().system('ag --python np.exp')
