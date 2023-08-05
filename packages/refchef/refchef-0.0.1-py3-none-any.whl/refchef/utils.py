f080d7671d3e99fdbba299e35db68062d816b6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:20 +0000

    Add package refchef/0.0.1/refchef-0.0.1-py3-none-any.whl

---
+++ packages/refchef/refchef-0.0.1-py3-none-any.whl/refchef/utils.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import subprocess
+import yaml
+import yamlloader
+from collections import OrderedDict, defaultdict
+
+def read_yaml(file_path):
+    """Simple function to read yaml file"""
+    with open(file_path) as yml:
+        dict_ = yaml.load(yml)
+    return dict_
+
+def save_yaml(object, file_path):
+	"""Saves dict object as yaml file in provided path"""
+	yaml.dump(object,
+			  open(file_path, 'w'),
+			  Dumper=yamlloader.ordereddict.CDumper,
+			  indent=2,
+			  default_flow_style=False)
+
+def ordered_load(stream, loader=yaml.SafeLoader, object_pairs_hook=OrderedDict):
+    '''
+     Load YAML as an Ordered Dict
+    :param stream:
+    :param loader:
+    :param object_pairs_hook:
+    :return:
+    Borrowed shamelessly from http://codegist.net/code/python2-yaml/
+    '''
+    class OrderedLoader(loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping
+    )
+    return yaml.load(stream, OrderedLoader)
+
+def processLogical(text):
+	"""
+	Turn text into the corresponding logical.
+
+    Arguments:
+    text - text to be coerced to a logical, if a corresponding logical exists
+	"""
+	text = str(text)
+	if(text == "true" or text == "True" or text == "TRUE" or text == "T" or text == "t" or text == "1" or text.lower() == 'yes'):
+		return True
+	elif(text == "false" or text == "False" or text == "FALSE" or text == "F" or text == "f" or text == "0" or text.lower() == 'no'):
+		return False
+	else:
+		print("Input has no logical analogue.")
+		return(text)
+
+
+#TODO split this function to handle platform and path separatly. Need to handle this more gracefully.
+def add_path(string, path):
+	"""Adds complete path to shell commands
+	Arguments: shell command retrieved from yaml file (string)"""
+	if ">" in string:
+		s = string.split(" > ")
+		command = s[0]
+		file_name = os.path.join(path, s[1])
+		new_s = command + " > " + file_name
+		if "*" in new_s:
+			new_s_split = new_s.split(" *")
+			return new_s_split[0] + " " + os.path.join(path, "*" + new_s_split[1])
+		else:
+			return new_s
+	elif "wget" in string:
+		s = string.split("wget ")
+		location = s[-1]
+		filename = location.split("/")[-1]
+		return "wget -O " + os.path.join(path, filename) + " " + location
+	elif "curl" in string:
+		s = string.split("curl ")
+		location = s[-1]
+		filename = location.split("/")[-1]
+		return "curl -o " + os.path.join(path, filename) + " " + location
+	elif " *" in string:
+		s = string.split(" *")
+		return s[0] + " " + os.path.join(path, "*" + s[1])
+	else:
+		return string
---
+++ packages/refchef/refchef-0.0.1-py3-none-any.whl/tests/test_utils.py
@@ -0,0 +1,64 @@
+import pytest
+import os
+import subprocess
+import yaml
+import time
+import sys
+import datetime
+import collections
+from collections import OrderedDict, defaultdict
+
+from refchef.utils import *
+from refchef.table_utils import *
+from refchef.config import Config
+
+@pytest.fixture
+def conf():
+    conf = Config("tests/data")
+    return conf
+
+def test_ordered_load():
+    print(os.getcwd())
+    data = ordered_load(open("tests/data/example.yml"))
+    assert type(data).__name__ == "OrderedDict"
+    # ordered_load reads in a YAML as an ordered dictionary, so its type should be OrderedDict
+
+def test_logical():
+    case1 = "True"
+    case2 = "true"
+    case3 = "TRUE"
+    case4 = "1"
+    case5 = "False"
+    case6 = "false"
+    case7 = "FALSE"
+    case8 = "0"
+    case9 = "illogical"
+    assert processLogical(case1) == True
+    assert processLogical(case2) == True
+    assert processLogical(case3) == True
+    assert processLogical(case4) == True
+    assert processLogical(case5) == False
+    assert processLogical(case6) == False
+    assert processLogical(case7) == False
+    assert processLogical(case8) == False
+    assert processLogical(case9) == case9
+    # processLogical turns text into its logical equivalent if such exists, so case9 should not be converted
+
+def test_add_path():
+    s = "md5 *.fa > final_checksums.md5"
+    assert add_path(s, "test/") == "md5 test/*.fa > test/final_checksums.md5"
+
+
+    s2 = "gunzip *.gz"
+    assert add_path(s2, "test/") == "gunzip test/*.gz"
+
+
+    t = read_yaml("tests/data/master.yaml")
+    if sys.platform == 'darwin':
+        assert add_path(t["reference_test1"]["levels"]["references"][0]["commands"][0], "test/") == 'curl -o test/chr1.fa.gz https://s3.us-east-2.amazonaws.com/refchef-tests/chr1.fa.gz'
+    else:
+        assert add_path(t["reference_test1"]["levels"]["references"][0]["commands"][0], "test/") == 'wget -O test/chr1.fa.gz https://s3.us-east-2.amazonaws.com/refchef-tests/chr1.fa.gz'
+
+def test_read_menu_from_local(conf):
+     a = read_menu_from_local(conf.git_local)
+     assert type(a) == dict
