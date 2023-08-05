7e195f24d4575dcc14d213685bc69b009a7994eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:28 +0000

    Add package pyboolean/0.1.post2/pyboolean-0.1.post2.tar.gz

---
+++ packages/pyboolean/pyboolean-0.1.post2.tar.gz/pyboolean-0.1.post2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+import pyboolean
+
+setup(name='pyboolean',
+      version='0.1.post-2',
+      description='Parse infix boolean expressions to RPN, evaluate and generate truth tables.',
+      url='https://github.com/PetarPeychev/pyboolean',
+      author='PetarPeychev',
+      author_email='petarpeychev98@gmail.com',
+      license='The Unlicense',
+      py_modules=['pyboolean'],
+      zip_safe=False)
