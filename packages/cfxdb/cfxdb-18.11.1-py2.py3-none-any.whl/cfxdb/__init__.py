465f99fb8f56198ce21bf6e590b248b6d136f77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package cfxdb/18.11.1/cfxdb-18.11.1-py2.py3-none-any.whl

---
+++ packages/cfxdb/cfxdb-18.11.1-py2.py3-none-any.whl/cfxdb/__init__.py
@@ -0,0 +1,11 @@
+##############################################################################
+#
+#                        Crossbar.io Fabric
+#     Copyright (C) Crossbar.io Technologies GmbH. All rights reserved.
+#
+##############################################################################
+
+from ._version import __version__
+
+from .eventstore import Event, Publication, Session
+from .schema import Schema
