11989b06d4b3ace7987383dd9954dcda3c8d7548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package tkshapes/0.0.3/tkshapes-0.0.3-py3-none-any.whl

---
+++ packages/tkshapes/tkshapes-0.0.3-py3-none-any.whl/tkshapes/__init__.py
@@ -0,0 +1,31 @@
+
+from .gcanvas import GCanvas
+
+from .gobject import GObject
+
+from .gobjects.gwire import GWire
+from .gobjects.goval import GOval
+from .gobjects.grect import GRect
+from .gobjects.gbuffergate import GBufferGate
+from .gobjects.gnotgate import GNotGate
+from .gobjects.gandgate import GAndGate
+from .gobjects.gorgate import GOrGate
+from .gobjects.gxorgate import GXOrGate
+
+from .gobjects.gnandgate import GNandGate
+from .gobjects.gnorgate import GNorGate
+from .gobjects.gxnorgate import GXNorGate
+
+from .gobjects.ggraphpaper import GGraphPaper
+from .gobjects.gpythonlogo import GPythonLogo
+
+from .gitem import (
+    GHorzLineItem,
+    GVertLineItem,
+    GLineItem,
+    GWireItem,
+    GRectItem,
+    GOvalItem,
+    GPolygonItem,
+)
+
