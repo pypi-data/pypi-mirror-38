0545204d11dd52b6c16506897ca08493069b5dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:59 +0000

    Add package jhproxy/0.1.0/jhproxy-0.1.0.tar.gz

---
+++ packages/jhproxy/jhproxy-0.1.0.tar.gz/jhproxy-0.1.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+import os
+
+def get_version():
+    """Dumb version parser"""
+    initfname = os.path.join(
+        os.path.split(os.path.realpath(__file__))[0],
+        'jhproxy',
+        '__init__.py')
+    version = None
+    with open(initfname) as f:
+        for l in f:
+            if '__version__' in l:
+                version = l.partition('#')[0].split('=')[1].strip().replace('"', '').replace("'", '')
+                break
+
+    if version is None:
+        raise RuntimeError("Unable to get package version number")
+
+    if '.' not in version:
+        raise RuntimeError("No dots find in version")
+
+    return version
+
+
+if __name__ == '__main__':
+    setup(
+        name="jhproxy",
+        author="Giovanni Pizzi",
+        packages=find_packages(),
+        description=
+        'A port proxy for JupyterHub when using the DockerSpawner, optionally with (token-based) authorization.',
+        url="https://github.com/aiidalab/jhproxy",
+        license="MIT",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.5",
+            "License :: OSI Approved :: MIT License",
+        ],
+        keywords=[
+        'jupyterhub', 'proxy', 'authorization', 'token'
+    ],
+        long_description=open(
+        os.path.join(
+            os.path.split(os.path.realpath(__file__))[0], 
+            'README.md'), encoding="utf-8").read(),
+        version=get_version(),
+        install_requires=['jupyterhub', 'dockerspawner', 'traitlets'],
+    )
