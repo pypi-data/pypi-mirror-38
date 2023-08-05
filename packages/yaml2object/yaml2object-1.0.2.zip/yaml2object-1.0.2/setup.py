c4415177d956de1a95f8945ad2a91513b4609674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package yaml2object/1.0.2/yaml2object-1.0.2.zip

---
+++ packages/yaml2object/yaml2object-1.0.2.zip/yaml2object-1.0.2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+
+if __name__ == '__main__':
+    setup(
+        name='yaml2object',
+        version='1.0.2',
+        long_description_content_type="text/markdown",
+        description='A simple solution that allows dot notation for YAML file.',
+        author='Ravi Sharma',
+        author_email='ravi.sharma.cs11@gmail.com',
+        license='Apache2',
+        url='https://github.com/imravishar/yaml2object',
+        keywords=['YAML', 'pyyaml', 'Python YAML', 'python yaml'],
+        packages=find_packages(exclude=['tests']),
+        classifiers=[
+            'Environment :: Console',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Operating System :: OS Independent',
+            'License :: OSI Approved :: Apache Software License',
+            'Intended Audience :: Developers',
+            'Natural Language :: English'
+        ],
+        install_requires=['pyyaml'],
+        zip_safe=True,
+        cmdclass={'install': install},
+    )
