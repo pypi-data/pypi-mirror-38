9c7216b687c5a71c078b81895ab970698e18f893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:18 +0000

    Add package adnotatio-server/0.0.3/adnotatio_server-0.0.3.tar.gz

---
+++ packages/adnotatio-server/adnotatio_server-0.0.3.tar.gz/adnotatio_server-0.0.3/adnotatio_server/_version.py
@@ -0,0 +1,16 @@
+__all__ = [
+    '__author__', '__author_email__', '__version__', '__git_uri__',
+    '__dependencies__'
+]
+
+__author__ = "Matthew Wardrop"
+__author_email__ = "mpwardrop@gmail.com"
+__version__ = "0.0.3"
+__git_uri__ = "https://github.com/matthewwardrop/adnotatio-server.git"
+
+
+__dependencies__ = [
+    'flask',
+    'sqlalchemy',
+    'decorator'
+]
