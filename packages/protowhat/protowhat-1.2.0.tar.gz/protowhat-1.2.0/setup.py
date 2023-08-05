0a330a3d0b3cf0d13d6f6a0dee7ce4b069b88236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package protowhat/1.2.0/protowhat-1.2.0.tar.gz

---
+++ packages/protowhat/protowhat-1.2.0.tar.gz/protowhat-1.2.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import re
+import ast
+from os import path
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+PACKAGE_NAME = 'protowhat'
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
+    README = fp.read()
+with open(path.join(HERE, PACKAGE_NAME, '__init__.py'), 'rb') as fp:
+    VERSION = str(ast.literal_eval(_version_re.search(
+        fp.read().decode('utf-8')).group(1)))
+
+setup(
+	name=PACKAGE_NAME,
+	version=VERSION,
+	packages=['protowhat', 'protowhat.checks'],
+	install_requires=['markdown2', 'jinja2'],
+        description = 'Prototype package for submission correctness testing',
+        long_description=README,
+        long_description_content_type='text/markdown',
+        license='GNU version 3',
+        author='DataCamp',
+        author_email='content-engineering@datacamp.com',
+        maintainer='Filip Schouwenaars',
+        maintainer_email='filip@datacamp.com',
+        url = 'https://github.com/datacamp/protowhat')
