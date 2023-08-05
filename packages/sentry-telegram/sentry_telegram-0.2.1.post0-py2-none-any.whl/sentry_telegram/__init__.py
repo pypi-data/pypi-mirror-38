46520875319fef17ba60159ac5922dc54ecc65ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:49 +0000

    Add package sentry-telegram/0.2.1.post0/sentry_telegram-0.2.1.post0-py2-none-any.whl

---
+++ packages/sentry-telegram/sentry_telegram-0.2.1.post0-py2-none-any.whl/sentry_telegram/__init__.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+"""
+Plugin for Sentry which allows sending notification via Telegram messenger.
+"""
+from django.conf import settings
+
+
+__version__ = '0.2.1post'
+
+
+if settings.configured:
+    from sentry.plugins import plugins, register
+
+    from plugin import TelegramNotificationsPlugin
+
+    if TelegramNotificationsPlugin.slug not in [plugin.slug for plugin in plugins.all()]:
+        register(TelegramNotificationsPlugin)
