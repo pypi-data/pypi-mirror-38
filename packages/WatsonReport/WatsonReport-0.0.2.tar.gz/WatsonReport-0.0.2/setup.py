3cfd6110c0fd374cf339f9934b8b2cce7f22e829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:30 +0000

    Add package WatsonReport/0.0.2/WatsonReport-0.0.2.tar.gz

---
+++ packages/WatsonReport/WatsonReport-0.0.2.tar.gz/WatsonReport-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='WatsonReport',
+    author="Kobe De Decker",
+    author_email="kobededecker@gmail.com",
+    description="watson reporting for imdc",
+    version='0.0.2',
+    packages=find_packages(),
+    url="https://gitlab.com/kobededecker/watsonreport",
+    scripts=[r".\scripts\watsonreport.bat", r".\scripts\watsonreport.py"],
+    license='MIT',
+)
