96839c4c65f00c7ca130cd771bced83ded3e8ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package multiscale-imaging/0.1.4/multiscale_imaging-0.1.4.tar.gz

---
+++ packages/multiscale-imaging/multiscale_imaging-0.1.4.tar.gz/multiscale_imaging-0.1.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+setup(
+    name='multiscale_imaging',
+    version='0.1.4',
+    author='Michael Pinkert',
+    author_email='mpinkert@wisc.edu',
+    description='An multiscale image processing library for the Laboratory of Optical and Computational Instrumentation',
+    long_description=readme,
+    url='https://github.com/uw-loci/multiscale',
+    license=license,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License"
+    ]
+)
+
