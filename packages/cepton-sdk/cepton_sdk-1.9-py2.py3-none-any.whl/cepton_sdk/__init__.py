4ed5b827c4609e66438678264dde5833305572b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package cepton-sdk/1.9/cepton_sdk-1.9-py2.py3-none-any.whl

---
+++ packages/cepton-sdk/cepton_sdk-1.9-py2.py3-none-any.whl/cepton_sdk/__init__.py
@@ -0,0 +1,25 @@
+__author__ = "Cepton Technologies"
+__version__ = "1.9"
+
+
+def __check_version():
+    c_version = cepton_sdk.c.get_version_string().split(".")[:2]
+    version = __version__.split(".")[:2]
+    if c_version != version:
+        raise RuntimeError("Library versions do not match!")
+
+
+try:
+    import cepton_sdk.c
+    from cepton_sdk.c import C_ErrorCode, C_Error, C_Warning
+except OSError:
+    # Allow loading python only parts of library
+    pass
+else:
+    from cepton_sdk.api import *
+    from cepton_sdk.core import *
+    from cepton_sdk.point import *
+    from cepton_sdk.sensor import *
+    import cepton_sdk.capture_replay
+
+    __check_version()
