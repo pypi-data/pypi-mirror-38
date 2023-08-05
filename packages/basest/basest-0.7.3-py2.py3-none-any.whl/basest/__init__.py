b7ebccdfafa47544e72f314b8ccda0022d6d2548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:01 +0000

    Add package basest/0.7.3/basest-0.7.3-py2.py3-none-any.whl

---
+++ packages/basest/basest-0.7.3-py2.py3-none-any.whl/basest/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016, 2018, Joshua Saxby <joshua.a.saxby@gmail.com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from __future__ import absolute_import, division, print_function
+
+from . import core, encoders, exceptions
+
+
+__all__ = ['core', 'encoders', 'exceptions']
+
+name = 'basest'
