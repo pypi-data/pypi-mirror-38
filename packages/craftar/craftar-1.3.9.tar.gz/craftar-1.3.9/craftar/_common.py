1d8a82732e3eba57244da256ba08a57e034e3332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:24 +0000

    Add package craftar/1.3.9/craftar-1.3.9.tar.gz

---
+++ packages/craftar/craftar-1.3.9.tar.gz/craftar-1.3.9/craftar/_common.py
@@ -0,0 +1,190 @@
+#  (C) Catchoom Technologies S.L.
+#  Licensed under the MIT license.
+#  https://github.com/catchoom/craftar-python/blob/master/LICENSE
+#  All warranties and liabilities are disclaimed.
+
+"Provides common internal utility fuctions for accessing CraftAR's API"
+
+import json
+import re
+import requests
+try:
+    from urllib import urlencode
+except ImportError:
+    # Fallback to maintain backward compatibility with Python 2
+    from urllib.parse import urlencode
+from craftar import settings
+
+HEADERS = {
+    'User-Agent': settings.USER_AGENT,
+    'content-type': 'application/json',
+}
+
+
+def _validate(object_type=None, data=None, uuid=None):
+    "Validate the parameters before asking the API"
+
+    if object_type:
+        assert object_type in settings.ALLOWED_OBJECT_TYPES, \
+            "Wrong object_type: %s" % object_type
+    if data:
+        assert type(data) is dict, "Wrong data"
+        assert data is not {}, "Empty data"
+    if uuid:
+        if object_type == 'token':
+            uuid4hex = re.compile('[0-9a-f]{16}\Z', re.I)
+            assert uuid4hex.match(uuid), "Wrong token: %s" % str(uuid)
+        else:
+            uuid4hex = re.compile('[0-9a-f]{32}\Z', re.I)
+            assert uuid4hex.match(uuid), "Wrong uuid: %s" % str(uuid)
+
+
+def _validate_response(response):
+    "Validate the response from the API. Raise API, HTTP and url errors"
+    try:
+        # if there is an error message
+        json_resp = response.json()
+        if "error_message" in json_resp:
+            msg = json_resp["error_message"]
+        else:
+            msg = json_resp["error"]
+        raise Exception("Error %s: %s" % (response.status_code, msg))
+    except (ValueError, KeyError):
+        # if there is not error message, but there is an error
+        response.raise_for_status()
+
+
+def _parse_object(_object):
+    "Return a parsed object. Ugly API urls are replaced by UUIDs"
+    # remove resource_uri
+    if "resource_uri" in _object:
+        del _object["resource_uri"]
+
+    # parse collection, item, app and version: set uuid instead of api uri
+    keys_to_parse = ["collection", "item", "version", "app", "tag"]
+    for key in keys_to_parse:
+        if key in _object:
+            _object[key] = _object[key].split("/")[-2]
+
+    # parse collections and tags
+    keys_to_parse = ["collections", "tags"]
+    for key in keys_to_parse:
+        if key in _object:
+            _object[key] = map(lambda uri: uri.split("/")[-2], _object[key])
+
+    return _object
+
+
+def _get_url(api_key, object_type, uuid=None, limit=None, offset=None,
+             filter=None, filters_dict=None):
+    "Return a valid API url, based on the parameters"
+    _validate(object_type=object_type, uuid=uuid)
+    resource_name = object_type
+    if uuid:
+        resource_name += "/%s" % uuid
+
+    url = "%s/api/%s/%s/?api_key=%s" % (settings.MANAGEMENT_HOSTNAME,
+                                        settings.MANAGEMENT_API_VERSION,
+                                        resource_name, api_key)
+    if limit:
+        url += "&limit=%s" % limit
+    if offset:
+        url += "&offset=%s" % offset
+    if filter:
+        if object_type == 'item' or object_type == 'token':
+            url += "&collection__uuid=%s" % filter
+        elif object_type == 'image':
+            url += "&item__uuid=%s" % filter
+    if filters_dict and isinstance(filters_dict, dict):
+        url += "&{}".format(urlencode(filters_dict))
+
+    return url
+
+
+def _get_object_list(api_key, object_type, limit=20, offset=0,
+                     filter=None, filters_dict=None):
+    "Get a list of objects"
+    _validate(object_type=object_type)
+
+    url = _get_url(api_key, object_type, None, limit, offset, filter,
+                   filters_dict)
+    response = requests.get(url)
+    _validate_response(response)
+
+    object_list = []
+    for unparsed_object in response.json()["objects"]:
+        parsed_object = _parse_object(unparsed_object)
+        object_list.append(parsed_object)
+    return object_list
+
+
+def _get_object(api_key, object_type, uuid):
+    "Get a single object"
+    _validate(object_type=object_type, uuid=uuid)
+    url = _get_url(api_key, object_type, uuid)
+    response = requests.get(url)
+    _validate_response(response)
+    parsed_object = _parse_object(response.json())
+    return parsed_object
+
+
+def _create_object(api_key, object_type, data):
+    "Create a single object without an attachment"
+    _validate(object_type=object_type, data=data)
+    response = requests.post(
+        url=_get_url(api_key, object_type),
+        data=json.dumps(data),
+        headers=HEADERS,
+    )
+    _validate_response(response)
+    parsed_object = _parse_object(response.json())
+    if response.status_code == 201:
+        return parsed_object
+
+
+def _create_object_multipart(api_key, object_type, files, data):
+    "Create a single object with an attachment (image file)"
+    _validate(object_type=object_type, data=data)
+    response = requests.post(
+        url=_get_url(api_key, object_type),
+        data=data,
+        files=files,
+    )
+    _validate_response(response)
+    parsed_object = _parse_object(response.json())
+    if response.status_code == 201:
+        return parsed_object
+
+
+def _update_object(api_key, object_type, uuid, data):
+    "Update a single object"
+    _validate(object_type=object_type, data=data, uuid=uuid)
+    response = requests.put(
+        url=_get_url(api_key, object_type, uuid),
+        data=json.dumps(data),
+        headers=HEADERS,
+    )
+    _validate_response(response)
+    return (response.status_code == 202)
+
+
+def _update_object_multipart(api_key, object_type, uuid, files, data):
+    "Update a single object with an attachment (image file)"
+    _validate(object_type=object_type, data=data, uuid=uuid)
+    response = requests.put(
+        url=_get_url(api_key, object_type, uuid),
+        data=data,
+        files=files,
+    )
+    _validate_response(response)
+    return (response.status_code == 202)
+
+
+def _delete_object(api_key, object_type, uuid):
+    "Update a single object"
+    _validate(object_type=object_type, uuid=uuid)
+    response = requests.delete(
+        url=_get_url(api_key, object_type, uuid),
+    )
+    _validate_response(response)
+    return (response.status_code == 204)
---
+++ packages/craftar/craftar-1.3.9.tar.gz/craftar-1.3.9/craftar/_management.py
@@ -0,0 +1,316 @@
+#  (C) Catchoom Technologies S.L.
+#  Licensed under the MIT license.
+#  https://github.com/catchoom/craftar-python/blob/master/LICENSE
+#  All warranties and liabilities are disclaimed.
+
+"Provides functions for accessing the CraftAR's management API."
+
+from craftar._common import _get_object_list, _get_object, _create_object, \
+    _create_object_multipart, _update_object, _update_object_multipart,  \
+    _delete_object
+from craftar import settings
+import json
+
+
+def _get_object_uri(object_type, uuid):
+    return "/api/%s/%s/%s/" % (settings.MANAGEMENT_API_VERSION,
+                               object_type, uuid)
+
+
+# Collections
+def get_collection_list(api_key, limit, offset, filters=None):
+    """Return a list of collections, paginated by @limit and @offset.
+    Filter by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "collection", limit, offset, filter=None,
+                            filters_dict=filters)
+
+
+def get_collection(api_key, uuid):
+    "Return a collection, identified by @uuid"
+    return _get_object(api_key, "collection", uuid)
+
+
+def create_collection(api_key, name, offline=False):
+    "Create a collection with a given @name (must be unique)"
+    data = {'name': name, 'offline': offline}
+    return _create_object(api_key, "collection", data)
+
+
+def update_collection(api_key, uuid, name=None):
+    "Update the collection name, identified by @uuid"
+    data = {}
+    if name is not None:
+        data["name"] = name
+    return _update_object(api_key, "collection", uuid, data)
+
+
+def delete_collection(api_key, uuid):
+    "Delete a collection, identified by @uuid"
+    return _delete_object(api_key, "collection", uuid)
+
+
+# Items
+def get_item_list(api_key, limit, offset, collection=None, filters=None):
+    """Return a list of items, paginated by @limit and @offset
+    Filter by @collection or by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "item", limit, offset, filter=collection,
+                            filters_dict=filters)
+
+
+def get_item(api_key, uuid):
+    "Return an item, identified by @uuid"
+    return _get_object(api_key, "item", uuid)
+
+
+def create_item(api_key, collection, name, url=None, custom=None,
+                trackable=None, content=None, tags=None):
+    """Create a collection with a given @name, belonging to @collection.
+    The fields @url, @custom, @trackable and @content are optional."""
+    data = {
+        'collection': _get_object_uri('collection', collection),
+        'name': name,
+    }
+    if url is not None:
+        data['url'] = url
+    if custom is not None:
+        data['custom'] = custom
+    if trackable is not None:
+        data['trackable'] = trackable
+    if content is not None:
+        data['content'] = content
+    if tags is not None:
+        data['tags'] = map(lambda uuid: _get_object_uri('tag', uuid), tags)
+
+    return _create_object(api_key, "item", data)
+
+
+def update_item(api_key, uuid, name=None, url=None, custom=None,
+                trackable=None, content=None, tags=None):
+    "Update an item, identified by @uuid"
+    data = {}
+    if name is not None:
+        data["name"] = name
+    if url is not None:
+        data["url"] = url
+    if custom is not None:
+        data["custom"] = custom
+    if trackable is not None:
+        data['trackable'] = trackable
+    if content is not None:
+        data['content'] = content
+    if tags is not None:
+        data['tags'] = map(lambda uuid: _get_object_uri('tag', uuid), tags)
+
+    return _update_object(api_key, "item", uuid, data)
+
+
+def delete_item(api_key, uuid):
+    "Delete an item, identified by @uuid"
+    return _delete_object(api_key, "item", uuid)
+
+
+# Images
+def get_image_list(api_key, limit, offset, item=None, filters=None):
+    """Return a list of images, paginated by @limit and @offset
+    Filter by @item or by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "image", limit, offset, filter=item,
+                            filters_dict=filters)
+
+
+def get_image(api_key, uuid):
+    "Return an image, identified by @uuid"
+    return _get_object(api_key, "image", uuid)
+
+
+def create_image(api_key, item, filename):
+    "Create an image from a @filename, belongs to @item"
+    files = {'file': open(filename, 'rb')}
+    data = {'item': _get_object_uri('item', item)}
+
+    return _create_object_multipart(api_key, "image", files, data)
+
+
+def update_image(api_key, uuid, filename):
+    "Update the image file, identified by @uuid"
+    files = {'file': open(filename, 'rb')}
+    return _update_object_multipart(api_key, "image", uuid, files, None)
+
+
+def delete_image(api_key, uuid):
+    "Delete an image, identified by @uuid"
+    return _delete_object(api_key, "image", uuid)
+
+
+# Tokens
+def get_token_list(api_key, limit, offset, collection=None, filters=None):
+    """Return a list of tokens, paginated by @limit and @offset.
+    Filter by @collection or by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "token", limit, offset,
+                            filter=collection, filters_dict=filters)
+
+
+def create_token(api_key, collection, tags=None):
+    "Create a token, belongs to @collection"
+    data = {'collection': _get_object_uri('collection', collection)}
+
+    if tags is not None:
+        data['tags'] = map(lambda uuid: _get_object_uri('tag', uuid), tags)
+
+    return _create_object(api_key, "token", data)
+
+
+def update_token(api_key, uuid, tags=None):
+    "Update the tags for a given token"
+    data = {}
+
+    if tags is not None:
+        data['tags'] = map(lambda uuid: _get_object_uri('tag', uuid), tags)
+
+    return _update_object(api_key, "token", uuid, data)
+
+
+def delete_token(api_key, token):
+    "Delete a token, identified by @token"
+    return _delete_object(api_key, "token", token)
+
+
+# Media objects
+def get_media_list(api_key, limit, offset, filters=None):
+    """Return a list of media objects, paginated by @limit and @offset
+    Filter by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "media", limit, offset,
+                            filters_dict=filters)
+
+
+def get_media(api_key, uuid):
+    "Return a media object, identified by @uuid"
+    return _get_object(api_key, "media", uuid)
+
+
+def create_media(api_key, filename):
+    "Create a media object from a @filename, belongs to @item"
+    files = {'file': open(filename, 'rb')}
+
+    return _create_object_multipart(api_key, "media", files, {})
+
+
+def create_video_media(api_key, url, name=None):
+    video_name = name or url.split("/")[-1]
+
+    return _create_object(api_key, "media", {
+        'mimetype': 'video',
+        'name': video_name,
+        'meta': json.dumps({'video-url': url})
+    })
+
+
+def delete_media(api_key, uuid):
+    "Delete an image, identified by @uuid"
+    return _delete_object(api_key, "media", uuid)
+
+
+# Tags
+def get_tag_list(api_key, limit, offset, collection=None, filters=None):
+    """Return a list of tags, paginated by @limit and @offset
+    Filter by @collection or by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "tag", limit, offset, filter=collection,
+                            filters_dict=filters)
+
+
+def get_tag(api_key, uuid):
+    "Return an item, identified by @uuid"
+    return _get_object(api_key, "tag", uuid)
+
+
+def create_tag(api_key, collection, name):
+    """Create a tag with a given @name, belonging to @collection."""
+    data = {
+        'collection': _get_object_uri('collection', collection),
+        'name': name,
+    }
+
+    return _create_object(api_key, "tag", data)
+
+
+def delete_tag(api_key, uuid):
+    "Delete a tag, identified by @uuid"
+    return _delete_object(api_key, "tag", uuid)
+
+
+# Applications
+def get_app_list(api_key, limit, offset, filters=None):
+    """Return a list of applications, paginated by @limit and @offset
+    Filter by @collection or by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "app", limit, offset,
+                            filters_dict=filters)
+
+
+def get_app(api_key, uuid):
+    "Return an application, identified by @uuid"
+    return _get_object(api_key, "app", uuid)
+
+
+def create_app(api_key, collection, name):
+    """Create an application with a given @name, belonging to @collection."""
+    data = {
+        'collection': _get_object_uri('collection', collection),
+        'name': name,
+    }
+
+    return _create_object(api_key, "app", data)
+
+
+# SDK Versions
+def get_version_list(api_key, limit, offset, filters=None):
+    """Return a list of SDK versions, paginated by @limit and @offset
+    Filter by @collection or by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "version", limit, offset,
+                            filters_dict=filters)
+
+
+def get_version(api_key, uuid):
+    "Return an SDK Version, identified by @uuid"
+    return _get_object(api_key, "version", uuid)
+
+
+# Collection Bundles
+def get_bundle_list(api_key, limit, offset, filters=None):
+    """Return a list of bundles, paginated by @limit and @offset
+    Filter by @collection or by providing a dictionary with filter name
+    and value with @filters as described in the API documentation."""
+    return _get_object_list(api_key, "collectionbundle", limit, offset,
+                            filters_dict=filters)
+
+
+def get_bundle(api_key, uuid):
+    "Return a bundle, identified by @uuid"
+    return _get_object(api_key, "collectionbundle", uuid)
+
+
+def create_bundle(api_key, collection, app, version, tag=None):
+    """Create a bundle with a given @name, belonging to @collection."""
+    data = {
+        "collection": collection,
+        "version": version,
+        "app": app,
+    }
+    if tag:
+        data["tag"] = tag
+
+    for object_type, uuid in data.items():
+        data[object_type] = _get_object_uri(object_type, uuid)
+
+    return _create_object(api_key, "collectionbundle", data)
+
+
+def delete_bundle(api_key, uuid):
+    "Delete a bundle, identified by @uuid"
+    return _delete_object(api_key, "collectionbundle", uuid)
