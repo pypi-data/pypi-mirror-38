aaf6613e98325eecb02801e695cc23093149e62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package solr-dsl/0.0.15/solr-dsl-0.0.15.tar.gz

---
+++ packages/solr-dsl/solr-dsl-0.0.15.tar.gz/solr-dsl-0.0.15/solr_dsl/search.py
@@ -0,0 +1,13 @@
+from .cursor import Cursor
+
+
+class Search():
+
+    def __init__(self, solr, query="*:*", **kwargs):
+        self.solr = solr
+        self.query = query
+        self.kwargs = kwargs
+
+    def scan(self):
+        """Iterate over documents that match the search."""
+        return Cursor(self.solr, self.query, **self.kwargs)
