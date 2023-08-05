cdfec742925f9aecf767f694c352dbae871da0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:24 +0000

    Add package apidecorators/0.0.5/apidecorators-0.0.5-py3-none-any.whl

---
+++ packages/apidecorators/apidecorators-0.0.5-py3-none-any.whl/apidecorators/hooks.py
@@ -0,0 +1,28 @@
+_on_insert = {}
+_on_update = {}
+_on_push = {}
+_on_pull = {}
+
+def on_insert(col):
+    def decorator(f):
+        _on_insert[col] = f
+        return f
+    return decorator
+
+def on_update(col):
+    def decorator(f):
+        _on_update[col] = f
+        return f
+    return decorator
+
+def on_push(col):
+    def decorator(f):
+        _on_push[col] = f
+        return f
+    return decorator
+
+def on_pull(col):
+    def decorator(f):
+        _on_pull[col] = f
+        return f
+    return decorator
