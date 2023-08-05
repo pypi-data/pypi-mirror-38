de20d1317443a58e2f9025f1842fb32ce92aa216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package pytest-docs/0.0.6/pytest_docs-0.0.6-py3-none-any.whl

---
+++ packages/pytest-docs/pytest_docs-0.0.6-py3-none-any.whl/pytest_docs/formatters/markdown.py
@@ -0,0 +1,22 @@
+from pytest_docs.formatter import Formatter
+
+
+class MarkdownFormatter(Formatter):
+    name = "md"
+    marker_prefix = "\n**Markers:**"
+
+    @staticmethod
+    def module_name_format(element):
+        return "# {}".format(element)
+
+    @staticmethod
+    def class_name_format(element):
+        return "## {}".format(element)
+
+    @staticmethod
+    def func_name_format(element):
+        return "### {}".format(element)
+
+    @staticmethod
+    def marker_format(marker):
+        return "- {}".format(marker)
