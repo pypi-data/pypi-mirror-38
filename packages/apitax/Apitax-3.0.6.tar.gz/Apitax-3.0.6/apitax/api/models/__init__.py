310b08b8e6bbfb7930ec538bdf956561397c48a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:49 +0000

    Add package apitax/3.0.6/Apitax-3.0.6.tar.gz

---
+++ packages/apitax/Apitax-3.0.6.tar.gz/Apitax-3.0.6/apitax/api/models/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from apitax.api.models.auth_response import AuthResponse
+from apitax.api.models.command import Command
+from apitax.api.models.error_response import ErrorResponse
+from apitax.api.models.execute import Execute
+from apitax.api.models.response import Response
+from apitax.api.models.script import Script
+from apitax.api.models.script_create import ScriptCreate
+from apitax.api.models.script_delete import ScriptDelete
+from apitax.api.models.script_rename import ScriptRename
+from apitax.api.models.script_save import ScriptSave
+from apitax.api.models.user import User
+from apitax.api.models.user_auth import UserAuth
+from apitax.api.models.user_create import UserCreate
+from apitax.api.models.user_delete import UserDelete
+from apitax.api.models.user_save import UserSave
