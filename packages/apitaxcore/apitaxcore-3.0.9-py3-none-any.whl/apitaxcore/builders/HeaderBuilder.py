87a44d429a3839434ae6e333eea86c583e3d4a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:13 +0000

    Add package apitaxcore/3.0.9/apitaxcore-3.0.9-py3-none-any.whl

---
+++ packages/apitaxcore/apitaxcore-3.0.9-py3-none-any.whl/apitaxcore/builders/HeaderBuilder.py
@@ -0,0 +1,7 @@
+from apitaxcore.builders.DictBuilder import DictBuilder
+
+
+# Builder class for creating headers dynamically
+class HeaderBuilder(DictBuilder):
+    def __init__(self):
+        super().__init__()
