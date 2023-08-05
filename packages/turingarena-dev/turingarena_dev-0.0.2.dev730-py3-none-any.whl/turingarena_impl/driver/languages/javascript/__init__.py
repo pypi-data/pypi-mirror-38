032942b213e5f53429f39b7961e58128747e891d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package turingarena-dev/0.0.2.dev730/turingarena_dev-0.0.2.dev730-py3-none-any.whl

---
+++ packages/turingarena-dev/turingarena_dev-0.0.2.dev730-py3-none-any.whl/turingarena_impl/driver/languages/javascript/__init__.py
@@ -0,0 +1,8 @@
+# TODO: temporarily disable because there are errors in skeleton and template generators.
+# language = Language(
+#     name="javascript",
+#     extension=".js",
+#     source=JavascriptAlgorithmSource,
+#     skeleton_generator=JavaScriptSkeletonCodeGen,
+#     template_generator=JavaScriptTemplateCodeGen,
+# )
