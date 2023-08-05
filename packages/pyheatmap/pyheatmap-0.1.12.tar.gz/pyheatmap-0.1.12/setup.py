a2a61260774b9342ef79f8cef50a2f04dd661741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:27 +0000

    Add package pyheatmap/0.1.12/pyheatmap-0.1.12.tar.gz

---
+++ packages/pyheatmap/pyheatmap-0.1.12.tar.gz/pyheatmap-0.1.12/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pyheatmap",
+    version="0.1.12",
+    packages=["pyheatmap", "pyheatmap.inc"],
+    url="https://github.com/oldj/pyheatmap",
+    license="MIT",
+    author="oldj",
+    author_email="oldj.wu@gmail.com",
+    description="Create heatmap by Python.",
+    requires=["pillow"]
+)
