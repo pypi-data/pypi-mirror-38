17d73b4ae27090e427f821da3d4783fbab5ccd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:10 +0000

    Add package ccobra/0.0.5/ccobra-0.0.5.tar.gz

---
+++ packages/ccobra/ccobra-0.0.5.tar.gz/ccobra-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ccobra',
+    description='The CCOBRA framework.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/CognitiveComputationLab/ccobra',
+    version='0.0.5',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    author='Nicolas Riesterer',
+    author_email='riestern@tf.uni-freiburg.de',
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    )
+)
