a106b455d36f8bba8a9543ec7ff12648f6454093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package Products.ZopeVersionControl/1.1.4/Products.ZopeVersionControl-1.1.4.tar.gz

---
+++ packages/Products.ZopeVersionControl/Products.ZopeVersionControl-1.1.4.tar.gz/Products.ZopeVersionControl-1.1.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.1.4'
+
+setup(
+    name='Products.ZopeVersionControl',
+    version=__version__,
+    description="Zope Version Control",
+    long_description=(open('README.rst').read() + "\n" +
+                      open('CHANGES.rst').read()),
+    classifiers=[
+        'Framework :: Zope2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license='ZPL',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    url='https://pypi.org/project/Products.ZopeVersionControl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'Acquisition',
+        'DateTime',
+        'transaction',
+        'six',
+        'ZODB',
+        'Zope2',
+    ],
+    entry_points={
+        'zodbupdate.decode': [
+            'decodes = Products.ZopeVersionControl:zodbupdate_decode_dict',
+        ],
+    },
+)
