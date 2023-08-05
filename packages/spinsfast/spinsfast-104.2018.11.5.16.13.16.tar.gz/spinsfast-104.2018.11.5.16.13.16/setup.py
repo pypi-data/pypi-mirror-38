8e27d59559aa795425aea916c4a60ce749c9d01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:50 +0000

    Add package spinsfast/104.2018.11.5.16.13.16/spinsfast-104.2018.11.5.16.13.16.tar.gz

---
+++ packages/spinsfast/spinsfast-104.2018.11.5.16.13.16.tar.gz/spinsfast-104.2018.11.5.16.13.16/setup.py
@@ -0,0 +1,66 @@
+from os.path import isdir, join, dirname, realpath
+from setuptools import setup, Extension
+from distutils.sysconfig import get_python_lib
+import os
+import glob
+import time
+
+try:
+    import numpy
+    numpy_inc = numpy.get_include()
+except:
+    numpy_inc = os.path.join(get_python_lib(plat_specific=1), 'numpy', 'core', 'include')
+
+## The following block is added for nicer behavior with `module`s on clusters
+from os import environ
+IncDirs = [numpy_inc, ]
+LibDirs = ["lib",]
+if "SRC_DIR" in environ:
+    IncDirs += [join(environ["SRC_DIR"], "include"),]
+else:
+    IncDirs += ["include",]
+## See if GSL_HOME is set; if so, use it
+if "GSL_HOME" in environ :
+    IncDirs += [environ["GSL_HOME"]+'/include']
+    LibDirs += [environ["GSL_HOME"]+'/lib']
+## See if FFTW3_HOME is set; if so, use it
+if "FFTW3_HOME" in environ :
+    IncDirs += [environ["FFTW3_HOME"]+'/include']
+    LibDirs += [environ["FFTW3_HOME"]+'/lib']
+    # fftw_rpath = '-Wl,-rpath,{FFTW3_HOME}/lib'.format(FFTW3_HOME=environ["FFTW3_HOME"])
+# If /opt/local directories exist, use them
+if isdir('/opt/local/include'):
+    IncDirs += ['/opt/local/include']
+if isdir('/opt/local/lib'):
+    LibDirs += ['/opt/local/lib']
+
+# Construct the version number, starting with spinsfast's own version (104) and appending the date
+# and time this python version was created.
+if "package_version" in environ:
+    version = environ["package_version"]
+else:
+    version = '104.'
+    if "datetime" in environ:
+        version += environ["datetime"]
+    else:
+        version += time.strftime("%Y.%m.%d.%H.%M.%S", time.gmtime())
+with open('python/_version.py', 'w') as f:
+    f.write('__version__ = "{0}"'.format(version))
+
+setup(name = 'spinsfast',
+      version = version,
+      packages = ['spinsfast'],
+      package_dir = {'spinsfast': 'python'},
+      url = 'https://github.com/moble/spinsfast',
+      maintainer = 'Mike Boyle',
+      maintainer_email = 'mob22@cornell.edu',
+      ext_modules = [Extension(
+          name = 'spinsfast.cextension',
+          sources = ['python/cextension.c'] + glob.glob('code/*.c'),
+          include_dirs=IncDirs,
+          libraries=['fftw3'],
+          library_dirs=LibDirs,
+          extra_compile_args=['-std=c99','-fPIC','-O3'],
+          #extra_link_args=[fftw_rpath],
+      )],
+)
