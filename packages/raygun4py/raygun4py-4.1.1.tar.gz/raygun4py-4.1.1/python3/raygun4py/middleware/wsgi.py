1869c2ee846b806f4efcbaa1db82970908fc8e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:49 +0000

    Add package raygun4py/4.1.1/raygun4py-4.1.1.tar.gz

---
+++ packages/raygun4py/raygun4py-4.1.1.tar.gz/raygun4py-4.1.1/python3/raygun4py/middleware/wsgi.py
@@ -0,0 +1,37 @@
+import logging
+from raygun4py import raygunprovider, http_utilities
+
+log = logging.getLogger(__name__)
+
+
+class Provider(object):
+
+    def __init__(self, app, apiKey):
+        self.app = app
+        self.sender = raygunprovider.RaygunSender(apiKey)
+
+    def __call__(self, environ, start_response):
+        if not self.sender:
+            log.error("Raygun-WSGI: Cannot send as provider not attached")
+
+        iterable = None
+
+        try:
+            iterable = self.app(environ, start_response)
+            for event in iterable:
+                yield event
+
+        except Exception as e:
+            request = http_utilities.build_wsgi_compliant_request(environ)
+            self.sender.send_exception(exception=e, request=request)
+            raise
+
+        finally:
+            if hasattr(iterable, 'close'):
+                try:
+                    iterable.close()
+                except Exception as e:
+                    rg_request_details = http_utilities.build_wsgi_compliant_request(environ)
+                    self.sender.send_exception(exception=e, request=rg_request_details)
+                    raise
+
---
+++ packages/raygun4py/raygun4py-4.1.1.tar.gz/raygun4py-4.1.1/setup.py
@@ -0,0 +1,51 @@
+import sys
+from setuptools import setup
+
+packages = ['raygun4py', 'raygun4py.middleware']
+
+if sys.version_info[0] == 2:
+    base_dir = 'python2'
+elif sys.version_info[0] == 3:
+    base_dir = 'python3'
+
+setup(
+    name='raygun4py',
+    version='4.1.1',
+    packages=packages,
+    package_dir= {
+        "raygun4py": base_dir + "/raygun4py"
+    },
+    license='LICENSE',
+    url='https://raygun.com',
+    author='Raygun',
+    author_email='hello@raygun.io',
+    description='Official Raygun provider for Python 2.7 and Python 3+',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'jsonpickle >= 0.9.2',
+        'blinker >= 1.3.0',
+        'requests >= 2.9.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'raygun4py = raygun4py.cli:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Communications',
+    ],
+)
