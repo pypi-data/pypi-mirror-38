75e5f9b17787497c1b42eb99c7935e8d051b0159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package pip-api/0.0.4/pip-api-0.0.4.tar.gz

---
+++ packages/pip-api/pip-api-0.0.4.tar.gz/pip-api-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    author='Dustin Ingram',
+    author_email='di@di.codes',
+    description='The official unofficial pip API',
+    install_requires=['packaging>=16.1', 'pip'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='pip-api',
+    packages=['pip_api'],
+    python_requires='>=2.7,!=3.0,!=3.1,!=3.2,!=3.3',
+    url='http://github.com/di/pip-api',
+    summary='The official unofficial pip API',
+    version='0.0.4',
+)
