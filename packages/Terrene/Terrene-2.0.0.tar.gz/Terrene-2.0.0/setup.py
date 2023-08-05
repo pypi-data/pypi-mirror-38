4888845c67340e92a6bc0f7dd886db04900af692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:51 +0000

    Add package Terrene/2.0.0/Terrene-2.0.0.tar.gz

---
+++ packages/Terrene/Terrene-2.0.0.tar.gz/Terrene-2.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+
+setup(
+    name="Terrene",
+    version="2.0.0",
+    description="Terrene's Python SDK",
+    url="https://docs.terrene.co",
+    author="Terrene",
+    author_email="developers@terrene.co",
+    license='MIT',
+    keywords="terrene",
+    packages=find_packages(),
+    install_requires=requirements,
+    test_requirements=test_requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
