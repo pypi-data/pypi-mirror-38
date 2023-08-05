955af530952f6086d08bf0e6c26ce4e28a7fb57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:41 +0000

    Add package ftw.testing/1.19.2/ftw.testing-1.19.2.tar.gz

---
+++ packages/ftw.testing/ftw.testing-1.19.2.tar.gz/ftw.testing-1.19.2/ftw/testing/freezer.py
@@ -0,0 +1,98 @@
+from contextlib import contextmanager
+from datetime import datetime
+from datetime import timedelta
+from forbiddenfruit import curse
+from mocker import expect
+from mocker import Mocker
+from time import mktime
+from time import tzname
+import pytz
+
+
+class FreezedClock(object):
+    """Freeze the clock.
+
+    Supported:
+      time.time()
+      datetime.now()
+      datetime.utcnow()
+    """
+
+    def __init__(self, new_now):
+        self.new_now = new_now
+
+    def forward(self, **kwargs):
+        self.new_now += timedelta(**kwargs)
+        self.__exit__(None, None, None)
+        self.__enter__()
+
+    def backward(self, **kwargs):
+        self.new_now -= timedelta(**kwargs)
+        self.__exit__(None, None, None)
+        self.__enter__()
+
+    def __enter__(self):
+        if type(self.new_now) != datetime:
+            raise ValueError(
+                'The freeze_date argument must be a datetime.datetime'
+                ' instance, got %s' % type(self.new_now).__name__)
+
+        self.mocker = Mocker()
+
+        # Replace "datetime.datetime.now" classmethod
+        self._previous_datetime_now = datetime.now
+
+        # Replace "datetime.datetime.utcnow" classmethod
+        self._previous_datetime_utcnow = datetime.utcnow
+
+        @classmethod
+        def freezed_now(klass, tz=None):
+            if not tz:
+                return self.new_now.replace(tzinfo=None)
+
+            # Time was frozen to a naive DT, but a TZ-aware time is being requested
+            # from now(). We assume the same TZ for freezing as requested by now.
+            elif self.new_now.tzinfo is None:
+                return self.new_now.replace(tzinfo=tz)
+
+            elif self.new_now.tzinfo != tz:
+                return tz.normalize(self.new_now.astimezone(tz))
+
+            return self.new_now
+
+        @classmethod
+        def freezed_utcnow(klass):
+            if self.new_now.tzinfo and self.new_now.tzinfo != pytz.UTC:
+                return pytz.UTC.normalize(self.new_now.astimezone(pytz.UTC))
+            return self.new_now
+
+        curse(datetime, 'now', freezed_now)
+        curse(datetime, 'utcnow', freezed_utcnow)
+
+        # Replace "time.time" function
+        # datetime.timetuple does not contain any timezone information, so this
+        # information will be lost. Moreover time.time should be in the system
+        # timezone, so we need to correct for the offset of timezone used in
+        # the freezing relative to the system timezone.
+        local_tz = pytz.timezone(tzname[0])
+        if self.new_now.tzinfo is None:
+            new_time = mktime(self.new_now.timetuple())
+        else:
+            new_time = mktime(self.new_now.tzinfo.normalize(self.new_now + local_tz._utcoffset).utctimetuple())
+        time_class = self.mocker.replace('time.time')
+        expect(time_class()).call(lambda: new_time).count(0, None)
+
+        self.mocker.replay()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.mocker.restore()
+        self.mocker.verify()
+        curse(datetime, 'now', self._previous_datetime_now)
+        curse(datetime, 'utcnow', self._previous_datetime_utcnow)
+
+
+@contextmanager
+def freeze(new_now=None):
+    with FreezedClock(new_now or datetime.now()) as clock:
+        yield clock
