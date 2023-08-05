7f2056201dc77d143f5e86f7a171c9fb29600c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package empatican/0.0.3/empatican-0.0.3.tar.gz

---
+++ packages/empatican/empatican-0.0.3.tar.gz/empatican-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='empatican',
+    version='0.0.3',
+    description='Empatica Physio Utilities',
+    url='http://github.com/kastman/empatican',
+    author='Erik Kastman',
+    author_email='erik.kastman@gmail.com',
+    packages=['empatican', 'empatican.interface', 'empatican.physio'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    entry_points={
+        'console_scripts': ['empatican=empatican.cli:main'],
+    },
+    license='MIT',
+    install_requires=['requests', 'pandas'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    zip_safe=True)
