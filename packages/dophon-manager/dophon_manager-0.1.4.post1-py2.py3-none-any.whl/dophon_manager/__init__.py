0ef44799fa4ed381bcfe49ebf6fab668ac7a7986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:09 +0000

    Add package dophon-manager/0.1.4.post1/dophon_manager-0.1.4.post1-py2.py3-none-any.whl

---
+++ packages/dophon-manager/dophon_manager-0.1.4.post1-py2.py3-none-any.whl/dophon_manager/__init__.py
@@ -0,0 +1,21 @@
+import argparse
+from dophon_manager.actions import *
+
+parser = argparse.ArgumentParser(description='dophon project manager')
+parser.add_argument('action', type=str, default='-h')
+parser.add_argument('--name', type=str, default='dophon_project')
+FLAGS = parser.parse_args()
+
+ARG_DICT = {
+    'new': ActionObj().new_action,
+    'init': ActionObj().init_action
+}
+
+__version__ = '0.1.0'
+
+
+def main():
+    # 执行项目管理初始化
+    action = FLAGS.action
+    if action in ARG_DICT:
+        ARG_DICT[action](FLAGS.name)
