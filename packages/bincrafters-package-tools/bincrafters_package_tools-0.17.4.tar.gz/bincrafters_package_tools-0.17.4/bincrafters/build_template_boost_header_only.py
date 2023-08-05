d42e8686e285b319ff2b0203819c7f74fc78233e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:45 +0000

    Add package bincrafters-package-tools/0.17.4/bincrafters_package_tools-0.17.4.tar.gz

---
+++ packages/bincrafters-package-tools/bincrafters_package_tools-0.17.4.tar.gz/bincrafters_package_tools-0.17.4/bincrafters/build_template_boost_header_only.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from bincrafters import build_template_header_only
+
+
+def get_builder(**kwargs):
+
+    # Bincrafters default is to upload only when stable, but boost is an exception
+    # Empty string allows boost packages upload for testing branch
+    os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = ""
+
+    return build_template_header_only.get_builder(**kwargs)
