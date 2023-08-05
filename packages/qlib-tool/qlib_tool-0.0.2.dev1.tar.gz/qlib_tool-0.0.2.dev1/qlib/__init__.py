7d509619bd222938340ce3e83c52df02066c3310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:11 +0000

    Add package qlib-tool/0.0.2.dev1/qlib_tool-0.0.2.dev1.tar.gz

---
+++ packages/qlib-tool/qlib_tool-0.0.2.dev1.tar.gz/qlib_tool-0.0.2.dev1/qlib/__init__.py
@@ -0,0 +1,9 @@
+import qlib.qlibbase
+from qlib.qlibbase import QLibBase
+__version__ = "0.0.2dev"
+def testlib():
+
+    qlb = QLibBase()
+    print(f'testing called from {qlb}') 
+    
+name="qlib_tool"
