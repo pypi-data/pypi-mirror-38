592e28eb6efd90817eb227b9be0e20240f4ae805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package cloudrun/0.2.0/cloudrun-0.2.0.tar.gz

---
+++ packages/cloudrun/cloudrun-0.2.0.tar.gz/cloudrun-0.2.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='cloudrun',
+    version='0.2.0',
+    description='Client utility for cloudrun.io',
+    author='The Cloudrun Authors',
+    author_email='contact@cloudrun.io',
+    url='https://cloudrun.io',
+    packages=['cloudrun'],
+    #scripts=['scripts/cloudrun'],
+    entry_points={
+        'console_scripts': ['cloudrun=cloudrun.client:main'],
+    },
+    install_requires=['requests>=2.9'],
+    include_package_data=True,
+    zip_safe=False,
+)
