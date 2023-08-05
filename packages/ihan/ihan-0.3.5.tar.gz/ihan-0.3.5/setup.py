18c6cb6776865e0a1c9cdbdfc6ded7c91288258a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package ihan/0.3.5/ihan-0.3.5.tar.gz

---
+++ packages/ihan/ihan-0.3.5.tar.gz/ihan-0.3.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='ihan',
+      version='0.3.5',
+      description='IHAN Client for feeding and back filling log files',
+      long_description=open('docs/index.rst').read(),
+      long_description_content_type='text/x-rst',
+      url='https://www.ihan.ee/',
+      project_urls={
+        "Bug Tracker": "https://github.com/marklit/ihan/issues",
+        "Documentation": "https://ihan.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/marklit/ihan",
+      },
+      author='Mark Litwintschik',
+      author_email='mark@marksblogg.com',
+      license='MIT',
+      scripts=['bin/ihan',],
+      install_requires=[
+          "Click",
+          "requests",
+      ],
+      zip_safe=False)
