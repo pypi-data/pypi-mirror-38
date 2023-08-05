49d76f8d76538ae1e9b13e4cedf3d0436cad687c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:56 +0000

    Add package hypml/0.0.2/hypml-0.0.2.tar.gz

---
+++ packages/hypml/hypml-0.0.2.tar.gz/hypml-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+  name = 'hypml',
+  packages = ['hypml'],
+  version = '0.0.2',
+  description = 'A utility for loading valid Hypothesis Markup Language (.hypml) documents.',
+  author = 'Blair Hudson',
+  author_email = 'blairhudson@me.com',
+  url = 'https://github.com/HypMachine/HypML', 
+  download_url = '',
+  keywords = ['data', 'machine', 'learning', 'science', 'algorithm', 'hypml'],
+  classifiers = [],
+  include_package_data=True
+)
