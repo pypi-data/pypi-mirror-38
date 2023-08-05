a056f3ca5b7c2eeb7366c6504441220b49892488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package postmarker/0.12.2/postmarker-0.12.2-py2.py3-none-any.whl

---
+++ packages/postmarker/postmarker-0.12.2-py2.py3-none-any.whl/postmarker/_compat.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+
+try:
+    from urllib.parse import urljoin
+except ImportError:  # Python 2.7
+    from urlparse import urljoin  # noqa
+
+
+try:
+    from unittest.mock import patch
+except ImportError:  # Python < 3.3
+    try:
+        from mock import patch  # noqa
+    except ImportError:
+        patch = None
+
+
+try:
+    from inspect import signature
+
+    def get_args(cls):
+        return list(signature(cls).parameters)
+
+except ImportError:
+    from inspect import getargspec
+
+    def get_args(cls):
+        return getargspec(cls.__init__).args[1:]
