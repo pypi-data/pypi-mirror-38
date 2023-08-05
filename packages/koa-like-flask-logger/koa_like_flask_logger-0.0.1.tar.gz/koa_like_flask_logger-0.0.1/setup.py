c8e213708371d56a35340e668c5357fc4cb57ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:16 +0000

    Add package koa-like-flask-logger/0.0.1/koa_like_flask_logger-0.0.1.tar.gz

---
+++ packages/koa-like-flask-logger/koa_like_flask_logger-0.0.1.tar.gz/koa_like_flask_logger-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="koa_like_flask_logger",
+    version="0.0.1",
+    description="Add a koa-like logger for flask",
+    url='https://github.com/paradoxxxzero/koa_like_flask_logger',
+    author='Florian Mounier',
+    packages=find_packages(),
+    install_requires=['flask'],
+    license='MIT',
+)
