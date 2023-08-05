3613dddef977ee4e9a22dcd8325764d40328a69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:18 +0000

    Add package python-decouple-aws/0.3.0/python_decouple_aws-0.3.0-py3-none-any.whl

---
+++ packages/python-decouple-aws/python_decouple_aws-0.3.0-py3-none-any.whl/decouple_aws/repository.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+import boto3
+
+from .exceptions import AWSException
+
+
+class RepositoryAwsSecretManager:
+    """
+    Retrieves option keys from AWS Secret Manager or falls back
+    to os.environ
+    """
+    data = {}
+
+    def __init__(self, source, region):
+        self.client = boto3.client('secretsmanager', region_name=region)
+        try:
+            response = self.client.get_secret_value(SecretId=source)
+            parsed_secrets = json.loads(response['SecretString'])
+            for k, v in parsed_secrets.items():
+                self.data[k] = v
+        except self.client.exceptions.ClientError as e:
+            raise AWSException(str(e)) from e
+
+    def __contains__(self, key):
+        return key in os.environ or key in self.data
+
+    def __getitem__(self, key):
+        return self.data[key]
