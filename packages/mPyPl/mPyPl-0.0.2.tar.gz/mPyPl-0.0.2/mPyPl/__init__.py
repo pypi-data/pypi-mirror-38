bd8f7a8b4238181dd56ef08885f8fa3b5ad903c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:46 +0000

    Add package mPyPl/0.0.2/mPyPl-0.0.2.tar.gz

---
+++ packages/mPyPl/mPyPl-0.0.2.tar.gz/mPyPl-0.0.2/mPyPl/__init__.py
@@ -0,0 +1,12 @@
+# mPyPl - Monadic Pipeline Library for Python
+# http://github.com/shwars/mPyPl
+
+from .mdict import *
+from .core import *
+from .funcs import *
+from .video import  *
+from .keras import *
+from .multiclass_datastream import *
+from .xmlstream import *
+from .utils.pipeutils import *
+from .utils.coreutils import *
---
+++ packages/mPyPl/mPyPl-0.0.2.tar.gz/mPyPl-0.0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+    
+setuptools.setup(
+    name='mPyPl',
+    packages=setuptools.find_packages(),
+    version='0.0.2',
+    install_requires=['pipe>=1.5.0','numpy>=1.11'],
+    description='Monadic Pipeline Library for Python',
+    author='Dmitri Soshnikov',
+    author_email='dmitri@soshnikov.com',
+    url='https://github.com/shwars/mPyPl',
+#    download_url='https://github.com/JulienPalard/Pipe/tarball/master',
+    long_description=readme,
+    long_description_content_type='text/markdown; charset=UTF-8',
+    license='MIT license',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+#        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
