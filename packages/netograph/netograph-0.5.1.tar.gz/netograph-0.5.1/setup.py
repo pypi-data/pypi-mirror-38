443b8656439e913d21bbb65037e80608c1ea7fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package netograph/0.5.1/netograph-0.5.1.tar.gz

---
+++ packages/netograph/netograph-0.5.1.tar.gz/netograph-0.5.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netograph",
+    version="0.5.1",
+    description="A Python interface to netograph.io.",
+    url="https://netograph.io",
+    author="Netograph Ltd",
+    author_email="aldo@netograph.io",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "grpcio>=1.13",
+        "googleapis-common-protos"
+    ],
+    extras_require={
+        'dev': [
+            "grpcio-tools",
+            "flake8",
+        ],
+    }
+)
