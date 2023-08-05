7106a166b5bda0708da8ccc6d99e0b905d7b9296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package pycopula/0.1.3/pycopula-0.1.3.tar.gz

---
+++ packages/pycopula/pycopula-0.1.3.tar.gz/pycopula-0.1.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+	long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+	required = f.read().splitlines()
+
+setup(name='pycopula',
+      version='0.1.3',
+      description='Python copulas library for dependency modeling',
+      author='Maxime Jumelle',
+      author_email='maxime@aipcloud.io',
+      license="Apache 2",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/MaximeJumelle/pycopula/',
+      install_requires=required,
+      packages=find_packages(include=['pycopula', 'pycopula.*']),
+     )
