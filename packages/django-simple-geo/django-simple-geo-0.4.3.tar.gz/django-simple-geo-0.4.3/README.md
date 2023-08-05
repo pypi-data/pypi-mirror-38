966c9f28226cc570342a43e213b744abedb46204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:10 +0000

    Add package django-simple-geo/0.4.3/django-simple-geo-0.4.3.tar.gz

---
+++ packages/django-simple-geo/django-simple-geo-0.4.3.tar.gz/django-simple-geo-0.4.3/README.md
@@ -0,0 +1,17 @@
+This project is a work in progress.
+
+##### Changes
+
+* 2018-11-07, v0.4.3: Django 2.x deprecation fixes.
+
+* 2018-05-04, v0.4.2: Google Geocode API fix.
+
+* 2018-05-03, v0.4.1: Google Geocode API update.
+
+* 2017-11-27, v0.4.0: Removing deprecated usage of GeoManager.
+
+* 2017-03-09, v0.3.0: Django 1.10 related fixes.
+
+* 2017-03-08, v0.2.0: Python3 related fixes.
+
+* 2015-04-28, v0.1.7: Django 1.9 deprecation warning fix. `get_model` related.
---
+++ packages/django-simple-geo/django-simple-geo-0.4.3.tar.gz/django-simple-geo-0.4.3/setup.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import simple_geo
+
+setup(
+    name='django-simple-geo',
+    description='Simple Django city, state, country geo-spatially aware model and utilities.',
+    long_description='View `django-simple-geo documentation on Github  <https://github.com/jaddison/django-simple-geo>`_.',
+    author='James Addison',
+    author_email='addi00+github.com@gmail.com',
+    packages=[
+        'simple_geo'
+    ],
+    version=simple_geo.__version__,
+    url='http://github.com/jaddison/django-simple-geo',
+    keywords=['django', 'geo', 'gis', 'geodjango', 'geocoding', 'longitude', 'latitude', 'geospatial', 'cities', 'coordinates'],
+    license='BSD',
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'License :: OSI Approved :: BSD License',
+      'Intended Audience :: Developers',
+      'Environment :: Web Environment',
+      'Programming Language :: Python',
+      'Framework :: Django',
+      'Topic :: Internet :: WWW/HTTP :: WSGI',
+    ],
+)
