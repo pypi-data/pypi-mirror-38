64784a6c81a650cb8c385dc462c5a37364573c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:38 +0000

    Add package django-plans-payments/0.4.0/django-plans-payments-0.4.0.tar.gz

---
+++ packages/django-plans-payments/django-plans-payments-0.4.0.tar.gz/django-plans-payments-0.4.0/plans_payments/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from . import models
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'variant',
+        'status',
+        'fraud_status',
+        'currency',
+        'total',
+        'created',
+        'modified',
+    )
+    readonly_fields = (
+        'created',
+        'modified',
+    )
+
+
+admin.site.register(models.Payment, PaymentAdmin)
