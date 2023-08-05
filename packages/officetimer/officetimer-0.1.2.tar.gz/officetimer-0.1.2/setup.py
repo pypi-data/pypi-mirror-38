2cd22af52cfd994eab47b6aff975eb59991acee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:14 +0000

    Add package officetimer/0.1.2/officetimer-0.1.2.tar.gz

---
+++ packages/officetimer/officetimer-0.1.2.tar.gz/officetimer-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='officetimer',
+    version='0.1.2',
+    packages=['officetimer'],
+    scripts=['bin/office_timer'],
+    url='https://github.com/adityakamble49/office-timer',
+    license='Apache-2.0',
+    author='adityakamble49',
+    author_email='adityakamble49@gmail.com',
+    description='Office Timer - Manage work life balance'
+)
