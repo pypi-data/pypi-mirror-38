ccfb69a737bbba5924d23807a42ce6639de0d3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:19 +0000

    Add package httpstan/0.6.1/httpstan-0.6.1-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/httpstan/httpstan-0.6.1-cp36-cp36m-manylinux1_x86_64.whl/httpstan/routes.py
@@ -0,0 +1,44 @@
+"""Routes for httpstan.
+
+Routes for the HTTP server are defined here.
+"""
+import json
+
+import apispec
+
+import httpstan
+import httpstan.schemas as schemas
+import httpstan.views as views
+
+
+def setup_routes(app):
+    """Add routes to Application.
+
+    Arguments:
+        app (aiohttp.Application): Application to which routes should be added.
+
+    """
+    app.router.add_get("/v1/health", views.handle_health)
+    app.router.add_post("/v1/models", views.handle_models)
+    app.router.add_post("/v1/models/{model_id}/params", views.handle_show_params)
+    app.router.add_post("/v1/models/{model_id}/fits", views.handle_create_fit)
+    app.router.add_get("/v1/models/{model_id}/fits/{fit_id}", views.handle_get_fit)
+
+
+def openapi_spec() -> str:
+    """Return OpenAPI (fka Swagger) spec for API."""
+    spec = apispec.APISpec(
+        title="httpstan API", version=httpstan.__version__, plugins=["apispec.ext.marshmallow"]
+    )
+    spec.add_path(path="/v1/health", view=views.handle_health)
+    spec.add_path(path="/v1/models", view=views.handle_models)
+    spec.add_path(path="/v1/models/{model_id}/params", view=views.handle_show_params)
+    spec.add_path(path="/v1/models/{model_id}/fits", view=views.handle_create_fit)
+    spec.add_path(path="/v1/models/{model_id}/fits/{fit_id}", view=views.handle_get_fit)
+    spec.definition("CreateModelRequest", schema=schemas.CreateModelRequest)
+    spec.definition("CreateFitRequest", schema=schemas.CreateFitRequest)
+    spec.definition("Error", schema=schemas.Error)
+    spec.definition("Fit", schema=schemas.Fit)
+    spec.definition("Model", schema=schemas.Model)
+    spec.definition("Parameter", schema=schemas.Parameter)
+    return json.dumps(spec.to_dict())
---
+++ packages/httpstan/httpstan-0.6.1-cp36-cp36m-manylinux1_x86_64.whl/httpstan/schemas.py
@@ -0,0 +1,58 @@
+import marshmallow
+import marshmallow.fields as fields
+import marshmallow.validate as validate
+
+
+class Status(marshmallow.Schema):
+    """Part of Error schema."""
+
+    code = fields.Integer(required=True)
+    status = fields.String(required=True)
+    message = fields.String(required=True)
+    details = fields.List(fields.Dict())
+
+
+class Error(marshmallow.Schema):
+    """Error schema.
+
+    Follows Google's API Design.
+    """
+
+    error = fields.Nested(Status, required=True)
+
+
+class CreateModelRequest(marshmallow.Schema):
+    program_code = fields.String(required=True)
+
+
+class Model(marshmallow.Schema):
+    name = fields.String(required=True)
+    compiler_output = fields.String(required=True)
+
+
+# TODO(AR): supported functions can be fetched from stub Python files
+SERVICES_FUNCTION_NAMES = frozenset(
+    {"stan::services::sample::hmc_nuts_diag_e", "stan::services::sample::hmc_nuts_diag_e_adapt"}
+)
+
+
+class CreateFitRequest(marshmallow.Schema):
+    function = fields.String(required=True, validate=validate.OneOf(SERVICES_FUNCTION_NAMES))
+    data = fields.Dict(missing={})
+
+
+class Fit(marshmallow.Schema):
+    # e.g., models/15d69926a05591e1/fits/66ff16fc9d25cd29
+    name = fields.String(required=True)
+
+
+class ShowParamsRequest(marshmallow.Schema):
+    data = fields.Dict(required=True)
+
+
+class Parameter(marshmallow.Schema):  # noqa
+    """Schema for single parameter."""
+
+    name = fields.String(required=True)
+    dims = fields.List(fields.Integer(), required=True)
+    constrained_names = fields.List(fields.String(), required=True)
