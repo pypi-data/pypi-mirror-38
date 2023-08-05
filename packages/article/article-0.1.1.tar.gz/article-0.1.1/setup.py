da08dc0f421fcdec45598fbd34f23a9150d8be19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:41 +0000

    Add package article/0.1.1/article-0.1.1.tar.gz

---
+++ packages/article/article-0.1.1.tar.gz/article-0.1.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='article',
+    version='0.1.1',
+    description='Turn your Python script into a report with inline figures.',
+    url='http://github.com/danijar/article',
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
