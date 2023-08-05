61c53d1af9146dfcc6fb01928265d73162ae3e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:36 +0000

    Add package WatsonReport/0.0.5/WatsonReport-0.0.5.tar.gz

---
+++ packages/WatsonReport/WatsonReport-0.0.5.tar.gz/WatsonReport-0.0.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='WatsonReport',
+    author="Kobe De Decker",
+    author_email="kobededecker@gmail.com",
+    description="watson reporting for imdc",
+    version='0.0.5',
+    packages=find_packages(),
+    install_requires=['pandas', 'td-watson'],
+    url="https://gitlab.com/kobededecker/watsonreport",
+    scripts=[r".\scripts\watsonreport.bat", r".\scripts\watsonreport.py"],
+    license='MIT',
+)
