9c7e5d20f070051a79c6ae59aeace126ad08b64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package bespin-cli/0.0.11/bespin_cli-0.0.11-py2.py3-none-any.whl

---
+++ packages/bespin-cli/bespin_cli-0.0.11-py2.py3-none-any.whl/bespin/exceptions.py
@@ -0,0 +1,27 @@
+
+class UserInputException(Exception):
+    pass
+
+
+class IncompleteJobFileException(UserInputException):
+    pass
+
+
+class InvalidFilePathException(UserInputException):
+    pass
+
+
+class FileDoesNotExistException(UserInputException):
+    pass
+
+
+class ProjectDoesNotExistException(UserInputException):
+    pass
+
+
+class JobDoesNotExistException(UserInputException):
+    pass
+
+
+class WorkflowConfigurationNotFoundException(UserInputException):
+    pass
