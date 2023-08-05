7b27b126be1212ccdebbf39872f8056330cbde5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package py-xsb/1.0.0/py-xsb-1.0.0.tar.gz

---
+++ packages/py-xsb/py-xsb-1.0.0.tar.gz/py-xsb-1.0.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name             ='py-xsb',
+      version          ='1.0.0',
+      description      ='Python interface for XSB',
+      long_description = open('README.md').read(),
+      url              ='https://github.com/gooofy/py-xsb',
+      classifiers      = [
+                          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                          'Topic :: Software Development :: Libraries :: Python Modules',
+                          'Operating System :: POSIX :: Linux',
+                          'Operating System :: Microsoft :: Windows',
+                          'Operating System :: MacOS :: MacOS X',
+                          'License :: OSI Approved :: Apache Software License',
+                          'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2.7',
+                          'Programming Language :: Python :: 3',
+                          'Programming Language :: Python :: 3.6',
+                          'Programming Language :: Prolog',
+                          'Intended Audience :: Developers',
+                          'Intended Audience :: Science/Research',
+                         ],
+      keywords         = 'XSB interface AI artificial intelligence reasoning programming in logic',
+      platforms        = 'Linux',
+      license          = 'Apache',
+      package_dir      = {'pyxsb': 'pyxsb'},
+      packages         = ['pyxsb'],
+      author           = "Guenter Bartsch",
+      author_email     = "guenter@zamia.org",
+      )
