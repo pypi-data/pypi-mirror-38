4173b17a71448d9a55af6a09d0a9f55bf107e239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:01 +0000

    Add package chronologicon/4.62/chronologicon-4.62.tar.gz

---
+++ packages/chronologicon/chronologicon-4.62.tar.gz/chronologicon-4.62/README.md
@@ -0,0 +1,74 @@
+# Chronologicon
+v4.62 — 181103
+
+A minimal time tracker, now rewritten for the command line. Chronologicon records your work sessions and displays graphs based on your projects.
+
+Install:
+`$ pip install chronologicon`
+
+&nbsp;
+
+
+![Example screenshot](screenshot.png)
+
+
+## Logs
+Chronologicon stores work sessions as *logs.* Each log has a named *discipline* and *project,* along with an optional note. The project should be self-explanatory; the discipline refers to the general type of work. I separate mine into *visual, code,* and *research*, but you should use whichever categories feel most suited to your workflow.
+
+The note is optional, but may be useful to you for recording the specific task you're working on.
+
+A list of all logs is saved in `logs.json`, in Chronologicon's save directory. This file can (and should) be backed up with the `$ chron -b` command.
+
+`stat.json` contains a more lightweight summary of these log data, which is used to display the graphs. It's overwritten every time you complete a log.
+
+
+## Commands
+`$ chron` with no arguments will run preflight checks. This will create any mission-critical files which are currently missing. If everything is OK, it returns nothing.
+
+```
+-s <args>   Start a new log timer
+-x          Complete the current log
+-v <args>   View stats & graphs
+-b          Backup the log data file
+-d <dir>    Change the save directory
+--cancel    Abort the current entry
+```
+
+The first time you use Chronologicon, you'll need to specify a save directory with the `-d` argument.
+
+
+## Usage
+
+`$ chron -d ~/Documents/Chron` Change the save directory to a folder on your computer.
+
+`$ chron -s 'discipline' 'project' 'note'`
+Create a new log with discipline, project, and (optionally) a note.
+
+`$ chron -x`
+Stop tracking and save the current log.
+
+
+## Combinations
+
+Because the commands Chronologicon takes are all technically optional arguments, you can use them in combination with each other:
+
+`$ chron -x -s <args>` will stop the current log and start a new one with the specified discipline and project.
+
+`$ chron -x -v` will stop the current log and then display statistics.
+
+I haven't tested every combination, so use them at your own risk (and make regular backups!)
+
+
+## Extra
+
+`$ chron -v verbose` will return additional projects (every project with a graph width of 1 character or more).
+
+`$ chron -v uniform` will make all project graphs full-width.
+
+Using `uniform` and `verbose` in combination will display all projects.
+
+The `stats.json` file is used for displaying graphs, but it's only updated when a new log is completed. To force an update to this file, you can use `$ chron -v refresh`.
+
+Chron stores in-progress logs in a temporary file in its install directory, meaning you can safely exit all terminal windows, restart your computer, and install updates while logging. I don't recommend relying on this feature, but I hope it can provide some peace of mind.
+
+— R
---
+++ packages/chronologicon/chronologicon-4.62.tar.gz/chronologicon-4.62/chronologicon/output.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Chronologicon v4.x
+# Rutherford Craze
+# https://craze.co.uk
+# 181028
+
+import chronologicon, operator
+
+STATS_FILENAME = chronologicon.STATS_FILENAME
+STATS = {}
+BAR_WIDTH = 50
+MVP_DISC = []
+
+# Console colour ANSI escapes
+class colors:
+	RED = u"\u001b[31m"
+	BLUE = u"\u001b[34m"
+	GREY = u"\u001b[37m"
+	RESET = u"\u001b[0m"
+	DEBUG = u"\u001b[35m"
+
+def GetDbt(byProject = None, graphWidth = BAR_WIDTH):
+	global MVP_DISC
+
+	if byProject is None:
+		dbt = sorted(STATS['discbytime'].items(), key=operator.itemgetter(1))
+	else:
+		dbt = sorted(STATS['projbydisc'][byProject].items(), key=operator.itemgetter(1))
+	dbtGraph = "  "
+	dbtKey = "  "
+	R = 0;
+
+	for discipline in range(len(dbt)):
+		if discipline < 3:
+			k = dbt[len(dbt) - 1 - discipline][0]
+			v = dbt[len(dbt) - 1 - discipline][1]
+			MVP_DISC.append(str(k))
+
+			if byProject is None:
+				bar = int(float(v) / float(STATS['totaltime']) * graphWidth)
+			else:
+				bar = int(float(v) / float(STATS['projbytime'][byProject]) * graphWidth)
+
+			R += bar
+
+			if MVP_DISC[0] == k:
+				dbtGraph += (bar * u"\u2588")
+				dbtKey += str(k) + "  "
+			elif len(MVP_DISC) > 1 and MVP_DISC[1] == k:
+				dbtGraph += colors.RED + (bar * u"\u2588") + colors.RESET
+				dbtKey += colors.RED + str(k) + colors.RESET + "  "
+			elif len(MVP_DISC) > 2 and MVP_DISC[2] == k:
+				dbtGraph += colors.BLUE + (bar * u"\u2588") + colors.RESET
+				dbtKey += colors.BLUE + str(k) + colors.RESET + "  "
+			else:
+				R -= bar # Ignore 'other' stuff for now, put it at the end
+
+	if R < graphWidth:
+		dbtGraph += colors.GREY + ((graphWidth - R) * u"\u2588") + colors.RESET
+		dbtKey += colors.GREY + "other" + colors.RESET
+
+	return (dbtGraph, dbtKey)
+
+def GetWbh():
+	height = 6
+	maxValue = max(STATS['workbyhour'].items(), key=operator.itemgetter(1))[1]
+	wbh = sorted(STATS['workbyhour'].items())
+	wbhClamped = []
+	wbhGraph = "  | "
+	wbhKey = "  | "
+
+	c = 0
+	for hour in range(24):
+		if c < len(wbh):
+			if wbh[c][0] == str(hour).zfill(2):
+				wbhClamped.append(int(float(wbh[c][1]) / float(maxValue) * height))
+				c += 1
+			else:
+				wbhClamped.append(0)
+		else: wbhClamped.append(0)
+
+	for row in range(height):
+		if row > 0:
+			wbhGraph += "\n  | "
+		for col in range(len(wbhClamped)):
+			if wbhClamped[col] >= height - row:
+				wbhGraph += u"\u2590\u258C"
+			else:
+				wbhGraph += "  "
+
+	for col in range(0, len(wbhClamped), 3):
+		wbhKey += str(col).zfill(2) + '    '
+
+	return(wbhGraph, wbhKey)
+
+def GetPbt(verbose = False, uniform = False):
+	if verbose == False:
+		projects = 5
+	else:
+		projects = len(STATS['projbytime'])
+
+	graphWidth = BAR_WIDTH;
+	pbt = sorted(STATS['projbytime'].items(), key=operator.itemgetter(1))
+	maxValue = max(STATS['projbytime'].items(), key=operator.itemgetter(1))[1]
+	pbtList = ""
+
+	for project in range(len(pbt)):
+		if project < projects:
+			k = pbt[len(pbt) - 1 - project][0]
+
+			# Skip blank project names
+			if k == '':
+				continue
+
+			v = pbt[len(pbt) - 1 - project][1]
+			spacer = BAR_WIDTH - len(k) - len(str(v)) + 1
+
+			if uniform:
+				pBarWidth = graphWidth
+			else:
+				pBarWidth = int(float(v) / float(maxValue) * graphWidth)
+
+			# Skip zero-width graph bars
+			if pBarWidth < 1:
+				continue
+
+			dbtGraph, dbtKey = GetDbt(k, pBarWidth)
+
+			pbtList += dbtGraph + "\n"
+			pbtList += "  " + str(k) + (spacer * ' ') + str(v / 60 / 60) + " h\n\n"
+
+	return pbtList
+
+
+def ViewStats(args):
+	global STATS
+	STATS = chronologicon.LoadStats()
+	STATS = STATS[0]
+
+	if STATS == False:
+		print("Unable to load file: " + STATS_FILENAME + ". Please ensure it exists.")
+		return
+
+	# Overview numbers
+	TotalEntries = "  Total Entries:    " + str(STATS['totallogs'])
+	TotalTime =    "  Total Time:       " + str(int(STATS['totaltime']/60/60)) + " h"
+	AvgEntry =     "  Average Log:      " + str(STATS['avgloglength']//60) + " m\n"
+
+	# Graphs
+	dbtGraph, dbtKey = GetDbt()
+	wbhGraph, wbhKey = GetWbh()
+
+	if len(args) > 0:
+		verbose = False
+		uniform = False
+
+		if 'refresh' in args:
+			chronologicon.SaveStats()
+			STATS = chronologicon.LoadStats() # Reload
+			STATS = STATS[0]
+
+		if 'verbose' in args:
+			verbose = True
+
+		if 'uniform' in args:
+			uniform = True
+
+		pbtList = GetPbt(verbose, uniform)
+	else:
+		pbtList = GetPbt()
+
+
+	print("\n\n  ─── Chronologicon 4.62 ─── Statistics Overview ───\n\n")
+
+	print(TotalEntries)
+	print(TotalTime)
+	print(AvgEntry)
+
+	print('\n  Work by discipline\n')
+	print(dbtGraph.encode('utf-8'))
+	print(dbtKey)
+
+	print('\n\n  Work by hour')
+	print(wbhGraph.encode('utf-8'))
+	print('  | ' + (BAR_WIDTH - 2) * '─')
+	print(wbhKey)
+
+	print('\n\n  Largest projects\n')
+	print(pbtList.encode('utf-8'))
