a7490f88ab2552fd39d1531f9261de3adea74323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package hdate/0.7.1/hdate-0.7.1.tar.gz

---
+++ packages/hdate/hdate-0.7.1.tar.gz/hdate-0.7.1/tests/conftest.py
@@ -0,0 +1,31 @@
+"""Fixtures for py.test."""
+
+import datetime
+import random
+from calendar import isleap
+
+import pytest
+
+import hdate
+
+
+@pytest.fixture
+def random_date():
+    """Generate a random valid date."""
+    year = random.randint(400, 2500)
+    month = random.randint(1, 12)
+    maxday = 31 if month in [1, 3, 5, 7, 8, 10, 12] else 30
+    if month == 2:
+        if isleap(year):
+            maxday = 29
+        else:
+            maxday = 28
+    day = random.randint(1, maxday)
+    return year, month, day
+
+
+@pytest.fixture
+def rand_date(random_date):
+    """Given a random date, generate arandom HDate."""
+    date = datetime.date(*random_date)
+    return hdate.HDate(date)
