cfbe29f89996b6916cc1b2fd6ec032999447af8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package zope.contentprovider/4.2.1/zope.contentprovider-4.2.1.tar.gz

---
+++ packages/zope.contentprovider/zope.contentprovider-4.2.1.tar.gz/zope.contentprovider-4.2.1/README.rst
@@ -0,0 +1,29 @@
+======================
+ zope.contentprovider
+======================
+
+.. image:: https://img.shields.io/pypi/v/zope.contentprovider.svg
+        :target: https://pypi.python.org/pypi/zope.contentprovider/
+        :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/zope.contentprovider.svg
+        :target: https://pypi.org/project/zope.contentprovider/
+        :alt: Supported Python versions
+
+.. image:: https://travis-ci.org/zopefoundation/zope.contentprovider.svg?branch=master
+        :target: https://travis-ci.org/zopefoundation/zope.contentprovider
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.contentprovider/badge.svg?branch=master
+        :target: https://coveralls.io/github/zopefoundation/zope.contentprovider?branch=master
+
+.. image:: https://readthedocs.org/projects/zopecontentprovider/badge/?version=latest
+        :target: https://zopecontentprovider.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+This package provides a framework to develop componentized Web GUI
+applications. Instead of describing the content of a page using a single
+template or static system of templates and METAL macros, content provider
+objects are dynamically looked up based on the setup/configuration of the
+application.
+
+Detailed documentation is available at https://zopecontentprovider.readthedocs.io
---
+++ packages/zope.contentprovider/zope.contentprovider-4.2.1.tar.gz/zope.contentprovider-4.2.1/setup.py
@@ -0,0 +1,95 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.contentprovider package"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+TESTS_REQUIRE = [
+    'zope.browserpage>=3.12',
+    'zope.testing',
+    'zope.testrunner',
+]
+
+
+setup(
+    name='zope.contentprovider',
+    version='4.2.1',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Content Provider Framework for Zope Templates',
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+    ),
+    keywords="zope3 content provider",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3',
+    ],
+    url='https://github.com/zopefoundation/zope.contentprovider',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope'],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+        ],
+    },
+    install_requires=[
+        'setuptools',
+        'zope.component',
+        'zope.event',
+        'zope.interface >= 3.8',
+        'zope.location',
+        'zope.publisher',
+        'zope.schema',
+        'zope.tales',
+    ],
+    include_package_data=True,
+    tests_require=TESTS_REQUIRE,
+    test_suite='zope.contentprovider.tests.test_suite',
+    zip_safe=False,
+)
