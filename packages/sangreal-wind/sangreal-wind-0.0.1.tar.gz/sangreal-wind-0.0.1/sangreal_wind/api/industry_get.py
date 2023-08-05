a0cd1895d25ddab507e16ccaacae8c9d88fa3ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:29 +0000

    Add package sangreal-wind/0.0.1/sangreal-wind-0.0.1.tar.gz

---
+++ packages/sangreal-wind/sangreal-wind-0.0.1.tar.gz/sangreal-wind-0.0.1/sangreal_wind/api/industry_get.py
@@ -0,0 +1,55 @@
+from fastcache import lru_cache
+from sangreal_calendar import adjust_trade_dt
+from sqlalchemy import func
+from sqlalchemy.exc import OperationalError
+
+from ..utils.engines import WIND_DB
+
+
+def industry(trade_dt, sid=None, level=1):
+    """返回trade_dt时间截面上对应sid的中信行业分类.
+
+    Args:
+        trade_dt: str or datetime.
+        sid: str or tuple.
+        level: level of zx industry.
+        
+    Returns:
+        DataFrame like |sid|trade_dt(datetime)|ind_sid|ind_name|
+    """
+    df = industry_all(level)
+    if sid is not None:
+        sid = {sid} if isinstance(sid, str) else set(sid)
+        df = df[df['sid'].isin(sid)]
+
+    df = df.loc[(df['entry_dt'] <= trade_dt) & (
+        (df['out_dt'] >= trade_dt) | (df['out_dt'].isnull()))].copy()
+    df['trade_dt'] = adjust_trade_dt(trade_dt)
+    return df[[
+        'sid',
+        'trade_dt',
+        'ind_name',
+    ]]
+
+
+@lru_cache()
+def industry_all(level=1):
+    clss = WIND_DB.ASHAREINDUSTRIESCLASSCITICS
+    ind_code = WIND_DB.ASHAREINDUSTRIESCODE
+    df = WIND_DB.query(
+        clss.S_INFO_WINDCODE, clss.ENTRY_DT, clss.REMOVE_DT,
+        ind_code.INDUSTRIESNAME).filter(ind_code.LEVELNUM == (level + 1))
+    try:
+        df = df.filter(
+            func.substring(clss.CITICS_IND_CODE, 1, 4) == func.substring(
+                ind_code.INDUSTRIESCODE, 1, 4)).to_df()
+    except OperationalError:
+        df = df.filter(
+            func.substr(clss.CITICS_IND_CODE, 1, 2 + 2 * level) == func.substr(
+                ind_code.INDUSTRIESCODE, 1, 2 + 2 * level)).to_df()
+    df.columns = ['sid', 'entry_dt', 'out_dt', 'ind_name']
+    return df
+
+
+if __name__ == '__main__':
+    print(industry_all().head())
