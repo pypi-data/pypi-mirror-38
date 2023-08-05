7fb8add488cd0dd0519f0360f082460bcc2d6244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:04 +0000

    Add package ugtm/1.1.8/ugtm-1.1.8.tar.gz

---
+++ packages/ugtm/ugtm-1.1.8.tar.gz/ugtm-1.1.8/ugtm/__init__.py
@@ -0,0 +1,7 @@
+from .ugtm_plot import *
+from .ugtm_landscape import *
+from .ugtm_gtm import *
+from .ugtm_kgtm import *
+from .ugtm_predictions import *
+from .ugtm_crossvalidate import *
+from .ugtm_preprocess import *
