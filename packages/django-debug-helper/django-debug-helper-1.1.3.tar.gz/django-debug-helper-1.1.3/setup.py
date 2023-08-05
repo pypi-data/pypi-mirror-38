44f3d4d85aa266bb7d2fcbe3a036adc103f8e2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:14 +0000

    Add package django-debug-helper/1.1.3/django-debug-helper-1.1.3.tar.gz

---
+++ packages/django-debug-helper/django-debug-helper-1.1.3.tar.gz/django-debug-helper-1.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-debug-helper",
+    version="1.1.3",
+    author="Mohd. Shafikur Rahman",
+    author_email="shafikshaon@gmail.com",
+    description="Django debug error related exception search on Google/ Stack Overflow.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shafikshaon/django-debug-helper",
+    packages=['django-debug-helper'],
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+    ],
+)
