7818cb10a3eb2441c41b520e54b75ff1f0efb38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:40 +0000

    Add package demo-project-test-time/1.1111114/demo_project_test_time-1.1111114.tar.gz

---
+++ packages/demo-project-test-time/demo_project_test_time-1.1111114.tar.gz/demo_project_test_time-1.1111114/demo_project_test_time/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import time
+import numpy as np
+import pandas as pd
+from test_module.test_main import abc
+
+df = pd.DataFrame({"id": [1001, 1002, 1003, 1004, 1005, 1006],
+                   "date": pd.date_range('20130102', periods=6),
+                   "city": ['Beijing ', 'SH', ' guangzhou ', 'Shenzhen', 'shanghai', 'BEIJING '],
+                   "age": [23, 44, 54, 32, 34, 32],
+                   "category": ['100-A', '100-B', '110-A', '110-C', '210-A', '130-F'],
+                   "price": [1200, np.nan, 2133, 5433, np.nan, 4432]},
+                  columns=['id', 'date', 'city', 'category', 'age', 'price'])
+
+
+def test():
+    print("hello world!")
+    print('The time is:', time.time())
+    print(df.shape)
+    abc()
+
+
+if __name__ == '__main__':
+    test()
