37c4741e27585a32b86370196eb651e5421a833a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:39 +0000

    Add package statuscheck/0.5.0/statuscheck-0.5.0-py2.py3-none-any.whl

---
+++ packages/statuscheck/statuscheck-0.5.0-py2.py3-none-any.whl/statuscheck/cli.py
@@ -0,0 +1,46 @@
+import sys
+import click
+
+from .__about__ import __url__
+from statuscheck.check import get_statuscheck_api
+from statuscheck.status_types import TYPE_GOOD, TYPE_UNKNOWN, TYPE_INCIDENT, TYPE_OUTAGE, \
+    TYPE_MAINTENANCE
+
+COLOR_GREEN = '\033[92m'
+COLOR_YELLOW = '\033[93m'
+COLOR_RED = '\033[91m'
+COLOR_DEFAULT = '\033[99m'
+
+STATUSES_COLORS_MAPPING = {
+    TYPE_UNKNOWN: COLOR_DEFAULT,
+    TYPE_GOOD: COLOR_GREEN,
+    TYPE_INCIDENT: COLOR_YELLOW,
+    TYPE_OUTAGE: COLOR_RED,
+    TYPE_MAINTENANCE: COLOR_DEFAULT,
+}
+
+
+@click.command()
+@click.argument('service')
+def main(service):
+    """Console script for statuscheck."""
+    try:
+        service_api = get_statuscheck_api(service)
+    except ModuleNotFoundError:
+        click.echo(f'"{service}" is not implemented, leave a note at {__url__}')
+        return 0
+    status = service_api.get_status()
+    status_type = service_api.get_type()
+    status_color = STATUSES_COLORS_MAPPING.get(status_type, '')
+    incident = service_api.get_active_incident()
+
+    if incident:
+        click.echo(f'{status_color}{status_type or status}: {incident}')
+        click.echo(f'More: {service_api.status_url}')
+    else:
+        click.echo(f'{status_color}{status_type or status}')
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
---
+++ packages/statuscheck/statuscheck-0.5.0-py2.py3-none-any.whl/statuscheck/services/_statusio.py
@@ -0,0 +1,45 @@
+import requests
+
+from statuscheck.services._base import BaseServiceAPI
+from statuscheck.status_types import TYPE_GOOD
+
+
+class BaseStatusioAPI(BaseServiceAPI):
+    STATUS_TYPE_MAPPING = {
+        'Operational': TYPE_GOOD,
+    }
+
+    domain_key = None
+
+    def _get_base_url(self):
+        if not self.domain_key:
+            raise NotImplementedError('Please, add domain key')
+        return f'https://api.status.io/1.0/status/{self.domain_key}'
+
+    def _get_status_data(self):
+        response = requests.get(self._get_base_url())
+        response.raise_for_status()
+        return response.json()['result']
+
+    def get_status(self):
+        if not self.data:
+            self.data = self._get_status_data()
+        return self.data['status_overall']['status']
+
+    def get_type(self):
+        status = self.get_status()
+        status_type = self.STATUS_TYPE_MAPPING.get(status, '')
+        if not status_type:
+            self.capture_log(status)
+        return status_type
+
+    def get_active_incident(self):
+        status_type = self.get_type()
+        if status_type == TYPE_GOOD:
+            return ''
+        incidents = self.data['incidents']
+        if incidents:
+            self.capture_log('NOT_OK', extra=self.data)
+            # TODO: clarify data format
+            return str(incidents[0])
+        return self.get_status()
