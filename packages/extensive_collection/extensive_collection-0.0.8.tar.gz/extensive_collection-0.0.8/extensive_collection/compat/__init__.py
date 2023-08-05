1652f3a46c855f789750ec96f1765a585060b091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package extensive_collection/0.0.8/extensive_collection-0.0.8.tar.gz

---
+++ packages/extensive_collection/extensive_collection-0.0.8.tar.gz/extensive_collection-0.0.8/extensive_collection/compat/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/1 9:48
+# @Author  : yangyong
+# @Email   : yangyong@findourlove.com
+# @File    : __init__.py
+# @Software: PyCharm
+import sys
+if sys.version_info[0] == 2:
+    str_ = unicode
+elif sys.version_info[0] == 3:
+    str_ = str
