4b4dc23eab1fa869245c88d78412f844b1943a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:14 +0000

    Add package MolMod-amg/0.0.3/MolMod-amg-0.0.3.tar.gz

---
+++ packages/MolMod-amg/MolMod-amg-0.0.3.tar.gz/MolMod-amg-0.0.3/README.md
@@ -0,0 +1,2 @@
+# MolMod 
+Molecular and crystal modeling tools
---
+++ packages/MolMod-amg/MolMod-amg-0.0.3.tar.gz/MolMod-amg-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools, os
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="MolMod-amg",
+    version="0.0.3",
+    author="Andriy Zhugayevych, Sergey Matveev, Dmitry Aksenov",
+    description="Manager for QC calculations",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    license = 'GPL',
+    keywords='Density Functional Theory Crystal modeling',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'paramiko'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+)
