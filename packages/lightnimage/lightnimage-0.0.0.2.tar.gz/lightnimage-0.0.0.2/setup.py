b330474409ac792f9a1803fc8e418d465743ea4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:11 +0000

    Add package lightnimage/0.0.0.2/lightnimage-0.0.0.2.tar.gz

---
+++ packages/lightnimage/lightnimage-0.0.0.2.tar.gz/lightnimage-0.0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='lightnimage',
+    version='0.0.0.2',
+    description='A library to analyze images of lightning strikes',
+    url='https://github.com/the16thpythonist/lightnimage',
+    author='the16thplayer',
+    author_email='jonseb1998@gmail.com',
+    license='MIT',
+    packages=[
+        'lightnimage'
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'jupyter'
+    ],
+    python_requires='~=3.6',
+    zip_safe=False
+)
