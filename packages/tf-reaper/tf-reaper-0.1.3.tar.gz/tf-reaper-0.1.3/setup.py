7334a5a9630e60f9f5e8b04b8950cfec9807492f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package tf-reaper/0.1.3/tf-reaper-0.1.3.tar.gz

---
+++ packages/tf-reaper/tf-reaper-0.1.3.tar.gz/tf-reaper-0.1.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='tf-reaper',
+    version='0.1.3',
+    license='MIT',
+    author='Threshing Floor Security, LLC',
+    author_email='info@threshingfloor.io',
+    description='Threshing Floor CLI utility for analyzing log files for noise.',
+    long_description=long_description,
+    entry_points={
+        'console_scripts': ['reaper=tf_reaper.reaper:main'],
+    },
+    packages=['tf_reaper'],
+    install_requires=['libtf>=0.1.2', 'requests>=2,<3', 'six'],
+    url='https://github.com/ThreshingFloor/cli.reaper.threshingfloor.io',
+    classifiers=['Development Status :: 3 - Alpha'],
+)
+
+message = """
+
+__________                                   
+\______   \ ____ _____  ______   ___________    
+ |       _// __ \\\\__  \ \\____ \\_/ __ \\_  __ \\ 
+ |    |   \  ___/ / __ \|  |_> >  ___/|  | \/ 
+ |____|_  /\___  >____  /   __/ \___  >__|    
+        \/     \/     \/|__|        \/        
+
+If you do not have an API key, request one at info@threshingfloor.io
+
+To get started, run \'$ reaper -h\'
+"""
+
+print(message)
