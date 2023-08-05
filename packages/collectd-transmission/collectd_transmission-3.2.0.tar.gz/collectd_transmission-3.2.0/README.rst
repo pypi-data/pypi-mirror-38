5ffa1038cb41bdc103b9b0bf7240741f244c9715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package collectd-transmission/3.2.0/collectd_transmission-3.2.0.tar.gz

---
+++ packages/collectd-transmission/collectd_transmission-3.2.0.tar.gz/collectd_transmission-3.2.0/README.rst
@@ -0,0 +1,92 @@
+Introduction
+============
+
+.. image:: https://img.shields.io/pypi/v/collectd_transmission.svg
+   :target: https://pypi.python.org/pypi/collectd_transmission
+   :alt: PyPi version
+
+.. image:: https://img.shields.io/pypi/dm/collectd_transmission.svg
+   :target: https://pypi.python.org/pypi/collectd_transmission
+   :alt: PyPI Monthly downloads
+
+.. image:: https://travis-ci.org/akosiaris/collectd_transmission.svg?branch=master
+   :target: https://travis-ci.org/akosiaris/collectd_transmission
+   :alt: Build Status
+
+.. image:: https://readthedocs.org/projects/collectd-transmission/badge/?version=latest
+   :target: https://readthedocs.org/projects/collectd-transmission/
+   :alt: Documentation Status
+
+.. image:: https://coveralls.io/repos/github/akosiaris/collectd_transmission/badge.svg?branch=master
+   :target: https://coveralls.io/github/akosiaris/collectd_transmission?branch=master
+   :alt: Code Coverage Status
+
+A python plugin for integrating collectd and transmission. With this
+installed, collectd will be querying transmission for the following:
+
+Per session and cumulative:
+
+* downloadedBytes
+* uploadedBytes
+* filesAdded
+* secondsActive
+
+General:
+
+* activeTorrentCount
+* blocklist\_size
+* downloadSpeed
+* uploadSpeed
+* pausedTorrentCount
+* torrentCount
+
+and creating the relevant RRD files (or pushing to graphite or whatever
+you have collectd doing)
+
+Some of these metrics are per session, some session cumulative, some are
+session independent. Their type 'current', 'cumulative', 'general' is set
+in the name of the metric
+
+Quick Installation
+==================
+
+Debian/Ubuntu assumed. Prereqs installation
+
+.. code-block:: bash
+
+    apt-get install collectd transmission-daemon python-transmissionrpc python-pip
+
+
+And then the actually software
+
+.. code-block:: bash
+
+    pip install collectd_transmission
+
+Configure
+=========
+
+Insert the following in your collectd.conf::
+
+    <Plugin python>
+        LogTraces false
+        Interactive false
+        Import "collectd_transmission"
+        <Module collectd_transmission>
+            username "myuser" # Required
+            password "mypass" # Required
+        </Module>
+    </Plugin>
+
+modified accordingly to your needs. Restart collectd and you are done.
+
+There should be rrds for transmission under collectd's data directory.
+Most probably that is /var/lib/collectd/rrd/<hostname>/transmission/
+
+Documentation
+=============
+
+If you want some actually documentation and more detailed installation
+and/or configuration instructions head over to:
+
+https://pythonhosted.org/collectd_transmission/
---
+++ packages/collectd-transmission/collectd_transmission-3.2.0.tar.gz/collectd_transmission-3.2.0/tests/tests.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import unittest
+import mock
+import sys
+from transmissionrpc.error import TransmissionError
+
+# Monkey patching the collectd module to facilitate testing
+mock_collectd = mock.MagicMock()
+sys.modules['collectd'] = mock_collectd
+
+# This is a hack to force tests to use the in-repo version of the module and not
+# the one installed by tox
+sys.path.insert(0, '.')
+import collectd_transmission
+
+
+class MethodTestCase(unittest.TestCase):
+    def setUp(self):
+        # Mock our configuration to enable testing
+        self.config = mock.Mock()
+        self.config.children = []
+        child1 = mock.Mock()
+        child1.key = 'username'
+        child1.values = ['myusername']
+        child2 = mock.Mock()
+        child2.key = 'password'
+        child2.values = ['mypassword']
+
+        self.config.children.append(child1)
+        self.config.children.append(child2)
+
+    def tearDown(self):
+        del self.config  # The rest will be handled by GC
+
+    def test_config(self):
+        collectd_transmission.config(self.config)
+
+    @mock.patch('collectd_transmission.transmissionrpc.Client')
+    def test_initialize(self, mock_Client):
+        collectd_transmission.config(self.config)
+        collectd_transmission.initialize()
+        mock_Client.assert_called_with(
+            address='http://localhost:9091/transmission/rpc',
+            user='myusername',
+            password='mypassword',
+            timeout=5)
+
+    @mock.patch('collectd_transmission.transmissionrpc.Client',
+            side_effect=TransmissionError)
+    def test_initialize_fail(self, mock_Client):
+        collectd_transmission.config(self.config)
+        collectd_transmission.initialize()
+        mock_Client.assert_called_with(
+            address='http://localhost:9091/transmission/rpc',
+            user='myusername',
+            password='mypassword',
+            timeout=5)
+
+    def test_shutdown(self):
+        collectd_transmission.shutdown()
+
+    @mock.patch('collectd_transmission.transmissionrpc.Client')
+    def test_get_stats(self, mock_Client):
+        collectd_transmission.data['client'] = mock_Client
+        collectd_transmission.get_stats()
+        mock_Client.session_stats.assert_called_with()
+
+    @mock.patch('collectd_transmission.transmissionrpc.Client')
+    def test_get_stats_wrong_transmissionrpc_version(self, mock_Client):
+        collectd_transmission.data['client'] = mock_Client
+        collectd_transmission.transmissionrpc.__version__ = '0.8'
+        try:
+            collectd_transmission.get_stats()
+        except RuntimeError:
+            return True
+
+    @mock.patch('collectd_transmission.transmissionrpc.Client')
+    def test_get_stats_transmissionError_exception(self, mock_Client):
+        mock_Client.session_stats = mock.MagicMock(
+            side_effect=TransmissionError('foo'))
+        collectd_transmission.data['client'] = mock_Client
+        collectd_transmission.get_stats()
+        mock_Client.session_stats.assert_called_with()
+
+    @mock.patch('collectd_transmission.transmissionrpc.Client')
+    def test_get_stats_none_client(self, mock_Client):
+        collectd_transmission.data['client'] = None
+        collectd_transmission.get_stats()
+
+
+if __name__ == '__main__':
+    unittest.main()
