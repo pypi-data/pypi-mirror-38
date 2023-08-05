471677c31e8ff7c823d86c4d0ab9f30d9c7928cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:52 +0000

    Add package segmentation-pipeline/0.11/segmentation_pipeline-0.11.tar.gz

---
+++ packages/segmentation-pipeline/segmentation_pipeline-0.11.tar.gz/segmentation_pipeline-0.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import setuptools
+setup(name='segmentation_pipeline',
+      version='0.11',
+      description='The funniest joke in the world',
+      url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
+      author='Petrochenko Pavel',
+      author_email='petrochenko.pavel.a@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      include_package_data=True,
+
+      install_requires=["numpy", "scipy","Pillow", "cython","matplotlib", "scikit-image","tensorflow>=1.3.0","keras>=2.0.8","imageio",
+"opencv-python",
+"h5py",
+"imgaug",
+"segmentation_models"],
+      zip_safe=False)
