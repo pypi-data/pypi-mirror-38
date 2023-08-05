c842746262beb1301be5927df430725e9ad0f0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package hdate/0.7.1/hdate-0.7.1-py2.py3-none-any.whl

---
+++ packages/hdate/hdate-0.7.1-py2.py3-none-any.whl/hdate/__init__.py
@@ -0,0 +1,10 @@
+"""
+Jewish calendrical date and times for a given location.
+
+HDate calculates and generates a represantation either in English or Hebrew
+of the Jewish calendrical date and times for a given location
+"""
+from hdate.date import HDate
+from hdate.zmanim import Zmanim
+
+__all__ = ['HDate', 'Zmanim']
