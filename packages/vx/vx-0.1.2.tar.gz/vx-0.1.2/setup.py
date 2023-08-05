42cbcf7541f9f3eb4a7ed4d0cab19dbae00c3e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package vx/0.1.2/vx-0.1.2.tar.gz

---
+++ packages/vx/vx-0.1.2.tar.gz/vx-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'vx',
+  packages = ['vx'], # this must be the same as the name above
+  version = '0.1.2',
+  description = 'Python implementation of the SBB genetic programming algorithm for classification tasks (library).',
+  author = 'Mat Kallada',
+  author_email = 'matkallada@gmail.com',
+  url = 'https://pypi.org/project/vx/', # use the URL to the github repo
+  keywords = ['machine learning', 'genetic programming', 'example'], # arbitrary keywords
+  classifiers = [],
+)
