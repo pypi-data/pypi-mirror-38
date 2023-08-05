d2ab0defe8860d6f54d1f7e67acf0105ee9fbb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package tkbeacon/1.3.0/tkbeacon-1.3.0.tar.gz

---
+++ packages/tkbeacon/tkbeacon-1.3.0.tar.gz/tkbeacon-1.3.0/tkbeacon/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from tkbeacon.api.concepts_api import ConceptsApi
+from tkbeacon.api.metadata_api import MetadataApi
+from tkbeacon.api.statements_api import StatementsApi
