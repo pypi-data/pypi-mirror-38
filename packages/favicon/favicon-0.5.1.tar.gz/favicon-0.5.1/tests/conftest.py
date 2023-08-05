cfe890653e12c4d563877fdf6160e7453460b598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package favicon/0.5.1/favicon-0.5.1.tar.gz

---
+++ packages/favicon/favicon-0.5.1.tar.gz/favicon-0.5.1/tests/conftest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pytest
+import requests_mock
+
+
+@pytest.fixture
+def m():
+    mocker = requests_mock.mock()
+    mocker.start()
+    yield mocker
+    mocker.stop()
