4c0ff86fe3ccf6df768bc0450513f04aa2d42811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:22 +0000

    Add package hffe/0.0.0/hffe-0.0.0.tar.gz

---
+++ packages/hffe/hffe-0.0.0.tar.gz/hffe-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hffe',
+      version='0.0.0',
+      description='Implements high-frequency financial econometrics tools.',
+      url='http://github.com/salompas/hffe',
+      author='Guilherme Salome',
+      author_email='guilhermesalome@gmail.com',
+      license='MIT',
+      packages=['hffe'],
+      zip_safe=False)
