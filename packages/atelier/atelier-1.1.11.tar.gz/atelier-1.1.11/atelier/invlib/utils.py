74763e162baa6450838b84acf8ae319822acef4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package atelier/1.1.11/atelier-1.1.11.tar.gz

---
+++ packages/atelier/atelier-1.1.11.tar.gz/atelier-1.1.11/atelier/invlib/utils.py
@@ -0,0 +1,214 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017-2018 Rumma & Ko Ltd
+# License: BSD, see LICENSE for more details.
+
+"""Utilities for atelier.invlib
+
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from invoke.exceptions import Exit
+
+from atelier.utils import confirm, cd
+
+
+def must_confirm(*args, **kwargs):
+    if not confirm(''.join(args)):
+        raise Exit("User failed to confirm.")
+
+
+def must_exist(p):
+    if not p.exists():
+        raise Exception("No such file: %s" % p.absolute())
+
+
+
+
+class DocTree(object):
+    """
+    Base class for a doctree descriptor.  Atelier currently supports
+    `Sphinx <http://www.sphinx-doc.org/en/stable/>`__ and `Nikola
+    <https://getnikola.com/>`__ docs.
+    """
+    src_path = None
+    out_path = None
+    has_intersphinx = False
+    
+    def __init__(self, prj, rel_doc_tree):
+        self.rel_path = rel_doc_tree
+        self.prj = prj
+        
+        if rel_doc_tree in ('', '.'):
+            src_path = prj.root_dir
+        else:
+            src_path = prj.root_dir.child(rel_doc_tree)
+        # The src_path may not exist if this is on a Project which
+        # has been created from a normally installed main_package
+        # (because there is has no source code).
+        if src_path.exists():
+            self.src_path = src_path
+        
+    def build_docs(self, ctx, *cmdline_args):
+        raise NotImplementedError()
+
+    def publish_docs(self, ctx):
+        # build_dir = docs_dir.child(ctx.build_dir_name)
+        if self.src_path is None:
+            return
+        build_dir = self.out_path
+        if build_dir.exists():
+            docs_dir = self.src_path
+            # name = '%s_%s' % (ctx.project_name, docs_dir.name)
+            # dest_url = ctx.docs_rsync_dest % name
+            if "%" in ctx.docs_rsync_dest:
+                name = '%s_%s' % (ctx.project_name, docs_dir.name)
+                dest_url = ctx.docs_rsync_dest % name
+            else:
+                dest_url = ctx.docs_rsync_dest.format(
+                    prj=ctx.project_name, docs=docs_dir.name)
+            self.publish_doc_tree(ctx, build_dir, dest_url)
+
+    def publish_doc_tree(self, ctx, build_dir, dest_url):
+        with cd(build_dir):
+            args = ['rsync', '-e', 'ssh', '-r']
+            args += ['--verbose']
+            args += ['--progress']  # show progress
+            args += ['--delete']  # delete files in dest
+            args += ['--times']  # preserve timestamps
+            
+            # the problem with --times is that it fails when several
+            # users can publish to the same server alternatively.
+            # Only the owner of a file can change the mtime, other
+            # users can't, even if they have write permission through
+            # the group.
+            
+            args += ['--exclude', '.doctrees']
+            args += ['./']  # source
+            args += [dest_url]  # dest
+            cmd = ' '.join(args)
+            # must_confirm("%s> %s" % (build_dir, cmd))
+            ctx.run(cmd, pty=True)
+
+
+class SphinxTree(DocTree):
+    """
+    The default docs builder using Sphinx.
+
+    :cmd:`sphinx-build`
+
+    .. command:: sphinx-build
+
+        http://www.sphinx-doc.org/en/stable/invocation.html#invocation-of-sphinx-build
+
+    
+    
+    """
+    has_intersphinx = True
+    
+    def __init__(self, prj, src_path):
+        super(SphinxTree, self).__init__(prj, src_path)
+        if self.src_path is None:
+            return
+        cfg = prj.config
+        self.out_path = self.src_path.child(cfg['build_dir_name'])
+        
+    def build_docs(self, ctx, *cmdline_args):
+        if self.src_path is None:
+            return
+        docs_dir = self.src_path
+        print("Invoking Sphinx in directory %s..." % docs_dir)
+        builder = 'html'
+        if ctx.use_dirhtml:
+            builder = 'dirhtml'
+        self.sphinx_build(ctx, builder, docs_dir, cmdline_args)
+        self.sync_docs_data(ctx, docs_dir)
+        
+    def sphinx_build(self, ctx, builder, docs_dir,
+                     cmdline_args=[], language=None, build_dir_cmd=None):
+        if self.out_path is None:
+            return
+        # args = ['sphinx-build', builder]
+        args = ['sphinx-build', '-b', builder]
+        args += ['-T'] # show full traceback on exception
+        args += cmdline_args
+        # ~ args += ['-a'] # all files, not only outdated
+        # ~ args += ['-P'] # no postmortem
+        # ~ args += ['-Q'] # no output
+        build_dir = self.out_path
+        if language is not None:
+            args += ['-D', 'language=' + language]
+            # needed in select_lang.html template
+            args += ['-A', 'language=' + language]
+            if language != ctx.languages[0]:
+                build_dir = build_dir.child(language)
+                # print 20130726, build_dir
+                
+        # seems that the default location for the .doctrees directory
+        # is no longer in .build but the source directory.
+        args += ['-d', build_dir.child('.doctrees')]
+        if ctx.tolerate_sphinx_warnings:
+            args += ['-w', 'warnings_%s.txt' % builder]
+        else:
+            args += ['-W']  # consider warnings as errors
+            # args += ['-vvv']  # increase verbosity
+        # args += ['-w'+Path(ctx.root_dir,'sphinx_doctest_warnings.txt')]
+        args += ['.', build_dir]
+        cmd = ' '.join(args)
+        print("Invoke {}".format(cmd))
+        with cd(docs_dir):
+            ctx.run(cmd, pty=True)
+        if build_dir_cmd is not None:
+            with cd(build_dir):
+                ctx.run(build_dir_cmd, pty=True)
+
+    def sync_docs_data(self, ctx, docs_dir):
+        # build_dir = docs_dir.child(ctx.build_dir_name)
+        if self.src_path is None:
+            return
+        build_dir = self.out_path
+        for data in ('dl', 'data'):
+            src = docs_dir.child(data).absolute()
+            if src.isdir():
+                target = build_dir.child('dl')
+                target.mkdir()
+                cmd = 'cp -ur %s %s' % (src, target.parent)
+                ctx.run(cmd, pty=True)
+        if False:
+            # according to http://mathiasbynens.be/notes/rel-shortcut-icon
+            for n in ['favicon.ico']:
+                src = docs_dir.child(n).absolute()
+                if src.exists():
+                    target = build_dir.child(n)
+                    cmd = 'cp %s %s' % (src, target.parent)
+                    ctx.run(cmd, pty=True)
+
+
+
+class NikolaTree(DocTree):
+    """Requires Nikola. 
+
+    Note that Nikola requires::
+
+        $ sudo apt install python-gdbm
+
+    """
+    def __init__(self, ctx, src_path):
+        super(NikolaTree, self).__init__(ctx, src_path)
+        if self.src_path is None:
+            return
+        self.out_path = self.src_path.child('output')
+        
+    def build_docs(self, ctx, *cmdline_args):
+        if self.src_path is None:
+            return
+        docs_dir = self.src_path
+        print("Invoking nikola build in in %s..." % docs_dir)
+        args = ['nikola', 'build']
+        args += cmdline_args
+        cmd = ' '.join(args)
+        with cd(docs_dir):
+            ctx.run(cmd, pty=True)
+        
+        
---
+++ packages/atelier/atelier-1.1.11.tar.gz/atelier-1.1.11/atelier/sphinxconf/interproject.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011-2018 Rumma & Ko Ltd
+# License: BSD, see LICENSE for more details.
+
+"""
+- `intersphinx_mapping` : The intersphinx entries for projects
+   managed in this atelier. Atelier gets this information by
+   checking for an attribute `intersphinx_urls` in the global
+   namespace of each project's main module.
+
+TODO: rename :func:`configure` to intersphinx_mapping
+
+"""
+import six
+
+from unipath import Path
+# from importlib import import_module
+from sphinx.util import logging
+logger = logging.getLogger(__name__)
+
+# from invoke import Context
+
+# import atelier
+from atelier.projects import load_projects, get_project_info_from_mod
+
+
+USE_LOCAL_BUILDS = False
+# Whether to use objects.inv files from other local doctrees if they
+# exist.  E.g. on Travis no other projects are installed from source.
+
+
+def configure(globals_dict, prjspec=None):
+    """
+    Install doctrees of all projects (or only some) into
+    `intersphinx_mapping` of your :xfile:`conf.py`
+
+    `prjspec` is a set of importable python modules or a string with a
+    space-separated list thereof.  Each of these modules must adhere
+    to the atelier convention of having two attributes
+    :envvar:`doc_trees` and :envvar:`intersphinx_urls`.  
+
+    If `prjspec` is not given, then all projects of the atelier that
+    come *before* the current project are added.  Projects are
+    iterated in the same order as they were defined in
+    :ref:`atelier.config`)
+    """
+    
+    intersphinx_mapping = dict()
+    # extlinks = dict()
+
+    # this = atelier.current_project
+    # if this is None:
+    #     raise Exception("current_project in {} is None!".format(globals_dict['__file__']))
+
+    this_conf_file = Path(globals_dict['__file__']).resolve()
+    
+    if prjspec:
+        if isinstance(prjspec, six.string_types):
+            prjspec = prjspec.split()
+        prjlist = [get_project_info_from_mod(n) for n in prjspec]
+            
+    else:
+        prjlist = []
+        for p in load_projects():
+            if this_conf_file.startswith(p.root_dir):
+                break
+            prjlist.append(p)
+        
+    # logger.info("20180907 prjlist {}".format(prjlist))
+    for prj in prjlist:
+        # This will load the `tasks.py` of other
+        # projects. Possible side effects.
+        # print("20180428 {} {}".format(prj.name, prj.config['doc_trees']))
+        # config = prj.inv_namespace.configuration()
+        # print("20180428 {} {}".format(prj.name, config['doc_trees']))
+        # ctx = Context(config)
+        # for doc_tree in prj.config['doc_trees']:
+        count = 0
+        for doc_tree in prj.get_doc_trees():
+            if not doc_tree.has_intersphinx:
+                logger.info("%s has no intersphinx", p)
+                continue
+            count += 1
+            p = None
+            if USE_LOCAL_BUILDS:
+                src_path = doc_tree.src_path
+                if src_path is None or this_conf_file == src_path.child('conf.py'):
+                    break
+                # p = prj.root_dir.child(doc_tree, '.build', 'objects.inv')
+                p = src_path.child('.build', 'objects.inv')
+                if p.exists():
+                    logger.info("Found local {}".format(p))
+                else:
+                    p = None
+                    logger.info("File %s does not exist", p)
+                
+            
+            # The unique identifier can be used to prefix cross-reference targets
+            # http://www.sphinx-doc.org/en/master/ext/intersphinx.html#confval-intersphinx_mapping
+            k = prj.nickname + doc_tree.rel_path
+            k = k.replace('_', '')
+            k = six.text_type(k)  # make sure it's not newstr from
+                                  # future because that can cause
+                                  # problems when intersphinx tries to
+                                  # sort them
+            # if doc_tree == 'docs':
+            #     k = prj.nickname
+            # else:
+            #     k = prj.nickname + doc_tree.replace('_', '')
+            urls = getattr(prj.main_package, 'intersphinx_urls', {})
+            url = urls.get(doc_tree.rel_path)
+            if url:
+                intersphinx_mapping[k] = (url, p)
+            elif p:
+                intersphinx_mapping[k] = p
+            elif prjspec:
+                logger.warning(
+                    "No intersphinx mapping for {} of {} ({})".format(
+                        doc_tree.rel_path, prj.nickname, urls))
+            # else:
+            #     logger.info(
+            #         "20180706 No intersphinx mapping for {} of {} ({})".format(
+            #             doc_tree.rel_path, prj.nickname, urls))
+
+        if count == 0 and prjspec:
+            logger.warning("No doctree for {}".format(prj))
+        # if prj.srcref_url:
+        #     k = '%s_srcref' % prj.nickname
+        #     extlinks[str(k)] = (prj.srcref_url, '')
+
+    # atelier.current_project = this
+    globals_dict.update(intersphinx_mapping=intersphinx_mapping)
+
+    # logger.info("20180706 intersphinx_mapping set to {}".format(
+    #     intersphinx_mapping))
+
+    # if False:  # no longer used
+    #     globals_dict.update(extlinks=extlinks)
