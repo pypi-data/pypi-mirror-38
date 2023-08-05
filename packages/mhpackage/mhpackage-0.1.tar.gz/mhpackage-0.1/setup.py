f732c385eaec352cac9fcf0bf09e47323c0b51c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package mhpackage/0.1/mhpackage-0.1.tar.gz

---
+++ packages/mhpackage/mhpackage-0.1.tar.gz/mhpackage-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mhpackage',
+      version='0.1',
+      description='The mhpackage joke in the world',
+      url='https://job4moshe@dev.azure.com/job4moshe/mhpackageProject/_git/mhpackageProject',
+      author='Flying Circus',
+      author_email='job4moshe@gmail.com',
+      license='MIT',
+      packages=['mhpackage'],
+      zip_safe=False)
