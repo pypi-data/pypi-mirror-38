8bb10d39d0d656cbbbf6c6a88a38f99f165c4fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package pyden/1.0.4/pyden-1.0.4-py3-none-any.whl

---
+++ packages/pyden/pyden-1.0.4-py3-none-any.whl/pyden/errors.py
@@ -0,0 +1,13 @@
+"""Define various exception types."""
+
+
+class PydenError(Exception):
+    """Define a generic pyden error."""
+
+    pass
+
+
+class RequestError(PydenError):
+    """Define a generic request error."""
+
+    pass
