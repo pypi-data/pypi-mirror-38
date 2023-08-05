ba2d8a0ab34c9dd701334022478edf1fcb8a189d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package bdc/1.0.5/bdc-1.0.5-py3.6.egg

---
+++ packages/bdc/bdc-1.0.5-py3.6.egg/bdc/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Contains metadata for bdc package
+
+File name: __init__.py
+Author: Adam Jenkins
+Date created: 10/30/2018
+Date last modified: 11/6/18
+Python Version: 3.6.5
+'''
+
+__author__ = 'Adam Jenkins'
+__version__ = '1.0.5'
+__name__ = 'bdc'
+__package__ = 'bdc'
+__url__ = 'https://www.github.com/adamjenkins1/BootableDiskCreator'
+__copyright__ = 'Copyright 2007, The Cogent Project'
+__license__ = 'MIT'
