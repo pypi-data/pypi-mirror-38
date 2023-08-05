a6265cb8c0b2b87c43ea508ca6cf11ac1791c403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:36 +0000

    Add package django-acmin/0.1.12/django-acmin-0.1.12.tar.gz

---
+++ packages/django-acmin/django-acmin-0.1.12.tar.gz/django-acmin-0.1.12/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+from acmin import __version__
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(CURRENT_PATH, 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+setup(
+    name='django-acmin',
+    version=__version__,
+    author='Hike Li',
+    author_email='hikelee@gmail.com',
+    url='https://github.com/hikelee/django-acmin',
+    description='Admin and API template for start a django web',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django :: 2.1',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    license='BSD',
+    packages=find_packages(),
+    install_requires=required,
+    include_package_data=True,
+    zip_safe=False,
+)
