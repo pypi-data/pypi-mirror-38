076636c5894f99a41d3fa6794d172c170c27a184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package termtosvg/0.6.0/termtosvg-0.6.0.tar.gz

---
+++ packages/termtosvg/termtosvg-0.6.0.tar.gz/termtosvg-0.6.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='termtosvg',
+    version='0.6.0',
+    license='BSD 3-clause license',
+    author='Nicolas Bedos',
+    author_email='nicolas.bedos@gmail.com',
+    description='Record terminal sessions as SVG animations',
+    long_description='A Linux terminal recorder written in Python '
+                     'which renders your command line sessions as '
+                     'standalone SVG animations.',
+    url='https://github.com/nbedos/termtosvg',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: System :: Shells',
+        'Topic :: Terminals'
+    ],
+    python_requires='>=3.5',
+    packages=['termtosvg'],
+    py_modules=[
+        'termtosvg.anim',
+        'termtosvg.asciicast',
+        'termtosvg.config',
+        'termtosvg.main',
+        'termtosvg.term',
+    ],
+    scripts=['scripts/termtosvg'],
+    include_package_data=True,
+    install_requires=[
+        'lxml',
+        'pyte',
+    ],
+    extras_require={
+        'dev': [
+            'coverage',
+            'pylint',
+            'twine',
+            'wheel',
+        ]
+    }
+)
