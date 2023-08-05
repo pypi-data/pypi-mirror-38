6cef7a21fd647c2a2cb001762fadaeb907fe7af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:59 +0000

    Add package xacc/0.1.11/xacc-0.1.11-cp33-cp33m-manylinux1_x86_64.whl

---
+++ packages/xacc/xacc-0.1.11-cp33-cp33m-manylinux1_x86_64.whl/xacc/include/quantum/gate/CNOT.hpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2017 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompanies this
+ * distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ *License is available at https://eclipse.org/org/documents/edl-v10.php
+ *
+ * Contributors:
+ *   Alexander J. McCaskey - initial API and implementation
+ *******************************************************************************/
+#ifndef QUANTUM_GATE_GATEQIR_INSTRUCTIONS_CNOT_HPP_
+#define QUANTUM_GATE_GATEQIR_INSTRUCTIONS_CNOT_HPP_
+
+#include "GateInstruction.hpp"
+
+namespace xacc {
+namespace quantum {
+
+/**
+ *
+ */
+class CNOT : public virtual GateInstruction {
+public:
+  CNOT() : GateInstruction("CNOT") {}
+  CNOT(std::vector<int> qbits) : GateInstruction("CNOT", qbits) {}
+  CNOT(int srcqbit, int tgtqbit) : CNOT(std::vector<int>{srcqbit, tgtqbit}) {}
+
+  virtual std::shared_ptr<GateInstruction> clone() {
+    return std::make_shared<CNOT>();
+  }
+
+  /**
+   * Return the description of this instance
+   * @return description The description of this object.
+   */
+  virtual const std::string description() const { return ""; }
+
+  DEFINE_VISITABLE()
+};
+
+} // namespace quantum
+} // namespace xacc
+#endif
