3abbd9b221d30e5e1822eeb2230190fe7f8a3a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:04 +0000

    Add package rich-text-renderer/0.2.2/rich_text_renderer-0.2.2.tar.gz

---
+++ packages/rich-text-renderer/rich_text_renderer-0.2.2.tar.gz/rich_text_renderer-0.2.2/rich_text_renderer/document_renderers.py
@@ -0,0 +1,15 @@
+from .base_node_renderer import BaseNodeRenderer
+
+
+class DocumentRenderer(BaseNodeRenderer):
+    def render(self, document):
+        result = []
+        for node in document["content"]:
+            renderer = self._find_renderer(node)
+
+            if renderer is None:
+                continue
+
+            result.append(renderer.render(node))
+
+        return "\n".join(result)
