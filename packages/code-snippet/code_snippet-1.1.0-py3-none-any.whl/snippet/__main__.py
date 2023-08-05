1afab4e647466c5db058617e185623935e317179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package code-snippet/1.1.0/code_snippet-1.1.0-py3-none-any.whl

---
+++ packages/code-snippet/code_snippet-1.1.0-py3-none-any.whl/snippet/__main__.py
@@ -0,0 +1,3 @@
+from snippet.cli import run_from_cli
+
+run_from_cli()
---
+++ packages/code-snippet/code_snippet-1.1.0-py3-none-any.whl/snippet/_version.py
@@ -0,0 +1,4 @@
+# This project's release version
+__version__ = '1.1.0'
+# This project's release commit hash
+COMMIT = "5d5a5cbd12ef49909d78c2247a2f988efb9c4894"
