a4630d1c41c6cf5f71a58e683029a3767522cef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:13 +0000

    Add package w7x/0.1.0.dev7/w7x-0.1.0.dev7.tar.gz

---
+++ packages/w7x/w7x-0.1.0.dev7.tar.gz/w7x-0.1.0.dev7/setup.py
@@ -0,0 +1,72 @@
+"""
+Publishing:
+    run tests
+    remember to change the version in setup(... ) below
+    $ git commit -am "<my message>"
+    $ git push
+    $ git tag -a v<my.version.id> -m "<comment to my version>"  # tag version
+    $ git push origin v<my.version.id>  # explicitly push tag to the shared server
+    $ python setup.py sdist
+    $ twine upload dist/*
+
+"""
+from setuptools import setup, find_packages
+exec(open("./w7x/__about__.py").read())
+
+setup(
+
+    name=__title__,
+    version=__version__,
+    description=__summary__,
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
+    packages=find_packages(),
+    url=__uri__,
+    project_urls={
+        'Documentation': __uri__,
+        'Source': __uri__,
+    },
+    test_require=[
+        'doctest',
+        'unittest'
+    ],
+    install_requires=[
+        'tfields',  # includes many other dependencies
+        'osa',
+        'symfit',
+        'joblib',
+    ],
+    entry_points={
+        'console_scripts': ['w7x = w7x.__main__:runDoctests']
+    },
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    python_requires='>=2.7',
+)
+
+
+# in the terminal:
+# pip install .
---
+++ packages/w7x/w7x-0.1.0.dev7.tar.gz/w7x-0.1.0.dev7/w7x/plotting/poincare.py
@@ -0,0 +1,58 @@
+"""
+specific methods for poincare plotting
+"""
+import tfields
+
+
+def plot_poincare_surfaces(poincareSurfaces, **kwargs):
+    """
+    Args:
+        poincareSurfaces (list of Points3D): each Points3D instance is one
+            fieldLine followed around the torus
+    """
+    methodName = kwargs.pop('methodName', 'scatter')
+    po = tfields.plotting.PlotOptions(kwargs)
+    rMin = po.pop('rMin', 4.0)
+    rMax = po.pop('rMax', 6.6)
+    zMin = po.pop('zMin', -1.3)
+    zMax = po.pop('zMax', +1.3)
+    phiRad = po.pop('phiRad', None)
+
+    po.set_default('yAxis', 2)
+    po.set_default('labelList', ['r (m)', r'$\phi$ (rad)', 'z (m)'])
+    if methodName is 'scatter':
+        po.set_default('marker', '.')
+        po.set_default('s', 1)
+    po.set_default('methodName', methodName)
+    colorGiven = True
+    if 'color' not in po.plotKwargs:
+        colorGiven = False
+        cmap, _, _ = po.getNormArgs()
+        color_cycle = tfields.plotting.color_cycle(cmap, len(poincareSurfaces))
+    artists = []
+    for surfacePoints in poincareSurfaces:
+        with surfacePoints.tmp_transform(tfields.bases.CYLINDER):
+            phiSurface = surfacePoints[:, 1]
+            if phiRad is None:
+                phiRad = phiSurface[0]
+            if bool((phiSurface != phiRad).any()):
+                continue
+            if not colorGiven:
+                po.set('color', next(color_cycle))
+            artists.append(surfacePoints.plot(axis=po.axis, **po.plotKwargs))
+    tfields.plotting.set_aspect_equal(po.axis)
+    po.axis.set_xlim(rMin, rMax)
+    po.axis.set_ylim(zMin, zMax)
+    return artists
+
+
+def plot_poincare_geometries(geometries, **kwargs):
+    po = tfields.plotting.PlotOptions(kwargs)
+    po.set_default('methodName', 'plot')
+    po.set_default('lw', 1)
+    artists = []
+    for p in range(len(geometries)):
+        for g in range(len(geometries[p])):
+            artists.extend(plot_poincare_surfaces(geometries[p][g], axis=po.axis,
+                                                **po.plotKwargs)) 
+    return artists
