1302cfb44a84a1194a7c1b115bccbc47facd0491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package avatar-generator/0.2/avatar-generator-0.2.tar.gz

---
+++ packages/avatar-generator/avatar-generator-0.2.tar.gz/avatar-generator-0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='avatar-generator',
+    version='0.2',
+    author="Guillaume Subiron",
+    author_email="maethor+pip@subiron.org",
+    description="Generates default avatars from a given string (such as username).",
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    package_data={'': ['*.otf', 'data/*.otf']},
+    include_package_data=True,
+    install_requires=['pillow'],
+    url='http://github.com/maethor/avatar-generator',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers"
+    ],
+    license="WTFPL",
+)
