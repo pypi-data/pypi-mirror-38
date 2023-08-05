fcae2c7bf7bb1dc6282741176673df772bc76411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:07 +0000

    Add package numpy-cloud/0.0.4/numpy_cloud-0.0.4.tar.gz

---
+++ packages/numpy-cloud/numpy_cloud-0.0.4.tar.gz/numpy_cloud-0.0.4/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="numpy_cloud",
+    version="0.0.4",
+    author="Numpy In The Cloud",
+    author_email="author@example.com",
+    description="Numpy in the cloud",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ortutay/numpy_cloud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
