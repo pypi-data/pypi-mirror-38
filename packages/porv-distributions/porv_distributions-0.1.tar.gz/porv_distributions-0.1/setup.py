38a77fcdd37a9d7afc6bdbb271ba43477b2eecc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:03 +0000

    Add package porv-distributions/0.1/porv_distributions-0.1.tar.gz

---
+++ packages/porv-distributions/porv_distributions-0.1.tar.gz/porv_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='porv_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['porv_distributions'],
+      author= 'Praveen Orvakanti',
+      author_email='praveen.orvakanti@gmail.com',
+      zip_safe=False)
