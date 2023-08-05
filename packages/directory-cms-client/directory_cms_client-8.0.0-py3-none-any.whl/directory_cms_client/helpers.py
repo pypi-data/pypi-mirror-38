4bf920ecee2aad64e1dd263108aa23314bf9fb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:29 +0000

    Add package directory-cms-client/8.0.0/directory_cms_client-8.0.0-py3-none-any.whl

---
+++ packages/directory-cms-client/directory_cms_client-8.0.0-py3-none-any.whl/directory_cms_client/helpers.py
@@ -0,0 +1,126 @@
+import abc
+from functools import wraps
+import json
+import logging
+from urllib.parse import urlencode
+
+from django.conf import settings
+from django.shortcuts import Http404
+
+from requests.exceptions import HTTPError, RequestException
+from w3lib.url import canonicalize_url
+
+
+logger = logging.getLogger(__name__)
+
+
+MESSAGE_CACHE_HIT = 'CMS fallback cache hit. Displaying cached content.'
+MESSAGE_CACHE_MISS = 'CMS fallback cache miss. Cannot display any content.'
+MESSAGE_NOT_FOUND = 'Page not found in CMS'
+
+
+class AbstractCMSResponse(abc.ABC):
+
+    def __init__(self, content, status_code, raw_response=None):
+        self.content = content
+        self.status_code = status_code
+        self.raw_response = raw_response
+
+    def raise_for_status(self):
+        if not 200 <= self.status_code < 300:
+            raise HTTPError(self.content)
+
+    def json(self):
+        return json.loads(self.content.decode('utf-8'))
+
+    @classmethod
+    def from_response(cls, response):
+        return cls(
+            content=response.content,
+            status_code=response.status_code,
+            raw_response=response,
+        )
+
+
+class CMSLiveResponse(AbstractCMSResponse):
+    pass
+
+
+class CMSCacheResponse(AbstractCMSResponse):
+    pass
+
+
+class CMSFailureResponse(AbstractCMSResponse):
+    pass
+
+
+def fallback(cache):
+    """
+    Caches CMS content retrieved by the client, thus allowing the cached
+    content to be used later if the live CMS content cannot be retrieved.
+
+    """
+
+    def get_cache_response(cache_key):
+        content = cache.get(cache_key)
+        if content:
+            return CMSCacheResponse(content=content, status_code=200)
+
+    def closure(func):
+        @wraps(func)
+        def wrapper(client, url, params={}, *args, **kwargs):
+            cache_key = canonicalize_url(url + '?' + urlencode(params))
+            try:
+                cms_response = func(
+                    client, url=url, params=params, *args, **kwargs
+                )
+            except RequestException:
+                # Failed to create the request e.g., the CMS is down, perhaps a
+                # timeout occurred, or even connection closed by CMS, etc.
+                response = get_cache_response(cache_key)
+                if response:
+                    logger.error(MESSAGE_CACHE_HIT, extra={'url': url})
+                else:
+                    raise
+            else:
+                log_context = {
+                    'status_code': cms_response.status_code, 'url': url
+                }
+                if cms_response.status_code == 404:
+                    logger.error(MESSAGE_NOT_FOUND, extra=log_context)
+                    return CMSLiveResponse.from_response(cms_response)
+                elif not cms_response.ok:
+                    # Successfully requested the content, but the response is
+                    # not OK (e.g., 500, 403, etc)
+                    response = get_cache_response(cache_key)
+                    if response:
+                        logger.error(MESSAGE_CACHE_HIT, extra=log_context)
+                    else:
+                        logger.exception(MESSAGE_CACHE_MISS, extra=log_context)
+                        response = CMSFailureResponse.from_response(
+                            cms_response
+                        )
+                else:
+                    cache.set(
+                        cache_key,
+                        cms_response.content,
+                        settings.DIRECTORY_CMS_API_CLIENT_CACHE_EXPIRE_SECONDS
+                    )
+                    response = CMSLiveResponse.from_response(cms_response)
+            return response
+        return wrapper
+    return closure
+
+
+def handle_cms_response(response):
+    if response.status_code == 404:
+        raise Http404()
+    response.raise_for_status()
+    return response.json()
+
+
+def handle_cms_response_allow_404(response):
+    if response.status_code == 404:
+        return {}
+    response.raise_for_status()
+    return response.json()
---
+++ packages/directory-cms-client/directory_cms_client-8.0.0-py3-none-any.whl/tests/test_client.py
@@ -0,0 +1,350 @@
+import json
+from unittest.mock import patch
+
+import pytest
+from freezegun import freeze_time
+import requests_mock
+import requests.exceptions
+from requests import Response
+
+from django.core.cache import caches
+
+from directory_cms_client import DirectoryCMSClient
+from directory_cms_client.version import __version__
+from directory_cms_client import helpers
+
+
+@pytest.fixture
+def default_client():
+    return DirectoryCMSClient(
+        base_url='http://example.com',
+        api_key='debug',
+        sender_id='test-sender',
+        timeout=5,
+        default_service_name='foo'
+    )
+
+
+@pytest.fixture
+def cms_cache():
+    return caches['cms_fallback']
+
+
+@pytest.fixture(autouse=True)
+def clear_cms_cache(cms_cache):
+    cms_cache.clear()
+
+
+def test_cms_client_list_by_page_type_draft(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/')
+        default_client.list_by_page_type('thing', draft_token='draft-token')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'draft_token': ['draft-token'],
+            'fields': ['*'],
+            'type': ['thing'],
+        }
+
+
+def test_cms_client_lookup_by_slug_language(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-slug/thing/')
+        default_client.lookup_by_slug('thing', language_code='de')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'service_name': ['foo'],
+            'lang': ['de'],
+            'fields': ['*'],
+        }
+
+
+def test_cms_client_lookup_by_slug_draft(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-slug/thing/')
+        default_client.lookup_by_slug('thing', draft_token='draft-token')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'service_name': ['foo'],
+            'draft_token': ['draft-token'],
+            'fields': ['*'],
+        }
+
+
+def test_cms_client_lookup_by_slug(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-slug/thing/')
+        default_client.lookup_by_slug('thing')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'service_name': ['foo'],
+            'fields': ['*'],
+        }
+
+
+def test_cms_client_lookup_by_full_path_language(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-full-path/')
+        default_client.lookup_by_full_path('thing', language_code='de')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'lang': ['de'],
+            'fields': ['*'],
+            'full_path': ['thing'],
+            'service_name': ['foo'],
+        }
+
+
+def test_cms_client_lookup_by_full_path_draft(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-full-path/')
+        default_client.lookup_by_full_path('thing', draft_token='draft-token')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'draft_token': ['draft-token'],
+            'fields': ['*'],
+            'full_path': ['thing'],
+            'service_name': ['foo'],
+        }
+
+
+def test_cms_client_lookup_by_full_path(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-full-path/')
+        default_client.lookup_by_full_path('thing')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'fields': ['*'],
+            'full_path': ['thing'],
+            'service_name': ['foo'],
+        }
+
+
+def test_cms_client_lookup_by_full_path_service_name(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-full-path/')
+        default_client.lookup_by_full_path(
+            'thing', service_name='test-service')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'fields': ['*'],
+            'full_path': ['thing'],
+            'service_name': ['test-service'],
+        }
+
+
+def test_cms_client_lookup_by_tag_draft(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-tag/thing/')
+        default_client.lookup_by_tag('thing', draft_token='draft-token')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'fields': ['*'],
+            'draft_token': ['draft-token'],
+            'service_name': ['foo'],
+        }
+
+
+def test_cms_client_lookup_by_tag(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-tag/thing/')
+        default_client.lookup_by_tag('thing')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'fields': ['*'],
+            'service_name': ['foo'],
+        }
+
+
+def test_cms_client_lookup_by_tag_service_name(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/api/pages/lookup-by-tag/thing/')
+        default_client.lookup_by_tag(
+            'thing', service_name='test-service')
+        request = mock.request_history[0]
+
+        assert request.qs == {
+            'fields': ['*'],
+            'service_name': ['test-service'],
+        }
+
+
+def test_timeout(default_client):
+    assert default_client.timeout == 5
+
+
+def test_sender_id(default_client):
+    assert default_client.request_signer.sender_id == 'test-sender'
+
+
+def test_version():
+    assert DirectoryCMSClient.version == __version__
+
+
+def test_good_response_cached(default_client, cms_cache):
+    expected_data = bytes(json.dumps({'key': 'value'}), 'utf8')
+    path = '/api/pages/lookup-by-slug/thing/'
+
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com' + path, content=expected_data)
+        default_client.lookup_by_slug('thing')
+
+    cache_key = path + '?fields=%5B%27%2A%27%5D&service_name=foo'
+    assert cms_cache.get(cache_key) == expected_data
+
+
+@freeze_time('2012-01-14')
+def test_good_response_cache_timeout(default_client, cms_cache, settings):
+    settings.DIRECTORY_CMS_API_CLIENT_CACHE_EXPIRE_SECONDS = 100
+    expected_data = bytes(json.dumps({'key': 'value'}), 'utf8')
+    path = '/api/pages/lookup-by-slug/thing/'
+
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com' + path, content=expected_data)
+        default_client.lookup_by_slug('thing')
+
+    cache_key = path + '?fields=%5B%27%2A%27%5D&service_name=foo'
+
+    key = cms_cache.make_key(cache_key)
+    cms_cache.validate_key(key)
+
+    assert cms_cache._expire_info.get(key) == 1326499300.0
+
+
+def test_bad_resonse_cache_hit(default_client, caplog):
+    path = '/api/pages/lookup-by-slug/thing/'
+    expected_data = bytes(json.dumps({'key': 'value'}), 'utf8')
+    url = 'http://example.com' + path
+
+    with requests_mock.mock() as mock:
+        mock.get(url, content=expected_data)
+        response_one = default_client.lookup_by_slug('thing')
+
+    with requests_mock.mock() as mock:
+        mock.get(url, status_code=400)
+        response_two = default_client.lookup_by_slug('thing')
+
+    assert response_one.status_code == 200
+    assert response_one.content == expected_data
+    assert isinstance(response_one, helpers.CMSLiveResponse)
+    assert isinstance(response_one.raw_response, Response)
+
+    assert response_two.status_code == 200
+    assert response_two.content == expected_data
+    assert isinstance(response_two, helpers.CMSCacheResponse)
+    assert response_two.raw_response is None
+
+    log = caplog.records[-1]
+    assert log.levelname == 'ERROR'
+    assert log.msg == helpers.MESSAGE_CACHE_HIT
+    assert log.status_code == 400
+    assert log.url == path
+
+
+def test_bad_response_cache_miss(default_client, caplog):
+    path = '/api/pages/lookup-by-slug/thing/'
+    url = 'http://example.com' + path
+
+    with requests_mock.mock() as mock:
+        mock.get(url, status_code=400)
+        response = default_client.lookup_by_slug('thing')
+
+    assert response.status_code == 400
+    assert isinstance(response, helpers.CMSFailureResponse)
+    assert isinstance(response.raw_response, Response)
+
+    log = caplog.records[-1]
+    assert log.levelname == 'ERROR'
+    assert log.msg == helpers.MESSAGE_CACHE_MISS
+    assert log.status_code == 400
+    assert log.url == path
+
+
+def test_bad_response_404(default_client, caplog):
+    path = '/api/pages/lookup-by-slug/thing/'
+    url = 'http://example.com' + path
+
+    with requests_mock.mock() as mock:
+        mock.get(url, status_code=404)
+        response = default_client.lookup_by_slug('thing')
+
+    assert response.status_code == 404
+    assert isinstance(response, helpers.CMSLiveResponse)
+    assert isinstance(response.raw_response, Response)
+
+    log = caplog.records[-1]
+    assert log.levelname == 'ERROR'
+    assert log.msg == helpers.MESSAGE_NOT_FOUND
+    assert log.status_code == 404
+    assert log.url == path
+
+
+def test_connection_error_cache_hit(default_client, caplog):
+    path = '/api/pages/lookup-by-slug/thing/'
+    expected_data = bytes(json.dumps({'key': 'value'}), 'utf8')
+    url = 'http://example.com' + path
+
+    with requests_mock.mock() as mock:
+        mock.get(url, content=expected_data)
+        response_one = default_client.lookup_by_slug('thing')
+
+    with patch('directory_client_core.base.AbstractAPIClient.get') as mock_get:
+        mock_get.side_effect = requests.exceptions.ConnectionError()
+        response_two = default_client.lookup_by_slug('thing')
+
+    assert response_one.status_code == 200
+    assert response_one.content == expected_data
+    assert isinstance(response_one, helpers.CMSLiveResponse)
+
+    assert response_two.status_code == 200
+    assert response_two.content == expected_data
+    assert isinstance(response_two, helpers.CMSCacheResponse)
+
+    log = caplog.records[-1]
+    assert log.levelname == 'ERROR'
+    assert log.msg == helpers.MESSAGE_CACHE_HIT
+    assert log.url == path
+
+
+def test_connection_error_cache_miss(default_client, caplog):
+    with patch('directory_client_core.base.AbstractAPIClient.get') as mock_get:
+        mock_get.side_effect = requests.exceptions.ConnectionError()
+
+        with pytest.raises(requests.exceptions.ConnectionError):
+            default_client.lookup_by_slug('thing')
+
+    assert len(caplog.records) == 0
+
+
+def test_cache_querystrings(default_client, cms_cache):
+    expected_data = bytes(json.dumps({'key': 'value'}), 'utf8')
+    path = '/api/pages/lookup-by-slug/thing/'
+
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com' + path, content=expected_data)
+        default_client.lookup_by_slug(
+            'thing',
+            language_code='de',
+            draft_token='2'
+        )
+
+    cache_key = (
+        path + '?draft_token=2&fields=%5B%27%2A%27%5D&lang=de&service_name=foo'
+    )
+    assert cms_cache.get(cache_key) == expected_data
+
+
+def test_ping(default_client):
+    with requests_mock.mock() as mock:
+        mock.get('http://example.com/healthcheck/ping/')
+        default_client.ping()
