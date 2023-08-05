51748a8e7e51a0b78e8dea95cc295e60a84f5c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:24 +0000

    Add package metapack/0.8.27/metapack-0.8.27.tar.gz

---
+++ packages/metapack/metapack-0.8.27.tar.gz/metapack-0.8.27/metapack/cli/update.py
@@ -0,0 +1,162 @@
+# Copyright (c) 2017 Civic Knowledge. This file is licensed under the terms of the
+# MIT License, included in this distribution as LICENSE
+
+"""
+CLI program for managing packages
+"""
+
+from metapack import Downloader
+from metapack.util import  ensure_dir
+from metapack.cli.core import update_name, process_schemas, MetapackCliMemo, list_rr, warn, prt, err
+import sys
+import requests
+from os.path import dirname, basename, exists, splitext
+
+downloader = Downloader.get_instance()
+
+
+def update(subparsers):
+    parser = subparsers.add_parser(
+        'update',
+        help='Update a metatab file or metatpack package',
+        epilog='Cache dir: {}\n'.format(str(downloader.cache.getsyspath('/'))))
+
+    parser.set_defaults(run_command=run_update)
+
+    parser.add_argument('metatabfile', nargs='?',
+                        help="Path or URL to a metatab file. If not provided, defaults to 'metadata.csv' ")
+
+    parser.set_defaults(handler=None)
+
+    ##
+    ## Build Group
+
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('-s', '--schemas', default=False, action='store_true',
+                       help='Rebuild the schemas for files referenced in the resource section')
+
+    group.add_argument('-n', '--name', action='store_true', default=False,
+                       help="Update the Name from the Datasetname, Origin and Version terms")
+
+    parser.add_argument('-C', '--clean', action='store_true', default=False,
+                        help='When building a schema, rebuild completely')
+
+    parser.add_argument('-F', '--force', action='store_true', default=False,
+                        help='Force the operation')
+
+    parser.add_argument('-E', '--eda', action='store_true', default=False,
+                        help='Create an EDA notebook for a resource')
+
+    parser.add_argument('-N', '--notebook', action='store_true', default=False,
+                        help='Create a new notebook')
+
+    parser.add_argument('-D', '--descriptions', action='store_true', default=False,
+                        help='Import descriptions for package references')
+
+def run_update(args):
+    m = MetapackCliMemo(args, downloader)
+
+    if m.args.schemas:
+        update_name(m.mt_file, fail_on_missing=False, report_unchanged=False)
+
+        process_schemas(m.mt_file, cache=m.cache, clean=m.args.clean)
+
+    elif m.mtfile_url.scheme == 'file' and m.args.name:
+        update_name(m.mt_file, fail_on_missing=True, force=m.args.force)
+
+    elif m.args.eda:
+        write_eda_notebook(m)
+
+    elif m.args.notebook:
+        write_notebook(m)
+
+    elif m.args.descriptions:
+        update_descriptions(m)
+
+
+def write_notebook(m):
+    # Get the EDA notebook file from Github
+    import nbformat
+
+    url = "https://raw.githubusercontent.com/Metatab/exploratory-data-analysis/master/package-notebook.ipynb"
+
+    r = requests.get(url, allow_redirects=True)
+    r.raise_for_status()
+
+    nb_path = 'notebooks/new-notebook.ipynb'
+
+    ensure_dir(dirname(nb_path))
+
+    if exists(nb_path):
+        err("Notebook {} already exists".format(nb_path))
+
+    with open(nb_path, 'wb') as f:
+        f.write(r.content)
+
+    prt('Wrote {}'.format(nb_path))
+
+
+def write_eda_notebook(m):
+    # Get the EDA notebook file from Github
+    import nbformat
+
+    url = "https://raw.githubusercontent.com/Metatab/exploratory-data-analysis/master/eda.ipynb"
+
+    resource = m.get_resource()
+
+    if not resource:
+        warn('Must specify a resource. Select one of:')
+        list_rr(m.doc)
+        sys.exit(0)
+
+    r = requests.get(url, allow_redirects=True)
+    r.raise_for_status()
+
+    nb_path = 'notebooks/{}-{}.ipynb'.format(splitext(basename(url))[0], resource.name)
+
+    ensure_dir(dirname(nb_path))
+
+    if exists(nb_path):
+        err("Notebook {} already exists".format(nb_path))
+
+    with open(nb_path, 'wb') as f:
+        f.write(r.content)
+
+    prt('Wrote {}'.format(nb_path))
+
+    with open(nb_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    for cell in nb['cells']:
+        if 'resource_name' in cell.get('metadata',{}).get('tags',[]):
+            cell.source = "resource_name='{}'".format(resource.name)
+
+    with open(nb_path, 'wt') as f:
+        nbformat.write(nb, f)
+
+
+
+def update_descriptions(m):
+
+    doc = m.doc
+
+    for ref in doc.references():
+
+        v = ref.find_first('Description')
+
+        ref['Description'] = ref.resource.description
+
+        print(ref.name, id(ref))
+        print("Updated '{}' to '{}'".format(ref.name, ref.description))
+
+    #print(m.doc.as_csv())
+
+    for ref in doc.references():
+        v = ref.find_first('Description')
+
+        print(ref.name, id(ref), ref.description)
+
+    doc.write_csv()
+
+
---
+++ packages/metapack/metapack-0.8.27.tar.gz/metapack-0.8.27/metapack/rowgenerator.py
@@ -0,0 +1,121 @@
+# Copyright (c) 2017 Civic Knowledge. This file is licensed under the terms of the
+# Revised BSD License, included in this distribution as LICENSE
+
+"""
+
+"""
+
+from rowgenerators import Source
+from metapack.jupyter.exec import execute_notebook
+from os.path import join, exists
+from csv import reader
+
+from rowgenerators.exceptions import AppUrlError
+from metapack.util import get_materialized_data_cache
+
+class JupyterNotebookSource(Source):
+    """Generate rows from an IPython Notebook.
+
+     This generator will execute a Jupyter notebook. Before execution, it adds an "%mt_materalize"
+     magic to the notebook, which will cause the target dataframe to be written to a temporary file, and
+     the temporary file is yielded to the caller. Not most efficient, but it fits the model.
+     """
+
+    def __init__(self, ref, cache=None, working_dir=None, env=None, doc=None, **kwargs):
+        super().__init__(ref, cache, working_dir, **kwargs)
+
+        self.env = env
+        self.doc = doc
+
+    def start(self):
+        pass
+
+    def finish(self):
+        pass
+
+
+    def __iter__(self):
+
+        from os.path import basename
+
+        self.start()
+
+        dr_name = get_materialized_data_cache(self.doc)
+
+        path = join(dr_name,  self.ref.target_file + ".csv")
+
+        # The first python notebook in the resources will get executed, cache the datafiles,
+        # then the remaining ones will just use the caches data.
+        if not exists(path):
+
+            # The execute_notebook() function will add a cell with the '%mt_materialize' magic,
+            # with a path that will case the file to be written to the same location as
+            # path, below.
+
+            if not self.ref.target_dataframe():
+                raise AppUrlError('Url did not specify a dataframe; use the "#" fragment ')
+
+            nb = execute_notebook(self.ref.path, dr_name, [self.ref.target_dataframe()], True, self.env)
+
+        with open(path) as f:
+            yield from reader(f)
+
+        self.finish()
+
+
+class PandasDataframeSource(Source):
+    """Iterates a pandas dataframe  """
+
+
+    def __init__(self, url, df, cache, working_dir=None, **kwargs):
+        super().__init__(url, cache, working_dir, **kwargs)
+
+        self._df = df
+
+    def __iter__(self):
+
+        import numpy as np
+
+        self.start()
+
+        df = self._df
+
+        if len(df.index.names) == 1 and df.index.names[0] is None and df.index.dtype != np.dtype('O'):
+            # For an unnamed, single index, assume that it is just a row number
+            # and we don't really need it
+
+            yield list(df.columns)
+
+            for index, row in df.iterrows():
+                yield list(row)
+
+        else:
+
+            # Otherwise, either there are more than
+
+            index_names = [n if n else "index{}".format(i) for i, n in enumerate(df.index.names)]
+
+            yield index_names + list(df.columns)
+
+            if len(df.index.names) == 1:
+                idx_list = lambda x: [x]
+            else:
+                idx_list = lambda x: list(x)
+
+            for index, row in df.iterrows():
+                yield idx_list(index) + list(row)
+
+
+        self.finish()
+
+
+def copy_reference(resource, doc, env, *args, **kwargs):
+    """A row-generating function that yields from a reference. This permits an upstream package to be
+    copied and modified by this package, while being formally referenced as a dependency
+
+    The function will generate rows from a reference that has the same name as the resource term
+    """
+
+    yield from doc.reference(resource.name)
+
+
