93964ca60fdf8b6d3d71c03bbe103a25d34b36d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package FormatRosterData/1.0.12/FormatRosterData-1.0.12-py3-none-any.whl

---
+++ packages/FormatRosterData/FormatRosterData-1.0.12-py3-none-any.whl/FormatRosterData/__init__.py
@@ -0,0 +1,6 @@
+##region ImportThisModule
+from FormatRosterData.Misc import *
+from FormatRosterData.FormatData import *
+from FormatRosterData.WebsiteScraping import *
+del FRDLog #FRDLog needn't be exposed
+##endregion
