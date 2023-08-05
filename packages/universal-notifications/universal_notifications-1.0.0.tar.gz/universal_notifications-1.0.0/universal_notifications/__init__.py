f1327ea546774c2a3d23bc29a021b81da178ebd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package universal-notifications/1.0.0/universal_notifications-1.0.0.tar.gz

---
+++ packages/universal-notifications/universal_notifications-1.0.0.tar.gz/universal_notifications-1.0.0/universal_notifications/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__title__ = "Universal Notifications"
+__version__ = "1.0.0"
+__author__ = "Pawel Krzyzaniak"
+__license__ = "MIT"
+__copyright__ = "Copyright 2017 Arabella"
+
+# Version synonym
+VERSION = __version__
