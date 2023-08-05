31722e61d636569c8a5e085ea5710344e2048c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package ioe/0.0.17/ioe-0.0.17.tar.gz

---
+++ packages/ioe/ioe-0.0.17.tar.gz/ioe-0.0.17/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ioe",
+    version="0.0.17",
+    author="Sergio Alonso",
+    author_email="sergio@sergioalonso.es",
+    description="The Python Internet of Energy Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/sergioalonso/ioe",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        "websockets",
+        "websocket-client"
+    ],
+    dependency_links=[
+      'git+https://github.com/sergio-alonso/python-jsonschema-objects/tarball/draft-handrews-json-schema-01#egg=python-jsonschema-objects-1.0.0.dev',
+    ],
+    include_package_data=True
+)
