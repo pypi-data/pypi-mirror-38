12767e538a199c97d814e02da6f2858b3bf0dc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package GradeCurve/1.0.5/GradeCurve-1.0.5.tar.gz

---
+++ packages/GradeCurve/GradeCurve-1.0.5.tar.gz/GradeCurve-1.0.5/GradeCurve/__init__.py
@@ -0,0 +1,3 @@
+from .Gaussiandistribution import Gaussian
+from .GradeCurve import GradeCurve
+from .Generaldistribution import Distribution
---
+++ packages/GradeCurve/GradeCurve-1.0.5.tar.gz/GradeCurve-1.0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GradeCurve',
+      version='1.0.5',
+      description='Curve grades on a test',
+      packages=['GradeCurve'],
+      author = 'Celestin Hermez',
+      author_email = 'celestinhermez@gmail.com',
+      zip_safe=False)
