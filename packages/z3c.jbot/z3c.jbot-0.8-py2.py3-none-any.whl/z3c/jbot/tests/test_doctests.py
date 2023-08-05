3eb27da25f4df419273be42411e88452201178cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package z3c.jbot/0.8/z3c.jbot-0.8-py2.py3-none-any.whl

---
+++ packages/z3c.jbot/z3c.jbot-0.8-py2.py3-none-any.whl/z3c/jbot/tests/test_doctests.py
@@ -0,0 +1,45 @@
+import doctest
+import re
+import unittest
+
+import six
+
+import zope.component
+import zope.interface
+
+from .common import setUp
+from .common import tearDown
+
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+
+class Py23DocChecker(doctest.OutputChecker):
+    def check_output(self, want, got, optionflags):
+        if six.PY2:
+            want = re.sub("b'(.*?)'", "'\\1'", want)
+        else:
+            want = re.sub("u'(.*?)'", "'\\1'", want)
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
+
+def test_suite():
+    globs = dict(
+        interface=zope.interface,
+        component=zope.component)
+
+    return unittest.TestSuite((
+        doctest.DocFileSuite(
+            'README.txt',
+            optionflags=OPTIONFLAGS,
+            setUp=setUp,
+            tearDown=tearDown,
+            globs=globs,
+            package="z3c.jbot",
+            checker=Py23DocChecker(),
+        ),
+    ))
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
