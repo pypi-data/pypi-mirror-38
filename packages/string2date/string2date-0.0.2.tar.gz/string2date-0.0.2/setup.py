f5b61ba6382566f8c43b44d7b41a0a2b3665a072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package string2date/0.0.2/string2date-0.0.2.tar.gz

---
+++ packages/string2date/string2date-0.0.2.tar.gz/string2date-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='string2date',
+      version='0.0.2',
+      description='string to date',
+      url='https://github.com/BigDataFounder/string2date',
+      author='hurpe',
+      author_email='hurpe.ma@gmail.com',
+      license='MIT',
+      packages=['string2date'])
