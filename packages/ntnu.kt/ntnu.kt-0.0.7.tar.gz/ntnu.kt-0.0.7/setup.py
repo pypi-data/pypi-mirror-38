01cf02bb41d8a3807f5fbe104ba4e648362c067d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package ntnu.kt/0.0.7/ntnu.kt-0.0.7.tar.gz

---
+++ packages/ntnu.kt/ntnu.kt-0.0.7.tar.gz/ntnu.kt-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="ntnu.kt",
+      version="0.0.7",
+      description="Standard library for NTNU Konstruksjonsteknikk",
+      url="https://github.com/ntnu-kt/ntnu.kt",
+      author="Teodor Heggelund",
+      author_email="teodor.heggelund@gmail.com",
+      license="MIT",
+      packages=["ntnu.kt"],
+      zip_safe=False
+      )
