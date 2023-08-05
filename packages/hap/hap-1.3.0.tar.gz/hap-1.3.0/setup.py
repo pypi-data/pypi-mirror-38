1bba11edb55672407dfb5f2016400de64fd6a9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package hap/1.3.0/hap-1.3.0.tar.gz

---
+++ packages/hap/hap-1.3.0.tar.gz/hap-1.3.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import io
+
+from setuptools import setup
+from os import path
+
+from hap import __version__
+
+
+long_description = "Hap! description"
+
+directory = path.abspath(path.dirname(__file__))
+with io.open(path.join(directory, "README.md"), encoding="utf-8") as fd:
+    long_description = fd.read()
+
+
+setup(name="hap",
+    packages=[
+        "hap",
+    ],
+    entry_points = {
+        "console_scripts": [
+            "hap = hap.bootstrap:main"
+        ]
+    },
+    test_suite="tests",
+    install_requires=[
+        "lxml==3.6.4",
+        "cssselect==1.0.0",
+    ],
+    version=__version__,
+    description="A simple HTML scraping tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Alexandru Catrina",
+    author_email="alex@codeissues.net",
+    license="MIT",
+    url="https://github.com/lexndru/hap",
+    download_url="https://github.com/lexndru/hap",
+    keywords=["html", "scraping", "crawler", "tool", "hap"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
