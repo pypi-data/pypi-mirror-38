d9e7529edc7b83690093219efc6eddad6c7545a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package pytest-random-order/1.0.2/pytest-random-order-1.0.2.tar.gz

---
+++ packages/pytest-random-order/pytest-random-order-1.0.2.tar.gz/pytest-random-order-1.0.2/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-random-order',
+    version='1.0.2',
+    author='Jazeps Basko',
+    author_email='jazeps.basko@gmail.com',
+    maintainer='Jazeps Basko',
+    maintainer_email='jazeps.basko@gmail.com',
+    license='MIT',
+    url='https://github.com/jbasko/pytest-random-order',
+    description='Randomise the order in which pytest tests are run with some control over the randomness',
+    long_description=read('README.rst'),
+    py_modules=[
+        'random_order.bucket_types',
+        'random_order.cache',
+        'random_order.config',
+        'random_order.plugin',
+        'random_order.shuffler',
+    ],
+    python_requires=">=3.5.0",
+    install_requires=[
+        'pytest>=2.9.2',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='pytest random test order shuffle',
+    entry_points={
+        'pytest11': [
+            'random_order = random_order.plugin',  # >=1.0.0
+        ],
+    },
+)
