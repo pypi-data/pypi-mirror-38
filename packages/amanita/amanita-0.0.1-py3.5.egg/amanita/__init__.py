f803b7cc1e24088598fe3548ae81f43c4c462dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package amanita/0.0.1/amanita-0.0.1-py3.5.egg

---
+++ packages/amanita/amanita-0.0.1-py3.5.egg/amanita/__init__.py
@@ -0,0 +1,6 @@
+"""Create custom python projects"""
+
+__title__ = 'amanita'
+__version__ = '0.0.1'
+__author__ = 'constrict0r <constrict0r@protonmail.com>'
+__all__ = []
---
+++ packages/amanita/amanita-0.0.1-py3.5.egg/amanita/amanita.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: amanita
+    :noindex:
+    :platform: Linux
+    :synopsis: create customizable python projects.
+
+.. moduleauthor:: constrict0r <constrict0r@protonmail.com>
+"""
+
+
+def enjoy():
+    """A tasty amanita.
+
+    Enjoy!!
+
+    Returns:
+        string: Tasty amanita.
+
+    """
+    amanita = """\
+          ___.....___
+      .'"    ()      o"`
+    .                    . 
+   .                      .
+  .              O         .       
+ .  ()   o___...___ O       .     
+. _.--'"'            '"'--._ .
+:"                          ";   
+ `-.__     :     :      __.-'
+       "'"-:      :-"'"       
+            J       L                   
+            :        :                     
+            J        L               
+           :         :
+           :         :           
+      '''|| `._____.'''|||
+    """
+    return amanita
