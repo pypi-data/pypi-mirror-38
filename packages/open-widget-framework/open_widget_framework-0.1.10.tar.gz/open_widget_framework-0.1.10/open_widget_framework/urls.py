428a85f40444fa9c3132ed03a6a828d6548e3a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:20 +0000

    Add package open-widget-framework/0.1.10/open_widget_framework-0.1.10.tar.gz

---
+++ packages/open-widget-framework/open_widget_framework-0.1.10.tar.gz/open_widget_framework-0.1.10/open_widget_framework/urls.py
@@ -0,0 +1,27 @@
+"""
+WidgetApp urls
+"""
+from django.conf.urls import url
+from open_widget_framework.views import (
+    WidgetListView,
+    WidgetView,
+    get_widget_lists,
+    get_widget_configurations,
+)
+
+
+urlpatterns = [
+    url(
+        r"^api/v1/configurations$", get_widget_configurations, name="get_configurations"
+    ),
+    url(
+        r"^api/v1/list/(?P<widget_list_id>\d*)$",
+        WidgetListView.as_view(),
+        name="widget_list_view",
+    ),
+    url(
+        r"^api/v1/list/(?P<widget_list_id>\d+)/widget/(?P<widget_id>\d*)$",
+        WidgetView.as_view(),
+        name="widget_view",
+    ),
+]
