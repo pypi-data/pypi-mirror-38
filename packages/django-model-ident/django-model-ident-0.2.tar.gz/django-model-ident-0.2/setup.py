1445db1acae537dda10c11d450364ba4a11d5b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package django-model-ident/0.2/django-model-ident-0.2.tar.gz

---
+++ packages/django-model-ident/django-model-ident-0.2.tar.gz/django-model-ident-0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import model_ident
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-model-ident',
+    version=model_ident.__version__,
+    description="Django app to provide classes quick pk only lookups",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Chaim Kirby',
+    author_email='chaim.kirby@gmail.com',
+    url='https://github.com/ckirby/django-model-ident',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
