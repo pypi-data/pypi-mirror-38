72ee92457d0b9cffe014546a67c1ab4fc6f8cab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package mp/0.3.3.1/mp-0.3.3.1.tar.gz

---
+++ packages/mp/mp-0.3.3.1.tar.gz/mp-0.3.3.1/mp/console.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+from mp import PythonInterpreter
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(prog='mp', usage='%(prog)s [options]')
+    parser.add_argument('-d', '--dir-process', help='Run the shell from that directory. (default: current directory)',
+                        default='.')
+    parser.add_argument('--debug', help='Catch the exception and interrupt process. (default: False)',
+                        action='store_true')
+    args = parser.parse_args()
+    cmd = PythonInterpreter(dir_process=args.dir_process)
+    cmd.begin_interactive(debug=args.debug)
