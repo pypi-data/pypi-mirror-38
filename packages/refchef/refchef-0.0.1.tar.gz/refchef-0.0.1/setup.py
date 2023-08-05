c619cdf16e93b5d61e7b37184b3fb474178cc3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:20 +0000

    Add package refchef/0.0.1/refchef-0.0.1.tar.gz

---
+++ packages/refchef/refchef-0.0.1.tar.gz/refchef-0.0.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="refchef",
+    version="0.0.1",
+    author="Andrew Leith & Fernando Gelin",
+    author_email="aleith@brown.edu",
+    description="Genome reference manager.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/compbiocore/refchef",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: MacOS",
+        "Operating System :: Unix"
+    ],
+    install_requires=[
+        "argparse",
+        "python-dotenv",
+        "PyYAML",
+        "PyGithub",
+        "yamlloader",
+        "pandas",
+        "terminaltables",
+        "mock",
+        "future"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    scripts=["scripts/refchef-cook", "scripts/refchef-menu"]
+)
