5b6f38b475b05ba991bda03ee2e10aed1a3ebe71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package mor/0.0.1/mor-0.0.1.tar.gz

---
+++ packages/mor/mor-0.0.1.tar.gz/mor-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mor",
+    version="0.0.1",
+    author="Vanneste Felix",
+    author_email="felix.vanneste@inria.fr",
+    description="This mor package is a simple GUI to help user perform model order reduction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SofaDefrost/ModelOrderReduction",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=['cheetah','PyQt4','yaml']
+)
---
+++ packages/mor/mor-0.0.1.tar.gz/mor-0.0.1/utility/graphScene.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+import os
+import yaml
+
+try:
+    from launcher import *
+except:
+    raise ImportError("You need to give to PYTHONPATH the path to sofa-launcher in order to use this tool\n"\
+                     +"Enter this command in your terminal (for temporary use) or in your .bashrc to resolve this:\n"\
+                     +"export PYTHONPATH=/PathToYourSofaSrcFolder/tools/sofa-launcher")
+
+def getGraphScene(node,getObj=False):
+    class Namespace(object):
+        pass
+    tmp = Namespace()
+    tmp.results = {}
+    tmp.results["tree"] = {}
+    tmp.results["obj"] = {}
+    # tmp.results = None
+
+    def buildTree(node,dic):
+        if node.name != "root":
+            if getObj:
+                dic[node] = {}
+            else:
+                dic[node.name] = {}
+                # dic = AnyNode(node.name = )
+
+        for child in node.getChildren():
+            if node.name != "root":
+                if getObj:
+                    buildTree(child,dic[node])
+                else:
+                    buildTree(child,dic[node.name])
+            else:
+                buildTree(child,dic)
+    def objTree(node,dic):
+
+        if getObj:
+            dic[node] = {}
+        else:
+            dic[node.name] = {}
+
+        for obj in node.getObjects():
+            # print('root '+str(type(obj).__name__)+' '+obj.getName())
+
+            if getObj:
+                dic[node][obj] = obj.getClassName()
+            else:
+                dic[node.name][obj.getName()] = obj.getClassName()
+
+        for child in node.getChildren():
+            if getObj:
+                objTree(child,dic)
+            else:
+                objTree(child,dic)
+
+    buildTree(node,tmp.results["tree"])
+    # print(tmp.results["tree"])
+    objTree(node,tmp.results["obj"])
+    # print(tmp.results["obj"])
+
+    # for key in tmp.results:
+    #     print(str(key)+' : '+str(tmp.results[key])+'\n')
+
+    return tmp.results
+
+def dumpGraphScene(node,fileName='graphScene.yml'):
+
+    data = getGraphScene(node)
+    with open(fileName, 'w') as ymlfile:
+        yaml.dump(data,ymlfile, default_flow_style=False)
+
+def importScene(filePath):
+    numiterations = 1
+    filename = "importScene.py"
+    path = os.path.dirname(os.path.abspath(__file__))+'/template/'
+    filesandtemplates = [ (open(path+filename).read(), filename)]
+
+    listSofaScene = [
+                        {"ORIGINALSCENE": filePath,
+                         "nbIterations":numiterations}
+                    ]
+
+    results = startSofa(listSofaScene, filesandtemplates, launcher=SerialLauncher())
+
+    with open(results[0]["directory"]+'/graphScene.yml', 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+    return cfg
