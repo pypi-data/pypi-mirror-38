1adcd05a78cea58cd31b7bfb6ad8d194ba1dd352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:08 +0000

    Add package converter-cli/1.0.5/converter-cli-1.0.5.tar.gz

---
+++ packages/converter-cli/converter-cli-1.0.5.tar.gz/converter-cli-1.0.5/convert/commands/audio.py
@@ -0,0 +1,38 @@
+from .base import Base
+
+from ..converter_utils import let_user_pick
+from ..converter_utils import print_message as prmsg
+from ..converter_utils import run_ffmpeg
+
+
+class Audio(Base):
+    """Get user input to execute different video conversions"""
+
+    def __init__(self, options, *args, **kwargs):
+        super().__init__(options, *args, **kwargs)
+        self.conversion_map = {
+            1: {
+                'option_text': 'Convert to .mp3 (320k)',
+                'extension': 'mp3',
+                'params': {
+                    'ar': 44100,
+                    'ac': 2,
+                    'ab': '320k ',
+                    'f': 'mp3'
+                }
+            },
+            2: {
+                'option_text': 'Convert to .wav',
+                'extension': 'wav',
+                'params': {}
+            }
+        }
+
+    def run(self):
+        """Run the Audio command."""
+        chosen_option = let_user_pick(self.conversion_map)
+        source_path, output_paths, params = self.get_user_input(
+            self.conversion_map[chosen_option])
+        for output_path in output_paths:
+            run_ffmpeg(source_path, output_path, params, self.options)
+        prmsg('completed')
---
+++ packages/converter-cli/converter-cli-1.0.5.tar.gz/converter-cli-1.0.5/convert/commands/base.py
@@ -0,0 +1,98 @@
+import os
+
+from termcolor import colored
+
+from ..converter_utils import clear
+from ..converter_utils import confirmator
+from ..converter_utils import multi_source
+from ..converter_utils import print_message as prmsg
+from ..converter_utils import single_source
+from ..converter_utils import validate_path
+
+
+class Base(object):
+    """A base command."""
+
+    def __init__(self, options, *args, **kwargs):
+        """Create the Base object (a command object).
+
+        :param options: command options
+        :type options: dict
+        """
+
+        self.options = options
+        self.args = args
+        self.kwargs = kwargs
+        clear()
+
+    def get_user_input(self, conversion_data):
+        """Set all needed variables via user input for the conversion.
+
+        :param conversion_data: command based data (see command init)
+        :type conversion_data: dict
+        :returns: source_path
+                  output_paths
+                  conversion_data_params
+        :rtype: string
+                list
+                list
+        """
+
+        if self.options['--multiple']:
+            source_folder, source_extension = multi_source()
+        else:
+            source_path, source_name, source_folder = single_source()
+
+        default_folder = '{}'.format(source_folder)
+        destination = input(colored(
+            "Enter path to destination folder "
+            "(Enter for same folder as source): ", 'green')) or default_folder
+        destination = validate_path(destination, 'folder')
+
+        output_paths = []
+
+        # multiple files flow
+        if self.options['--multiple']:
+            confirmator(
+                self.options,
+                **{'ori_ext': source_extension,
+                   'ori_folder': source_folder,
+                   'out_ext': conversion_data['extension'],
+                   'out_folder': destination})
+
+            # output_paths = get_multiple_outputs(
+            #     source_folder, source_extension)
+
+            folder = os.fsencode(source_folder)
+            for file in os.listdir(folder):
+                # get source filename
+                filename = os.fsdecode(file)
+                # set needed vars based on filename
+                source_path = os.path.join(source_folder, filename)
+                source_name = os.path.splitext(os.path.split(filename)[1])[0]
+                source_ext = os.path.splitext(os.path.split(filename)[1])[1]
+
+                # check extension fits
+                if source_ext == '.{}'.format(source_extension):
+                    output_path = '{}{}.{}'.format(
+                        destination, source_name, conversion_data['extension'])
+                    output_paths.append(output_path)
+
+        # single file flow
+        else:
+            # do not show confirmation message if the option is enabled
+            confirmator(
+                self.options,
+                **{'ori_path': source_path,
+                   'out_ext': conversion_data['extension'],
+                   'out_folder': destination})
+
+            output_paths = ['{}{}.{}'.format(
+                destination, source_name, conversion_data['extension'])]
+
+        return source_path, output_paths, conversion_data['params']
+
+    def run(self):
+        """All commands must implement this method."""
+        raise NotImplementedError(
+            'You must implement the run() method yourself!')
