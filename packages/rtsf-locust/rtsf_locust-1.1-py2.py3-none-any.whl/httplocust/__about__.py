9b91c8f1706079845b89903939d798a1e2a49f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package rtsf-locust/1.1/rtsf_locust-1.1-py2.py3-none-any.whl

---
+++ packages/rtsf-locust/rtsf_locust-1.1-py2.py3-none-any.whl/httplocust/__about__.py
@@ -0,0 +1,29 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+Current module: httplocust.__about__
+
+Rough version history:
+v1.0    Original version to use
+
+********************************************************************
+    @AUTHOR:  Administrator-Bruce Luo(罗科峰)
+    MAIL:     luokefeng@163.com
+    RCS:      httplocust.__about__,  v1.0 2018年10月23日
+    FROM:   2018年10月23日
+********************************************************************
+======================================================================
+
+Provide a function for the automation test
+
+'''
+
+
+__title__           = "rtsf-locust"
+__short_desc__      = "http(s) load testing, base on locust and rtsf-http"
+__version__         = "1.1"
+__autor__           = u"罗科峰"
+__author_email__    = "lkf20031988@163.com"
+__license__         = 'MIT'
+
+HOME_PAGE           = "https://github.com/RockFeng0/rtsf-locust"
