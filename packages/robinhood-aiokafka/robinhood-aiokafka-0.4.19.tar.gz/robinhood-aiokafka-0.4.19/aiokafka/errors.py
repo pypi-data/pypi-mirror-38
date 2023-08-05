c61a7d18f31c2e5cf5c11f1b25571209db28f284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package robinhood-aiokafka/0.4.19/robinhood-aiokafka-0.4.19.tar.gz

---
+++ packages/robinhood-aiokafka/robinhood-aiokafka-0.4.19.tar.gz/robinhood-aiokafka-0.4.19/aiokafka/errors.py
@@ -0,0 +1,48 @@
+from rhkafka.errors import *  # noqa
+from rhkafka.errors import InvalidMessageError, KafkaError
+__all__ = [
+    # kafka-python errors
+    "KafkaError", "ConnectionError", "NodeNotReadyError",
+    "KafkaTimeoutError", "UnknownTopicOrPartitionError",
+    "UnrecognizedBrokerVersion", "NotLeaderForPartitionError",
+    "LeaderNotAvailableError", "TopicAuthorizationFailedError",
+    "OffsetOutOfRangeError", "MessageSizeTooLargeError", "for_code", "NoError",
+    "StaleMetadata", "CorrelationIdError", "NoBrokersAvailable",
+    "RebalanceInProgressError", "IllegalGenerationError",
+    "UnknownMemberIdError", "GroupLoadInProgressError",
+    "GroupCoordinatorNotAvailableError", "NotCoordinatorForGroupError",
+    "GroupAuthorizationFailedError", "IllegalStateError",
+    "UnsupportedVersionError", "CorruptRecordException", "InvalidMessageError",
+    # aiokafka custom errors
+    "ConsumerStoppedError", "NoOffsetForPartitionError", "RecordTooLargeError",
+    "ProducerClosed",
+]
+
+
+class CorruptRecordException(InvalidMessageError):
+    pass
+
+
+class ConsumerStoppedError(Exception):
+    """ Raised on `get*` methods of Consumer if it's cancelled, even pending
+        ones.
+    """
+
+
+class IllegalOperation(Exception):
+    """ Raised if you try to execute an operation, that is not available with
+        current configuration. For example trying to commit if no group_id was
+        given.
+    """
+
+
+class NoOffsetForPartitionError(KafkaError):
+    pass
+
+
+class RecordTooLargeError(KafkaError):
+    pass
+
+
+class ProducerClosed(KafkaError):
+    pass
---
+++ packages/robinhood-aiokafka/robinhood-aiokafka-0.4.19.tar.gz/robinhood-aiokafka-0.4.19/aiokafka/structs.py
@@ -0,0 +1,18 @@
+import collections
+from rhkafka.common import OffsetAndMetadata, TopicPartition
+
+__all__ = [
+    "OffsetAndMetadata", "TopicPartition", "RecordMetadata", "ConsumerRecord"
+]
+
+RecordMetadata = collections.namedtuple(
+    'RecordMetadata', ['topic', 'partition', 'topic_partition', 'offset',
+                       'timestamp', 'timestamp_type'])
+
+ConsumerRecord = collections.namedtuple(
+    "ConsumerRecord", ["topic", "partition", "offset", "timestamp",
+                       "timestamp_type", "key", "value", "checksum",
+                       "serialized_key_size", "serialized_value_size"])
+
+OffsetAndTimestamp = collections.namedtuple(
+    "OffsetAndTimestamp", ["offset", "timestamp"])
