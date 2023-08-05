f390a7f91c66f499135121ad6f285a6eec170b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package tl.eggdeps/0.5.1/tl.eggdeps-0.5.1.tar.gz

---
+++ packages/tl.eggdeps/tl.eggdeps-0.5.1.tar.gz/tl.eggdeps-0.5.1/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""Compute a dependency graph between active Python eggs.
+"""
+
+from setuptools import setup, find_packages
+
+
+longdesc = "\n\n".join((open("README.rst").read(),
+                        open("ABOUT.rst").read()))
+
+entry_points = {
+    "console_scripts": [
+        "eggdeps = tl.eggdeps.cli:eggdeps",
+    ],
+    }
+
+install_requires = [
+    "setuptools",
+    ]
+
+tests_require = [
+    "zope.testing",
+    ]
+
+extras_require = {
+    "test": tests_require,
+    }
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Zope Public License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2 :: Only",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development",
+    "Topic :: Utilities",
+    ]
+
+setup(name="tl.eggdeps",
+      version='0.5.1',
+      description=__doc__.strip(),
+      long_description=longdesc,
+      keywords="egg eggs dependencies dependency graph tree",
+      classifiers=classifiers,
+      author="Thomas Lotze",
+      author_email="thomas@thomas-lotze.de",
+      url="https://github.com/tlotze/tl.eggdeps",
+      license="ZPL 2.1",
+      packages=find_packages(),
+      entry_points=entry_points,
+      install_requires=install_requires,
+      extras_require=extras_require,
+      tests_require=tests_require,
+      include_package_data=True,
+      test_suite="tl.eggdeps.tests.test_suite",
+      namespace_packages=["tl"],
+      zip_safe=False,
+      )
---
+++ packages/tl.eggdeps/tl.eggdeps-0.5.1.tar.gz/tl.eggdeps-0.5.1/tl/eggdeps/tests.py
@@ -0,0 +1,73 @@
+from zope.testing import doctest
+from zope.testing.doctest import DocTestSuite, DocFileSuite
+import os.path
+import pkg_resources
+import setuptools.tests.test_resources
+import tl.eggdeps.requirements
+import unittest
+
+
+def make_dist(filename, depends=""):
+    metadata = setuptools.tests.test_resources.Metadata(("depends.txt",
+                                                         depends))
+    return pkg_resources.Distribution.from_filename(filename,
+                                                    metadata=metadata)
+
+
+def make_working_set(*dists):
+    ws = pkg_resources.WorkingSet([])
+    for dist in dists:
+        ws.add(dist)
+    return ws
+
+
+def _sprint(value):
+    if isinstance(value, set):
+        return "set([%s])" % ", ".join(repr(elem) for elem in sorted(value))
+    if isinstance(value, dict):
+        return "{%s}" % ", ".join("%r: %s" % (key, _sprint(value))
+                                  for key, value in sorted(value.iteritems()))
+    return repr(value)
+
+
+def sprint(value):
+    print _sprint(value)
+
+
+def sort_specs(specs):
+    return sorted(specs, cmp=lambda a, b: cmp(a.project_name, b.project_name))
+
+
+class Options(dict):
+
+    version_numbers = False
+    once = False
+    terse = False
+    cluster = False
+    version_specs = False
+
+    def __init__(self, *args, **kwargs):
+        self.__dict__ = self
+        super(Options, self).__init__(*args, **kwargs)
+
+
+def test_suite():
+    return unittest.TestSuite([
+        DocTestSuite(tl.eggdeps.requirements)
+        ] + [
+        DocFileSuite(filename,
+                     package="tl.eggdeps",
+                     globs=dict(make_dist=make_dist,
+                                make_working_set=make_working_set,
+                                sprint=sprint,
+                                sort_specs=sort_specs,
+                                Options=Options,
+                                ),
+                     optionflags=doctest.NORMALIZE_WHITESPACE |
+                                 (0 if filename == 'plaintext.txt'
+                                  else doctest.ELLIPSIS) |
+                                 doctest.REPORT_NDIFF,
+                     )
+        for filename in sorted(os.listdir(os.path.dirname(__file__)))
+        if filename.endswith(".txt")
+        ])
