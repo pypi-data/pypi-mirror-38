13c64a4cbd3f3cade8393aa8a11557a781ee5f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:23 +0000

    Add package TwitterCloud/0.1/TwitterCloud-0.1.tar.gz

---
+++ packages/TwitterCloud/TwitterCloud-0.1.tar.gz/TwitterCloud-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TwitterCloud',
+      version='0.1',
+      description='Create a wordcloud with a timeline for yourself or that tweep on your timeline',
+      url='https://github.com/tinumide/TwiterCloud.git',
+      author='Tinuade Adeleke',
+      author_email='summittinuade@gmail.com',
+      license='MIT',
+      packages=['TwitterCloud'],
+      zip_safe=False)
