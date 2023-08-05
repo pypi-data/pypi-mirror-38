0af7b12d902b01fdee130461b846a7d093b61e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:04 +0000

    Add package qindomClient/1.0.5/qindomClient-1.0.5.tar.gz

---
+++ packages/qindomClient/qindomClient-1.0.5.tar.gz/qindomClient-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qindomClient",
+    version="1.0.5",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=['request'],
+)
