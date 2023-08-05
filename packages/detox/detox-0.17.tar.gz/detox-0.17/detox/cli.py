c41ac94d7b21d712a851ba5c44d0f6999e31d81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package detox/0.17/detox-0.17.tar.gz

---
+++ packages/detox/detox-0.17.tar.gz/detox-0.17/detox/cli.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import sys
+
+from tox.session import prepare as tox_prepare
+
+from detox import __version__
+from detox.proc import Detox
+
+
+def main(args=None):
+    args = sys.argv[1:] if args is None else args
+    if args and args[0] == "--version":
+        print("detox {} running as plugin in tox:".format(__version__))
+        # fall through to let tox add its own version info ...
+    config = tox_prepare(args)
+    detox = Detox(config)
+    if not hasattr(config.option, "quiet_level") or not config.option.quiet_level:
+        detox.startloopreport()
+    return detox.runtestsmulti(detox.toxsession.evaluated_env_list())
