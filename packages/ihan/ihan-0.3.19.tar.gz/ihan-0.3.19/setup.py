aecaa767e788339bc0c27ecd7bdaa7b8de5cc570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:47 +0000

    Add package ihan/0.3.19/ihan-0.3.19.tar.gz

---
+++ packages/ihan/ihan-0.3.19.tar.gz/ihan-0.3.19/setup.py
@@ -0,0 +1,43 @@
+import os
+from   setuptools import setup
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+_read = lambda filename: open(os.path.join(dir_path, filename)).read()
+
+setup(name='ihan',
+      version=_read('VERSION'),
+      description='IHAN Client for feeding and back filling log files',
+      long_description=_read('docs/index.rst'),
+      long_description_content_type='text/x-rst',
+      url='https://www.ihan.ee/',
+      project_urls={
+        "Bug Tracker": "https://github.com/marklit/ihan/issues",
+        "Documentation": "https://ihan.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/marklit/ihan",
+      },
+      author='Mark Litwintschik',
+      author_email='mark@marksblogg.com',
+      license='MIT',
+      packages=['ihan'],
+      scripts=['bin/ihan',],
+      install_requires=[
+          "Click",
+          "requests",
+      ],
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: System :: Networking :: Monitoring',
+        ],
+      zip_safe=False)
