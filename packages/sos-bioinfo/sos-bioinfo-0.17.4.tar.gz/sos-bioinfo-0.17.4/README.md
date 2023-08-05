a71e24b2cae1a30aced469d45c201245c4917516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:49 +0000

    Add package sos-bioinfo/0.17.4/sos-bioinfo-0.17.4.tar.gz

---
+++ packages/sos-bioinfo/sos-bioinfo-0.17.4.tar.gz/sos-bioinfo-0.17.4/README.md
@@ -0,0 +1,6 @@
+[![PyPI version](https://badge.fury.io/py/sos-bioinfo.svg)](https://badge.fury.io/py/sos-bioinfo)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/6bfa650c4f1f4db1a96f010a6c30b4f5)](https://www.codacy.com/app/BoPeng/sos-bioinfo?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=vatlab/sos-bioinfo&amp;utm_campaign=Badge_Grade)
+
+# sos-bioinfo
+Bioinformatics extensions for SoS. Please refer to [SoS Homepage](http://vatlab.github.io/SoS/) for details.
+
---
+++ packages/sos-bioinfo/sos-bioinfo-0.17.4.tar.gz/sos-bioinfo-0.17.4/test/test_preview.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
+# Distributed under the terms of the 3-clause BSD License.
+
+import os
+import sys
+import unittest
+from sos_notebook.test_utils import sos_kernel, get_std_output
+from ipykernel.tests.utils import execute
+
+class TestPreview(unittest.TestCase):
+    def setUp(self):
+        self.resource_dir = os.path.abspath(os.path.split(__file__)[0])
+
+    @unittest.skipIf(sys.platform == 'win32', 'pysam is not installed under windows')
+    def testMagicPreview(self):
+        with sos_kernel() as kc:
+            # preview bam file
+            iopub = kc.iopub_channel
+            execute(kc=kc, code='''
+%preview -n {}/sim_reads_aligned.bam
+'''.format(self.resource_dir))
+            stdout, stderr = get_std_output(iopub)
+            self.assertEqual(stderr, '')
+            self.assertTrue('PG' in stdout)
+
+            # preview sam file
+            execute(kc=kc, code='''
+%preview -n {}/sim_reads_aligned.sam
+'''.format(self.resource_dir))
+            stdout, stderr = get_std_output(iopub)
+            self.assertEqual(stderr, '')
+            self.assertTrue('PG' in stdout)
+
+
+if __name__ == '__main__':
+    unittest.main()
