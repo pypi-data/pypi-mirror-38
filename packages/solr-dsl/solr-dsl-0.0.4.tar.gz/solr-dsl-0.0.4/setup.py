9674e0f5574d90a0d9c42835c3c130ba9e8ba9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:08 +0000

    Add package solr-dsl/0.0.4/solr-dsl-0.0.4.tar.gz

---
+++ packages/solr-dsl/solr-dsl-0.0.4.tar.gz/solr-dsl-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="solr-dsl",
+    version="0.0.4",
+    author="Elliot Penson",
+    author_email="elliotpenson@gmail.com",
+    description="Python client for Solr",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/it-com-engineering/solr-dsl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
