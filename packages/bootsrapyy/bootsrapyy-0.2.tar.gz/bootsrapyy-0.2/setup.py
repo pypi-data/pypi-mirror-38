b3b69036390d77333184f114f5fd8d35d481cb22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:11 +0000

    Add package bootsrapyy/0.2/bootsrapyy-0.2.tar.gz

---
+++ packages/bootsrapyy/bootsrapyy-0.2.tar.gz/bootsrapyy-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='bootsrapyy',
+    packages=['bootsrapyy'],
+    version='0.2',
+    description='Calculate statistical features from text',
+    author='Ganes',
+    author_email='shivam5992@gmail.com',
+    package_data={'': ['data/*.zip']},
+    include_package_data=True,
+    license='MIT',
+    classifiers=(
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        ),
+)
