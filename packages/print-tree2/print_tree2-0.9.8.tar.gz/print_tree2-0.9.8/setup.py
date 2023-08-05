050fd5b4a04081199245b578f31f03b237e50fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package print-tree2/0.9.8/print_tree2-0.9.8.tar.gz

---
+++ packages/print-tree2/print_tree2-0.9.8.tar.gz/print_tree2-0.9.8/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "rb") as fin:
+    long_description = fin.read().decode("utf-8")
+
+setuptools.setup(
+    name="print_tree2",
+    version="0.9.8",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liwt31/print-tree",
+    license="MIT",
+    author="Weitang Li",
+    author_email="liwt31@163.com",
+    description="Print trees",
+)
