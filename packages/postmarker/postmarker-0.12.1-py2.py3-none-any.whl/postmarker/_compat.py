ea94c582944223ff12a7fad8eb5f76b8fefd50ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:48 +0000

    Add package postmarker/0.12.1/postmarker-0.12.1-py2.py3-none-any.whl

---
+++ packages/postmarker/postmarker-0.12.1-py2.py3-none-any.whl/postmarker/_compat.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+
+try:
+    from urllib.parse import urljoin
+except ImportError:  # Python 2.7
+    from urlparse import urljoin  # noqa
+
+
+try:
+    from unittest.mock import patch
+except ImportError:  # Python < 3.3
+    from mock import patch  # noqa
+
+
+try:
+    from inspect import signature
+
+    def get_args(cls):
+        return list(signature(cls).parameters)
+
+except ImportError:
+    from inspect import getargspec
+
+    def get_args(cls):
+        return getargspec(cls.__init__).args[1:]
---
+++ packages/postmarker/postmarker-0.12.1-py2.py3-none-any.whl/postmarker/pytest.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from __future__ import absolute_import
+
+import pytest
+
+from ._compat import patch
+from .core import PostmarkClient, requests
+
+
+@pytest.yield_fixture
+def postmark_request():
+    """
+    Mocks network requests to Postmark API.
+    """
+    with patch('postmarker.core.requests.Session.request', wraps=requests.Session().request) as patched:
+        with patch('postmarker.core.requests.Session.send'):
+            yield patched
+
+
+@pytest.yield_fixture
+def postmark_client(postmark_request):
+    client = PostmarkClient(server_token='SERVER_TOKEN', account_token='ACCOUNT_TOKEN')
+    client.mock = postmark_request
+    yield client
