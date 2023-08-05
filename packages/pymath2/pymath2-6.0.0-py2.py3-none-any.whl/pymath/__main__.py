7e7a9827d3b04ca394da0126a5bbb508cd787bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package pymath2/6.0.0/pymath2-6.0.0-py2.py3-none-any.whl

---
+++ packages/pymath2/pymath2-6.0.0-py2.py3-none-any.whl/pymath/__main__.py
@@ -0,0 +1,8 @@
+from decimal import Decimal
+from fractions import Fraction
+from itertools import (combinations, combinations_with_replacement,
+                       permutations, product)
+from math import *
+from statistics import *
+
+from pymath import *
---
+++ packages/pymath2/pymath2-6.0.0-py2.py3-none-any.whl/pymath/number_theory/__init__.py
@@ -0,0 +1,4 @@
+from .analytic import *
+from .factor import *
+from .number_types import *
+from .primes import *
