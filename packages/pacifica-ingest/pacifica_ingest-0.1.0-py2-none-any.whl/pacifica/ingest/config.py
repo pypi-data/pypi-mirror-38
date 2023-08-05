2eb3015d94c6eb5c694cbc5e5c63fe001bc35724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:43 +0000

    Add package pacifica-ingest/0.1.0/pacifica_ingest-0.1.0-py2-none-any.whl

---
+++ packages/pacifica-ingest/pacifica_ingest-0.1.0-py2-none-any.whl/pacifica/ingest/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Configuration reading and validation module."""
+try:
+    from ConfigParser import SafeConfigParser
+except ImportError:  # pragma: no cover python 2 vs 3 issue
+    from configparser import SafeConfigParser
+from pacifica.ingest.globals import CONFIG_FILE
+
+
+def get_config():
+    """Return the ConfigParser object with defaults set."""
+    configparser = SafeConfigParser({
+        'peewee_url': 'sqliteext:///db.sqlite3'
+    })
+    configparser.add_section('database')
+    configparser.read(CONFIG_FILE)
+    return configparser
