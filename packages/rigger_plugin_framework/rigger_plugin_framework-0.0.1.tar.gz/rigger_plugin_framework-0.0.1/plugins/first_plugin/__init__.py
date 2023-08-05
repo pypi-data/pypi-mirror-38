b30bb29e5897f6cfcea5b9e85d1a476ad7a11366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:53 +0000

    Add package rigger_plugin_framework/0.0.1/rigger_plugin_framework-0.0.1.tar.gz

---
+++ packages/rigger_plugin_framework/rigger_plugin_framework-0.0.1.tar.gz/rigger_plugin_framework-0.0.1/plugins/first_plugin/__init__.py
@@ -0,0 +1,7 @@
+from plugins.first_plugin.first_plugin import FirstPlugin
+from rigger_plugin_framework.plugin_manager import PluginManager
+
+PluginManager.register(FirstPlugin)
+
+
+
---
+++ packages/rigger_plugin_framework/rigger_plugin_framework-0.0.1.tar.gz/rigger_plugin_framework-0.0.1/plugins/first_plugin/first_plugin.py
@@ -0,0 +1,4 @@
+from rigger_plugin_framework.plugin import Plugin
+
+class FirstPlugin(Plugin):
+    pass
