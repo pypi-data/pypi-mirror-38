f836e7e133fa0faa5ad05e51474c9c97c252f58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package twod/0.1.0/twod-0.1.0.tar.gz

---
+++ packages/twod/twod-0.1.0.tar.gz/twod-0.1.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['twod']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'twod',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'jnyjny',
+    'author_email': 'erik.oshaughnessy@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
