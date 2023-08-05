b9e106d2aa48e334f8a8b98430638c8ac279409c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:33 +0000

    Add package example-probability/1.3/example_probability-1.3.tar.gz

---
+++ packages/example-probability/example_probability-1.3.tar.gz/example_probability-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='example_probability',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['example_probability'],
+      author = 'Andrew Paster',
+      author_email = 'myemail@something.com',
+    zip_safe=False)
