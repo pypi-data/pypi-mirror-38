c75af631cb2bfeec9fa0321acb2e747054ab0255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package qindomClient/1.0.6/qindomClient-1.0.6-py2-none-any.whl

---
+++ packages/qindomClient/qindomClient-1.0.6-py2-none-any.whl/qit/__init__.py
@@ -0,0 +1,13 @@
+from qit.Qclient import Qclient
+
+if __name__ == "__main__":
+    client = Qclient('wangbo', 'C3783151-AE12-4F18-BA6D-FA1F024E3178')
+    # print(client.get_methods())
+    print(client.get_token())
+    #
+    # r = client.train_model_s3(
+    #     training_data="wangbo/census_train_X_y_df.csv", clf_type='QBOOST_IT')
+    #
+    # modelId = r['body']
+    # print(modelId)
+    print(client.predict_sync("wangbo/census_valid_X.csv", modelId=141965))
