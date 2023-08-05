5ddcc5c1050fdbbdbf88f0c416a1621f17cf5c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:49 +0000

    Add package guillotina-cms/1.0a13/guillotina_cms-1.0a13-py3-none-any.whl

---
+++ packages/guillotina-cms/guillotina_cms-1.0a13-py3-none-any.whl/guillotina_cms/content/id_generator.py
@@ -0,0 +1,34 @@
+from guillotina import configure
+from guillotina.interfaces import IIDGenerator
+from guillotina_cms.interfaces import ICMSLayer
+
+
+@configure.adapter(
+    for_=(ICMSLayer),
+    provides=IIDGenerator)
+class IDGenerator(object):
+    """Default IDGenerator adapter.
+
+    Requires request to adapt on different layers. Returns the urls path id.
+    """
+
+    def __init__(self, request):
+        self.request = request
+
+    def __call__(self, data):
+
+        if 'title' in data:
+            new_title = data['title'].lower().replace(' ', '-')
+            return new_title
+        if '@type' in data and data['@type'] == 'Image':
+            try:
+                return data['image']['filename']
+            except KeyError:
+                return None
+        elif '@type' in data and data['@type'] == 'File':
+            try:
+                return data['file']['filename']
+            except KeyError:
+                return None
+        else:
+            return None
