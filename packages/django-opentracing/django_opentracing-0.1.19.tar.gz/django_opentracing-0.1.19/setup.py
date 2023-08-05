29a9e4847dd61a91b063dd1586965d51447d69f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package django-opentracing/0.1.19/django_opentracing-0.1.19.tar.gz

---
+++ packages/django-opentracing/django_opentracing-0.1.19.tar.gz/django_opentracing-0.1.19/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import versioneer
+
+version=versioneer.get_version()
+setup(
+    name='django_opentracing',
+    cmdclass=versioneer.get_cmdclass(),
+    version=version,
+    url='https://github.com/opentracing-contrib/python-django/',
+    download_url='https://github.com/opentracing-contrib/python-django/tarball/'+version,
+    license='BSD',
+    author='Kathy Camenzind',
+    author_email='kcamenzind@lightstep.com',
+    description='OpenTracing support for Django applications',
+    long_description=open('README.rst').read(),
+    packages=['django_opentracing', 'tests'],
+    platforms='any',
+    install_requires=[
+        'django<2',
+        'opentracing>=1.1,<2',
+        'six',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
