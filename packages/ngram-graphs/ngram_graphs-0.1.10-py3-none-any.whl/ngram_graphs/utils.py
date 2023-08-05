b2344b31484330b3b603d6b5b3bf57ce19396921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:10 +0000

    Add package ngram-graphs/0.1.10/ngram_graphs-0.1.10-py3-none-any.whl

---
+++ packages/ngram-graphs/ngram_graphs-0.1.10-py3-none-any.whl/ngram_graphs/utils.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from ngram_graphs.TextGraph import IGraphTextGraph, NetworkxTextGraph
+
+
+def find_node(graph, node_idx):
+    return graph.vs[node_idx]
+
+
+def find_edge(graph, source, target):
+    if isinstance(graph, IGraphTextGraph.IGraphTextGraph):
+        for edge in graph.es:
+            if (source, target) == (IGraphTextGraph.find_node_name(graph, edge.source), IGraphTextGraph.find_node_name(graph, edge.target)):
+                return edge
+        return None
+    elif isinstance(graph, NetworkxTextGraph.NetworkxTextGraph):
+        if graph.has_edge(source, target):
+            return graph.edges[source, target]
+        return None
+
+
+def generate_model_graph(graphs: List, lr: float = 0.5):
+    model = graphs[0]
+    for graph in graphs[1:]:
+        model.update(graph, lr)
+    return model
