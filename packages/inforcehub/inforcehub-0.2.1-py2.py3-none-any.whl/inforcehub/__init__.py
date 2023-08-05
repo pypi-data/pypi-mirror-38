fa03c85348e0ee982d794ea942756de495bf8f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:14 +0000

    Add package inforcehub/0.2.1/inforcehub-0.2.1-py2.py3-none-any.whl

---
+++ packages/inforcehub/inforcehub-0.2.1-py2.py3-none-any.whl/inforcehub/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Inforcehub"
+__email__ = "matt.gosden@inforcehub.com"
+__version__ = "0.2.1"
+
+from inforcehub.colors import InforcehubColors
+from inforcehub.anonymize import Anonymize
+
+
+__all__ = ["InforcehubColors", "Anonymize"]
