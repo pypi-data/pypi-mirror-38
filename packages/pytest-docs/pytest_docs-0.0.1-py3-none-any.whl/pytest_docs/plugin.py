185f25932f0b9569f76b3b42d1c9fbd9c17a556f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:11 +0000

    Add package pytest-docs/0.0.1/pytest_docs-0.0.1-py3-none-any.whl

---
+++ packages/pytest-docs/pytest_docs-0.0.1-py3-none-any.whl/pytest_docs/plugin.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from .element import Element
+from .formatters.markdown import MarkdownFormatter
+from .formatters.restuctured import RSTFormatter
+
+FORMATTERS = {
+    MarkdownFormatter.name: MarkdownFormatter(),
+    RSTFormatter.name: RSTFormatter()
+}
+
+
+class DocPlugin:
+
+    def __init__(self, config):
+        self.config = config
+        self.path = config.getvalue('docs_path')
+        self.format_type = config.getvalue('docs_type')
+
+    def pytest_runtestloop(self, session):
+        if not self.path:
+            return
+
+        doc_tree = Element.create_doc_tree(session.items)
+
+        fmt = FORMATTERS[self.format_type]
+        out = fmt.create_document(doc_tree)
+
+        path = Path(self.path)
+        path.write_text(out)
+
+    def pytest_terminal_summary(self, terminalreporter, exitstatus):
+        if self.path:
+            terminalreporter.write_sep('-', 'generated doc file: {}'.format(self.path))
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('docs generator')
+    group.addoption(
+        '--docs',
+        dest='docs_path',
+        help='create documentation given path'
+    )
+    group.addoption(
+        '--doc-type',
+        dest='docs_type',
+        default='md',
+        help='Choose document type',
+        choices=list(FORMATTERS)
+    )
+
+
+def pytest_configure(config):
+    docs = DocPlugin(config)
+    config.pluginmanager.register(docs, 'pytest-docs')
