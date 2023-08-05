c1ff7019d7bf82eacca8588602722eabbc50feb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package aiorpcX/0.9.1/aiorpcX-0.9.1.tar.gz

---
+++ packages/aiorpcX/aiorpcX-0.9.1.tar.gz/aiorpcX-0.9.1/aiorpcx/__init__.py
@@ -0,0 +1,16 @@
+from .curio import *
+from .framing import *
+from .jsonrpc import *
+from .socks import *
+from .session import *
+from .util import *
+
+_version = (0, 9, 1)
+_version_str = '.'.join(str(part) for part in _version)
+
+__all__ = (curio.__all__ +
+           framing.__all__ +
+           jsonrpc.__all__ +
+           socks.__all__ +
+           session.__all__ +
+           util.__all__)
