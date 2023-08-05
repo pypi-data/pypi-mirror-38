07c9feec6f779fff8ed7e8d6b7c679c1bbe93dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package txkernel/0.1.0/txkernel-0.1.0-py2.py3-none-any.whl

---
+++ packages/txkernel/txkernel-0.1.0-py2.py3-none-any.whl/txkernel/__init__.py
@@ -0,0 +1,6 @@
+# txkernel
+# Copyright (C) 2018  guysv
+
+# This file is part of txkernel which is released under GPLv2.
+# See file LICENSE or go to https://www.gnu.org/licenses/gpl-2.0.txt
+# for full license details.
