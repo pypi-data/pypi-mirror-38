272b899c751fac8356cb759e9b25819ce557c5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:18 +0000

    Add package mashumaro/0.4/mashumaro-0.4.tar.gz

---
+++ packages/mashumaro/mashumaro-0.4.tar.gz/mashumaro-0.4/mashumaro/serializer/base/dict.py
@@ -0,0 +1,29 @@
+from typing import Mapping
+
+from mashumaro.serializer.base.metaprogramming import CodeBuilder
+
+
+class DataClassDictMixin:
+    def __init_subclass__(cls, **kwargs):
+        builder = CodeBuilder(cls)
+        builder.add_from_dict()
+        builder.add_to_dict()
+
+    def to_dict(
+            self,
+            use_bytes: bool = False,
+            use_enum: bool = False,
+            use_datetime: bool = False) -> dict:
+        pass
+
+    @classmethod
+    def from_dict(
+            cls,
+            d: Mapping,
+            use_bytes: bool = False,
+            use_enum: bool = False,
+            use_datetime: bool = False):
+        pass
+
+
+__all__ = [DataClassDictMixin]
