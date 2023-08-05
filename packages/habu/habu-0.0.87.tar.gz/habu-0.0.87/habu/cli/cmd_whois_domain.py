8d297d59b7169aa5ac229dd42fbed5542fd5759a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:55 +0000

    Add package habu/0.0.87/habu-0.0.87.tar.gz

---
+++ packages/habu/habu-0.0.87.tar.gz/habu-0.0.87/habu/cli/cmd_whois_domain.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import json
+import warnings
+
+import click
+import whois
+
+
+def remove_duplicates(data):
+    for key in data.keys():
+        if type(data[key]) == type([]):
+            if hasattr(data[key][0], 'lower'):
+                data[key] = list(set([ item.lower() for item in data[key] ]))
+            else:
+                data[key] = data[key][0]
+
+    if type(data['domain_name']) == type([]):
+        data['domain_name'] = data['domain_name'][0]
+
+    return data
+
+
+@click.command()
+@click.argument('domain')
+def cmd_whois_domain(domain):
+    """Simple whois client to check domain names.
+
+    Example:
+
+    \b
+    $ habu.whois.domain portantier.com
+    {
+        "domain_name": "portantier.com",
+        "registrar": "Amazon Registrar, Inc.",
+        "whois_server": "whois.registrar.amazon.com",
+        ...
+    """
+
+    warnings.filterwarnings("ignore")
+
+    data = whois.whois(domain)
+    data = remove_duplicates(data)
+
+    print(json.dumps(data, indent=4, default=str))
+
+if __name__ == '__main__':
+    cmd_whois_domain()
