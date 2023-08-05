ed5fd27ac9be03a017e3a4c0ca59564f09b86ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package transmart/0.2.4/transmart-0.2.4.tar.gz

---
+++ packages/transmart/transmart-0.2.4.tar.gz/transmart-0.2.4/transmart/api/auth.py
@@ -0,0 +1,144 @@
+import abc
+import requests
+from getpass import getpass
+import time
+
+
+class Authenticator(metaclass=abc.ABCMeta):
+
+    def __init__(self, url, username=None, password=None, realm=None, client_id=None):
+        self.url = url
+        self.user = username or input('Username: ')
+        self.password = password
+        self.realm = realm
+        self.client_id = client_id or self._default_client_id
+        self._access_token = None
+        self.get_token()
+
+    @property
+    @abc.abstractmethod
+    def _default_client_id(self) -> str:
+        pass
+
+    @property
+    @abc.abstractmethod
+    def access_token(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    def get_token(self):
+        pass
+
+    @abc.abstractmethod
+    def refresh(self):
+        pass
+
+
+class LegacyAuth(Authenticator):
+    _default_client_id = 'glowingbear-js'
+
+    @property
+    def access_token(self):
+        return self._access_token
+
+    def get_token(self):
+        r = requests.post(
+            "{}/oauth/token".format(self.url),
+            params=dict(
+                grant_type='password',
+                client_id=self.client_id,
+                username=self.user
+            ),
+            data=dict(
+                password=self.password or getpass("tranSMART password: ")
+            )
+        )
+
+        r.raise_for_status()
+
+        self._access_token = r.json().get('access_token')
+
+    def refresh(self):
+        self.get_token()
+
+
+class KeyCloakAuth(Authenticator):
+    _default_client_id = 'transmart-client'
+
+    def __init__(self, *args, **kwargs):
+        self.refresh_token = None
+        self.timeout = None
+        super().__init__(*args, **kwargs)
+
+    @property
+    def handle(self):
+        return '{}/auth/realms/{}/protocol/openid-connect/token'.format(self.url, self.realm)
+
+    @property
+    def access_token(self):
+        if self.timeout is not None and time.time() > self.timeout:
+            self.refresh()
+
+        return self._access_token
+
+    def get_token(self):
+        r = requests.post(
+            url=self.handle,
+            data=dict(
+                grant_type='password',
+                client_id=self.client_id,
+                username=self.user,
+                password=self.password or getpass("KeyCloak password: ")
+            )
+        )
+
+        r.raise_for_status()
+
+        if r.json().get('expires_in'):
+            self.timeout = time.time() + r.json().get('expires_in')
+
+        self.refresh_token = r.json().get('refresh_token')
+        self._access_token = r.json().get('access_token')
+
+    def refresh(self):
+        r = requests.post(
+            url=self.handle,
+            data=dict(
+                grant_type='refresh_token',
+                refresh_token=self.refresh_token,
+                client_id=self.client_id
+            )
+        )
+
+        if r.json().get('error') == 'invalid_grant':
+            self.get_token()
+            return
+
+        if r.json().get('expires_in'):
+            self.timeout = time.time() + r.json().get('expires_in')
+
+        self._access_token = r.json().get('access_token')
+
+
+def get_auth(host, user=None, password=None, kc_url=None, kc_realm=None, client_id=None) -> Authenticator:
+    """
+    Returns appropriate authenticator depending on the provided parameter.
+    If kc_url is provided returns the KeyCloakAuth, else LegacyAuth.
+
+    :param host: transmart api host.
+    :param user: username for authentication, will be asked if not provided.
+    :param password: password for authentication, will be asked if not provided.
+    :param kc_url: KeyCloak hostname (e.g. https://keycloak-test.thehyve.net)
+    :param kc_realm: Realm that is registered for the transmart api host to listen.
+    :param client_id: client id in keycloak.
+    :return: Authenticator
+    """
+
+    if kc_url:
+        return KeyCloakAuth(url=kc_url,
+                            realm=kc_realm,
+                            username=user,
+                            password=password,
+                            client_id=client_id)
+    else:
+        return LegacyAuth(host, user, password, client_id)
---
+++ packages/transmart/transmart-0.2.4.tar.gz/transmart-0.2.4/transmart/main.py
@@ -0,0 +1,35 @@
+from .api.v1.api import TransmartV1
+from .api.v2.api import TransmartV2
+
+
+def get_api(host, api_version=2, user=None, password=None, kc_url=None,
+            kc_realm=None, print_urls=False, interactive=True, client_id=None, **kwargs):
+    """
+    Create the python transmart client by providing user credentials.
+
+    :param host: a transmart URL (e.g. http://transmart-test.thehyve.net)
+    :param api_version: either 1 or 2. Default is 2.
+    :param user: if not given, it asks for it.
+    :param password: if not given, it asks for it.
+    :param kc_url: KeyCloak hostname (e.g. https://keycloak-test.thehyve.net)
+    :param kc_realm: Realm that is registered for the transmart api host to listen.
+    :param print_urls: print the url of handles being used.
+    :param interactive: automatically build caches for interactive use.
+    :param client_id: client id in keycloak.
+    """
+    api_versions = (1, 2)
+
+    if api_version not in api_versions:
+        raise ValueError("Not a valid TranSMART API version. Choose from: " + str(api_versions))
+
+    api = TransmartV1 if api_version == 1 else TransmartV2
+
+    return api(host=host,
+               user=user,
+               password=password,
+               kc_url=kc_url,
+               kc_realm=kc_realm,
+               print_urls=print_urls,
+               interactive=interactive,
+               client_id=client_id,
+               **kwargs)
