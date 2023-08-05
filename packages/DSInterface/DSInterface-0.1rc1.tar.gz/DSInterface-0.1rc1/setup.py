9ce0adc06f1329457861165c4dc0fbe17c721b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:03 +0000

    Add package DSInterface/0.1rc1/DSInterface-0.1rc1.tar.gz

---
+++ packages/DSInterface/DSInterface-0.1rc1.tar.gz/DSInterface-0.1rc1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='DSInterface',
+    version='0.1rc1',
+    description="A tiny python port of WPILib's driverstation class",
+    url='https://github.com/frc5024/DSInterface',
+    author='Evan Pratten',
+    author_email='ewpratten@gmail.com',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['pynetworktables'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ),
+)
