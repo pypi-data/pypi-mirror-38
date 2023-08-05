746907bfb2af15d58b0093470e835adfcc6e89ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package zai/0.0.1/zai-0.0.1.tar.gz

---
+++ packages/zai/zai-0.0.1.tar.gz/zai-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='zai',
+    version='0.0.1',
+    author='Team Ozero',
+    author_email='team-ozero@zalando.de',
+    description='zai placeholder package',
+    long_description='zai placeholder package',
+    url='https://github.com/zalando',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
