88a6155d662d68410a26b3ae56279ef5bdfd3271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:23 +0000

    Add package precession/1.0.3/precession-1.0.3.tar.gz

---
+++ packages/precession/precession-1.0.3.tar.gz/precession-1.0.3/precession/__init__.py
@@ -0,0 +1,8 @@
+from .precession import __author__
+from .precession import __email__
+from .precession import __copyright__
+from .precession import __license__
+from .precession import __version__
+from .precession import __doc__
+
+from .precession import *
