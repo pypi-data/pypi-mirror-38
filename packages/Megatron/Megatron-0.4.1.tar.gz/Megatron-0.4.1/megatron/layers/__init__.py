0399b564c3c59316238307d0bd5c9c4ec72fe200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:47 +0000

    Add package Megatron/0.4.1/Megatron-0.4.1.tar.gz

---
+++ packages/Megatron/Megatron-0.4.1.tar.gz/Megatron-0.4.1/megatron/layers/__init__.py
@@ -0,0 +1,13 @@
+from .core import Input, StatefulLayer, Lambda
+from . import image
+from . import missing
+from . import numeric
+from . import shaping
+from . import text
+from . import wrappers
+from .image import *
+from .missing import *
+from .numeric import *
+from .shaping import *
+from .text import *
+from .wrappers import *
