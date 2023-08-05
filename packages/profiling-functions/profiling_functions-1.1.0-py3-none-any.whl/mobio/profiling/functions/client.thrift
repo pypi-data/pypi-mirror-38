30ff20fef0a844fa90bcadac410e5807cad3877a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:14 +0000

    Add package profiling-functions/1.1.0/profiling_functions-1.1.0-py3-none-any.whl

---
+++ packages/profiling-functions/profiling_functions-1.1.0-py3-none-any.whl/mobio/profiling/functions/client.thrift
@@ -0,0 +1,6 @@
+
+service ProfilingService {
+
+    string build_query(1: string merchant_id, 2: string business_case_id, 3: string json_pf),
+    string add_user(1: string merchant_id, 2: i32 source, 3: string json_str_data, 4: string business_case_id)
+}
