3eaac891bcb813c27ab979ba782f557ee4ab0be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package ergal/0.1.3/ergal-0.1.3.tar.gz

---
+++ packages/ergal/ergal-0.1.3.tar.gz/ergal-0.1.3/src/ergal/cli.py
@@ -0,0 +1,15 @@
+""" ERGAL CLI """
+
+import os
+import argparse
+
+
+print("""
+  _  _         |
+ |/ |  /| //|  |
+        |   
+
+Welcome to ERGAL, the Elegant and Readable General API Library    
+""")
+
+
