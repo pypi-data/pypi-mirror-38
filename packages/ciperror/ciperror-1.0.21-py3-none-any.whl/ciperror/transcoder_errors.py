e484485ce59e7ccd76b407456a96da8ccb16fcc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package ciperror/1.0.21/ciperror-1.0.21-py3-none-any.whl

---
+++ packages/ciperror/ciperror-1.0.21-py3-none-any.whl/ciperror/transcoder_errors.py
@@ -0,0 +1,19 @@
+from ciperror import BaseCipError
+
+
+class TranscoderServiceRequestError(BaseCipError):
+    def __init__(self, message):
+        super().__init__(
+            code="TRE001",
+            message="Erro no request para o Transcoder Service: {}".format(message),
+            friendly_message="Erro no request para o Transcoder Service.",
+            http_status=500)
+
+
+class TranscoderJobNotFoundError(BaseCipError):
+    def __init__(self, message):
+        super().__init__(
+            code="TRE002",
+            message="Job de transcoder não encontrado: {}".format(message),
+            friendly_message="Job de transcoder não encontrado.",
+            http_status=404)
