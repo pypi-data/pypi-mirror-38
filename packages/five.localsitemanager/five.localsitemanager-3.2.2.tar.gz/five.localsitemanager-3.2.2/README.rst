8525d873c4a0ec0f3a9b6d160d6e5cd00499fbb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:11 +0000

    Add package five.localsitemanager/3.2.2/five.localsitemanager-3.2.2.tar.gz

---
+++ packages/five.localsitemanager/five.localsitemanager-3.2.2.tar.gz/five.localsitemanager-3.2.2/README.rst
@@ -0,0 +1,17 @@
+Introduction
+============
+
+Overview
+--------
+
+five.localsitemanager attempts to provide a local site manager implementation
+that is as close to the zope.interface / zope.component implementation as
+possible. Some reservations that do not conflict with the original API have
+been made to ease the path with CMF.
+
+Developer Resources
+-------------------
+
+- Source code:
+
+  https://github.com/zopefoundation/five.localsitemanager
---
+++ packages/five.localsitemanager/five.localsitemanager-3.2.2.tar.gz/five.localsitemanager-3.2.2/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+version = '3.2.2'
+
+setup(
+    name='five.localsitemanager',
+    version=version,
+    url='https://github.com/zopefoundation/five.localsitemanager',
+    license='ZPL 2.1',
+    description='Local site manager implementation for Zope.',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Zope :: 4',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+    keywords='zope five sitemanager',
+    packages=['five', 'five.localsitemanager'],
+    package_dir={'': 'src'},
+    namespace_packages=['five'],
+    include_package_data=True,
+    install_requires=[
+        'Acquisition',
+        'persistent',
+        'setuptools',
+        'six',
+        'zope.component',
+        'zope.event',
+        'zope.interface >= 3.8',
+        'zope.location',
+        'zope.lifecycleevent',
+        'zope.site',
+        'zope.testing',
+        'Zope >= 4.0b1',
+    ],
+    zip_safe=False,
+)
