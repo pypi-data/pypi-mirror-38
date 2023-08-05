d6a0f0d4ea75d12f3b2919fd3f5357a5f87c8bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package mechanizeretry/1.5/mechanizeretry-1.5.tar.gz

---
+++ packages/mechanizeretry/mechanizeretry-1.5.tar.gz/mechanizeretry-1.5/CHANGES.rst
@@ -0,0 +1,28 @@
+==========
+Change Log
+==========
+
+Version 1.4
+-----------
+
+* Add a Manifest.in file
+
+Version 1.3
+-----------
+
+* Update setup.py to include the license and long description.
+
+Version 1.2
+-----------
+
+* Update md to be rst.
+
+Version 1.1
+-----------
+
+* Fixed logging to support Python 2.6.
+
+Version 1.0
+-----------
+
+* First release.
---
+++ packages/mechanizeretry/mechanizeretry-1.5.tar.gz/mechanizeretry-1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mechanizeretry',
+    version='1.5',
+    install_requires=['mechanize'],
+    packages=['mechanizeretry'],
+    url='https://github.com/TetrationAnalytics/mechanizeretry',
+    license=open('LICENSE.txt').read(),
+    author='Mike Timm',
+    author_email='mtimm@tetrationanalytics.com',
+    description='Adds hang protection and retries to mechanize',
+    long_description=open('README.rst').read(),
+    keywords=['mechanize', 'retry'],
+    classifiers=['Development Status :: 5 - Production/Stable'],
+)
