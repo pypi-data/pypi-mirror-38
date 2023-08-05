b7397a53fea28ba8e52cff40a28da1abb21551b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:07 +0000

    Add package djangocms-googlemap/1.2.0/djangocms-googlemap-1.2.0.tar.gz

---
+++ packages/djangocms-googlemap/djangocms-googlemap-1.2.0.tar.gz/djangocms-googlemap-1.2.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+from djangocms_googlemap import __version__
+
+REQUIREMENTS = [
+    'django-cms>=3.4.5',
+    'django-filer'
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+
+setup(
+    name='djangocms-googlemap',
+    version=__version__,
+    author='Divio AG',
+    author_email='info@divio.com',
+    url='https://github.com/divio/djangocms-googlemap',
+    license='BSD',
+    description='Adds Google Maps plugins to django CMS.',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
