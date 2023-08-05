3fbef560fc0b3b654543835b3106ea90bdaad8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package qlib-tool/0.1/qlib_tool-0.1.tar.gz

---
+++ packages/qlib-tool/qlib_tool-0.1.tar.gz/qlib_tool-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qlib_tool',
+      version='0.1',
+      description='QLib For OiL n Gas',
+      url='https://github.com/Q-Engineering/qlib',
+      author='qengineering',
+      author_email='david@q.engineering',
+      license='MIT',
+      packages=['qlib'],
+      zip_safe=False)
