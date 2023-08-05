d9b09f1147a8b4ae43fbd953f7d89b2ea8873247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:49 +0000

    Add package aiscalator/0.1.0/aiscalator-0.1.0.tar.gz

---
+++ packages/aiscalator/aiscalator-0.1.0.tar.gz/aiscalator-0.1.0/aiscalator/airflow/airflow_command.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Apache Software License 2.0
+#
+# Copyright (c) 2018, Christophe Duong
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Implementations of commands for Airflow
+"""
+import logging
+import os
+from aiscalator.core.config import AiscalatorConfig
+from aiscalator.core.utils import subprocess_run
+
+
+def docker_compose(conf: AiscalatorConfig):
+    """
+    Prepare a docker-compose command
+
+    Parameters
+    ----------
+    conf : AiscalatorConfig
+       Configuration object for the application
+    Returns
+    -------
+    List
+        The array of commands to start a docker-compose
+        with the proper parameters
+    """
+    dockerfile = conf.find_user_config_file(
+        "config/docker-compose-CeleryExecutor.yml"
+    )
+    commands = ["docker-compose"]
+    for env in conf.user_env_file():
+        if os.path.isfile(env):
+            commands += ["--env-file", env]
+    commands += ["-f", dockerfile]
+    return commands
+
+
+def airflow_setup(conf: AiscalatorConfig):
+    """
+    Setup the airflow configuration files and environment
+
+    Parameters
+    ----------
+    conf : AiscalatorConfig
+        Configuration object for the application
+
+    """
+    # docker build --build-arg DOCKER_GID=`getent group docker |
+    # cut -d ':' -f 3` --rm -t aiscalator/airflow .
+    # TODO : to implement
+    logging.error("Not implemented yet")
+    pass
+
+
+def airflow_up(conf: AiscalatorConfig):
+    """
+    Starts an airflow environment
+
+    Parameters
+    ----------
+    conf : AiscalatorConfig
+        Configuration object for the application
+
+    """
+    commands = docker_compose(conf) + ["up", "-d"]
+    subprocess_run(commands, no_redirect=True)
+
+
+def airflow_down(conf: AiscalatorConfig):
+    """
+    Stop an airflow environment
+
+    Parameters
+    ----------
+    conf : AiscalatorConfig
+        Configuration object for the application
+
+    """
+    commands = docker_compose(conf) + ["down"]
+    subprocess_run(commands, no_redirect=True)
+
+
+def airflow_cmd(conf: AiscalatorConfig, service="webserver", cmd=None):
+    """
+    Execute an airflow subcommand
+
+    Parameters
+    ----------
+    conf : AiscalatorConfig
+        Configuration object for the application
+    service : string
+        service name of the container where to run the command
+    cmd : list
+        subcommands to run
+    """
+    commands = docker_compose(conf) + [
+        "run", "--rm", service,
+    ]
+    if cmd is not None:
+        commands += cmd
+    else:
+        commands += ["airflow"]
+    subprocess_run(commands, no_redirect=True)
---
+++ packages/aiscalator/aiscalator-0.1.0.tar.gz/aiscalator-0.1.0/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Apache Software License 2.0
+#
+# Copyright (c) 2018, Christophe Duong
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The setup script."""
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    'pytz>=2018.5',
+    'PyYAML>=3.13',
+]
+
+setup(
+    author="Christophe Duong",
+    author_email='chris@aiscalate.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    description="AIscalate your Jupyter Notebook Prototypes into Airflow Data Products",
+    entry_points={
+        'console_scripts': [
+            'aiscalator=aiscalator.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme + '\n\n' + history,
+    keywords='data science jupyter notebook prototype data engineering product airflow',
+    name='aiscalator',
+    packages=find_packages(),
+    package_data={'config': ['*']},
+    include_package_data=True,
+    setup_requires=['pytest-runner'],
+    test_suite='tests',
+    tests_require=['pytest'],
+    url='https://github.com/Aiscalate/aiscalator',
+    version='0.1.0',
+    zip_safe=False,
+)
