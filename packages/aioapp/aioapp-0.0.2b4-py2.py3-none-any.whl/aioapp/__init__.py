1890a728e69502d462d13e0cb229668806fff2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:31 +0000

    Add package aioapp/0.0.2b4/aioapp-0.0.2b4-py2.py3-none-any.whl

---
+++ packages/aioapp/aioapp-0.0.2b4-py2.py3-none-any.whl/aioapp/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for aioapp."""
+
+__author__ = """Konstantin Stepanov"""
+__version__ = '0.0.2b4'
+
+from . import app, error
+
+__all__ = ['app', 'error']
