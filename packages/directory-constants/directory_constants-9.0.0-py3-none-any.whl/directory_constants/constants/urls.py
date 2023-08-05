744d0b986c3e31f1ee1e5d446b5650e56eb88e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:17 +0000

    Add package directory-constants/9.0.0/directory_constants-9.0.0-py3-none-any.whl

---
+++ packages/directory-constants/directory_constants-9.0.0-py3-none-any.whl/directory_constants/constants/urls.py
@@ -0,0 +1,44 @@
+GREAT_HOME = 'https://great.gov.uk/'
+GREAT_EXPORT_HOME = 'https://great.gov.uk/'
+CUSTOM_PAGE = 'https://great.gov.uk/custom/'
+
+# EXPORTING PERSONAS
+EXPORTING_NEW = 'https://great.gov.uk/new/'
+EXPORTING_OCCASIONAL = 'https://great.gov.uk/occasional/'
+EXPORTING_REGULAR = 'https://great.gov.uk/regular/'
+
+# GUIDANCE/ARTICLE SECTIONS
+GUIDANCE_MARKET_RESEARCH = 'https://great.gov.uk/market-research/'
+GUIDANCE_CUSTOMER_INSIGHT = 'https://great.gov.uk/customer-insight/'
+GUIDANCE_FINANCE = 'https://great.gov.uk/finance/'
+GUIDANCE_BUSINESS_PLANNING = (
+    'https://great.gov.uk/business-planning/')
+GUIDANCE_GETTING_PAID = 'https://great.gov.uk/getting-paid/'
+GUIDANCE_OPERATIONS_AND_COMPLIANCE = (
+    'https://great.gov.uk/operations-and-compliance/')
+
+# SERVICES
+SERVICES_EVENTS = 'https://events.trade.gov.uk/'
+SERVICES_EXOPPS = 'https://great.gov.uk/export-opportunities/'
+SERVICES_FAB = 'https://find-a-buyer.export.great.gov.uk/'
+SERVICES_GET_FINANCE = (
+    'https://great.gov.uk/get-finance/')
+SERVICES_SOO = 'https://selling-online-overseas.export.great.gov.uk/'
+
+# INFO
+INFO_CONTACT_US_DIRECTORY = (
+    'https://contact-us.export.great.gov.uk/directory/FeedbackForm/')
+INFO_CONTACT_US_SSO = (
+    'https://contact-us.export.great.gov.uk/single_sign_on/FeedbackForm/')
+INFO_TERMS_AND_CONDITIONS = 'https://great.gov.uk/terms-and-conditions/'
+INFO_ABOUT = 'https://great.gov.uk/about/'
+INFO_PRIVACY_AND_COOKIES = 'https://great.gov.uk/privacy-and-cookies/'
+INFO_DIT = (
+    'https://www.gov.uk/government/organisations/'
+    'department-for-international-trade')
+
+# FEEDBACK
+FEEDBACK_FORM_DIRECTORY = (
+    'https://contact-us.export.great.gov.uk/directory/FeedbackForm')
+FEEDBACK_FORM_SSO = (
+    'https://contact-us.export.great.gov.uk/single_sign_on/FeedbackForm')
