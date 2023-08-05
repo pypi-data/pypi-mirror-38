66fd4f1cfcffa15fe6a8154a985c5b1ab3f232a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:26 +0000

    Add package wanghongyan/0.1/wanghongyan-0.1.tar.gz

---
+++ packages/wanghongyan/wanghongyan-0.1.tar.gz/wanghongyan-0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(name='wanghongyan',
+	version='0.1',
+	description='',
+	author='wanghongyan',
+	author_email='258156979@qq.com',
+	install_requires=[],
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	url='https://github.com/258156979/wanghongyan',
+	keywords=''
+)
