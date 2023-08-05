09cacace3e7c6b7cdcd9a24dd1364d1880c64852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:12 +0000

    Add package azurerm/0.9.10/azurerm-0.9.10-py3.6.egg

---
+++ packages/azurerm/azurerm-0.9.10-py3.6.egg/azurerm/__init__.py
@@ -0,0 +1,17 @@
+'''azurerm - library for easy Azure Resource Manager calls from Python'''
+
+from .acs import *
+from .adalfns import *
+from .amsrp import *
+from .container import *
+from .computerp import *
+from .cosmosdbrp import *
+from .deployments import *
+from .insightsrp import *
+from .networkrp import *
+from .resourcegroups import *
+from .storagerp import *
+from .subfns import *
+from .restfns import *
+from .templates import *
+from .vmimages import *
