d64368881778569946f1772b40a739a3ec64ae4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package turingarena-dev/0.0.2.dev732/turingarena_dev-0.0.2.dev732-py3-none-any.whl

---
+++ packages/turingarena-dev/turingarena_dev-0.0.2.dev732-py3-none-any.whl/turingarena_impl/evaluation/util.py
@@ -0,0 +1,18 @@
+from turingarena_common.evaluation_events import EvaluationEventType
+
+
+def evaluation_text(evaluation_events):
+    return "".join(
+        e.payload
+        for e in evaluation_events
+        if e.type is EvaluationEventType.TEXT
+    )
+
+
+def evaluation_goals(evaluation_events):
+    return {
+        e.payload["goal"]: e.payload["result"]
+        for e in evaluation_events
+        if e.type is EvaluationEventType.DATA
+        if e.payload["type"] == "goal_result"
+    }
