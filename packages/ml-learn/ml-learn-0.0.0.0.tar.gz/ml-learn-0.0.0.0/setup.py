990cc458271b0ef7d36b77ac80f4c52a4405ff68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:43 +0000

    Add package ml-learn/0.0.0.0/ml-learn-0.0.0.0.tar.gz

---
+++ packages/ml-learn/ml-learn-0.0.0.0.tar.gz/ml-learn-0.0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup
+
+setup(
+        name="ml-learn",
+        version="0.0.0.0",
+        author="Smirk",
+        author_email="smirk.cao@gmail.com",
+        url="http://www.smirk.info",
+        description="",
+        packages=["ml-learn"],
+        install_requires=[],
+        )
