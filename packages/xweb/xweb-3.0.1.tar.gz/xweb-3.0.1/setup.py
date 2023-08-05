9921968765f26d101464b59cf8f2bb06f9d70e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:34 +0000

    Add package xweb/3.0.1/xweb-3.0.1.tar.gz

---
+++ packages/xweb/xweb-3.0.1.tar.gz/xweb-3.0.1/setup.py
@@ -0,0 +1,40 @@
+import ast
+import re
+
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('xweb.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+            f.read().decode('utf-8')).group(1)))
+
+setup(name='xweb',
+      version=version,
+      description='High performance web framework built with uvloop and httptools.',
+      author='gaojiuli',
+      long_description=open('README.md', 'r', encoding='utf8').read(),
+      long_description_content_type="text/markdown",
+      author_email='gaojiuli@gmail.com',
+      url='https://github.com/gaojiuli/xweb',
+      py_modules=['xweb'],
+      install_requires=[
+          'asyncio',
+          'ujson',
+          'uvloop',
+          'httptools',
+          'gunicorn',
+          'jsonschema'
+      ],
+      license='MIT',
+      platforms='any',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+                   'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+                   'Topic :: Software Development :: Libraries :: Application Frameworks',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   ],
+      )
