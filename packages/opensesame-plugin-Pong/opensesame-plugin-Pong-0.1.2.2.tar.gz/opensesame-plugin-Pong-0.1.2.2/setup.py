2c1543e5b7f1564c4e72c1d262d5db1e4d03de64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:57 +0000

    Add package opensesame-plugin-Pong/0.1.2.2/opensesame-plugin-Pong-0.1.2.2.tar.gz

---
+++ packages/opensesame-plugin-Pong/opensesame-plugin-Pong-0.1.2.2.tar.gz/opensesame-plugin-Pong-0.1.2.2/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+"""
+No rights reserved. All files in this repository are released into the public
+domain.
+"""
+
+from setuptools import setup
+
+setup(
+	# Some general metadata. By convention, a plugin is named:
+	# opensesame-plugin-[plugin name]
+	name='opensesame-plugin-Pong',
+	version='0.1.2.2',
+	description='Create a Pong experiment',
+	author='Mark Span',
+	author_email='m.m.span@rug.nl',
+	url='https://github.com/markspan/Pong',
+	# Classifiers used by PyPi if you upload the plugin there
+	classifiers=[
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering',
+		'Environment :: Win32 (MS Windows)',
+		'Environment :: Win32 (MS Windows)',
+		'Environment :: MacOS X',
+		'Environment :: Other Environment',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 3',
+	],
+	#install_requires=['pylsl'],
+	# The important bit that specifies how the plugin files should be installed,
+	# so that they are found by OpenSesame. This is a bit different from normal
+	# Python modules, because an OpenSesame plugin is not a (normal) Python
+	# module.
+	data_files=[
+		# First the target folder.
+		('share/opensesame_plugins/Pong',
+		# Then a list of files that are copied into the target folder. Make sure
+		# that these files are also included by MANIFEST.in!
+		[
+			'Pong.md',
+			'Pong.png',
+			'Pong_large.png',
+			'Pong.py',
+			'info.yaml',
+			]
+		)]
+	)
