b04b91442dddd2a00e9f4e0561af6fbafa46d781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:13 +0000

    Add package django-script-codes/0.1.0/django_script_codes-0.1.0-py3.5.egg

---
+++ packages/django-script-codes/django_script_codes-0.1.0-py3.5.egg/script_codes/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.core.validators import MaxValueValidator
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class Script(TimeStampedModel):
+    code = models.CharField(max_length=4, unique=True)
+    number = models.PositiveSmallIntegerField(
+        unique=True, validators=[MaxValueValidator(999)])
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.code, self.number, self.name)
