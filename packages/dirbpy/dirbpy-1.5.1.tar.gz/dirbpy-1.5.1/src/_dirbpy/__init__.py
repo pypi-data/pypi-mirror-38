366dc344c0328e0355351a067a3246eb29579549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:10 +0000

    Add package dirbpy/1.5.1/dirbpy-1.5.1.tar.gz

---
+++ packages/dirbpy/dirbpy-1.5.1.tar.gz/dirbpy-1.5.1/src/_dirbpy/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "URLBruteforcer", 
+    "main",
+    "WordDictonary"
+]
+
+__version__ = '1.5.1'
+
+__author__ = "Marc-Olivier Bouchard"
+
+__license__ = "MIT"
+
+__email__ = "mo.bouchard1997@gmail.com"
