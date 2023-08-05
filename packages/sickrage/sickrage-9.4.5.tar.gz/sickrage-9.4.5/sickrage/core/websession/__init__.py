bc5ea923ef5935e81f377aa5836edda8aa4050f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:15 +0000

    Add package sickrage/9.4.5/sickrage-9.4.5.tar.gz

---
+++ packages/sickrage/sickrage-9.4.5.tar.gz/sickrage-9.4.5/sickrage/core/websession/__init__.py
@@ -0,0 +1,239 @@
+# Author: echel0n <echel0n@sickrage.ca>
+# URL: https://sickrage.ca
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import io
+import ssl
+import urllib2
+
+import certifi
+import cfscrape
+import requests
+from cachecontrol import CacheControlAdapter
+from fake_useragent import UserAgent
+from requests import Session
+from requests.utils import dict_from_cookiejar
+from urllib3 import disable_warnings
+
+import sickrage
+from sickrage.core.helpers import chmodAsParent, remove_file_failed
+from sickrage.core.helpers.encoding import to_unicode
+
+
+def _add_proxies():
+    if sickrage.app.config.proxy_setting:
+        sickrage.app.log.debug("Using global proxy: " + sickrage.app.config.proxy_setting)
+        scheme, address = urllib2.splittype(sickrage.app.config.proxy_setting)
+        address = ('http://{}'.format(sickrage.app.config.proxy_setting),
+                   sickrage.app.config.proxy_setting)[scheme]
+        return {"http": address, "https": address}
+
+
+class WebSession(Session):
+    def __init__(self, proxies=None, cache=True):
+        super(WebSession, self).__init__()
+
+        # setup caching adapter
+        if cache:
+            adapter = CacheControlAdapter()
+            self.mount('http://', adapter)
+            self.mount('https://', adapter)
+
+        # add proxies
+        self.proxies = proxies or _add_proxies()
+
+        # add hooks
+        self.hooks['response'] += [WebHooks.log_url, WebHooks.redirect_posts, WebHooks.cloudflare]
+
+    @staticmethod
+    def _get_ssl_cert(verify):
+        """
+        Configure the ssl verification.
+
+        We need to overwrite this in the request method. As it's not available in the session init.
+        :param verify: SSL verification on or off.
+        """
+        return certifi.where() if all([sickrage.app.config.ssl_verify, verify]) else False
+
+    def request(self, method, url, verify=False, random_ua=False, *args, **kwargs):
+        self.headers.update({'Accept-Encoding': 'gzip, deflate',
+                             'User-Agent': (sickrage.app.user_agent, UserAgent().random)[random_ua]})
+
+        if not verify: disable_warnings()
+
+        response = super(WebSession, self).request(method, url, verify=self._get_ssl_cert(verify), *args, **kwargs)
+
+        try:
+            # check web response for errors
+            response.raise_for_status()
+        except requests.exceptions.SSLError as e:
+            if ssl.OPENSSL_VERSION_INFO < (1, 0, 1, 5):
+                sickrage.app.log.info(
+                    "SSL Error requesting url: '{}' You have {}, try upgrading OpenSSL to 1.0.1e+".format(
+                        e.request.url, ssl.OPENSSL_VERSION))
+
+            if sickrage.app.config.ssl_verify:
+                sickrage.app.log.info(
+                    "SSL Error requesting url: '{}', try disabling cert verification in advanced settings".format(
+                        e.request.url))
+        except Exception:
+            pass
+
+        return response
+
+    def download(self, url, filename, **kwargs):
+        try:
+            r = self.get(url, timeout=10, stream=True, **kwargs)
+            if r.status_code >= 400:
+                return False
+
+            with io.open(filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+
+            chmodAsParent(filename)
+        except Exception as e:
+            sickrage.app.log.debug("Failed to download file from {} - ERROR: {}".format(url, e))
+            remove_file_failed(filename)
+            return False
+
+        return True
+
+    @staticmethod
+    def normalize_url(url, secure=False):
+        url = str(url)
+        segments = url.split('/')
+        correct_segments = []
+
+        for segment in segments:
+            if segment != '':
+                correct_segments.append(segment)
+
+        first_segment = str(correct_segments[0])
+        if first_segment.find(('http', 'https')[secure]) == -1:
+            correct_segments = [('http:', 'https:')[secure]] + correct_segments
+
+        correct_segments[0] += '/'
+
+        return '/'.join(correct_segments) + '/'
+
+
+class WebHooks(object):
+    @staticmethod
+    def log_url(response, **kwargs):
+        """Response hook to log request URL."""
+        request = response.request
+        sickrage.app.log.debug('{} URL: {} [Status: {}]'.format(request.method, request.url, response.status_code))
+        sickrage.app.log.debug('User-Agent: {}'.format(request.headers['User-Agent']))
+
+        if request.method.upper() == 'POST':
+            if isinstance(request.body, unicode):
+                sickrage.app.log.debug('With post data: {}'.format(request.body))
+            else:
+                sickrage.app.log.debug('With post data: {}'.format(to_unicode(request.body)))
+
+    @staticmethod
+    def cloudflare(resp, **kwargs):
+        """
+        Bypass CloudFlare's anti-bot protection.
+        """
+
+        def is_cloudflare_challenge(resp):
+            """Check if the response is a Cloudflare challange.
+            Source: goo.gl/v8FvnD
+            """
+            return (
+                    resp.status_code == 503
+                    and resp.headers.get('Server', '').startswith('cloudflare')
+                    and b'jschl_vc' in resp.content
+                    and b'jschl_answer' in resp.content
+            )
+
+        if is_cloudflare_challenge(resp):
+            sickrage.app.log.debug('CloudFlare protection detected, trying to bypass it')
+
+            # Get the session used or create a new one
+            session = getattr(resp, 'session', requests.Session())
+
+            # Get the original request
+            original_request = resp.request
+
+            # Get the CloudFlare tokens and original user-agent
+            tokens, user_agent = cfscrape.get_tokens(original_request.url)
+
+            # Add CloudFlare tokens to the session cookies
+            session.cookies.update(tokens)
+
+            # Add CloudFlare Tokens to the original request
+            original_cookies = dict_from_cookiejar(original_request._cookies)
+            original_cookies.update(tokens)
+            original_request.prepare_cookies(original_cookies)
+
+            # The same User-Agent must be used for the retry
+            # Update the session with the CloudFlare User-Agent
+            session.headers['User-Agent'] = user_agent
+
+            # Update the original request with the CloudFlare User-Agent
+            original_request.headers['User-Agent'] = user_agent
+
+            # Remove hooks from original request
+            original_hooks = original_request.hooks
+            original_request.hooks = session.hooks
+
+            # Resend the request
+            cf_resp = session.send(original_request, allow_redirects=True, **kwargs)
+
+            if cf_resp.ok:
+                sickrage.app.log.debug('CloudFlare successfully bypassed.')
+
+            # Add original hooks back to original request
+            cf_resp.hooks = original_hooks
+
+            return cf_resp
+        else:
+            return resp
+
+    @staticmethod
+    def redirect_posts(resp, **kwargs):
+        """Response hook to handle post method for URLs that are redirected"""
+
+        if resp.request.method == 'POST' and resp.headers.get('Location'):
+            # Get the session used or create a new one
+            session = getattr(resp, 'session', requests.Session())
+
+            # Get the original request
+            original_request = resp.request
+
+            # Set new URL
+            location = resp.headers['Location']
+            sickrage.app.log.debug('URL redirection detected for {} to {}'.format(original_request.url, location))
+            original_request.url = location
+
+            # Remove hooks from original request
+            original_hooks = original_request.hooks
+            original_request.hooks = session.hooks
+
+            # Resend the request
+            resp = session.send(original_request, **kwargs)
+
+            # Add original hooks back to original request
+            resp.hooks = original_hooks
+
+        return resp
---
+++ packages/sickrage/sickrage-9.4.5.tar.gz/sickrage-9.4.5/sickrage/providers/torrent/torrentleech.py
@@ -0,0 +1,167 @@
+# Author: Idan Gutman
+# URL: https://sickrage.ca
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from urlparse import urljoin
+
+from requests.utils import dict_from_cookiejar
+
+import sickrage
+from sickrage.core.caches.tv_cache import TVCache
+from sickrage.core.helpers import bs4_parser, convert_size, try_int
+from sickrage.providers import TorrentProvider
+
+
+class TorrentLeechProvider(TorrentProvider):
+    def __init__(self):
+        super(TorrentLeechProvider, self).__init__("TorrentLeech", 'https://www.torrentleech.org', True)
+
+        self.urls.update({
+            'login': '{base_url}/user/account/login'.format(**self.urls),
+            'search': '{base_url}/torrents/browse'.format(**self.urls),
+        })
+
+        self.username = None
+        self.password = None
+
+        # self.enable_cookies = True
+        # self.required_cookies = ('tluid', 'tlpass')
+
+        self.minseed = None
+        self.minleech = None
+
+        self.proper_strings = ['PROPER', 'REPACK', 'REAL', 'RERIP']
+
+        self.cache = TVCache(self, min_time=20)
+
+    # def login(self):
+    #     return self.cookie_login('log in')
+
+    def login(self):
+        cookies = dict_from_cookiejar(self.session.cookies)
+        if any(cookies.values()) and cookies.get('member_id'):
+            return True
+
+        login_params = {
+            'username': self.username,
+            'password': self.password,
+            'login': 'submit',
+            'remember_me': 'on',
+        }
+
+        try:
+            response = self.session.post(self.urls['login'], data=login_params, timeout=30).text
+        except Exception:
+            sickrage.app.log.warning("Unable to connect to provider")
+            return False
+
+        if '<title>Login :: TorrentLeech.org</title>' in response:
+            sickrage.app.log.warning("Invalid username or password. Check your settings")
+            return False
+
+        return True
+
+    def search(self, search_strings, age=0, ep_obj=None, **kwargs):
+        results = []
+
+        if not self.login():
+            return results
+
+        for mode in search_strings:
+            sickrage.app.log.debug("Search Mode: %s" % mode)
+            for search_string in search_strings[mode]:
+                if mode != 'RSS':
+                    sickrage.app.log.debug("Search string: %s" % search_string)
+
+                    categories = ["2", "7", "35"]
+                    categories += ["26", "32", "44"] if mode == "Episode" else ["27"]
+                    if ep_obj and ep_obj.show and ep_obj.show.is_anime:
+                        categories += ["34"]
+                else:
+                    categories = ["2", "26", "27", "32", "7", "34", "35", "44"]
+
+                search_params = {
+                    "categories": ",".join(categories),
+                    "query": search_string
+                }
+
+                try:
+                    data = self.session.get(self.urls["search"], params=search_params).text
+                    results += self.parse(data, mode)
+                except Exception:
+                    sickrage.app.log.debug("No data returned from provider")
+
+        return results
+
+    def parse(self, data, mode, **kwargs):
+        """
+        Parse search results from data
+        :param data: response data
+        :param mode: search mode
+        :return: search results
+        """
+
+        results = []
+
+        def process_column_header(td):
+            result = ''
+            if td.a:
+                result = td.a.get('title')
+            if not result:
+                result = td.get_text(strip=True)
+            return result
+
+        with bs4_parser(data) as html:
+            torrent_table = html.find('table', id='torrenttable')
+            torrent_rows = torrent_table('tr') if torrent_table else []
+
+            # Continue only if one Release is found
+            if len(torrent_rows) < 2:
+                sickrage.app.log.debug("Data returned from provider does not contain any torrents")
+                return results
+
+            labels = [process_column_header(label) for label in torrent_rows[0]('th')]
+
+            for row in torrent_rows[1:]:
+                cells = row('td')
+
+                try:
+                    name = cells[labels.index('Name')]
+                    title = name.find('a').get_text(strip=True)
+                    download_url = row.find('td', class_='quickdownload').find('a')
+                    if not all([title, download_url]):
+                        continue
+
+                    download_url = urljoin(self.urls['base_url'], download_url['href'])
+
+                    seeders = try_int(cells[labels.index('Seeders')].get_text(strip=True))
+                    leechers = try_int(cells[labels.index('Leechers')].get_text(strip=True))
+
+                    size = convert_size(cells[labels.index('Size')].get_text(), -1)
+
+                    results += [
+                        {'title': title, 'link': download_url, 'size': size, 'seeders': seeders, 'leechers': leechers}
+                    ]
+
+                    if mode != 'RSS':
+                        sickrage.app.log.debug("Found result: {}".format(title))
+                except Exception:
+                    sickrage.app.log.error("Failed parsing provider.")
+
+        return results
