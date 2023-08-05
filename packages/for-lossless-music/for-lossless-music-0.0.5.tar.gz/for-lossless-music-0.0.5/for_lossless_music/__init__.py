e7895e9562331651b37fcd243442a8f21130d79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package for-lossless-music/0.0.5/for-lossless-music-0.0.5.tar.gz

---
+++ packages/for-lossless-music/for-lossless-music-0.0.5.tar.gz/for-lossless-music-0.0.5/for_lossless_music/__init__.py
@@ -0,0 +1,14 @@
+
+from .moresound import Moresound, Source, str2source
+from .song import songs2table, FoundSong, foundsong2table
+from .download import download
+
+__version__ = '0.0.5'
+
+
+def find_best_quality(songs):
+    ranks = ['FLAC', 'APE', '320MP3', '192AAC', '192OGG', '128MP3', '96AAC', '48AAC', '24AAC']
+    for r in ranks:
+        if r in songs:
+            return r
+    return None
