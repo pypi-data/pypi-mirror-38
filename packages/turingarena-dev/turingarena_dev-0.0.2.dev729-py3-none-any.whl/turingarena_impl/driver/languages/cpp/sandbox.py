e55604ae3998c166d26fb8b811c86cc93b94b942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:49 +0000

    Add package turingarena-dev/0.0.2.dev729/turingarena_dev-0.0.2.dev729-py3-none-any.whl

---
+++ packages/turingarena-dev/turingarena_dev-0.0.2.dev729-py3-none-any.whl/turingarena_impl/driver/languages/cpp/sandbox.py
@@ -0,0 +1,36 @@
+import errno
+import os
+import sys
+from seccomplite import Filter, ERRNO, ALLOW, TRAP
+
+
+def init_sandbox():
+    filter = Filter(TRAP)
+    # no need to specify arguments of read/write (there should not be any other readable/writable fd)
+    for syscall in [
+        "read", "write", "readv", "writev",  # base I/O
+        "lseek", "ioctl", "fstat",
+        "exit", "exit_group", "rt_sigreturn",
+        "mmap", "munmap", "mremap", "brk",
+        "execve",
+        "arch_prctl", "uname", "set_tid_address",
+        "time",
+    ]:
+        filter.add_rule(ALLOW, syscall)
+    for syscall in [
+        "access", "madvise", "readlink",
+    ]:
+        filter.add_rule(ERRNO(errno.EACCES), syscall)
+    filter.load()
+
+
+def main():
+    init_sandbox()
+
+    # From here every system call that is not allowed will result in an Exception
+
+    os.execl(sys.argv[1], sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
