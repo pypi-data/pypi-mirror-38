616dc6634d59ee0400c309d71617aa1bf2e3ad43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:11 +0000

    Add package addicted3/3.0.0/addicted3-3.0.0.tar.gz

---
+++ packages/addicted3/addicted3-3.0.0.tar.gz/addicted3-3.0.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+import os
+import re
+
+def read(*names):
+    values = dict()
+    for name in names:
+        filename = name + '.rst'
+        if os.path.isfile(filename):
+            fd = open(filename)
+            value = fd.read()
+            fd.close()
+        else:
+            value = ''
+        values[name] = value
+    return values
+
+
+long_description = """
+%(README)s
+
+News
+====
+%(CHANGES)s
+""" % read('README', 'CHANGES')
+
+def get_version(pkg):
+    path = os.path.join(os.path.dirname(__file__),pkg,'__init__.py')
+    with open(path) as fh:
+        m = re.search(r'^__version__\s*=\s*[\'"]([^\'"]+)[\'"]',fh.read(),re.M)
+    if m:
+        return m.group(1)
+    raise RuntimeError("Unable to find __version__ string in %s." % path)
+
+setup(name='addicted3',
+      version=get_version('addicted'),
+      description='Enhanced dict for python 3',
+      long_description=long_description,
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3",
+      ],
+      keywords='attribute AttrDict addict addicted textops naghelp',
+      url='https://github.com/elapouya/addicted3',
+      author='Eric Lapouyade',
+      author_email='elapouya@gmail.com',
+      license='LGPL 2.1',
+      packages=['addicted'],
+      install_requires = ['noattr'],
+      eager_resources = [],
+      zip_safe=False)
