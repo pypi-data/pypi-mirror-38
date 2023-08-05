041447e9f71603e0538bb5169d4f7f60c38b91fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package zepusu/0.1.1/zepusu-0.1.1.tar.gz

---
+++ packages/zepusu/zepusu-0.1.1.tar.gz/zepusu-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name='zepusu',
+    author='Andrew Rabert',
+    author_email='ar@nullsum.net',
+    url='https://gitlab.com/nvllsvm/zepusu',
+    license='MIT',
+    packages=['zepusu'],
+    install_requires=['pyzmq'],
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    entry_points={
+        'console_scripts': ['zepusu=zepusu:main']
+    }
+)
