f3ff4837f2d787060f73516bdba5ba1f84e6c4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:19 +0000

    Add package sentiments/1.0.1/sentiments-1.0.1.tar.gz

---
+++ packages/sentiments/sentiments-1.0.1.tar.gz/sentiments-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+
+    name="sentiments",
+
+    version="1.0.1",
+
+    py_modules=['analyzer'],
+
+    author="Samuel",
+
+    author_email="sam.ansong@codethingy.com",
+
+    description="Performs simple sentiment analysis on user comments "
+)
+
+
