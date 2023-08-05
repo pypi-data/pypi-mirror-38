809589d5905e4e4f71141ccf5c5d91860a74bbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package BiblioPixelAnimations/3.20181103.185007/BiblioPixelAnimations-3.20181103.185007.tar.gz

---
+++ packages/BiblioPixelAnimations/BiblioPixelAnimations-3.20181103.185007.tar.gz/BiblioPixelAnimations-3.20181103.185007/BiblioPixelAnimations/matrix/ImageShow.py
@@ -0,0 +1,20 @@
+from bibliopixel.animation import BaseMatrixAnim
+from bibliopixel import image
+import os
+
+
+class ImageShow(BaseMatrixAnim):
+
+    def __init__(self, layout, imagePath=None, offset=(0, 0), **kwds):
+        super().__init__(layout, **kwds)
+        if imagePath is None:
+            cur_dir = os.path.dirname(os.path.realpath(__file__))
+            imagePath = os.path.abspath(os.path.join(cur_dir, '../../Graphics/ml_logo.bmp'))
+        self.img = imagePath
+        self.offset = offset
+
+    def pre_run(self):
+        self.layout.setTexture(image.loadImage(self.layout, imagePath=self.img, offset=self.offset))
+
+    def step(self, amt):
+        self.layout.fillScreen()
---
+++ packages/BiblioPixelAnimations/BiblioPixelAnimations-3.20181103.185007.tar.gz/BiblioPixelAnimations-3.20181103.185007/BiblioPixelAnimations/strip/PartyMode.py
@@ -0,0 +1,21 @@
+from bibliopixel.animation import BaseStripAnim
+from bibliopixel import colors
+
+
+class PartyMode(BaseStripAnim):
+    """Stobe Light Effect."""
+    COLOR_DEFAULTS = ('colors', [colors.Red, colors.Green, colors.Blue]),
+
+    def pre_run(self):
+        self._step = 0
+
+    def step(self, amt=1):
+        amt = 1  # anything other than 1 would be just plain silly
+
+        if self._step % 2 == 0:
+            color = self.palette(self._step / 2)
+            self.layout.fill(color, self._start, self._end)
+        else:
+            self.layout.all_off()
+
+        self._step += amt
