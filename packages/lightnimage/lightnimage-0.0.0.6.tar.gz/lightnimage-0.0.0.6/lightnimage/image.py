a16d59b30be9ecbb3631336266e0e20d262543e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:02 +0000

    Add package lightnimage/0.0.0.6/lightnimage-0.0.0.6.tar.gz

---
+++ packages/lightnimage/lightnimage-0.0.0.6.tar.gz/lightnimage-0.0.0.6/lightnimage/image.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+
+class Lightnimage:
+    """
+    A numpy array wrapper object for manipulating grayscale images
+
+    CHANGELOG
+
+    Added 04.11.2018
+
+    @author Jonas Teufel
+    """
+
+    def __init__(self, img):
+        """
+
+        CHANGELOG
+
+        Added 04.11.2018
+
+        @param np.ndarray img: The array should be two dimensions, which means only grayscale images
+        """
+        self.original = img
+        self.array = img
+
+    def darken(self, threshold, replace=0):
+        """
+
+        CHANGELOG
+
+        Added 04.11.2018
+
+        @param int threshold:
+        @param int replace:
+        @return:
+        """
+        it = np.nditer(self.array, flags=['multi_index'], op_flags=['writeonly'])
+        while not it.finished:
+            if it[0] <= threshold:
+                it[0] = replace
+            it.iternext()
+
+    def lighten(self, threshold, replace=255):
+        """
+
+        CHANGELOG
+
+        Added 04.11.2018
+
+        @param int threshold:
+        @param int replace:
+        @return:
+        """
+        it = np.nditer(self.array, flags=['multi_index'], op_flags=['writeonly'])
+        while not it.finished:
+            if it[0] >= threshold:
+                it[0] = replace
+            it.iternext()
+
+    def invert(self):
+        """
+
+        CHANGELOG
+
+        Added 04.11.2018
+
+        @return:
+        """
+        it = np.nditer(self.array, flags=['multi_index'], op_flags=['writeonly'])
+        while not it.finished:
+            it[0] = 255 - it[0]
+            it.iternext()
+
+    def subtract(self, other, threshold=10, replace=255, invert=False,):
+        """
+
+        CHANGELOG
+
+        Added 04.11.2018
+
+        @param Lightnimage other:   The other image, which is supposed to be subtracted from this one
+        @param int threshold:
+        @param int replace:
+        @param bool invert:
+        @return:
+        """
+        new = np.zeros(self.array.shape)
+        it = np.nditer(new, flags=['multi_index'], op_flags=['writeonly'])
+        while not it.finished:
+            value_self = self.array[it.multi_index]
+            value_other = other.array[it.multi_index]
+            diff = abs(int(value_self) - int(value_other))
+
+            if diff <= threshold:
+                it[0] = replace
+            else:
+                it[0] = abs(int(value_self) - int(value_other))
+                if invert:
+                    it[0] = 255 - it[0]
+
+            it.iternext()
+
+        return Lightnimage(new)
+
