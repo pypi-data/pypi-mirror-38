324df97bf2e2b71b7ce7b895c467023d37f8a6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:43 +0000

    Add package sl-learn/0.0.0.0/sl-learn-0.0.0.0.tar.gz

---
+++ packages/sl-learn/sl-learn-0.0.0.0.tar.gz/sl-learn-0.0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup
+
+setup(
+        name="sl-learn",
+        version="0.0.0.0",
+        author="Smirk",
+        author_email="smirk.cao@gmail.com",
+        url="http://www.smirk.info",
+        description="",
+        packages=["sl-learn"],
+        install_requires=[],
+        )
---
+++ packages/sl-learn/sl-learn-0.0.0.0.tar.gz/sl-learn-0.0.0.0/sl-learn/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+def hello():
+    print("Statistics Learning Method")
