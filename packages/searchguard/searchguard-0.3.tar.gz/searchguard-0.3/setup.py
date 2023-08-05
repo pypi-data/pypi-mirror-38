73d062221d5cfa109eeaa5f5758163f593f46ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package searchguard/0.3/searchguard-0.3.tar.gz

---
+++ packages/searchguard/searchguard-0.3.tar.gz/searchguard-0.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+from contextlib import suppress
+from setuptools import setup, find_packages
+
+project_root = os.path.dirname(__file__)
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+long_description = ''
+
+with suppress(OSError):
+    with open(os.path.join(project_root, 'README.md'), 'rt') as readme_fh:
+        long_description = readme_fh.read()
+
+setup(
+    name='searchguard',
+    version='0.3',
+    packages=find_packages(exclude=['tests']),
+    license='MIT',
+    url='https://github.com/ByteInternet/searchguard-python',
+    author='Jeroen van Heugten',
+    author_email='jeroen@byte.nl',
+    classifiers=classifiers,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'requests==2.20'
+    ],
+)
+
