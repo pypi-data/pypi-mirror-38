8d393ce5c8cb7af262cfe60d43efe460cdf6b1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:20 +0000

    Add package udata-ldap/0.3.3.dev46/udata_ldap-0.3.3.dev46-py2.py3-none-any.whl

---
+++ packages/udata-ldap/udata_ldap-0.3.3.dev46-py2.py3-none-any.whl/udata_ldap/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+udata LDAP
+
+LDAP authentification for udata with optionnal Kerberos suppport.
+'''
+from __future__ import unicode_literals
+
+__version__ = '0.3.3.dev'
+__description__ = 'LDAP authentification for udata with optional Kerberos suppport.'
+
+
+def init_app(app):
+    from .ldap import manager
+    from .views import bp, redirect_to_login
+
+    app.register_blueprint(bp)
+    app.view_functions['security.login'] = redirect_to_login
+
+    manager.init_app(app)
