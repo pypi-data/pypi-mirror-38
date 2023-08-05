ea3229b13a77fc11be2de7dbb3a05ade195de086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:23 +0000

    Add package numpythia/1.0.1/numpythia-1.0.1.tar.gz

---
+++ packages/numpythia/numpythia-1.0.1.tar.gz/numpythia-1.0.1/README.rst
@@ -0,0 +1,103 @@
+.. -*- mode: rst -*-
+
+numpythia: The interface between PYTHIA and NumPy
+=================================================
+
+.. image:: https://img.shields.io/pypi/v/numpythia.svg
+   :target: https://pypi.python.org/pypi/numpythia
+
+.. image:: https://zenodo.org/badge/DOI/10.5281/zenodo.1471492.svg
+  :target: https://doi.org/10.5281/zenodo.1471492
+
+.. image:: https://travis-ci.org/scikit-hep/numpythia.svg?branch=master
+   :target: https://travis-ci.org/scikit-hep/numpythia
+
+.. image:: https://coveralls.io/repos/github/scikit-hep/numpythia/badge.svg?branch=master
+   :target: https://coveralls.io/github/scikit-hep/numpythia?branch=master
+
+numpythia provides an interface between `PYTHIA
+<http://home.thep.lu.se/Pythia/>`_ and `NumPy
+<http://www.numpy.org/>`_ allowing you to generate events as NumPy arrays of
+particle four-momenta. By default numpythia only depends on NumPy and builds
+internal copies of the PYTHIA and `HepMC <http://hepmc.web.cern.ch/hepmc/>`_
+source code.
+
+Standalone Installation
+-----------------------
+
+To simply use the built-in PYTHIA and HepMC::
+
+   pip install -v numpythia
+
+And you're good to go!
+
+Support for building against an external PYTHIA is on the wishlist.
+
+Strict dependencies
+-------------------
+
+- `Python <http://docs.python-guide.org/en/latest/starting/installation/>`__ (2.7+, 3.4+)
+- `Numpy <https://scipy.org/install.html>`__
+- `six <https://six.readthedocs.io/>`__
+
+Getting started
+---------------
+
+.. code-block:: python
+
+   from numpythia import Pythia, hepmc_write, hepmc_read
+   from numpythia import STATUS, HAS_END_VERTEX, ABS_PDG_ID
+   from numpythia.testcmnd import get_cmnd
+   from numpy.testing import assert_array_equal
+
+   pythia = Pythia(get_cmnd('w'), random_state=1)
+
+   selection = ((STATUS == 1) & ~HAS_END_VERTEX &
+               (ABS_PDG_ID != 12) & (ABS_PDG_ID != 14) & (ABS_PDG_ID != 16))
+
+   # generate events while writing to ascii hepmc
+   for event in hepmc_write('events.hepmc', pythia(events=1)):
+      array1 = event.all(selection)
+
+   # read the same event back from ascii hepmc
+   for event in hepmc_read('events.hepmc'):
+      array2 = event.all(selection)
+
+   assert_array_equal(array1, array2)
+
+The dtype of any array of particle information is:
+
+.. code-block:: python
+
+   np.dtype([('E', 'f8'), ('px', 'f8'), ('py', 'f8'), ('pz', 'f8'), ('mass', 'f8'),
+             ('prodx', 'f8'), ('prody', 'f8'), ('prodz', 'f8'), ('prodt', 'f8'),
+             ('pdgid', 'i4'), ('status', 'i4')])
+
+Also see `pyjet <https://github.com/scikit-hep/pyjet>`_ for jet clustering.
+
+Tutorial
+--------
+
+PYTHIA settings can be passed in one of three ways: through the `**kwargs` arguments of the constructor `Pythia(..., **kwargs)`:
+
+.. code-block:: python
+
+   pythia = Pythia(..., Beams_eCM=13000.)
+
+Or as a dictionary:
+
+.. code-block:: python
+
+   pythia = Pythia(..., params={'Beams:eCM':  13000.})
+
+Or via a Python command file:
+
+.. code-block:: python
+
+   pythia = Pythia(config='path/to/config.cmd')
+
+The full list of settings can be found on the  `PYTHIA homepage <http://home.thep.lu.se/Pythia/>`_.
+
+Note that the ":" in settings names is replaced by a "_"  if using `kwargs`.
+`kwargs` take precedence over `params` and they both take precedence over `config`.
+Example config files can be found under the `numpythia.testcmnd` directory.
---
+++ packages/numpythia/numpythia-1.0.1.tar.gz/numpythia-1.0.1/setup.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+
+import sys
+
+# Check Python version
+if sys.version_info < (2, 7):
+    sys.exit("numpythia only supports python 2.7 and above")
+
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
+
+try:
+    # Try to use setuptools if installed
+    from setuptools import setup, Extension
+    from pkg_resources import parse_version, get_distribution
+
+    if get_distribution('setuptools').parsed_version < parse_version('0.7'):
+        # setuptools is too old (before merge with distribute)
+        raise ImportError
+
+    from setuptools.command.build_ext import build_ext as _build_ext
+    from setuptools.command.install import install as _install
+    use_setuptools = True
+
+except ImportError:
+    # Use distutils instead
+    from distutils.core import setup, Extension
+    from distutils.command.build_ext import build_ext as _build_ext
+    from distutils.command.install import install as _install
+    use_setuptools = False
+
+import os
+import fnmatch
+import platform
+import subprocess
+from distutils.sysconfig import customize_compiler
+
+# monkey-patch distutils for parallel compilation
+def parallelCCompile(self, sources, output_dir=None, macros=None,
+                     include_dirs=None, debug=0, extra_preargs=None,
+                     extra_postargs=None, depends=None):
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(
+        output_dir, macros, include_dirs, sources, depends, extra_postargs)
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+    import multiprocessing
+    import multiprocessing.pool
+    N = multiprocessing.cpu_count()
+    def _single_compile(obj):
+        try:
+            src, ext = build[obj]
+        except KeyError:
+            return
+        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+    # convert to list, imap is evaluated on-demand
+    pool = multiprocessing.pool.ThreadPool(N)
+    try:
+        list(pool.imap(_single_compile, objects))
+    except:
+        pool.close()
+        pool.join()
+        raise
+    return objects
+
+import distutils.ccompiler
+distutils.ccompiler.CCompiler.compile = parallelCCompile
+
+# Prevent setup from trying to create hard links
+# which are not allowed on AFS between directories.
+# This is a hack to force copying.
+try:
+    del os.link
+except AttributeError:
+    pass
+
+local_path = os.path.dirname(os.path.abspath(__file__))
+# setup.py can be called from outside the source directory
+os.chdir(local_path)
+sys.path.insert(0, local_path)
+
+def recursive_glob(path, pattern):
+    matches = []
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, pattern):
+            matches.append(os.path.join(root, filename))
+    return matches
+
+libnumpythia = Extension(
+    'numpythia._libnumpythia',
+    sources=['numpythia/src/_libnumpythia.cpp'] +
+        recursive_glob('numpythia/src/extern/hepmc3.0.0/src', '*.cc') +
+        recursive_glob('numpythia/src/extern/pythia8226/src', '*.cc'),
+    depends=[],
+    language='c++',
+    include_dirs=[
+        'numpythia/src',
+        'numpythia/src/extern/hepmc3.0.0/include',
+        'numpythia/src/extern/pythia8226/include',
+    ],
+    extra_compile_args=[
+        '-std=c++11',  # for HepMC
+        '-Wno-unused-function',
+        '-Wno-write-strings',
+    ],
+    define_macros=[
+        ('XMLDIR', '""'),
+    ],
+)
+
+
+class build_ext(_build_ext):
+    user_options = _build_ext.user_options + [
+        ('external-fastjet', None, None),
+    ]
+
+    def initialize_options(self):
+        _build_ext.initialize_options(self)
+        self.external_fastjet = False
+
+    def finalize_options(self):
+        global libnumpythia
+        #global external_fastjet
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process
+        try:
+            del builtins.__NUMPY_SETUP__
+        except AttributeError:
+            pass
+        import numpy
+        libnumpythia.include_dirs.append(numpy.get_include())
+
+    def build_extensions(self):
+        # Remove the "-Wstrict-prototypes" compiler option, which isn't valid
+        # for C++.
+        customize_compiler(self.compiler)
+        try:
+            self.compiler.compiler_so.remove('-Wstrict-prototypes')
+        except (AttributeError, ValueError):
+            pass
+        _build_ext.build_extensions(self)
+
+
+class install(_install):
+    user_options = _install.user_options + [
+        ('external-fastjet', None, None),
+    ]
+
+    def initialize_options(self):
+        _install.initialize_options(self)
+        self.external_fastjet = False
+
+    def finalize_options(self):
+        global external_fastjet
+        if self.external_fastjet:
+            external_fastjet = True
+        _install.finalize_options(self)
+
+
+# Only add numpy to *_requires lists if not already installed to prevent
+# pip from trying to upgrade an existing numpy and failing.
+try:
+    import numpy
+except ImportError:
+    build_requires = ['numpy']
+else:
+    build_requires = []
+# Take Python 2-3 compatibility package 'six' from outside (https://github.com/benjaminp/six)
+build_requires.append('six')
+
+if use_setuptools:
+    setuptools_options = dict(
+        setup_requires=build_requires,
+        install_requires=build_requires,
+        extras_require={
+            'with-numpy': ('numpy',),
+        },
+        zip_safe=False,
+    )
+else:
+    setuptools_options = dict()
+
+setup(
+    name='numpythia',
+    version='1.0.1',
+    description='The interface between PYTHIA and NumPy',
+    long_description=''.join(open('README.rst').readlines()),
+    maintainer='the Scikit-HEP admins',
+    maintainer_email='scikit-hep-admins@googlegroups.com',
+    license='GPLv3',
+    url='http://github.com/scikit-hep/numpythia',
+    packages=[
+        'numpythia',
+        'numpythia.testcmnd',
+    ],
+    package_data={
+        'numpythia': [
+            'testcmnd/*.cmnd',
+            'src/extern/pythia8226/share/*',
+        ],
+    },
+    ext_modules=[libnumpythia],
+    cmdclass={
+        'build_ext': build_ext,
+        'install': install,
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: C++',
+        'Programming Language :: Cython',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    **setuptools_options
+)
