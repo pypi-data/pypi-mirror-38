1272e37c7865b0b03e0b009dcab87b73e94eff4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:39 +0000

    Add package statuscheck/0.5.0/statuscheck-0.5.0.tar.gz

---
+++ packages/statuscheck/statuscheck-0.5.0.tar.gz/statuscheck-0.5.0/statuscheck/services/__init__.py
@@ -0,0 +1,22 @@
+SERVICES = [
+    'atlassian',
+    'closeio',
+    'cloudamqp',
+    'discord',
+    'docker',
+    'github',
+    'gitlab',
+    'heroku',
+    'npm',
+    'pagerduty',
+    'pypi',
+    'redislabs',
+    'salesforce',
+    'sentry',
+    'servicemax',
+    'slack',
+    'travis',
+    'twilio',
+]
+
+__all__ = SERVICES
