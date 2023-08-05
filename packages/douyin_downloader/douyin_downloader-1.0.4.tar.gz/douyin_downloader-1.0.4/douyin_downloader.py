7e3be96299d2dca21632c27f0241b69c76144b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package douyin_downloader/1.0.4/douyin_downloader-1.0.4.tar.gz

---
+++ packages/douyin_downloader/douyin_downloader-1.0.4.tar.gz/douyin_downloader-1.0.4/douyin_downloader.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-10-18 23:38:24
+# @Author  : He Liang (heianghit@foxmail.com)
+# @Link    : https://github.com/HeLiangHIT
+
+'''
+抖音下载器: 异步下载抖音视频。 follow 参数用于指定是否是下载关注的用户视频。
+
+Usage:
+  douyin_downloader.py [--dir=dir] [--concurrency=concurrency] [--follow=follow] <user> <action> follow
+  douyin_downloader.py [--dir=dir] [--concurrency=concurrency] [--follow=follow] <user> <action>
+  douyin_downloader.py --version
+
+Options:
+  --dir=dir                    select file save dir. * default: '$HOME/Movies/douyin/'
+  --concurrency=concurrency    select the concurrency number of downloader. [default: 20]
+'''
+
+from douyin_tool import *
+from docopt import docopt
+import sys
+
+# 默认配置参数
+_SAVE_DIR = "%s/Movies/douyin/" % os.path.expanduser('~') # os.environ['HOME']
+
+
+# 生产-消费 流程 的消费者 _receiver
+async def download_videos(_receiver, downloader):
+    while True:
+        async for video in _receiver:
+            # video = await _receiver.receive() # may get repeated tail
+            if video is None:
+                return # 结束下载
+            url = video['video_url']
+            if downloader.is_file_downloaded(video['name']) or url is None:
+                logging.info(f"{video['name']} is already downloaded or expired!")
+                continue
+            
+            logging.debug(f"downloading {url} ... ")
+            content = await downloader.download_file(url)
+            if content is not None:
+                await downloader.save_file(video['name'], content)
+                logging.info(f"download {video['name']} from {url} succ")
+            else:
+                logging.error(f"download {video['name']} from {url} FAIL!")
+
+
+# 生产-消费 流程 的生产者 _sender
+async def generate_videos(_sender, func_dict, user, action, repeat_func):
+    '''根据用户选择调用指定函数'''
+    async for video in func_dict[action](user, repeat_func=repeat_func):
+        await _sender.send(video)
+    await _sender.send(None)
+
+
+# 爬取单个用户的视频
+async def crawler_user_video(user, func_dict, action, save_dir, concurrency):
+    '''下载指定用户指定的'''
+    logging.info(f"start download {user}'s favorite video to {save_dir} with {concurrency} concurrency ...")
+    downloader = AsyncDownloader(os.path.join(save_dir, user, action))
+
+    async with trio.open_nursery() as nursery:
+        _sender, _receiver = trio.open_memory_channel(concurrency) # 并行数量
+        nursery.start_soon(generate_videos, _sender, func_dict, user, action, downloader.is_file_downloaded)
+        nursery.start_soon(download_videos, _receiver, downloader)
+
+    logging.info(f"video for user with user_id={user} downloads finished!")
+
+
+# 主函数
+async def main(user, action, follow, save_dir, concurrency):
+    dy = DouyinTool()
+    func_dict = {
+        "favorite" : dy.get_favorite_list,
+        "post" : dy.get_post_list,
+    }
+
+    if action not in func_dict.keys():
+        logging.critical(f"action={action} is not supported!")
+        exit(-1)
+    if not user.isdigit():
+        logging.critical(f"user={user} is illegal!")
+        exit(-1)
+
+    if follow:
+        user_ids = set()
+        logging.info(f"Crawler {user}'s follow list...")
+        async for people in dy.get_follow_list(user):
+            user_ids.add(people['user_id'])
+        logging.info(f"there are {len(user_ids)} followed users need for crawler, let's begin!")
+        for _user in user_ids:
+            await crawler_user_video(_user, func_dict, action, save_dir, concurrency)
+    else:
+        await crawler_user_video(user, func_dict, action, save_dir, concurrency)
+
+    logging.info("all videos are downloaded! congratulations!!!")
+
+
+def cmd_run():
+    '''命令行输入参数'''
+    user = user_input("Input user_id:")
+    _action = user_input("Choose action[1.favorite, 2.post] *default 1:", ['1', '2'], '1')
+    action = ('favorite', 'post')[int(_action) - 1]
+    _follow = user_input("Do you want to follow user? [yes/no] *default no:", ['yes', 'no'], 'no')
+    follow = _follow == 'yes'
+    save_dir = user_input(f"Input video save dir *default {_SAVE_DIR}:", [], _SAVE_DIR)
+    concurrency = 20
+    print(f"begin to download {'following of ' if follow else ''}{user}'s {action} video to {save_dir} ... ")
+
+    trio.run(main, user, action, follow, save_dir, concurrency)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        cmd_run()
+    else:
+        arguments = docopt(__doc__, version="douyin_downloader 0.0.1")
+        save_dir = arguments["--dir"] if arguments["--dir"] else _SAVE_DIR # 不会取doc里`*default`的值
+        concurrency = int(arguments["--concurrency"])
+        user = arguments['<user>']
+        action = arguments['<action>']
+        follow = arguments['follow'] # True if set, else False
+        trio.run(main, user, action, follow, save_dir, concurrency)
+
+
+
+
+
---
+++ packages/douyin_downloader/douyin_downloader-1.0.4.tar.gz/douyin_downloader-1.0.4/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-10-21 10:29:28
+# @Author  : He Liang (heianghit@foxmail.com)
+# @Link    : https://github.com/HeLiangHIT
+
+
+from setuptools import setup
+import codecs, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='douyin_downloader', #pypi中的名称，pip或者easy_install安装时使用的名称
+    version='1.0.4',
+    py_modules=['util', 'douyin_tool', 'douyin_downloader'], # 需要打包的 Python 单文件列表
+    url='https://github.com/HeLiangHIT/douyin_downloader',
+    license='GPLv3',
+    author='He Liang',
+    author_email='heianghit@foxmail.com',
+    keywords="douyin trio",
+    install_requires=[ # 需要安装的依赖
+        'arrow>=0.12.1',
+        'asks>=2.2.0',
+        'trio>=0.9.0',
+    ],
+    # packages = [], # 需要处理的包目录
+
+    description='The douyin video downloader',
+    long_description='''douyin_downloader
+    抖音视频下载器，批量下载自己喜欢过的视频/上传的视频/关注用户发布的视频/关注用户喜欢的视频。
+    用法：执行 douyin_downloader 命令，根据提示输入合法参数即可。
+    ''',
+    # 添加这个选项，在 windows 下Python目录的 scripts 下生成exe文件，注意：模块与函数之间是冒号:
+    entry_points={'console_scripts': [
+        'douyin_downloader = douyin_downloader:cmd_run',
+    ]},
+    # 此项需要，否则卸载时报windows error
+    zip_safe=False,
+
+    classifiers=[ # 程序的所属分类列表
+        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    extras_require={},
+)
