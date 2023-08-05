bfabcbb27a1bb7e7dea6e56d7709d9e50f3d8c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:51 +0000

    Add package periscopy/0.0.3/periscopy-0.0.3.tar.gz

---
+++ packages/periscopy/periscopy-0.0.3.tar.gz/periscopy-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+INSTALL_REQUIRES = [
+    'numpy>=1.9.3',
+    'pandas>=0.15.2',
+    'matplotlib>=1.4.3']
+
+long_description = "Welcome to Periscopy - a home for beautiful visualizations."
+
+setuptools.setup(
+    name="periscopy",
+    version="0.0.3",
+    author="Christine Quan",
+    author_email="christine.quan11@gmail.com",
+    description="periscopy: business data visualization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/christinequan/periscopy",
+    install_requires=INSTALL_REQUIRES,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
