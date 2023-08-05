7b9066e773de62c22da0a8126780341b40ba7ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:14 +0000

    Add package mh-package/0.15/mh_package-0.15-py2-none-any.whl

---
+++ packages/mh-package/mh_package-0.15-py2-none-any.whl/mh_package/__init__.py
@@ -0,0 +1,14 @@
+# from mh_package import Module1
+from mh_package import Birds
+
+def joke():
+    print ("in joke....0.15")
+    return (u'my mh_package version 0.15.')
+
+if __name__ == '__main__':
+    print ('hello world...')
+    joke()
+    # Module1.foo1()
+    myBird = Birds.Birds()
+    myBird.printMembers()
+
