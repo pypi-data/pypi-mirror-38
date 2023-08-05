473dad73b8ad39a0f4e0e74a030e54dbe57d0c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:54 +0000

    Add package hydrostats/0.73/hydrostats-0.73.tar.gz

---
+++ packages/hydrostats/hydrostats-0.73.tar.gz/hydrostats-0.73/README.md
@@ -0,0 +1,17 @@
+[![Build Status](https://travis-ci.org/waderoberts123/Hydrostats.svg?branch=master)](https://travis-ci.org/waderoberts123/Hydrostats)
+[![codecov](https://codecov.io/gh/waderoberts123/Hydrostats/branch/master/graph/badge.svg)](https://codecov.io/gh/waderoberts123/Hydrostats)
+
+Tools for use in comparison studies, specifically for use in the field of hydrology
+
+- **Documentation:** https://waderoberts123.github.io/Hydrostats/
+- **Source:** https://github.com/waderoberts123/Hydrostats
+- **Bug reports:** https://github.com/waderoberts123/Hydrostats/issues
+
+Hydrostats provides:
+
+- Preprocessing Tools
+- Visualization Tools
+- Access to over 60 error metrics through the [HydroErr package](https://github.com/waderoberts123/HydroErr) dependency
+- A library of relevant metrics to measure forecast skill
+
+[![Powered by BYU Hydroinformatics](https://img.shields.io/badge/powered%20by-BYU%20HydroInformatics-blue.svg)](http://worldwater.byu.edu/)
---
+++ packages/hydrostats/hydrostats-0.73.tar.gz/hydrostats-0.73/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+
+with open('README.md') as f:
+    README = f.read()
+
+setup(
+    name='hydrostats',
+    packages=['hydrostats'],
+    version='0.73',
+    description='Tools for use in comparison studies, specifically for use in the field '
+                'of hydrology',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Wade Roberts',
+    author_email='waderoberts123@gmail.com',
+    url='https://github.com/waderoberts123/hydrostats',
+    download_url='https://github.com/waderoberts123/Hydrostats/archive/0.73.tar.gz',
+    keywords=['hydrology', 'error', 'metrics', 'comparison', 'statistics', 'forecast', 'observed'],
+    classifiers=["Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 ],
+    license='MIT',
+    install_requires=[
+        'numpy >= 1.14.2',
+        'numba == 0.39.0',
+        'pandas >= 0.22.0',
+        'matplotlib >= 2.2.0',
+        'scipy >= 1.0.0',
+        'openpyxl >= 2.5.2',
+        'HydroErr'
+    ],
+)
