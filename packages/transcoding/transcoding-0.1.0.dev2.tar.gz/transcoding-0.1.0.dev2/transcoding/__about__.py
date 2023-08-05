ed62134ebd80e661efdd206c60310ed668f82a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:18 +0000

    Add package transcoding/0.1.0.dev2/transcoding-0.1.0.dev2.tar.gz

---
+++ packages/transcoding/transcoding-0.1.0.dev2.tar.gz/transcoding-0.1.0.dev2/transcoding/__about__.py
@@ -0,0 +1,26 @@
+__all__ = [
+    "__version__",
+    "__title__",
+    "__summary__",
+    "__keywords__",
+    "__uri__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+    "__dependencies__",
+]
+__version__ = '0.1.0.dev2'
+__title__ = 'transcoding'
+__summary__ = "transcodings for standardized file in- and output"
+__keywords__ = "file input output transcoding standardize"
+__uri__ = 'https://gitlab.mpcdf.mpg.de/dboe/transcoding'
+__author__ = "Daniel Boeckenhoff"
+__email__ = "daniel.boeckenhoff@ipp.mpg.de"
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "2018 %s" % __author__
+__dependencies__ = [
+        'parse',
+        'more_itertools',
+        'future',
+    ]
