7667607fdc1509dd10d37eccb3b8424521b65454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package wofrysrw/1.0.24/wofrysrw-1.0.24.tar.gz

---
+++ packages/wofrysrw/wofrysrw-1.0.24.tar.gz/wofrysrw-1.0.24/wofrysrw/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofrysrw SETUP.PY
+short_version = '1.0.24'
+version = '1.0.24'
+full_version = '1.0.24.dev0+bcb2ac8'
+git_revision = 'bcb2ac8666a623621b5670611119ed7d815417ad'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
