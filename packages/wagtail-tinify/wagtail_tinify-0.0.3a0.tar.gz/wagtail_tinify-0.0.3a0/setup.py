b9889084c585ae401e6eed83a587b73ad04816ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package wagtail-tinify/0.0.3a0/wagtail_tinify-0.0.3a0.tar.gz

---
+++ packages/wagtail-tinify/wagtail_tinify-0.0.3a0.tar.gz/wagtail_tinify-0.0.3a0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='wagtail_tinify',
+    version='0.0.3a',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Automatically compress images (.jpg and .png) using tinypng.com.',
+    long_description=readme,
+    url='https://github.com/KalobTaulien/wagtail_tinify',
+    author='Kalob Taulien',
+    author_email='kalob@kalob.io',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Wagtail',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    keywords='development',
+    install_requires=[
+        'wagtail>=2.1.0',
+        'Django>=2.0',
+        'tinify>=1.5.1'
+    ]
+)
