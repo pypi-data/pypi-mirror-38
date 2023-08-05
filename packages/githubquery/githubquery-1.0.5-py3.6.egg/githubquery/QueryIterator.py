a614ba7d971aa587135da25ca2d29689442c31dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package githubquery/1.0.5/githubquery-1.0.5-py3.6.egg

---
+++ packages/githubquery/githubquery-1.0.5-py3.6.egg/githubquery/QueryIterator.py
@@ -0,0 +1,15 @@
+class QueryIterator(object):
+
+    def __init__(self, github_session, arguments, results):
+        self.github_session = github_session
+        self.arguments = arguments
+        self.pos = arguments.get('p')
+        self.results = results
+
+    def next(self):
+        self.github_session.proxy = self.github_session.obtain_new_proxy()
+        self.pos += 1
+        args = self.arguments
+        args['p'] = self.pos
+
+        self.results = self.github_session.search_handler(args)
