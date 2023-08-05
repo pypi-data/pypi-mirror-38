01e9e42ce698e8238659cb632f9e235d7bbd7ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:24 +0000

    Add package turingarena-dev/0.0.2.dev726/turingarena_dev-0.0.2.dev726-py3-none-any.whl

---
+++ packages/turingarena-dev/turingarena_dev-0.0.2.dev726-py3-none-any.whl/turingarena_common/commands.py
@@ -0,0 +1,61 @@
+from collections import namedtuple
+
+RemoteCommandParameters = namedtuple("RemoteCommandParameters", [
+    "log_level",
+    "stderr_isatty",
+    "local_execution",
+    "command",
+])
+
+LocalExecutionParameters = namedtuple("LocalExecutionParameters", [
+    "git_dir",
+])
+
+LocalExecutionParameters.DEFAULT = LocalExecutionParameters(
+    git_dir="/run/turingarena/db.git",
+)
+
+Pack = namedtuple("Pack", [
+    "repository",
+    "oid",
+])
+
+GitRepository = namedtuple("GitRepository", [
+    "url",
+    "branch",
+    "depth",
+])
+
+WorkingDirectory = namedtuple("WorkingDirectory", [
+    "pack",
+    "current_directory",
+])
+
+EvaluateRequest = namedtuple("EvaluateRequest", [
+    "submission",
+    "working_directory",
+    "seed",
+])
+
+EvaluateCommandParameters = namedtuple("EvaluateCommandParameters", [
+    "evaluate_request",
+    "raw_output",
+])
+
+FileCommandParameters = namedtuple("FileCommandParameters", [
+    "working_directory",
+    "command",
+])
+
+FileCatCommandParameters = namedtuple("FileCatCommandParameters", [
+    "path",
+])
+
+InfoCommandParameters = namedtuple("InfoCommandParameters", [
+    "what",
+])
+
+TestCommandParameters = namedtuple("TestCommandParameters", [
+    "pytest_arguments",
+    "working_directory",
+])
