a79d42fb33e02bbf3ed7796a6cd20ffb9464ff4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:42 +0000

    Add package upload-dnatools-36/0.1/upload_dnatools_36-0.1.tar.gz

---
+++ packages/upload-dnatools-36/upload_dnatools_36-0.1.tar.gz/upload_dnatools_36-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'upload_dnatools_36',
+version = '0.1',
+description = 'textbook example',
+url = 'http://example.com',
+author = 'Example',
+author_email = 'Example@gmail.com',
+license = 'Example',
+packages = ['package_1'])
