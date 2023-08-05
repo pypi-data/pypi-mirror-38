1c8efed980cb46a24dc82a9affcc0ce4da0c315d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:55 +0000

    Add package qlib/0.0.2.dev4/qlib-0.0.2.dev4-py3-none-any.whl

---
+++ packages/qlib/qlib-0.0.2.dev4-py3-none-any.whl/qlib/qlibbase.py
@@ -0,0 +1,11 @@
+class QLibBase():
+   
+    version=''
+
+    def  __init__(self,ver='0.0.1'):
+        self.version = ver
+
+    def doSay(self,message="Called from QLibBase"):
+        return f"{message} @version={self.version}"
+
+        
