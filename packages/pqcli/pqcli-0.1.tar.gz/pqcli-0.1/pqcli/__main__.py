83200e904ec1c937279bdc7f61d642b33eddd773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package pqcli/0.1/pqcli-0.1.tar.gz

---
+++ packages/pqcli/pqcli-0.1.tar.gz/pqcli-0.1/pqcli/__main__.py
@@ -0,0 +1,29 @@
+import argparse
+from pathlib import Path
+
+import xdg
+
+from pqcli.roster import Roster
+from pqcli.ui import Ui
+
+SAVE_PATH = Path(xdg.XDG_CONFIG_HOME) / "pqcli" / "save.dat"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog="pqcli")
+    parser.add_argument("--cheats", help="enable cheats", action="store_true")
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    roster = Roster.load(SAVE_PATH)
+
+    try:
+        Ui(roster, args).run()
+    finally:
+        roster.save()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pqcli/pqcli-0.1.tar.gz/pqcli-0.1/pqcli/ui/double_line_box.py
@@ -0,0 +1,32 @@
+import urwid
+
+
+class DoubleLineBox(urwid.AttrMap):
+    def __init__(
+        self,
+        top_widget: urwid.Widget,
+        bottom_widget: urwid.Widget,
+        top_title: str = "",
+        bottom_title: str = "",
+    ) -> None:
+        self.top_line_box = urwid.LineBox(
+            urwid.AttrMap(top_widget, "linebox-content", "linebox-content"),
+            title=top_title,
+            bline="",
+        )
+
+        self.bottom_line_box = urwid.LineBox(
+            urwid.AttrMap(bottom_widget, "linebox-content", "linebox-content"),
+            title=bottom_title,
+            tlcorner="├",
+            trcorner="┤",
+        )
+
+        super().__init__(
+            urwid.Pile([self.top_line_box, ("pack", self.bottom_line_box)]),
+            "linebox",
+            "linebox-focus",
+        )
+
+    def set_bottom_title(self, text: str) -> None:
+        self.bottom_line_box.set_title(text)
