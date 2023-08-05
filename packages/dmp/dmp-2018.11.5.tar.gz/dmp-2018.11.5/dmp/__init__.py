54e88741f6039925867a57103353e40bebcdf19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:14 +0000

    Add package dmp/2018.11.5/dmp-2018.11.5.tar.gz

---
+++ packages/dmp/dmp-2018.11.5.tar.gz/dmp-2018.11.5/dmp/__init__.py
@@ -0,0 +1,4 @@
+from .diff_match_patch import diff_match_patch, patch_obj, __author__, __doc__
+
+__version__ = "2018.11.05"
+__packager__ = "John Reese (john@noswap.com)"
---
+++ packages/dmp/dmp-2018.11.5.tar.gz/dmp-2018.11.5/setup.py
@@ -0,0 +1,39 @@
+import datetime
+from pathlib import Path
+from setuptools import setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("dmp/__init__.py") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.split('"')[1]
+
+setup(
+    name="dmp",
+    description="Repackaging of Google's Diff Match and Patch libraries. Offers robust algorithms to perform the operations required for synchronizing plain text.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    version=version,
+    author="Neil Fraser",
+    author_email="fraser@google.com",
+    url="https://github.com/jreese/dmp",
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Processing",
+    ],
+    license="Apache",
+    packages=["dmp", "dmp.tests"],
+    setup_requires=["setuptools>=38.6.0"],
+    install_requires=[],
+)
