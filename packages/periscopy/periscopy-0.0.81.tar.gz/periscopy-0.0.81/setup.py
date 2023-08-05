82f18247e3528b666a410b1e084ea9f8b39bb86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:58 +0000

    Add package periscopy/0.0.81/periscopy-0.0.81.tar.gz

---
+++ packages/periscopy/periscopy-0.0.81.tar.gz/periscopy-0.0.81/setup.py
@@ -0,0 +1,32 @@
+INSTALL_REQUIRES = [
+    'numpy>=1.9.3',
+    'pandas>=0.15.2',
+    'matplotlib>=1.4.3']
+
+long_description = "Welcome to Periscopy - a home for beautiful visualizations."
+
+try:
+    from setuptools import setup
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+
+
+if __name__ == "__main__":
+    setup(
+        name="periscopy",
+        version="0.0.81",
+        author="Christine Quan",
+        author_email="christine.quan11@gmail.com",
+        description="periscopy: business data visualization",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/christinequan/periscopy",
+        install_requires=INSTALL_REQUIRES,
+        packages=['periscopy'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+    )
