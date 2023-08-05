9743bade9cd4079d023a6a0bdf3d1d24a35fbf04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:15 +0000

    Add package w7x/0.1.0.dev8/w7x-0.1.0.dev8.tar.gz

---
+++ packages/w7x/w7x-0.1.0.dev8.tar.gz/w7x-0.1.0.dev8/w7x/__about__.py
@@ -0,0 +1,31 @@
+__version__ = '0.1.0.dev8'
+__author__ = "Daniel Boeckenhoff"
+__email__ = "daniel.boeckenhoff@ipp.mpg.de"
+__all__ = [
+    "__version__",
+    "__title__",
+    "__summary__",
+    "__keywords__",
+    "__uri__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+    "__dependencies__",
+]
+__title__ = 'w7x'
+__summary__ = ("Convenience methods for magnetic field "
+               "evaluations evolved around the stellarator Wendelstein 7-X."),
+__keywords__ = "Wendelstein 7-X magnetic field field line diffusion VMEC EXTENDER",
+__uri__ = 'https://gitlab.mpcdf.mpg.de/dboe/w7x'
+__author__ = "Daniel Boeckenhoff"
+__email__ = "daniel.boeckenhoff@ipp.mpg.de"
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "2018 %s" % __author__
+__dependencies__ = [
+        'tfields',  # includes many other dependencies
+        'osa',
+        'symfit',
+        'joblib',
+        'transcoding',
+    ],
