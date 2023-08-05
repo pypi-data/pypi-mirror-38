d09c5deee0008519fc56f05c0e20177d6b445e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:52 +0000

    Add package BENPPy/1.0.7/BENPPy-1.0.7.tar.gz

---
+++ packages/BENPPy/BENPPy-1.0.7.tar.gz/BENPPy-1.0.7/BENPPy/__init__.py
@@ -0,0 +1,4 @@
+from .BayesENproteomics import *
+
+version = '1.0.7'
+
---
+++ packages/BENPPy/BENPPy-1.0.7.tar.gz/BENPPy-1.0.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+def readme():
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+configuration = {
+    'name' : 'BENPPy',
+    'version': '1.0.7',
+    'description' : 'BayesENproteomics in Python',
+    'long_description' : readme(),
+    'classifiers' : [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: OS Independent',
+    ],
+    'keywords' : 'proteomics PTMs MCMC',
+    'url' : 'https://github.com/VenkMallikarjun/BENPPy',
+    'author' : 'Venkatesh Mallikarjun',
+    'author_email' : 'vjmallikarjun@gmail.com',
+    'license' : 'MIT License',
+    'packages' : ['BENPPy'],
+    'install_requires': ['pymc3']
+    }
+
+setuptools.setup(**configuration)
