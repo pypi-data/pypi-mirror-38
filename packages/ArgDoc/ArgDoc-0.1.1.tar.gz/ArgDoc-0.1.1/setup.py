da47a1cd64c5cf369fa5272216476576b53c232d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:21 +0000

    Add package ArgDoc/0.1.1/ArgDoc-0.1.1.tar.gz

---
+++ packages/ArgDoc/ArgDoc-0.1.1.tar.gz/ArgDoc-0.1.1/setup.py
@@ -0,0 +1,41 @@
+# setup.py
+
+from setuptools import setup
+from distutils.util import convert_path
+from sphinx.setup_command import BuildDoc
+
+main_ns = {}
+ver_path = convert_path('argdoc/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+cmdclass = {'build_sphinx': BuildDoc}
+
+name = 'ArgDoc'
+
+with open('README.md', 'r') as rm:
+    long_description = rm.read()
+
+setup(name=name,
+      version=main_ns['__version__'],
+      author='Jeremy Solbrig',
+      author_email='jeremy.solbrig@colostate.edu',
+      description='A package for reducing copy/paste of argument descriptions in docstrings',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      py_modules=['argdoc'],
+      cmdclass=cmdclass,
+      command_options={
+          'build_sphinx': {
+              'project': ('setup.py', name),
+              'version': ('setup.py', main_ns['__version__']),
+              'source_dir': ('setup.py', 'doc')}},
+      install_requires=['sphinx', 'sphinxcontrib-programoutput'],
+      url='https://github.com/jsolbrig/argdoc',
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.7',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent']
+     )
+
