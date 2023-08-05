f5198c07661e1c2ed7fbcf6b1f00abbdb470a4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:25 +0000

    Add package stem-registration/0.0.23/stem_registration-0.0.23.tar.gz

---
+++ packages/stem-registration/stem_registration-0.0.23.tar.gz/stem_registration-0.0.23/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="stem_registration",
+    version="0.0.23",
+    author="Dmitry Shevelev",
+    author_email="shevelev.dmitriy@stemsc.com",
+    description="A small registration package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.stemsc.com/shevelev/registration",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'django',
+        'django-registration-redux',
+        'django-bootstrap4',
+        'django-cities-light',
+        'django-select2',
+    ]
+)
