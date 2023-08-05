f1833a5d7bbb4f12909d5876992838cd783fa33d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package plumpy/0.11.5/plumpy-0.11.5.tar.gz

---
+++ packages/plumpy/plumpy-0.11.5.tar.gz/plumpy-0.11.5/plumpy/exceptions.py
@@ -0,0 +1,32 @@
+__all__ = ['KilledError', 'UnsuccessfulResult', 'InvalidStateError', 'PersistenceError']
+
+
+class KilledError(Exception):
+    """The process was killed."""
+
+
+class Unsupported(Exception):
+    pass
+
+
+class LockError(Exception):
+    pass
+
+
+class ProcessExcepted(Exception):
+    pass
+
+
+class InvalidStateError(Exception):
+    pass
+
+
+class UnsuccessfulResult(object):
+    """The result of the process was unsuccessful"""
+
+    def __init__(self, result=None):
+        self.result = result
+
+
+class PersistenceError(Exception):
+    pass
