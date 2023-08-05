b5179ecfb6f1d29188569016c7a4e5df6b2d2e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:03 +0000

    Add package lapdog/0.7.0/lapdog-0.7.0.tar.gz

---
+++ packages/lapdog/lapdog-0.7.0.tar.gz/lapdog-0.7.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+setup(
+    name = 'lapdog',
+    version = '0.7.0',
+    packages = [
+        'lapdog',
+        'lapdog.api'
+    ],
+    package_data={
+        '':[
+            'wdl_pipeline.yaml',
+            'vue/.babelrc',
+            'vue/index.html',
+            'vue/package.json',
+            'vue/webpack.config.js',
+            'vue/src/main.js',
+            'vue/src/App.vue',
+            'vue/src/Pages/*.vue',
+            'api/swagger/lapdog.yaml'
+        ],
+    },
+    description = 'A wrapper for FISS and dalmatian',
+    author = 'Broad Institute - Cancer Genome Computational Analysis',
+    author_email = 'gdac@broadinstitute.org',
+    long_description = 'A wrapper for FISS and dalmatian',
+    entry_points = {
+        'console_scripts': [
+            'lapdog = lapdog.__main__:main'
+        ]
+    },
+    install_requires = [
+        'firecloud-dalmatian',
+        'google-cloud-storage',
+        'pyyaml',
+        'agutil',
+        'flask_cors',
+        'crayons',
+        'connexion',
+        'oauth2client',
+        'requests'
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+    ],
+)
