9c37abf91db43abfe90f43150fd3d5249fbbaca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:15 +0000

    Add package mh-package/0.3/mh_package-0.3.tar.gz

---
+++ packages/mh-package/mh_package-0.3.tar.gz/mh_package-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mh_package',
+      version='0.3',
+      description='My mh_package version 0.3',
+      url='https://job4moshe@dev.azure.com/job4moshe/mh_package/_git/mh_package',
+      author='Moshe Hayoun',
+      author_email='job4moshe@gmail.com',
+      license='MIT',
+      packages=['mh_package'],
+      zip_safe=False)
