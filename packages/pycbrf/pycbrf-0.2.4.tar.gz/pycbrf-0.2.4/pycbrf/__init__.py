b4638f516c55e4b6358b61e4b67e01ba3908502e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:51 +0000

    Add package pycbrf/0.2.4/pycbrf-0.2.4.tar.gz

---
+++ packages/pycbrf/pycbrf-0.2.4.tar.gz/pycbrf-0.2.4/pycbrf/__init__.py
@@ -0,0 +1,9 @@
+from .banks import Banks
+from .rates import ExchangeRates
+
+
+VERSION = (0, 2, 4)
+"""Application version number tuple."""
+
+VERSION_STR = '.'.join(map(str, VERSION))
+"""Application version number string."""
