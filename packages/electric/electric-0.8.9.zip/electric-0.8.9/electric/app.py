ba2384c285f9266a2a4b8a8c2c7b39739a7f229d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package electric/0.8.9/electric-0.8.9.zip

---
+++ packages/electric/electric-0.8.9.zip/electric-0.8.9/electric/app.py
@@ -0,0 +1,74 @@
+import logging
+from flask import Flask, Response, request
+from flask_cors import CORS
+from flask_restful import Api
+
+from rest_interface import \
+    DialogCloseResource, \
+    ZMQCommsResource, \
+    StatusResource, \
+    SystemStorageResource, \
+    ChannelResource, \
+    ControlRegisterResource, \
+    PresetListResource, \
+    PresetResource, ChargeResource, DischargeResource, \
+    BalanceResource, MeasureIRResource, StopResource, \
+    PresetOrderResource, AddNewPresetResource, StoreResource, UnifiedResource, PushTokenResource, \
+    CaseFanResource
+
+application = Flask(__name__, instance_path='/etc')
+cors_app = CORS(application)
+
+format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+handler = logging.StreamHandler()
+formatter = logging.Formatter(format)
+handler.setFormatter(formatter)
+application.logger.addHandler(handler)
+application.logger.setLevel(logging.DEBUG)
+
+api = Api(application)
+api.add_resource(UnifiedResource, "/unified")
+api.add_resource(StatusResource, "/status")
+api.add_resource(ZMQCommsResource, "/zmq/bounce")
+api.add_resource(SystemStorageResource, "/system")
+api.add_resource(ControlRegisterResource, "/control")
+api.add_resource(ChargeResource, "/charge/<channel_id>/<preset_memory_slot>")
+api.add_resource(DischargeResource, "/discharge/<channel_id>/<preset_memory_slot>")
+api.add_resource(StoreResource, "/store/<channel_id>/<preset_memory_slot>")
+api.add_resource(BalanceResource, "/balance/<channel_id>/<preset_memory_slot>")
+api.add_resource(MeasureIRResource, "/measureir/<channel_id>")
+api.add_resource(StopResource, "/stop/<channel_id>")
+api.add_resource(ChannelResource, "/channel/<channel_id>")
+api.add_resource(PresetResource, "/preset/<preset_memory_slot>")
+api.add_resource(PresetListResource, "/preset")
+api.add_resource(AddNewPresetResource, "/addpreset")
+api.add_resource(PresetOrderResource, "/presetorder")
+api.add_resource(DialogCloseResource, "/closedialog/<channel_id>")
+api.add_resource(PushTokenResource, "/pushtoken/<token>")
+api.add_resource(CaseFanResource, "/casefan")
+
+# application.config['TRAP_HTTP_EXCEPTIONS']=True
+#
+# @application.errorhandler(Exception)
+# def handle_error(e):
+#     try:
+#         if e.code < 400:
+#             return Response.force_type(e, request.environ)
+#         elif e.code == 404:
+#             return "The page you're looking for was not found", 404
+#     except:
+#         return make_error_page("Error", "Something went wrong"), 500
+#
+
+# @application.errorhandler(ObjectNotFoundException)
+# def handle_object_not_found(error):
+#     response = jsonify(str(error))
+#     response.status_code = error.status_code
+#     return response
+#
+#
+# @application.errorhandler(BadRequestException)
+# def handle_bad_request(error):
+#     response = jsonify(str(error))
+#     response.status_code = error.status_code
+#     return response
---
+++ packages/electric/electric-0.8.9.zip/electric-0.8.9/electric/worker/router.py
@@ -0,0 +1,69 @@
+import logging
+
+import electric.testing_control as testing_control
+import electric.worker.cache as cache
+
+logger = logging.getLogger('electric.worker.router')
+
+
+#
+# NOTE: this caches ONLY the GET requests, not the other uses of get_device_info() that are internal to the operation
+# of the run_op/stop_op methods.
+#
+
+
+def route_message(charger, method, args):
+    fan_controller = cache.values.get_case_fan_controller()
+
+    if method == "get_device_info":
+        if testing_control.values.bypass_caches:
+            return charger.get_device_info()
+        return cache.values.get_device_info()
+    elif method == "get_channel_status":
+        if testing_control.values.bypass_caches:
+            return charger.get_channel_status(args["channel"])
+        return cache.values.get_channel_status(args["channel"])
+    elif method == "get_control_register":
+        return charger.get_control_register()
+    elif method == "set_active_channel":
+        return charger.set_active_channel(args["channel"])
+    elif method == "get_system_storage":
+        return charger.get_system_storage()
+    elif method == "save_system_storage":
+        return charger.save_system_storage(args["storage"])
+    elif method == "select_memory_program":
+        return charger.select_memory_program(args["memory_slot"], args["channel"])
+    elif method == "save_full_preset_list":
+        return charger.save_full_preset_list(args["preset_list"])
+    elif method == "get_full_preset_list":
+        return charger.get_full_preset_list()
+    elif method == "get_preset":
+        return charger.get_preset(args["memory_slot"])
+    elif method == "delete_preset_at_index":
+        return charger.delete_preset_at_index(args["memory_slot"])
+    elif method == "add_new_preset":
+        return charger.add_new_preset(args["preset"])
+    elif method == "save_preset_to_memory_slot":
+        return charger.save_preset_to_memory_slot(args["preset"], args["memory_slot"], args["write_to_flash"], args["verify_write"])
+    elif method == "stop_operation":
+        return charger.stop_operation(args["channel"])
+    elif method == "run_operation":
+        return charger.run_operation(args["op"], args["channel"], args["memory_slot"])
+    elif method == "measure_ir":
+        return charger.measure_ir(args["channel"])
+    elif method == "turn_off_logging":
+        return charger.turn_off_logging()
+    elif method == "set_beep_properties":
+        return charger.set_beep_properties(args["beep_index"], args["enabled"], args["volume"])
+    elif method == "get_case_fan_info":
+        # This returns a CaseFan object (the state of the case fan controller)
+        return fan_controller.fan
+    elif method == "set_case_fan_prefs":
+        # Updates the state of the fan controller
+        fan_controller.set_control_onoff(args.control)
+        fan_controller.set_temp_threshold(args.threshold)
+        fan_controller.set_temp_hysteresis(args.hysteresis)
+        fan_controller.set_gpio_pin(args.gpio)
+        return fan_controller.save_prefs()
+    else:
+        raise IOError("Unknown method name, cannot execute anything: {0}".format(method))
