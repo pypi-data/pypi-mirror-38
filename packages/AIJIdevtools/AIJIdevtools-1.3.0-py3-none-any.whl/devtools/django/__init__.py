aab63d165cbb9c05fdef068fe882a37c8777dce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:08 +0000

    Add package AIJIdevtools/1.3.0/AIJIdevtools-1.3.0-py3-none-any.whl

---
+++ packages/AIJIdevtools/AIJIdevtools-1.3.0-py3-none-any.whl/devtools/django/__init__.py
@@ -0,0 +1,19 @@
+from typing import Dict, List
+from operator import or_
+from functools import reduce
+
+try:
+    from django.db.models import Q
+except ModuleNotFoundError:
+    pass
+
+
+def Q_any(key_list_maps: Dict[str, List]):
+    return reduce(
+        or_,
+        (
+            Q(**{key: value})
+            for key in key_list_maps.keys()
+            for value in key_list_maps[key]
+        ),
+        Q())
