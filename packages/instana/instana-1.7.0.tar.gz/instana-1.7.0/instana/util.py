d4aab6662d2de653dc90353f9ed3e7aa208b9c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:07 +0000

    Add package instana/1.7.0/instana-1.7.0.tar.gz

---
+++ packages/instana/instana-1.7.0.tar.gz/instana-1.7.0/instana/util.py
@@ -0,0 +1,113 @@
+import binascii
+import json
+import os
+import random
+import re
+import struct
+import sys
+import time
+
+import pkg_resources
+
+from .log import logger
+
+if sys.version_info.major is 2:
+    string_types = basestring
+else:
+    string_types = str
+
+_rnd = random.Random()
+_current_pid = 0
+
+BAD_ID_LONG = 3135097598  # Bad Cafe in base 10
+BAD_ID_HEADER = "BADDCAFE"  # Bad Cafe
+
+
+def generate_id():
+    """ Generate a 64bit signed integer for use as a Span or Trace ID """
+    global _current_pid
+
+    pid = os.getpid()
+    if (_current_pid != pid):
+        _current_pid = pid
+        _rnd.seed(int(1000000 * time.time()) ^ pid)
+    return _rnd.randint(-9223372036854775808, 9223372036854775807)
+
+
+def id_to_header(id):
+    """ Convert a 64bit signed integer to an unsigned base 16 hex string """
+
+    try:
+        if not isinstance(id, int):
+            return BAD_ID_HEADER
+
+        byteString = struct.pack('>q', id)
+        return str(binascii.hexlify(byteString).decode('UTF-8').lstrip('0'))
+    except Exception as e:
+        logger.debug(e)
+        return BAD_ID_HEADER
+
+
+def header_to_id(header):
+    """ Convert an unsigned base 16 hex string into a 64bit signed integer """
+
+    if not isinstance(header, string_types):
+        return BAD_ID_LONG
+
+    try:
+        # Test that header is truly a hexadecimal value before we try to convert
+        int(header, 16)
+
+        # Pad the header to 16 chars
+        header = header.zfill(16)
+        r = binascii.unhexlify(header)
+        return struct.unpack('>q', r)[0]
+    except ValueError:
+        return BAD_ID_LONG
+
+
+def to_json(obj):
+    try:
+        return json.dumps(obj, default=lambda obj: {k.lower(): v for k, v in obj.__dict__.items()},
+                          sort_keys=False, separators=(',', ':')).encode()
+    except Exception as e:
+        logger.info("to_json: ", e, obj)
+
+
+def package_version():
+    try:
+        version = ""
+        version = pkg_resources.get_distribution('instana').version
+    except pkg_resources.DistributionNotFound:
+        version = 'unknown'
+    finally:
+        return version
+
+
+def get_py_source(file):
+    """
+    Retrieves and returns the source code for any Python
+    files requested by the UI via the host agent
+
+    @param file [String] The fully qualified path to a file
+    """
+    try:
+        response = None
+        pysource = ""
+
+        if regexp_py.search(file) is None:
+            response = {"error": "Only Python source files are allowed. (*.py)"}
+        else:
+            with open(file, 'r') as pyfile:
+                pysource = pyfile.read()
+
+            response = {"data": pysource}
+
+    except Exception as e:
+        response = {"error": str(e)}
+    finally:
+        return response
+
+
+# Used by get_py_source
+regexp_py = re.compile('\.py$')
