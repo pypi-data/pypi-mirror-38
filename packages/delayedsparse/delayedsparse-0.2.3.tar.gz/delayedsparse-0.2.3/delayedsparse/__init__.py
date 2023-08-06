3d1d701bf5fa683d87cdc1de19f265787403c021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package delayedsparse/0.2.3/delayedsparse-0.2.3.tar.gz

---
+++ packages/delayedsparse/delayedsparse-0.2.3.tar.gz/delayedsparse-0.2.3/delayedsparse/__init__.py
@@ -0,0 +1,8 @@
+
+from .delayedsparse import safe_sparse_dot, delayedspmatrix,delayedspmatrix_t,isdelayedspmatrix
+from .extmath2 import randomized_svd
+from .ca import CA
+from .pca import PCA
+from .cca import CCA
+
+from .__version__ import __version__
