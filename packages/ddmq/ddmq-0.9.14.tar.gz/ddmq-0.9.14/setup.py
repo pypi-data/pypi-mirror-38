df8e91dc5bc5bfc2843243989c4c5b11cf4c983e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:05 +0000

    Add package ddmq/0.9.14/ddmq-0.9.14.tar.gz

---
+++ packages/ddmq/ddmq-0.9.14.tar.gz/ddmq-0.9.14/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ddmq",
+    version="0.9.14",
+    author="Martin Dahlö",
+    author_email="m.dahlo@gmail.com",
+    description="A file based serverless messaging queue",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ddmq/ddmq",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pyyaml',
+    ],
+
+    entry_points={
+        'console_scripts': ['ddmq = ddmq.cli:main',
+        ],
+    }
+)
