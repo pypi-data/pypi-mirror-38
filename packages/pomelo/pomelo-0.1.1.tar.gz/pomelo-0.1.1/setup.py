29490319231d748eec449f3c651e4b6da7bc8b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:23 +0000

    Add package pomelo/0.1.1/pomelo-0.1.1.tar.gz

---
+++ packages/pomelo/pomelo-0.1.1.tar.gz/pomelo-0.1.1/setup.py
@@ -0,0 +1,56 @@
+import codecs
+import os
+import sys
+  
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+  
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+  
+  
+  
+NAME = "pomelo"
+  
+PACKAGES = ["pomelo",]
+  
+DESCRIPTION = "get hoster info or thread etc."
+  
+LONG_DESCRIPTION = read("README.txt")
+  
+KEYWORDS = "ssh python python3"
+  
+AUTHOR = "Andrew liu"
+  
+AUTHOR_EMAIL = "1185978974@qq.com"
+  
+URL = "https://pypi.python.org/pypi/pomelo"
+  
+VERSION = "0.1.1"
+  
+LICENSE = "GPL"
+  
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    install_requires = ['paramiko'],
+    include_package_data=True,
+    zip_safe=True,
+)
+  
