ed9d5b6e99731f39cd5c26e47190f6dcba5aa4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package SetMem/0.1.1/SetMem-0.1.1.tar.gz

---
+++ packages/SetMem/SetMem-0.1.1.tar.gz/SetMem-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='SetMem',
+    version='0.1.1',
+    packages=['SetMem',],
+    license='',
+    url='https://pypi.org/SetMem',
+    long_description=open('README.txt').read(),
+)
