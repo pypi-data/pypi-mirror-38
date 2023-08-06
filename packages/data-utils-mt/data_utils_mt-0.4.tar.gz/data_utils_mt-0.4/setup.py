450e42987ffa04dfc6394339e60cd906fab73d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:12 +0000

    Add package data-utils-mt/0.4/data_utils_mt-0.4.tar.gz

---
+++ packages/data-utils-mt/data_utils_mt-0.4.tar.gz/data_utils_mt-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='data_utils_mt',
+      version='0.4',
+      description='Utility functions for pandas and Data Analysis.',
+      packages=['data_utils_mt'],
+      author='Miguel Tasende',
+      zip_safe=False)
