6af29cfb2440d27d061d1f9c5314f50e3f1b1060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:16 +0000

    Add package pybpod-gui-plugin-alyx/0.2/pybpod_gui_plugin_alyx-0.2-py3-none-any.whl

---
+++ packages/pybpod-gui-plugin-alyx/pybpod_gui_plugin_alyx-0.2-py3-none-any.whl/pybpod_alyx_module/settings.py
@@ -0,0 +1,9 @@
+import os
+
+
+ROTARYENCODER_PLUGIN_ICON = os.path.join( os.path.dirname(__file__), 'resources', 'rotary.png' )
+
+
+ALYX_PLUGIN_ADDRESS = 'URL TO ALYX'
+ALYX_PLUGIN_USERNAME = None
+ALYX_PLUGIN_PASSWORD = None
