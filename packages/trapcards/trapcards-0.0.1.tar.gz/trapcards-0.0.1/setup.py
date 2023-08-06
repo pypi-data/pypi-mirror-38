8ed8022cadacb4a34a2b73121d8b7efede2aa912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:05 +0000

    Add package trapcards/0.0.1/trapcards-0.0.1.tar.gz

---
+++ packages/trapcards/trapcards-0.0.1.tar.gz/trapcards-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trapcards",
+    version="0.0.1",
+    author="Rabbinhat",
+    author_email="zhengyangma9517@gmail.com",
+    description="A collections of mathmatic dev kits that the author designed for daily coding.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
