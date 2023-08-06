127f66da693affb9de37530393f0038b0dd6ba59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:58 +0000

    Add package PyBusiness/0.0.5/PyBusiness-0.0.5.tar.gz

---
+++ packages/PyBusiness/PyBusiness-0.0.5.tar.gz/PyBusiness-0.0.5/README.md
@@ -0,0 +1,30 @@
+**Usage**
+============
+
+**Email**
+-------
+
+```
+
+from PyBusiness import email
+```
+
+**Syntax**
+
+```
+
+email(your_address, addressee, your_passwd, subject_of_email, message)
+```
+
+**Example**
+
+```
+
+email("johndoe@whatever.com", "friend@whatever.com", "password", "Greetings", "Hello")
+```
+
+-------------
+
+Now Enjoy this video named [crab rave](https://www.youtube.com/watch?v=LDU_Txk06tM) .
+
+**And please support [pypi.org](https://pypi.org) by [donating](https://donate.pypi.org) to them**
---
+++ packages/PyBusiness/PyBusiness-0.0.5.tar.gz/PyBusiness-0.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyBusiness",
+    version="0.0.5",
+    author="J. Nma",
+    author_email="wooldoughnutspi@gmail.com",
+    description="An easy way to use domesticated utilities for purposes such as business",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://saythanks-com-python-email.webnode.com',
+    keywords='personal_use business_use multi-purpose business',
+    python_requires='>=3',
+    project_urls={
+    'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+    'Funding': 'https://donate.pypi.org',
+    'Say Thanks!': 'https://saythanks-com-python-email.webnode.com',
+    'Source': 'https://saythanks-com-python-email.webnode.com',
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+	"Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
