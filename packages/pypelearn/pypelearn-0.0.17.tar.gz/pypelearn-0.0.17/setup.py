9eb334818869a0cc14a3bb8ac3f1609f5a701100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:55 +0000

    Add package pypelearn/0.0.17/pypelearn-0.0.17.tar.gz

---
+++ packages/pypelearn/pypelearn-0.0.17.tar.gz/pypelearn-0.0.17/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pypelearn",
+    version="0.0.17",
+    author="Alex Harston",
+    author_email="alex@harston.io",
+    description="A pipeline library for testing and validating time series data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexharston/pypelearn",
+    packages=find_packages(),
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
