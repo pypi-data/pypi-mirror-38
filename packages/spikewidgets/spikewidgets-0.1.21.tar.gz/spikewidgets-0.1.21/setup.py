a045094d526c1534287b48702b1d526dafc78325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:57 +0000

    Add package spikewidgets/0.1.21/spikewidgets-0.1.21.tar.gz

---
+++ packages/spikewidgets/spikewidgets-0.1.21.tar.gz/spikewidgets-0.1.21/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+pkg_name="spikewidgets"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.1.21",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="Python widgets for use with spike sorting and electrophysiology",
+    url="https://github.com/magland/spikewidgets",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'ipython',
+        'vdom',
+        'ipywidgets',
+        'matplotlib',
+        'spikeinterface',
+        'ml_ms4alg',
+        'pandas'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    )
+)
