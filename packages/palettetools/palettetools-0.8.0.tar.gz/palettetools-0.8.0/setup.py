12a3e912b9810514ba05b8e837511a7a65e1d58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package palettetools/0.8.0/palettetools-0.8.0.tar.gz

---
+++ packages/palettetools/palettetools-0.8.0.tar.gz/palettetools-0.8.0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+
+setup(
+    name='palettetools',
+    version='0.8.0',
+    license='BSD 2-Clause License',
+    description='Color palette tools for processing imgix-served images.',
+    long_description= '%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
+    author='sherwinski',
+    author_email='sherwinhb@gmail.com',
+    url='https://github.com/sherwinski/palette-tools',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        'image', 'processing', 'color', 'palette', 'tools', 'imgix'
+    ],
+    install_requires=[
+        'requests'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+)
