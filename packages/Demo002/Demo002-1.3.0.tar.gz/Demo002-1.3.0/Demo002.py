58366175ec1147be56d3ea7983eb9513090fc7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:56 +0000

    Add package Demo002/1.3.0/Demo002-1.3.0.tar.gz

---
+++ packages/Demo002/Demo002-1.3.0.tar.gz/Demo002-1.3.0/Demo002.py
@@ -0,0 +1,12 @@
+#------------------------------------------------------------------#
+#                   Traversing a List
+#------------------------------------------------------------------#
+def traversing_list (the_list,indent=False,level=0,):# define a function for traversing list -->level=0 meaning this parameter is optional.
+    for son_list in the_list:
+        if isinstance(son_list,list):
+            traversing_list(son_list,indent,level+1)#if son_list is also a list we can
+        else:
+            if  indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(son_list)
