4d19a1f3106b9ee788be1130bdfd26c0aa16e135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:41 +0000

    Add package mh/0.0.9/mh-0.0.9-py2-none-any.whl

---
+++ packages/mh/mh-0.0.9-py2-none-any.whl/mh/mh.py
@@ -0,0 +1,16 @@
+# eg.py
+'''
+eg code
+'''
+
+EG_VAR='ABCD'
+
+def testFunction():
+    ''' returns a test string '''
+    return 'OK'
+
+class Mh (object):
+    def runMh(self):
+        print ('OK RUN')
+        return 100
+
