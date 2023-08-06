a11e3b8d188499e3615ac664e6c1afda0ab9ee62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:55 +0000

    Add package QForestMath/0.0.4/QForestMath-0.0.4-py3-none-any.whl

---
+++ packages/QForestMath/QForestMath-0.0.4-py3-none-any.whl/QForestMath/__init__.py
@@ -0,0 +1,11 @@
+name = "QForestMath"
+
+from . import add
+from . import subtract
+from . import multiply
+from . import divide
+from . import qforest_matherror
+# from .qforest_matherror import QForestMathError, RegisterError
+
+
+
