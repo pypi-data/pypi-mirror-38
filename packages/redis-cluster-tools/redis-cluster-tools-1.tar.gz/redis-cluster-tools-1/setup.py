60e4b857cb292493361d3e0de26d3ec07fa66ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:58 +0000

    Add package redis-cluster-tools/1/redis-cluster-tools-1.tar.gz

---
+++ packages/redis-cluster-tools/redis-cluster-tools-1.tar.gz/redis-cluster-tools-1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="redis-cluster-tools",
+    version="1",
+    author="Anton Aleksandrov",
+    author_email="antonaleksandrov24@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AntonAleksandrov13/python-redis-tool.git",
+    packages=['cli'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['redis-tool=cli.__main__:main'],
+    }
+)
