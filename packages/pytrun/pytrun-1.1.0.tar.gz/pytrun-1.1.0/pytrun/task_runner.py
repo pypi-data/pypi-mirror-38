646ecc1373f2fdd5a253a6cdf7428b8938cd11c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:47 +0000

    Add package pytrun/1.1.0/pytrun-1.1.0.tar.gz

---
+++ packages/pytrun/pytrun-1.1.0.tar.gz/pytrun-1.1.0/pytrun/task_runner.py
@@ -0,0 +1,54 @@
+"""Define TaskRunner."""
+import yaml
+import os
+
+
+class TaskRunner(object):
+    """Define TaskRunner."""
+
+    def __init__(self, file):
+        """Load tasks definition."""
+        # parse file to config
+        self.config = self._get_config(file)
+
+    def run(self, tasks):
+        for task in tasks:
+            self._run_task(task)
+
+    def _run_task(self, task):
+        """Run a task."""
+        cmds = self.config[task]
+        if isinstance(cmds, dict):
+            tasks = cmds.get('tasks')
+            if tasks:
+                return self._run_subtasks(tasks)
+            else:
+                raise ValueError('Wrong description for task {}.'.format(task))
+
+        elif isinstance(cmds, list):
+            for cmd in cmds:
+                print("--- [start:{}] - {}".format(task, cmd))
+                os.system(cmd)
+                print("--- [finished:{}] - {}".format(task, cmd))
+
+    def _run_subtasks(self, tasks):
+        for task in tasks:
+            self._run_task(task)
+
+    def _get_config(self, file):
+        abs_path = os.path.abspath(file)
+        filename = abs_path.split('.')[0]
+        ext_list = [
+            'yml',
+            'yaml',
+        ]
+
+        for ext in ext_list:
+            pathname = '{}.{}'.format(filename, ext)
+            try:
+                with open(pathname, 'r') as stream:
+                    return yaml.load(stream)
+            except FileNotFoundError:
+                pass
+
+        raise FileNotFoundError("{} can't be found!".format(file))
