ffb48042240900672e66c6fc2d24f3b23128f740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:09 +0000

    Add package kgekit/0.1.2/kgekit-0.1.2.tar.gz

---
+++ packages/kgekit/kgekit-0.1.2.tar.gz/kgekit-0.1.2/processing_scripts/match_triple_labels.py
@@ -0,0 +1,18 @@
+import sys
+from collections import defaultdict
+import kgekit.io
+import kgekit.data
+import random
+from itertools import chain, dropwhile
+
+def match_triple_labels(label_filename, translation_filename):
+    entities, _ = kgekit.io.read_translation(translation_filename)
+    labels = kgekit.io.read_labels(label_filename, ' ')
+    filtered_labels = list(map(lambda l: (labels[l[0]], l[1]), dropwhile(lambda x: x[0] not in entities, labels)))
+    return filtered_labels
+
+if __name__ == '__main__':
+    labels = match_triple_labels(sys.argv[1], sys.argv[2])
+    with open('matched_labels.txt', 'w') as f:
+        for l in labels:
+            f.write(" ".join(l) + "\n")
