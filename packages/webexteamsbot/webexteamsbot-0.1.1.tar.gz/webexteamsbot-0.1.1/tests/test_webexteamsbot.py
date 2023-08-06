637ce44aed687640aeb1fd0736ecd13652f4151a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:26 +0000

    Add package webexteamsbot/0.1.1/webexteamsbot-0.1.1.tar.gz

---
+++ packages/webexteamsbot/webexteamsbot-0.1.1.tar.gz/webexteamsbot-0.1.1/tests/test_webexteamsbot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `webexteamsbot` package."""
+
+
+import unittest
+
+from webexteamsbot import webexteamsbot
+
+
+class TestWebexteamsbot(unittest.TestCase):
+    """Tests for `webexteamsbot` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
