6944943e6fd4a99dd50866fddd8034f67eb58ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:50 +0000

    Add package ipinfo/1.1.0/ipinfo-1.1.0-py3-none-any.whl

---
+++ packages/ipinfo/ipinfo-1.1.0-py3-none-any.whl/ipinfo_wrapper/cache/interface.py
@@ -0,0 +1,16 @@
+import abc
+
+class CacheInterface(metaclass=abc.ABCMeta):
+    """Interface for using custom cache."""
+
+    @abc.abstractmethod
+    def __contains__(self, key):
+        pass
+
+    @abc.abstractmethod
+    def __setitem__(self, key, value):
+        pass
+
+    @abc.abstractmethod
+    def __getitem__(self, key):
+        pass
---
+++ packages/ipinfo/ipinfo-1.1.0-py3-none-any.whl/ipinfo_wrapper/exceptions.py
@@ -0,0 +1,3 @@
+class RequestQuotaExceededError(Exception):
+    """Error indicating that users monthly request quota has been passed."""
+    pass
