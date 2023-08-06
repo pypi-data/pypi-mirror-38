4c46c0a5ea613545d201ddb276e6dac91b5df461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:42 +0000

    Add package mother-of-dragons/0.2.1/mother-of-dragons-0.2.1.tar.gz

---
+++ packages/mother-of-dragons/mother-of-dragons-0.2.1.tar.gz/mother-of-dragons-0.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mother-of-dragons",
+    version="0.2.1",
+    author="Brenden Matthews",
+    author_email="brenden@diddyinc.com",
+    description="Python-based management tool for DragonMint and Innosilicon miners",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brndnmtthws/mother-of-dragons",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts':
+            ['mother-of-dragons=mother_of_dragons.main:main'],
+    },
+)
