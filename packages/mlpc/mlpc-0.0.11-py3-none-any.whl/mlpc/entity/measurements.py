1d5ecb87ac3e748c6cfc946a262b957d5a141e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:09 +0000

    Add package mlpc/0.0.11/mlpc-0.0.11-py3-none-any.whl

---
+++ packages/mlpc/mlpc-0.0.11-py3-none-any.whl/mlpc/entity/measurements.py
@@ -0,0 +1,10 @@
+from mlpc.entity.base_entity import BaseEntity
+from mlpc.utils.log import debug
+
+
+class Measurements(BaseEntity):
+    def __init__(self, storage):
+        super().__init__(storage)
+
+    def save(self, measurement_name, measurement):
+        self._storage.save_resource("measurement", measurement_name, measurement, "txt")
