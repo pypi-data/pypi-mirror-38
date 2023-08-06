fedb380b2be1b18c17528400e66edde61edd0843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package fastavro/0.21.14/fastavro-0.21.14.tar.gz

---
+++ packages/fastavro/fastavro-0.21.14.tar.gz/fastavro-0.21.14/.travis_build_wheel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+# The version comes in as something like 3.6, but we need it to be 36 for the
+# path in the container so we remove the period
+TRAVIS_PYTHON_VERSION=$1
+TRAVIS_PYTHON_VERSION=${TRAVIS_PYTHON_VERSION/.}
+
+/opt/python/*${TRAVIS_PYTHON_VERSION}*m/bin/pip install cython
+FASTAVRO_USE_CYTHON=1 /opt/python/*${TRAVIS_PYTHON_VERSION}*m/bin/python setup.py bdist_wheel
+
+# Fix wheel
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist
+done
+
+# Remove unfixed wheel
+rm -rf dist/fastavro-*-linux_*
