7531bc6f60aa5a55a20b19d33bd0daa1a6c97bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:36 +0000

    Add package X3DH/0.5.4/X3DH-0.5.4.tar.gz

---
+++ packages/X3DH/X3DH-0.5.4.tar.gz/X3DH-0.5.4/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "x3dh"))
+
+import version
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name = "X3DH",
+    version = version.__version__,
+    description = (
+        "A python implementation of the Extended Triple Diffie-Hellman key agreement " +
+        "protocol."
+    ),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Syndace/python-x3dh",
+    author = "Tim Henkes",
+    author_email = "tim@cifg.io",
+    license = "MIT",
+    packages = find_packages(),
+    install_requires = [ "cryptography>=1.7.1", "XEdDSA>=0.4.2" ],
+    python_requires  = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
+    zip_safe = False,
+    classifiers = [
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+
+        "Topic :: Communications :: Chat",
+        "Topic :: Security :: Cryptography",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+        "Programming Language :: Python :: Implementation :: CPython",
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
