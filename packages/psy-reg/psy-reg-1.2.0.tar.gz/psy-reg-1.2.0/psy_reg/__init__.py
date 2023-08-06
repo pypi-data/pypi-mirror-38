2621810c4c18465cee22dc0ae016a7c9cc02809b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:18 +0000

    Add package psy-reg/1.2.0/psy-reg-1.2.0.tar.gz

---
+++ packages/psy-reg/psy-reg-1.2.0.tar.gz/psy-reg-1.2.0/psy_reg/__init__.py
@@ -0,0 +1,9 @@
+"""psy-reg: Psyplot plugin for visualizing and calculating regression plots
+
+This package contains the plotters for interactive visualization tasks with the
+abilities to calculate and visualize regression plots.
+This package uses statsmodels and scipy for it's calculations.
+"""
+from psy_reg.version import __version__
+
+__author__ = "Philipp Sommer (philipp.sommer@unil.ch)"
