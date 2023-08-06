ba753e833bacda476bef2b6aec6134e3e0e86fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:52 +0000

    Add package django-bootstrapper/0.0.4/django_bootstrapper-0.0.4-py3.6.egg

---
+++ packages/django-bootstrapper/django_bootstrapper-0.0.4-py3.6.egg/EGG-INFO/scripts/django-bootstrapper.py
@@ -0,0 +1,5 @@
+#!/Users/ma0/projects/contraslash/django-bootstrapper/django_bootstrapper_env/bin/python
+from django_bootstrapper import execute_from_command_line
+
+if __name__ == "__main__":
+    execute_from_command_line()
