68c898638b97b7d8b4cc34dd2383dcf771da4d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:53 +0000

    Add package spoof/1.3.0/spoof-1.3.0.tar.gz

---
+++ packages/spoof/spoof-1.3.0.tar.gz/spoof-1.3.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+  name='spoof',
+  version='1.3.0',
+  description='HTTP server for testing environments',
+  long_description=open('README.rst').read(),
+  author='Lex Scarisbrick',
+  author_email='lex@scarisbrick.org',
+  license='MIT',
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Software Development :: Testing :: Traffic Generation'
+  ],
+  url='https://github.com/lexsca/spoof.git',
+  py_modules=['spoof'],
+  setup_requires=['pytest-runner'],
+  tests_require=['pytest', 'pytest-cov', 'mock', 'requests'],
+)
