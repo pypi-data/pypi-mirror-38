a26212f116ef963ef219e0ca12a3f5e99f8f4a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:41 +0000

    Add package gwsurrogate/0.8.7/gwsurrogate-0.8.7.tar.gz

---
+++ packages/gwsurrogate/gwsurrogate-0.8.7.tar.gz/gwsurrogate-0.8.7/setup.py
@@ -0,0 +1,43 @@
+import sys
+
+try:
+  from setuptools import setup
+  setup
+except ImportError: # currently not supported
+  raise ImportError("GWSurrogate requires setuptools")
+  #from distutils.core import setup # currently not supported
+  #setup
+
+# To render markdown. See https://github.com/pypa/pypi-legacy/issues/148
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md').read()
+
+
+setup(name='gwsurrogate',
+      version='0.8.7',
+      author='Jonathan Blackman, Scott Field, Chad Galley, Vijay Varma',
+      author_email='sfield@umassd.edu',
+      packages=['gwsurrogate'],
+      license='MIT',
+      include_package_data=True,
+      contributors=[
+      # Alphabetical by last name.
+      ""],
+      description='An easy to use interface to gravitational wave surrogate models',
+      long_description=long_description,
+      # will start new downloads if these are installed in a non-standard location
+      install_requires=["gwtools"],
+      classifiers=[
+                'Intended Audience :: Other Audience',
+                'Intended Audience :: Science/Research',
+                'Natural Language :: English',
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering',
+                'Topic :: Scientific/Engineering :: Mathematics',
+                'Topic :: Scientific/Engineering :: Physics',
+      ],
+)
