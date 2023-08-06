a4b5a58e1444b7265e97a666838344d6312af988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package pytoolbox/12.2.3/pytoolbox-12.2.3.tar.gz

---
+++ packages/pytoolbox/pytoolbox-12.2.3.tar.gz/pytoolbox-12.2.3/pytoolbox/django/models/managers/mixins.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Mix-ins for building your own model managers.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .. import utils
+from ..query import mixins as query_mixins
+from .... import module
+
+_all = module.All(globals())
+
+AtomicGetUpdateOrCreateMixin = query_mixins.AtomicGetUpdateOrCreateMixin
+AtomicGetRestoreOrCreateMixin = query_mixins.AtomicGetRestoreOrCreateMixin
+CreateModelMethodMixin = query_mixins.CreateModelMethodMixin
+StateMixin = query_mixins.StateMixin
+
+
+class RelatedModelMixin(object):
+
+    def get_related_manager(self, field):
+        return utils.get_related_manager(self.model, field)
+
+    def get_related_model(self, field):
+        return utils.get_related_model(self.model, field)
+
+
+__all__ = _all.diff(globals())
---
+++ packages/pytoolbox/pytoolbox-12.2.3.tar.gz/pytoolbox-12.2.3/pytoolbox/regex.py
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import re
+
+from . import module
+
+_all = module.All(globals())
+
+TIME_REGEX_PARTS = ['[0-2]', '[0-9]', ':', '[0-5]', '[0-9]', ':', '[0-5]', '[0-9]']
+UUID_REGEX = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+
+
+def embed_in_regex(string, regex_parts, index, as_string=True):
+    """
+    **Example usage**
+
+    >>> from pytoolbox.unittest import asserts
+    >>> asserts.equal(embed_in_regex('L', ['[a-z]', '[a-z]'], 0), (0, 'L[a-z]'))
+    >>> asserts.equal(embed_in_regex('L', ['[a-z]', '[a-z]'], 1), (1, '[a-z]L'))
+    >>> asserts.equal(embed_in_regex('L', ['[a-z]', '[a-z]'], 1, as_string=False), (1, ['[a-z]', 'L']))
+    """
+    regex = regex_parts[:]
+    regex[index:index + len(string)] = string
+    return index, ''.join(regex) if as_string else regex
+
+
+def findall_partial(string, regex_parts):
+    """
+    **Example usage**
+
+    >>> from pytoolbox.unittest import asserts
+    >>> result = [i for s, r, i in findall_partial(':', TIME_REGEX_PARTS)]
+    >>> asserts.list_equal(result, [2, 5])
+    >>> result = [embed_in_regex(s, r, i) for s, r, i in findall_partial('12:15:2', TIME_REGEX_PARTS)]
+    >>> asserts.list_equal(result, [(0, '12:15:2[0-9]')])
+    >>> result = [embed_in_regex(s, r, i) for s, r, i in findall_partial('18:2', TIME_REGEX_PARTS)]
+    >>> asserts.list_equal(result, [(0, '18:2[0-9]:[0-5][0-9]'), (3, '[0-2][0-9]:18:2[0-9]')])
+    >>> result = [embed_in_regex(s, r, i) for s, r, i in findall_partial('59:1', TIME_REGEX_PARTS)]
+    >>> asserts.list_equal(result, [(3, '[0-2][0-9]:59:1[0-9]')])
+    """
+    for index in xrange(0, len(regex_parts) - len(string) + 1):  # noqa
+        regex = regex_parts[index:index + len(string)]
+        match = re.search(''.join(regex), string)
+        if match:
+            yield string, regex_parts, index
+
+
+__all__ = _all.diff(globals())
