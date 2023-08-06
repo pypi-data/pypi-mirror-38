0f5af27e3831508b0a8784c9297d50f976362e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:50 +0000

    Add package notifyr/1.0/notifyr-1.0.tar.gz

---
+++ packages/notifyr/notifyr-1.0.tar.gz/notifyr-1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+      name='notifyr',
+      version='1.0',
+      description='Object notification tool that implements observer design pattern at runtime',
+      url='https://github.com/victorcmoura/notifyr',
+      author='Victor Moura',
+      author_email='victor_cmoura@hotmail.com',
+      license='GPL-3.0',
+      packages=setuptools.find_packages(),
+      zip_safe=False
+)
