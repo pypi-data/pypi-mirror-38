b68cc4f2b756ce29e457cf13170391448620551a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:03 +0000

    Add package z3c.flashmessage/2.1/z3c.flashmessage-2.1-py2.py3-none-any.whl

---
+++ packages/z3c.flashmessage/z3c.flashmessage-2.1-py2.py3-none-any.whl/z3c/flashmessage/tests.py
@@ -0,0 +1,31 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2007 Zope Foundation and Contributors
+# See also LICENSE.txt
+# $Id$
+"""Test harness"""
+
+import doctest
+import z3c.flashmessage
+import zope.app.wsgi.testlayer
+import zope.publisher.browser
+import zope.security.management
+
+
+FlashMessageLayer = zope.app.wsgi.testlayer.BrowserLayer(
+    z3c.flashmessage, allowTearDown=True)
+
+
+def setUp(test):
+    request = zope.publisher.browser.TestRequest()
+    zope.security.management.newInteraction()
+    interaction = zope.security.management.getInteraction()
+    interaction.add(request)
+
+
+def test_suite():
+    suite = doctest.DocFileSuite(
+        'README.rst',
+        optionflags=doctest.ELLIPSIS,
+        setUp=setUp)
+    suite.layer = FlashMessageLayer
+    return suite
