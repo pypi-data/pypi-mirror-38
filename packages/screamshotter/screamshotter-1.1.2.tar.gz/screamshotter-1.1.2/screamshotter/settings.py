bd1a267e9fd4ae7cb402f4250d7633da74f53b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:31 +0000

    Add package screamshotter/1.1.2/screamshotter-1.1.2.tar.gz

---
+++ packages/screamshotter/screamshotter-1.1.2.tar.gz/screamshotter-1.1.2/screamshotter/settings.py
@@ -0,0 +1,44 @@
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'booh!')
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*')]
+
+DEBUG = False
+
+ROOT_URLCONF = 'screamshotter.urls'
+
+INSTALLED_APPS = (
+    'screamshot',
+)
+
+DISK_CACHE_SIZE = 50 * 1000
+
+SCREAMSHOT_CONFIG = {
+    'CLI_ARGS': ['--ssl-protocol=any',
+                 '--disk-cache=true',
+                 '--max-disk-cache-size=%s' % DISK_CACHE_SIZE],
+}
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'simple': {'format': '%(levelname)s %(asctime)s %(name)s %(message)s'},
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': os.getenv('LOG_FILE', 'screamshotter.log'),
+        },
+    },
+    'loggers': {
+        'screamshot': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
