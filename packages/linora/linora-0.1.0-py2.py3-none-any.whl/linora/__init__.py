f847e6e8651b7393da05b54c87e0eec95968bb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:42 +0000

    Add package linora/0.1.0/linora-0.1.0-py2.py3-none-any.whl

---
+++ packages/linora/linora-0.1.0-py2.py3-none-any.whl/linora/__init__.py
@@ -0,0 +1,10 @@
+
+
+from linora import XGBClassifier
+from linora import XGBRanker
+from linora import XGBRegressor
+
+__version__ = '0.1.0'
+__author__ = 'JinQing Lee, Gaojie Wei'
+
+#__all__ = ['XGBClassifier', 'XGBRanker', 'XGBRegressor']
