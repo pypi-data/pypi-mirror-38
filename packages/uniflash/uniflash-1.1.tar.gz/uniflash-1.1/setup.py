a56914b0b37c020d17548a64e4e7b364105f21de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:08 +0000

    Add package uniflash/1.1/uniflash-1.1.tar.gz

---
+++ packages/uniflash/uniflash-1.1.tar.gz/uniflash-1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='uniflash',
+    version='1.1',
+    description='Reflash Logitech Unifying Receivers easily',
+    long_description=long_description,
+    author='athn',
+    author_email='jem@seethis.link',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyusb',
+        'intelhex',
+        'hexdump'
+    ]
+)
