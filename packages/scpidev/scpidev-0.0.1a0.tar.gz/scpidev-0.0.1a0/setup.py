e1342406b12e157e1c349d17cb13e4784907645e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:52 +0000

    Add package scpidev/0.0.1a0/scpidev-0.0.1a0.tar.gz

---
+++ packages/scpidev/scpidev-0.0.1a0.tar.gz/scpidev-0.0.1a0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='scpidev',
+    version='0.0.1a',
+    author='Simon Bertling',
+    author_email='simon.bertling@gmx.de',
+    description='A python package to turn your device into an SCPI end point',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/mrberti/pyscpidev',
+    license='MIT',
+    packages=['scpidev'],
+    #zip_safe=False,
+)
