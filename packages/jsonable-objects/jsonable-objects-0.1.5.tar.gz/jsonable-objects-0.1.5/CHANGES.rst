627042d7046da88fd5d13410ec3e8f2aac0b2074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package jsonable-objects/0.1.5/jsonable-objects-0.1.5.tar.gz

---
+++ packages/jsonable-objects/jsonable-objects-0.1.5.tar.gz/jsonable-objects-0.1.5/CHANGES.rst
@@ -0,0 +1,38 @@
+Changes
+=======
+
+0.1.5 (2018-11-11)
+------------------
+
+- typed mapping/sequence proxy.
+
+
+0.1.4 (2018-11-04)
+------------------
+
+- full field validation in ``__init__`` of the proxy classes.
+- loose field setting restriction if a format is given.
+
+
+0.1.3 (2018-11-04)
+------------------
+
+- Add attr-like decorator.
+
+
+0.1.2 (2018-10-31)
+------------------
+
+- Add `JsonableProxy.__repr__()`
+
+
+0.1.1 (2018-10-31)
+------------------
+
+- Correct COPYING files.
+
+
+0.1.0 (2018-10-31)
+------------------
+
+- Initial release.
---
+++ packages/jsonable-objects/jsonable-objects-0.1.5.tar.gz/jsonable-objects-0.1.5/setup.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+#
+#   jsonable-objects: JSON-able objects
+#   Copyright (C) 2015-2017 mete0r <mete0r@sarangbang.or.kr>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import with_statement
+from contextlib import contextmanager
+import io
+import os.path
+import re
+
+
+def setup_dir(f):
+    ''' Decorate f to run inside the directory where setup.py resides.
+    '''
+    setup_dir = os.path.dirname(os.path.abspath(__file__))
+
+    def wrapped(*args, **kwargs):
+        with chdir(setup_dir):
+            return f(*args, **kwargs)
+
+    return wrapped
+
+
+@contextmanager
+def chdir(new_dir):
+    old_dir = os.path.abspath(os.curdir)
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(old_dir)
+
+
+@setup_dir
+def import_setuptools():
+    try:
+        import setuptools
+        return setuptools
+    except ImportError:
+        pass
+
+    import ez_setup
+    ez_setup.use_setuptools()
+    import setuptools
+    return setuptools
+
+
+@setup_dir
+def readfile(path):
+    with io.open(path, encoding='utf-8') as f:
+        return f.read()
+
+
+@setup_dir
+def get_version():
+    source = readfile('src/jsonable_objects/__init__.py')
+    version_match = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]',
+                              source, re.M)
+    if not version_match:
+        raise RuntimeError('Unable to find version string.')
+    return version_match.group(1)
+
+
+def alltests():
+    import sys
+    import unittest
+    import zope.testrunner.find
+    import zope.testrunner.options
+    here = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    args = sys.argv[:]
+    defaults = ['--test-path', here]
+    options = zope.testrunner.options.get_options(args, defaults)
+    suites = list(zope.testrunner.find.find_suites(options))
+    return unittest.TestSuite(suites)
+
+
+install_requires_filename = 'requirements.in'
+install_requires = readfile(install_requires_filename)
+
+tests_require = readfile('requirements/test.in')
+
+
+setup_info = {
+    'name': 'jsonable-objects',
+    'version': get_version(),
+    'description': 'JSON-able objects',
+    'long_description': '\n'.join([readfile('README.rst'),
+                                   readfile('CHANGES.rst')]),
+
+    'author': 'mete0r',
+    'author_email': 'mete0r@sarangbang.or.kr',
+    'license': 'GNU Lesser General Public License v3 or later (LGPLv3+)',
+    'url': 'https://github.com/mete0r/jsonable-objects',
+
+    'packages': [
+        'jsonable_objects',
+    ],
+    # do not use '.'; just omit to specify setup.py directory
+    'package_dir': {
+        '': 'src',
+    },
+    'install_requires': install_requires,
+    'test_suite': '__main__.alltests',
+    'tests_require': tests_require,
+    'extras_require': {
+        'test': tests_require,
+    },
+    'classifiers': [
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',  # noqa
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    'keywords': [
+        'json',
+    ],
+    'zip_safe': False,
+}
+
+
+project_root_directory = os.path.abspath(os.path.dirname(__file__))
+requirements_path = 'requirements.txt'
+
+
+@setup_dir
+def main():
+    setuptools = import_setuptools()
+    setuptools.setup(**setup_info)
+
+
+if __name__ == '__main__':
+    main()
