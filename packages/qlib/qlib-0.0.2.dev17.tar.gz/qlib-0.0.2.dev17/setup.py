6db22b4abfa430e1126f7450165eb36e0dfdbc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:30 +0000

    Add package qlib/0.0.2.dev17/qlib-0.0.2.dev17.tar.gz

---
+++ packages/qlib/qlib-0.0.2.dev17.tar.gz/qlib-0.0.2.dev17/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='qlib',
+      version='0.0.2dev17',
+      author="Q Engineering Dev Team",
+      author_email="david@q.engineering",
+      description="A Q Library for Data Scientist",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/Q-Engineering/qlib",
+      packages=setuptools.find_packages(),
+      classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+      ],
+      project_urls={  # Optional
+         'Bug Reports': 'https://someurl',
+         'Funding': 'https://someurl',
+         'Subscriptions': 'http://someurl',
+         'Source': 'https://someurl',
+    },
+)
