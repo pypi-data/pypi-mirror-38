c6824f5cfd4ebf7e00cf5aaca643c1e098e8c38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:20 +0000

    Add package plp.CSharp/0.0.0a1/plp.CSharp-0.0.0a1.zip

---
+++ packages/plp.CSharp/plp.CSharp-0.0.0a1.zip/plp.CSharp-0.0.0a1/tests/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2003-2018 Adam Karpierz
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from __future__ import absolute_import
+
+__all__ = ('top_dir', 'test_dir')
+
+import sys, os, importlib
+if sys.version_info.major <= 2:  # pragma: no cover
+    sys.modules["unittest"] = importlib.import_module("unittest2")
+    sys.modules["unittest.mock"] = importlib.import_module("mock")
+sys.dont_write_bytecode = True
+test_dir = os.path.dirname(os.path.abspath(__file__))
+top_dir = os.path.dirname(test_dir)
+del sys, os, importlib
