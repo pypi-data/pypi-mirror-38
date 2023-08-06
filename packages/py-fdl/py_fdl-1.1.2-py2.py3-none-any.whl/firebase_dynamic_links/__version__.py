f178f4621b2597232d7a905f0dbe3edd74da751c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:05 +0000

    Add package py-fdl/1.1.2/py_fdl-1.1.2-py2.py3-none-any.whl

---
+++ packages/py-fdl/py_fdl-1.1.2-py2.py3-none-any.whl/firebase_dynamic_links/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'py-fdl'
+__description__ = 'Python client for Firebase Dynamic Links'
+__url__ = 'https://github.com/heykarimoff/py-fdl'
+__version__ = '1.1.2'
+__build__ = 0x010102
+__author__ = 'Mukhammad Karimov'
+__author_email__ = 'hey@karimoff.me'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Mukhammad Karimov'
+__keywords__ = 'firebase dynamic links super links'
