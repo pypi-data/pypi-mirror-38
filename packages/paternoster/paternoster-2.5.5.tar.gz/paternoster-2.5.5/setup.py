1b61bd48e0b29e36524d64e86e0c38253b23d18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:43 +0000

    Add package paternoster/2.5.5/paternoster-2.5.5.tar.gz

---
+++ packages/paternoster/paternoster-2.5.5.tar.gz/paternoster-2.5.5/setup.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    print("Paternoster needs setuptools.", file=sys.stderr)
+    print("Please install it using your package-manager or pip.", file=sys.stderr)
+    sys.exit(1)
+
+setup(name='paternoster',
+      version='2.5.5',
+      description='Paternoster provides users with the ability to run certain tasks as '
+                  'root or another user, while ensuring safety by providing a common '
+                  'interface and battle tested parameter parsing/checking.',
+      author='uberspace.de',
+      author_email='hallo@uberspace.de',
+      url='https://github.com/uberspace/paternoster',
+      packages=[
+          'paternoster',
+          'paternoster.runners',
+          'paternoster.types',
+      ],
+      entry_points = {
+          'console_scripts': ['paternoster=paternoster.shebang:main'],
+      },
+      install_requires=[
+          'tldextract>=2.0.1',
+          'six',
+      ],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: System Administrators',
+          'Topic :: System :: Systems Administration',
+          'Topic :: Security',
+          'Topic :: Utilities',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
+      ],
+      zip_safe=True,
+      )
