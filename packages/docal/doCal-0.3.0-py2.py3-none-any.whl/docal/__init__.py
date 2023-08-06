8e932928beb117b6b010a423408a4c6f39a3deaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:53 +0000

    Add package docal/0.3.0/doCal-0.3.0-py2.py3-none-any.whl

---
+++ packages/docal/doCal-0.3.0-py2.py3-none-any.whl/docal/__init__.py
@@ -0,0 +1,8 @@
+'''initializiation module'''
+
+from .parsing import eqn
+from .calculation import cal
+from .formatting import format_quantity as fmt
+from .document import document
+
+name = 'docal'
