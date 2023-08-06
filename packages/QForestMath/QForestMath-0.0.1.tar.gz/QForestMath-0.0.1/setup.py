4f0f63766343fe503b914f11c4bdd726e391fe56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:33 +0000

    Add package QForestMath/0.0.1/QForestMath-0.0.1.tar.gz

---
+++ packages/QForestMath/QForestMath-0.0.1.tar.gz/QForestMath-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QForestMath",
+    version="0.0.1",
+    author="Sashwat Anagolum",
+    author_email="sashwat.anagolum@gmail.com",
+    description="A small math wrapper for QISkit.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SashwatAnagolum/DoNew/tree/master/qforest_math",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
