41ca980ce89a8ce266c0ccf3d230f32b6c755219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:27 +0000

    Add package jputils/0.0.1/jputils-0.0.1.tar.gz

---
+++ packages/jputils/jputils-0.0.1.tar.gz/jputils-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jputils',
+      version='0.0.1',
+      description='util modules by Jade',
+      url='https://github.com/indiflex/jputils',
+      author='Jade IndiFlex',
+      author_email='indiflex.corp@gmail.com',
+      license='MIT',
+      packages=['jputils'],
+      install_requires=['requests'],
+      zip_safe=False)
