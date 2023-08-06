5a72a08e0e520ce19a4be3775136a388b8e415ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:49 +0000

    Add package dampr/0.1.2/dampr-0.1.2.tar.gz

---
+++ packages/dampr/dampr-0.1.2.tar.gz/dampr-0.1.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name='dampr',
+    version="0.1.2",
+    description="Data Processing implementation in Python",
+    url="https://www.github.com/Refefer/Dampr",
+    packages=find_packages(BASE),
+    test_suite="tests",
+    author='Andrew Stanton',
+    author_email="refefer@gmail.com",
+    classifiers=[
+       "Development Status :: 3 - Alpha",
+       "License :: OSI Approved :: Apache Software License",
+       "Programming Language :: Python :: 2",
+       "Programming Language :: Python :: 3",
+       "Operating System :: OS Independent"
+      ]
+    )
