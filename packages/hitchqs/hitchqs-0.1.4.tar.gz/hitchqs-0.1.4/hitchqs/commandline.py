fcd1a36ae222c287f61b9a2eb635e302f2859f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:49 +0000

    Add package hitchqs/0.1.4/hitchqs-0.1.4.tar.gz

---
+++ packages/hitchqs/hitchqs-0.1.4.tar.gz/hitchqs-0.1.4/hitchqs/commandline.py
@@ -0,0 +1,26 @@
+from path import Path
+import dirtemplate
+import os
+import sys
+
+
+TEMPLATE_DIR = Path(__file__).realpath().abspath().dirname() / "templates"
+
+
+def run():
+    arguments = sys.argv[1:]
+    assert len(arguments) == 1
+    template = arguments[0]
+    assert template in ["simplekey", "explainkey", "hitchstory", "tutorialhitchstory"]
+    if Path("hitch").exists():
+        print((
+            "Directory '{}' already exists here, "
+            "remove it to run quickstart again."
+        ).format("hitch"))
+        sys.exit(1)
+    cwd = Path(os.getcwd()).abspath()
+    dirtemplate.DirTemplate(
+        name="hitch", src=TEMPLATE_DIR / template, dest=cwd
+    ).ensure_built()
+    Path("builddb.sqlite").remove()
+    print("Quickstart run successfully!")
