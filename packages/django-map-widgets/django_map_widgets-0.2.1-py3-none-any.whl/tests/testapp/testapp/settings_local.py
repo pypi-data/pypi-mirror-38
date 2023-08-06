4816e13dbd9d4d19682d2187ad69094e8b45de29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:51 +0000

    Add package django-map-widgets/0.2.1/django_map_widgets-0.2.1-py3-none-any.whl

---
+++ packages/django-map-widgets/django_map_widgets-0.2.1-py3-none-any.whl/tests/testapp/testapp/settings_local.py
@@ -0,0 +1,12 @@
+GOOGLE_MAP_API_KEY = "AIzaSyC6BeCYCBSWDdC3snYRFKWw18bd9MA-uu4"
+
+
+MAP_WIDGETS = {
+    "GooglePointFieldWidget": (
+        ("zoom", 15),
+        ("mapCenterLocation", [51.5073509, -0.12775829999998223]),
+        ("markerFitZoom", 11),
+        ("GooglePlaceAutocompleteOptions", {'componentRestrictions': {'country': 'uk'}})
+    ),
+    "GOOGLE_MAP_API_KEY": GOOGLE_MAP_API_KEY,
+}
