435dd99e5089651255b5d2c7f2a4d4e0119f342c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:45 +0000

    Add package docxmerge-sdk/0.0.4/docxmerge_sdk-0.0.4-py3-none-any.whl

---
+++ packages/docxmerge-sdk/docxmerge_sdk-0.0.4-py3-none-any.whl/docxmerge_sdk/swagger_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from docxmerge_replace_me.api.api_api import ApiApi
+from docxmerge_replace_me.api.config_api import ConfigApi
+from docxmerge_replace_me.api.management_api import ManagementApi
+from docxmerge_replace_me.api.payments_api import PaymentsApi
+from docxmerge_replace_me.api.ping_api import PingApi
+from docxmerge_replace_me.api.templates_api import TemplatesApi
