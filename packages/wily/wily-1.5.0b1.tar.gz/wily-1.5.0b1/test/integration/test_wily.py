31c5c031ed3f68fccf483072533c7f76ea67cb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:17 +0000

    Add package wily/1.5.0b1/wily-1.5.0b1.tar.gz

---
+++ packages/wily/wily-1.5.0b1.tar.gz/wily-1.5.0b1/test/integration/test_wily.py
@@ -0,0 +1,41 @@
+import wily.__main__ as main
+from mock import patch
+from textwrap import dedent
+from click.testing import CliRunner
+from git import Repo, Actor
+import pathlib
+import pytest
+
+
+def test_list_metrics(builddir):
+    """
+    Test that list-metrics works with a build
+    """
+    with patch("wily.logger") as logger:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ["--path", builddir, "list-metrics"])
+        assert result.exit_code == 0, result.stdout
+
+
+def test_clean(builddir):
+    """ Test the clean feature """
+    with patch("wily.logger") as logger:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ["--path", builddir, "clean", "--yes"])
+        assert result.exit_code == 0, result.stdout
+    cache_path = pathlib.Path(builddir) / ".wily"
+    assert not cache_path.exists()
+
+
+def test_clean_no_cache(tmpdir):
+    with patch("wily.logger") as logger:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ["--path", tmpdir, "clean"])
+        assert result.exit_code == 1, result.stdout
+
+
+def test_list_metrics_no_cache(tmpdir):
+    with patch("wily.logger") as logger:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ["--path", tmpdir, "list-metrics"])
+        assert result.exit_code == 1, result.stdout
