08dbc56baad3bcc2d6af62bb7df9cd0ef60860d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:58 +0000

    Add package plpy-ext/0.0.1/plpy_ext-0.0.1.tar.gz

---
+++ packages/plpy-ext/plpy_ext-0.0.1.tar.gz/plpy_ext-0.0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='plpy_ext',
+    version='0.0.1',
+    author='Filantus',
+    author_email='filantus@mail.ru',
+    py_modules=['plpy_ext'],
+)
