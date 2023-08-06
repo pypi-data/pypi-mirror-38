48bdff3b3b4d1c8e16a63c3fed3811c6f9e73f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:06 +0000

    Add package datamaestro-text/0.0.1/datamaestro_text-0.0.1.tar.gz

---
+++ packages/datamaestro-text/datamaestro_text-0.0.1.tar.gz/datamaestro_text-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datamaestro_text',
+    version='0.0.1',
+    description='Text related datasets',
+    author='Benjamin Piwowarski',
+    author_email='benjamin@piwowarski.fr',
+    license='MIT',
+    python_requires='>=3.5',
+    packages=["datamaestro_text"],
+    install_requires=[
+        'datamaestro'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    entry_points={
+        'datamaestro.repositories': [
+            'text = datamaestro_text:Repository'
+        ]
+
+    }
+)
