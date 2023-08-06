97d9334475f0d1b1b81f2c28c5fcb2d3f62c1f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:52 +0000

    Add package choochoo/0.6.1/choochoo-0.6.1.tar.gz

---
+++ packages/choochoo/choochoo-0.6.1.tar.gz/choochoo-0.6.1/ch2/stoats/display/__init__.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+
+from ...squeal.tables.pipeline import Pipeline
+
+
+def build_pipeline(log, session, type, factory, date, schedule=None):
+    for cls, args, kargs in Pipeline.all(log, session, type):
+        log.info('Building %s (%s, %s)' % (cls, args, kargs))
+        yield from cls(log).build(session, factory, date, *args, schedule=schedule, **kargs)
+
+
+class Displayer:
+
+    def __init__(self, log):
+        self._log = log
+
+    @abstractmethod
+    def build(self, s, f, date, **kargs):
+        raise NotImplemented()
