a69c755821748561744989af52297c4712b412ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:54 +0000

    Add package volcano-twistedclient/0.2.3/volcano-twistedclient-0.2.3.tar.gz

---
+++ packages/volcano-twistedclient/volcano-twistedclient-0.2.3.tar.gz/volcano-twistedclient-0.2.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import json
+
+with open('requirements.txt') as f:
+    requires = list(filter(lambda x: not not x, map(lambda x: x.strip(), f.readlines())))
+
+with open('info.json') as f:
+    info = json.load(f)
+
+setup(
+    name='volcano-twistedclient',
+    version=info['version'],
+    description='Volcano client for Twisted',
+    author='Vinogradov D',
+    author_email='dgrapes@gmail.com',
+    license='MIT',
+    packages=['volcano.twistedclient'],
+    zip_safe=False,
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
