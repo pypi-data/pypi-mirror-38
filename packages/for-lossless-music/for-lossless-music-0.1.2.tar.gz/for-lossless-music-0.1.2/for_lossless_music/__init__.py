c4a5e5d4ef2529f983f82483d038ce15809c493a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:59 +0000

    Add package for-lossless-music/0.1.2/for-lossless-music-0.1.2.tar.gz

---
+++ packages/for-lossless-music/for-lossless-music-0.1.2.tar.gz/for-lossless-music-0.1.2/for_lossless_music/__init__.py
@@ -0,0 +1,23 @@
+
+from .moresound import Moresound, Source, str2source
+from .song import songs2table, FoundSong, foundsong2table
+from .download import download
+from .qqmusic import QQMusic
+from .song import Song
+from .quality import *
+
+__version__ = '0.1.2'
+
+
+def search(keyword, source: Source, page=1, num=20):
+    if source == Source.QQ:
+        return QQMusic.search(keyword, page, num)
+    else:
+        return Moresound.search(keyword, source, page, num)
+
+
+def get_download_urls(song: Song):
+    if song.source == Source.QQ:
+        return QQMusic.get_download_urls(song)
+    else:
+        return Moresound.get_download_urls(song)
