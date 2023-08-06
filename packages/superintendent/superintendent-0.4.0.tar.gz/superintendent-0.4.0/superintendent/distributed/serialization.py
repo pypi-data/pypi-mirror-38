33a7a674515c39f91a6e97ae6effd8527e477b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package superintendent/0.4.0/superintendent-0.4.0.tar.gz

---
+++ packages/superintendent/superintendent-0.4.0.tar.gz/superintendent-0.4.0/superintendent/distributed/serialization.py
@@ -0,0 +1,52 @@
+import json
+from typing import Any, Optional
+
+import numpy as np
+import pandas as pd
+
+
+class DataEncoder(json.JSONEncoder):
+    def default(self, obj):
+        """
+        Serialize numpy or pandas objects to json.
+        """
+        if isinstance(obj, np.ndarray):
+            return {"__type__": "__np.ndarray__", "__content__": obj.tolist()}
+        elif isinstance(obj, pd.DataFrame):
+            return {
+                "__type__": "__pd.DataFrame__",
+                "__content__": obj.to_dict(orient="split"),
+            }
+        elif isinstance(obj, pd.Series):
+            return {
+                "__type__": "__pd.Series__",
+                "__content__": {
+                    "dtype": str(obj.dtype),
+                    "index": list(obj.index),
+                    "data": obj.tolist(),
+                    "name": obj.name,
+                },
+            }
+
+
+def data_decoder(obj):
+    if "__type__" in obj:
+        if obj["__type__"] == "__np.ndarray__":
+            return np.array(obj["__content__"])
+        elif obj["__type__"] == "__pd.DataFrame__":
+            return pd.DataFrame(**obj["__content__"])
+        elif obj["__type__"] == "__pd.Series__":
+            return pd.Series(**obj["__content__"])
+    return obj
+
+
+def data_dumps(obj: Any) -> Optional[str]:
+    if obj is None:
+        return None
+    return json.dumps(obj, cls=DataEncoder)
+
+
+def data_loads(obj: Optional[str]) -> Any:
+    if obj is None:
+        return None
+    return json.loads(obj, object_hook=data_decoder)
