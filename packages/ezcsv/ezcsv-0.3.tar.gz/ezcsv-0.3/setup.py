8923468b21525825af2280153ab25ce2a240ce55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:40 +0000

    Add package ezcsv/0.3/ezcsv-0.3.tar.gz

---
+++ packages/ezcsv/ezcsv-0.3.tar.gz/ezcsv-0.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+import os
+
+# Get the long description from the README file
+with open(os.path.join(".", "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ezcsv",
+    version="0.3",
+    description="Functions to do obvious things with CSVs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Brandon Istenes",
+    author_email="brandonesbox@gmail.com",
+    url="https://github.com/brandones/ezcsv",
+    packages=["ezcsv"],
+    project_urls={"Source": "https://github.com/brandones/ezcsv"},
+)
