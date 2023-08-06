8b9cac967d07b055de82b17270f1eba123be7a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:07 +0000

    Add package eb2/0.0.2/eb2-0.0.2.tar.gz

---
+++ packages/eb2/eb2-0.0.2.tar.gz/eb2-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+	name='eb2',
+	version='0.0.2',
+	scripts=['eb2'],
+	author="Jonathas Hortense",
+	author_email="jonathas.hortense@daliaresearch.com",
+	description="Elastic Beanstalk ssh cli using the internal IP address",
+	# long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/jonathortense/homebrew-eb2",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'awsebcli',
+		'boto3'
+	],
+)
