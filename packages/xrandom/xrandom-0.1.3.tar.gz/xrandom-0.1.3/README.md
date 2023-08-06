672bc880351437888dd0e547b029fd6456df6b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:37 +0000

    Add package xrandom/0.1.3/xrandom-0.1.3.tar.gz

---
+++ packages/xrandom/xrandom-0.1.3.tar.gz/xrandom-0.1.3/README.md
@@ -0,0 +1,15 @@
+# xrandom
+
+Random something.
+
+## Installation
+
+```
+$ pip3 install xrandom
+```
+
+## Usage
+
+- `xrandom.salt()`
+- `first, last = xrandom.name()`
+- ...
---
+++ packages/xrandom/xrandom-0.1.3.tar.gz/xrandom-0.1.3/xrandom/xrandom.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+import random
+
+
+def salt(length: int = None):
+    length = length or random.randint(8, 12)
+    seed = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    s = []
+    for i in range(length):
+        s.append(random.choice(seed))
+    return ''.join(s)
+
+
+def man_name():
+    import requests
+    # https://randomuser.me/
+    r = requests.get('https://randomuser.me/api/?gender=male&nat=us')
+    name = r.json()['results'][0]['name']
+    # picture = r.json()['results'][0]['picture']['large'].strip()
+    first_name = name['first'].strip().capitalize()
+    last_name = name['last'].strip().capitalize()
+    return first_name, last_name
