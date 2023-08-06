b04579494f132ee9134d2a9a46af1f2f040f71c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:31 +0000

    Add package processcontroller/1.4.1.dev1/processcontroller-1.4.1.dev1.tar.gz

---
+++ packages/processcontroller/processcontroller-1.4.1.dev1.tar.gz/processcontroller-1.4.1.dev1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='processcontroller',
+    versioning='dev',
+    setup_requires='setupmeta',
+    url='https://yourlabs.io/oss/processcontroller',
+    description='Alternative pexpect for linux and python3',
+    author='Thomas Mignot',
+    author_email='tmig@yourlabs.org',
+    license='MIT',
+    keywords='pexpect subprocess',
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+    ],
+)
