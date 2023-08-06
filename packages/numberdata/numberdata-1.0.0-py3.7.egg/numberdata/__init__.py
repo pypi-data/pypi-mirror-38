a26bb9356be38aa587d8430821bfcf8382c5857a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:48 +0000

    Add package numberdata/1.0.0/numberdata-1.0.0-py3.7.egg

---
+++ packages/numberdata/numberdata-1.0.0-py3.7.egg/numberdata/__init__.py
@@ -0,0 +1,16 @@
+import math  
+  
+# print the square root of  0  
+
+def calculateSquareRoot(num):
+	return math.sqrt(num) 
+
+def calculateSquare(num):
+	square = num * num
+	return square	
+
+def calculateCube(num):
+	cube = num * num * num
+	return cube
+def calculatePower(num1,num2):
+	return num1 ** num2
