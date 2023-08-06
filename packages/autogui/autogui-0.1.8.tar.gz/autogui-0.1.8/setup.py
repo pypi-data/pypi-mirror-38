f1118b501959b53596459d9d6ae2f6f3a5cd8b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:52 +0000

    Add package autogui/0.1.8/autogui-0.1.8.tar.gz

---
+++ packages/autogui/autogui-0.1.8.tar.gz/autogui-0.1.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+libpath = r"autogui\src\GUILibrary\bin\Release"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autogui",
+    version="0.1.8",
+    author="Alex Lundberg,Richard Boettcher",
+    author_email="alex.lundberg@gmail.com,richard.boettcher@sandc.com",
+    description="Records and automates Winform and WPF applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lundbird/AutoGui",
+    packages=['autogui'],
+    package_data={'autogui':['src/GUILibrary/bin/Release/*.dll']},
+    classifiers=(
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ),
+    install_requires=['pythonnet'],
+    license='MIT',
+)
