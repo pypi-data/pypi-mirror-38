aea1325003eb5d0d0ee05060b32d02f8b31d9715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:09 +0000

    Add package featexp/0.0.5/featexp-0.0.5.tar.gz

---
+++ packages/featexp/featexp-0.0.5.tar.gz/featexp-0.0.5/featexp/__init__.py
@@ -0,0 +1,7 @@
+from featexp.base import get_grouped_data
+from featexp.base import get_trend_correlation
+from featexp.base import univariate_plotter
+from featexp.base import draw_plots
+from featexp.base import get_trend_stats
+from featexp.base import get_trend_changes
+from featexp.base import get_univariate_plots
