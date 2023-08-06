ff826dbed677354b96a7169bdf35e7d1dcad96ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:07 +0000

    Add package hw-nfr-cs410-pip-test/0.0.4/hw_nfr_cs410_pip_test-0.0.4-py3-none-any.whl

---
+++ packages/hw-nfr-cs410-pip-test/hw_nfr_cs410_pip_test-0.0.4-py3-none-any.whl/hw_nfr_cs410_pip_test/__init__.py
@@ -0,0 +1,4 @@
+from .feature_mining import feature_mining
+
+name = "hw_nfr_cs410_pip_test"
+
---
+++ packages/hw-nfr-cs410-pip-test/hw_nfr_cs410_pip_test-0.0.4-py3-none-any.whl/hw_nfr_cs410_pip_test/hw_nfr_cs410_pip_test.py
@@ -0,0 +1,10 @@
+class feature_mining:
+    """
+    Documentation, important, blablabla.
+    """
+    def say_hello(self):
+        """
+        Say hello function, just prints hello.
+        :return: nothing
+        """
+        print('Hello')
