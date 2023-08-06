737708db19cf047adc3404aa1e7a606a7b6a7335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:57 +0000

    Add package hw-nfr-cs410-pip-test/0.0.1/hw_nfr_cs410_pip_test-0.0.1.tar.gz

---
+++ packages/hw-nfr-cs410-pip-test/hw_nfr_cs410_pip_test-0.0.1.tar.gz/hw_nfr_cs410_pip_test-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Example Package - hw_nfr_cs410_pip
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ packages/hw-nfr-cs410-pip-test/hw_nfr_cs410_pip_test-0.0.1.tar.gz/hw_nfr_cs410_pip_test-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hw_nfr_cs410_pip_test",
+    version="0.0.1",
+    author="H.W., N.F",
+    author_email="norbiein@gmail.com",
+    description="Implicit feature mining",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nfreundlich/CS410_CourseProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
