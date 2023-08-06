bac6553f6962c54426a6bb7ea1af3a8fc0b21ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:21 +0000

    Add package apikeystore/0.0.9/apikeystore-0.0.9-py3-none-any.whl

---
+++ packages/apikeystore/apikeystore-0.0.9-py3-none-any.whl/apikeystore/__version__.py
@@ -0,0 +1,9 @@
+
+
+__title__ = "apikeystore"
+__version__ = '0.0.9'
+__author__ = 'Eugene Wright'
+__author_email__ = "eugene@ewright3.com"
+__license__ = "MIT"
+__copyright__ = 'Copyright 2018 Eugene Wright'
+__homepage__ = "https://bitbucket.org/ewright3/apikeystore/"
