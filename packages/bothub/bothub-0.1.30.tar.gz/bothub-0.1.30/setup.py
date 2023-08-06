60ba585b61f739851be4ca01ede5c67d034c6b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:14 +0000

    Add package bothub/0.1.30/bothub-0.1.30.tar.gz

---
+++ packages/bothub/bothub-0.1.30.tar.gz/bothub-0.1.30/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+from codecs import open
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='bothub',
+    version='0.1.30',
+    description=u'Build serverless chatbot on BotHub.Studio',
+    long_description=LONG_DESCRIPTION,
+    author='Jeongsoo Park',
+    author_email='jeongsoo.park@bothub.studio',
+    url='https://github.com/bothub-studio/bothub-sdk-python',
+    license="AGPL v3",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'zmq',
+        'pyaml',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+        'requests-mock',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Chat',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Utilities'
+    ],
+    keywords='bothub serverless chatbot framework',
+)
