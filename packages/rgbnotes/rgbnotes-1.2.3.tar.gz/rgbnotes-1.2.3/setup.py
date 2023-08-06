72f249a72d1a4aae3f2ce1e8d7cacc18a46fc740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:46 +0000

    Add package rgbnotes/1.2.3/rgbnotes-1.2.3.tar.gz

---
+++ packages/rgbnotes/rgbnotes-1.2.3.tar.gz/rgbnotes-1.2.3/setup.py
@@ -0,0 +1,26 @@
+import sys
+from setuptools import setup, find_packages
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+
+setup(name='rgbnotes',
+      version='1.2.3',
+      description='Python bindings for the RGB Notes API',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      author='rgbnotes',
+      author_email='support@rgbnotes.com',
+      url='https://github.com/rgbnotes/rgb-python',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'requests >= 0.8.8',
+      ])
