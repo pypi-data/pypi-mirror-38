6ee6ea562a46f65812223c34c802838a32bc850e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:16 +0000

    Add package menzalib/0.4.19/menzalib-0.4.19.tar.gz

---
+++ packages/menzalib/menzalib-0.4.19.tar.gz/menzalib-0.4.19/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+      name="menzalib",
+      version="0.4.19",
+      author="Lettera, Francesco215",
+      author_email="",
+      description="Funzioni utili per lab3",
+      url="",
+      packages=setuptools.find_packages(),
+      )
