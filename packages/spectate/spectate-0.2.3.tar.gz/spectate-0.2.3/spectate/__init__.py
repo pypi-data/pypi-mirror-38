c6e5b6f87940bf3289164d72d2748e8731ea3c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:50 +0000

    Add package spectate/0.2.3/spectate-0.2.3.tar.gz

---
+++ packages/spectate/spectate-0.2.3.tar.gz/spectate-0.2.3/spectate/__init__.py
@@ -0,0 +1,16 @@
+"""
+.. currentmodule:: spectate
+
+Spectate
+========
+
+.. automodule:: spectate.spectate
+    :members:
+    :ignore-module-all:
+
+.. automodule:: spectate.mvc
+    :members:
+"""
+
+from .spectate import *
+from ._version import __version__
