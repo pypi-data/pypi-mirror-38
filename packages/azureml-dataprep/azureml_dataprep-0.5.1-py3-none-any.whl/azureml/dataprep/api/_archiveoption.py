acebdd7aff398abf863ecaabe4124a378dc6266d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:35 +0000

    Add package azureml-dataprep/0.5.1/azureml_dataprep-0.5.1-py3-none-any.whl

---
+++ packages/azureml-dataprep/azureml_dataprep-0.5.1-py3-none-any.whl/azureml/dataprep/api/_archiveoption.py
@@ -0,0 +1,14 @@
+from enum import Enum
+from .engineapi.typedefinitions import ArchiveType
+
+
+class ArchiveOptions:
+    def __init__(self,
+                 archive_type: ArchiveType,
+                 entry_glob: str = None):
+        """
+        :param archive_type: The type of archive file.
+        :param entry_glob: The glob pattern for entries in archive file.
+        """
+        self.archive_type = archive_type
+        self.entry_glob = entry_glob
