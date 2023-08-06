40db3afe1f3412863393bb5a51a82915ba5a891e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:15 +0000

    Add package peddy/0.4.3/peddy-0.4.3.tar.gz

---
+++ packages/peddy/peddy-0.4.3.tar.gz/peddy-0.4.3/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup
+
+def get_version():
+    """Get the version info from the mpld3 package without importing it"""
+    import ast
+
+    with open(os.path.join("peddy", "__init__.py"), "r") as init_file:
+        module = ast.parse(init_file.read())
+
+    version = (ast.literal_eval(node.value) for node in ast.walk(module)
+             if isinstance(node, ast.Assign)
+             and node.targets[0].id == "__version__")
+    try:
+        return next(version)
+    except StopIteration:
+        raise ValueError("version could not be located")
+
+setup(
+    version=get_version(),
+    name='peddy',
+    description="pleasingly pythonic pedigree manipulation",
+    packages=['peddy', 'peddy.tests'],
+    long_description=open('README.md').read(),
+    author="Brent Pedersen",
+    author_email="bpederse@gmail.com",
+    zip_safe=False,
+    test_suite='nose.collector',
+    # To provide executable scripts, use entry points in preference to the
+       # "scripts" keyword. Entry points provide cross-platform support and
+       # allow pip to create the appropriate form of executable for the
+       # target platform.
+    entry_points=dict(
+        console_scripts=[
+            'peddy = peddy.__main__:cli',
+        ],
+    ),
+    include_package_data=True,
+    tests_require='nose',
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Scientific/Engineering :: Bio-Informatics']
+)
