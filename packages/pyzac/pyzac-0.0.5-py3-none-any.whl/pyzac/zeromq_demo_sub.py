bf5d1f2f7e2ad0ffe43c93aa53a2588041ebde2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:41 +0000

    Add package pyzac/0.0.5/pyzac-0.0.5-py3-none-any.whl

---
+++ packages/pyzac/pyzac-0.0.5-py3-none-any.whl/pyzac/zeromq_demo_sub.py
@@ -0,0 +1,14 @@
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+# We can connect to several endpoints if we desire, and receive from all.
+socket.connect("tcp://127.0.0.1:2000")
+
+# We must declare the socket as of type SUBSCRIBER, and pass a prefix filter.
+# Here, the filter is the empty string, wich means we receive all messages.
+# We may subscribe to several filters, thus receiving from all.
+socket.setsockopt(zmq.SUBSCRIBE, "")
+
+message = socket.recv_pyobj()
+print(message.get(1)[2])
