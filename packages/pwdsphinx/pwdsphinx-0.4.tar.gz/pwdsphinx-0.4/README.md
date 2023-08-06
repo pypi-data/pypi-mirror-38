f4e7e4aff361f02d48f02ed44eaa7c747804f7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:15 +0000

    Add package pwdsphinx/0.4/pwdsphinx-0.4.tar.gz

---
+++ packages/pwdsphinx/pwdsphinx-0.4.tar.gz/pwdsphinx-0.4/README.md
@@ -0,0 +1,201 @@
+sphinx: a password **S**tore that **P**erfectly **H**ides from **I**tself
+(**N**o **X**aggeration)
+
+pwdsphinx is python wrapper around libsphinx - a cryptographic password storage
+as described in https://eprint.iacr.org/2015/1099
+
+## Dependencies
+
+You need [libsphinx](https://github.com/stef/libsphinx).
+
+You need also to install `pysodium` using either your OS package
+manager or pip.
+
+If you want to use also the websphinx browser extension you need to
+install also an X11 variant of pinentry from the gnupg project:
+
+ - either `apt-get install pinentry-qt`
+ - or `apt-get install pinentry-gtk2`
+ - or `apt-get install pinentry-gnome3`
+ - or `apt-get install pinentry-fltk`
+
+(or anything equivalent to `apt-get install` on your OS)
+
+## Installation
+
+`pip3 install pwdsphinx` should get you started.
+
+## API
+
+`sphinxlib` is a `ctypes`-based python wrapper around [libsphinx](https://github.com/stef/libsphinx), so
+you can build whatever you fancy immediately in python. The interface
+exposed wraps the 3 sphinx functions from the library like this:
+
+```
+def challenge(pwd)
+```
+
+returns bfac and chal
+
+```
+def respond(chal, secret)
+```
+return the response
+
+```
+def finish(pwd, bfac, resp)
+```
+
+returns the raw 32 byte password.
+
+The functions for the PAKE (OPAQUE) protocol are not yet exposed.
+
+## Server/Client
+
+Since the sphinx protocol only makes sense if the "device" is
+somewhere else than where you type your password, pitchforked sphinx
+comes with a server implemented in py3 which you can host off-site
+from your usual desktop/smartphone. Also a client is supplied which is
+able to communicate with the server and manage passwords.
+
+Both the client and the server can be configured by any of the
+following files:
+ - `/etc/sphinx/config`
+ - `~/.sphinxrc`
+ - `~/.config/sphinx/config`
+ - `./sphinx.cfg`
+
+Files are parsed in this order, this means global settings can be
+overridden by per-user and per-directory settings.
+
+### oracle - the server
+
+The server can be "configured" by changing the variables in the
+`[server]` section of the config file.
+
+The `address` is the IP address on which the server is listening,
+default is `localhost` - you might want to change that.
+
+The `port` where the server is listening is by default 2355.
+
+`datadir` specifies the data directory where all the device "secrets"
+are stored, this defaults to "data/" in the current directory. You
+might want to back up this directory from time to time to an encrypted
+medium.
+
+`verbose` enables logging to standard output.
+
+`keydir` is the directory where the server stores its secret key, that
+is used to sign messages to the clients.
+
+Change these settings to fit your needs. Starting the server
+can be done simply by:
+
+```
+./oracle.py
+```
+
+### sphinx - the client
+
+This is the client that connects to the oracle to manage passwords
+using the sphinx protocol.
+
+#### Client Configuration
+
+Like the server, the client can be configured changing the settings in
+the `[client]` section of the config file. The `host` and `port` should
+match what you set in the server.
+
+The datadir (default: `~/.sphinx`) variable holds the location for
+your client parameters. Particularly it contains a salt (by default
+`~/.sphinx/salt`) which is used to calculate the ids for secrets on
+the server, and more importantly it also contains a secret key
+(default: `~/.sphinx/key`) that is used to sign every message sent to
+the server to authorize the operations on your passwords. Both the
+salt and the key is generated automatically if not available. You
+might want to back up and encrypt both the salt and the key.
+
+#### Authorization
+
+All operations are authenticated by your (default: `~/.sphinx/key`)
+file which is used to sign all operations. You should protect this
+file, so that only you can operate on your passwords.
+
+#### Operations
+
+The client provides the following operations: Create, Get, Change,
+Delete. Note there is no command to list "records", as the server does
+not contain any textual information about what it stores. All
+operations need a username and a site this password belongs to.
+
+#### Create password
+
+Creating a new password for a site is easy, pass your "master"
+password on standard input to the client, and provide parameters like
+in this example:
+
+```
+echo 'my master password' | ./sphinx.py create username https://example.com ulsd 0
+```
+
+The parameters to the client are `create` for the operation, then
+`username` for the username on the site `https://example.com` then a
+combination of the letters `ulsd` and the `0` for the size of the
+final password. The letters `ulsd` stand in order for the following
+character classes: `u` upper-case letters, `l` lower-case letters, `s`
+symbols and `d` for digits.
+
+Note, you can actually use different "master" passwords for different
+user/site combinations.
+
+#### Get password
+
+Getting a password from the sphinx oracle works by running the
+following command:
+
+```
+echo 'my master password' | ./sphinx.py get username https://example.com
+```
+
+Here again you supply your master password on standard input, provide
+the `get` operation as the first parameter, your `username` as the 2nd
+and the `site` as the 3rd parameter. The resulting password is
+returned on standard output.
+
+#### Change password
+
+You might want to (be forced to regularly) change your password, this
+is easy while you can keep your master password the unchanged (or you
+can change it too, if you want). The command is this:
+
+```
+echo 'my master password' | ./sphinx.py change username https://example.com
+```
+
+Here again you supply your master password on standard input. This
+master password can be the same, but can also be a new password if you
+want to change also the master password. You provide the `change`
+operation as the first parameter to the client, your `username` as the
+2nd and the `site` as the 3rd parameter. Your new new password is
+returned on standard output.
+
+After changing the password, you will get back the old password, until you commit the changes with
+
+```
+echo 'my master password' | ./sphinx.py commit username https://example.com
+```
+
+#### Deleting passwords
+
+In case you want to delete a password, you can do using the following
+command:
+
+```
+./sphinx.py delete username https://example.com
+```
+
+You provide the `delete` operation as the first parameter to the
+client, your `username` as the 2nd and the `site` as the 3rd
+parameter. This command does not need anything on standard input, nor
+does it provide anything on standard output in case everything goes
+well.
---
+++ packages/pwdsphinx/pwdsphinx-0.4.tar.gz/pwdsphinx-0.4/pwdsphinx/sphinx.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python3
+
+import sys, os, asyncio, io, struct, binascii, platform
+import pysodium
+
+try:
+  from pwdsphinx import bin2pass, sphinxlib
+  from pwdsphinx.config import getcfg
+except ImportError:
+  import bin2pass, sphinxlib
+  from config import getcfg
+
+win=False
+if platform.system() == 'Windows':
+  win=True
+
+cfg = getcfg('sphinx')
+
+verbose = cfg['client'].getboolean('verbose')
+address = cfg['client']['address']
+port = cfg['client']['port']
+datadir = cfg['client']['datadir']
+
+CREATE=b'\x00'
+GET=b'\x66'
+COMMIT=b'\x99'
+CHANGE=b'\xaa'
+DELETE=b'\xff'
+
+class SphinxClientProtocol(asyncio.Protocol):
+  def __init__(self, message, loop,b,pwd,handler,cb):
+    self.b = b
+    self.pwd=pwd
+    self.message = message
+    self.loop = loop
+    self.handler = handler
+    self.cb = cb
+
+  def connection_made(self, transport):
+    transport.write(self.message)
+    if verbose: print('Data sent: {!r}'.format(self.message))
+
+  def data_received(self, data):
+    if verbose: print('Data received: ', data)
+
+    try:
+      data = pysodium.crypto_sign_open(data, self.handler.getserverkey())
+    except ValueError:
+      raise ValueError('invalid signature.\nabort')
+
+    if data!=b'ok' and (data[:-42] == b'fail' or len(data)!=sphinxlib.DECAF_255_SER_BYTES+42):
+        raise ValueError('fail')
+
+    if not self.b:
+      self.cb()
+      return
+
+    rwd=sphinxlib.finish(self.pwd, self.b, data[:sphinxlib.DECAF_255_SER_BYTES])
+
+    if self.handler.namesite is not None:
+      if self.handler.namesite['name'].encode() not in self.handler.list(self.handler.namesite['site']):
+        self.handler.cacheuser(self.handler.namesite)
+
+    rule = data[sphinxlib.DECAF_255_SER_BYTES:]
+    if len(rule)!=42:
+      raise ValueError('fail')
+    rk = pysodium.crypto_generichash(self.handler.getkey(),self.handler.getsalt())
+    rule = pysodium.crypto_secretbox_open(rule[24:], rule[:24],rk)
+    rule = struct.unpack(">H",rule)[0]
+    size = (rule & 0x7f)
+    rule = {c for i,c in enumerate(('u','l','s','d')) if (rule >> 7) & (1 << i)}
+    self.cb(bin2pass.derive(rwd,rule,size).decode())
+
+  def connection_lost(self, exc):
+    if verbose:
+        print('The server closed the connection')
+        print('Stop the event loop')
+    self.loop.stop()
+
+class SphinxHandler():
+  def __init__(self, datadir):
+    self.datadir=datadir
+    self.namesite = None
+
+  def getkey(self):
+    datadir = os.path.expanduser(self.datadir)
+    try:
+      fd = open(datadir+'key', 'rb')
+      key = fd.read()
+      fd.close()
+      return key
+    except FileNotFoundError:
+      if not os.path.exists(datadir):
+        os.mkdir(datadir,0o700)
+      pk, sk = pysodium.crypto_sign_keypair()
+      with open(datadir+'key','wb') as fd:
+        if not win: os.fchmod(fd.fileno(),0o600)
+        fd.write(sk)
+      return sk
+
+  def getsalt(self):
+    datadir = os.path.expanduser(self.datadir)
+    try:
+      fd = open(datadir+'salt', 'rb')
+      salt = fd.read()
+      fd.close()
+      return salt
+    except FileNotFoundError:
+      if not os.path.exists(datadir):
+        os.mkdir(datadir,0o700)
+      salt = pysodium.randombytes(32)
+      with open(datadir+'salt','wb') as fd:
+        if not win: os.fchmod(fd.fileno(),0o600)
+        fd.write(salt)
+      return salt
+
+  def getusers(self, id):
+    datadir = os.path.expanduser(self.datadir)
+    try:
+      with open(datadir+binascii.hexlify(id).decode(), 'rb') as fd:
+        return [x.strip() for x in fd.readlines()]
+    except FileNotFoundError:
+        return None
+
+  def deluser(self, id, user):
+    userfile = os.path.expanduser(self.datadir+binascii.hexlify(id).decode())
+    try:
+      with open(userfile, 'rb') as fd:
+        users=[x.strip() for x in fd.readlines() if x.strip() != user.encode()]
+      if users != []:
+        with open(userfile, 'wb') as fd:
+            # skip rules
+            fd.write(rules)
+            fd.write(b'\n'.join(users))
+      else:
+        os.unlink(userfile)
+    except FileNotFoundError:
+      return None
+
+  def cacheuser(self, namesite):
+    site=namesite['site']
+    user=namesite['name']
+    salt = self.getsalt()
+    hostid = pysodium.crypto_generichash(site, salt, 32)
+    userfile = os.path.expanduser(self.datadir+binascii.hexlify(hostid).decode())
+    try:
+      if not os.path.exists(userfile):
+        with open(userfile, 'wb') as fd:
+            fd.write(user.encode())
+      else:
+        with open(userfile, 'ab') as fd:
+            fd.write(b"\n"+user.encode())
+    except:
+      pass
+
+  def getserverkey(self):
+    datadir = os.path.expanduser(self.datadir)
+    try:
+      with open(datadir+'server-key.pub', 'rb') as fd:
+        key = fd.read()
+      return key
+    except FileNotFoundError:
+      pass
+    # try in installation dir
+    BASEDIR = os.path.dirname(os.path.abspath(__file__))
+    try:
+      with open(BASEDIR+'/server-key.pub', 'rb') as fd:
+        key = fd.read()
+      return key
+    except FileNotFoundError:
+      print("no server key found, please install it")
+      sys.exit(1)
+
+  def getid(self, host, user):
+    salt = self.getsalt()
+    return pysodium.crypto_generichash(b''.join((user.encode(),host.encode())), salt, 32)
+
+  def doSphinx(self, message, host, b, pwd, cb):
+    self.hostid=pysodium.crypto_generichash(host, self.getsalt(), 32)
+    signed=pysodium.crypto_sign(message,self.getkey())
+    loop = asyncio.get_event_loop()
+    coro = loop.create_connection(lambda: SphinxClientProtocol(signed, loop, b, pwd, self, cb), address, port)
+    try:
+      loop.run_until_complete(coro)
+      loop.run_forever()
+    except:
+      raise
+
+  def create(self, cb, pwd, user, host, char_classes, size=0):
+    if set(char_classes) - {'u','l','s','d'}:
+      raise ValueError("error: rules can only contain ulsd.")
+    try: size=int(size)
+    except:
+      raise ValueError("error: size has to be integer.")
+    self.namesite={'name': user, 'site': host}
+
+    rules = sum(1<<i for i, c in enumerate(('u','l','s','d')) if c in char_classes)
+    # pack rule
+    rule=struct.pack('>H', (rules << 7) | (size & 0x7f))
+    # encrypt rule
+    sk = self.getkey()
+    rk = pysodium.crypto_generichash(sk,self.getsalt())
+    nonce = pysodium.randombytes(pysodium.crypto_secretbox_NONCEBYTES)
+    rule = nonce+pysodium.crypto_secretbox(rule,nonce,rk)
+
+    b, c = sphinxlib.challenge(pwd)
+    message = b''.join([CREATE,
+                        self.getid(host, user),
+                        c,
+                        rule,
+                        pysodium.crypto_sign_sk_to_pk(sk)])
+    self.doSphinx(message, host, b, pwd, cb)
+
+  def get(self, cb, pwd, user, host):
+    b, c = sphinxlib.challenge(pwd)
+    self.namesite={'name': user, 'site': host}
+    message = b''.join([GET,
+                        self.getid(host, user),
+                        c])
+    self.doSphinx(message, host, b, pwd, cb)
+
+  def change(self, cb, pwd, user, host):
+    b, c = sphinxlib.challenge(pwd)
+    self.namesite={'name': user, 'site': host}
+    message = b''.join([CHANGE,
+                        self.getid(host, user),
+                        c])
+    self.doSphinx(message, host, b, pwd, cb)
+
+  def commit(self, cb, user, host):
+    message = b''.join([COMMIT,self.getid(host, user)])
+    salt = self.getsalt()
+    self.namesite={'name': user, 'site': host}
+    def callback():
+      return
+    self.doSphinx(message, host, None, None, callback)
+
+  def delete(self, user, host):
+    message = b''.join([DELETE,self.getid(host, user)])
+    salt = self.getsalt()
+    hostid = pysodium.crypto_generichash(host, salt, 32)
+    def callback():
+      self.deluser(hostid,user)
+    self.doSphinx(message, host, None, None, callback)
+
+  def list(self, host):
+    salt = self.getsalt()
+    hostid = pysodium.crypto_generichash(host, salt, 32)
+    return self.getusers(hostid) or []
+
+def main():
+  def usage():
+    print("usage: %s create <user> <site> [u][l][d][s] [<size>]" % sys.argv[0])
+    print("usage: %s <get|change|commit|delete> <user> <site>" % sys.argv[0])
+    print("usage: %s list <site>" % sys.argv[0])
+    sys.exit(1)
+
+  if len(sys.argv) < 2: usage()
+
+  handler = SphinxHandler(datadir)
+
+  if sys.argv[1] == 'create':
+    if len(sys.argv) not in (5,6): usage()
+    pwd = sys.stdin.buffer.read()
+    if len(sys.argv) == 6:
+      size=sys.argv[5]
+    else:
+      size = 0
+    handler.create(print, pwd, sys.argv[2], sys.argv[3], sys.argv[4], size)
+  elif sys.argv[1] == 'get':
+    if len(sys.argv) != 4: usage()
+    # needs id, challenge, sig(id)
+    pwd = sys.stdin.buffer.read()
+    handler.get(print, pwd, sys.argv[2], sys.argv[3])
+  elif sys.argv[1] == 'change':
+    if len(sys.argv) != 4: usage()
+    # needs id, challenge, sig(id)
+    pwd = sys.stdin.buffer.read()
+    handler.change(print, pwd, sys.argv[2], sys.argv[3])
+  elif sys.argv[1] == 'commit':
+    if len(sys.argv) != 4: usage()
+    handler.commit(print, sys.argv[2], sys.argv[3])
+  elif sys.argv[1] == 'delete':
+    if len(sys.argv) != 4: usage()
+    handler.delete(sys.argv[2], sys.argv[3])
+  elif sys.argv[1] == 'list':
+    if len(sys.argv) != 3: usage()
+    print(b'\n'.join(handler.list(sys.argv[2])).decode())
+  else:
+    usage()
+
+if __name__ == '__main__':
+  main()
