4f782f80fec9fb73e45518089baa11bdc113236c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:05 +0000

    Add package plone.alterego/1.1.2/plone.alterego-1.1.2.tar.gz

---
+++ packages/plone.alterego/plone.alterego-1.1.2.tar.gz/plone.alterego-1.1.2/CHANGES.rst
@@ -0,0 +1,46 @@
+Changelog
+=========
+
+.. You should *NOT* be adding new change log entries to this file.
+   You should create a file in the news directory instead.
+   For helpful instructions, please see:
+   https://github.com/plone/plone.releaser/blob/master/ADD-A-NEWS-ITEM.rst
+
+.. towncrier release notes start
+
+1.1.2 (2018-11-21)
+------------------
+
+Bug fixes:
+
+- Update code to follow Plone styleguide.
+  [gforcada]
+
+1.1 (2016-11-01)
+----------------
+
+New features:
+
+- Add compatibility with Python 3. [datakurre]
+
+
+1.0.1 (2016-08-11)
+------------------
+
+Fixes:
+
+- Use zope.interface decorator.
+  [gforcada]
+
+
+1.0 (2011-04-30)
+----------------
+
+- Use doctest from the stdlib instead of from zope.testing
+  [davisagli]
+
+
+1.0a1 (2009-04-17)
+------------------
+
+- Initial release.
---
+++ packages/plone.alterego/plone.alterego-1.1.2.tar.gz/plone.alterego-1.1.2/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '1.1.2'
+
+setup(
+    name='plone.alterego',
+    version=version,
+    description='Low level support for dynamic modules',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.rst').read()),
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.0',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='Plone schema interface',
+    author='Laurence Rowe',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/plone.alterego',
+    license='LGPL',
+    packages=find_packages(),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.component',
+        'zope.interface',
+    ],
+    extras_require={
+        'test': []
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
