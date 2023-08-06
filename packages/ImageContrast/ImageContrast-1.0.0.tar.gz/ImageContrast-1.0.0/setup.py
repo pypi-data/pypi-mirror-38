ce42224ac27e8450d5303a8d618b03dec3c5f508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:20 +0000

    Add package ImageContrast/1.0.0/ImageContrast-1.0.0.tar.gz

---
+++ packages/ImageContrast/ImageContrast-1.0.0.tar.gz/ImageContrast-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'ImageContrast',
+    version = '1.0.0',
+    py_modules = ['ImageContrast'],
+    author = 'luoyanchao',
+    author_email = 'luoyanchao2007@163.com',
+    description = 'Screenshots and contrast, the difference between figure and return value similarity ',
+    )
