1e3ca1fe84a11ad3febe10a3a8751c03b1860714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:08 +0000

    Add package asphalt-serialization/4.0.3/asphalt_serialization-4.0.3-py3-none-any.whl

---
+++ packages/asphalt-serialization/asphalt_serialization-4.0.3-py3-none-any.whl/asphalt/serialization/component.py
@@ -0,0 +1,67 @@
+import logging
+from typing import Dict, Any, Optional, List, Tuple  # noqa: F401
+
+from asphalt.core import Component, Context, PluginContainer, merge_config
+from typeguard import check_argument_types
+
+from asphalt.serialization.api import Serializer, CustomizableSerializer
+
+serializer_types = PluginContainer('asphalt.serialization.serializers', Serializer)
+logger = logging.getLogger(__name__)
+
+
+class SerializationComponent(Component):
+    """
+    Creates one or more serializer resources.
+
+    Every serializer resource will be available in the context as the following types:
+
+    * :class:`~asphalt.serialization.api.Serializer`
+    * :class:`~asphalt.serialization.api.CustomizableSerializer` (if the serializer implements it)
+    * its actual type
+
+    Serializers can be configured in two ways:
+
+    #. a single serializer, with configuration supplied directly as keyword arguments to this
+        component's constructor (with the resource name being ``default`` and the context attribute
+        matching the backend name)
+    #. multiple serializers, by providing the ``serializers`` option where each key is the resource
+        name and each value is a dictionary containing that serializer's configuration (with the
+        context attribute matching the resource name by default)
+
+    Each serializer configuration has two special options that are not passed to the constructor of
+    the backend class:
+
+    * backend: entry point name of the serializer backend class (required)
+    * context_attr: name of the context attribute of the serializer resource
+
+    :param serializers: a dictionary of resource name ⭢ constructor arguments for the chosen
+        backend class
+    :param default_serializer_args: default values for constructor keyword arguments
+    """
+
+    def __init__(self, serializers: Dict[str, Optional[Dict[str, Any]]] = None,
+                 **default_serializer_args) -> None:
+        assert check_argument_types()
+        if not serializers:
+            default_serializer_args.setdefault(
+                'context_attr', default_serializer_args.get('backend'))
+            serializers = {'default': default_serializer_args}
+
+        self.serializers = []  # type: List[Tuple]
+        for resource_name, config in serializers.items():
+            merged = merge_config(default_serializer_args, config or {})
+            type_ = merged.pop('backend', resource_name)
+            context_attr = merged.pop('context_attr', resource_name)
+            serializer = serializer_types.create_object(type_, **merged)
+            self.serializers.append((resource_name, context_attr, serializer))
+
+    async def start(self, ctx: Context):
+        for resource_name, context_attr, serializer in self.serializers:
+            types = [Serializer, type(serializer)]
+            if isinstance(serializer, CustomizableSerializer):
+                types.append(CustomizableSerializer)
+
+            ctx.add_resource(serializer, resource_name, context_attr, types=types)
+            logger.info('Configured serializer (%s / ctx.%s; type=%s)', resource_name,
+                        context_attr, serializer.mimetype)
---
+++ packages/asphalt-serialization/asphalt_serialization-4.0.3-py3-none-any.whl/asphalt/serialization/serializers/json.py
@@ -0,0 +1,76 @@
+from json.decoder import JSONDecoder
+from json.encoder import JSONEncoder
+from typing import Dict, Any, Union
+
+from typeguard import check_argument_types
+
+from asphalt.core import resolve_reference
+from asphalt.serialization.api import CustomizableSerializer
+from asphalt.serialization.object_codec import DefaultCustomTypeCodec
+
+
+class JSONTypeCodec(DefaultCustomTypeCodec):
+    """Default state wrapper implementation for :class:`~.JSONSerializer`."""
+
+    def register_object_encoder_hook(self, serializer: 'JSONSerializer') -> None:
+        self.serializer = serializer
+        serializer.encoder_options['default'] = self.default_encoder
+        serializer._encoder = JSONEncoder(**serializer.encoder_options)
+
+    def register_object_decoder_hook(self, serializer: 'JSONSerializer') -> None:
+        self.serializer = serializer
+        serializer.decoder_options['object_hook'] = self.default_decoder
+        serializer.decoder_options.pop('object_pairs_hook', None)
+        serializer._decoder = JSONDecoder(**serializer.decoder_options)
+
+
+class JSONSerializer(CustomizableSerializer):
+    """
+    Serializes objects using JSON (JavaScript Object Notation).
+
+    See the :mod:`json` module documentation in the standard library for more information on
+    available options.
+
+    Certain options can resolve references to objects:
+
+    * ``encoder_options['default']``
+    * ``decoder_options['object_hook']``
+    * ``decoder_options['object_pairs_hook']``
+
+    :param encoder_options: keyword arguments passed to :class:`~json.JSONEncoder`
+    :param decoder_options: keyword arguments passed to :class:`~json.JSONDecoder`
+    :param encoding: the text encoding to use for converting to and from bytes
+    :param custom_type_codec: wrapper to use to wrap custom types after marshalling
+    """
+
+    __slots__ = ('encoder_options', 'decoder_options', 'encoding', 'custom_type_codec',
+                 '_encoder', '_decoder', '_marshallers', '_unmarshallers')
+
+    def __init__(self, encoder_options: Dict[str, Any] = None,
+                 decoder_options: Dict[str, Any] = None, encoding: str = 'utf-8',
+                 custom_type_codec: Union[JSONTypeCodec, str] = None) -> None:
+        assert check_argument_types()
+        super().__init__(resolve_reference(custom_type_codec) or JSONTypeCodec())
+        self.encoding = encoding
+
+        self.encoder_options = encoder_options or {}
+        self.encoder_options['default'] = resolve_reference(self.encoder_options.get('default'))
+        self._encoder = JSONEncoder(**self.encoder_options)
+
+        self.decoder_options = decoder_options or {}
+        self.decoder_options['object_hook'] = resolve_reference(
+            self.decoder_options.get('object_hook'))
+        self.decoder_options['object_pairs_hook'] = resolve_reference(
+            self.decoder_options.get('object_pairs_hook'))
+        self._decoder = JSONDecoder(**self.decoder_options)
+
+    def serialize(self, obj) -> bytes:
+        return self._encoder.encode(obj).encode(self.encoding)
+
+    def deserialize(self, payload: bytes):
+        text_payload = payload.decode(self.encoding)
+        return self._decoder.decode(text_payload)
+
+    @property
+    def mimetype(self):
+        return 'application/json'
