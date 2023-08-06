548f58e7600e3cfdc8aeae0e23089d39962e2297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:57 +0000

    Add package django-djasync/0.2/django-djasync-0.2.tar.gz

---
+++ packages/django-djasync/django-djasync-0.2.tar.gz/django-djasync-0.2/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    # name='django-myapp',
+    name='django-djasync',
+    version='0.2',
+    packages=['djasync'],
+    description='A line of description',
+    long_description=README,
+    author='Kshitij Mhatre',
+    author_email='kshitijpmhatre@gmail.com',
+    url='https://github.com/KshitijMhatre/scratchpad/',
+    license='MIT',
+    install_requires=[
+        'Django',
+    ]
+)
