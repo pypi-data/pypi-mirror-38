202c4012d0d4a436ab854c4b34c325213137164b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:15 +0000

    Add package ml-cli/0.0.2/ml-cli-0.0.2.tar.gz

---
+++ packages/ml-cli/ml-cli-0.0.2.tar.gz/ml-cli-0.0.2/README.md
@@ -0,0 +1,3 @@
+# Machine Learning CLI
+
+This is an attempt to abstract lots of the repeatedly used commands in training and testing machine learning projects.
---
+++ packages/ml-cli/ml-cli-0.0.2.tar.gz/ml-cli-0.0.2/cli.py
@@ -0,0 +1,79 @@
+import os
+import click
+import yaml
+from clint.textui import colored
+from pyfiglet import figlet_format
+
+
+class Config:
+    configuration = None
+    CONFIG_FILE = '.ml-cli.yml'
+    sudo_prefix = ''
+
+
+def configuration_exists():
+    return os.path.isfile(Config.CONFIG_FILE)
+
+
+def require_configuration():
+    if not configuration_exists():
+        click.echo('New Project :: Please, call init command before calling other commands!')
+        exit()
+
+
+def load_configuration():
+    global configuration
+    if configuration_exists():
+        with open(Config.CONFIG_FILE, 'r') as f:
+            Config.configuration = yaml.load(f).get('configuration')
+
+
+def command_factory(cmd):
+    return "{}{}".format(Config.sudo_prefix, cmd)
+
+
+@click.group()
+@click.option('--root/--no-root', default=False)
+def cli(root):
+    click.echo(figlet_format("ML-CLI", font='slant'))
+
+    if root:
+        Config.sudo_prefix = 'sudo '
+
+    load_configuration()
+
+
+@cli.command()
+@click.option('--repository', required=True)
+def init(repository):
+    click.echo('Creating YAML configuration file ...')
+    default_configuration = {
+        'configuration': {
+            'repository': repository.lower()
+        }
+    }
+    with open(Config.CONFIG_FILE, 'w') as f:
+        f.write(yaml.dump(default_configuration, default_flow_style=False))
+
+
+@cli.command()
+def docker_build():
+    require_configuration()
+
+    cmd = command_factory("docker build -t {} .".format(Config.configuration['repository']))
+    click.echo(colored.green('$ {}'.format(cmd)))
+    os.system(cmd)
+
+
+@cli.command()
+@click.option('--gpu/--no-gpu', default=False)
+def docker_bash(gpu):
+    require_configuration()
+
+    cmd = command_factory("docker run -v `pwd`:/usr/local/src/code {}--rm -it {} bash".format('--runtime=nvidia ' if gpu else '', Config.configuration['repository']))
+    click.echo(colored.green('$ {}'.format(cmd)))
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    cli()
