bf7a1b93952a4ff00219ac70055e1d75a413b935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:46 +0000

    Add package raspberrysystem/0.1.1/raspberrysystem-0.1.1.tar.gz

---
+++ packages/raspberrysystem/raspberrysystem-0.1.1.tar.gz/raspberrysystem-0.1.1/raspberrysystem/__main__.py
@@ -0,0 +1,7 @@
+import raspberrysystem
+
+def main():
+  print('foo = ', 1)
+
+if __name__ == '__main__':
+  main()
---
+++ packages/raspberrysystem/raspberrysystem-0.1.1.tar.gz/raspberrysystem-0.1.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='raspberrysystem',
+      version='0.1.1',
+      description='The funniest joke in the world',
+      url='http://github.com/raubreywhite/raspberrysystem',
+      author='Richard',
+      author_email='w@rwhite.no',
+      license='MIT',
+      packages=['raspberrysystem'],
+      zip_safe=False)
