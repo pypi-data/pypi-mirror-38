a5af388748bbf8f9c48803872383d02ac83afb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:44 +0000

    Add package python-fossil-delta/18.11.27/python-fossil-delta-18.11.27.tar.gz

---
+++ packages/python-fossil-delta/python-fossil-delta-18.11.27.tar.gz/python-fossil-delta-18.11.27/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='python-fossil-delta',
+    version='18.11.27',
+    description='Delta compression algorithm from fossil SCM',
+    long_description=open('README.md', 'rt').read(),
+    url='https://github.com/ggicci/python-fossil-delta',
+    author='Ggicci',
+    author_email='ggicci@163.com',
+    license='MIT',
+    keywords='fossil-delta compression algorithm',
+    setup_requires=['cffi>=1.11.5'],
+    package_dir={'': 'src'},
+    packages=['fossil_delta'],
+    package_data={
+        'fossil_delta': ['*.h', '*.c', 'build/*.py'],
+    },
+    cffi_modules=[
+        'src/fossil_delta/build/fossil_delta_build.py:ffibuilder',
+    ],
+    install_requires=['cffi>=1.11.5'],
+    platforms='any',
+)
---
+++ packages/python-fossil-delta/python-fossil-delta-18.11.27.tar.gz/python-fossil-delta-18.11.27/src/fossil_delta/delta.py
@@ -0,0 +1,71 @@
+"""
+This module encapsulates the C API of fossil delta compression algorithm to
+simplify its use in Python.
+"""
+
+from ._delta import ffi, lib
+
+
+def create_delta(src, dst):
+    """
+    Create a new delta.
+
+    Args:
+        src: The source or pattern content. (bytes)
+        dst: The target content. (bytes)
+
+    Returns:
+        The delta. (bytes)
+    """
+    assert isinstance(src, bytes), 'src must be type of bytes'
+    assert isinstance(dst, bytes), 'dst must be type of bytes'
+
+    # The delta is written into a preallocated buffer, zDelta, which
+    # should be at least 60 bytes longer than the target file, zOut.
+    out = ffi.new('char[]', len(dst) + 60)
+    n = lib.delta_create(
+        src,
+        ffi.cast('unsigned int', len(src)),
+        dst,
+        ffi.cast('unsigned int', len(dst)),
+        out,
+    )
+
+    if n < 0:
+        raise Exception('lib.delta_create failed, return code: %d' % n)
+
+    return bytes(ffi.unpack(out, n))
+
+
+def apply_delta(src, delta):
+    """
+    Apply a delta.
+
+    Args:
+        src: The source or pattern content.
+        delta: Delta to apply to the pattern.
+
+    Returns:
+        The patched result.
+    """
+    assert isinstance(src, bytes), 'src must be type of bytes'
+    assert isinstance(delta, bytes), 'delta must be type of bytes'
+
+    out_size = lib.delta_output_size(delta, len(delta))
+    if out_size < 0:
+        raise Exception(
+            'lib.delta_output_size failed, return code: %d' % out_size)
+
+    out = ffi.new('char[]', out_size + 1)
+    n = lib.delta_apply(
+        src,
+        ffi.cast('int', len(src)),
+        delta,
+        ffi.cast('int', len(delta)),
+        out,
+    )
+
+    if n < 0:
+        raise Exception('lib.delta_apply failed, return code: %d' % n)
+
+    return bytes(ffi.unpack(out, n))
