1a9ae3fd7d61caaa61e6c7c7fe1648cd216c63e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:29 +0000

    Add package pantsbuild.pants/1.10.0/pantsbuild.pants-1.10.0-cp27-none-macosx_10_11_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-1.10.0-cp27-none-macosx_10_11_x86_64.whl/pants/backend/python/tasks/pex_build_util.py
@@ -0,0 +1,171 @@
+# coding=utf-8
+# Copyright 2017 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+from builtins import str
+
+from future.utils import PY2
+from pex.fetcher import Fetcher
+from pex.resolver import resolve
+from twitter.common.collections import OrderedSet
+
+from pants.backend.python.pex_util import expand_and_maybe_adjust_platform
+from pants.backend.python.subsystems.python_repos import PythonRepos
+from pants.backend.python.subsystems.python_setup import PythonSetup
+from pants.backend.python.targets.python_binary import PythonBinary
+from pants.backend.python.targets.python_distribution import PythonDistribution
+from pants.backend.python.targets.python_library import PythonLibrary
+from pants.backend.python.targets.python_requirement_library import PythonRequirementLibrary
+from pants.backend.python.targets.python_tests import PythonTests
+from pants.base.build_environment import get_buildroot
+from pants.base.exceptions import TaskError
+from pants.build_graph.files import Files
+
+
+def is_python_target(tgt):
+  # We'd like to take all PythonTarget subclasses, but currently PythonThriftLibrary and
+  # PythonAntlrLibrary extend PythonTarget, and until we fix that (which we can't do until
+  # we remove the old python pipeline entirely) we want to ignore those target types here.
+  return isinstance(tgt, (PythonLibrary, PythonTests, PythonBinary))
+
+
+def has_python_sources(tgt):
+  return is_python_target(tgt) and tgt.has_sources()
+
+
+def is_local_python_dist(tgt):
+  return isinstance(tgt, PythonDistribution)
+
+
+def has_resources(tgt):
+  return isinstance(tgt, Files) and tgt.has_sources()
+
+
+def has_python_requirements(tgt):
+  return isinstance(tgt, PythonRequirementLibrary)
+
+
+def _create_source_dumper(builder, tgt):
+  if type(tgt) == Files:
+    # Loose `Files` as opposed to `Resources` or `PythonTarget`s have no (implied) package structure
+    # and so we chroot them relative to the build root so that they can be accessed via the normal
+    # python filesystem APIs just as they would be accessed outside the chrooted environment.
+    # NB: This requires we mark the pex as not zip safe so these `Files` can still be accessed in
+    # the context of a built pex distribution.
+    chroot_path = lambda relpath: relpath
+    builder.info.zip_safe = False
+  else:
+    chroot_path = lambda relpath: os.path.relpath(relpath, tgt.target_base)
+
+  dump = builder.add_resource if has_resources(tgt) else builder.add_source
+  buildroot = get_buildroot()
+  return lambda relpath: dump(os.path.join(buildroot, relpath), chroot_path(relpath))
+
+
+def dump_sources(builder, tgt, log):
+  dump_source = _create_source_dumper(builder, tgt)
+  log.debug('  Dumping sources: {}'.format(tgt))
+  for relpath in tgt.sources_relative_to_buildroot():
+    try:
+      # Necessary to avoid py_compile from trying to decode non-ascii source code into unicode.
+      # Python 3's py_compile can safely handle unicode in source files, meanwhile.
+      if PY2:
+        relpath = relpath.encode('utf-8')
+      dump_source(relpath)
+    except OSError:
+      log.error('Failed to copy {} for target {}'.format(relpath, tgt.address.spec))
+      raise
+
+  if (getattr(tgt, '_resource_target_specs', None) or
+      getattr(tgt, '_synthetic_resources_target', None)):
+    # No one should be on old-style resources any more.  And if they are,
+    # switching to the new python pipeline will be a great opportunity to fix that.
+    raise TaskError('Old-style resources not supported for target {}.  '
+                    'Depend on resources() targets instead.'.format(tgt.address.spec))
+
+
+def dump_requirement_libs(builder, interpreter, req_libs, log, platforms=None):
+  """Multi-platform dependency resolution for PEX files.
+
+  :param builder: Dump the requirements into this builder.
+  :param interpreter: The :class:`PythonInterpreter` to resolve requirements for.
+  :param req_libs: A list of :class:`PythonRequirementLibrary` targets to resolve.
+  :param log: Use this logger.
+  :param platforms: A list of :class:`Platform`s to resolve requirements for.
+                    Defaults to the platforms specified by PythonSetup.
+  """
+  reqs = [req for req_lib in req_libs for req in req_lib.requirements]
+  dump_requirements(builder, interpreter, reqs, log, platforms=platforms)
+
+
+def dump_requirements(builder, interpreter, reqs, log, platforms=None):
+  """Multi-platform dependency resolution for PEX files.
+
+  :param builder: Dump the requirements into this builder.
+  :param interpreter: The :class:`PythonInterpreter` to resolve requirements for.
+  :param reqs: A list of :class:`PythonRequirement` to resolve.
+  :param log: Use this logger.
+  :param platforms: A list of :class:`Platform`s to resolve requirements for.
+                    Defaults to the platforms specified by PythonSetup.
+  """
+  deduped_reqs = OrderedSet(reqs)
+  find_links = OrderedSet()
+  blacklist = PythonSetup.global_instance().resolver_blacklist
+  for req in deduped_reqs:
+    log.debug('  Dumping requirement: {}'.format(req))
+    if not (req.key in blacklist and interpreter.identity.matches(blacklist[req.key])):
+      builder.add_requirement(req.requirement)
+    if req.repository:
+      find_links.add(req.repository)
+
+  # Resolve the requirements into distributions.
+  distributions = resolve_multi(interpreter, deduped_reqs, platforms, find_links)
+  locations = set()
+  for platform, dists in distributions.items():
+    for dist in dists:
+      if dist.location not in locations:
+        log.debug('  Dumping distribution: .../{}'.format(os.path.basename(dist.location)))
+        builder.add_distribution(dist)
+      locations.add(dist.location)
+
+
+def resolve_multi(interpreter, requirements, platforms, find_links):
+  """Multi-platform dependency resolution for PEX files.
+
+  Returns a list of distributions that must be included in order to satisfy a set of requirements.
+  That may involve distributions for multiple platforms.
+
+  :param interpreter: The :class:`PythonInterpreter` to resolve for.
+  :param requirements: A list of :class:`PythonRequirement` objects to resolve.
+  :param platforms: A list of :class:`Platform`s to resolve for.
+  :param find_links: Additional paths to search for source packages during resolution.
+  :return: Map of platform name -> list of :class:`pkg_resources.Distribution` instances needed
+           to satisfy the requirements on that platform.
+  """
+  python_setup = PythonSetup.global_instance()
+  python_repos = PythonRepos.global_instance()
+  platforms = platforms or python_setup.platforms
+  find_links = find_links or []
+  distributions = {}
+  fetchers = python_repos.get_fetchers()
+  fetchers.extend(Fetcher([path]) for path in find_links)
+
+  for platform in platforms:
+    requirements_cache_dir = os.path.join(python_setup.resolver_cache_dir,
+                                          str(interpreter.identity))
+    distributions[platform] = resolve(
+      requirements=[req.requirement for req in requirements],
+      interpreter=interpreter,
+      fetchers=fetchers,
+      platform=expand_and_maybe_adjust_platform(interpreter=interpreter, platform=platform),
+      context=python_repos.get_network_context(),
+      cache=requirements_cache_dir,
+      cache_ttl=python_setup.resolver_cache_ttl,
+      allow_prereleases=python_setup.resolver_allow_prereleases,
+      pkg_blacklist=python_setup.resolver_blacklist,
+      use_manylinux=python_setup.use_manylinux)
+
+  return distributions
---
+++ packages/pantsbuild.pants/pantsbuild.pants-1.10.0-cp27-none-macosx_10_11_x86_64.whl/pants/engine/build_files.py
@@ -0,0 +1,342 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import collections
+import functools
+import logging
+from builtins import next, str
+from os.path import dirname, join
+
+import six
+
+from pants.base.project_tree import Dir
+from pants.base.specs import (AscendantAddresses, DescendantAddresses, SiblingAddresses,
+                              SingleAddress, Specs)
+from pants.build_graph.address import Address, BuildFileAddress
+from pants.build_graph.address_lookup_error import AddressLookupError
+from pants.engine.addressable import AddressableDescriptor, BuildFileAddresses
+from pants.engine.fs import DirectoryDigest, FilesContent, PathGlobs, Snapshot
+from pants.engine.mapper import AddressFamily, AddressMap, AddressMapper, ResolveError
+from pants.engine.objects import Locatable, SerializableFactory, Validatable
+from pants.engine.rules import RootRule, SingletonRule, TaskRule, rule
+from pants.engine.selectors import Get, Select
+from pants.engine.struct import Struct
+from pants.util.dirutil import fast_relpath_optional, recursive_dirname
+from pants.util.filtering import create_filters, wrap_filters
+from pants.util.objects import TypeConstraintError, datatype
+
+
+logger = logging.getLogger(__name__)
+
+
+class ResolvedTypeMismatchError(ResolveError):
+  """Indicates a resolved object was not of the expected type."""
+
+
+def _key_func(entry):
+  key, value = entry
+  return key
+
+
+@rule(AddressFamily, [Select(AddressMapper), Select(Dir)])
+def parse_address_family(address_mapper, directory):
+  """Given an AddressMapper and a directory, return an AddressFamily.
+
+  The AddressFamily may be empty, but it will not be None.
+  """
+  patterns = tuple(join(directory.path, p) for p in address_mapper.build_patterns)
+  path_globs = PathGlobs(include=patterns,
+                         exclude=address_mapper.build_ignore_patterns)
+  snapshot = yield Get(Snapshot, PathGlobs, path_globs)
+  files_content = yield Get(FilesContent, DirectoryDigest, snapshot.directory_digest)
+
+  if not files_content:
+    raise ResolveError('Directory "{}" does not contain any BUILD files.'.format(directory.path))
+  address_maps = []
+  for filecontent_product in files_content.dependencies:
+    address_maps.append(AddressMap.parse(filecontent_product.path,
+                                         filecontent_product.content,
+                                         address_mapper.parser))
+  yield AddressFamily.create(directory.path, address_maps)
+
+
+class UnhydratedStruct(datatype(['address', 'struct', 'dependencies'])):
+  """A product type that holds a Struct which has not yet been hydrated.
+
+  A Struct counts as "hydrated" when all of its members (which are not themselves dependencies
+  lists) have been resolved from the graph. This means that hydrating a struct is eager in terms
+  of inline addressable fields, but lazy in terms of the complete graph walk represented by
+  the `dependencies` field of StructWithDeps.
+  """
+
+  def __hash__(self):
+    return hash(self.struct)
+
+
+def _raise_did_you_mean(address_family, name):
+  names = [a.target_name for a in address_family.addressables]
+  possibilities = '\n  '.join(':{}'.format(target_name) for target_name in sorted(names))
+  raise ResolveError('"{}" was not found in namespace "{}". '
+                     'Did you mean one of:\n  {}'
+                     .format(name, address_family.namespace, possibilities))
+
+
+@rule(UnhydratedStruct, [Select(AddressMapper), Select(Address)])
+def resolve_unhydrated_struct(address_mapper, address):
+  """Given an AddressMapper and an Address, resolve an UnhydratedStruct.
+
+  Recursively collects any embedded addressables within the Struct, but will not walk into a
+  dependencies field, since those should be requested explicitly by rules.
+  """
+
+  address_family = yield Get(AddressFamily, Dir(address.spec_path))
+
+  struct = address_family.addressables.get(address)
+  addresses = address_family.addressables
+  if not struct or address not in addresses:
+    _raise_did_you_mean(address_family, address.target_name)
+
+  dependencies = []
+  def maybe_append(outer_key, value):
+    if isinstance(value, six.string_types):
+      if outer_key != 'dependencies':
+        dependencies.append(Address.parse(value,
+                                          relative_to=address.spec_path,
+                                          subproject_roots=address_mapper.subproject_roots))
+    elif isinstance(value, Struct):
+      collect_dependencies(value)
+
+  def collect_dependencies(item):
+    for key, value in sorted(item._asdict().items(), key=_key_func):
+      if not AddressableDescriptor.is_addressable(item, key):
+        continue
+      if isinstance(value, collections.MutableMapping):
+        for _, v in sorted(value.items(), key=_key_func):
+          maybe_append(key, v)
+      elif isinstance(value, collections.MutableSequence):
+        for v in value:
+          maybe_append(key, v)
+      else:
+        maybe_append(key, value)
+
+  collect_dependencies(struct)
+
+  yield UnhydratedStruct(
+    next(build_address for build_address in addresses if build_address == address),
+    struct,
+    dependencies)
+
+
+def hydrate_struct(symbol_table_constraint, address_mapper, unhydrated_struct):
+  """Hydrates a Struct from an UnhydratedStruct and its satisfied embedded addressable deps.
+
+  Note that this relies on the guarantee that DependenciesNode provides dependencies in the
+  order they were requested.
+  """
+  dependencies = yield [Get(symbol_table_constraint, Address, a) for a in unhydrated_struct.dependencies]
+  address = unhydrated_struct.address
+  struct = unhydrated_struct.struct
+
+  def maybe_consume(outer_key, value):
+    if isinstance(value, six.string_types):
+      if outer_key == 'dependencies':
+        # Don't recurse into the dependencies field of a Struct, since those will be explicitly
+        # requested by tasks. But do ensure that their addresses are absolute, since we're
+        # about to lose the context in which they were declared.
+        value = Address.parse(value,
+                              relative_to=address.spec_path,
+                              subproject_roots=address_mapper.subproject_roots)
+      else:
+        value = dependencies[maybe_consume.idx]
+        maybe_consume.idx += 1
+    elif isinstance(value, Struct):
+      value = consume_dependencies(value)
+    return value
+  # NB: Some pythons throw an UnboundLocalError for `idx` if it is a simple local variable.
+  maybe_consume.idx = 0
+
+  # 'zip' the previously-requested dependencies back together as struct fields.
+  def consume_dependencies(item, args=None):
+    hydrated_args = args or {}
+    for key, value in sorted(item._asdict().items(), key=_key_func):
+      if not AddressableDescriptor.is_addressable(item, key):
+        hydrated_args[key] = value
+        continue
+
+      if isinstance(value, collections.MutableMapping):
+        container_type = type(value)
+        hydrated_args[key] = container_type((k, maybe_consume(key, v))
+                                            for k, v in sorted(value.items(), key=_key_func))
+      elif isinstance(value, collections.MutableSequence):
+        container_type = type(value)
+        hydrated_args[key] = container_type(maybe_consume(key, v) for v in value)
+      else:
+        hydrated_args[key] = maybe_consume(key, value)
+    return _hydrate(type(item), address.spec_path, **hydrated_args)
+
+  yield consume_dependencies(struct, args={'address': address})
+
+
+def _hydrate(item_type, spec_path, **kwargs):
+  # If the item will be Locatable, inject the spec_path.
+  if issubclass(item_type, Locatable):
+    kwargs['spec_path'] = spec_path
+
+  try:
+    item = item_type(**kwargs)
+  except TypeConstraintError as e:
+    raise ResolvedTypeMismatchError(e)
+
+  # Let factories replace the hydrated object.
+  if isinstance(item, SerializableFactory):
+    item = item.create()
+
+  # Finally make sure objects that can self-validate get a chance to do so.
+  if isinstance(item, Validatable):
+    item.validate()
+
+  return item
+
+
+@rule(BuildFileAddresses, [Select(AddressMapper), Select(Specs)])
+def addresses_from_address_families(address_mapper, specs):
+  """Given an AddressMapper and list of Specs, return matching BuildFileAddresses.
+
+  Raises a AddressLookupError if:
+     - there were no matching AddressFamilies, or
+     - the Spec matches no addresses for SingleAddresses.
+  """
+  # Capture a Snapshot covering all paths for these Specs, then group by directory.
+  snapshot = yield Get(Snapshot, PathGlobs, _spec_to_globs(address_mapper, specs))
+  dirnames = {dirname(f.stat.path) for f in snapshot.files}
+  address_families = yield [Get(AddressFamily, Dir(d)) for d in dirnames]
+
+  # NB: `@memoized` does not work on local functions.
+  def by_directory():
+    if by_directory.cached is None:
+      by_directory.cached = {af.namespace: af for af in address_families}
+    return by_directory.cached
+  by_directory.cached = None
+
+  def raise_empty_address_family(spec):
+    raise ResolveError('Path "{}" does not contain any BUILD files.'.format(spec.directory))
+
+  def exclude_address(spec):
+    if specs.exclude_patterns:
+      return any(p.search(spec) is not None for p in specs.exclude_patterns_memo())
+    return False
+
+  def filter_for_tag(tag):
+    return lambda t: tag in [str(t_tag) for t_tag in t.kwargs().get("tags", [])]
+
+  include_target = wrap_filters(create_filters(specs.tags if specs.tags else '', filter_for_tag))
+
+  addresses = []
+  addresses_to_exclude = []
+  included = set()
+  def include(address_families, predicate=None):
+    matched = False
+    for af in address_families:
+      for (a, t) in af.addressables.items():
+        if (predicate is None or predicate(a)):
+          should_exclude_address = exclude_address(a.spec)
+          if should_exclude_address:
+            addresses_to_exclude.append(a)
+          if include_target(t) and (not should_exclude_address):
+            matched = True
+            if a not in included:
+              addresses.append(a)
+              included.add(a)
+    return matched
+
+  for spec in specs.dependencies:
+    if type(spec) is DescendantAddresses:
+      matched = include(
+        af
+        for af in address_families
+        if fast_relpath_optional(af.namespace, spec.directory) is not None
+      )
+      if not matched:
+        raise AddressLookupError(
+          'Spec {} does not match any targets.'.format(spec))
+    elif type(spec) is SiblingAddresses:
+      address_family = by_directory().get(spec.directory)
+      if not address_family:
+        raise_empty_address_family(spec)
+      include([address_family])
+    elif type(spec) is SingleAddress:
+      address_family = by_directory().get(spec.directory)
+      if not address_family:
+        raise_empty_address_family(spec)
+      # spec.name here is generally the root node specified on commandline. equality here implies
+      # a root node i.e. node specified on commandline.
+      if not include([address_family], predicate=lambda a: a.target_name == spec.name):
+        if len(addresses) == 0 and len(addresses_to_exclude) == 0:
+          _raise_did_you_mean(address_family, spec.name)
+    elif type(spec) is AscendantAddresses:
+      include(
+        af
+        for af in address_families
+        if fast_relpath_optional(spec.directory, af.namespace) is not None
+      )
+    else:
+      raise ValueError('Unrecognized Spec type: {}'.format(spec))
+  yield BuildFileAddresses(addresses)
+
+
+def _spec_to_globs(address_mapper, specs):
+  """Given a Specs object, return a PathGlobs object for the build files that it matches."""
+  patterns = set()
+  for spec in specs.dependencies:
+    if type(spec) is DescendantAddresses:
+      patterns.update(join(spec.directory, '**', pattern)
+                      for pattern in address_mapper.build_patterns)
+    elif type(spec) in (SiblingAddresses, SingleAddress):
+      patterns.update(join(spec.directory, pattern)
+                      for pattern in address_mapper.build_patterns)
+    elif type(spec) is AscendantAddresses:
+      patterns.update(join(f, pattern)
+                      for pattern in address_mapper.build_patterns
+                      for f in recursive_dirname(spec.directory))
+    else:
+      raise ValueError('Unrecognized Spec type: {}'.format(spec))
+  return PathGlobs(include=patterns, exclude=address_mapper.build_ignore_patterns)
+
+
+def create_graph_rules(address_mapper, symbol_table):
+  """Creates tasks used to parse Structs from BUILD files.
+
+  :param address_mapper_key: The subject key for an AddressMapper instance.
+  :param symbol_table: A SymbolTable instance to provide symbols for Address lookups.
+  """
+  symbol_table_constraint = symbol_table.constraint()
+
+  partial_hydrate_struct = functools.partial(hydrate_struct, symbol_table_constraint)
+  partial_hydrate_struct.__name__ = 'hydrate_struct'
+
+  return [
+    # A singleton to provide the AddressMapper.
+    SingletonRule(AddressMapper, address_mapper),
+    # Support for resolving Structs from Addresses.
+    TaskRule(
+      symbol_table_constraint,
+      [Select(AddressMapper),
+       Select(UnhydratedStruct)],
+      partial_hydrate_struct,
+      input_gets=[Get(symbol_table_constraint, Address)],
+    ),
+    resolve_unhydrated_struct,
+    # BUILD file parsing.
+    parse_address_family,
+    # Spec handling: locate directories that contain build files, and request
+    # AddressFamilies for each of them.
+    addresses_from_address_families,
+    # Root rules representing parameters that might be provided via root subjects.
+    RootRule(Address),
+    RootRule(BuildFileAddress),
+    RootRule(BuildFileAddresses),
+    RootRule(Specs),
+  ]
