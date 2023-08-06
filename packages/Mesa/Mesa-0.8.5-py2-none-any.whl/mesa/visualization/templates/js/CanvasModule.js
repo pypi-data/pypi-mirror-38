23331b398d21a629c69f89d95ffb4fde8f57e81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:42 +0000

    Add package Mesa/0.8.5/Mesa-0.8.5-py2-none-any.whl

---
+++ packages/Mesa/Mesa-0.8.5-py2-none-any.whl/mesa/visualization/templates/js/CanvasModule.js
@@ -0,0 +1,35 @@
+var CanvasModule = function(canvas_width, canvas_height, grid_width, grid_height) {
+	// Create the element
+	// ------------------
+
+	// Create the tag with absolute positioning :
+	var canvas_tag = `<canvas width="${canvas_width}" height="${canvas_height}" class="world-grid"/>`
+
+
+	// Append it to body:
+	var canvas = $(canvas_tag)[0];
+	var interaction_canvas = $(canvas_tag)[0];
+
+	//$("body").append(canvas);
+	$("#elements").append(canvas);
+	$("#elements").append(interaction_canvas);
+
+	// Create the context for the agents and interactions and the drawing controller:
+	var context = canvas.getContext("2d");
+
+	// Create an interaction handler using the
+	var interactionHandler = new InteractionHandler(canvas_width, canvas_height, grid_width, grid_height, interaction_canvas.getContext("2d"));
+	var canvasDraw = new GridVisualization(canvas_width, canvas_height, grid_width, grid_height, context, interactionHandler);
+
+	this.render = function(data) {
+		canvasDraw.resetCanvas();
+		for (var layer in data)
+			canvasDraw.drawLayer(data[layer]);
+		canvasDraw.drawGridLines("#eee");
+	};
+
+	this.reset = function() {
+		canvasDraw.resetCanvas();
+	};
+
+};
