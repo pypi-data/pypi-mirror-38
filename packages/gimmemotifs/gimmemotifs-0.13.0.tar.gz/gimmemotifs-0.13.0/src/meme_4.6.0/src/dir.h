de88f176849d11197bd88d28812f48c0e06e5b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:23 +0000

    Add package gimmemotifs/0.13.0/gimmemotifs-0.13.0.tar.gz

---
+++ packages/gimmemotifs/gimmemotifs-0.13.0.tar.gz/gimmemotifs-0.13.0/src/meme_4.6.0/src/dir.h
@@ -0,0 +1,3 @@
+#define MEME_DIR "/home/simon"
+#define ETC_DIR "/home/simon/etc" 
+#define BIN_DIR "/home/simon/bin" 
---
+++ packages/gimmemotifs/gimmemotifs-0.13.0.tar.gz/gimmemotifs-0.13.0/test/test_tools.py
@@ -0,0 +1,70 @@
+import platform
+import unittest
+import tempfile
+import os
+from gimmemotifs.tools import __tools__, get_tool
+from gimmemotifs.comparison import MotifComparer
+from gimmemotifs.motif import motif_from_consensus
+
+class TestMotifProgram(unittest.TestCase):
+    """ A test class for motifprograms """
+
+    def setUp(self):
+        self.data_dir = "test/data/motifprogram"
+        self.fa = os.path.join("test/data/denovo/input.fa")
+        self.bg_fa = os.path.join("test/data/denovo/random.fa")
+
+    def isInstalled(self, bin):
+        return os.path.exists(bin) and os.access(bin, os.X_OK)
+    
+    def ap1_included(self, motifs):
+        ap1 = motif_from_consensus("TGASTCA")
+        mc = MotifComparer()
+        for motif in motifs:
+            match = mc.get_closest_match(ap1, motif)
+            if match["TGASTCA"][1][3] < 1e-6:
+                return True
+        return False
+
+    def test_tools(self):
+        """Test motif prediction tools."""
+        params = {
+                "background": self.bg_fa,
+                "organism": "hg38",
+                "width": 7,
+                }
+        
+        for tool_name in sorted(__tools__):
+            if tool_name in [
+                    "gadem",  # sometimes crashes on invalid pointer
+                    "jaspar", 
+                    "xxmotif", # takes too long
+                    "trawler", # unpredictable, sometimes doesn't find the motif
+                    "weeder", # doesn't work at the moment
+                    ]:
+                continue
+           
+            if platform.system() == "Darwin":
+                # No support for osx
+                if tool_name in [
+                    "amd",
+                    "hms",
+                    "improbizer",
+                    "motifsampler",
+                    "posmo",
+                    "meme", # until the bioconda package is fixed
+                    "memew", # until the bioconda package is fixed
+                    ]:
+                    continue
+
+            t = get_tool(tool_name)
+            print("Testing {}...".format(t))
+            
+            (motifs, stderr, stdout) =  t.run(self.fa, params)
+            self.assertTrue(self.ap1_included(motifs))
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
