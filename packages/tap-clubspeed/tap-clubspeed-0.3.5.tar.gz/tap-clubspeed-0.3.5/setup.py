24fccdf149a69221c7795d760c27dbdd66dd43c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:59 +0000

    Add package tap-clubspeed/0.3.5/tap-clubspeed-0.3.5.tar.gz

---
+++ packages/tap-clubspeed/tap-clubspeed-0.3.5.tar.gz/tap-clubspeed-0.3.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-clubspeed',
+      version='0.3.5',
+      description='Singer.io tap for extracting data from the Clubspeed API',
+      author='lambtron',
+      author_email="andyjiang@gmail.com",
+      url='https://andyjiang.com',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_clubspeed'],
+      install_requires=[
+          'singer-python==5.1.5',
+          'requests==2.19.1'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-clubspeed=tap_clubspeed:main
+      ''',
+      packages=['tap_clubspeed'],
+      include_package_data=True,
+)
