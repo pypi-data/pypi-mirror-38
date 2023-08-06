d0959dec5cc9c9e83694ab188f948209e6bd0270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:45 +0000

    Add package z3c.password/1.0.0/z3c.password-1.0.0.tar.gz

---
+++ packages/z3c.password/z3c.password-1.0.0.tar.gz/z3c.password-1.0.0/setup.py
@@ -0,0 +1,82 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup (
+    name='z3c.password',
+    version='1.0.0',
+    author = "Stephan Richter, Roger Ineichen and the Zope Community",
+    author_email = "zope3-dev@zope.org",
+    description = "Password generation and verification utility for Zope3",
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 z3c password verification",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        "Programming Language :: Python :: Implementation :: PyPy",
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3'],
+    url = 'http://pypi.org/project/z3c.password',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'z3c.coverage',
+            'zope.password',
+            'zope.pluggableauth',
+            'zope.testing',
+            ],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.component',
+        'zope.exceptions',
+        'zope.i18nmessageid',
+        'zope.i18n',
+        'zope.interface',
+        'zope.schema',
+        'zope.security',
+        ],
+    tests_require = [
+        'zope.password',
+        'zope.pluggableauth',
+        'zope.testing',
+        ],
+    test_suite='z3c.password.tests.test_suite',
+    zip_safe = False,
+)
