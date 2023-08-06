9f03c21b1d80b84d8078aeb01b0617eee9fbb03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:42 +0000

    Add package metawiki/0.1.1/metawiki-0.1.1.tar.gz

---
+++ packages/metawiki/metawiki-0.1.1.tar.gz/metawiki-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='metawiki',
+    version='0.1.1',
+    description='A simple map for urls of definitions in notable sources.',
+    url='https://github.com/mindey/metawiki',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='AGPL',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires=['parse'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    zip_safe=False
+)
