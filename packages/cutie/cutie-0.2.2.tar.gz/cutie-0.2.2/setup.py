30e2a9b96d596230a65ca9c8ef3aa67348041b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:55 +0000

    Add package cutie/0.2.2/cutie-0.2.2.tar.gz

---
+++ packages/cutie/cutie-0.2.2.tar.gz/cutie-0.2.2/setup.py
@@ -0,0 +1,35 @@
+"""Setup module for PyPI / pip integration.
+"""
+
+import setuptools
+
+
+with open('readme.md', encoding='utf-8') as file:
+    LONG_DESCRIPTION = file.read()
+    # long_description = long_description.split('<!---START--->')[-1]
+    #                                    .split('<!---END--->')[0]
+    # long_description = long_description.replace('<!---PYPI', '')
+    # long_description = long_description.replace('PYPI--->', '')
+
+setuptools.setup(
+    name='cutie',
+    version='0.2.2',
+    author='Hans',
+    author_email='contact.kamik423@gmail.com',
+    description='Commandline User Tools for Input Easification',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kamik423/cutie',
+    py_modules=['cutie'],
+    license='MIT',
+    install_requires=['colorama', 'readchar'],
+    python_requires='>=3.5',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
