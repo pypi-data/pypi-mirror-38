f3665045f808ac2f62ee8811a3cac44f44726939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:54 +0000

    Add package msa/1.0.1/msa-1.0.1.tar.gz

---
+++ packages/msa/msa-1.0.1.tar.gz/msa-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='msa',
+    version='1.0.1',
+    description='Maven settings administrator',
+    url='https://github.com/Gunmer/maven-setting-administrator.git',
+    author='Gunmer',
+    author_email='csosaur@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    packages=['msa', 'msa.action', 'msa.util'],
+    scripts=['bin/msa'],
+    entry_points={'console_scripts': ['src=msn.command_line:main']}
+)
