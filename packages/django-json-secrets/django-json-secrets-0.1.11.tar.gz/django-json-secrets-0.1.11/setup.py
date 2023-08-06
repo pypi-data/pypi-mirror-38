000b4c6f281b9b617f0042cb1b26d5f7c99e0790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:16 +0000

    Add package django-json-secrets/0.1.11/django-json-secrets-0.1.11.tar.gz

---
+++ packages/django-json-secrets/django-json-secrets-0.1.11.tar.gz/django-json-secrets-0.1.11/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-json-secrets',
+    version='0.1.11',
+    description='JSON secrets to Django settings module',
+    long_description=README,
+    # long_description_content_type='text/markdown',
+    author='lhy',
+    author_email='dev@lhy.kr',
+    license='MIT',
+    packages=[
+        'djs',
+    ],
+    url='https://github.com/LeeHanYeong/django-json-secrets',
+    zip_safe=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
