e1374ec164479b388a06a6142a3e53f3efeaa849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:15 +0000

    Add package Giraffe2D/1.0/Giraffe2D-1.0.tar.gz

---
+++ packages/Giraffe2D/Giraffe2D-1.0.tar.gz/Giraffe2D-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='Giraffe2D',
+      version='1.0',
+      description='Simple Pygame Powered Game Engine',
+      author='Saugat Siddki Jarif',
+      author_email='saugatjarif@gmail.com',
+      url='https://github.com/CrypticCoding/Girrafee2D',
+      package_dir = {'': 'Engine'}
+       
+)
+
