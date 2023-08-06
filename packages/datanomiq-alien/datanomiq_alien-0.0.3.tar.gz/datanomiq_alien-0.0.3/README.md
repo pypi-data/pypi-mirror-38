3105ea0c5d6c93b3e50533bd9834c94e4dd2c478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:37 +0000

    Add package datanomiq-alien/0.0.3/datanomiq_alien-0.0.3.tar.gz

---
+++ packages/datanomiq-alien/datanomiq_alien-0.0.3.tar.gz/datanomiq_alien-0.0.3/README.md
@@ -0,0 +1,1 @@
+# datanomiq_alien
---
+++ packages/datanomiq-alien/datanomiq_alien-0.0.3.tar.gz/datanomiq_alien-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='datanomiq_alien',
+    version='0.0.3',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=['datanomiq_alien'],
+    author='Aakash Chugh',
+    author_email='aakash.chugh@datanomiq.de',
+    keywords=['example'],
+    url='https://github.com/aakashdatanomiqer/datanomiq_alien'
+)
