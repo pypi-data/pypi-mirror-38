709d1105a2cd531aa10e793885f73be411974db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:56 +0000

    Add package pytest-resource/0.1.0/pytest_resource-0.1.0-py3.6.egg

---
+++ packages/pytest-resource/pytest_resource-0.1.0-py3.6.egg/pytest_resource.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+@pytest.fixture
+def resource(request):
+    """
+    指定されたpathからファイルを読み込む
+
+    @pytest.mark.resource(PATH)
+    def test_hoge(resource):
+        pass
+    """
+    resource = request.node.get_closest_marker('resource')
+    if resource:
+        path = resource.args[0]
+        with open(path, 'r') as f:
+            data = f.read()
+
+        return data
+
+    else:
+        raise ValueError("Resource not found")
