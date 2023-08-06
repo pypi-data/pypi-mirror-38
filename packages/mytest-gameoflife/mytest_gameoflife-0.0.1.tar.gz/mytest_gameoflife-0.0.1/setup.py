b6ba276be0066b0c1aab2135fc4ac29dd6a27c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:53 +0000

    Add package mytest-gameoflife/0.0.1/mytest_gameoflife-0.0.1.tar.gz

---
+++ packages/mytest-gameoflife/mytest_gameoflife-0.0.1.tar.gz/mytest_gameoflife-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mytest_gameoflife',
+        version='0.0.1',
+        description='This is a project for the course Advanced Scientific Programming in Python - Uppsala University',
+        url='https://github.com/wangd3109/mytest_gameoflife',
+        author='Duo Wang',
+        author_email='wangd3109@gmail.com',
+        license='MIT',
+        packages=['mytest_gameoflife'],
+        zip_safe=False)
