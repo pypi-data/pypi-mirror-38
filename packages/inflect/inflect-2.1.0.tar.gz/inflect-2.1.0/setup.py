d7d4f593eccbd255aaa5b0d5c9a7bbca18735e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:39 +0000

    Add package inflect/2.1.0/inflect-2.1.0.tar.gz

---
+++ packages/inflect/inflect-2.1.0.tar.gz/inflect-2.1.0/setup.py
@@ -0,0 +1,38 @@
+import inflect
+
+import setuptools
+
+
+setuptools.setup(
+    name="inflect",
+    version=inflect.__version__,
+    description="Correctly generate plurals, singular nouns, ordinals, "
+    "indefinite articles; convert numbers to words",
+    author="Paul Dyson",
+    author_email="pwdyson@yahoo.com",
+    maintainer="Alex Gronholm",
+    maintainer_email="alex.gronholm@nextday.fi",
+    url="https://github.com/jazzband/inflect",
+    py_modules=["inflect"],
+    provides=["inflect"],
+    keywords=["plural", "inflect", "participle"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    setup_requires=["setuptools_scm"],
+)
