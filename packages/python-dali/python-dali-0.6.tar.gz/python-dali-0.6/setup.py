9b993a1b8ca7eea77071b6eba900b877fa717b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:07 +0000

    Add package python-dali/0.6/python-dali-0.6.tar.gz

---
+++ packages/python-dali/python-dali-0.6.tar.gz/python-dali-0.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup
+import unittest
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='python-dali',
+    version='0.6',
+    description='Interface to DALI lighting systems',
+    long_description=readme(),
+    author='Stephen Early',
+    author_email='steve@assorted.org.uk',
+    url='https://github.com/sde1000/python-dali',
+    packages=[
+        'dali',
+        'dali.device',
+        'dali.driver',
+        'dali.gear'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    license='LGPL3+',
+    keywords='lighting DALI development',
+    test_suite='dali.tests',
+)
