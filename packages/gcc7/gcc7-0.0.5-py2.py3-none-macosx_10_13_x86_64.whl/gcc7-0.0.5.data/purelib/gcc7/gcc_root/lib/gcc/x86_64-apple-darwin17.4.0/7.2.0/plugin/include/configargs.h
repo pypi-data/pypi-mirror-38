809d13a3a8b09c53c83921d18f22cab5e345086a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:29 +0000

    Add package gcc7/0.0.5/gcc7-0.0.5-py2.py3-none-macosx_10_13_x86_64.whl

---
+++ packages/gcc7/gcc7-0.0.5-py2.py3-none-macosx_10_13_x86_64.whl/gcc7-0.0.5.data/purelib/gcc7/gcc_root/lib/gcc/x86_64-apple-darwin17.4.0/7.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/travis/gcc-7.2.0/configure --prefix /Users/travis/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
