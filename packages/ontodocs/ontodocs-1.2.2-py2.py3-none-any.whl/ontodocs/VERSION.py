ee5d7daa528f16af7b24196ab4cf5f9c064d91f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:30 +0000

    Add package ontodocs/1.2.2/ontodocs-1.2.2-py2.py3-none-any.whl

---
+++ packages/ontodocs/ontodocs-1.2.2-py2.py3-none-any.whl/ontodocs/VERSION.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+#  -*- coding: UTF-8 -*-
+
+##################
+#
+#  metadata
+#
+##################
+
+__version__ = "1.2.2"  #pypi latest: https://pypi.org/project/ontodocs/
+
+__copyright__ = "CopyRight (C) 2015-2017 by Michele Pasin"
+__license__ = "GNU"
+__author__ = "Michele Pasin"
+__author_email__ = "michele dot pasin at gmail dot com"
+
+USAGE = "ontodocs [options] [uri]"
+VERSION = "v" + __version__
+
+AGENT = "%s/%s" % (__name__, __version__)
