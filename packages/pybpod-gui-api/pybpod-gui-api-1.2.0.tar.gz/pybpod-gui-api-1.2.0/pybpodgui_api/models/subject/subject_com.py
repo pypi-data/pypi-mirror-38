fb1ca1f466b72fd6053686e4754bd5ab4ff75d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:16 +0000

    Add package pybpod-gui-api/1.2.0/pybpod-gui-api-1.2.0.tar.gz

---
+++ packages/pybpod-gui-api/pybpod-gui-api-1.2.0.tar.gz/pybpod-gui-api-1.2.0/pybpodgui_api/models/subject/subject_com.py
@@ -0,0 +1,24 @@
+import logging
+
+from pybpodgui_api.exceptions.wrong_subject_error import WrongSubjectConfigured
+
+from .subject_base import SubjectBase
+
+logger = logging.getLogger(__name__)
+
+
+class SubjectCom(SubjectBase):
+
+    def can_run_task(self):
+
+        if self.setup is None or self.setup == 0:
+            raise Exception('Please select a setup before proceeding.')
+
+        if not( len(self.setup.subjects)==1 and self in self.setup.subjects ):
+            raise WrongSubjectConfigured('The subject is not properly configured in the selected setup.')
+
+        return True
+
+    def run_task(self):
+        if self.can_run_task():
+            self.setup.run_task()
