cfb42d6ea864763863a7515d13bc4be325c4a402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:35 +0000

    Add package ImageContrast/1.0.1/ImageContrast-1.0.1.tar.gz

---
+++ packages/ImageContrast/ImageContrast-1.0.1.tar.gz/ImageContrast-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'ImageContrast',
+    version = '1.0.1',
+    py_modules = ['ImageContrast'],
+    author = 'luoyanchao',
+    author_email = 'luoyanchao2007@163.com',
+    description = 'ImageContrast',
+    url = '',
+    )
