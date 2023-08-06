d951eb143ec7aa41e1d858fb8d6fca05534fdfc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:42 +0000

    Add package vector3d/1.1.1/vector3d-1.1.1.tar.gz

---
+++ packages/vector3d/vector3d-1.1.1.tar.gz/vector3d-1.1.1/setup.py
@@ -0,0 +1,28 @@
+name = "vector3d"
+v = "1.1.1"
+author = "Cyrmax"
+email = "bkm.grotschool@yandex.ru"
+description = "Vector3D is a small package for processing 3D vector in decartian system and some vector-related function, such as distance between two points, angle between vectors, ETC"
+url = "https://github.com/kirill-belousov/vector3d"
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=name,
+    version=v,
+    author=author,
+    author_email=email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=url,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
