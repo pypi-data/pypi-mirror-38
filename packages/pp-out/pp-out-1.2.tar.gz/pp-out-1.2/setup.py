2549ff274746557454fb6cc9ac6ad679ceafee3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:10 +0000

    Add package pp-out/1.2/pp-out-1.2.tar.gz

---
+++ packages/pp-out/pp-out-1.2.tar.gz/pp-out-1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pp-out',
+    version='1.2',
+    packages=find_packages(),
+    author="Chang Hao",
+    author_email="mixpplus@gmail.com",
+    description=("This is a tools by changhao maked"),
+    entry_points={
+            'console_scripts':
+                [
+                    'run = pp_out.command_line:main',
+                    'go = pp_out.command_line:go',
+                ],
+        },
+    zip_safe=False,
+)
