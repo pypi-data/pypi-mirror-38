90755453f8c5edb48d9ecda6f406d6eeddcd2a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:10 +0000

    Add package pyenttec/1.1/pyenttec-1.1.tar.gz

---
+++ packages/pyenttec/pyenttec-1.1.tar.gz/pyenttec-1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+with open('README.md') as f:
+    readme = f.read()
+
+requires = ['pyserial']
+tests_require = ['nose']
+
+setup(
+    name='pyenttec',
+    install_requires=requires,
+    tests_require=tests_require,
+    test_suite="nose.collector",
+    version='1.1',
+    author='Chris Macklin',
+    author_email='chris@imaginaryphotons.com',
+    license='GPL3',
+    description='Control an Enttec Pro DMX from python.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    py_modules=['pyenttec'],
+)
