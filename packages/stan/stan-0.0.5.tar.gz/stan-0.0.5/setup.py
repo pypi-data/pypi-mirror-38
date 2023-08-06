d7d3d2fa8a99808749cd14e7c39ec8663690d1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:53 +0000

    Add package stan/0.0.5/stan-0.0.5.tar.gz

---
+++ packages/stan/stan-0.0.5.tar.gz/stan-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name='stan',
+  version='0.0.5',
+  author='Unknown',
+  author_email='vipcalio@gmail.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="stan is what you want",
+  packages=['stan'],
+  scripts=[]
+)
