8f68f7a8a501ffe314c38ce4841560b10482313b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package py-pgtest/0.1.11/py-pgtest-0.1.11.tar.gz

---
+++ packages/py-pgtest/py-pgtest-0.1.11.tar.gz/py-pgtest-0.1.11/py_pgtest/pgtap_prefix.sql
@@ -0,0 +1,21 @@
+\unset ECHO
+\set QUIET 1
+-- Turn off echo and keep things quiet.
+
+-- Format the output for nice TAP.
+\pset format unaligned
+\pset tuples_only true
+\pset pager off
+
+-- Revert all changes on failure.
+\set ON_ERROR_ROLLBACK 1
+\set ON_ERROR_STOP true
+
+SET client_min_messages TO WARNING;
+
+CREATE SCHEMA IF NOT EXISTS tap;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA tap GRANT EXECUTE ON FUNCTIONS TO PUBLIC;
+ALTER DEFAULT PRIVILEGES IN SCHEMA tap GRANT USAGE ON TYPES TO PUBLIC;
+
+CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
