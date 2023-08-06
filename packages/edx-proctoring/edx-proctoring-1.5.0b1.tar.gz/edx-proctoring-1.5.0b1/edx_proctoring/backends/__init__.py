83f9920dffc5a0d10298e10c9ca44f940869e4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package edx-proctoring/1.5.0b1/edx-proctoring-1.5.0b1.tar.gz

---
+++ packages/edx-proctoring/edx-proctoring-1.5.0b1.tar.gz/edx-proctoring-1.5.0b1/edx_proctoring/backends/__init__.py
@@ -0,0 +1,14 @@
+"""
+All supporting Proctoring backends
+"""
+from django.apps import apps
+
+
+def get_backend_provider(exam=None):
+    """
+    Returns an instance of the configured backend provider
+    """
+    backend_name = None
+    if exam and exam['backend']:
+        backend_name = exam['backend']
+    return apps.get_app_config('edx_proctoring').get_backend(name=backend_name)
