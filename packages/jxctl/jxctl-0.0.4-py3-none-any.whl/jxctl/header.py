ffb06ed831f688d3b04df004f35e89d5e9bd53b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:34 +0000

    Add package jxctl/0.0.4/jxctl-0.0.4-py3-none-any.whl

---
+++ packages/jxctl/jxctl-0.0.4-py3-none-any.whl/jxctl/header.py
@@ -0,0 +1,7 @@
+__author__ = 'Deepankumar Loganathan'
+__copyright__ = 'Copyright 2018, jxctl'
+__credits__ = ['Deepankumar Loganathan']
+#__license__ = 'GPL'
+__version__ = '0.0.3'
+__maintainer__ = 'Deepankumar Loganathan'
+__email__ = 'deepan0433@gmail.com'
