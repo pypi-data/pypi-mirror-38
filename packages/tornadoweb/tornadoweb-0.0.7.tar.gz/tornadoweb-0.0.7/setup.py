1a68b6ea15cb4f7348627e3b779b2f217c8a20f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:10 +0000

    Add package tornadoweb/0.0.7/tornadoweb-0.0.7.tar.gz

---
+++ packages/tornadoweb/tornadoweb-0.0.7.tar.gz/tornadoweb-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+
+long_description = ""
+
+setup(
+    name="tornadoweb",
+    version="0.0.7",
+    author="9wfox",
+    author_email="568628130@qq.com",
+    description="tornado web",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/9wfox/tornadoweb",
+    packages=["tornadoweb"],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/tornadoweb/tornadoweb-0.0.7.tar.gz/tornadoweb-0.0.7/tornadoweb/__init__.py
@@ -0,0 +1,12 @@
+"""
+    MVC package
+"""
+from .app import *
+from .config import *
+#ConfigLoader.load()
+from .utility import *
+from .web import *
+
+set_default_encoding()
+
+
