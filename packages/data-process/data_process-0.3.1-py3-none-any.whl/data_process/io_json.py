d5c2cc952a8118d8e58c16521ded5bdd4e8fefa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:42 +0000

    Add package data-process/0.3.1/data_process-0.3.1-py3-none-any.whl

---
+++ packages/data-process/data_process-0.3.1-py3-none-any.whl/data_process/io_json.py
@@ -0,0 +1,16 @@
+import os
+import json
+from .green_dict import json_dumps
+
+
+def read_json(fp):
+    if not os.path.exists(fp):
+        return None
+
+    with open(fp) as f:
+        return json.loads(f.read())
+
+
+def write_json(data, out_path, **kwargs):
+    with open(out_path, "w") as f:
+        return f.write(json_dumps(data, **kwargs))
