05277aa55ab57c0cf109272d066e83834436eebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:01 +0000

    Add package aws-lambda-builders/0.0.2.dev0/aws_lambda_builders-0.0.2.dev0.tar.gz

---
+++ packages/aws-lambda-builders/aws_lambda_builders-0.0.2.dev0.tar.gz/aws_lambda_builders-0.0.2.dev0/aws_lambda_builders/exceptions.py
@@ -0,0 +1,37 @@
+"""
+Collection of public exceptions raised by this library
+"""
+
+
+class LambdaBuilderError(Exception):
+
+    MESSAGE = ''
+
+    def __init__(self, **kwargs):
+        Exception.__init__(self, self.MESSAGE.format(**kwargs))
+
+
+class UnsupportedManifestError(LambdaBuilderError):
+    MESSAGE = "A builder for the given capabilities '{capabilities}' was not found"
+
+
+class WorkflowNotFoundError(LambdaBuilderError):
+    """
+    Raised when a workflow matching the given capabilities was not found
+    """
+    MESSAGE = "Unable to find a workflow matching given capability: " \
+              "{language}, {dependency_manager}, {application_framework}"
+
+
+class WorkflowFailedError(LambdaBuilderError):
+    """
+    Raised when the build failed, for well-known cases
+    """
+    MESSAGE = "{workflow_name}:{action_name} - {reason}"
+
+
+class WorkflowUnknownError(LambdaBuilderError):
+    """
+    Raised when the build ran into an unexpected error
+    """
+    MESSAGE = "{workflow_name}:{action_name} - {reason}"
---
+++ packages/aws-lambda-builders/aws_lambda_builders-0.0.2.dev0.tar.gz/aws_lambda_builders-0.0.2.dev0/aws_lambda_builders/workflows/python_pip/workflow.py
@@ -0,0 +1,36 @@
+"""
+Python PIP Workflow
+"""
+
+from aws_lambda_builders.workflow import BaseWorkflow, Capability
+from aws_lambda_builders.actions import CopySourceAction
+
+from .actions import PythonPipBuildAction
+
+
+class PythonPipWorkflow(BaseWorkflow):
+
+    NAME = "PythonPipBuilder"
+
+    CAPABILITY = Capability(language="python",
+                            dependency_manager="pip",
+                            application_framework=None)
+
+    def __init__(self,
+                 source_dir,
+                 artifacts_dir,
+                 scratch_dir,
+                 manifest_path,
+                 runtime=None, **kwargs):
+
+        super(PythonPipWorkflow, self).__init__(source_dir,
+                                                artifacts_dir,
+                                                scratch_dir,
+                                                manifest_path,
+                                                runtime=runtime,
+                                                **kwargs)
+
+        self.actions = [
+            PythonPipBuildAction(artifacts_dir, manifest_path, runtime),
+            CopySourceAction(source_dir, artifacts_dir),
+        ]
