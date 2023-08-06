6d2caf97eaf1c2afe17589cb15e667d6a4ce1617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:33 +0000

    Add package tqsdk/0.8.7/tqsdk-0.8.7.tar.gz

---
+++ packages/tqsdk/tqsdk-0.8.7.tar.gz/tqsdk-0.8.7/tqsdk/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+__author__ = 'chengzhi'
+__version__ = '0.8.7'
+name = "tqsdk"
+
+from tqsdk.api import *
+from tqsdk.lib import *
+from tqsdk.backtest import *
