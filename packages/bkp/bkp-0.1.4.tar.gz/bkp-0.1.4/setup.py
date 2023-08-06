a2bcd3d5b18dafcf688badb6475e1fbc8cbe24d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:09 +0000

    Add package bkp/0.1.4/bkp-0.1.4.tar.gz

---
+++ packages/bkp/bkp-0.1.4.tar.gz/bkp-0.1.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+readme_path = Path(__file__).parent / 'README.rst'
+
+with open(readme_path) as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'bkp',
+    version = '0.1.4',
+    description = 'Creates backups of your files and directories.',
+    author = 'Grzegorz Krason',
+    author_email = 'grzegorz.krason@gmail.com',
+    url = 'https://github.com/gergelyk/bkp',
+    license = 'MIT',
+    packages = find_packages(),
+    keywords = 'backup'.split(),
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    python_requires='~=3.6',
+    entry_points = {
+        'console_scripts': ['bkp = bkp:main'],
+        },
+    install_requires = [
+        'click',
+    ],
+    classifiers = [
+        'Programming Language :: Python',
+        'Topic :: Desktop Environment',
+        'Topic :: Office/Business',
+        'Topic :: System',
+        'Topic :: Utilities',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+    ],
+)
