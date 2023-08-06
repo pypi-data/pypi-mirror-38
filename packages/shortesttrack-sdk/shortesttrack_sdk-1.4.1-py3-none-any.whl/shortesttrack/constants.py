686296c5014425874b22c027c2fe5b7a82ddd0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:12 +0000

    Add package shortesttrack-sdk/1.4.1/shortesttrack_sdk-1.4.1-py3-none-any.whl

---
+++ packages/shortesttrack-sdk/shortesttrack_sdk-1.4.1-py3-none-any.whl/shortesttrack/constants.py
@@ -0,0 +1,9 @@
+# Performance Output Parameters types
+TYPE_DEFAULT = 'default'
+TYPE_TRAINED_MODEL = 'trained_model'
+TYPE_RESULT_MATRIX = 'result_matrix'
+TYPE_ANALYSIS_RESULT = 'analysis_result'
+
+# Analysis types
+ANALYSIS_TYPE_BATCH = 'batch'
+ANALYSIS_TYPE_ITERATIVE = 'iterative'
