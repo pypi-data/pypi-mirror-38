ff276308588f72fcae18076e5ca518f7e64b2497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:28 +0000

    Add package sphinx-markdown-builder/0.0.5/sphinx_markdown_builder-0.0.5-py2.py3-none-any.whl

---
+++ packages/sphinx-markdown-builder/sphinx_markdown_builder-0.0.5-py2.py3-none-any.whl/sphinx_markdown_builder/markdown_writer.py
@@ -0,0 +1,11 @@
+from nb2plots.doctree2md import Translator, Writer
+
+class MarkdownTranslator(Translator):
+    def visit_title(self, node):
+        self.add((self.section_level + 1) * '#' + ' ')
+
+    def visit_subtitle(self, node):
+        self.add((self.section_level + 2) * '#' + ' ')
+
+class MarkdownWriter(Writer):
+    translator_class = MarkdownTranslator
