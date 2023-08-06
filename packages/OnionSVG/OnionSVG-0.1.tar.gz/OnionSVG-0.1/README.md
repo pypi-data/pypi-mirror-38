7d002e717bd88ebbd1be14d84dccfaf4839d49df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:45 +0000

    Add package OnionSVG/0.1/OnionSVG-0.1.tar.gz

---
+++ packages/OnionSVG/OnionSVG-0.1.tar.gz/OnionSVG-0.1/README.md
@@ -0,0 +1,8 @@
+# OnionSVG
+Peel your SVG files with Python.
+
+### Dependencies:
+* `lxml` for reading SVG files
+* `cairosvg` to render SVG. On Windows, `cairo` has to be installed manually:
+an easy way to do this is to just use the appropriate `cairo.dll` provided by
+[preshing/cairo-windows](https://github.com/preshing/cairo-windows/releases).
---
+++ packages/OnionSVG/OnionSVG-0.1.tar.gz/OnionSVG-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    name = 'OnionSVG',
+    version = 'v0.1',
+    description = 'Peel your SVG files with Python',
+    author = 'ybnd',
+    author_email = 'ybnd@tuta.io',
+    url = 'https://github.com/ybnd/OnionSVG',
+    download_url = 'https://github.com/ybnd/OnionSVG/archive/v0.1.tar.gz',
+    install_requires = ['lxml', 'cairosvg'],
+)
