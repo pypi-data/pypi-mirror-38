c8787244781cfd61ea61eea37e0d8ccb546ffb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:36 +0000

    Add package pydecensooru/0.1.0/pydecensooru-0.1.0.tar.gz

---
+++ packages/pydecensooru/pydecensooru-0.1.0.tar.gz/pydecensooru-0.1.0/pydecensooru/__about__.py
@@ -0,0 +1,14 @@
+# Copyright 2018 miruka
+# This file is part of pydecensooru, licensed under LGPLv3.
+
+"Danbooru post decensoring for Python"
+
+__pkg_name__ = "pydecensooru"
+__version__  = "0.1.0"
+__status__   = "Development"
+# __status__ = "Production"
+
+__author__   = "miruka"
+__email__    = "miruka@disroot.org"
+
+__license__  = "LGPLv3"
