63f7328e1c39be6799e41bf2ad75ccc1e67a2742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:07 +0000

    Add package ds-common/0.0.2/ds_common-0.0.2.tar.gz

---
+++ packages/ds-common/ds_common-0.0.2.tar.gz/ds_common-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ds_common',
+      version='0.0.2',
+      description='Common services for DataSHIELD Poll-Qua mechanism',
+      url='https://github.com/juliangruendner/ds_common',
+      author='Julian Gruendner',
+      author_email='julian.gruendner@fau.de ',
+      license='GNU',
+      packages=['ds_http'],
+      zip_safe=False)
