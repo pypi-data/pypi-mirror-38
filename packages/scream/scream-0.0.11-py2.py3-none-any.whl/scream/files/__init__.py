44bfe879ec082bb6646e68750799f1d086e1eb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:37 +0000

    Add package scream/0.0.11/scream-0.0.11-py2.py3-none-any.whl

---
+++ packages/scream/scream-0.0.11-py2.py3-none-any.whl/scream/files/__init__.py
@@ -0,0 +1,7 @@
+from .git import GitIgnore
+from .module import ModuleExample, TestExample
+from .readme import PackageReadme, MonorepoReadme
+from .scream import Scream
+from .setup import SetupCfg, SetupPy
+from .tox import Tox
+from .util import File
