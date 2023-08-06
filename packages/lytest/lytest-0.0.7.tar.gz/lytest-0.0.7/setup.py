dc94365a866caf519fa608cf5dba4692bae83b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:54 +0000

    Add package lytest/0.0.7/lytest-0.0.7.tar.gz

---
+++ packages/lytest/lytest-0.0.7.tar.gz/lytest-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+
+def readme():
+    with open('README.md', 'r') as fx:
+      return fx.read()
+
+
+setup(name='lytest',
+      version='0.0.7',
+      description='Regression testing for klayout and phidl',
+      long_description=readme(),
+      author='Alex Tait, Adam McCaughan, Sonia Buckley, Jeff Chiles, Jeff Shainline, Rich Mirin, Sae Woo Nam',
+      author_email='alexander.tait@nist.gov',
+      license='MIT',
+      packages=['lytest'],
+      install_requires=['klayout', 'pytest', 'lygadgets'],
+      # entry_points={'console_scripts': ['lytest_store=lytest.command_line:cm_store_ref',
+      #                                   'lytest_xortest=lytest.command_line:cm_xor_test',
+      #                                   'lytest_diff=lytest.command_line:cm_diff']},
+      entry_points={'console_scripts': ['lytest=lytest.command_line:cm_main']},
+      cmdclass={},
+      )
