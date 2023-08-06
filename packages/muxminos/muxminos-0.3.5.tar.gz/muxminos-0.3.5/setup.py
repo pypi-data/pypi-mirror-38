d1920ea59e83cf91e7e7faaf7745b41a05882df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:54 +0000

    Add package muxminos/0.3.5/muxminos-0.3.5.tar.gz

---
+++ packages/muxminos/muxminos-0.3.5.tar.gz/muxminos-0.3.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='muxminos',
+    version='0.3.5',
+    author='hujianxin',
+    author_email='hujianxin@xiaomi.com',
+    include_package_data=True,
+    install_requires=['Click', 'gitpython', 'pyyaml', 'configobj'],
+    license='Apache License',
+    description='A cmd tool for tmuxinator generating configuration file for minos',
+    py_modules=['muxminos'],
+    entry_points={
+        'console_scripts': [
+            'muxminos=muxminos:cli',
+            'mm=muxminos:cli'
+        ]
+    }
+)
