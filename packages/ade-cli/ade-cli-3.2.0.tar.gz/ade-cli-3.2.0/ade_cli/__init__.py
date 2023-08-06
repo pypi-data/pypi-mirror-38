a6efbd08dd1607dc9955f502e6e5c5dc70fbe4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:48 +0000

    Add package ade-cli/3.2.0/ade-cli-3.2.0.tar.gz

---
+++ packages/ade-cli/ade-cli-3.2.0.tar.gz/ade-cli-3.2.0/ade_cli/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 - 2018  Ternaris.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Agile Development Environment"""
+
+#__version__ = '4.0.0dev'  # development towards 4.0.0 on master branch
+__version__ = '3.2.0'  # Use for releases
