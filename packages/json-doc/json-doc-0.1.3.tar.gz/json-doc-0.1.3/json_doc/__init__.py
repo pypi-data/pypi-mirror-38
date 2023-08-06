0300f6ddcf0db93f4eba20e1b86996b279e30ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:10 +0000

    Add package json-doc/0.1.3/json-doc-0.1.3.tar.gz

---
+++ packages/json-doc/json-doc-0.1.3.tar.gz/json-doc-0.1.3/json_doc/__init__.py
@@ -0,0 +1,11 @@
+"""
+JSON-Doc
+"""
+
+__author__ = 'Fatih Piristine'
+__email__ = 'fatihpiristine@gmail.com'
+__version__ = '0.1.3'
+
+from .json_doc import json_doc_get, json_doc_set, json_doc_has, \
+    json_doc_pop, json_doc_append, json_doc_extend, json_doc_replace, \
+    json_doc_replace_re
