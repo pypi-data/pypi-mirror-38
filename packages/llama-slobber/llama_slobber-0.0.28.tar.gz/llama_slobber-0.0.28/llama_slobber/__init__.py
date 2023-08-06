13858e4bdc0a61b213655c3ea084b078a41a724f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:33 +0000

    Add package llama-slobber/0.0.28/llama_slobber-0.0.28.tar.gz

---
+++ packages/llama-slobber/llama_slobber-0.0.28.tar.gz/llama_slobber-0.0.28/llama_slobber/__init__.py
@@ -0,0 +1,15 @@
+from llama_slobber.ll_local_io import get_session
+from llama_slobber.ll_local_io import get_page_data
+from llama_slobber.ll_season import get_season
+from llama_slobber.ll_leagues import get_leagues
+from llama_slobber.ll_rundles import get_rundles
+from llama_slobber.ll_onedays import get_onedays
+from llama_slobber.ll_qhist import get_qhist
+from llama_slobber.ll_matchday import get_matchday
+from llama_slobber.ll_metrics1 import format_float
+from llama_slobber.ll_metrics1 import calc_hun
+from llama_slobber.ll_rundle_members import get_rundle_members
+from llama_slobber.ll_rundle_members import get_rundle_personal
+from llama_slobber.ll_personal_data import get_personal_data
+
+name = "llama_slobber"
