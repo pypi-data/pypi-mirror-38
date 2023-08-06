8173ea14609cb06f64fde4d1ee3e0a7b471e2ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:01 +0000

    Add package bert-tokenizer/0.1.0/bert_tokenizer-0.1.0.tar.gz

---
+++ packages/bert-tokenizer/bert_tokenizer-0.1.0.tar.gz/bert_tokenizer-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import setuptools
+
+
+
+
+setuptools.setup(
+    name="bert_tokenizer",
+    version="0.1.0",
+    author="Li Fang",
+    author_email="golifang1234@gmail.com",
+    description="A Tokenizer for Bert model",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
