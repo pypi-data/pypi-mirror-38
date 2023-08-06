97366395b51c3113f7365e8ab8f6ee8f5ae6cca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:49 +0000

    Add package avocado-framework/66.0/avocado-framework-66.0.tar.gz

---
+++ packages/avocado-framework/avocado-framework-66.0.tar.gz/avocado-framework/selftests/unit/test_result.py
@@ -0,0 +1,29 @@
+import argparse
+import unittest
+
+from avocado.core.result import Result
+
+
+class FakeJobMissingUniqueId(object):
+
+    def __init__(self, args):
+        self.args = args
+
+
+class FakeJob(object):
+
+    def __init__(self, args):
+        self.args = args
+        self.unique_id = '0000000000000000000000000000000000000000'
+
+
+class ResultTest(unittest.TestCase):
+
+    def test_result_job_without_id(self):
+        args = argparse.Namespace()
+        Result(FakeJob(args))
+        self.assertRaises(AttributeError, Result, FakeJobMissingUniqueId(args))
+
+
+if __name__ == '__main__':
+    unittest.main()
