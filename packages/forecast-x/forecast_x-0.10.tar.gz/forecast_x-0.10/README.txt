221fdcd64ac8cefb6c33d4a6b313b1976f27390d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:49 +0000

    Add package forecast-x/0.10/forecast_x-0.10.tar.gz

---
+++ packages/forecast-x/forecast_x-0.10.tar.gz/forecast_x-0.10/README.txt
@@ -0,0 +1,14 @@
+# Forecast_x
+python toolkit for Naive models for time series
+
+
+## Installation
+
+## Documentation
+Documentation will be available soon!.
+
+## Support
+
+
+## Code of Conduct
+Everyone interacting with this project's codebases, issue trackers, and mailing lists is expected to follow the Code of Conduct.
---
+++ packages/forecast-x/forecast_x-0.10.tar.gz/forecast_x-0.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+  name = 'forecast_x',          
+  version = '0.10',     
+  license='MIT',  
+  description = 'Naive Models for time series forecasting',  
+  author = 'Alejandro De Barros',                 
+  author_email = 'alejandrodbn@gmail.com',
+  url = 'https://github.com/alejandrodbn/forecast', 
+  keywords = ['Forecast', 'Python3', 'Time Series'],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Manufacturing',  
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.6',
+  ],
+)
