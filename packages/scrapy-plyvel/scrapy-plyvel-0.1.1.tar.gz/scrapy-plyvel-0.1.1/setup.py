8fed0617742a208ab46782285227a9aca7f131ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package scrapy-plyvel/0.1.1/scrapy-plyvel-0.1.1.tar.gz

---
+++ packages/scrapy-plyvel/scrapy-plyvel-0.1.1.tar.gz/scrapy-plyvel-0.1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['scrapy_plyvel']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['plyvel>=1.0,<2.0']
+
+setup_kwargs = {
+    'name': 'scrapy-plyvel',
+    'version': '0.1.1',
+    'description': 'Scrapy cache storage using plyvel',
+    'long_description': None,
+    'author': 'Ratson',
+    'author_email': 'github@ratson.name',
+    'url': 'https://github.com/ratson/scrapy-plyvel',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
