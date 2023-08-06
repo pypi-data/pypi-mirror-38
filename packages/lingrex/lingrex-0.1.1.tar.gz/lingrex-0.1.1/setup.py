d446c5064e73c7a70e9f460abbdf79d07b6db278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:45 +0000

    Add package lingrex/0.1.1/lingrex-0.1.1.tar.gz

---
+++ packages/lingrex/lingrex-0.1.1.tar.gz/lingrex-0.1.1/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+        name='lingrex',
+        author='Johann-Mattis List',
+        url='https://github.com/lingpy/lingrex',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
+        version='0.1.1',
+        packages=find_packages(where='src'),
+        package_dir={'': 'src'},
+        install_requires=['lingpy', 'sinopy'],
+        )
