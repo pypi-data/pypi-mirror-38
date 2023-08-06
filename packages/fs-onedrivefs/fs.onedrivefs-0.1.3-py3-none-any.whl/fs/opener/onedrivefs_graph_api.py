8d8906b58d561b420d8e1f2f8fb68187ba357aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package fs-onedrivefs/0.1.3/fs.onedrivefs-0.1.3-py3-none-any.whl

---
+++ packages/fs-onedrivefs/fs.onedrivefs-0.1.3-py3-none-any.whl/fs/opener/onedrivefs_graph_api.py
@@ -0,0 +1,31 @@
+from time import time
+
+from .base import Opener
+
+def _SaveToken(token):
+	pass
+
+class OneDriveFSGraphAPIOpener(Opener):
+	protocols = ["onedrive"]
+
+	@staticmethod
+	def open_fs(fs_url, parse_result, writeable, create, cwd):
+		_, _, directory = parse_result.resource.partition('/')
+
+		# this is missing various fields that hopefully aren't necessary
+		token = {
+			"token_type": "Bearer",
+			"access_token": parse_result.params.get("access_token"),
+			"refresh_token": parse_result.params.get("refresh_token")
+		}		
+
+		fs = OneDriveFSGraphAPI(
+			clientId=parse_result.params["client_id"],
+			clientSecret=parse_result.params.get("client_secret"),
+			token=token,
+			SaveToken=_SaveToken)
+
+		if directory:
+			return fs.opendir(directory)
+		else:
+			return fs
