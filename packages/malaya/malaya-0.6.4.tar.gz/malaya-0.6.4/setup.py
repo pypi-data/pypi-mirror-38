916ba41da46aff595aa4dc830c43934d81ba3805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:25 +0000

    Add package malaya/0.6.4/malaya-0.6.4.tar.gz

---
+++ packages/malaya/malaya-0.6.4.tar.gz/malaya-0.6.4/setup.py
@@ -0,0 +1,43 @@
+import os
+from distutils.core import setup
+from distutils import dir_util
+from distutils import sysconfig
+import setuptools
+
+
+__packagename__ = 'malaya'
+
+setuptools.setup(
+    name = __packagename__,
+    packages = setuptools.find_packages(),
+    version = '0.6.4',
+    description = 'Natural-Language-Toolkit for bahasa Malaysia, powered by Deep Learning.',
+    author = 'huseinzol05',
+    author_email = 'husein.zol05@gmail.com',
+    url = 'https://github.com/DevconX/Malaya',
+    download_url = 'https://github.com/DevconX/Malaya/archive/master.zip',
+    keywords = ['nlp', 'bm'],
+    install_requires = [
+        'xgboost==0.80',
+        'sklearn',
+        'scikit-learn==0.19.1',
+        'requests',
+        'fuzzywuzzy',
+        'tqdm',
+        'nltk',
+        'unidecode',
+        'numpy',
+        'scipy',
+        'python-levenshtein',
+        'pandas',
+        'tensorflow==1.5',
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing',
+    ],
+    long_description = open('readme-pypi.srt').read(),
+)
