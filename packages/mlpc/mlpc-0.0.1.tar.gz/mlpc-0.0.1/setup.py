81b011e35446d97af6a56ad1b26296effa4a190d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:37 +0000

    Add package mlpc/0.0.1/mlpc-0.0.1.tar.gz

---
+++ packages/mlpc/mlpc-0.0.1.tar.gz/mlpc-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlpc",
+    version="0.0.1",
+    author="Snorre Visnes",
+    author_email="snorre.visnes+mlpc@gmail.com",
+    description="A python beginners attempt at a micro framework for ML projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://tbd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
