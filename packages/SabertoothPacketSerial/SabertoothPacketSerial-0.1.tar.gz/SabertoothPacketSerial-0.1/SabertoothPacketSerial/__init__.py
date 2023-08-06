08bf9fa58da1521d4ac112fdf8558db5e2bef49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:07 +0000

    Add package SabertoothPacketSerial/0.1/SabertoothPacketSerial-0.1.tar.gz

---
+++ packages/SabertoothPacketSerial/SabertoothPacketSerial-0.1.tar.gz/SabertoothPacketSerial-0.1/SabertoothPacketSerial/__init__.py
@@ -0,0 +1,8 @@
+from .SabertoothPacketSerial import SabertoothPacketSerial
+
+from .version import __version__
+
+name = "SabertoothPacketSerial"
+__author__ = 'Darren Poulson'
+__license__ = 'GPL'
+
