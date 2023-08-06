2f592421a6500964dc226ccf229fc7a6f7c2762e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:39 +0000

    Add package roboteq/0.4/roboteq-0.4.tar.gz

---
+++ packages/roboteq/roboteq-0.4.tar.gz/roboteq-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="roboteq",
+    version="0.4",
+    url="https://github.com/daltonpearson/roboteq",
+    license="MIT",
+    author="Dalton Pearson",
+    author_email="daltonpearson1997@gmail.com",
+    description="Port of the roboteq API",
+    packages=find_packages(exclude=["tests"]),
+    long_description=open("README.md").read(),
+    zip_safe=False,
+)
