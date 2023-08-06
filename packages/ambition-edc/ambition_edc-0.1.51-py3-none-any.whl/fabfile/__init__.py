af500bdcee84c5d30c59a2030d5145195b0b1e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:27 +0000

    Add package ambition-edc/0.1.51/ambition_edc-0.1.51-py3-none-any.whl

---
+++ packages/ambition-edc/ambition_edc-0.1.51-py3-none-any.whl/fabfile/__init__.py
@@ -0,0 +1,4 @@
+from .freeze import freeze, local_freeze, diff_freeze
+from .gunicorn import restart_gunicorn
+from .update import update
+from .virtual_env import update_develop_virtualenv, update_virtualenv
