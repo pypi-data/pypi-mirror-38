b866f0493e8dc20ee0282c126eefc32474ac76af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:11 +0000

    Add package prestans/2.5.3/prestans-2.5.3-py2-none-any.whl

---
+++ packages/prestans/prestans-2.5.3-py2-none-any.whl/prestans/rest/request.py
@@ -0,0 +1,217 @@
+import webob
+
+from prestans import deserializer
+from prestans import exception
+from prestans.http import VERB
+from prestans.parser import AttributeFilter
+from prestans.types import DataCollection
+
+
+class Request(webob.Request):
+    """
+    Request is parsed REST Request; it's inherits and relies on Webob.Request to
+    do the heavy lifting of parsing HTTP requests. It adds on top parsing of
+    REST bodies and parameter sets based on rules set by the prestans app.
+
+    It's responsible for making sense of the prestans headers and making them
+    available to the RequestHandler
+    """
+
+    def __init__(self, environ, charset, logger, deserializers, default_deserializer):
+
+        super(Request, self).__init__(environ=environ, charset=charset)
+        self._logger = logger
+        self._deserializers = deserializers
+        self._default_deserializer = default_deserializer
+        self._attribute_filter = None
+        self._selected_deserializer = None
+        self._parameter_set = None
+
+        self._charset = charset
+
+        self._body_template = None
+        self._parsed_body = None
+
+    @property
+    def method(self):
+        return self.environ['REQUEST_METHOD']
+
+    @property
+    def logger(self):
+        return self._logger
+
+    @property
+    def parsed_body(self):
+
+        if self.body_template is None:
+            msg = "access to request.parsed_body is not allowed when body_template is set to None"
+            raise AttributeError(msg)
+
+        self.parse_body()
+
+        return self._parsed_body
+
+    @property
+    def supported_mime_types(self):
+        return [deserializer.content_type() for deserializer in self._deserializers]
+
+    @property
+    def supported_mime_types_str(self):
+        return ''.join(str(mime_type) + ',' for mime_type in self.supported_mime_types)[:-1]
+
+    @property
+    def selected_deserializer(self):
+        return self._selected_deserializer
+
+    @property
+    def default_deserializer(self):
+        return self._default_deserializer
+
+    def set_deserializer_by_mime_type(self, mime_type):
+        """
+        :param mime_type:
+        :return:
+
+        Used by content_type_set to set get a reference to the serializer object
+        """
+
+        for deserializer in self._deserializers:
+            if deserializer.content_type() == mime_type:
+                self._selected_deserializer = deserializer
+                return
+
+        raise exception.UnsupportedContentTypeError(mime_type, self.supported_mime_types_str)
+
+    @property
+    def attribute_filter(self):
+        return self._attribute_filter
+
+    @attribute_filter.setter
+    def attribute_filter(self, value):
+        """
+        Attribute filter that used to parse the request_body. This must be set
+        before the body_template parameter is set
+        """
+        self._attribute_filter = value
+
+    @property
+    def parameter_set(self):
+        """
+        Accessor for parameter_set
+        """
+        return self._parameter_set
+
+    @parameter_set.setter
+    def parameter_set(self, value):
+        """
+        Mutator for parameter_set
+        """
+        self._parameter_set = value
+
+    @property
+    def body_template(self):
+        return self._body_template
+
+    @body_template.setter
+    def body_template(self, value):
+        """
+        Must be an instance of a prestans.types.DataCollection subclass; this is
+        generally set during the RequestHandler lifecycle. Setting this spwans the
+        parsing process of the body. If the HTTP verb is GET an AssertionError is
+        thrown. Use with extreme caution.
+        """
+
+        if self.method == VERB.GET:
+            raise AssertionError("body_template cannot be set for GET requests")
+
+        if value is None:
+            self.logger.warning("body_template is None, parsing will be ignored")
+            return
+
+        if not isinstance(value, DataCollection):
+            msg = "body_template must be an instance of %s.%s" % (
+                DataCollection.__module__,
+                DataCollection.__name__
+            )
+            raise AssertionError(msg)
+
+        self._body_template = value
+
+        # get a deserializer based on the Content-Type header
+        # do this here so the handler gets a chance to setup extra serializers
+        self.set_deserializer_by_mime_type(self.content_type)
+
+    def parse_body(self):
+
+        if self._parsed_body is None and self._body_template is not None:
+            # parse the body using the deserializer
+            unserialized_body = self.selected_deserializer.loads(self.body)
+
+            # valiate the body using the template and attribute_filter
+            self._parsed_body = self._body_template.validate(unserialized_body, self.attribute_filter, self.is_minified)
+
+    def register_deserializers(self, deserializers):
+
+        if not isinstance(deserializers, list):
+            deserializers = [deserializers]
+
+        # todo: should this prevent duplicates for same mime-type?
+
+        for new_deserializer in deserializers:
+
+            if not isinstance(new_deserializer, deserializer.Base):
+                msg = "registered deserializer %s does not inherit from prestans.serializer.DeSerializer" % (
+                      new_deserializer.__class__.__name__
+                )
+                raise TypeError(msg)
+
+        self._deserializers = self._deserializers + deserializers
+
+    def get_response_attribute_filter(self, template_filter, template_model=None):
+        """
+        Prestans-Response-Attribute-List can contain a client's requested
+        definition for attributes required in the response. This should match
+        the response_attribute_filter_template?
+
+        :param template_filter:
+        :param template_model: the expected model that this filter corresponds to
+        :return:
+        :rtype: None | AttributeFilter
+        """
+
+        if template_filter is None:
+            return None
+
+        if 'Prestans-Response-Attribute-List' not in self.headers:
+            return None
+
+        # header not set results in a None
+        attribute_list_str = self.headers['Prestans-Response-Attribute-List']
+
+        # deserialize the header contents
+        json_deserializer = deserializer.JSON()
+        attribute_list_dictionary = json_deserializer.loads(attribute_list_str)
+
+        # construct an AttributeFilter
+        attribute_filter = AttributeFilter(
+            from_dictionary=attribute_list_dictionary,
+            template_model=template_model
+        )
+
+        #: Check template? Do this even through we might have template_model
+        #: in case users have made a custom filter
+        evaluated_filter = attribute_filter.conforms_to_template_filter(template_filter)
+
+        return evaluated_filter
+
+    @property
+    def is_minified(self):
+        """
+        :return:
+        :rtype: bool
+        """
+
+        if 'Prestans-Minification' not in self.headers:
+            return False
+
+        return self.headers['Prestans-Minification'].upper() == "ON"
---
+++ packages/prestans/prestans-2.5.3-py2-none-any.whl/prestans/serializer.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#
+#  prestans, A WSGI compliant REST micro-framework
+#  http://prestans.org
+#
+#  Copyright (c) 2017, Anomaly Software Pty Ltd.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#      * Neither the name of Anomaly Software nor the
+#        names of its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL ANOMALY SOFTWARE BE LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+import sys
+
+from prestans import exception
+from prestans.types import DataCollection
+
+__all__ = ['Base', 'JSON', 'XMLPlist']
+
+
+class Base(object):
+
+    def dumps(self, serializable_object):
+        raise NotImplementedError
+
+    def handler_body_type(self):
+        raise NotImplementedError
+
+    def content_type(self):
+        raise NotImplementedError
+
+
+class JSON(Base):
+
+    def dumps(self, serializable_object):
+        
+        import json
+        try:
+            return json.dumps(serializable_object, ensure_ascii=False, sort_keys=True)
+        except Exception as exp:
+            raise exception.SerializationFailedError("JSON: %s" % exp)
+
+    def handler_body_type(self):
+        return DataCollection
+
+    def content_type(self):
+        return 'application/json'
+
+
+class XMLPlist(Base):
+    """
+    Uses Apple's Property List format to serialize collections to XML.
+
+    Refer to:
+     - https://docs.python.org/2/library/plistlib.html
+     - https://docs.python.org/3/library/plistlib.html
+    """
+
+    def dumps(self, serializable_object):
+
+        import plistlib
+
+        try:
+            if sys.version_info < (3,):
+                plist_str = plistlib.writePlistToString(serializable_object)
+            else:
+                plist_str = plistlib.dumps(serializable_object)
+        except Exception as exp:
+            raise exception.SerializationFailedError("XMLPlist: %s" % exp)
+
+        return plist_str
+
+    def handler_body_type(self):
+        return DataCollection
+
+    def content_type(self):
+        return 'application/xml'
