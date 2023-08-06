25dc2365e54d65a504aab341468102f43f83a7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:58 +0000

    Add package flyingtrain/0.1.2/flyingtrain-0.1.2.tar.gz

---
+++ packages/flyingtrain/flyingtrain-0.1.2.tar.gz/flyingtrain-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flyingtrain",
+    version="0.1.2",
+    author="Chu-Hsuan Lee",
+    author_email="joseph.chuhsuanlee@gmail.com",
+    description="package for bonial challenge",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chuhsuanlee/flyingtrain",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          "ijson==2.3",
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
