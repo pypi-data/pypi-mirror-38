0fb067dade97656ae59150aae45449a1489c712a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:58 +0000

    Add package open-widget-framework/0.1.18/open_widget_framework-0.1.18.tar.gz

---
+++ packages/open-widget-framework/open_widget_framework-0.1.18.tar.gz/open_widget_framework-0.1.18/open_widget_framework/views.py
@@ -0,0 +1,86 @@
+"""
+WidgetApp views
+"""
+from json import loads
+
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+
+from open_widget_framework.models import WidgetInstance, WidgetList
+from open_widget_framework.utils import get_widget_class_configurations
+from open_widget_framework.widget_class_base import WidgetBase
+
+# TODO: validate with widget list
+
+
+def get_widget_lists(request):
+    """
+    API endpoint for returning a list of all WidgetList ids
+    """
+    return Response([widget_list['id'] for widget_list in WidgetList.objects.all().values('id')])
+
+
+def get_widget_configurations(request):
+    """
+    API endpoint for getting all available widget classes and their configurations
+    """
+    return Response(get_widget_class_configurations())
+
+
+class WidgetListViewSet(ModelViewSet):
+
+
+class WidgetViewSet(ModelViewSet):
+    serializer_class = WidgetBase
+
+    def get_queryset(self):
+        return WidgetInstance.objects.filter(widget_list_id=self.kwargs['widget_list_id'])
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return Response(serializer.render_with_title())
+
+    def create(self, request, *args, **kwargs):
+        """
+        API endpoint to create a widget instance on a list after validating the data with a serializer
+        class
+        """
+        super().create(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        API endpoint to delete a specified widget
+        """
+        super().destroy(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        """
+        API endpoint to update the data for a widget instance
+        """
+        super().update(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+        API endpoint to partial update a widget
+        """
+        if 'position' in request.data:
+            queryset = self.get_queryset()
+            target_pos = max(0, min(queryset.count() - 1, request.data.position))
+            target_widget = self.get_object()
+            current_pos = target_widget.position
+
+            for widget in self.get_queryset():
+                if target_pos >= widget.position > current_pos:
+                    widget.position -= 1
+                    widget.save()
+                elif current_pos > widget.position >= target_pos:
+                    widget.position += 1
+                    widget.save()
+
+        super().update(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
