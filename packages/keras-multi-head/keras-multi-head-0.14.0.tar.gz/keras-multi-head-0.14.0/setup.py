571cd51b9cdd38d79ed36e2f40b35d1cfdac8917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:16 +0000

    Add package keras-multi-head/0.14.0/keras-multi-head-0.14.0.tar.gz

---
+++ packages/keras-multi-head/keras-multi-head-0.14.0.tar.gz/keras-multi-head-0.14.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='keras-multi-head',
+    version='0.14.0',
+    packages=['keras_multi_head'],
+    url='https://github.com/CyberZHG/keras-multi-head',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='A wrapper layer for stacking layers horizontally',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'Keras',
+        'keras-self-attention==0.31.0',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
