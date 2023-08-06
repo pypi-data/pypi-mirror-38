876a506a3c658a6935909a8f35ccca7f0a125da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:51 +0000

    Add package kronjob/2.0.0/kronjob-2.0.0.tar.gz

---
+++ packages/kronjob/kronjob-2.0.0.tar.gz/kronjob-2.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='kronjob',
+    version='2.0.0',
+    description='Generate Kubernetes Job/CronJob specs without the boilerplate.',
+    install_requires=[
+        'kubernetes==4.0.0',
+        'inflection==0.3.1',
+        'jsonschema==2.6.0',
+        'crontab==0.22.1',
+        'PyYAML==3.12'
+    ],
+    url='https://github.com/better/kronjob',
+    author='Better Mortgage',
+    author_email='accounts@better.com',
+    keywords='kubernetes job cronjob schedule template',
+    entry_points={'console_scripts': ['kronjob=kronjob:main']},
+    license='MIT',
+    packages=['kronjob'],
+    package_data={'kronjob': ['schema.json']}
+)
