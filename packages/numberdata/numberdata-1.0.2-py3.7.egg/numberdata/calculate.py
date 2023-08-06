1e0120ed1eef05a494ab07103ed8fcb94ae904b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:54 +0000

    Add package numberdata/1.0.2/numberdata-1.0.2-py3.7.egg

---
+++ packages/numberdata/numberdata-1.0.2-py3.7.egg/numberdata/calculate.py
@@ -0,0 +1,9 @@
+# the main code file of packege containing 2 methods to calculate square and cube of a number
+
+def calculateSquare(num):
+	square = num * num
+	return square	
+
+def calculateCube(num):
+	cube = num * num * num
+	return cube
