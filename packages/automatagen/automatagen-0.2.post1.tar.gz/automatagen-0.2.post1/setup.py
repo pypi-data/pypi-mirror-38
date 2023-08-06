64992a194949cbff3db1a7136a8bda713fdd276f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:57 +0000

    Add package automatagen/0.2.post1/automatagen-0.2.post1.tar.gz

---
+++ packages/automatagen/automatagen-0.2.post1.tar.gz/automatagen-0.2.post1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='automatagen',
+      version='0.2.post-1',
+      description='Generate random terrain using cellular automata.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/PetarPeychev/automatagen',
+      author='PetarPeychev',
+      author_email='petarpeychev98@gmail.com',
+      packages=setuptools.find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: Public Domain",
+          "Operating System :: OS Independent",
+       ],)
