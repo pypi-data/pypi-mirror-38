291b6f3e9a92dcf834e79c5af68997858fa54d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:36 +0000

    Add package xanity/0.1b16/xanity-0.1b16-py2-none-any.whl

---
+++ packages/xanity/xanity-0.1b16-py2-none-any.whl/xanity/constants.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+from .common import Constants
+
+RELATIVE_PATHS = Constants({'src':'src',
+                  'include':'include',
+                  'experiments':'experiments',
+                  'analyses':'analyses',
+                  'run_data':'data/runs',
+                  'persistent_data':'data/persistent',
+                  'saved_data':'data/saved',
+                  'xanity_data':'.xanity'
+                  })
+
+COMMANDS = Constants({
+        'RUN': ['run'],
+        'ANAL': ['anal', 'analyze', 'analyse', 'analysis', 'analyses'],
+        'SETUP': ['setup'],
+        'INIT': ['init', 'initialize','initialise'],
+        })
+
+ACTIONS = Constants({
+        'RUN': 'run',
+        'ANAL': 'anal',
+        'INIT': 'init',
+        'SETUP': 'setup',
+})
+
+ACTIVITIES = Constants({
+        'CONST': 'constructing_xanity_object',
+        'ORIENT': 'orienting',
+        'EXP': 'experimenting',
+        'ANAL': 'analyzing',
+        'WAIT': 'waiting',
+})
+
+DIRNAMES = Constants({
+        'SAVED_VARS': "xanity_variables",
+        })
