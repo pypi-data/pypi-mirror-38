05905b6702a14f2245f45582c86530530e0e3d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package TimedDict/0.2.3/TimedDict-0.2.3.tar.gz

---
+++ packages/TimedDict/TimedDict-0.2.3.tar.gz/TimedDict-0.2.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='TimedDict',
+    version='0.2.3',
+    packages=['TimedDict'],
+    url='https://github.com/hjortlund/TimedDict',
+    license='MIT',
+    author='Christoffer Hjortlund',
+    author_email='hjortlund@gmail.com',
+    description='Extending the dict data type, with a rolling window feature, that automatically purge elements falling outside the defined window.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+)
