d3dfad581603f95879009da313b8e34da1414ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:43 +0000

    Add package duckietown-foobar/0.1.1/duckietown_foobar-0.1.1.tar.gz

---
+++ packages/duckietown-foobar/duckietown_foobar-0.1.1.tar.gz/duckietown_foobar-0.1.1/src/duckietown_foobar/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8 
+__version__ = '0.1.1'
+
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger('dt-foobar')
+logger.setLevel(logging.DEBUG)
+
+logger.info('duckietown_foobar %s' % __version__)
+
+from .algo import *
+
