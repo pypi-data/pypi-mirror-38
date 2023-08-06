6bb6a64870123d2ae7524a27c6af6dc80b6e00ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package ftw.simplelayout/1.23.1/ftw.simplelayout-1.23.1.tar.gz

---
+++ packages/ftw.simplelayout/ftw.simplelayout-1.23.1.tar.gz/ftw.simplelayout-1.23.1/ftw/simplelayout/utils.py
@@ -0,0 +1,48 @@
+from ftw.simplelayout.interfaces import IBlockProperties
+from ftw.simplelayout.interfaces import ISimplelayoutBlock
+from plone import api
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.dexterity.utils import safe_utf8
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility
+from zope.component import queryMultiAdapter
+from zope.component.hooks import getSite
+
+
+def normalize_portal_type(portal_type):
+    normalizer = getUtility(IIDNormalizer)
+    return normalizer.normalize(portal_type)
+
+
+def get_block_html(block):
+    properties = queryMultiAdapter((block, block.REQUEST),
+                                   IBlockProperties)
+    viewname = properties.get_current_view_name()
+    return block.restrictedTraverse(safe_utf8(viewname))()
+
+
+def get_block_types():
+    platform = getSite()
+    types_tool = getToolByName(platform, 'portal_types')
+
+    dexterity_ftis = filter(
+        IDexterityFTI.providedBy, types_tool.objectValues())
+
+    return filter(
+        lambda fti: ISimplelayoutBlock.__identifier__ in fti.behaviors,
+        dexterity_ftis)
+
+
+def unrestricted_uuidToObject(uuid):
+    platform = getSite()
+    catalog = getToolByName(platform, 'portal_catalog', None)
+    if catalog is None:
+        return None
+
+    result = catalog.unrestrictedSearchResults(UID=uuid)
+    if len(result) != 1:
+        return None
+
+    with api.env.adopt_roles(roles=['Manager']):
+        return result[0].getObject()
