caa6c3cd1a42cc026bdade0dd49b8d396053c331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:08 +0000

    Add package primed-avro/0.0.2b0/primed-avro-0.0.2b0.tar.gz

---
+++ packages/primed-avro/primed-avro-0.0.2b0.tar.gz/primed-avro-0.0.2b0/primed_avro/schema.py
@@ -0,0 +1,18 @@
+import json
+
+
+class SchemaMeta:
+    def __init__(self, subject, version, id, schema):
+        self.subject = subject
+        self.version = version
+        self.id = id
+        if isinstance(schema, str):
+            self.schema = json.loads(schema)
+        elif isinstance(schema, dict):
+            self.schema = schema
+        else:
+            raise Exception(
+                "Unsupported schema type, expected `str` or `dict` but was {}".format(
+                    type(schema)
+                )
+            )
