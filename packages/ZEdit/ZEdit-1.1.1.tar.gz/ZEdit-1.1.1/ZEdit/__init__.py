e3e8eccea516844485d67bb1f2e6a71514eb31c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:07 +0000

    Add package ZEdit/1.1.1/ZEdit-1.1.1.tar.gz

---
+++ packages/ZEdit/ZEdit-1.1.1.tar.gz/ZEdit-1.1.1/ZEdit/__init__.py
@@ -0,0 +1,15 @@
+name = "ZEdit"
+
+# set current directory to immediate parent
+import sys, os
+MY_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def run():
+  cwd = os.getcwd()
+  os.chdir(MY_DIR)
+  import main
+  main.run()
+  os.chdir(cwd)
+  
+    
+run()
