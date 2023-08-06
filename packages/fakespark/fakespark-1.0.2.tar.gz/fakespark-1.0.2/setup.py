81b39f5d00e00498a624579de47ddba0197a465f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:57 +0000

    Add package fakespark/1.0.2/fakespark-1.0.2.tar.gz

---
+++ packages/fakespark/fakespark-1.0.2.tar.gz/fakespark-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fakespark',
+    version='1.0.2',
+    packages=['tools', 'fakespark'],
+    url='',
+    license='',
+    author='chenzhongrun',
+    author_email='chenzhongrun@aliyun.com',
+    description=''
+)
