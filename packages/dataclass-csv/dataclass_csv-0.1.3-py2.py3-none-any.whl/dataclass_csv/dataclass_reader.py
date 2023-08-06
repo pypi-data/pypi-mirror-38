415841477595678e607695bd25df1da6f188eb7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:45 +0000

    Add package dataclass-csv/0.1.3/dataclass_csv-0.1.3-py2.py3-none-any.whl

---
+++ packages/dataclass-csv/dataclass_csv-0.1.3-py2.py3-none-any.whl/dataclass_csv/dataclass_reader.py
@@ -0,0 +1,113 @@
+import dataclasses
+
+from csv import DictReader
+
+from .field_mapper import FieldMapper
+
+
+class DataclassReader:
+    def __init__(
+        self,
+        f,
+        cls,
+        fieldnames=None,
+        restkey=None,
+        restval=None,
+        dialect='excel',
+        *args,
+        **kwds
+    ):
+
+        if not f:
+            raise ValueError('The f argument is required')
+
+        if cls is None or not dataclasses.is_dataclass(cls):
+            raise ValueError('cls argument needs to be a dataclass')
+
+        self.cls = cls
+        self.optional_fields = self._get_optional_fields()
+        self.field_mapping = {}
+
+        self.reader = DictReader(
+            f,
+            fieldnames,
+            restkey,
+            restval,
+            dialect,
+            *args,
+            **kwds,
+        )
+
+    def _get_optional_fields(self):
+        return [
+            field.name
+            for field in dataclasses.fields(self.cls)
+            if not isinstance(field.default, dataclasses._MISSING_TYPE)
+        ]
+
+    def _add_to_mapping(self, property_name, csv_fieldname):
+        self.field_mapping[property_name] = csv_fieldname
+
+    def _get_value(self, row, field):
+        try:
+            key = (
+                field.name
+                if field.name not in self.field_mapping.keys()
+                else self.field_mapping.get(field.name)
+            )
+            value = row[key]
+        except KeyError:
+            if field.name in self.optional_fields:
+                return field.default
+            else:
+                raise KeyError(
+                    f'The value {field.name} is missing in the CSV file.'
+                )
+        else:
+            if not value and field.name in self.optional_fields:
+                return field.default
+            elif not value and field.name not in self.optional_fields:
+                raise ValueError(
+                    (
+                        f'The field {field.name} is required. Verify if any '
+                        'row in the CSV file is missing this data.'
+                    )
+                )
+            else:
+                return value
+
+    def _process_row(self, row):
+
+        values = []
+
+        for field in dataclasses.fields(self.cls):
+
+            value = self._get_value(row, field)
+
+            try:
+                transformed_value = field.type(value)
+            except ValueError:
+                raise ValueError(
+                    (
+                        f'The field {field.name} is defined as {field.type} '
+                        f'but received a value of type {type(value)}.'
+                    )
+                )
+            else:
+                values.append(transformed_value)
+
+        return self.cls(*values)
+
+    def __next__(self):
+        row = next(self.reader)
+        return self._process_row(row)
+
+    def __iter__(self):
+        return self
+
+    def map(self, csv_fieldname):
+        return FieldMapper(
+            lambda property_name: self._add_to_mapping(
+                property_name, csv_fieldname
+            )
+        )
