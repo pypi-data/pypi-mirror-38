fd04b09e5ba3daf5f31772a8f3473ccfa90b0419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:02 +0000

    Add package codewatch/0.0.5/codewatch-0.0.5.tar.gz

---
+++ packages/codewatch/codewatch-0.0.5.tar.gz/codewatch-0.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Libraries ",
+]
+
+
+setup(
+    name='codewatch',
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    version='0.0.5',
+    scripts=['bin/codewatch'],
+    install_requires=[
+        'astroid==1.6.4',  # 2.0 onwards is py3 only
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.7"
+ )
---
+++ packages/codewatch/codewatch-0.0.5.tar.gz/codewatch-0.0.5/tests/test_node_visitor.py
@@ -0,0 +1,106 @@
+import astroid
+import pytest
+
+from codewatch.node_visitor import (
+    NodeVisitor,
+    NodeVisitorMaster,
+    count_calling_files,
+)
+from codewatch.stats import Stats
+
+
+FUNCTION_CALL_CODE = """\
+def function():
+    pass
+function()"""
+
+SIMPLE_METHOD_CODE = """\
+class SomeClass(object):
+    @staticmethod
+    def simple_method(self):
+            pass
+SomeClass.simple_method()"""
+
+NESTED_METHOD_CODE = """\
+class InnerClass(object):
+    def inner_method(self):
+        pass
+
+class OuterClass(object):
+    def outer_method(self):
+        return InnerClass()
+OuterClass.outer_method().inner_method()"""
+
+NESTED_METHOD_IN_ATTR_CODE = """\
+class InnerClass2(object):
+    def inner_method(self):
+        pass
+
+class OuterClass2(object):
+    inner = InnerClass2()
+OuterClass2.inner.inner_method()"""
+
+
+@pytest.mark.parametrize(
+    "stats_namespace,code,module_name,expected_callable_qname,expected_stats",
+    [
+        (
+            "COUNT_FUNCTION_CALLS",
+            FUNCTION_CALL_CODE,
+            "function_module",
+            "function_module.function",
+            {"COUNT_FUNCTION_CALLS": {"function_module.py": 1}},
+        ),
+        (
+            "COUNT_SIMPLE_METHOD_CALLS",
+            SIMPLE_METHOD_CODE,
+            "simple_method_module",
+            "simple_method_module.SomeClass.simple_method",
+            {"COUNT_SIMPLE_METHOD_CALLS": {"simple_method_module.py": 1}},
+        ),
+        (
+            "COUNT_NESTED_METHOD_CALLS",
+            NESTED_METHOD_CODE,
+            "nested_method_module",
+            "nested_method_module.InnerClass.inner_method",
+            {"COUNT_NESTED_METHOD_CALLS": {"nested_method_module.py": 1}},
+        ),
+        (
+            "COUNT_NESTED_METHOD_IN_ATTR_CALLS",
+            NESTED_METHOD_IN_ATTR_CODE,
+            "nested_method_in_attr_module",
+            "nested_method_in_attr_module.InnerClass2.inner_method",
+            {
+                "COUNT_NESTED_METHOD_IN_ATTR_CALLS": {
+                    "nested_method_in_attr_module.py": 1
+                }
+            },
+        ),
+    ],
+)
+def test_count_calling_files_function(
+    stats_namespace, code, module_name, expected_callable_qname, expected_stats
+):
+    NodeVisitorMaster.node_visitor_registry = []
+
+    module = astroid.parse(code, module_name)
+
+    assert len(NodeVisitorMaster.node_visitor_registry) == 0
+
+    count_calling_files(stats_namespace, expected_callable_qname)
+
+    assert len(NodeVisitorMaster.node_visitor_registry) == 1
+
+    stats = Stats()
+    NodeVisitorMaster.visit(stats, module, module_name + ".py")
+
+    assert stats == expected_stats
+
+
+def test_sets_stats_and_file_path():
+    stats = Stats()
+    file_path = "/mock/path"
+    node_visitor = NodeVisitor(stats, file_path)
+
+    assert node_visitor.stats == stats
+    assert node_visitor.rel_file_path == file_path
