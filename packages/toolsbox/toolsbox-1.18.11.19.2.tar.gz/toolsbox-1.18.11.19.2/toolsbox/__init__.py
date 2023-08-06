b033fda664d369b30ecd4312495e41b9e415c09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:04 +0000

    Add package toolsbox/1.18.11.19.2/toolsbox-1.18.11.19.2.tar.gz

---
+++ packages/toolsbox/toolsbox-1.18.11.19.2.tar.gz/toolsbox-1.18.11.19.2/toolsbox/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/18 16:42
+# @Author  : play4fun
+# @File    : __init__.py
+# @Software: PyCharm
+
+"""
+__init__.py:
+"""
+
+__version__ = '1.18.11.19.2'
+__author__ = 'play4fun <play4fun@foxmail.com>'
