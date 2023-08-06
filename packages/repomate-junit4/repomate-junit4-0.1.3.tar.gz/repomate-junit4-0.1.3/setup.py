4ff667cc5f9f0a5de14eecfe3701c85dd063ef93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:58 +0000

    Add package repomate-junit4/0.1.3/repomate-junit4-0.1.3.tar.gz

---
+++ packages/repomate-junit4/repomate-junit4-0.1.3.tar.gz/repomate-junit4-0.1.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open('README.md', mode='r', encoding='utf-8') as f:
+    readme = f.read()
+
+test_requirements = [
+    'appdirs', 'daiquiri', 'pytest', 'pytest-cov>=2.5.1', 'pytest-mock',
+    'codecov'
+]
+required = ['repomate-plug', 'daiquiri', 'colored']
+
+setup(
+    name='repomate-junit4',
+    version='0.1.3',
+    description='JUnit-4.12 plugin for repomate',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Simon Larsén',
+    author_email='slarse@kth.se',
+    url='https://github.com/slarse/repomate-junit4',
+    download_url=
+    'https://github.com/slarse/repomate-junit4/archive/v0.1.3.tar.gz',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    tests_require=test_requirements,
+    install_requires=required,
+    extras_require=dict(TEST=test_requirements),
+    include_package_data=True,
+    zip_save=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+    ])
