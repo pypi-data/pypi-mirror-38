d1d18cee8c3a895ef3a54875485bd1a4211649d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:41 +0000

    Add package elastic-apm/4.0.0/elastic_apm-4.0.0-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/elastic-apm/elastic_apm-4.0.0-cp27-cp27m-manylinux1_x86_64.whl/elasticapm/instrumentation/packages/redis.py
@@ -0,0 +1,28 @@
+from elasticapm.instrumentation.packages.base import AbstractInstrumentedModule
+from elasticapm.traces import capture_span
+
+
+class RedisInstrumentation(AbstractInstrumentedModule):
+    name = "redis"
+
+    instrument_list = [("redis.client", "Redis.execute_command"), ("redis.client", "StrictRedis.execute_command")]
+
+    def call(self, module, method, wrapped, instance, args, kwargs):
+        if len(args) > 0:
+            wrapped_name = str(args[0])
+        else:
+            wrapped_name = self.get_wrapped_name(wrapped, instance, method)
+
+        with capture_span(wrapped_name, "cache.redis", leaf=True):
+            return wrapped(*args, **kwargs)
+
+
+class RedisPipelineInstrumentation(AbstractInstrumentedModule):
+    name = "redis"
+
+    instrument_list = [("redis.client", "BasePipeline.execute")]
+
+    def call(self, module, method, wrapped, instance, args, kwargs):
+        wrapped_name = self.get_wrapped_name(wrapped, instance, method)
+        with capture_span(wrapped_name, "cache.redis", leaf=True):
+            return wrapped(*args, **kwargs)
