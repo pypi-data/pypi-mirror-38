f21b242e63b20be949e4ea516b05095500e4e0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:48 +0000

    Add package nimscommon/0.1/nimscommon-0.1.tar.gz

---
+++ packages/nimscommon/nimscommon-0.1.tar.gz/nimscommon-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nimscommon',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['nimscommon'],
+      zip_safe=False)
