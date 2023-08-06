320dd6189663273bfc84cbeb9dd90ff87fc3350d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:46 +0000

    Add package aiohttp-devtools/0.10.4/aiohttp_devtools-0.10.4-py35.py36-none-any.whl

---
+++ packages/aiohttp-devtools/aiohttp_devtools-0.10.4-py35.py36-none-any.whl/aiohttp_devtools/runserver/log_handlers.py
@@ -0,0 +1,58 @@
+import logging
+import re
+
+import click
+
+from ..logs import get_log_format
+
+
+class AuxiliaryHandler(logging.Handler):
+    prefix = click.style('◆', fg='blue')
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        m = re.match(r'^(\[.*?\] )', log_entry)
+        time = click.style(m.groups()[0], fg='magenta')
+        msg = log_entry[m.end():]
+        if record.levelno in {logging.INFO, logging.DEBUG} and msg.startswith('>'):
+            if msg.endswith(' 304 0B'):
+                msg = '{} {}'.format(self.prefix, click.style(msg[2:], dim=True))
+            else:
+                msg = '{} {}'.format(self.prefix, msg[2:])
+        else:
+            msg = click.style(msg, **get_log_format(record))
+        click.echo(time + msg)
+
+
+dbtb = '/_debugtoolbar/'
+check = '?_checking_alive=1'
+
+
+class AiohttpAccessHandler(logging.Handler):
+    prefix = click.style('●', fg='blue')
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        m = re.match(r'^(\[.*?\] )', log_entry)
+        time = click.style(m.groups()[0], fg='magenta')
+        msg = log_entry[m.end():]
+        try:
+            method, path, _, code, size = msg.split(' ')
+        except ValueError:
+            click.echo(time + msg)
+        else:
+            size = fmt_size(int(size))
+            msg = '{method} {path} {code} {size}'.format(method=method, path=path, code=code, size=size)
+            if (code, size) == ('304', '0B') or path.startswith(dbtb) or path.endswith(check):
+                msg = click.style(msg, dim=True)
+            msg = '{} {}'.format(self.prefix, msg)
+            click.echo(time + msg)
+
+
+def fmt_size(num):
+    if not num:
+        return ''
+    if num < 1024:
+        return '{:0.0f}B'.format(num)
+    else:
+        return '{:0.1f}KB'.format(num / 1024)
