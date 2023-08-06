bf5a003ea9e96257344a531721174e1b71218cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:34 +0000

    Add package topsdk/18.11.18/topsdk-18.11.18.tar.gz

---
+++ packages/topsdk/topsdk-18.11.18.tar.gz/topsdk-18.11.18/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='topsdk',
+    version='18.11.18',#按日期
+    author='top',
+    author_email='xuteng.xt@alibaba-inc.com',
+    packages=find_packages(),
+    install_requires=[],
+    license='MIT',
+)
+
