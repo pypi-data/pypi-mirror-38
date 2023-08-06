2754fb537cb373542346fe16ba0ddd080277dc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:37 +0000

    Add package setcode/1.1.0/setcode-1.1.0.zip

---
+++ packages/setcode/setcode-1.1.0.zip/setcode-1.1.0/setcode.py
@@ -0,0 +1,4 @@
+import py_compile
+def readimg(imgpath):
+    print(imgpath)
+# py_compile.compile(r"./pyc.py")
---
+++ packages/setcode/setcode-1.1.0.zip/setcode-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+     name='setcode',
+     version='1.1.0',
+     py_modules=['setcode'],
+     author='courtney',
+     url='http://www..//..',
+     description='a simple code ....',
+)
