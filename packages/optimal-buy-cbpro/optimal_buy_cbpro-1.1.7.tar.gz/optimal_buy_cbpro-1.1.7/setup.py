5b99619828e6c0fbb25e0d5e7b57358c6507bffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:48 +0000

    Add package optimal-buy-cbpro/1.1.7/optimal_buy_cbpro-1.1.7.tar.gz

---
+++ packages/optimal-buy-cbpro/optimal_buy_cbpro-1.1.7.tar.gz/optimal_buy_cbpro-1.1.7/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+requires = [
+    'cbpro==1.1.4',
+    'requests==2.20.1',
+    'coinmarketcap>=5.0.3',
+    'sqlalchemy==1.2.14',
+    'python-dateutil==2.7.5',
+]
+test_requires = [
+    'pytest-cov',
+    'pytest>=3.5.0',
+]
+
+
+setup(
+    name='optimal_buy_cbpro',
+    version='1.1.7',
+    description='Buy the coins, optimally!',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Brenden Matthews',
+    author_email='brenden@diddyinc.com',
+    url='https://github.com/brndnmtthws/optimal-buy-cbpro',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts':
+            ['optimal-buy-cbpro=optimal_buy_cbpro.optimal_buy_cbpro:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
+
+    install_requires=requires,
+    tests_require=test_requires,
+    extras_require={
+        'test': test_requires,
+    },
+)
