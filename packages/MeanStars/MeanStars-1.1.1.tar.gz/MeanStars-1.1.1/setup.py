a09139651e9a3510b086741ed34806090c3247ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:36 +0000

    Add package MeanStars/1.1.1/MeanStars-1.1.1.tar.gz

---
+++ packages/MeanStars/MeanStars-1.1.1.tar.gz/MeanStars-1.1.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MeanStars",
+    version="1.1.1",
+    author="Dmitry Savransky",
+    author_email="ds264@cornell.edu",
+    description="Automated property interpolation and color calculations for main sequence stars",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dsavransky/MeanStars",
+    packages=['MeanStars'],
+    package_data={'MeanStars': ['EEM_dwarf_UBVIJHK_colors_Teff.txt']},
+    install_requires=[
+          'scipy',
+          'numpy',
+          'astropy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
