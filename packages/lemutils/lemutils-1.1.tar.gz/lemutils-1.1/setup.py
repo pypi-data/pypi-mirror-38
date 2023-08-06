e79029717e52e0ffe266bfc91614736d3b91c351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:47 +0000

    Add package lemutils/1.1/lemutils-1.1.tar.gz

---
+++ packages/lemutils/lemutils-1.1.tar.gz/lemutils-1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lemutils",
+    version="1.1",
+    author="notlem",
+    author_email="datjboy@gmail.com",
+    description="utils for yeetbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blankLeM/lemutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent",
+    ],
+)
