0e757aea1ed527977775961f68f5efac4222ea35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:58 +0000

    Add package cronably/1.2.0/cronably-1.2.0.tar.gz

---
+++ packages/cronably/cronably-1.2.0.tar.gz/cronably-1.2.0/cronably/cronably_main.py
@@ -0,0 +1,41 @@
+from cronably.actions.pre_action.pre_actions import PreActions
+from cronably.context.context import Context
+
+ext_cronably = None
+
+class Cronably(object):
+
+    def __init__(self, **kwargs):
+        global ext_cronably
+        self.annotation_params = kwargs
+        self.context = None
+        ext_cronably = self
+
+
+    def __call__(self, original_func):
+        decorator_self = self
+        def execute(*args):
+            preaction = decorator_self.pre_actions()
+            decorator_self.context.execute(original_func, decorator_self.annotation_params, preaction.repetition)
+            decorator_self.post_actions()
+        return execute
+
+    def pre_actions(self):
+        global context
+        if not self.annotation_params:
+            self.annotation_params = {}
+        actions = PreActions(self.annotation_params)
+        self.context = Context()
+        return actions
+
+
+
+    def post_actions(self):
+        print "post actions"
+
+def exist_job(name):
+    context = ext_cronably.context
+    if not context:
+        context = Context()
+    return context.check_exist_job(name)
+
