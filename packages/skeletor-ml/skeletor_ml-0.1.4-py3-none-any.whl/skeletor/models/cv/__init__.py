8b8016bb874c49bcb8ff628bd8ebe1ef7af34c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:11 +0000

    Add package skeletor-ml/0.1.4/skeletor_ml-0.1.4-py3-none-any.whl

---
+++ packages/skeletor-ml/skeletor_ml-0.1.4-py3-none-any.whl/skeletor/models/cv/__init__.py
@@ -0,0 +1,13 @@
+""" Computer vision network architectures """
+from . import vgg
+from . import dpn
+from . import lenet
+from . import senet
+from . import pnasnet
+from . import densenet
+from . import googlenet
+from . import resnet
+from . import resnext
+from . import preact_resnet
+from . import mobilenet
+from . import mobilenetv2
