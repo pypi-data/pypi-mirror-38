b859549e6496e76c4b0c0b2fe8a08d88d127b833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:19 +0000

    Add package metapost/0.0.2/metapost-0.0.2.tar.gz

---
+++ packages/metapost/metapost-0.0.2.tar.gz/metapost-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metapost",
+    version="0.0.2",
+    author="Hsufeng Lee",
+    author_email="thittalee@gmail.com",
+    description="Tools for interpreting MetaPost",
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thitta/Python-MetaPost",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
