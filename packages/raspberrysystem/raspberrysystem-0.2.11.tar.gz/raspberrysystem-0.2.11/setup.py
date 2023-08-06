20d0293542096d7c05e2bbc045f5f51fa3b85f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:44 +0000

    Add package raspberrysystem/0.2.11/raspberrysystem-0.2.11.tar.gz

---
+++ packages/raspberrysystem/raspberrysystem-0.2.11.tar.gz/raspberrysystem-0.2.11/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+def readme():
+  '''Get long description from readme file'''
+  with open('README.md') as f:
+    return f.read()
+
+
+def version():
+  '''Get version from version file'''
+  with open('VERSION') as f:
+    return f.read().strip()
+
+setuptools.setup(name='raspberrysystem',
+      version=version(),
+      entry_points={
+      'console_scripts': [
+                         'raspberrysystem = raspberrysystem.__main__:main'
+                          ]
+                 },
+      description='The funniest joke in the world',
+      long_description=readme(),
+      url='http://github.com/raubreywhite/raspberrysystem',
+      author='Richard',
+      author_email='w@rwhite.no',
+      license='MIT',
+      packages=['raspberrysystem'],
+      zip_safe=False)
