95fd865601a0d18d7a0e9d0538cb1d7edcee9481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:24 +0000

    Add package codewatch/0.0.6/codewatch-0.0.6-py3-none-any.whl

---
+++ packages/codewatch/codewatch-0.0.6-py3-none-any.whl/codewatch/node_visitor.py
@@ -0,0 +1,215 @@
+import importlib
+from collections import namedtuple
+from functools import wraps
+
+from astroid import inference_tip
+from astroid.nodes import (
+    Call,
+    ImportFrom,
+    Import,
+)
+from astroid.node_classes import NodeNG
+from astroid.exceptions import InferenceError
+from astroid.transforms import TransformVisitor
+
+
+class NodeVisitor(TransformVisitor):
+    def __init__(self, stats, rel_file_path):
+        self.stats = stats
+        self.rel_file_path = rel_file_path
+        super(NodeVisitor, self).__init__()
+
+    def _add_codewatch_annotations(self, node):
+        CodewatchNodeAnnotations = namedtuple(
+            "CodewatchNodeAnnotations", ["stats", "rel_file_path"]
+        )
+        node._codewatch = CodewatchNodeAnnotations(
+            self.stats, self.rel_file_path
+        )
+        return node
+
+    def _transform(self, node):
+        cls = node.__class__
+        if cls not in self.transforms:
+            # no transform registered for this class of node
+            return node
+        annotated_node = self._add_codewatch_annotations(node)
+        return super(NodeVisitor, self)._transform(annotated_node)
+
+
+def _astroid_interface_for_visitor(visitor_function):
+    """Turn codewatch visitors into astroid-compatible transform functions
+
+    codewatch visitors can make use of 3 args, the node, stats, and the
+    relative file path you were visited for
+
+    astroid transforms must take only the node
+
+    By annotating the node with stats and relative file path, we can make our
+    codewatch visitors compatible with astroid transform functions.
+    """
+
+    @wraps(visitor_function)
+    def call_visitor(annotated_node, *args, **kwargs):
+        return visitor_function(
+            annotated_node,
+            annotated_node._codewatch.stats,
+            annotated_node._codewatch.rel_file_path,
+            *args,
+            **kwargs
+        )
+
+    return call_visitor
+
+
+def visit(node_type, predicate=None, change_node_inference=None):
+    def decorator(fn):
+        NodeVisitorMaster.register_visitor(
+            node_type, fn, predicate, change_node_inference
+        )
+        return fn
+
+    return decorator
+
+
+def track_troublesome_module_usages(qname, importer=None):
+    if importer is None:
+        importer = importlib.import_module
+
+    module_name = '.'.join(qname.split('.')[:-1])
+    trouble_attribute = qname.split('.')[-1]
+
+    def track_import(stats, rel_file_path):
+        file_module = rel_file_path.split('.')
+        stats = stats.namespaced('TROUBLESOME').namespaced(
+            '.'.join(file_module[:-1]),
+        )
+        stats.increment('IMPORT_COUNT')
+
+    def visit_import(import_node, stats, rel_file_path):
+        for name, alias in import_node.names:
+            if name == qname:
+                track_import(stats, rel_file_path)
+        return import_node
+
+    def visit_importfrom(import_from_node, stats, rel_file_path):
+        modname = import_from_node.modname
+
+        for name, alias in import_from_node.names:
+            if name == '*':
+                module = importer(module_name)
+                if trouble_attribute in dir(module):
+                    track_import(stats, rel_file_path)
+            else:
+                imported_qname = modname + '.' + name
+                if imported_qname == qname:
+                    track_import(stats, rel_file_path)
+        return import_from_node
+
+    NodeVisitorMaster.register_visitor(Import, visit_import, None)
+    NodeVisitorMaster.register_visitor(ImportFrom, visit_importfrom, None)
+
+
+def count_calling_files(stats_namespace, expected_callable_qname):
+    if stats_namespace is None:
+        raise Exception("count_calling_files() requires a valid namespace")
+
+    expected_callable_name = expected_callable_qname.split(".")[-1]
+
+    def record_stats(stats, rel_file_path):
+        stats = stats.namespaced(stats_namespace)
+        stats.increment(rel_file_path)
+
+    def visit_call(call_node, stats, rel_file_path):
+        """A visitor function to gather call stats.
+
+        astroid.nodes.Call nodes are from one of two forms:
+          symbol()
+        or
+          some.expression.attribute()
+
+        AST are structured differently in each case. We detect and handle both.
+        """
+        callable_as_attribute = hasattr(call_node.func, "attrname")
+        if callable_as_attribute:
+            callable_name = call_node.func.attrname
+        else:
+            if not hasattr(call_node.func, "name"):
+                return call_node
+            callable_name = call_node.func.name
+
+        # Optimization: Start with a cheap guard before astroid inference
+        if callable_name != expected_callable_name:
+            return call_node
+
+        try:
+            inferred_types = call_node.func.inferred()
+        except InferenceError:
+            return call_node
+
+        found_matching_inferred_qname = any(
+            inferred_type.qname() == expected_callable_qname
+            for inferred_type in inferred_types
+        )
+
+        if not found_matching_inferred_qname:
+            return call_node
+
+        record_stats(stats, rel_file_path)
+
+        return call_node
+
+    NodeVisitorMaster.register_visitor(Call, visit_call, None)
+
+
+class NodeVisitorMaster(object):
+    node_visitor_registry = []
+
+    @classmethod
+    def register_visitor(
+        cls, node, visitor_function, predicate=None, change_node_inference=None
+    ):
+        wrapped = _astroid_interface_for_visitor(visitor_function)
+
+        if not issubclass(node, NodeNG):
+            raise Exception(
+                "visitor_function {v} registered for invalid node type. "
+                "Please use a NodeNG subclass from the astroid.nodes module."
+            )
+
+        cls.node_visitor_registry.append(
+            (node, wrapped, predicate, change_node_inference)
+        )
+
+    @classmethod
+    def _initialize_node_visitors(cls, stats, rel_file_path):
+        initialized_node_visitors = []
+        for (
+            node,
+            node_visitor_function,
+            predicate,
+            change_node_inference,
+        ) in cls.node_visitor_registry:
+            node_visitor_obj = NodeVisitor(stats, rel_file_path)
+
+            if change_node_inference is not None:
+                node_visitor_obj.register_transform(
+                    node, inference_tip(change_node_inference), predicate,
+                )
+
+            node_visitor_obj.register_transform(
+                node,
+                node_visitor_function,
+                predicate,
+            )
+            initialized_node_visitors.append(node_visitor_obj)
+        return initialized_node_visitors
+
+    @classmethod
+    def visit(cls, stats, node, rel_file_path):
+        node_visitors_initialized = cls._initialize_node_visitors(
+            stats, rel_file_path
+        )
+
+        for node_visitor_initialized in node_visitors_initialized:
+            node_visitor_initialized.visit(node)
---
+++ packages/codewatch/codewatch-0.0.6-py3-none-any.whl/tests/config_modules/integration_config.py
@@ -0,0 +1,156 @@
+"""
+This module is used by integration tests
+"""
+
+import os
+
+from codewatch import assertion, visit
+
+from astroid import nodes, MANAGER
+from astroid.builder import AstroidBuilder
+
+
+# only visit this file itself
+def file_filter(file_name):
+    this_file = os.path.basename(__file__)
+
+    # make sure it's a .py file (not .pyc)
+    this_file = os.path.splitext(this_file)[0] + '.py'
+    return file_name == this_file
+
+
+def directory_filter(_):
+    return True
+
+
+class A(object):
+    class objects(object):
+        @staticmethod
+        def get():
+            pass
+
+
+A.objects.get()
+
+
+def is_a_object_get(node):
+    if not hasattr(node.func, "expr"):
+        return False
+
+    if not hasattr(node.func.expr, "expr"):
+        return False
+
+    if not node.func.expr.expr.name == 'A':
+        return False
+
+    if not node.func.expr.attrname == 'objects':
+        return False
+
+    if not node.func.attrname == 'get':
+        return False
+
+    return True
+
+
+def infer_objects_get_as_a(node, context=None):
+    builder = AstroidBuilder(MANAGER)
+    m = builder.string_build(
+        """\
+class A(object):
+    class objects(object):
+        @staticmethod
+        def get():
+            pass"""
+    )
+    class_node = m.body[0]
+
+    return (class_node.instantiate_class(),)
+
+
+@visit(
+    nodes.Call,
+    predicate=is_a_object_get,
+    change_node_inference=infer_objects_get_as_a,
+)
+def call_visitor(node, stats, _rel_file_path):
+    inf_types = node.inferred()
+    stats.append("inferred A.objects.get()", inf_types)
+
+
+def always_true_predicate(_node):
+    return True
+
+
+@visit(
+    nodes.Call,
+    change_node_inference=infer_objects_get_as_a,
+    predicate=always_true_predicate,
+)
+def predicate_visitor_inference(_node, stats, _rel_file_path):
+    stats.increment("predicate_visitor_inference")
+
+
+@visit(
+    nodes.Call,
+    predicate=always_true_predicate,
+)
+def predicate_visitor(_node, stats, _rel_file_path):
+    stats.increment("predicate_visitor")
+
+
+@visit(nodes.Expr)
+def count_expressions(node, stats, _rel_file_path):
+    stats.increment('num_expressions')
+    return node
+
+
+@visit(nodes.ImportFrom)
+def count_imports(node, stats, _rel_file_path):
+    stats.increment('num_import_from')
+    return node
+
+
+@assertion()
+def correctly_rewritten_inference(stats):
+    inference_results = stats.get('inferred A.objects.get()')
+    if len(inference_results) != 1:
+        return (
+            False,
+            "Too many possible inferences {i}".format(i=inference_results),
+        )
+    qname = inference_results[0].qname()
+    return qname == '.A', "bad inferrence {qname}".format(qname=qname)
+
+
+@assertion()
+def num_import_from_more_than_zero(stats):
+    err = 'num_import_from is not more than 0'
+    return stats.get('num_import_from', 0) > 0, err
+
+
+@assertion()
+def expressions_more_than_zero(stats):
+    return stats.get('num_expressions', 0) > 0, 'not more than zero'
+
+
+@assertion(label='custom_label_always_true')
+def always_true(_stats):
+    return True, None
+
+
+@assertion(stats_namespaces=['level1'])
+def always_false(_stats):
+    return False, 'should always be false'
+
+
+@assertion()
+def predicate_works(stats):
+    return stats.get('predicate_visitor', -1) > 0, 'predicate not working'
+
+
+@assertion()
+def predicate_inference_works(stats):
+    return (
+        stats.get('predicate_visitor_inference', -1) > 0,
+        'predicate not working'
+    )
