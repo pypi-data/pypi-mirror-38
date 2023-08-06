d8b1cabea4af274b2264bdb6dd42a658f6c4e1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:26 +0000

    Add package encodermap/0.0.5/encodermap-0.0.5.tar.gz

---
+++ packages/encodermap/encodermap-0.0.5.tar.gz/encodermap-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("description.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='encodermap',
+      version='0.0.5',
+      description='python library for dimensionality reduction',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Tobias Lemke',
+      url="https://github.com/AG-Peter/encodermap",
+      packages=['encodermap'],
+      install_requires=[
+          'numpy',
+          'matplotlib',
+          'scipy',
+          'MDAnalysis',
+          'tqdm'
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent",
+      ])
