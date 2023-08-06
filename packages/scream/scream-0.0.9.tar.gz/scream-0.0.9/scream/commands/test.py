c6206ad804435714d48ee722fcf734d20ea752e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:49 +0000

    Add package scream/0.0.9/scream-0.0.9.tar.gz

---
+++ packages/scream/scream-0.0.9.tar.gz/scream-0.0.9/scream/commands/test.py
@@ -0,0 +1,33 @@
+import logging
+import subprocess
+
+from scream.detect_changed_packages import get_changed_packages_and_dependents
+
+
+def test(dry_run=False, all=False):
+    """Tests all packages that have changed, or packages who's dependencies have changed.
+    The tox testing environments can support running package tests against different python versions, etc.
+    Args:
+        dry_run (bool): if True, just print what packages have changed, and what packages would be tested.
+        all (bool): if True, immediately test all packages (overrides dry-run).
+
+    """
+    if all:
+        logging.info("Testing all packages...")
+        subprocess.call(["tox"])
+        return
+
+    impacted_packages = get_changed_packages_and_dependents()
+
+    if dry_run or not impacted_packages:
+        return
+
+    toxenvs_to_test_list = []
+
+    for _, package in impacted_packages.items():
+        for pyversion in package.tox_pyversions:
+            toxenvs_to_test_list.append(pyversion + '-' + package.package_name)
+
+    toxenvs_to_test_str = ','.join(toxenvs_to_test_list)
+
+    subprocess.call(["tox", "-e", toxenvs_to_test_str])
