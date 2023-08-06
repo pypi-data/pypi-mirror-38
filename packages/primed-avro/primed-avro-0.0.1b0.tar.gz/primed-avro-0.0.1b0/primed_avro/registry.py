23c00eaf64eff6eadc656bde0babbb9349f5a63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:39 +0000

    Add package primed-avro/0.0.1b0/primed-avro-0.0.1b0.tar.gz

---
+++ packages/primed-avro/primed-avro-0.0.1b0.tar.gz/primed-avro-0.0.1b0/primed_avro/registry.py
@@ -0,0 +1,32 @@
+import requests
+
+
+class ConfluentSchemaRegistryClient:
+    def __init__(self, url):
+        self.url = url
+
+    def get_schema(self, subject, version="latest"):
+        r = requests.get(
+            "{url}/subjects/{subject}/versions/{version}".format(
+                url=self.url, subject=subject, version=version
+            ),
+            headers={"Content-Type": "application/vnd.schemaregistry.v1+json"},
+        )
+
+        if r.status_code == 200:
+            js = r.json()
+
+            if "error_code" in js:
+                raise Exception(
+                    "failed to retrieve schema, error_code: {error_code}, msg: {msg}".format(
+                        error_code=js["error_code"], msg=js["msg"]
+                    )
+                )
+
+            return SchemaMeta(**js)
+        else:
+            raise Exception(
+                "failed to retrieve schema due to connection problems, status code: {status_code}".format(
+                    status_code=r.status_code
+                )
+            )
