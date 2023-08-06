67e43da4d6381294eb63da0b5070a37ec5ea6f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:02 +0000

    Add package protobackend/0.2.1/protobackend-0.2.1.tar.gz

---
+++ packages/protobackend/protobackend-0.2.1.tar.gz/protobackend-0.2.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name='protobackend',
+	version='0.2.1',
+	packages=['protobackend'],
+        description = 'Generic backend',
+        author = 'Michael Chow',
+        author_email = 'michael@datacamp.com',
+        url = 'https://github.com/datacamp/protobackend'
+)
