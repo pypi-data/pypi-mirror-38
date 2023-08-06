e61f8a0df1dba7a196e7956e7abf3163536bad24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:45 +0000

    Add package gapipy/2.17.0/gapipy-2.17.0.tar.gz

---
+++ packages/gapipy/gapipy-2.17.0.tar.gz/gapipy-2.17.0/gapipy/resources/tour/__init__.py
@@ -0,0 +1,19 @@
+# flake8: NOQA
+from .accommodation import Accommodation
+from .activity import Activity
+from .departure import Departure
+from .departure_component import DepartureComponent
+from .image import Image
+from .itinerary import (
+    Duration,
+    Itinerary,
+    ItineraryHighlights,
+    ItineraryMedia,
+    OptionalActivity,
+)
+from .promotion import Promotion
+from .single_supplement import SingleSupplement
+from .tour import Tour
+from .tour_category import TourCategory
+from .tour_dossier import TourDossier
+from .transport import Transport
