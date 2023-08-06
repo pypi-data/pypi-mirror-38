83aa5dc6434de753cff6422c1a9979667a75b36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:48 +0000

    Add package xbox-smartglass-core/1.0.12/xbox-smartglass-core-1.0.12.tar.gz

---
+++ packages/xbox-smartglass-core/xbox-smartglass-core-1.0.12.tar.gz/xbox-smartglass-core-1.0.12/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name="xbox-smartglass-core",
+    version="1.0.12",
+    author="OpenXbox",
+    description="A library to interact with the Xbox One gaming console via the SmartGlass protocol.",
+    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    license="GPL",
+    keywords="xbox one smartglass",
+    url="https://github.com/OpenXbox/xbox-smartglass-core-python",
+    packages=[
+        'xbox.sg',
+        'xbox.sg.utils',
+        'xbox.sg.scripts',
+        'xbox.sg.packet'
+    ],
+    namespace_packages=['xbox'],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+    install_requires=[
+        'xbox-webapi>=1.1.2',
+        'construct==2.9.41',
+        'cryptography==2.3.1',
+        'appdirs==1.4.3',
+        'gevent==1.3.7',
+        'urwid==2.0.1',
+        'dpkt==1.9.1',
+        'aenum==2.1.2;python_version<="3.5"'
+    ],
+    tests_require=[
+        'pytest',
+        'flake8',
+        'tox'
+    ],
+    extras_require={
+        'dev': [
+            'bumpversion',
+            'watchdog',
+            'coverage',
+            'Sphinx',
+            'wheel',
+            'twine'
+        ]
+    },
+    test_suite="tests",
+    entry_points={
+        'console_scripts': [
+            'xbox-pcap=xbox.sg.scripts.pcap:main',
+            'xbox-discover=xbox.sg.scripts.discover:main',
+            'xbox-poweron=xbox.sg.scripts.poweron:main',
+            'xbox-poweroff=xbox.sg.scripts.poweroff:main',
+            'xbox-client=xbox.sg.scripts.client:main',
+            'xbox-recrypt=xbox.sg.scripts.recrypt:main',
+            'xbox-text=xbox.sg.scripts.text:main',
+            'xbox-input=xbox.sg.scripts.input:main',
+            'xbox-tui=xbox.sg.scripts.tui:main',
+        ]
+    }
+)
