de5d2ce69c970cae36cf2c4f4bb5e429bce3b7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:34 +0000

    Add package gesture-recognizer/0.3.7/gesture_recognizer-0.3.7-py3-none-any.whl

---
+++ packages/gesture-recognizer/gesture_recognizer-0.3.7-py3-none-any.whl/gesture_recognizer/GestureRecognizer.py
@@ -0,0 +1,27 @@
+# importing requried modules
+import cv2
+import numpy
+import os
+import threading
+import time
+import importlib.util
+
+class GestureRecognizer(object):
+	def __init__(self, interval=1.0/30.0, print_pos=True):
+		self.interval = interval
+		self.print_pos = print_pos
+	def start_recognizing(self):
+
+		# import the class
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		filename_fist_rec = 'FistRecognizer.py'
+		full_path = "%s/%s" % (dir_path, filename_fist_rec)
+		spec = importlib.util.spec_from_file_location("FistRecognizer", full_path)
+		gesture_recognition = importlib.util.module_from_spec(spec)
+		spec.loader.exec_module(gesture_recognition)
+		FistRecognizerService = gesture_recognition.FistRecognizer(interval, print_pos)
+
+		# threading code
+		thread = threading.Thread(target=FistRecognizerService.recognize_fist, args=())
+		thread.daemon = True # Daemonize thread
+		thread.start() # Start the execution
---
+++ packages/gesture-recognizer/gesture_recognizer-0.3.7-py3-none-any.whl/gesture_recognizer/test.py
@@ -0,0 +1,6 @@
+import GestureRecognizer as gr
+import time
+rec = gr.GestureRecognizer()
+rec.start_recognizing()
+time.sleep(10)
+time.sleep(10)
