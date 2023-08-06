02e6b503b7b58fc9ad32aa3ac1423f86f99c58cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:05 +0000

    Add package barycorrpy/0.2.2/barycorrpy-0.2.2.tar.gz

---
+++ packages/barycorrpy/barycorrpy-0.2.2.tar.gz/barycorrpy-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+
+setup(name='barycorrpy',
+      version='0.2.2',
+      description='Barycentric Velocity correction at 1 cm/s level',
+      long_description=readme(),
+      url='https://github.com/shbhuk/barycorrpy',
+      author='Shubham Kanodia',
+      author_email='shbhuk@gmail.com',
+      install_requires=['astropy>2','jplephem','numpy','scipy','astroquery'],
+      packages=['barycorrpy'],
+      license='GPLv3',
+      classifiers=['Topic :: Scientific/Engineering :: Astronomy'],
+      keywords='Barycentric Correction Astronomy Spectroscopy Radial Velocity',
+      include_package_data=True
+      )
+      
