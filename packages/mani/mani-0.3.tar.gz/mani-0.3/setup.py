094c1420d8ad901f389064aceb493926d53dd807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:16 +0000

    Add package mani/0.3/mani-0.3.tar.gz

---
+++ packages/mani/mani-0.3.tar.gz/mani-0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name='mani',
+      version='0.3',
+      packages=['mani'],
+      install_requires=['redis',
+                        'python-redis-lock'],
+      setup_requires=['setuptools'],
+      tests_require=['flake8'],
+      description='Distributed cron',
+      author='Sherin Kurian',
+      author_email='sherinkurian.123@gmail.com',
+      url='https://github.com/sherinkurian/mani',
+      download_url='https://github.com/sherinkurian/mani/tarball/0.1',
+      keywords=['cron', 'distributed', 'redis'],
+      package_data={'mani.version': ['VERSION']},
+      classifiers=['Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Operating System :: Unix',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   'Topic :: System',
+                   'Topic :: Software Development',
+                   'Development Status :: 4 - Beta'])
