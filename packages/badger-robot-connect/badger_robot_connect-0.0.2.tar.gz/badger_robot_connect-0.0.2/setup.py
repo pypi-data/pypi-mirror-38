dafe5585920342a438bd537ad4d2e2c04f2dcd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:47 +0000

    Add package badger-robot-connect/0.0.2/badger_robot_connect-0.0.2.tar.gz

---
+++ packages/badger-robot-connect/badger_robot_connect-0.0.2.tar.gz/badger_robot_connect-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='badger_robot_connect',
+    version='0.0.2',
+    author='Tommy Vandermolen',
+    author_email='tommy.vandermolen@gmail.com',
+    description='Wireless Control Client for BADGER Robot',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
