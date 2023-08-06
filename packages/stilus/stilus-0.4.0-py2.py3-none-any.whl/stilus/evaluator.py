26c099c3298adec9fbe07058f3e9151777c63268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:31 +0000

    Add package stilus/0.4.0/stilus-0.4.0-py2.py3-none-any.whl

---
+++ packages/stilus/stilus-0.4.0-py2.py3-none-any.whl/stilus/evaluator.py
@@ -0,0 +1,9 @@
+
+class Evaluator:
+
+    def __init__(self, root, options):
+        self.root = root
+        self.options = options
+
+    def evaluate(self):
+        return self.root
---
+++ packages/stilus/stilus-0.4.0-py2.py3-none-any.whl/stilus/stilus.py
@@ -0,0 +1,29 @@
+from stilus.parser import Parser
+from stilus.visitor.compiler import Compiler
+
+
+class Renderer:
+
+    def __init__(self, s: str, options: dict):
+        self.s = s
+        self.options = options
+
+    def render(self):
+
+        self.parser = Parser(self.s, self.options)
+        ast = self.parser.parse()
+
+        # self.evaluator = Evaluator(ast, self.options)
+        # ast = self.evaluator.evaluate()
+
+        # self.normalizer = Normalizer(ast, self.options)
+        # ast = self.normalizer.normalize()
+
+        self.compiler = Compiler(ast, self.options)
+        css = self.compiler.compile()
+
+        return css
+
+
+def render(s, options) -> str:
+    return Renderer(s, options).render()
