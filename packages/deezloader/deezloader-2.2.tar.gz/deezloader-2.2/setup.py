c34078ece1f9d6923dd4a4017f1931d727791889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:47 +0000

    Add package deezloader/2.2/deezloader-2.2.tar.gz

---
+++ packages/deezloader/deezloader-2.2.tar.gz/deezloader-2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+      name = "deezloader",
+      version = "2.2",
+      description = "Downloads songs, albums or playlists from deezer",
+      license = "Apache-2.0",
+      author = "An0nimia",
+      author_email = "An0nimia@protonmail.com",
+      packages = ["deezloader"],
+      install_requires = ['bs4', 'mutagen', 'requests', 'spotipy', 'tqdm']
+)
