4c9d740ebd8cf7c939ad292b04620d1d9afb7251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:37 +0000

    Add package kutt/1.1.4/kutt-1.1.4.tar.gz

---
+++ packages/kutt/kutt-1.1.4.tar.gz/kutt-1.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='kutt',
+    version='1.1.4',
+    author='Amirali Esfandiari',
+    author_email='amiralinull@gmail.com',
+    url='http://gitlab.com/univa64/kutt-cli',
+    py_modules=['kutt'],
+    install_requires=['click', 'requests'],
+    packages=['kutt'],
+    package_dir={'kutt': 'kutt'},
+    entry_points = '''
+        [console_scripts]
+        kutt=kutt.cli:cli
+    '''
+)
