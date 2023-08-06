c2fb38941d4b1c0a874e98640d65c642b43e136f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:13 +0000

    Add package ciperror/1.0.25/ciperror-1.0.25-py3-none-any.whl

---
+++ packages/ciperror/ciperror-1.0.25-py3-none-any.whl/ciperror/callback_errors.py
@@ -0,0 +1,10 @@
+from ciperror import BaseCipError
+
+
+class CallBackError(BaseCipError):
+    def __init__(self, service, message):
+        super().__init__(
+            code="CBE001",
+            message="Erro ao realizar callback da requisição para o serviço: {} - message: {}".format(service, message),
+            friendly_message="Erro ao notificar serviço {}".format(service),
+            http_status=500)
