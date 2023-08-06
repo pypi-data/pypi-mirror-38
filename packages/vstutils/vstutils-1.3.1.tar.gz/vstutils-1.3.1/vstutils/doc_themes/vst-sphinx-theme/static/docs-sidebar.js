e6ebb30416235cfc9e99c28ed8ad139962a74fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:29 +0000

    Add package vstutils/1.3.1/vstutils-1.3.1.tar.gz

---
+++ packages/vstutils/vstutils-1.3.1.tar.gz/vstutils-1.3.1/vstutils/doc_themes/vst-sphinx-theme/static/docs-sidebar.js
@@ -0,0 +1,52 @@
+function saveHideMenuSettings()
+{
+    if(window.innerWidth > 991)
+    {
+        if($('body').hasClass('sidebar-collapse'))
+        {
+            localStorage.setItem('hideMenuInDocs', false);
+        }
+        else
+        {
+            localStorage.setItem('hideMenuInDocs', true);
+        }
+    }
+}
+
+$( document ).ready(function() {
+
+    var headings = $('.sidebar-menu .toctree-l1');
+
+    for(var i=0; i<headings.length; i++)
+    {
+        var t = headings[i].children[0].innerText;
+        $(headings[i].children[0]).empty();
+        $(headings[i].children[0]).prepend('<i class="nav-icon fa fa-book"></i>');
+
+        if(headings[i].children[1])
+        {
+            $(headings[i].children[0]).append('<p><span class="li-header-span "><i class=" li-header-span-i menu-text-data">' + t + '</i></span><i class="right fa fa-angle-left ico-menu"></i></p>');
+        }
+        else
+        {
+            $(headings[i].children[0]).append('<p><span class="li-header-span "><i class=" li-header-span-i menu-text-data">' + t + '</i></span></p>');
+        }
+
+        $(headings[i].children[0]).addClass("nav-link");
+        $(headings[i]).addClass("nav-item");
+
+        if(headings[i].children[1])
+        {
+            let child_ul = headings[i].children[1];
+
+            $(child_ul).addClass("menu-treeview-menu nav nav-treeview");
+        }
+    }
+
+    let hide_menu = localStorage.getItem('hideMenuInDocs');
+
+    if(hide_menu && hide_menu == 'true')
+    {
+        $('body').addClass('sidebar-collapse')
+    }
+});
