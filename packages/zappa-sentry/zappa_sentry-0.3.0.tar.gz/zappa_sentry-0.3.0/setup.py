7441c6d0b0aafaddcd6d1c9025f0c186ace3cc97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:37 +0000

    Add package zappa-sentry/0.3.0/zappa_sentry-0.3.0.tar.gz

---
+++ packages/zappa-sentry/zappa_sentry-0.3.0.tar.gz/zappa_sentry-0.3.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+  name = 'zappa_sentry',
+  packages = ['zappa_sentry'],
+  version = '0.3.0',
+  description = 'Easy integration with sentry for zappa apps',
+  author = 'João Miguel Neves',
+  author_email = 'joao@silvaneves.org',
+  url = 'https://github.com/jneves/zappa-sentry',
+  download_url = 'https://github.com/jneves/zappa-sentry/archive/0.2.3.tar.gz',
+  keywords = 'logging zappa sentry',
+  install_requires=[
+    'sentry-sdk'
+  ],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6',
+  ]
+)
