0e6e8c56f22111412d625ee89b723ba835948d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package elm/0.1.3/elm-0.1.3-py2.py3-none-any.whl

---
+++ packages/elm/elm-0.1.3-py2.py3-none-any.whl/elm/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Augusto Almeida'
+__email__ = 'augustocbenvenuto@gmail.com'
+__version__ = '0.1.3'
+
+from .elmk import ELMKernel
+from .elmr import ELMRandom
+from .mltools import *
