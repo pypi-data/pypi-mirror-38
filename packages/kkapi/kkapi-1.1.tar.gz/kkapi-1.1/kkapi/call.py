c8b465ea1e8f61168629380bd2dc6c7bf923b383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:53 +0000

    Add package kkapi/1.1/kkapi-1.1.tar.gz

---
+++ packages/kkapi/kkapi-1.1.tar.gz/kkapi-1.1/kkapi/call.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from akad.ttypes import MediaType
+from .client import LINE
+from types import *
+
+def loggedIn(func):
+    def checkLogin(*args, **kwargs):
+        if args[0].isLogin:
+            return func(*args, **kwargs)
+        else:
+            args[0].callback.other('You want to call the function, you must login to LINE')
+    return checkLogin
+    
+class LineCall(object):
+    isLogin = False
+    client  = None
+
+    def __init__(self, client):
+        if type(client) is not LINE:
+            raise Exception("You need to set LineClient instance to initialize LineCall")
+        self.client = client
+        
+    def acquireCallRoute(self, to):
+        return self.client.call.acquireCallRoute(to)
+        
+    def acquireGroupCallRoute(self, groupId, mediaType=MediaType.AUDIO):
+        return self.client.call.acquireGroupCallRoute(groupId, mediaType)
+        
+    def acquireGroupVideoCallRoute(self, groupId, mediaType=MediaType.VIDEO):
+        return self.call.acquireGroupVideoCallRoute(groupId, mediaType)
+
+    def getGroupCall(self, ChatMid):
+        return self.client.call.getGroupCall(ChatMid)
+        
+    def inviteIntoGroupCall(self, chatId, contactIds=[], mediaType=MediaType.AUDIO):
+        return self.client.call.inviteIntoGroupCall(chatId, contactIds, mediaType)
+        
+    def inviteIntoGroupVideoCall(self, chatId, contactIds=[], mediaType=MediaType.VIDEO):
+        return self.call.inviteIntoGroupVideoCall(chatId, contactIds, mediaType)
+        
+class Call(object):
+    isLogin = False
+
+    def __init__(self):
+        self.isLogin = True
+        
+    @loggedIn
+    def acquireCallRoute(self, to):
+        return self.call.acquireCallRoute(to)
+        
+    @loggedIn
+    def acquireGroupCallRoute(self, groupId, mediaType=MediaType.AUDIO):
+        return self.call.acquireGroupCallRoute(groupId, mediaType)
+
+    @loggedIn
+    def getGroupCall(self, ChatMid):
+        return self.call.getGroupCall(ChatMid)
+        
+    @loggedIn
+    def inviteIntoGroupCall(self, chatId, contactIds=[], mediaType=MediaType.AUDIO):
+        return self.call.inviteIntoGroupCall(chatId, contactIds, mediaType)
