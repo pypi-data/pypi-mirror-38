5c60f436662665e115bcad2405327e5d91ca77e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:06 +0000

    Add package pypelearn/0.0.19/pypelearn-0.0.19.tar.gz

---
+++ packages/pypelearn/pypelearn-0.0.19.tar.gz/pypelearn-0.0.19/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pypelearn",
+    version="0.0.19",
+    author="Alex Harston",
+    author_email="alex@harston.io",
+    description="A pipeline library for testing and validating time series data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexharston/pypelearn",
+    license="MIT",
+    python_requires='>=3',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='pypelearn machine learning time series pipeline',
+    packages=find_packages(),
+    install_requires=['statsmodels', 'argparse', 'pandas', 'matplotlib', 'numpy', 'scipy', 'sklearn', 'pickle', 'tensorflow'],
+)
