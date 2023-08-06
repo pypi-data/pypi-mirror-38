008e2f33cbfbabe43b5788b336b6b5d588490e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:14 +0000

    Add package pixelhouse/0.2.0/pixelhouse-0.2.0.tar.gz

---
+++ packages/pixelhouse/pixelhouse-0.2.0.tar.gz/pixelhouse-0.2.0/pixelhouse/_version.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.0"
+
+"""
+Version: 0.2.0: Improved gradients (multi colors, discrete, and transparency). Changed gradient API. Fixed a few canvas bugs. Added canvas properties like C.xmax.
+Version: 0.1.5: Escape animations with any key
+Version: 0.1.4: Added a wave effect transform
+Version: 0.1.3: Moved ColorLoversPalette to just palette
+Version: 0.1.2: Fixed MANIFEST! Missing assets
+Version: 0.1.1: Refactored .filter to .filters to prevent namespace collision
+Version: 0.1.0: First release! Added subpixel resolution to prior work.
+"""
