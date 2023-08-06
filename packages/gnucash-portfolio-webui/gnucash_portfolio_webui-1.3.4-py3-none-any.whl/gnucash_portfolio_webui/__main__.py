ef48c00ea5504a0e9631771ce490714163026cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:26 +0000

    Add package gnucash-portfolio-webui/1.3.4/gnucash_portfolio_webui-1.3.4-py3-none-any.whl

---
+++ packages/gnucash-portfolio-webui/gnucash_portfolio_webui-1.3.4-py3-none-any.whl/gnucash_portfolio_webui/__main__.py
@@ -0,0 +1,5 @@
+"""
+This file used to contain the same content as app.py. It was used to run the py <foldername> 
+directly. If this is needed, try to reuse the code in app.py.
+Having app.py file to run is needed for pywebview and running the Flask app from other Python code.
+"""
