03ed057140a1e624f2dd82fe58eb46fc179f3f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:37 +0000

    Add package pasted-client/0.0.6/pasted-client-0.0.6.tar.gz

---
+++ packages/pasted-client/pasted-client-0.0.6.tar.gz/pasted-client-0.0.6/setup.py
@@ -0,0 +1,49 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import setuptools
+
+
+with open('README.rst', 'r') as r_file:
+    README = r_file.read()
+
+
+setuptools.setup(
+    name='pasted-client',
+    version='0.0.6',
+    description='Pasted client. Paste files or STDIN to a raw object.',
+    long_description=README,
+    author='Kevin Carter',
+    author_email='kevin@cloudnull.com',
+    url='http://github.com/cloudnull/pasted-client',
+    install_requires=[
+        'requests'
+    ],
+    py_modules=[
+        'pasteraw'
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+    entry_points = {
+        "console_scripts": [
+            "pasted = pasted_client.pasted:cli"
+        ]
+    }
+)
