0e4fb0b524b4a02005ea48785c92c93ee69424d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:50 +0000

    Add package seletivo-lattes/1.0.1/seletivo-lattes-1.0.1.tar.gz

---
+++ packages/seletivo-lattes/seletivo-lattes-1.0.1.tar.gz/seletivo-lattes-1.0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='seletivo-lattes',
+      version='1.0.1',
+      description='A simple script to get LATTES CV from a list of candidates in the CNPq Database',
+      url='http://github.com/storopoli/seletivo-lattes',
+      author='Jose Eduardo Storopoli',
+      author_email='thestoropoli@gmail.com',
+      license='MIT',
+      packages=['seletivo-lattes'],
+      install_requires=[
+          'pandas',
+          'requests',
+          'bs4',
+          'time',
+      ],
+      zip_safe=False)
