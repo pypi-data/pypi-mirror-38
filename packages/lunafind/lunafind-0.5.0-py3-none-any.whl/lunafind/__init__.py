d8d89419c00684c990027392415927bd2dd1e0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:08 +0000

    Add package lunafind/0.5.0/lunafind-0.5.0-py3-none-any.whl

---
+++ packages/lunafind/lunafind-0.5.0-py3-none-any.whl/lunafind/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2018 miruka
+# This file is part of lunafind, licensed under LGPLv3.
+
+import logzero
+import blessed
+
+TERM = blessed.Terminal()
+
+LOG = logzero.setup_logger(
+    formatter = logzero.LogFormatter(
+        datefmt = "%H:%M:%S",
+        fmt     = ("%(color)s%(levelname)1.1s %(asctime)s | "
+                   "%(message)s%(end_color)s"),
+        colors  = {10: TERM.cyan, 20: TERM.green, 30: TERM.yellow,
+                   40: TERM.red, 50: TERM.magenta}
+    )
+)
+
+# pylint: disable=wrong-import-position
+
+from .__about__ import __doc__
+from . import config
+config.reload()
+
+from .post import Post
+from .stream import Stream
+from .album import Album
