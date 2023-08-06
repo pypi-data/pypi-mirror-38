21e830d0f368eb3fc4ad5d27a33895ec0efc2c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package xterm/0.1.9/xterm-0.1.9.tar.gz

---
+++ packages/xterm/xterm-0.1.9.tar.gz/xterm-0.1.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name="xterm",
+	version="0.1.9",
+	author="mat",
+	author_email="fake@email.lol",
+	description="Custom keyboard and mouse events for xTerm",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://repl.it/@mat1/Terminal-thing",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
