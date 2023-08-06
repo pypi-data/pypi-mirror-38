9a1d1af47cc2f2ad25b97b89cf64e23f6c2ccd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:49 +0000

    Add package django-test-utils3/0.11.4/django-test-utils3-0.11.4.tar.gz

---
+++ packages/django-test-utils3/django-test-utils3-0.11.4.tar.gz/django-test-utils3-0.11.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-test-utils3',
+    version='0.11.4',
+    packages=find_packages(),
+    author='Jon Froiland',
+    author_email='jon@crowdkeep.com',
+    description='A package to help testing in Django',
+    url='https://github.com/phroiland/django-test-utils',
+    download_url='https://github.com/phroiland/django-test-utils',
+    test_suite='test_project.run_tests.run_tests',
+    include_package_data=True,
+    install_requires=[
+        'BeautifulSoup4',
+        'twill-3', 'django', 'six', 'django-celery'
+    ]
+)
