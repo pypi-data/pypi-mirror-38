b4d1424b615a1a805fdfd416c45faaac14d6589c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:17 +0000

    Add package beerme/0.1/beerme-0.1.tar.gz

---
+++ packages/beerme/beerme-0.1.tar.gz/beerme-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='beerme',
+      version=version,
+      description="Untapped beer list scraper",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Jared Rodriguez',
+      author_email='jared@blacknode.net',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+        'console_scripts': [
+            'beerme=beerme.beerme:main'
+        ]
+      }
+      )
