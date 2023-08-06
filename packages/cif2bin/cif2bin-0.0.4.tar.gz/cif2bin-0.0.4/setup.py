2474fb595c7867863d8554e3995feb1508a76ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:23 +0000

    Add package cif2bin/0.0.4/cif2bin-0.0.4.tar.gz

---
+++ packages/cif2bin/cif2bin-0.0.4.tar.gz/cif2bin-0.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cif2bin",
+    version="0.0.4",
+    author="Maxim Lippeveld",
+    author_email="lippeveld.maxim@gmail.com",
+    description="Convert Amnis ImageStream cif-files to a binary format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.ugent.be/mlippeve/cif2bin",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "javabridge==1.0.18",
+        "python-bioformats==1.5.2",
+        "numpy==1.15.4",
+        "joblib==0.13.0",
+        "tqdm==4.28.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cif2bin=cif2bin.main:main"
+        ]
+    }
+)
