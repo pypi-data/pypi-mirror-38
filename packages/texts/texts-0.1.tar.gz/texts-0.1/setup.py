78e852e41aa9c415dca9b7b62a5872f41f6c07ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:20 +0000

    Add package texts/0.1/texts-0.1.tar.gz

---
+++ packages/texts/texts-0.1.tar.gz/texts-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='texts',
+      version='0.1',
+      description='Lesson on OOP through linguistics.',
+      url='http://github.com/infosmith/texts',
+      author='infosmith',
+      author_email='davidsmith@kenzie.academy',
+      license='proprietary',
+      packages=['texts'],
+      zip_safe=False)
