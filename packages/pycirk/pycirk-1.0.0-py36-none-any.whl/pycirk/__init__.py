a814dcfa59c0de205770361b64580c2f6d010a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:45 +0000

    Add package pycirk/1.0.0/pycirk-1.0.0-py36-none-any.whl

---
+++ packages/pycirk/pycirk-1.0.0-py36-none-any.whl/pycirk/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pycirk."""
+
+__author__ = """Franco Donati"""
+__email__ = 'f.donati@cml.leidenuniv.nl'
+__version__ = '1.0.0'
+
+from pycirk.pycirk import Start
