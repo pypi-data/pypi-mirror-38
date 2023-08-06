0953c223ae4c9081493e9a3591d7907fc5d6ecfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:44 +0000

    Add package fifolock/0.0.13/fifolock-0.0.13.tar.gz

---
+++ packages/fifolock/fifolock-0.0.13.tar.gz/fifolock-0.0.13/fifolock.py
@@ -0,0 +1,49 @@
+import asyncio
+import collections
+
+
+class FifoLock():
+
+    def __init__(self):
+        self._waiters = collections.deque()
+        self._holds = collections.defaultdict(int)
+
+    def __call__(self, lock_mode_type):
+        return _FifoLockContextManager(self._waiters, self._holds, lock_mode_type)
+
+
+class _FifoLockContextManager():
+
+    def __init__(self, waiters, holds, lock_mode_type):
+        self._waiters = waiters
+        self._holds = holds
+        self._lock_mode_type = lock_mode_type
+
+    def _maybe_acquire(self):
+        while self._waiters:
+
+            if self._waiters[0].cancelled():
+                self._waiters.popleft()
+
+            elif self._waiters[0].is_compatible(self._holds):
+                waiter = self._waiters.popleft()
+                self._holds[type(waiter)] += 1
+                waiter.set_result(None)
+
+            else:
+                break
+
+    async def __aenter__(self):
+        lock_mode = self._lock_mode_type()
+        self._waiters.append(lock_mode)
+        self._maybe_acquire()
+
+        try:
+            await lock_mode
+        except asyncio.CancelledError:
+            self._maybe_acquire()
+            raise
+
+    async def __aexit__(self, _, __, ___):
+        self._holds[self._lock_mode_type] -= 1
+        self._maybe_acquire()
