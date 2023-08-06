d9d83a65e1a2f937428460ab14c913edbc793e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:43 +0000

    Add package fair/1.3.6/fair-1.3.6.tar.gz

---
+++ packages/fair/fair-1.3.6.tar.gz/fair-1.3.6/fair/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: zebedeen
+# @Date:   2017-04-10 10:41:20
+# @Last Modified by:   Chris Smith
+# @Last Modified time: 2018-01-11 19:23:00
+
+
+# # # ------------ IMPORT REQUIRED MODULES ------------ # # #
+from . import forward
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
