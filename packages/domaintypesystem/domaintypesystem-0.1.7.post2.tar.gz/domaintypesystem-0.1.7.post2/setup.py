d409b96b253072de80a349aa4dbe7675ec6c3dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:40 +0000

    Add package domaintypesystem/0.1.7.post2/domaintypesystem-0.1.7.post2.tar.gz

---
+++ packages/domaintypesystem/domaintypesystem-0.1.7.post2.tar.gz/domaintypesystem-0.1.7.post2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='domaintypesystem',
+    version='0.1.7r2',
+    description='Decentralized type system',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Alecks Gates',
+    author_email='agates@mail.agates.io',
+    keywords=[],
+    url='https://gitlab.com/agates/domain-type-system',
+    python_requires='>=3.5',
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Adaptive Technologies',
+        'Topic :: Home Automation',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+    install_requires=[
+        'blosc~=1.5',
+        'capnpy~=0.5'
+    ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={'domaintypesystem': ['schema/*']},
+    entry_points={
+        'console_scripts': [
+        ],
+    }
+)
