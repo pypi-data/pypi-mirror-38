6d0225942f44df8cd0df691d9438bcf639af3a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:26 +0000

    Add package pydotfiles/0.0.5/pydotfiles-0.0.5.tar.gz

---
+++ packages/pydotfiles/pydotfiles-0.0.5.tar.gz/pydotfiles-0.0.5/pydotfiles/__init__.py
@@ -0,0 +1,6 @@
+name = "pydotfiles"
+
+__all__ = ['api', 'models', 'utils']
+
+# Enables single-sourcing of the version number: https://packaging.python.org/guides/single-sourcing-package-version/
+__version__ = "0.0.5"
