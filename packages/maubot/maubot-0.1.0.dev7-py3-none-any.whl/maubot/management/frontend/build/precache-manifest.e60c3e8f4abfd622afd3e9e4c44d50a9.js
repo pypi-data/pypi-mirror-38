f8392ed48945e4f403d36bec0813f056b5767961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package maubot/0.1.0.dev7/maubot-0.1.0.dev7-py3-none-any.whl

---
+++ packages/maubot/maubot-0.1.0.dev7-py3-none-any.whl/maubot/management/frontend/build/precache-manifest.e60c3e8f4abfd622afd3e9e4c44d50a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a57b766207fe6c113be29df03f171a6d",
+    "url": "./static/media/upload.a57b7662.svg"
+  },
+  {
+    "revision": "1851431378ea0e1c6a63c7261ee65d9e",
+    "url": "./static/media/plus.18514313.svg"
+  },
+  {
+    "revision": "b6b6c4db699010301b02593710377f70",
+    "url": "./static/media/chevron-right.b6b6c4db.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c6ac9c529399a022b320",
+    "url": "./static/js/main.c6ac9c52.chunk.js"
+  },
+  {
+    "revision": "63a001b12a42e9e73f51",
+    "url": "./static/js/1.63a001b1.chunk.js"
+  },
+  {
+    "revision": "c6ac9c529399a022b320",
+    "url": "./static/css/main.cc7ddce7.chunk.css"
+  },
+  {
+    "revision": "83bc1991144576f16a8881c41d0a4391",
+    "url": "./index.html"
+  }
+];
