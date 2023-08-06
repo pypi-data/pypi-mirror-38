914b71cbbccbb8cad70bedfe8850a8466e1fdc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:40 +0000

    Add package locushandler/0.1.1.dev0/locushandler-0.1.1.dev0.tar.gz

---
+++ packages/locushandler/locushandler-0.1.1.dev0.tar.gz/locushandler-0.1.1.dev0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='locushandler',
+    version='0.1.1dev',
+    url='https://github.com/LocusAnalytics/LocusHandler',
+    long_description=open('README.md').read(),
+    install_requires=[
+          'pandas',],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
