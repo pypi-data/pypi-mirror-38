0e8e1efdb3d5a376ee887a6fe81722cec26fa015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:43 +0000

    Add package dragon-rest/0.2.2/dragon-rest-0.2.2.tar.gz

---
+++ packages/dragon-rest/dragon-rest-0.2.2.tar.gz/dragon-rest-0.2.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dragon-rest",
+    version="0.2.2",
+    author="Brenden Matthews",
+    author_email="brenden@diddyinc.com",
+    description="Python wrapper for DragonMint/Innosilicon REST API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brndnmtthws/dragon-rest",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
