a8f3f595899ff150a5ae63ed421a92585eac8cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:53 +0000

    Add package NotebookScripter/1.0.2/NotebookScripter-1.0.2.tar.gz

---
+++ packages/NotebookScripter/NotebookScripter-1.0.2.tar.gz/NotebookScripter-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='NotebookScripter',
+    version='1.0.2',
+    packages=('NotebookScripter',),
+    url='https://github.com/breathe/NotebookScripter',
+    license='MIT',
+    author='N. Ben Cohen',
+    author_email='breathevalue@icloud.com',
+    install_requires=(),
+    tests_require=(),
+    description='Expose ipython jupyter notebooks as callable functions.  More info here https://github.com/breathe/NotebookScripter',
+    long_description='Expose ipython jupyter notebooks as callable functions.  More info here https://github.com/breathe/NotebookScripter',
+    classifiers=(
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython')
+)
