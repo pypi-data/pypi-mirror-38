710a8dc58c400ab9b314efce4581727692d5532b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:17 +0000

    Add package firstLoop/1.2.0/firstLoop-1.2.0.tar.gz

---
+++ packages/firstLoop/firstLoop-1.2.0.tar.gz/firstLoop-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='firstLoop',
+    version='1.2.0',
+    py_modules=['firstLoop'],
+    author='hfPython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)
