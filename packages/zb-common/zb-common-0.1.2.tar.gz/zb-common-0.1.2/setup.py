420ebe6b994c3dfd23e551114ccc424860f833ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:33 +0000

    Add package zb-common/0.1.2/zb-common-0.1.2.tar.gz

---
+++ packages/zb-common/zb-common-0.1.2.tar.gz/zb-common-0.1.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+setuptools.setup(
+    name = "zb-common",
+    version = "0.1.2",
+    author = "Manjunath Somashekar",
+    author_email = "ujnamss@gmail.com",
+    packages = ["zb_common"],
+    description = "A package containing all the common utilities for the Zerobugz project",
+    url = "http://pypi.python.org/pypi/zb-common/",
+    keywords = ["synthetic", "data", "generation"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+A package containing all the common utilities for the Zerobugz project
+-----------------------------------------------------
+
+This version requires Python 2.7 or later
+""",
+    install_requires=[
+        "requests",
+        "slackclient",
+        "future>=0.16.0",
+        "configparser"
+    ],
+)
