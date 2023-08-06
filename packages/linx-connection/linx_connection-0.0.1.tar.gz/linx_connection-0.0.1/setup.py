5a897433f61e3d1cc076bdce490a017bb3f45649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package linx-connection/0.0.1/linx_connection-0.0.1.tar.gz

---
+++ packages/linx-connection/linx_connection-0.0.1.tar.gz/linx_connection-0.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='linx_connection',
+    version='0.0.1',
+    author='Andrew Anderson',
+    author_email='andrew-anderson.neo@yandex.ru',
+    description='Module for talking to Arduino by serial port',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Mr-Andersen/LabWink',
+    packages=setuptools.find_packages(),
+    license='GNU GPL',
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: '
+            'GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+    ),
+    install_requires=['serial', 'ruamel.yaml']
+)
