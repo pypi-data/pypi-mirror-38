87b19017324173cca01bafc9c95fca17a953fe08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:55 +0000

    Add package qComputing/1.8.2/qComputing-1.8.2.tar.gz

---
+++ packages/qComputing/qComputing-1.8.2.tar.gz/qComputing-1.8.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qComputing',
+    version='1.8.2',
+    py_modules=['Data', 'Gates', 'NoisyEvolution', 'Operations', 'QCircuit', 'qubit_class', 'SpecialStates', 'Graph'],
+    description='A package which deals with light weight routine quantum computing stuff',
+    author='Amara Katabarwa',
+    author_email="Henriamaa@gmail.com",
+    url='https://github.com/akataba/qComputing',
+    packages=find_packages(),
+    classifiers=[
+                  "Programming Language :: Python :: 3",
+                  "License :: OSI Approved :: MIT License",
+                  "Operating System :: OS Independent",
+              ],
+    )
