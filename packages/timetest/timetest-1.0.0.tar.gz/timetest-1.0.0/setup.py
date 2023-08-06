f71270c26f805d2a088d37e3c12a8f5e6a070714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:38 +0000

    Add package timetest/1.0.0/timetest-1.0.0.tar.gz

---
+++ packages/timetest/timetest-1.0.0.tar.gz/timetest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='timetest',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Zhiltsov Arseney',
+    author_email='nimondars@gmail.com',
+    url='https://github.com/Nimond/TimeTest',
+)
