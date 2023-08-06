9d861c341374bb83e0a0159b9e8372ac1bea3466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:00 +0000

    Add package formation/0.1.10/formation-0.1.10-py2.py3-none-any.whl

---
+++ packages/formation/formation-0.1.10-py2.py3-none-any.whl/formation/middleware.py
@@ -0,0 +1,18 @@
+def correlation_id():
+    # TODO
+
+def retry(max_retries=3):
+    def retry_middleware(ctx, call):
+        try:
+            res = call(ctx)
+            return res
+        except Exception as ex:
+            retries = ctx.get("formation.retry", 0)
+            if retries > max_retries:
+                raise ex
+            ctx["formation.retry"] = 1 + retries
+            # TODO exponential backoff
+            res = retry_middleware(ctx, call)
+            return res
+
+    return retry_middleware
