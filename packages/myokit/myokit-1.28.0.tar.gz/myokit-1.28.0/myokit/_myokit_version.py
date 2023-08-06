49ca5a316ec4a2b0df1e40e6022c2b8996c1c23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:37 +0000

    Add package myokit/1.28.0/myokit-1.28.0.tar.gz

---
+++ packages/myokit/myokit-1.28.0.tar.gz/myokit-1.28.0/myokit/_myokit_version.py
@@ -0,0 +1,29 @@
+#
+# Myokit's version info
+#
+# This file is part of Myokit
+#  Copyright 2011-2018 Maastricht University, University of Oxford
+#  Licensed under the GNU General Public License v3.0
+#  See: http://myokit.org
+#
+import sys
+
+# True if this is a release, False for a development version
+RELEASE = True
+
+# Version as a tuple (major, minor, revision)
+#  - Changes to major are rare
+#  - Changes to minor indicate new features, possible slight backwards
+#    incompatibility
+#  - Changes to revision indicate bugfixes, tiny new features
+VERSION_INT = 1, 28, 0
+
+# String version of the version number
+VERSION = '.'.join([str(x) for x in VERSION_INT])
+if not RELEASE:  # pragma: no cover
+    VERSION_INT += ('dev', )
+    VERSION += '.dev'
+
+# Don't expose x on Python2
+if sys.hexversion < 0x03000000:  # pragma: no python 3 cover
+    del(x)
