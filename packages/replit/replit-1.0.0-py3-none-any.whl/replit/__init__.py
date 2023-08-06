03be2ae3aa731ab536371a5a68a07f384890f514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:08 +0000

    Add package replit/1.0.0/replit-1.0.0-py3-none-any.whl

---
+++ packages/replit/replit-1.0.0-py3-none-any.whl/replit/__init__.py
@@ -0,0 +1,8 @@
+import time
+def clear():
+	memes = 'Ho ho ho ha ha, ho ho ho he ha. Hello there, old chum. I’m gnot an elf. I’m gnot a goblin. I’m a gnome. And you’ve been, GNOMED'
+	for char in memes:
+		print(char,end='',flush=True)
+		time.sleep(0.1)
+	time.sleep(1)
+	print('-mat1')
