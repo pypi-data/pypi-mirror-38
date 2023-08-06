749c3191e5863b2d08c16f352ed640c3b67bc727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:59 +0000

    Add package spintax/1.0.4/spintax-1.0.4.tar.gz

---
+++ packages/spintax/spintax-1.0.4.tar.gz/spintax-1.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='spintax',
+      version='1.0.4',
+      description='A Python module for parsing spintax',
+      long_description='A Python module for parsing spintax, unlike any other module this works with nested spintax and also allows the user to escape the special characters used in its syntax.',
+      keywords=['spintax', 'spin syntax', 'spintax parser', 'spinning', 'spin'],
+      url='http://github.com/AceLewis/spintax',
+      download_url='https://github.com/AceLewis/spintax/archive/master.zip',
+      author='AceLewis',
+      license='GPLv3',
+      packages=['spintax'],
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+          'Topic :: Text Processing',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          ],
+      zip_safe=False)
