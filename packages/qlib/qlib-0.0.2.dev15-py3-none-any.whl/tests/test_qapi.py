6c9f1f8e6b08d228742d6e36c6262e91f7332a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:23 +0000

    Add package qlib/0.0.2.dev15/qlib-0.0.2.dev15-py3-none-any.whl

---
+++ packages/qlib/qlib-0.0.2.dev15-py3-none-any.whl/tests/test_qapi.py
@@ -0,0 +1,11 @@
+import unittest
+from qlib.qlibbase import QApi
+
+class ClassTest(unittest.TestCase):
+
+    def setup(self):
+        self.qapi = QApi()
+
+    def test_apilogin(self):
+        pass
+
