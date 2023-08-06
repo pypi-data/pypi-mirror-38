55c4280236637db111aa478846314e8786fca63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:34 +0000

    Add package PyStarter/0.1.2/PyStarter-0.1.2-py2.py3-none-any.whl

---
+++ packages/PyStarter/PyStarter-0.1.2-py2.py3-none-any.whl/pystarter/pystarter.py
@@ -0,0 +1,10 @@
+# Imports
+import sys
+
+def versionFind(type='int'):
+    version_tuple = sys.version_info
+    print(version_tuple)
+    print(type)
+
+def pystarterVersion():
+    return '0.1.2'
