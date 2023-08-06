1bff8df82c17cffa4da84274cee840b3619167f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:49 +0000

    Add package nameko-mongodb/0.0.3.dev0/nameko-mongodb-0.0.3.dev0.tar.gz

---
+++ packages/nameko-mongodb/nameko-mongodb-0.0.3.dev0.tar.gz/nameko-mongodb-0.0.3.dev0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='nameko-mongodb',
+    version='0.0.3-dev',
+    description='Simple MongoDb dependency for nameko (microservices framework). Based on https://github.com/saiqi/nameko-mongodb',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Alex Shinkevich',
+    author_email='alex.shinkevich@gmail.com',
+    url='https://github.com/alexshin/nameko-mongodb',
+    packages=['nameko_mongodb'],
+    install_requires=[
+        'nameko>=2.11.0',
+        'pymongo'
+    ],
+    extra_requires=[
+        'pytest==3.0.6'
+    ],
+    zip_safe=True,
+    license='Apache License, Version 2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ]
+)
