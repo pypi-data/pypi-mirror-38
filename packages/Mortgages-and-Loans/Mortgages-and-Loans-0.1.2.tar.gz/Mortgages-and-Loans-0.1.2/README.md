94b6df6188035d89067efa203e52ea777e5e54d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package Mortgages-and-Loans/0.1.2/Mortgages-and-Loans-0.1.2.tar.gz

---
+++ packages/Mortgages-and-Loans/Mortgages-and-Loans-0.1.2.tar.gz/Mortgages-and-Loans-0.1.2/README.md
@@ -0,0 +1,16 @@
+# Mortgages-and-Loans
+[![PyPI version](https://badge.fury.io/py/Mortgages-and-Loans.svg)](https://badge.fury.io/py/Mortgages-and-Loans) [![CodeFactor](https://www.codefactor.io/repository/github/harens/mortgages-and-loans/badge)](https://www.codefactor.io/repository/github/harens/mortgages-and-loans) [![Requirements Status](https://requires.io/github/harens/Mortgages-and-Loans/requirements.svg?branch=master)](https://requires.io/github/harens/Mortgages-and-Loans/requirements/?branch=master) ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/Mortgages-and-Loans.svg) [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+
+__Simple Python Scripts for Complex Mortgages__
+
+## Installation
+
+Run the command:
+```
+pip install Mortgages-and-Loans
+```
+Or download the latest version [here](https://github.com/harens/Mortgages-and-Loans/archive/master.zip)
+
+## License
+
+This project is licensed under the [GNU General Public License v3.0](https://github.com/harens/Mortgages-and-Loans/blob/master/LICENSE)
---
+++ packages/Mortgages-and-Loans/Mortgages-and-Loans-0.1.2.tar.gz/Mortgages-and-Loans-0.1.2/setup.py
@@ -0,0 +1,48 @@
+# /usr/bin/env python
+
+# This file is part of Mortgages-and-Loans.
+
+# Types-and-Variables is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Mortgages-and-Loans is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Mortgages-and-Loans.  If not, see <http://www.gnu.org/licenses/>.
+
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="Mortgages-and-Loans",
+    version="0.1.2",
+    install_requires=["re", "string"],
+    description="Simple Python Scripts for Complex Mortgages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="harens",
+    author_email="harensdeveloper@gmail.com",
+    url="https://harens.github.io",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords="homework, mortgages, loans",
+)
