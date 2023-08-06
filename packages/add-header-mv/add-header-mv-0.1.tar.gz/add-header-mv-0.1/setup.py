cbeb3c012cd203c669d2ba76d20f9801a213f219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:15 +0000

    Add package add-header-mv/0.1/add-header-mv-0.1.tar.gz

---
+++ packages/add-header-mv/add-header-mv-0.1.tar.gz/add-header-mv-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='add-header-mv',
+    version='0.1',
+    author='tdkihrr',
+    author_email='huanghezhao@outlook.com',
+    url='https://github.com/tdkihrr/add-header-mv',
+    keywords='tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    python_requires='>=3.4.0',
+    description='Add header to files and move them',
+    platforms='any',
+    packages=[
+        'hammal', 'hammal'
+    ],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'add-header-mv=hammal:main'
+        ]
+    }
+)
