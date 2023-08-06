32fd070d0ed8712eed85e961ce79f33e5cf1de9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:11 +0000

    Add package VKPy/0.1.1/VKPy-0.1.1.tar.gz

---
+++ packages/VKPy/VKPy-0.1.1.tar.gz/VKPy-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="VKPy",
+    version="0.1.1",
+    author="irdkwmnsb",
+    author_email="me@alzhanov.ru",
+    description="VK bot development micro framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/irdkwmnsb/VKPy",
+    packages=setuptools.find_packages(),
+    install_requires=['requests', 'vk'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
