9497b81edadf9b672f8e3501ad3cdfea6fa90c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:55 +0000

    Add package dgenome/1.1.5.4a3/dgenome-1.1.5.4a3.tar.gz

---
+++ packages/dgenome/dgenome-1.1.5.4a3.tar.gz/dgenome-1.1.5.4a3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(version=version,
+      long_description=long_description,
+      long_description_content_type='text/x-rst')
