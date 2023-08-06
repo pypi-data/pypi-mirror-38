93c6075dfe432b2c6423dcbef6cfd15b27151e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:24 +0000

    Add package dev-env/0.5.8/dev_env-0.5.8.tar.gz

---
+++ packages/dev-env/dev_env-0.5.8.tar.gz/dev_env-0.5.8/bin/run_unit_tests.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+while true
+do
+    case "${1:-}" in
+        --help)
+            shift
+            echo "usage: $(basename "$0") [--help] [<dir1> <dir2> ... <dir N>]" >&2
+            exit 1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+USER=$(stat -c "%u" "$0")
+GROUP=$(stat -c "%g" "$0")
+
+docker run \
+    --rm \
+    --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
+    --volume "$DEV_ENV_SOURCE_CODE:/app" \
+    "$DEV_ENV_DOCKER_IMAGE" \
+    run_unit_tests.sh "$DEV_ENV_PACKAGE" "$USER" "$GROUP" "$@"
+
+sed -i -e "s|/app/|$DEV_ENV_SOURCE_CODE/|g" "$DEV_ENV_SOURCE_CODE/.coverage"
+
+exit 0
