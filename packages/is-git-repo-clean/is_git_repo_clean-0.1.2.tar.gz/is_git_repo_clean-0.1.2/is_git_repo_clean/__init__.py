c5195e451d686ba73ed555a4190d139eb268d26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package is-git-repo-clean/0.1.2/is_git_repo_clean-0.1.2.tar.gz

---
+++ packages/is-git-repo-clean/is_git_repo_clean-0.1.2.tar.gz/is_git_repo_clean-0.1.2/is_git_repo_clean/__init__.py
@@ -0,0 +1,3 @@
+from .src.check import check, checkSync, NotAGitRepoException
+
+__all__ = ["check", "checkSync", "NotAGitRepoException"]
---
+++ packages/is-git-repo-clean/is_git_repo_clean-0.1.2.tar.gz/is_git_repo_clean-0.1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="is_git_repo_clean",
+    version="0.1.2",
+    author="phil",
+    author_email="philip.olson@pm.me",
+    description="A simple function to test whether your git repo is clean",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olsonpm/py_is-git-repo-clean",
+    packages=setuptools.find_packages(),
+    license="WTFNMFPL-1.0",
+)
