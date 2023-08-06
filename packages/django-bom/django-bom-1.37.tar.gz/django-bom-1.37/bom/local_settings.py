fbf8676cd74c090c4472d235f724169da5c716dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:19 +0000

    Add package django-bom/1.37/django-bom-1.37.tar.gz

---
+++ packages/django-bom/django-bom-1.37.tar.gz/django-bom-1.37/bom/local_settings.py
@@ -0,0 +1,11 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'supersecretkey'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+BOM_CONFIG = {
+    'octopart_api_key': '46b1a32d',
+}
