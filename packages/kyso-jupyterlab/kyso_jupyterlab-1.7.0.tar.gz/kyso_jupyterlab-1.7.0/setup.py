45140689d4294f80d02654a069e55f3663776271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:44 +0000

    Add package kyso-jupyterlab/1.7.0/kyso_jupyterlab-1.7.0.tar.gz

---
+++ packages/kyso-jupyterlab/kyso_jupyterlab-1.7.0.tar.gz/kyso_jupyterlab-1.7.0/setup.py
@@ -0,0 +1,21 @@
+"""
+Setup Module to setup Python Handlers (Git Handlers) for the Git Plugin.
+"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='kyso_jupyterlab',
+    version='1.7.0',
+    author='Git Intern Team, Noah Stapp, Jenna Landy, Alena Mueller',
+    description="A server extension for JupyterLab's git extension",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'notebook',
+        'psutil'
+    ],
+    package_data={'kyso_jupyterlab': ['*']},
+)
