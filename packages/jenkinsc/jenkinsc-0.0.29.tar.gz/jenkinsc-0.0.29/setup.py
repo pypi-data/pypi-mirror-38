e19b7630d5146ae9fc1d70ba801d20f244a14d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package jenkinsc/0.0.29/jenkinsc-0.0.29.tar.gz

---
+++ packages/jenkinsc/jenkinsc-0.0.29.tar.gz/jenkinsc-0.0.29/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+
+setup(
+    name='jenkinsc',
+    packages=find_packages(include=('jenkinsc', )),
+    version='0.0.29',
+    description='bulletproof jenkins client',
+    author='Alex Buchkovsky',
+    long_description=long_description,
+    author_email='olex.buchkovsky@gmail.com',
+    url='https://github.com/ahcub/jenkinsc',
+    keywords=['jenkins', 'automation', 'ci', 'client', 'python'],
+)
