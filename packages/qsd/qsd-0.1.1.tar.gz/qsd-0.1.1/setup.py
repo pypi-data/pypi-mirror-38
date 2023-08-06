a7bfa4f4fe66300ed65e128e99e2f99583273288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:35 +0000

    Add package qsd/0.1.1/qsd-0.1.1.tar.gz

---
+++ packages/qsd/qsd-0.1.1.tar.gz/qsd-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+#from setuptools.command.develop import develop
+#from setuptools.command.install import install
+
+setup(
+        name="qsd",
+        version="0.1.1",
+        packages=find_packages(exclude=['*test']),
+        author="Gareth Sion Jones",
+        author_email="garethsion@googlemail.com",
+        project_urls={
+            'Documentation': 'https://qsd.readthedocs.io/',
+            'Source': 'https://github.com/garethsion/qsd.git',
+            },
+        python_requires='>3.4.0',
+        install_requires=['numpy','scipy']
+    )
