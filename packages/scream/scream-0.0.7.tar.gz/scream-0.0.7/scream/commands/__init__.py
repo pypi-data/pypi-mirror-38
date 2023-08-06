3e4a1a30d3d1ecb0d372113535d07f51d258d0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:53 +0000

    Add package scream/0.0.7/scream-0.0.7.tar.gz

---
+++ packages/scream/scream-0.0.7.tar.gz/scream-0.0.7/scream/commands/__init__.py
@@ -0,0 +1,4 @@
+from .new_package import new_package
+from .install import install
+from .init import init_monorepo
+from .test import test
---
+++ packages/scream/scream-0.0.7.tar.gz/scream-0.0.7/scream/files/__init__.py
@@ -0,0 +1,7 @@
+from .git import GitIgnore
+from .readme import PackageReadme, MonorepoReadme
+from .module import ModuleExample, TestExample
+from .scream import Scream
+from .setup import SetupCfg, SetupPy
+from .tox import Tox
+from .util import File
