aeb982026f547b741b8ea5c7d9fc23a0861b171e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:00 +0000

    Add package mashumaro/0.9/mashumaro-0.9.tar.gz

---
+++ packages/mashumaro/mashumaro-0.9.tar.gz/mashumaro-0.9/mashumaro/exceptions.py
@@ -0,0 +1,58 @@
+class MissingField(Exception):
+    def __init__(self, field_name, field_type, holder_class):
+        self.field_name = field_name
+        self.field_type = field_type
+        self.holder_class = holder_class
+
+    @property
+    def field_type_name(self):
+        try:
+            return f"{self.field_type.__module__}.{self.field_type.__name__}"
+        except AttributeError:
+            return str(self.field_type)
+
+    @property
+    def holder_class_name(self):
+        try:
+            return f"{self.holder_class.__module__}." \
+                   f"{self.holder_class.__name__}"
+        except AttributeError:
+            return str(self.holder_class)
+
+    def __str__(self):
+        return f'Field "{self.field_name}" of type {self.field_type_name}' \
+               f' is missing in {self.holder_class_name} instance'
+
+
+class UnserializableDataError(Exception):
+    pass
+
+
+class UnserializableField(UnserializableDataError):
+    def __init__(self, field_name, field_type, holder_class, msg=None):
+        self.field_name = field_name
+        self.field_type = field_type
+        self.holder_class = holder_class
+        self.msg = msg
+
+    @property
+    def field_type_name(self):
+        try:
+            return f"{self.field_type.__module__}.{self.field_type.__name__}"
+        except AttributeError:
+            return str(self.field_type)
+
+    @property
+    def holder_class_name(self):
+        try:
+            return f"{self.holder_class.__module__}." \
+                   f"{self.holder_class.__name__}"
+        except AttributeError:
+            return str(self.holder_class)
+
+    def __str__(self):
+        s = f'Field "{self.field_name}" of type {self.field_type_name} ' \
+               f'in {self.holder_class_name} is not serializable'
+        if self.msg:
+            s += f': {self.msg}'
+        return s
