42a3dd829dddda1b8383411c9628d280e8725fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:17 +0000

    Add package srapi/0.0.1.4.dev0/srapi-0.0.1.4.dev0.tar.gz

---
+++ packages/srapi/srapi-0.0.1.4.dev0.tar.gz/srapi-0.0.1.4.dev0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="srapi",
+    version="0.0.1.4dev",
+    author="iWeeti",
+    description="Api wrapper for the some random api.",
+    packages=['srapi']
+)
