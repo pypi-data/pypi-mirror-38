f3944768d9c303c8fbb1b73f7dc70bfb44c16d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package indexor/0.1.0/indexor-0.1.0.tar.gz

---
+++ packages/indexor/indexor-0.1.0.tar.gz/indexor-0.1.0/examples/first_example.py
@@ -0,0 +1,5 @@
+from indexor.indexor import Indexor
+
+ml = Indexor()
+
+ml.hello()
---
+++ packages/indexor/indexor-0.1.0.tar.gz/indexor-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='indexor',
+    version='0.1.0',
+    description='The best indexor',
+    url='https://github.com/kpedrozag/myfirstpkg',
+    author='Kevin Pedroza Goenaga',
+    author_email='kevinpedroza26@gmail.com',
+    license='GNU GPLv3',
+    keywords='print indexor message',
+    packages=['indexor'],
+    package_dir={'indexor': 'indexor'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
