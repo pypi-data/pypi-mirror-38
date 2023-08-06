52552f82595065b986fee49c230b69390f12d870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:59 +0000

    Add package divinegift/0.8.3/divinegift-0.8.3.tar.gz

---
+++ packages/divinegift/divinegift-0.8.3.tar.gz/divinegift-0.8.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages 
+
+setup(name='divinegift',
+      version='0.8.3',
+      description='It is a Divine Gift',
+      long_description='The most useful package for you, young s7_it programer :)',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3',
+                   "Operating System :: OS Independent", ],
+      keywords='s7_it',
+      url='https://github.com/Malanris/DivineGift.git',
+      author='Malanris',
+      author_email='admin@malanris.ru',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['sqlalchemy', 'requests', 'mailer'],
+      include_package_data=True,
+      zip_safe=False)
