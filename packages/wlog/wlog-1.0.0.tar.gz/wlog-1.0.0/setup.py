3f40eb8a160fad05cb6e7dfb0340e1625fd9247f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:52 +0000

    Add package wlog/1.0.0/wlog-1.0.0.tar.gz

---
+++ packages/wlog/wlog-1.0.0.tar.gz/wlog-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 19:11:53 2018
+
+@author: 017253
+"""
+
+from setuptools import setup
+
+setup(
+    name='wlog',
+    version='1.0.0',
+    description='A log python project, logger',
+    url='https://github.com/fyingzhu/Python_logging',
+    author='fyingzhu',
+    author_email='fyingzhu@126.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='log logger logging python',
+    packages=['wlog'],
+)
