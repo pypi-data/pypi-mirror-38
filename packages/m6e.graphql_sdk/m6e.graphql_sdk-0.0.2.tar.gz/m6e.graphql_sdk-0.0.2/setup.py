da0c95eb0ba0f67b894e93e0455c43417311fe17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:21 +0000

    Add package m6e.graphql_sdk/0.0.2/m6e.graphql_sdk-0.0.2.tar.gz

---
+++ packages/m6e.graphql_sdk/m6e.graphql_sdk-0.0.2.tar.gz/m6e.graphql_sdk-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='m6e.graphql_sdk',
+    packages=['m6e.grpc_sdk'],
+    version='0.0.2',
+    description='',
+    author='',
+    author_email='',
+    include_package_data=True,
+    url='https://github.com/m6e/grpc-sdk',
+    download_url='',
+    keywords=[],
+    package_data={'m6e.grpc_sdk':['*', '*/*', '*/*/*']},
+)
