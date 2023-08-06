8b7e58aa4ce7722a2d7735409cb5d55d20b4e179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:17 +0000

    Add package peek-admin/1.2.1/peek-admin-1.2.1.tar.gz

---
+++ packages/peek-admin/peek-admin-1.2.1.tar.gz/peek-admin-1.2.1/peek_admin/build-web/src/plugin-home-links.ts
@@ -0,0 +1,81 @@
+// This file is auto generated, the git version is blank and .gitignored
+export const homeLinks = [
+    {
+        "name": "peek_plugin_pof_diagram", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_diagram", 
+        "title": "DMS Diagram"
+    }, 
+    {
+        "name": "peek_core_device", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_device", 
+        "title": "Device"
+    }, 
+    {
+        "name": "peek_plugin_diagram", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_diagram", 
+        "title": "Diagram"
+    }, 
+    {
+        "name": "peek_plugin_generic_diagram_menu", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_generic_diagram_menu", 
+        "title": "Diagram Generic Menu"
+    }, 
+    {
+        "name": "peek_plugin_docdb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_docdb", 
+        "title": "DocumentDB"
+    }, 
+    {
+        "name": "peek_core_email", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_email", 
+        "title": "Email"
+    }, 
+    {
+        "name": "peek_plugin_graphdb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_graphdb", 
+        "title": "Graph DB"
+    }, 
+    {
+        "name": "peek_plugin_livedb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_livedb", 
+        "title": "LiveDB"
+    }, 
+    {
+        "name": "peek_plugin_pof_graphdb_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_graphdb_loader", 
+        "title": "PoF Connectivity Model Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_equipment_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_equipment_loader", 
+        "title": "PoF Equipment Detail Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_diagram_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_diagram_loader", 
+        "title": "PoF HV Diagram Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_livedb_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_livedb_loader", 
+        "title": "PoF Realtime Loader"
+    }, 
+    {
+        "name": "peek_plugin_search", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_search", 
+        "title": "Search"
+    }
+];
