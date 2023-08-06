1f108663dd61be7dbbcb03c3ac494b6b75b0f1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:44 +0000

    Add package tflibs/0.5.3/tflibs-0.5.3-py3-none-any.whl

---
+++ packages/tflibs/tflibs-0.5.3-py3-none-any.whl/tflibs/model.py
@@ -0,0 +1,44 @@
+"""
+    Model
+    ~~~~~
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class Model:
+    @staticmethod
+    def model_fn(features, labels, mode, params):
+        raise NotImplementedError
+
+    @classmethod
+    def add_model_args(cls, argparser, parse_args):
+        pass
+
+    @classmethod
+    def add_train_args(cls, argparser, parse_args):
+        pass
+
+    @classmethod
+    def add_eval_args(cls, argparser, parse_args):
+        pass
+
+
+class Network:
+    def __init__(self, is_chief, features, labels=None):
+        self._is_chief = is_chief
+        self._features = features
+        self._labels = labels
+
+    @property
+    def is_chief(self):
+        return self._is_chief
+
+    @property
+    def features(self):
+        return self._features
+
+    @property
+    def labels(self):
+        return self._labels
