882a2489ec75ad6c60a96401164f8197d39b6f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:03 +0000

    Add package tronapi/3.0.1/tronapi-3.0.1.tar.gz

---
+++ packages/tronapi/tronapi-3.0.1.tar.gz/tronapi-3.0.1/tronapi/constants.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------
+# Copyright (c) iEXBase. All rights reserved.
+# Licensed under the MIT License.
+# See License.txt in the project root for license information.
+# --------------------------------------------------------------------
+
+# Tron nodes
+DEFAULT_FULL_NODE = "https://api.trongrid.io"
+DEFAULT_SOLIDITY_NODE = "https://api.trongrid.io"
+DEFAULT_EVENT_SERVER = 'https://api.trongrid.io'
