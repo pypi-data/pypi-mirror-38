29279aa6a772b63023c52c38d2a44dfb097e06cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:44 +0000

    Add package pysoa/0.55.1/pysoa-0.55.1-py27.py35.py36-none-any.whl

---
+++ packages/pysoa/pysoa-0.55.1-py27.py35.py36-none-any.whl/pysoa/test/compatibility.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+
+try:
+    import mock
+    # First we try to import the Python 2 backport library of Mock, because if the project is using it, we should use it
+except ImportError as e:
+    try:
+        from unittest import mock  # noqa
+        # Next we try to import the built-in unittest.mock, which is only available on Python 3
+    except ImportError:
+        # We can get here only on Python 2, only if Mock isn't installed, so we raise the original import error for Mock
+        raise e
+
+
+__all__ = (
+    'mock'
+)
