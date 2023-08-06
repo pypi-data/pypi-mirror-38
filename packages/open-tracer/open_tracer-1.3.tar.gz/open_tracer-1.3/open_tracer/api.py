9615dbf6cd5e0f4f396d764a875fbca8ec14ee38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:41 +0000

    Add package open-tracer/1.3/open_tracer-1.3.tar.gz

---
+++ packages/open-tracer/open_tracer-1.3.tar.gz/open_tracer-1.3/open_tracer/api.py
@@ -0,0 +1,30 @@
+"""
+author: arun.rs
+26th September 2018
+"""
+from datetime import datetime
+from requests import request
+from trace import trace_resource
+from trace_request import TraceRequest
+
+def service(type, url, **kwargs):
+    """
+    :summary: Wrapper method that traces all the HTTP requests made
+    :param type: Service type
+    :param url: URL to connect to
+    :param kwargs: Key word arguments
+    :return: Response
+    """
+    if not 'headers' in kwargs:
+        headers = {}
+    headers['trace_id'], headers['parent_id'], headers['child_id'] = TraceRequest().get_all_ids()
+    status = True
+    start_time = datetime.now()
+    try:
+        return request(type, url, **kwargs)
+    except Exception as e:
+        status = False
+        raise
+    finally:
+        end_time = datetime.now()
+        trace_resource(type, url, start_time, end_time, status)
