2c568e59608f333623a0642e93e7e07faeb09b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:48 +0000

    Add package QuerysetPaginator/0.0.2/QuerysetPaginator-0.0.2.tar.gz

---
+++ packages/QuerysetPaginator/QuerysetPaginator-0.0.2.tar.gz/QuerysetPaginator-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QuerysetPaginator",
+    version="0.0.2",
+    author="GetMeLive",
+    author_email="tech@getmelive.in",
+    description="This package helps in paginating evaluated querysets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GetMeLive/queryset-paginator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
