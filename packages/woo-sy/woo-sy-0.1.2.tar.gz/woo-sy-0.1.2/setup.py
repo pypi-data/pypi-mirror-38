dabd494f5144ec5221082206d6a2695bf6d03929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:30 +0000

    Add package woo-sy/0.1.2/woo-sy-0.1.2.tar.gz

---
+++ packages/woo-sy/woo-sy-0.1.2.tar.gz/woo-sy-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['woo_sy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['etsy_py>=1.0,<2.0',
+ 'python-wordpress-xmlrpc>=2.3,<3.0',
+ 'woocommerce>=1.2,<2.0']
+
+setup_kwargs = {
+    'name': 'woo-sy',
+    'version': '0.1.2',
+    'description': 'WooCommerce + Etsy = woo-sy | A tool for syncing listings between the two',
+    'long_description': None,
+    'author': 'Alexander VanTol',
+    'author_email': 'alexander.m.vantol@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
