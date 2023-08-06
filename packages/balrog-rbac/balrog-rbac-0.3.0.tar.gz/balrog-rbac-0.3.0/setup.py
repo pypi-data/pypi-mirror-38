459d6db2798700807a1d38d355ab183641709601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:46 +0000

    Add package balrog-rbac/0.3.0/balrog-rbac-0.3.0.tar.gz

---
+++ packages/balrog-rbac/balrog-rbac-0.3.0.tar.gz/balrog-rbac-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="balrog-rbac",
+    version="0.3.0",
+    description="A simple RBAC library for Python",
+    author="Enrique Pina Monserrat",
+    author_email='lord.feratum@gmail.com',
+    url='https://github.com/LordFeratum/balrog',
+    download_url='https://github.com/LordFeratum/balrog/archive/0.3.0.tar.gz',
+    packages=["balrog"],
+    provides=["balrog"],
+)
