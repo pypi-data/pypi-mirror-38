eedf67a37dcd004761c156571484411f513e9d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:50 +0000

    Add package tornado_opentracing/1.0.1/tornado_opentracing-1.0.1.tar.gz

---
+++ packages/tornado_opentracing/tornado_opentracing-1.0.1.tar.gz/tornado_opentracing-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+version = open('VERSION').read()
+setup(
+    name='tornado_opentracing',
+    version=version,
+    url='https://github.com/opentracing-contrib/python-tornado/',
+    download_url='https://github.com/opentracing-contrib/python-tornado/tarball/'+version,
+    license='Apache License 2.0',
+    author='Carlos Alberto Cortez',
+    author_email='calberto.cortez@gmail.com',
+    description='OpenTracing support for Tornado applications',
+    long_description=open('README.rst').read(),
+    packages=['tornado_opentracing'],
+    platforms='any',
+    install_requires=[
+        'tornado',
+        'opentracing>=2.0,<2.1',
+        'wrapt',
+    ],
+    extras_require={
+        'tests': [
+            'flake8<3',  # see https://github.com/zheller/flake8-quotes/issues/29
+            'flake8-quotes',
+            'pytest>=2.7,<3',
+            'pytest-cov',
+        ],
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
