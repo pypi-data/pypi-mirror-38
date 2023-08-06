fcb0276877f3beb25fc51534165bc9f6f2ddf20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:43 +0000

    Add package drfaddons/0.1.0/drfaddons-0.1.0.tar.gz

---
+++ packages/drfaddons/drfaddons-0.1.0.tar.gz/drfaddons-0.1.0/drfaddons/__init__.py
@@ -0,0 +1,6 @@
+name = "drfaddons"
+
+__title__ = 'Django REST Framework - Add Ons'
+__version__ = '0.1.0'
+__author__ = '101Loop'
+__license__ = 'GPLv3'
---
+++ packages/drfaddons/drfaddons-0.1.0.tar.gz/drfaddons-0.1.0/drfaddons/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+
+class CreateUpdateAdmin(admin.ModelAdmin):
+
+    readonly_fields = ()
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Makes created_by & create_date readonly when editing.
+        """
+        fields = list(self.readonly_fields) or []
+        if 'created_by' not in fields:
+            fields.append('created_by')
+        if 'create_date' not in fields:
+            fields.append('create_date')
+        if 'update_date' not in fields:
+            fields.append('update_date')
+        return fields
+
+    def save_model(self, request, obj, form, change):
+        import datetime
+
+        if change:
+            obj.update_date = datetime.datetime.now()
+        else:
+            obj.created_by = request.user
+            obj.create_date = datetime.datetime.now()
+        super(CreateUpdateAdmin, self).save_model(request, obj, form, change)
+
+
+class CreateUpdateHiddenAdmin(CreateUpdateAdmin):
+    """
+    Hide a model from Application page but allows addition of object
+    from inside of other models.
+    """
+    def get_model_perms(self, request):
+        return {}
