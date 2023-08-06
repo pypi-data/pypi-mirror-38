5965e8dc6974b45b79bd775d110c3bc3d4454c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:36 +0000

    Add package fonduer/0.3.6/fonduer-0.3.6-py3-none-any.whl

---
+++ packages/fonduer/fonduer-0.3.6-py3-none-any.whl/fonduer/candidates/__init__.py
@@ -0,0 +1,4 @@
+from fonduer.candidates.candidates import CandidateExtractor
+from fonduer.candidates.mentions import MentionExtractor, MentionFigures, MentionNgrams
+
+__all__ = ["CandidateExtractor", "MentionExtractor", "MentionFigures", "MentionNgrams"]
