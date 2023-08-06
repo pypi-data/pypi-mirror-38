f46e59fd7597b39bcf3d9589e9ad903652ed5ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:11 +0000

    Add package carehome/2.1.3/carehome-2.1.3.tar.gz

---
+++ packages/carehome/carehome-2.1.3.tar.gz/carehome-2.1.3/carehome/exc.py
@@ -0,0 +1,49 @@
+"""Provides exception classes."""
+
+
+class CarehomeError(Exception):
+    """All other errors should inherit from this class."""
+
+
+class InheritanceError(CarehomeError):
+    """An error in the inheritance hierarchy."""
+
+
+class DuplicateParentError(InheritanceError):
+    """This object already has this parent."""
+
+
+class DuplicateChildError(InheritanceError):
+    """This object already has this child."""
+
+
+class ParentIsChildError(InheritanceError):
+    """This parent is already a child of this object."""
+
+
+class DatabaseError(CarehomeError):
+    """There is a problem in the database."""
+
+
+class NoSuchEventError(CarehomeError):
+    """No such event."""
+
+
+class Flake8NotFound(CarehomeError):
+    """Flake8 not found."""
+
+
+class LoadError(CarehomeError):
+    """Error loading something."""
+
+
+class LoadPropertyError(LoadError):
+    """Error loading a property."""
+
+
+class LoadMethodError(LoadError):
+    """Error loading a method."""
+
+
+class LoadObjectError(LoadError):
+    """Error loading an object."""
