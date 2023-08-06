16c03360d8b8d46bb40fa2ac0e730c79bb53039b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:37 +0000

    Add package finesm/1.0.0/finesm-1.0.0.tar.gz

---
+++ packages/finesm/finesm-1.0.0.tar.gz/finesm-1.0.0/README.md
@@ -0,0 +1,40 @@
+# finesm [![Build Status](https://travis-ci.org/wesleyks/finesm.svg?branch=master)](https://travis-ci.org/wesleyks/finesm) [![Coverage Status](https://coveralls.io/repos/github/wesleyks/finesm/badge.svg?branch=master)](https://coveralls.io/github/wesleyks/finesm?branch=master)
+
+## Installation
+
+`pip install finesm`
+
+## Usage
+
+```python
+from finesm import State, StateMachine
+
+class MyStateMachine(StateMachine):
+    stop = State(default=True)
+    go = State()
+
+    @stop.on_message('switch')
+    def stop_switch(self):
+        self.set_state(go)
+
+    @stop.on_exit
+    def stop_exit(self):
+        print('exiting stop state')
+
+    @go.on_enter
+    def go_enter(self):
+        print('entering go state')
+
+    @go.on_update
+    def go_update(self):
+        print('tick')
+
+
+sm = MyStateMachine()
+sm.state  # stop
+sm.update()  #
+sm.send_message('switch')  # exiting stop state
+                           # entering go state
+sm.update()  # tick
+
+```
---
+++ packages/finesm/finesm-1.0.0.tar.gz/finesm-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='finesm',
+
+    version='1.0.0',
+
+    description='A fine state machine',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/wesleyks/finesm',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    keywords='finite-state machine state',
+
+    packages=['finesm'],
+)
