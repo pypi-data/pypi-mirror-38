520dfd8416cf55d5c3e8c4b39981c5f47c2821f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package PysparkProxy/0.0.14/PysparkProxy-0.0.14.tar.gz

---
+++ packages/PysparkProxy/PysparkProxy-0.0.14.tar.gz/PysparkProxy-0.0.14/pyspark_proxy/proxy.py
@@ -0,0 +1,183 @@
+import os
+import sys
+import uuid
+import json
+import pickle
+import base64
+
+import requests
+
+PROXY_URL = os.environ.get('PYSPARK_PROXY_URL', 'http://127.0.0.1:8765')
+
+class Proxy(object):
+    _PROXY = True
+    _id = None
+    _args = None
+    _kwargs = None
+    _module = None
+    _class = None
+    _func_chain = []
+
+    def __init__(self, *args, **kwargs):
+        self._id = str(uuid.uuid4())
+        self._kwargs = kwargs
+        self._class = self.__class__.__name__
+        self._args = args
+        self._module = sys.modules[self.__class__.__module__].__name__.replace('pyspark_proxy', 'pyspark')
+
+        if 'no_init' not in kwargs:
+            self._create_object()
+
+        # pickled objects returned from the server can have the pyspark path
+        # module instead of pyspark_proxy module. this creates an alias for
+        # pyspark to point to pyspark_proxy so hopefully pickleable objects will
+        # use the pyspark_proxy version of the object.
+        if 'pyspark' not in sys.modules:
+            sys.modules['pyspark'] = sys.modules['pyspark_proxy']
+
+    def _create_object(self):
+        args, kwargs = self._prepare_args(self._args, self._kwargs)
+
+        body = {
+                'module': self._module,
+                'class': self._class,
+                'kwargs': kwargs,
+                'args': args,
+                'id': self._id
+                }
+
+        r = requests.post(PROXY_URL+'/create', json=body)
+
+    # for a single function call
+    # ex: df.write.csv('foo.csv')
+    # ex: df.count()
+    def _call(self, base_obj, path, function_args):
+        args, kwargs = self._prepare_args(function_args[0], function_args[1])
+
+        body = {
+                'id': base_obj,
+                'path': path,
+                'args': args,
+                'kwargs': kwargs
+                }
+
+        r = requests.post(PROXY_URL+'/call', json=body)
+        res_json = r.json()
+
+        return self._handle_response(res_json)
+
+    # for chained function calls
+    # ex: df.format('json').save('bar.json')
+    # 
+    # currently any implemented function doesn't return an object
+    # if in a later time _call_chain needs to return an object, might
+    # merge this with the regular _call function
+    def _call_chain(self, base_obj):
+        body = {
+            'id': base_obj,
+            'stack': self._func_chain
+            }
+
+        r = requests.post(PROXY_URL+'/call_chain', json=body)
+        res_json = r.json()
+
+        self._func_chain = []
+
+        return self._handle_response(res_json)
+
+    # for class function calls
+    # ex: SQLContext.getOrCreate(spark_context)
+    #
+    # This might need to be reworked once more class methods are
+    # implemented and there is a better understanding of requirements
+    @classmethod
+    def _call_class_method(cls, function, function_args):
+        args = []
+
+        for x in function_args[0]:
+            if hasattr(x, '_PROXY'):
+                args.append({'_PROXY_ID': x._id})
+            else:
+                args.append(x)
+
+        body = {
+            'class': cls.__name__,
+            'module': cls.__module__.replace('pyspark_proxy', 'pyspark'),
+            'function': function,
+            'args': args,
+            'kwargs': function_args[1]
+        }
+
+        r = requests.post(PROXY_URL+'/call_class_method', json=body)
+        res_json = r.json()
+
+        return res_json
+
+    # __getitem__ server call 
+    # ex: df['age']
+    def _get_item(self, item):
+        body = {
+            'id': self._id,
+            'item': item
+            }
+
+        r = requests.get(PROXY_URL+'/get_item', json=body)
+        return r.json()
+
+    # parses the response json from the server and returns the proper object
+    def _handle_response(self, resp):
+        if resp['stdout'] != []:
+            print('\n'.join(resp['stdout']))
+
+        if resp['exception']:
+            raise Exception(resp['exception'])
+
+        if resp['object']:
+            if 'id' in resp:
+                if resp['class'] == 'DataFrame':
+                    from pyspark_proxy.sql.dataframe import DataFrame
+
+                    return DataFrame(resp['id'])
+                elif resp['class'] == 'Column':
+                    from pyspark_proxy.sql.column import Column
+
+                    return Column(resp['id'])
+                else:
+                    return resp
+            elif 'pickle' == resp['class']:
+                return pickle.loads(base64.b64decode(resp['value']))
+            else:
+                return resp['value']
+        else:
+            return None
+
+    # catch all function
+    def __getattr__(self, name):
+        def method(*args, **kwargs):
+            return self._call(self._id, name, (args, kwargs))
+
+        return method
+
+    # since we can't send actual objects over to the server, we 
+    # need to replace any pyspark related objects passed in functions
+    # with a placeholder so the actual pyspark object on the server
+    # gets passed in with the function call
+    def _prepare_args(self, args, kwargs):
+        prepared_args = []
+        prepared_kwargs = {}
+
+        for a in args:
+            if hasattr(a, '_PROXY'):
+                prepared_args.append({'_PROXY_ID': a._id})
+            else:
+                prepared_args.append(a)
+
+        for a in kwargs:
+            v = kwargs[a]
+
+            if hasattr(v, '_PROXY'):
+                prepared_kwargs[a] = {'_PROXY_ID': v._id}
+            else:
+                prepared_kwargs[a] = v
+
+        return prepared_args, prepared_kwargs
---
+++ packages/PysparkProxy/PysparkProxy-0.0.14.tar.gz/PysparkProxy-0.0.14/pyspark_proxy/server/server.py
@@ -0,0 +1,276 @@
+import uuid
+import os
+import cloudpickle
+import pickle
+import base64
+import logging
+import types
+
+logger = logging.getLogger()
+
+import findspark
+findspark.init()
+
+import pyspark
+import pyspark.sql.functions
+
+from flask import Flask, request, jsonify
+from flask.logging import default_handler
+
+from pyspark_proxy.server.capture import Capture
+
+app = Flask(__name__)
+
+objects = {}
+
+# looks at incomming arguments to see if there are any
+# pyspark objects that should be retrieved and passed in
+def arg_objects(request_args, request_kwargs={}):
+    global objects
+
+    args = []
+    kwargs = {}
+
+    for a in request_args:
+        if type(a) == dict:
+            if '_PROXY_ID' in a:
+                id = a['_PROXY_ID']
+                logger.info('Retrieving object id: %s' % id)
+                obj = objects[id]
+                args.append(obj)
+            elif '_CLOUDPICKLE' in a:
+                obj = cloudpickle.loads(base64.b64decode(a['_CLOUDPICKLE']))
+                args.append(obj)
+        # spark strictly typechecks some arguments expecting strings but
+        # decoding json will turn strings into unicode objects
+        elif type(a) == unicode:
+            args.append(str(a))
+        else:
+            args.append(a)
+
+    for k in request_kwargs:
+        v = request_kwargs[k]
+
+        if type(v) == dict:
+            if '_PROXY_ID' in v:
+                id = v['_PROXY_ID']
+                logger.debug('Retrieving object id: %s' % id)
+
+                kwargs[k] = objects[id]
+            else:
+                kwargs[k] = v
+        else:
+            kwargs[k] = v
+
+    return args, kwargs
+
+# checks if any objects are created via a function call
+# and returns the corresponding object
+#
+# pandas: pickles and returns a pandas dataframe
+# pyspark.*: returns class and corresponding object id
+def object_response(obj, exception, paths=[], stdout=[]):
+    global objects
+
+    result = {
+            'object': False,
+            'stdout': stdout,
+            'exception': exception
+            }
+
+    # if obj is not None and type(obj) != types.FunctionType:
+    if obj is not None:
+        result['object'] = True
+        result['class'] = obj.__class__.__name__
+
+        if 'pyspark' in str(obj.__class__) or type(obj) == types.FunctionType:
+            id = str(uuid.uuid4())
+
+            result['id'] = id
+
+            logger.info('Adding object id %s to the stack' % id)
+            objects[id] = obj
+        # the last string in paths is the function that gets called
+        elif paths[-1] == 'toPandas' or paths[-1] == 'collect':
+            result['class'] = 'pickle'
+            result['value'] = base64.b64encode(pickle.dumps(obj, 2))
+        else:
+            result['value'] = obj
+
+    return result
+
+@app.route('/create', methods=['POST'])
+def create():
+    global objects
+    
+    req = request.json
+
+    logger.info('/create')
+    logger.info(req)
+
+    module_paths = req['module'].split('.')
+    base_module = __import__(module_paths[0])
+
+    module = base_module
+
+    # __import__ only imports the top level module
+    # use loop through the rest via getattr to get the 
+    # last module with the needed object
+    if len(module_paths) > 1:
+        for m in module_paths[1:]:
+            module = getattr(module, m)
+
+    callable = getattr(module, req['class'])
+    args, kwargs = arg_objects(req['args'], req['kwargs'])
+    objects[req['id']] = callable(*args, **kwargs)
+
+    return req['id']
+
+@app.route('/call', methods=['POST'])
+def call():
+    global objects
+    
+    req = request.json
+
+    logger.info('/call')
+    logger.info(req)
+
+    result_exception = None
+    res_obj = None
+    paths = []
+
+    if req['id'] == 'pyspark':
+        base_obj = pyspark
+    else:
+        base_obj = objects[req['id']]
+
+    if req['path'] != None:
+        paths = req['path'].split('.')
+
+    func = base_obj
+    args, kwargs = arg_objects(req['args'], req['kwargs'])
+
+    try:
+        for p in paths:
+            func = getattr(func, p)
+        
+        with Capture() as stdout:
+            if callable(func):
+                res_obj = func(*args, **kwargs)
+            else:
+                res_obj = func
+    except Exception as e:
+        result_exception = str(e)
+
+    return jsonify(object_response(res_obj, result_exception, paths, stdout))
+
+
+@app.route('/call_chain', methods=['POST'])
+def call_chain():
+    global objects
+
+    req = request.json
+
+    logger.info('/call_chain')
+    logger.info(req)
+
+    base_obj = objects[req['id']]
+
+    obj = base_obj
+    res_obj = None
+    result_exception = None
+
+    try:
+        with Capture() as stdout:
+            for s in req['stack']:
+                obj = getattr(obj, s['func'])
+
+                if callable(obj):
+                    obj = obj(*s['args'], **s['kwargs'])
+
+                    # only check for returned objects for the
+                    # last call in the chain
+                    if s == req['stack'][-1]:
+                        res_obj = obj
+    except Exception as e:
+        result_exception = str(e)
+
+    return jsonify(object_response(res_obj, result_exception, req['stack'], stdout))
+
+@app.route('/call_class_method', methods=['POST'])
+def call_class_method():
+    global objects
+
+    req = request.json
+
+    logger.info('/call_call_method')
+    logger.info(req)
+
+    res_obj = None
+    result_exception = None
+
+    module_paths = req['module'].split('.')
+    base_module = __import__(module_paths[0])
+
+    module = base_module
+
+    if len(module_paths) > 1:
+        for m in module_paths[1:]:
+            module = getattr(module, m)
+
+    callable = getattr(module, req['class'])
+    args, kwargs = arg_objects(req['args'], req['kwargs'])
+
+    # should this capture stdout?
+    try:
+        res_obj = callable(*args, **kwargs)
+    except Exception as e:
+        result_exception = str(e)
+
+    return jsonify(object_response(res_obj, result_exception, module_paths))
+
+@app.route('/get_item', methods=['GET'])
+def get_item():
+    global objects
+
+    result_exception = None
+    req = request.json
+
+    logger.info('/get_item')
+    logger.info(req)
+
+    try:
+        base_obj = objects[req['id']]
+        res_obj = base_obj[req['item']]
+    except Exception as e:
+        result_exception = str(e)
+
+    return jsonify(object_response(res_obj, result_exception))
+
+@app.route('/clear', methods=['POST', 'GET'])
+def clear():
+    global objects
+
+    logger.info('/clear')
+
+    sc = pyspark.SparkContext.getOrCreate()
+    sc.stop()
+
+    objects = {}
+
+    return 'ok'
+
+def run(*args, **kwargs):
+    if 'debug' not in kwargs or ('debug' in kwargs and kwargs['debug'] == False):
+        app.logger.removeHandler(default_handler)
+        app.logger = logger
+
+        logger.info('Starting pyspark proxy web server')
+
+    if 'port' not in kwargs:
+        kwargs['port'] = 8765
+
+    app.run(*args, **kwargs)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, port=8765)
