8d105f3c61e89ab84d7c5f07093b666b68e9ff75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:39 +0000

    Add package traits-enaml/0.3.0/traits-enaml-0.3.0.tar.gz

---
+++ packages/traits-enaml/traits-enaml-0.3.0.tar.gz/traits-enaml-0.3.0/README.rst
@@ -0,0 +1,25 @@
+traits-enaml
+============
+
+Traits-Enaml is an extension library to facilitate interoperation of `Enaml
+>=0.8.9` and above with `Traits` and allow a programmer to drive enaml views
+using traits models, enable/chaco components and mayavi 3D scenes.
+
+References
+==========
+
+ * Enaml:  https://github.com/nucleic/enaml
+ * Traits: https://github.com/enthought/traits
+ * Enable: https://github.com/enthought/enable
+ * Mayavi: https://github.com/enthought/mayavi
+
+Status
+======
+
+.. image:: https://api.travis-ci.org/enthought/traits-enaml.png?branch=master
+   :target: https://travis-ci.org/enthought/traits-enaml
+   :alt: Build status
+
+.. image:: http://codecov.io/github/enthought/traits-enaml/coverage.svg?branch=master
+   :target: http://codecov.io/github/enthought/traits-enaml?branch=master
+   :alt: Coverage status
---
+++ packages/traits-enaml/traits-enaml-0.3.0.tar.gz/traits-enaml-0.3.0/setup.py
@@ -0,0 +1,33 @@
+#----------------------------------------------------------------------------
+#
+#  Copyright (c) 2013-14, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in /LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+#----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='traits-enaml',
+    version='0.3.0',
+    author='Enthought, Inc',
+    author_email='info@enthought.com',
+    url='https://github.com/enthought/traits-enaml',
+    description='Utilities for interoperation between Traits and Enaml',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    package_data={'traits_enaml.testing.tests': ['*.enaml'],
+                  'traits_enaml.widgets': ['*.enaml']},
+    install_requires=['enaml>=0.8.9', 'six', 'traits', 'traitsui'],
+    extras_require={
+        'enable': ['enable'],
+        'mayavi': ['mayavi'],
+        'opengl': ['pyopengl']})
