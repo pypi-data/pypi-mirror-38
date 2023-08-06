ab87a4d1bcd872fd86a016ef9b2027c3a5e6a1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:54 +0000

    Add package searchguard/0.4/searchguard-0.4.tar.gz

---
+++ packages/searchguard/searchguard-0.4.tar.gz/searchguard-0.4/searchguard/exceptions.py
@@ -0,0 +1,46 @@
+class SearchGuardException(Exception):
+    pass
+
+
+class CreateUserException(SearchGuardException):
+    pass
+
+
+class DeleteUserException(SearchGuardException):
+    pass
+
+
+class CreateRoleException(SearchGuardException):
+    pass
+
+
+class DeleteRoleException(SearchGuardException):
+    pass
+
+
+class ViewRoleException(SearchGuardException):
+    pass
+
+
+class ViewUserException(SearchGuardException):
+    pass
+
+
+class ModifyUserException(SearchGuardException):
+    pass
+
+
+class ModifyRoleException(SearchGuardException):
+    pass
+
+
+class ListUsersException(SearchGuardException):
+    pass
+
+
+class CheckUserExistsException(SearchGuardException):
+    pass
+
+
+class CheckRoleExistsException(SearchGuardException):
+    pass
