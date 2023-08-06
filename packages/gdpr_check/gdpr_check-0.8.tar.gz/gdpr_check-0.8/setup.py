18785339f88b71d606b611f8446152403af7b9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:46 +0000

    Add package gdpr_check/0.8/gdpr_check-0.8.tar.gz

---
+++ packages/gdpr_check/gdpr_check-0.8.tar.gz/gdpr_check-0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='gdpr_check',
+    version='0.8',
+    description='Check if a user is covered by GDPR. Compare IP address to list of EEA/EU countries.',
+    url='http://github.com/revmischa/gdpr_check',
+    author='Mischa Spiegelmock',
+    author_email='revmischa@cpan.org',
+    license='ABRMS',
+    packages=['gdpr_check'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='gdpr eea eu ip check geoip country',
+    setup_requires=['setuptools>=38.6.0'],
+    install_requires=[
+        'maxminddb>=1.4.0',
+        # 'maxminddb-geolite2>=2018.308',
+        'requests>=2.18.4',
+        'geoip2>=2.8.0',
+    ],
+)
