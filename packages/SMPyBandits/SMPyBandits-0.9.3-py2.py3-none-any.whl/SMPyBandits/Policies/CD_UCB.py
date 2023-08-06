4a4b03e4b5d1e5ee285387a1684ffeb27a5c87c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:51 +0000

    Add package SMPyBandits/0.9.3/SMPyBandits-0.9.3-py2.py3-none-any.whl

---
+++ packages/SMPyBandits/SMPyBandits-0.9.3-py2.py3-none-any.whl/SMPyBandits/Policies/CD_UCB.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+r""" The CD-UCB generic policy and CUSUM-UCB and PHT-UCB policies for non-stationary bandits.
+
+- Reference: [["A Change-Detection based Framework for Piecewise-stationary Multi-Armed Bandit Problem". F. Liu, J. Lee and N. Shroff. arXiv preprint arXiv:1711.03539, 2017]](https://arxiv.org/pdf/1711.03539)
+- It runs on top of a simple policy, e.g., :class:`Policy.UCB.UCB`, and :func:`CUSUM_IndexPolicy` is a wrapper:
+
+    >>> policy = CUSUM_IndexPolicy(nbArms, UCB)
+    >>> # use policy as usual, with policy.startGame(), r = policy.choice(), policy.getReward(arm, r)
+
+- It uses an additional :math:`\mathcal{O}(\tau_\max)` memory for a game of maximum stationary length :math:`\tau_\max`.
+
+.. warning:: This implementation is still experimental!
+.. warning:: It can only work on basic index policy based on empirical averages (and an exploration bias), like :class:`Policy.UCB.UCB`, and cannot work on any Bayesian policy (for which we would have to remember all previous observations in order to reset the history with a small history)!
+"""
+from __future__ import division, print_function  # Python 2 compatibility
+
+__author__ = "Lilian Besson"
+__version__ = "0.9"
+
+
+import numpy as np
+
+try:
+    from .with_proba import with_proba
+    from .BaseWrapperPolicy import BaseWrapperPolicy
+    from .UCB import UCB as DefaultPolicy, UCB
+except ImportError:
+    from with_proba import with_proba
+    from BaseWrapperPolicy import BaseWrapperPolicy
+    from UCB import UCB as DefaultPolicy, UCB
+
+
+#: Default probability of random exploration :math:`\alpha`.
+PROBA_RANDOM_EXPLORATION = 0.1
+
+#: Should we fully restart the algorithm or simply reset one arm empirical average ?
+FULL_RESTART_WHEN_REFRESH = False
+
+#: Precision of the test.
+EPSILON = 0.5
+
+#: Default value of :math:`\lambda`.
+LAMBDA = 1
+
+#: Hypothesis on the speed of changes: between two change points, there is at least :math:`M * K` time steps, where K is the number of arms, and M is this constant.
+MIN_NUMBER_OF_OBSERVATION_BETWEEN_CHANGE_POINT = 100
+
+
+from scipy.special import comb
+
+def compute_h_alpha_from_input_parameters(horizon, max_nb_random_events, nbArms, epsilon, lmbda, M):
+    r""" Compute the values :math:`C_1^+, C_1^-, C_1, C_2, h` from the formulas in Theorem 2 and Corollary 2 in the paper."""
+    T = int(max(1, horizon))
+    UpsilonT = int(max(1, max_nb_random_events))
+    K = int(max(1, nbArms))
+    print("compute_h_alpha_from_input_parameters() with:\nT = {}, UpsilonT = {}, K = {}, epsilon = {}, lmbda = {}, M = {}".format(T, UpsilonT, K, epsilon, lmbda, M))  # DEBUG
+    C2 = np.log(3) + 2 * np.exp(- 2 * epsilon**2 * M) / lmbda
+    C1_minus = np.log(((4 * epsilon) / (1-epsilon)**2) * comb(M, int(np.floor(2 * epsilon * M))) * (2 * epsilon)**M + 1)
+    C1_plus = np.log(((4 * epsilon) / (1+epsilon)**2) * comb(M, int(np.ceil(2 * epsilon * M))) * (2 * epsilon)**M + 1)
+    C1 = min(C1_minus, C1_plus)
+    if C1 == 0: C1 = 1  # FIXME
+    h = 1/C1 * np.log(T / UpsilonT)
+    alpha = K * np.sqrt((C2 * UpsilonT)/(C1 * T) * np.log(T / UpsilonT))
+    print("Gave C2 = {}, C1- = {} and C1+ = {} so C1 = {}, and h = {} and alpha = {}".format(C2, C1_minus, C1_plus, C1, h, alpha))  # DEBUG
+    return h, alpha
+
+
+
+# --- The very generic class
+
+class CD_IndexPolicy(BaseWrapperPolicy):
+    r""" The CD-UCB generic policy for non-stationary bandits, from [["A Change-Detection based Framework for Piecewise-stationary Multi-Armed Bandit Problem". F. Liu, J. Lee and N. Shroff. arXiv preprint arXiv:1711.03539, 2017]](https://arxiv.org/pdf/1711.03539).
+    """
+    def __init__(self, nbArms,
+            full_restart_when_refresh=FULL_RESTART_WHEN_REFRESH,
+            epsilon=EPSILON,
+            proba_random_exploration=PROBA_RANDOM_EXPLORATION,
+            policy=DefaultPolicy,
+            lower=0., amplitude=1., *args, **kwargs
+        ):
+        super(CD_IndexPolicy, self).__init__(nbArms, policy=policy, lower=lower, amplitude=amplitude, *args, **kwargs)
+        # New parameters
+        self.epsilon = epsilon  #: Parameter :math:`\varepsilon` for the test.
+        alpha = max(0, min(1, proba_random_exploration))  # crop to [0, 1]
+        self.proba_random_exploration = alpha  #: What they call :math:`\alpha` in their paper: the probability of uniform exploration at each time.
+        self._full_restart_when_refresh = full_restart_when_refresh  # Should we fully restart the algorithm or simply reset one arm empirical average ?
+        # Internal memory
+        self.all_rewards = [[] for _ in range(self.nbArms)]  #: Keep in memory all the rewards obtained since the last restart on that arm.
+        self.last_pulls = np.full(nbArms, -1)  #: Keep in memory the times where each arm was last seen. Start with -1 (never seen)
+
+    def __str__(self):
+        return r"CD-{}($\varepsilon={:.3g}$, $\alpha={:.3g}$)".format(self._policy.__name__, self.epsilon, self.proba_random_exploration)
+
+    def choice(self):
+        r""" With a probability :math:`\alpha`, play uniformly at random, otherwise, pass the call to ``choice`` of the underlying policy."""
+        if with_proba(self.proba_random_exploration):
+            return np.random.randint(0, self.nbArms - 1)
+        return self.policy.choice()
+
+    def getReward(self, arm, reward):
+        """ Give a reward: increase t, pulls, and update cumulated sum of rewards and update small history (sliding window) for that arm (normalized in [0, 1]).
+
+        - Reset the whole empirical average if the change detection algorithm says so.
+        """
+        super(CD_IndexPolicy, self).getReward(arm, reward)
+        # Get reward
+        reward = (reward - self.lower) / self.amplitude
+        # We seen it one more time
+        self.last_pulls[arm] += 1
+        # Store it in place for the empirical average of that arm
+        self.all_rewards[arm].append(reward)
+        if self.detect_change(arm):
+            print("For a player {} a change was detected at time {} for arm {} after seeing reward = {}!".format(self, self.t, arm, reward))  # DEBUG
+            # print("The current pulls vector is =", self.pulls)  # DEBUG
+            # print("The current last pulls vector is =", self.last_pulls)  # DEBUG
+            # print("The current rewards vector is =", self.rewards)  # DEBUG
+            # print("The current sum/mean of all rewards for this arm is =", np.sum(self.all_rewards[arm]), np.mean(self.all_rewards[arm]))  # DEBUG
+            # Fully restart the algorithm ?!
+            if self._full_restart_when_refresh:
+                self.startGame(createNewPolicy=False)
+            # Or simply reset one of the empirical averages?
+            else:
+                self.rewards[arm] = np.sum(self.all_rewards[arm])
+                self.pulls[arm] = len(self.all_rewards[arm])
+            # reset current memory for THIS arm
+            self.last_pulls[arm] = 1
+            self.all_rewards[arm] = [reward]
+        # we update the total number of samples available to the underlying policy
+        # self.policy.t = sum(self.last_pulls)  # FIXME try this back!
+
+    def detect_change(self, arm):
+        """ Try to detect a change in the current arm.
+
+        .. warning:: This is not implemented for the generic CD algorithm, it has to be implement by a child of the class :class:`CD_IndexPolicy`.
+        """
+        raise NotImplementedError
+
+
+# --- Different change detection algorithms
+
+class SlidingWindowRestart_IndexPolicy(CD_IndexPolicy):
+    r""" A more generic implementation is the :class:`Policies.SlidingWindowRestart` class.
+
+    .. warning:: I have no idea if what I wrote is correct or not!
+    """
+
+    def detect_change(self, arm):
+        """ Try to detect a change in the current arm.
+
+        .. warning:: This one is simply using a sliding-window of fixed size = 100. A more generic implementation is the :class:`Policies.SlidingWindowRestart` class.
+        """
+        tau = self.M * self.nbArms
+        if self.last_pulls[arm] >= tau and self.pulls[arm] >= tau:
+            # Compute the empirical average for that arm
+            empirical_average = self.rewards[arm] / self.pulls[arm]
+            # And the small empirical average for that arm
+            small_empirical_average = np.mean(self.last_rewards[arm])
+            if np.abs(empirical_average - small_empirical_average) >= self.epsilon:
+                return True
+        return False
+
+
+# --- Different change detection algorithms
+
+class CUSUM_IndexPolicy(CD_IndexPolicy):
+    r""" The CUSUM-UCB generic policy for non-stationary bandits, from [["A Change-Detection based Framework for Piecewise-stationary Multi-Armed Bandit Problem". F. Liu, J. Lee and N. Shroff. arXiv preprint arXiv:1711.03539, 2017]](https://arxiv.org/pdf/1711.03539).
+    """
+    def __init__(self, nbArms,
+            horizon=None, max_nb_random_events=None,
+            full_restart_when_refresh=FULL_RESTART_WHEN_REFRESH,
+            epsilon=EPSILON,
+            lmbda=LAMBDA,
+            min_number_of_observation_between_change_point=MIN_NUMBER_OF_OBSERVATION_BETWEEN_CHANGE_POINT,
+            policy=DefaultPolicy,
+            lower=0., amplitude=1., *args, **kwargs
+        ):
+        super(CUSUM_IndexPolicy, self).__init__(nbArms, epsilon=epsilon, full_restart_when_refresh=full_restart_when_refresh, policy=policy, lower=lower, amplitude=amplitude, *args, **kwargs)
+        # New parameters
+        self.max_nb_random_events = max_nb_random_events
+        self.M = min_number_of_observation_between_change_point  #: Parameter :math:`M` for the test.
+        h, alpha = compute_h_alpha_from_input_parameters(horizon, max_nb_random_events, nbArms, epsilon, lmbda, min_number_of_observation_between_change_point)
+        self.threshold_h = h  #: Parameter :math:`h` for the test (threshold).
+        alpha = max(0, min(1, alpha))  # crop to [0, 1]
+        self.proba_random_exploration = alpha  #: What they call :math:`\alpha` in their paper: the probability of uniform exploration at each time.
+
+    def __str__(self):
+        return r"CUSUM-{}($\varepsilon={:.3g}$, $\Upsilon_T={:.3g}$, $M={:.3g}$, $h={:.3g}$, $\alpha={:.3g}$)".format(self._policy.__name__, self.epsilon, self.max_nb_random_events, self.M, self.threshold_h, self.proba_random_exploration)
+
+    def detect_change(self, arm):
+        r""" Detect a change in the current arm, using the two-sided CUSUM algorithm [Page, 1954].
+
+        - For each *data* k, compute:
+
+        .. math::
+
+            s_k^- &= (y_k - \hat{u}_0 - \varepsilon) 1(k > M),\\
+            s_k^+ &= (\hat{u}_0 - y_k - \varepsilon) 1(k > M),\\
+            g_k^+ &= max(0, g_{k-1}^+ + s_k^+),\\
+            g_k^- &= max(0, g_{k-1}^- + s_k^-),\\
+
+        - The change is detected if :math:`\max(g_k^+, g_k^-) > h` where :attr:`threshold_h` is the threshold of the test,
+        - And :math:`\hat{u}_0 = \frac{1}{M} \sum_{k=1}^{M} y_k` is the mean of the first M samples, where M is :attr:`M` the min number of observation between change points.
+        """
+        gp, gm = 0, 0
+        data_y = self.all_rewards[arm]
+        # First we use the first M samples to calculate the average :math:`\hat{u_0}`.
+        u0hat = np.mean(data_y[:self.M])
+        for k, y_k in enumerate(data_y):
+            if k <= self.M:
+                continue
+            sp = u0hat - y_k - self.epsilon  # no need to multiply by (k > self.M)
+            sm = y_k - u0hat - self.epsilon  # no need to multiply by (k > self.M)
+            gp, gm = max(0, gp + sp), max(0, gm + sm)
+            if max(gp, gm) >= self.threshold_h:
+                return True
+        return False
+
+
+class PHT_IndexPolicy(CUSUM_IndexPolicy):
+    r""" The PHT-UCB generic policy for non-stationary bandits, from [["A Change-Detection based Framework for Piecewise-stationary Multi-Armed Bandit Problem". F. Liu, J. Lee and N. Shroff. arXiv preprint arXiv:1711.03539, 2017]](https://arxiv.org/pdf/1711.03539).
+    """
+
+    def __str__(self):
+        return r"PHT-{}($\varepsilon={:.3g}$, $\Upsilon_T={:.3g}$, $M={:.3g}$, $h={:.3g}$, $\alpha={:.3g}$)".format(self._policy.__name__, self.epsilon, self.max_nb_random_events, self.M, self.threshold_h, self.proba_random_exploration)
+
+    def detect_change(self, arm):
+        r""" Detect a change in the current arm, using the two-side PHT algorithm [Hinkley, 1971].
+
+        - For each *data* k, compute:
+
+        .. math::
+
+            s_k^- &= y_k - \hat{y}_k - \varepsilon,\\
+            s_k^+ &= \hat{y}_k - y_k - \varepsilon,\\
+            g_k^+ &= max(0, g_{k-1}^+ + s_k^+),\\
+            g_k^- &= max(0, g_{k-1}^- + s_k^-),\\
+
+        - The change is detected if :math:`\max(g_k^+, g_k^-) > h` where :attr:`threshold_h` is the threshold of the test,
+        - And :math:`\hat{y}_k = \frac{1}{k} \sum_{s=1}^{k} y_s` is the mean of the first k samples.
+        """
+        gp, gm = 0, 0
+        data_y = self.all_rewards[arm]
+        # First we use the first M samples to calculate the average :math:`\hat{u_0}`.
+        for k, y_k in enumerate(data_y):
+            y_k_hat = np.mean(data_y[:k])
+            sp = y_k_hat - y_k - self.epsilon
+            sm = y_k - y_k_hat - self.epsilon
+            gp, gm = max(0, gp + sp), max(0, gm + sm)
+            if max(gp, gm) >= self.threshold_h:
+                return True
+        return False
---
+++ packages/SMPyBandits/SMPyBandits-0.9.3-py2.py3-none-any.whl/SMPyBandits/Policies/LM_DSEE.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+r""" The LM-DSEE policy for non-stationary bandits, from [["On Abruptly-Changing and Slowly-Varying Multiarmed Bandit Problems", by Lai Wei, Vaibhav Srivastava, 2018, arXiv:1802.08380]](https://arxiv.org/pdf/1802.08380)
+
+- It uses an additional :math:`\mathcal{O}(\tau_\max)` memory for a game of maximum stationary length :math:`\tau_\max`.
+
+.. warning:: This implementation is still experimental!
+"""
+from __future__ import division, print_function  # Python 2 compatibility
+
+__author__ = "Lilian Besson"
+__version__ = "0.9"
+
+
+from enum import Enum  # For the different states
+import numpy as np
+
+try:
+    from .BasePolicy import BasePolicy
+except ImportError:
+    from BasePolicy import BasePolicy
+
+
+#: Different states during the Musical Chair algorithm
+State = Enum('State', ['Exploration', 'Exploitation'])
+
+
+# --- Class
+
+class LM_DSEE(BasePolicy):
+    r""" The LM-DSEE policy for non-stationary bandits, from [["On Abruptly-Changing and Slowly-Varying Multiarmed Bandit Problems", by Lai Wei, Vaibhav Srivastava, 2018, arXiv:1802.08380]](https://arxiv.org/pdf/1802.08380)
+    """
+
+    def __init__(self, nbArms,
+            nu=0.5, DeltaMin=0.5, a=1, b=2,
+            lower=0., amplitude=1., *args, **kwargs
+        ):
+        super(LM_DSEE, self).__init__(nbArms, lower=lower, amplitude=amplitude, *args, **kwargs)
+        # Parameters
+        self.a = a  #: Parameter :math:`a` for the LM-DSEE algorithm.
+        self.b = b  #: Parameter :math:`b` for the LM-DSEE algorithm.
+        assert 0 < DeltaMin < 1, "Error: for a LM_DSEE policy, the parameter 'DeltaMin' should be in (0,1) but was = {}".format(DeltaMin)  # DEBUG
+        gamma = 2 / DeltaMin**2
+        self.l = nbArms * np.ceil(gamma * np.log(b)) / a  #: Parameter :math:`\ell` for the LM-DSEE algorithm.
+        self.gamma = gamma  #: Parameter :math:`\gamma` for the LM-DSEE algorithm.
+        assert 0 < nu < 1, "Error: for a LM_DSEE policy, the parameter 'nu' should be in (0,1) but was = {}".format(nu)  # DEBUG
+        rho = (1 - nu) / (1 + nu)
+        self.rho = rho  #: Parameter :math:`\rho` for the LM-DSEE algorithm.
+        # Internal memory
+        self.phase = State.Exploration  #: Current phase, exploration or exploitation
+        self.current_exploration_arm = None  #: Currently explored arm
+        self.current_exploitation_arm = None  #: Currently exploited arm
+        self.batch_number = 1  #: Number of batch
+        self.length_of_current_phase = None  #: Length of the current phase, either computed from :func:`length_exploration_phase` or func:length_exploitation_phase``
+        self.step_of_current_phase = 0  #: Timer inside the current phase
+        self.all_rewards = [[] for _ in range(self.nbArms)]
+
+    def __str__(self):
+        return r"LM-DSEE($\gamma={:.3g}$, $\rho={:.3g}$, $l={:.3g}$, $b={:.3g}$)".format(self.gamma, self.rho, self.l, self.b)
+
+    def startGame(self):
+        """ Start the game (fill pulls and rewards with 0)."""
+        super(LM_DSEE, self).startGame()
+        self.current_exploration_arm = None
+        self.current_exploitation_arm = None
+        self.batch_number = 1
+        self.length_of_current_phase = None
+        self.step_of_current_phase = 0
+        self.all_rewards = [[] for _ in range(self.nbArms)]
+
+    def length_exploration_phase(self, verbose=True):
+        r""" Compute the value of the current exploration phase:
+
+        .. math:: L_1(k) = L(k) = \lceil \gamma \log(k^{\rho} l b)\rceil.
+
+        .. warning:: I think there is a typo in the paper, as their formula are weird (like :math:`al` is defined from :math:`a`).
+        """
+        value_Lk = self.gamma * np.log((self.batch_number**self.rho) * self.l * self.b)
+        length = max(1, int(np.ceil(value_Lk)))
+        if verbose: print("Length of exploration phase: computed to be = {} for batch number = {}...".format(length, self.batch_number))  # DEBUG
+        return length
+
+    def length_exploitation_phase(self, verbose=True):
+        r""" Compute the value of the current exploitation phase:
+
+        .. math:: L_2(k) = \lceil a k^{\rho} l\rceil - K L_1(k).
+
+        .. warning:: I think there is a typo in the paper, as their formula are weird (like :math:`al` is defined from :math:`a`).
+        """
+        large_value = int(np.ceil(self.a * (self.batch_number**self.rho) * self.l))
+        Lk = self.length_exploration_phase(verbose=False)
+        length = max(1, large_value - self.nbArms * Lk)
+        if verbose: print("Length of exploitation phase: computed to be = {} for batch number = {}...".format(length, self.batch_number))  # DEBUG
+        return length
+
+    def getReward(self, arm, reward):
+        """ Get a reward from an arm."""
+        super(LM_DSEE, self).getReward(arm, reward)
+        reward = (reward - self.lower) / self.amplitude
+        self.all_rewards[arm].append(reward)
+
+    def choice(self):
+        """ Chose an arm following the different phase of growing lenghts according to the LM-DSEE algorithm."""
+        # print("For a {} policy: t = {}, current_exploration_arm = {}, current_exploitation_arm = {}, batch_number = {}, length_of_current_phase = {}, step_of_current_phase = {}".format(self, self.t, self.current_exploration_arm, self.current_exploitation_arm, self.batch_number, self.length_of_current_phase, self.step_of_current_phase))  # DEBUG
+        # 1) exploration
+        if self.phase == State.Exploration:
+            # beginning of exploration phase
+            if self.current_exploration_arm is None:
+                self.current_exploration_arm = 0
+            # if length of current exploration phase not computed, do it
+            if self.length_of_current_phase is None:
+                self.length_of_current_phase = self.length_exploration_phase()
+            # if in a phase, do it
+            if self.step_of_current_phase < self.length_of_current_phase:
+                self.step_of_current_phase += 1
+            else:  # done for this arm
+                self.current_exploration_arm += 1  # go for next arm
+                # if done for all the arms, go to exploitation
+                if self.current_exploration_arm >= self.nbArms:
+                    self.length_of_current_phase = None  # flag to start the next one
+                    self.phase = State.Exploitation
+                    self.step_of_current_phase = 0
+                    self.current_exploration_arm = 0
+                    # note that this last update might force to sample the arm 0 instead of arm K-1, once in a while...
+            return self.current_exploration_arm
+        # ---
+        # 2) exploitation
+        # ---
+        elif self.phase == State.Exploitation:
+            # beginning of exploitation phase
+            if self.length_of_current_phase is None:
+                self.length_of_current_phase = self.length_exploitation_phase()
+            if self.current_exploitation_arm is None:
+                # compute exploited arm
+                mean_rewards = [np.mean(rewards_of_arm_k) for rewards_of_arm_k in self.all_rewards]
+                j_epch_k = np.argmax(mean_rewards)
+                print("A {} player at time {} and batch number {} observed the mean rewards = {} and will play {} for this exploitation phase.".format(self, self.t, self.batch_number, mean_rewards, j_epch_k))  # DEBUG
+                self.current_exploitation_arm = j_epch_k
+                # erase current memory
+                self.all_rewards = [[] for _ in range(self.nbArms)]
+            # if in a phase, do it
+            if self.step_of_current_phase < self.length_of_current_phase:
+                self.step_of_current_phase += 1
+            # otherwise, reinitialize
+            else:
+                self.phase = State.Exploration
+                self.length_of_current_phase = None  # flag to start the next one
+                self.step_of_current_phase = 0
+                self.current_exploration_arm = 0
+                self.batch_number += 1
+            return self.current_exploitation_arm
+        else:
+            raise ValueError("Error: LM_DSEE should only be in phase Exploration or Exploitation.")
+
+
