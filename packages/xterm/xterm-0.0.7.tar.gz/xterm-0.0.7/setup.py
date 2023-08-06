2501592ba390e090f441d1e0ce82d6a238eeaecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:57 +0000

    Add package xterm/0.0.7/xterm-0.0.7.tar.gz

---
+++ packages/xterm/xterm-0.0.7.tar.gz/xterm-0.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xterm",
+    version="0.0.7",
+    author="mat",
+    author_email="fake-email@getpranked.lol",
+    description="stuff i guess",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://repl.it/@mat1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
