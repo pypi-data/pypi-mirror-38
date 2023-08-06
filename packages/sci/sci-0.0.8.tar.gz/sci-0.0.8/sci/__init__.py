5a9f7e181d3a715b50bea5bbb109dbda63c2f83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:32 +0000

    Add package sci/0.0.8/sci-0.0.8.tar.gz

---
+++ packages/sci/sci-0.0.8.tar.gz/sci-0.0.8/sci/__init__.py
@@ -0,0 +1,19 @@
+"""
+The sci package contains helper and wrapper functions
+for scientists  
+"""
+#from sci.store import *
+#from sci.db import *
+#from sci.compute import *
+#from sci.json import *
+#from sci.new import *
+#from sci.old import *
+#from sci.tools import *
+
+import sci.compute
+import sci.store
+import sci.db
+import sci.json
+import sci.tools
+import sci.old
+import sci.new
