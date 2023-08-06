bf91c47c4008c7af94ab725c803cd357a15bd598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:48 +0000

    Add package ringcentral-bot-framework/0.0.5/ringcentral_bot_framework-0.0.5.tar.gz

---
+++ packages/ringcentral-bot-framework/ringcentral_bot_framework-0.0.5.tar.gz/ringcentral_bot_framework-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ringcentral_bot_framework",
+    version="0.0.5",
+    author="Drake Zhao @ RingCentral",
+    author_email="drake.zhao@ringcentral.com",
+    description="RingCentral Chatbot Framework for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zxdong262/ringcentral-chatbot-python",
+    packages=setuptools.find_packages(),
+    keywords=['ringcentral', 'bot', 'framework'],
+    install_requires=[i.strip() for i in open('requirements.txt').readlines()],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
