db1b0ba17c8408cd3af3c400f0569bfcf109871b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:09 +0000

    Add package django-nimbus-api/1.1.4/django_nimbus_api-1.1.4-py2.py3-none-any.whl

---
+++ packages/django-nimbus-api/django_nimbus_api-1.1.4-py2.py3-none-any.whl/django_nimbus_api/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+"""
+Package Description
+"""
+import os
+import sys
+import logging
+import json
+from datetime import date
+from functools import wraps
+
+__all__ = [
+    "__author__", "__copyright__", "__author_email__", "__license__", "__summary__",
+    "__title__", "__url__", "__version__", "__maintainer__", "__maintainer_email__",
+    "__platforms__",
+]
+
+__version__ = '1.1.4'
+__title__ = "django_nimbus_api"
+
+__author__ = "william"
+__author_email__ = "william.ren@live.cn"
+__maintainer__ = "william"
+__maintainer_email__ = "william.ren@live.cn"
+__url__ = "https://github.com/williamren"
+__summary__ = "djangorestframework extend"
+__platforms__ = "Any"
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2001-{0} {1}".format(date.today().year, __author__)
+
