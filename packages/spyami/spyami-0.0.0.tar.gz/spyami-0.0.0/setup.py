e6743ff57d45866e3b6c811b6f2cd807b3ddd1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:50 +0000

    Add package spyami/0.0.0/spyami-0.0.0.tar.gz

---
+++ packages/spyami/spyami-0.0.0.tar.gz/spyami-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spyami',
+    version='',
+    packages=['spyami', 'spyami.item', 'spyami.spider', 'spyami.mainloop', 'spyami.pipeline', 'spyami.selector'],
+    url='',
+    license='',
+    author='chenzhongrun',
+    author_email='',
+    description=''
+)
---
+++ packages/spyami/spyami-0.0.0.tar.gz/spyami-0.0.0/spyami/selector/selector.py
@@ -0,0 +1,6 @@
+from lxml import etree
+from scrapy.selector import Selector
+
+
+class AmiSelector(Selector):
+    pass
