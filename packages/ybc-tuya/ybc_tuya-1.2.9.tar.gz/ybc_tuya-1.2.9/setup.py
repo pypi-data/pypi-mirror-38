f37be4c8f2d71c53c340eba1b282ee8367907e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:54 +0000

    Add package ybc-tuya/1.2.9/ybc_tuya-1.2.9.tar.gz

---
+++ packages/ybc-tuya/ybc_tuya-1.2.9.tar.gz/ybc_tuya-1.2.9/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ybc_tuya',
+      version='1.2.9',
+      description='Turtle to draw.',
+      long_description='Turtle to draw.',
+      author='hurs',
+      author_email='hurs@fenbi.com',
+      keywords=['python', 'draw', 'turtle'],
+      url='http://pip.zhenguanyu.com/',
+      packages=['ybc_tuya'],
+      package_data={'ybc_tuya': ['*.py']},
+      license='MIT'
+     )
