77b6f3e0378f1e34c8b73bf19a386a98886d42f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package linen/0.0.1/linen-0.0.1.tar.gz

---
+++ packages/linen/linen-0.0.1.tar.gz/linen-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name='linen',
+  version='0.0.1',
+  author='Unknown',
+  author_email='vipcalio@gmail.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="linen is what you want",
+  packages=['linen'],
+  scripts=[]
+)
