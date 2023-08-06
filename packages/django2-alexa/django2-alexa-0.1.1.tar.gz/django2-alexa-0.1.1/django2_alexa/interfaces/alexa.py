3f55ae8dd47e3696ea0bd8dc424f3eb8a6023eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:37 +0000

    Add package django2-alexa/0.1.1/django2-alexa-0.1.1.tar.gz

---
+++ packages/django2-alexa/django2-alexa-0.1.1.tar.gz/django2-alexa-0.1.1/django2_alexa/interfaces/alexa.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.http import HttpResponseServerError
+from django.views.decorators.csrf import csrf_exempt
+
+from django2_alexa.interfaces.request.alexa import LaunchRequest
+from django2_alexa.utils.s3_verification import is_valid_request
+
+
+def launch(func):
+    @csrf_exempt
+    def wrapper(request, *args, **kwargs):
+        if getattr(settings, "ALEXA_VERIFY_CONN", False) and not is_valid_request(request):
+            # TODO: Troubleshooting part in docs
+            return HttpResponseServerError("Amazon Server verification failed.")
+        lr = LaunchRequest(request)
+        return func(request, lr, *args, **kwargs)
+    return wrapper
+
+
+def intent(name: str):
+    def inner(func):
+        @csrf_exempt
+        def wrapper(request, *args, **kwargs):
+            if getattr(settings, "ALEXA_VERIFY_CONN", False) and not is_valid_request(request):
+                return HttpResponseServerError("Amazon Server verification failed.")
+            return func(request, *args, **kwargs)
+        return wrapper
+    return inner
