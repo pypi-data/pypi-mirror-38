f7f5e91fd1c8220e28e8f4e3c73d1976e832af83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:55 +0000

    Add package friendsreco/0.0.4/friendsreco-0.0.4.tar.gz

---
+++ packages/friendsreco/friendsreco-0.0.4.tar.gz/friendsreco-0.0.4/friendsreco/run.py
@@ -0,0 +1,33 @@
+import argparse
+import asyncio
+import logging
+import uvloop
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+from .logger import configure as configure_logging
+from .api import Api
+
+parser = argparse.ArgumentParser(description='Friends Recommendations API')
+parser.add_argument('--neo4j-url', '-n', type=str, required=True)
+parser.add_argument('--port', '-p', type=int, default=8080)
+parser.add_argument('--no-initial-friendships', '-i', action='store_false')
+parser.add_argument('--debug', '-d', action='store_true')
+
+args = parser.parse_args()
+
+configure_logging(args.debug)
+
+api = Api(args.neo4j_url, args.no_initial_friendships)
+
+
+def main():
+    from .app import app, register_cleanup
+
+    register_cleanup(api.cleanup)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(api.start())
+
+    app.run(port=args.port,
+            access_log=logging.getLogger('friendsreco.access'))
---
+++ packages/friendsreco/friendsreco-0.0.4.tar.gz/friendsreco-0.0.4/setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3.7
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+import os.path
+import sys
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+version = open(os.path.join(root_dir, 'VERSION')).read()
+long_description = open(os.path.join(root_dir, 'README.md')).read()
+
+install_requires = [
+    'connexion>=2.0.2',
+    'aioneo4j>=0.0.5',
+    'aiohttp>=3.4.4',
+    'ujson>=1.35',
+    'uvloop>=0.11.3'
+]
+
+tests_require = [
+    'pytest',
+    'pytest-cov'
+]
+
+setup_requires = [
+    'pytest-runner',
+    'flake8'
+]
+
+
+class PyTest(TestCommand):
+
+    user_options = [
+        ('cov-html=', None, 'Generate html report'),
+        ('filter=', None, "Pytest setence to filter (see pytest '-k' option)")
+    ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ['--cov', 'friendsreco', '-xvv']
+        self.cov_html = False
+        self.filter = False
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+
+        if self.cov_html:
+            self.pytest_args.extend(['--cov-report', 'html'])
+        else:
+            self.pytest_args.extend(['--cov-report', 'term-missing'])
+
+        if self.filter:
+            self.pytest_args.extend(['-k', self.filter])
+
+        self.pytest_args.extend(['tests'])
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='friendsreco',
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    description='A simple HTTP friends recommendations service.',
+    long_description=long_description,
+    author='Diogo Dutra',
+    author_email='diogodutradamata@gmail.com',
+    url='https://github.com/dutradda/friendsreco',
+    keywords='recomendations service graph neo4j',
+    license='MIT',
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    cmdclass={'test': PyTest},
+    test_suite='tests',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+    entry_points={
+        'console_scripts': [
+            'friendsreco = friendsreco.run:main'
+        ]
+    }
+)
