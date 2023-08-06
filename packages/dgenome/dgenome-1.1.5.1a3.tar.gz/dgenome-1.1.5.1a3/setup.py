fb5654a192bcb7896cd214a8056750a82ceb68bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:56 +0000

    Add package dgenome/1.1.5.1a3/dgenome-1.1.5.1a3.tar.gz

---
+++ packages/dgenome/dgenome-1.1.5.1a3.tar.gz/dgenome-1.1.5.1a3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+import os
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
+
+setup(version=version)
