89ac715ea7d607fbdb6080cb6e01d0eb2c7df4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:54 +0000

    Add package redis-gt/1.2.0/redis_gt-1.2.0.tar.gz

---
+++ packages/redis-gt/redis_gt-1.2.0.tar.gz/redis_gt-1.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+requires = [
+    'redis==2.10.6',
+]
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='redis_gt',
+    version='1.2.0',
+    description='Global throttling with Redis.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sukobuto/python-redis-gt',
+    author='sukobuto',
+    author_email='sukobuto@gmail.com',
+    license='MIT',
+    keywords='throttle redis async',
+    packages=[
+        'redis_gt',
+    ],
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
