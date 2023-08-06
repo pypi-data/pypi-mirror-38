761036a3a9ba6806d87e385d388f37fa1823b725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:55 +0000

    Add package terminal-text/1.2/terminal-text-1.2.tar.gz

---
+++ packages/terminal-text/terminal-text-1.2.tar.gz/terminal-text-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+     name='terminal-text',    
+     version='1.2',                         
+     scripts=['termtext'],
+     description='Its a simple commandline tool that allows you send text messages and mms to your phone using terminal',
+     install_requires=[
+        "docopt",
+    ],
+    classifiers=[
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+    		'Programming Language :: Python :: 3.6'
+   ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+ )
