8a341441c0dd4aa7741899a360136011c20d028e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:14 +0000

    Add package arcus/0.1.0/arcus-0.1.0.tar.gz

---
+++ packages/arcus/arcus-0.1.0.tar.gz/arcus-0.1.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+install_requirements = [
+    'requests',
+    'pytz'
+]
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='arcus',
+    version='0.1.0',
+    author='Cuenca',
+    author_email='dev@cuenca.com',
+    description='Arcus API Client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cuenca-mx/arcus-python',
+    packages=setuptools.find_packages(),
+    install_requires=install_requirements,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    extras_require={
+        'dev': [
+            'pytest>=3',
+            'pycodestyle'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
