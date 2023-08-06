1176c20aee6867c09816fd9a8dfd2ba3a887f7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:49 +0000

    Add package git-app-version/1.0.1/git_app_version-1.0.1-py2.py3-none-any.whl

---
+++ packages/git-app-version/git_app_version-1.0.1-py2.py3-none-any.whl/git_app_version/__main__.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+'''
+    Main module
+'''
+# from __future__ import unicode_literals
+
+import os
+import re
+
+import click
+from git.exc import InvalidGitRepositoryError, NoSuchPathError
+from tabulate import tabulate
+
+import git_app_version
+from git_app_version.dumper import FileDumper
+from git_app_version.githandler import RESERVED_KEYS, GitHandler
+
+
+def print_version(ctx, param, value):
+    '''
+    display application version
+    '''
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('git-app-version ' + git_app_version.__version__)
+    ctx.exit()
+
+
+CONTEXT_SETTINGS = {'help_option_names': ['-h', '--help']}
+
+
+class MetadataParamType(click.ParamType):
+    '''
+    Click paramerer Type to parse <key>=<value> option
+    '''
+    name = 'metadata'
+
+    def convert(self, value, param, ctx):
+        '''
+        convert row option value to dict
+        '''
+        try:
+            match = re.match(r'^([^=]+)=(.*)$', value)
+            if not match:
+                raise ValueError(
+                    u'%s is not a valid meta data string e.g. : <key>=<value>'
+                    % value
+                )
+
+            if match.group(1) in RESERVED_KEYS:
+                raise ValueError(u'%s is a reserved key' % match.group(1))
+
+            return {match.group(1): match.group(2).strip('"\'')}
+
+        except ValueError as exc:
+            self.fail(str(exc), param, ctx)
+
+
+METADATA = MetadataParamType()
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    '--version',
+    '-V',
+    is_flag=True,
+    callback=print_version,
+    expose_value=False,
+    is_eager=True
+)
+# @click.option('--verbose', '-v', count=True)
+@click.option('--quiet', '-q', is_flag=True, help='silent mode')
+@click.option(
+    '--output',
+    '-o',
+    default='version',
+    help='output file path (without extension).'
+    ' Default is \'<repository-path>/version\'.'
+)
+@click.option(
+    '--format',
+    '-f',
+    'output_formats',
+    type=click.Choice(['all', 'json', 'yml', 'xml', 'ini', 'csv', 'sh']),
+    multiple=True,
+    default=['json'],
+    help='output file format and extension,'
+    ' use \'all\' to output all format, can be set several times ,'
+    ' Default is json.'
+)
+@click.option(
+    '--namespace',
+    '-n',
+    default='',
+    help='namespace like notation in version file, use dot separator'
+    ' to segment namespaces e.g.: \'foo.bar.git\'.'
+    ' Default is \'app_version\' for XML and INI'
+    ' and no namespace for JSON and YAML.'
+    ' Never used for CSV or Shell file.'
+)
+@click.option(
+    '--meta',
+    '-m',
+    type=METADATA,
+    multiple=True,
+    help='meta data to add, format = "<key>=<value>", can be set several times'
+)
+@click.option(
+    '--csv-delimiter',
+    '-d',
+    'csv_delimiter',
+    default=u',',
+    help='CSV delimiter, default=","'
+)
+@click.option(
+    '--csv-eol',
+    '-e',
+    'csv_eol',
+    type=click.Choice(['lf', 'crlf']),
+    default="lf",
+    help='CSV end of line,'
+    ' lf = Unix new line, crlf = windows new line, default=lf'
+)
+@click.option(
+    '--csv-quote',
+    '-u',
+    'csv_quote',
+    default=u'"',
+    help='CSV quoting character, default=\'"\''
+)
+@click.argument(
+    'repository',
+    type=click.Path(
+        exists=True, resolve_path=True, file_okay=False, readable=True
+    ),
+    default=os.getcwd()
+)
+@click.argument('commit', default='HEAD')
+@click.pass_context
+def dump(
+    ctx, repository, commit, output, output_formats, namespace, meta, quiet,
+    csv_delimiter, csv_quote, csv_eol
+):
+    '''
+    Get Git commit informations and store them in a config file
+
+    \b
+    REPOSITORY git repository path, Default is the current directory.
+    COMMIT     git commit to check, Default is HEAD.
+    '''
+
+    try:
+        # pprint(repository)
+        # pprint(type(repository))
+        vcs = GitHandler(repository)
+
+        data = vcs.get_infos(commit=commit)
+
+        # add metadatas
+        for item in meta:
+            for key, val in item.items():
+                data[key] = val
+
+        if not quiet:
+            print_commit_table(data)
+
+        dumper = FileDumper()
+        if not quiet and len(output_formats):
+            click.echo("written to :")
+
+        if 'all' in output_formats:
+            output_formats = ['json', 'yml', 'xml', 'ini', 'csv', 'sh']
+
+        for output_format in output_formats:
+            dest = dumper.dump(
+                data=data,
+                fileformat=output_format,
+                target=output,
+                cwd=repository,
+                namespace=namespace,
+                csv_delimiter=csv_delimiter,
+                csv_quote=csv_quote,
+                csv_eol=csv_eol
+            )
+            if not quiet:
+                click.echo(dest)
+
+        ctx.exit(0)
+
+    except (InvalidGitRepositoryError, NoSuchPathError):
+        msg = u'The directory \'{}\' is not a git repository.'
+        click.echo(msg.format(click.format_filename(repository)))
+        ctx.exit(1)
+
+
+def print_commit_table(data):
+    '''
+    display a dict as a Table in standard output
+    '''
+    click.echo('Git commit :')
+    keys = sorted(data.keys())
+    table = []
+    for key in keys:
+        if isinstance(data[key], list):
+            item = ' '.join(data[key])
+        else:
+            item = data[key]
+
+        table.append([key, item])
+
+    click.echo(tabulate(table, tablefmt='simple'))
+
+
+if __name__ == '__main__':
+    dump()
---
+++ packages/git-app-version/git_app_version-1.0.1-py2.py3-none-any.whl/git_app_version/githandler.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+'''
+    Git manipulation
+'''
+from __future__ import unicode_literals
+
+import re
+
+from git import GitCommandError, Repo
+
+import git_app_version.helper.date as dthelper
+
+RESERVED_KEYS = (
+    'abbrev_commit', 'author_date', 'author_email', 'author_name',
+    'author_timestamp', 'branches', 'commit_date', 'commit_timestamp',
+    'committer_email', 'committer_name', 'deploy_date', 'deploy_timestamp',
+    'full_commit', 'message', 'top_branches', 'version'
+)
+
+
+class GitHandler(object):
+    '''
+    Git
+    '''
+
+    def __init__(self, path):
+        self.repo = Repo(path)
+
+    def get_deploy_date(self):
+        '''
+        get current date
+        '''
+
+        return dthelper.utcnow()
+
+    def get_version(self, commit='HEAD', default=''):
+        '''
+        get human readable version
+        result of `git describe --tag --always`
+        '''
+
+        try:
+            version = self.repo.git.describe('--tag', '--always',
+                                             commit).strip()
+        except GitCommandError:
+            version = ''
+
+        if not version:
+            version = default
+
+        return version
+
+    def get_branches(self, commit='HEAD'):
+        '''
+        get remote branches which commit belong
+        result of `git branch --remote --no-color --contains=<commit>`
+        '''
+
+        raw = self.repo.git.branch(
+            "--no-color", "--remote", "--contains=" + commit
+        )
+        raw_branches = raw.splitlines()
+
+        regex_point = re.compile(r'->')  # remove git reference pointing
+
+        branches = []
+        for raw_branch in raw_branches:
+            branch = raw_branch.strip()
+            match = regex_point.search(branch)
+            if not match:
+                branches.append(branch)
+
+        return branches
+
+    def get_top_branches(self, branches, abbrev_commit=None):
+        '''
+        get remote branches which commit belong and is the branch HEAD
+        '''
+
+        top_branch = []
+
+        for branch in branches:
+            if abbrev_commit == self.get_abbrev_commit(branch):
+                top_branch.append(branch)
+
+        return top_branch
+
+    def remove_remote_prefix(self, branches):
+        '''
+        remove git remote prefix from branch name
+        e.g.: origin/master = master
+        '''
+        regex_remote = re.compile(r'^[^/]+/')  # remove git remote prefix
+
+        clean_branches = []
+        for branch in branches:
+            clean_branches.append(regex_remote.sub('', branch))
+
+        return clean_branches
+
+    def get_committer_name(self, commit='HEAD'):
+        '''
+        get git committer name
+        '''
+
+        return self.repo.commit(commit).committer.name
+
+    def get_committer_email(self, commit='HEAD'):
+        '''
+        get git committer email
+        '''
+
+        return self.repo.commit(commit).committer.email
+
+    def get_author_name(self, commit='HEAD'):
+        '''
+        get git author name
+        '''
+
+        return self.repo.commit(commit).author.name
+
+    def get_author_email(self, commit='HEAD'):
+        '''
+        get git author email
+        '''
+
+        return self.repo.commit(commit).author.email
+
+    def get_commit_date(self, commit='HEAD'):
+        '''
+        get git commit date
+        '''
+
+        return self.repo.commit(commit).committed_datetime
+
+    def get_author_date(self, commit='HEAD'):
+        '''
+        get git authoring date
+        '''
+
+        return self.repo.commit(commit).authored_datetime
+
+    def get_full_commit(self, commit='HEAD'):
+        '''
+        get git commit full SHA1 hash
+        '''
+
+        return self.repo.commit(commit).hexsha
+
+    def get_abbrev_commit(self, commit='HEAD'):
+        '''
+        get git commit shorten SHA1 hash
+        '''
+
+        return self.repo.commit(commit).hexsha[0:7]
+
+    def get_message(self, commit='HEAD'):
+        '''
+        get git commit message
+        '''
+
+        return self.repo.commit(commit).message.strip()
+
+    def get_infos(self, commit='HEAD'):
+        '''
+        get all git commit data
+        '''
+
+        deploy_date = self.get_deploy_date()
+        abbrev_commit = self.get_abbrev_commit(commit)
+        commit_date = self.get_commit_date(commit)
+        author_date = self.get_author_date(commit)
+
+        branches = self.get_branches(commit)
+        top_branch = self.get_top_branches(
+            branches=branches, abbrev_commit=abbrev_commit
+        )
+
+        return {
+            'branches': self.remove_remote_prefix(branches),
+            'top_branches': self.remove_remote_prefix(top_branch),
+            'version': self.get_version(commit, default=abbrev_commit),
+            'abbrev_commit': abbrev_commit,
+            'full_commit': self.get_full_commit(commit),
+            'message': self.get_message(commit),
+            'author_name': self.get_author_name(commit),
+            'author_email': self.get_author_email(commit),
+            'committer_name': self.get_committer_name(commit),
+            'committer_email': self.get_committer_email(commit),
+            'commit_date': dthelper.iso8601_from_datetime(commit_date),
+            'commit_timestamp': dthelper.timestamp_from_datetime(commit_date),
+            'author_date': dthelper.iso8601_from_datetime(author_date),
+            'author_timestamp': dthelper.timestamp_from_datetime(author_date),
+            'deploy_date': dthelper.iso8601_from_datetime(deploy_date),
+            'deploy_timestamp': dthelper.timestamp_from_datetime(deploy_date),
+        }
