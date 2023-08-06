6bf6a1b7df9d3d1448966a8e9f61ba73fae9cfd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:10 +0000

    Add package vault-psycopg2/0.1.6/vault-psycopg2-0.1.6.tar.gz

---
+++ packages/vault-psycopg2/vault-psycopg2-0.1.6.tar.gz/vault-psycopg2-0.1.6/vault_psycopg2/__init__.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+
+class VaultPsycopg:
+    _vault_wrapper = None
+    _database_connection = None
+
+    database_config = {}
+    vault_config = {}
+
+    def __init__(self, database_config=None, vault_config=None):
+        self.vault_config = vault_config
+        self.database_config = database_config
+        if vault_config:
+            from vault_psycopg2.vault import Vault
+            self._vault_wrapper = Vault.instance(**vault_config)
+
+    @property
+    def connection(self):
+        if self._database_connection:
+            try:
+                cursor = self._database_connection.cursor()
+                cursor.execute('SELECT 1')
+                return self._database_connection
+            except psycopg2.Error:
+                pass
+
+        if self._vault_wrapper and self.database_config.get('role') is not None:
+            credentials = self._vault_wrapper.client.read('database/creds/' + self.database_config.get('role'))
+            user = credentials['data']['username']
+            password = credentials['data']['password']
+        else:
+            user = self.database_config.get('user')
+            password = self.database_config.get('password')
+
+        self._database_connection = psycopg2.connect(
+            host=self.database_config.get('host'),
+            port=self.database_config.get('port'),
+            dbname=self.database_config.get('dbname'),
+            user=user,
+            password=password,
+        )
+
+        return self._database_connection
