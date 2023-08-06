d2441024aaf12535e52df489fa3d3823a802c59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package mlpc/0.0.7/mlpc-0.0.7.tar.gz

---
+++ packages/mlpc/mlpc-0.0.7.tar.gz/mlpc-0.0.7/mlpc/entity/plot.py
@@ -0,0 +1,9 @@
+from mlpc.entity.base_entity import BaseEntity
+
+
+class Plot(BaseEntity):
+    def __init__(self, storage):
+        super().__init__(storage)
+
+    def get_filepath_for_writing(self, plot_name, file_type):
+        return self._storage.get_filepath_for_writing("plot", plot_name)
