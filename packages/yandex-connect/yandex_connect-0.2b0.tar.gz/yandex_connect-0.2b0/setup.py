82ab4415b1f3ad34a72ffd33850868c0c5ee1470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:31 +0000

    Add package yandex-connect/0.2b0/yandex_connect-0.2b0.tar.gz

---
+++ packages/yandex-connect/yandex_connect-0.2b0.tar.gz/yandex_connect-0.2b0/setup.py
@@ -0,0 +1,19 @@
+# coding: utf8
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='yandex_connect',
+      version='0.02b',
+      description='API Yandex Connect',
+      url='http://github.com/zt50tz/yandex-connect',
+      author='Alexeev Nick',
+      author_email='n@akolka.ru',
+      license='MIT',
+      packages=['yandex_connect'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
