26693f52e457a7ee377400ba235a910b8f49f4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:16 +0000

    Add package Taobao-Spider/1.0.1/Taobao-Spider-1.0.1.tar.gz

---
+++ packages/Taobao-Spider/Taobao-Spider-1.0.1.tar.gz/Taobao-Spider-1.0.1/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "Taobao-Spider",      #这里是pip项目发布的名称
+    version = "1.0.1",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "Taobao"," spider","淘宝","爬虫"),
+    description = "A spider of taobaogoods",
+    long_description = "A spider of taobaogoods",
+    license = "MIT Licence",
+
+    url = "https://github.com/xjkj123/Taobao-Spider",     #项目相关文件地址，一般是github
+    author = "MRX",
+    author_email = "xjkj123@icloud.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['requests', 'pandas', 'sqlite3', 'tqdm']
+)
+
