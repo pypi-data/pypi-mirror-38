330dfb0fb1bd36f454af6c5fd2fccbb88330cb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:29 +0000

    Add package harmony-tools/0.1a0/harmony_tools-0.1a0.tar.gz

---
+++ packages/harmony-tools/harmony_tools-0.1a0.tar.gz/harmony_tools-0.1a0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = '0.1a0'
+
+
+setup(
+    name='harmony_tools',
+    version=version,
+    packages=find_packages(),
+    package_dir={'harmony_tools': 'harmony_tools'},
+    entry_points={
+        'console_scripts': ['harmony_tools=harmony_tools.cli:main'],
+    },
+    license='MIT',
+    url='https://github.com/a1fred/harmony_tools',
+    author='a1fred',
+    author_email='demalf@gmail.com',
+    classifiers=[
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite="tests",
+)
