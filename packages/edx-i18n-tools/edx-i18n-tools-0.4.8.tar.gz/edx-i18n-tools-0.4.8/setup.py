f994b8f6acf6ffb54dc6cb2008ebb1e5d05569b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:34 +0000

    Add package edx-i18n-tools/0.4.8/edx-i18n-tools-0.4.8.tar.gz

---
+++ packages/edx-i18n-tools/edx-i18n-tools-0.4.8.tar.gz/edx-i18n-tools-0.4.8/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='edx-i18n-tools',
+    version='0.4.8',
+    description='edX Internationalization Tools',
+    author='edX',
+    author_email='oscm@edx.org',
+    url='https://github.com/edx/i18n-tools',
+    packages=[
+        'i18n',
+    ],
+    install_requires=[
+        'django>=1.8,<2.0',
+        'polib',
+        'path.py>=7.0',
+        'pyYaml',
+        'six',
+    ],
+    entry_points={
+        'console_scripts': [
+            'i18n_tool = i18n.main:main',
+        ],
+    },
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+    ],
+)
