10f04656d2cf291d722d3de15add9dc9f6a8d3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:17 +0000

    Add package rigger_plugin_framework/0.0.16/rigger_plugin_framework-0.0.16.tar.gz

---
+++ packages/rigger_plugin_framework/rigger_plugin_framework-0.0.16.tar.gz/rigger_plugin_framework-0.0.16/rigger_plugin_framework/plugin.py
@@ -0,0 +1,31 @@
+
+class Plugin:
+    __slots__ = ()
+
+    @staticmethod
+    def get_plugin_type():
+        return 0
+
+    @staticmethod
+    def get_plugin_name():
+        return ""
+
+    @staticmethod
+    def doc():
+        return "you shoud descript plug's function here"
+
+    def on_enable(self):
+        pass
+
+    def on_disable(self):
+        pass
+
+    def on_start(self):
+        pass
+
+    def on_stop(self):
+        pass
+
+    def on_load(self):
+        pass
+
