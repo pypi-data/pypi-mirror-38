d6bf9e3d3f8f917ae9d02e3f4c80c745cc66b929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:51 +0000

    Add package imreadeval/0.2.2/imreadeval-0.2.2-py2-none-any.whl

---
+++ packages/imreadeval/imreadeval-0.2.2-py2-none-any.whl/imreadeval/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__author__ = 'QU Xiaofeng'
+
+from .imread import imread, imread_eval, import_imread_from
+
+__all__ = [
+	'imread',
+	'imread_eval',
+	'import_imread_from'
+	]
