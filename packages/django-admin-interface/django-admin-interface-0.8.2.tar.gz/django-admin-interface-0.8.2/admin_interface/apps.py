0a3724c852359e81f19c9982ab342b1a75ffdfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:42 +0000

    Add package django-admin-interface/0.8.2/django-admin-interface-0.8.2.tar.gz

---
+++ packages/django-admin-interface/django-admin-interface-0.8.2.tar.gz/django-admin-interface-0.8.2/admin_interface/apps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+class AdminInterfaceConfig(AppConfig):
+
+    name = 'admin_interface'
+    verbose_name = 'Admin Interface'
+
+    def ready(self):
+
+        from admin_interface import settings
+        from admin_interface.models import Theme
+
+        settings.check_installed_apps()
+        post_migrate.connect(
+            Theme.post_migrate_handler, sender=self)
