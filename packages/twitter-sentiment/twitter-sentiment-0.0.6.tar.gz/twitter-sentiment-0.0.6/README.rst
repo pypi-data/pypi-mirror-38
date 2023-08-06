800654b28f4f4b964d12b048054a4defe56b63ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:01 +0000

    Add package twitter-sentiment/0.0.6/twitter-sentiment-0.0.6.tar.gz

---
+++ packages/twitter-sentiment/twitter-sentiment-0.0.6.tar.gz/twitter-sentiment-0.0.6/README.rst
@@ -0,0 +1,28 @@
+Python library to Explore Emotions Behind Tweets
+================================================
+
+|license| |python_version| |coverage| |test|
+
+python-sentiment is a Python package leveraging NLP algorithm and the Twitter API to score the sentiment of a tweet.
+
+Installation 
+------------ 
+Installing twitter-sentiment is simple, you just have to use pip.
+::
+
+    pip install twitter-sentiment
+
+
+Documentation
+-------------  
+Documentation is available at `twitter-sentiment.readthedocs.io <https://twitter-sentiment.readthedocs.io/en/latest/index.html>`_
+
+twitter-sentiment in a nutshel  
+------------------------------
+twitter-sentiment let you score a tweet/list of tweets on a scale of -1 to 1. A score below 0 represents a negative tweet, while a score greater than 0 represents a positive tweet. To score a tweet, twitter-sentiment levereage TextBlob Naive Byaise NLP library. More information can be find at `textblob.readthedocs.io <https://textblob.readthedocs.io/en/dev/advanced_usage.html#sentiment-analyzers>`_
+
+.. |license|  image:: https://img.shields.io/packagist/l/doctrine/orm.svg?style=flat-square   :alt: Packagist
+.. |python_version| image:: https://img.shields.io/badge/Python-3-yellow.svg?style=flat-square
+.. |test| image:: https://img.shields.io/teamcity/codebetter/bt428.svg?style=flat-square   :alt: TeamCity CodeBetter
+.. |coverage| image:: https://img.shields.io/teamcity/coverage/bt428.svg?style=flat-square   :alt: TeamCity CodeBetter Coverage
+
---
+++ packages/twitter-sentiment/twitter-sentiment-0.0.6.tar.gz/twitter-sentiment-0.0.6/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+from setuptools.command.install import install
+
+import os
+import sys
+
+VERSION = '0.0.6'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+class VerifyVersion(install):
+    """
+    Custom command to verify the package version matches the git tag version
+    """
+    description = "Command ran to check if the new uploaded version matches the git tag"
+    
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = f'Tag {tag} does not match current version {VERSION} in setup.py'
+            sys.exit(info)
+
+setuptools.setup(
+    name="twitter-sentiment",
+    version=VERSION,
+    author="Teddy Crepineau",
+    author_email="teddy.crepineau@gmail.com",
+    description="Twitter sentiment is a Python library leveraging NLP "
+                "and the Twitter API to determine the emotion of a tweet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TeddyCr/twitter-sentiment",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Other Audience",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    cmdclass= {
+        'verify': VerifyVersion,
+    }
+)
