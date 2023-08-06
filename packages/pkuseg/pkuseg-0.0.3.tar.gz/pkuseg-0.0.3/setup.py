34fed62063549efd5f10a0b20cc0b24107ed85db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:14 +0000

    Add package pkuseg/0.0.3/pkuseg-0.0.3.tar.gz

---
+++ packages/pkuseg/pkuseg-0.0.3.tar.gz/pkuseg-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+long_description='pkuseg-python'
+
+setuptools.setup(
+        name="pkuseg",
+        version="0.0.3",
+        author="Lanco",
+        author_email="luoruixuan97@pku.edu.cn",
+        description="A small package for Chinese word segmentation",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/lancopku/pkuseg-python",
+        packages=setuptools.find_packages(),
+        package_data={'': ['*.txt*']},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+        )
