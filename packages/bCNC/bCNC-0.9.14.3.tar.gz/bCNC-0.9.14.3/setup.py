46e9ba109177eeefe199a60102934cffc66d8943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:16 +0000

    Add package bCNC/0.9.14.3/bCNC-0.9.14.3.tar.gz

---
+++ packages/bCNC/bCNC-0.9.14.3.tar.gz/bCNC-0.9.14.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "bCNC",
+	version = "0.9.14.3",
+	description='Swiss army knife for all your CNC/g-code needs',
+	license="GPL",
+	#long_description=long_description,
+	packages = find_packages(),
+	author = "Harvie",
+	#author_email='foomail@foo.com',
+	url="https://github.com/vlachoudis/bCNC",
+	install_requires = [
+		'pyserial>=3.4',
+		'numpy>=1.15.4',
+		'opencv-python>=3.4.2.17',
+		'Pillow>=5.3.0',
+	],
+
+	entry_points = {
+		'console_scripts': [
+			#'bCNC = {package}.{module}:{main_function}',
+			#'bCNC = bCNC.bCNC:main',
+			'bCNC = bCNC.__main__:main',
+		]
+	}
+)
