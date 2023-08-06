6e12535484f2232c057f905ddc0e50f4f3806d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:16 +0000

    Add package tcod/8.1.0/tcod-8.1.0.tar.gz

---
+++ packages/tcod/tcod-8.1.0.tar.gz/tcod-8.1.0/libtcod/src/libtcod/color/canvas.cpp
@@ -0,0 +1,60 @@
+/* libtcod
+ * Copyright © 2008-2018 Jice and the libtcod contributers.
+ * All rights reserved.
+ *
+ * libtcod 'The Doryen library' is a cross-platform C/C++ library for roguelike
+ * developers.
+ * Its source code is available from:
+ * https://github.com/libtcod/libtcod
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of copyright holder nor the names of its contributors may not
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "canvas.h"
+
+#include <stdexcept>
+
+#include "../../vendor/lodepng.h"
+
+namespace tcod {
+namespace image {
+Image load(const std::string& filename)
+{
+  unsigned img_width, img_height;
+  std::vector<unsigned char> img_data;
+  if (lodepng::decode(img_data, img_width, img_height, filename)) {
+    throw std::runtime_error("File not found: " + filename);
+  }
+  Image image(img_width, img_height);
+  std::vector<unsigned char>::iterator img_iter = img_data.begin();
+  for (int y = 0; y < image.height(); ++y) {
+    for (int x = 0; x < image.width(); ++x) {
+      image.at(x, y) = ColorRGBA{
+          img_iter[0], img_iter[1], img_iter[2], img_iter[3]};
+      img_iter += 4;
+    }
+  }
+  return image;
+}
+} // namespace image
+} // namespace tcod
