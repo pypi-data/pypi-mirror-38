e5ceeabcead98a3d60315be20554b631c00f1e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:15 +0000

    Add package stdlib-list/0.5.0/stdlib-list-0.5.0.tar.gz

---
+++ packages/stdlib-list/stdlib-list-0.5.0.tar.gz/stdlib-list-0.5.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+import versioneer
+
+
+try:
+    with open('README.rst') as f:
+        long_description = f.read()
+except IOError:
+    long_description = ""
+
+
+setuptools.setup(
+    name='stdlib-list',
+    license='MIT',
+    author='Jack Maney',
+    author_email='jackmaney@gmail.com',
+    url='https://github.com/jackmaney/python-stdlib-list',
+    version=versioneer.get_version(),
+    extras_require={"develop": ["sphinx"]},
+    description='A list of Python Standard Libraries (2.6-7, 3.2-6).',
+    long_description=long_description,
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    cmdclass=versioneer.get_cmdclass(),
+)
