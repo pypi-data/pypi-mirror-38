f2444b24796cc54bb232288e65fbd7cbd25e4974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:16 +0000

    Add package screamshotter/1.1.1/screamshotter-1.1.1.tar.gz

---
+++ packages/screamshotter/screamshotter-1.1.1.tar.gz/screamshotter-1.1.1/screamshotter/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import include, url
+
+
+urlpatterns = [
+    url(r'',  include('screamshot.urls', namespace='screamshot', app_name='screamshot')),
+]
---
+++ packages/screamshotter/screamshotter-1.1.1.tar.gz/screamshotter-1.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='screamshotter',
+    version='1.1.1',
+    author='Mathieu Leplatre',
+    author_email='mathieu.leplatre@makina-corpus.com',
+    url='https://github.com/makinacorpus/django-screamshot',
+    description='Web pages capture server',
+    long_description='',
+    install_requires=[
+        'django-screamshot',
+        'django==1.11.11',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=['Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Programming Language :: Python :: 2.7'],
+)
