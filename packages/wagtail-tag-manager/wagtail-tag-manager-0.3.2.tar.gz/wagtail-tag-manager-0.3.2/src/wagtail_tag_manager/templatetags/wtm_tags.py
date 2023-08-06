13f6cfb56b4aa8dcb44f30034a2c8dca6d19e56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:19 +0000

    Add package wagtail-tag-manager/0.3.2/wagtail-tag-manager-0.3.2.tar.gz

---
+++ packages/wagtail-tag-manager/wagtail-tag-manager-0.3.2.tar.gz/wagtail-tag-manager-0.3.2/src/wagtail_tag_manager/templatetags/wtm_tags.py
@@ -0,0 +1,66 @@
+from django import template
+from django.conf import settings
+from django.templatetags.static import static
+from django.urls import reverse
+
+from wagtail_tag_manager.forms import ConsentForm
+from wagtail_tag_manager.models import Tag
+from wagtail_tag_manager.strategy import TagStrategy
+
+register = template.Library()
+
+
+@register.inclusion_tag(
+    "wagtail_tag_manager/templatetags/instant_tags.html", takes_context=True
+)
+def wtm_instant_tags(context):
+    context["tags"] = []
+    request = context.get("request", None)
+
+    if request is not None:
+        strategy = TagStrategy(request)
+        context["tags"] = [tag.get("element").decode() for tag in strategy.result]
+
+    return context
+
+
+@register.inclusion_tag("wagtail_tag_manager/templatetags/lazy_manager.html")
+def wtm_lazy_manager():
+    return {
+        "config": {"state_url": reverse("wtm:state"), "lazy_url": reverse("wtm:lazy")}
+    }
+
+
+@register.inclusion_tag(
+    "wagtail_tag_manager/templatetags/cookie_bar.html", takes_context=True
+)
+def wtm_cookie_bar(context, include_form=False):
+    request = context.get("request")
+    cookie_state = TagStrategy(request).cookie_state
+
+    return {
+        "manage_view": getattr(settings, "WTM_MANAGE_VIEW", True),
+        "include_form": include_form,
+        "form": ConsentForm(initial=cookie_state),
+        "style": static("wtm.bundle.css"),
+    }
+
+
+@register.inclusion_tag(
+    "wagtail_tag_manager/templatetags/manage_form.html", takes_context=True
+)
+def wtm_manage_form(context):
+    request = context.get("request")
+    cookie_state = TagStrategy(request).cookie_state
+
+    return {"form": ConsentForm(initial=cookie_state)}
+
+
+@register.inclusion_tag(
+    "wagtail_tag_manager/templatetags/status_table.html", takes_context=True
+)
+def wtm_status_table(context):
+    context["tags"] = {}
+    for tag_type in Tag.get_types():
+        context["tags"][tag_type] = Tag.objects.filter(tag_type=tag_type)
+    return context
