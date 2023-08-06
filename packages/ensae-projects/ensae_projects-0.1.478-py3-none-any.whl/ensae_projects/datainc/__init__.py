c17ca17202602c8be0cd721b188b871b9e899128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:46 +0000

    Add package ensae-projects/0.1.478/ensae_projects-0.1.478-py3-none-any.whl

---
+++ packages/ensae-projects/ensae_projects-0.1.478-py3-none-any.whl/ensae_projects/datainc/__init__.py
@@ -0,0 +1,8 @@
+"""
+@file
+@brief  Shortcuts to data
+"""
+
+from .data_exception import PasswordException
+from .data_helper import change_encoding, enumerate_text_lines, convert_dates, change_encoding_improve, clean_column_name_sql_dump
+from .datazips import besancon_df
