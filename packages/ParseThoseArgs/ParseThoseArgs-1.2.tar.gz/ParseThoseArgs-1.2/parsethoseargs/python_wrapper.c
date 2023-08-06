c0e18c8d52f145da58b68960feca176c2b0415d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:06 +0000

    Add package ParseThoseArgs/1.2/ParseThoseArgs-1.2.tar.gz

---
+++ packages/ParseThoseArgs/ParseThoseArgs-1.2.tar.gz/ParseThoseArgs-1.2/parsethoseargs/python_wrapper.c
@@ -0,0 +1,40 @@
+#include "Python.h"
+#include "c_parser.c"
+// Does all needed includes.
+
+static char module_docstring[] = "This module is the C code for the argument parser.";
+static char _c_parser_docstring[] = "This is the actual C argument parser.";
+// Adds the docstrings.
+
+static PyObject *func_c_parser(PyObject *self, PyObject *args) {
+    ArgParserStructure structure;
+    if (!PyArg_ParseTuple(args, "siis", &structure.ParsingString, &structure.IgnoreQuotes, &structure.HitAllArgs, &structure.LastParse)) {
+        return NULL;
+    }
+    ArgParserStructure NewParse = ArgParser_Next(structure);
+    PyObject* obj = Py_BuildValue("siis", NewParse.ParsingString, NewParse.IgnoreQuotes, NewParse.HitAllArgs, NewParse.LastParse);
+    return obj;
+}
+// The main function.
+
+static PyMethodDef module_methods[] = {
+    {"ArgParser_Next", func_c_parser, METH_VARARGS, _c_parser_docstring},
+    {NULL}
+};
+// Defines the module methods.
+
+static struct PyModuleDef _coremodule = {
+    PyModuleDef_HEAD_INIT,
+    "_parsethoseargs_c_parser",   /* name of module */
+    module_docstring, /* module documentation, may be NULL */
+    -1,       /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+    module_methods,
+};
+// Defines the module.
+
+PyMODINIT_FUNC PyInit__parsethoseargs_c_parser(void)
+{
+    return PyModule_Create(&_coremodule);
+}
+// Initialises the module.
