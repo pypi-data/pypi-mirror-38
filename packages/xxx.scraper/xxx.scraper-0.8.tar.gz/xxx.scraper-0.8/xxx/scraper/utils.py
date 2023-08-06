96e4ed1ae950791b72a05cdbf80aaccfba07a01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:33 +0000

    Add package xxx.scraper/0.8/xxx.scraper-0.8.tar.gz

---
+++ packages/xxx.scraper/xxx.scraper-0.8.tar.gz/xxx.scraper-0.8/xxx/scraper/utils.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger('Bridge')
+
+cities_dict = {
+    'lvov': 'Lviv'
+}
+
+timeout = 10
+agents = [
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.14.1) Presto/2.12.388 Version/12.16'
+]
