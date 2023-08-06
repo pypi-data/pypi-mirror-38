3d5f96402ba74c4dd39125cd0c8f275d9ea1ce87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:51 +0000

    Add package add-header-mv/0.2/add-header-mv-0.2.tar.gz

---
+++ packages/add-header-mv/add-header-mv-0.2.tar.gz/add-header-mv-0.2/README.md
@@ -0,0 +1,19 @@
+# Hammal
+
+Install:
+
+```bash
+$ pip install add-header-mv
+```
+
+Usage:
+
+```bash
+$ add-header-mv <arg1> <arg2> <arg3>
+```
+
+`arg1` is file postfix, maybe `.py` or `.java`.
+
+`arg2` is what you want to add to head of file, maybe `import os` or `package what.you.want;`.
+
+`arg3` is target dir name, maybe `../otherDir` or `/User/xxx/doc`.
---
+++ packages/add-header-mv/add-header-mv-0.2.tar.gz/add-header-mv-0.2/hammal/hammal.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+
+def main():
+    if len(sys.argv) < 4:
+        print('''
+    add-header-mv <arg1> <arg2> <arg3>
+    - arg1 : file name postfix, use it to get files, such as '.py' or '.java'
+    - arg2 : header you want to add, such as 'import os' or 'package what.you.want;'
+    - arg3 : target dir you want to move files to
+        '''.strip())
+        return None
+    file_postfix = sys.argv[1]
+    added_header = sys.argv[2]
+    target_path = sys.argv[3]
+    all_dirs_and_files = os.listdir()
+    java_files = tuple(
+        filter(lambda x: x.endswith(file_postfix), all_dirs_and_files))
+    for java_file in java_files:
+        p = open(java_file, 'rt')
+        privious = p.read()
+        p.close()
+        f = open(java_file, 'wt')
+        f.write(added_header + '\n')
+        f.write(privious)
+        f.close()
+        p_abspath = os.path.abspath(java_file)
+        os.rename(p_abspath, target_path + '/' + java_file)
