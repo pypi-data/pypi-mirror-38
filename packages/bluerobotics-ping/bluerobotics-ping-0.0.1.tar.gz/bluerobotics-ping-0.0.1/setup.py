2a02e4428525b67ad34daeff09bc5d0e6ecac194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:13 +0000

    Add package bluerobotics-ping/0.0.1/bluerobotics-ping-0.0.1.tar.gz

---
+++ packages/bluerobotics-ping/bluerobotics-ping-0.0.1.tar.gz/bluerobotics-ping-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='bluerobotics-ping',
+      version='0.0.1',
+      description='A python module for the Blue Robotics Ping1D echosounder',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Blue Robotics',
+      author_email='support@bluerobotics.com',
+      url='https://www.bluerobotics.com',
+      packages=find_packages(), install_requires=['pyserial'],
+      classifiers=[
+          "Programming Language :: Python",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ]
+      )
