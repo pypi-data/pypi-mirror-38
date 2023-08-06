03a639e57967d02c281ad81ca28e021f6e2bcde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:07 +0000

    Add package Darwinex-ticks/0.0.8/Darwinex-ticks-0.0.8.tar.gz

---
+++ packages/Darwinex-ticks/Darwinex-ticks-0.0.8.tar.gz/Darwinex-ticks-0.0.8/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+DESCRIPTION = 'Darwinex tick data downloader Python API'
+# ere = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with open('README.md') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+setup(
+    name='Darwinex-ticks',
+    version='0.0.8',
+    packages=['darwinex_ticks'],
+    url='https://github.com/paduel/darwinex_ticks',
+    license='MIT',
+    author='Paduel',
+    author_email='paduel@gmail.com',
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
