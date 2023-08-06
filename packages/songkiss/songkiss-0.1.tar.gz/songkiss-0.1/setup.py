0a5160f1a43ffadb27f4b1394c120900f18bb376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:03 +0000

    Add package songkiss/0.1/songkiss-0.1.tar.gz

---
+++ packages/songkiss/songkiss-0.1.tar.gz/songkiss-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Try to convert markdown README to rst format for PyPI.
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='songkiss',
+    version='0.1',
+    description='SongKick API for GigBot usage',
+    long_description=long_description,
+    author='Stoplight',
+    author_email='support@apimatic.io',
+    url='https://stoplight.io',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.9.1, <3.0',
+        'jsonpickle>=0.7.1, <1.0',
+        'cachecontrol>=0.11.7, <1.0',
+        'python-dateutil>=2.5.3, <3.0'
+    ]
+)
---
+++ packages/songkiss/songkiss-0.1.tar.gz/songkiss-0.1/songkiss/controllers/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    'base_controller',
+    'search_controller',
+    'artists_controller',
+]
