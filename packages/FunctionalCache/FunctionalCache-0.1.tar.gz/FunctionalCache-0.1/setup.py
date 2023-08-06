6805879478fb0a33ea9687a4ea46b7000426f67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:17 +0000

    Add package FunctionalCache/0.1/FunctionalCache-0.1.tar.gz

---
+++ packages/FunctionalCache/FunctionalCache-0.1.tar.gz/FunctionalCache-0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from distutils.core import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='FunctionalCache',
+    description='SQLite function cache',
+    version='0.1',
+    packages=['functional_cache', ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/thomas9911/FunctionalCache',
+    author='thomas9911',
+    author_email='thomastimmer11@hotmail.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities"
+    ]
+)
