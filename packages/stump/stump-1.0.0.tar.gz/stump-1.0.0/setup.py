12c5966e386b5353078e2c0fe9865e2f79ab2cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package stump/1.0.0/stump-1.0.0.tar.gz

---
+++ packages/stump/stump-1.0.0.tar.gz/stump-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='stump',
+      version='1.0.0',
+      description='Logs program flow with decorators',
+      long_description=readme(),
+      url='http://github.com/EricCrosson/stump',
+      setup_requires=['pytest-runner',],
+      tests_require=['pytest'],
+      author='Eric Crosson',
+      author_email='eric.s.crosson@utexas.edu',
+      license='MIT',
+      packages=['stump'],
+      include_package_data=True,
+      zip_safe=False)
