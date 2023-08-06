a804f9a93f363fe33ccb1bbd671661b98ea90ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:44 +0000

    Add package elit/0.1.26.dev1543122014/elit-0.1.26.dev1543122014.tar.gz

---
+++ packages/elit/elit-0.1.26.dev1543122014.tar.gz/elit-0.1.26.dev1543122014/elit/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.1.26'
+version = '0.1.26'
+full_version = '0.1.26.dev1543122014'
+dev_version = '1543122014'
+release = False
+if not release:
+    version = full_version
