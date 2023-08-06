df8474f32db8f780301e8090c0a086356680bbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:09 +0000

    Add package mlpc/0.0.11/mlpc-0.0.11.tar.gz

---
+++ packages/mlpc/mlpc-0.0.11.tar.gz/mlpc-0.0.11/mlpc/entity/data.py
@@ -0,0 +1,14 @@
+from mlpc.entity.base_entity import BaseEntity
+from mlpc.utils.log import debug
+
+
+class Data(BaseEntity):
+    def __init__(self, storage):
+        super().__init__(storage)
+
+    def get_folderpath_for_writing(self, dataset_name):
+        return self._storage.get_folderpath_for_writing("data", dataset_name)
+
+    def save(self, dataset_name, dataset):
+        self._storage.save_resource("data", dataset_name, dataset)
+
