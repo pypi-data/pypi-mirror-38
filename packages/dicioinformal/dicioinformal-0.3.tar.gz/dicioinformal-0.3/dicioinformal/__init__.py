abb72be8e18c03ffc3eb60680eacb0653329ad1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:47 +0000

    Add package dicioinformal/0.3/dicioinformal-0.3.tar.gz

---
+++ packages/dicioinformal/dicioinformal-0.3.tar.gz/dicioinformal-0.3/dicioinformal/__init__.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def definicao(query):
+	r = requests.get('https://www.dicionarioinformal.com.br/'+query)
+	soup = BeautifulSoup(r.text, "html.parser")
+	nome = query
+	tit = soup.find_all('h3','di-blue')
+	if tit == None:
+		tit = soup.find_all('h3','di-blue-link')
+	title = []
+	for i in tit:
+		a = i.find('a')
+		if a != None:
+			title.append(a.get('title'))
+	if a == None:
+		tit = soup.find_all('h3','di-blue-link')
+	for i in tit:
+		a = i.find('a')
+		if a != None:
+			title.append('você quiz dizer: {}'.format(a.get('title')))
+	ti = soup.find_all('p','text-justify')
+	tit = []
+	for i in ti:
+		ti = i.get_text()[17:].replace('''\n                ''','')
+		tit.append(ti)
+	des = soup.find_all('blockquote','text-justify')
+	desc = []
+	for i in des:
+		try:
+			des = i.get_text().replace('\n'[0],'').replace('                 ','').replace('''\n                ''','')
+		except:
+			des = ''
+		desc.append(des)
+	result = []
+	max = 0
+	for i in title:
+		try:
+			b = {'title':i,'tit':tit[max],'desc':desc[max]}
+			max += 1
+			result.append(b)
+		except:
+			pass
+	return dict(results=result)
