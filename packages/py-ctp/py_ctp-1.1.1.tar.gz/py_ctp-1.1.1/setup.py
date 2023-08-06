70860f640c180704e538e9f61d48dd06be11fd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:30 +0000

    Add package py-ctp/1.1.1/py_ctp-1.1.1.tar.gz

---
+++ packages/py-ctp/py_ctp-1.1.1.tar.gz/py_ctp-1.1.1/setup.py
@@ -0,0 +1,67 @@
+from os import path as os_path
+from setuptools import setup, find_packages, Distribution
+import requests
+
+this_directory = os_path.abspath(os_path.dirname(__file__))
+
+# 读取文件内容
+
+
+def read_file(filename):
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+# 将markdown格式转换为rst格式
+def md_to_rst(from_file, to_file):
+    r = requests.post(url='http://c.docverter.com/convert',
+                      data={'to': 'rst', 'from': 'markdown'},
+                      files={'input_files[]': open(from_file, 'rb')})
+    if r.ok:
+        with open(to_file, "wb") as f:
+            f.write(r.content)
+
+
+# 获取依赖
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines()
+            if not line.startswith('#')]
+
+
+long_description = read_file('README.md')
+long_description_content_type = "text/markdown",  # 指定包文档格式为markdown
+md_to_rst("README.md", "README.rst")  # md 格式显示不正确
+if os_path.exists('README.rst'):
+    long_description = open('README.rst', encoding="utf-8").read()
+    long_description_content_type = "text/x-rst",
+
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(foo):
+        return True
+
+
+setup(
+    name='py_ctp',  # 包名
+    python_requires='>=3.4.0',  # python环境
+    version='1.1.1',  # 包的版本
+    description="python package for CTP",  # 包简介，显示在PyPI上
+    long_description=long_description,  # 读取的Readme文档内容
+    long_description_content_type=long_description_content_type,  # 指定包文档格式为markdown
+    author="HaiFeng",  # 作者相关信息
+    author_email='haifengat@vip.qq.com',
+    url='https://gitee.com/haifengat/hf_ctp_py_proxy',
+    # 指定包信息，还可以用find_packages()函数
+    # packages=find_packages(),
+    packages=['py_ctp'],
+    install_requires=read_requirements('requirements.txt'),  # 指定需要安装的依赖
+    include_package_data=True,
+    license="LICENSE",
+    keywords=['haifeng', 'CTP', 'future'],
+    package_data={  # Optional
+        'bin': ['*.dll', '*.so'],
+    },
+    distclass=BinaryDistribution
+)
