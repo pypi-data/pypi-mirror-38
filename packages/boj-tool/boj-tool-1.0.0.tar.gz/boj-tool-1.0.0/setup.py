5e55fd77bc5dceb4b0d2ff31d485bb19a3161188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:03 +0000

    Add package boj-tool/1.0.0/boj-tool-1.0.0.tar.gz

---
+++ packages/boj-tool/boj-tool-1.0.0.tar.gz/boj-tool-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+from setuptools import setup
+from os import path
+
+py_modules = [
+    'requests',
+    'bs4',
+    'xdg'
+]
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='boj-tool',
+      version='1.0.0',
+      description='A tool for submitting to BOJ',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Ryang Sohn',
+      author_email='loop.infinitely@gmail.com',
+      install_requires=py_modules,
+      entry_points={
+          'console_scripts': [
+              'boj=boj:main'
+          ]
+      },
+      packages=setuptools.find_packages(),
+      project_urls={
+          'Bug Reports': 'https://github.com/sohnryang/boj-tool/issues',
+          'Source': 'https://github.com/sohnryang/boj-tool'
+      })
