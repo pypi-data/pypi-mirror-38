c1f42e8236ea1636fa029ec54e27470e2755b38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:57 +0000

    Add package slither-analyzer/0.3.0/slither-analyzer-0.3.0.tar.gz

---
+++ packages/slither-analyzer/slither-analyzer-0.3.0.tar.gz/slither-analyzer-0.3.0/slither/detectors/examples/backdoor.py
@@ -0,0 +1,30 @@
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+
+
+class Backdoor(AbstractDetector):
+    """
+    Detect function named backdoor
+    """
+
+    ARGUMENT = 'backdoor'  # slither will launch the detector with slither.py --mydetector
+    HELP = 'Function named backdoor (detector example)'
+    IMPACT = DetectorClassification.HIGH
+    CONFIDENCE = DetectorClassification.HIGH
+
+    def detect(self):
+        ret = []
+
+        for contract in self.slither.contracts_derived:
+            # Check if a function has 'backdoor' in its name
+            for f in contract.functions:
+                if 'backdoor' in f.name:
+                    # Info to be printed
+                    info = 'Backdoor function found in {}.{} ({})\n'
+                    info = info.format(contract.name, f.name, f.source_mapping_str)
+                    # Print the info
+                    self.log(info)
+                    # Add the result in ret
+                    source = f.source_mapping
+                    ret.append({'vuln': 'backdoor', 'contract': contract.name, 'sourceMapping' : source})
+
+        return ret
