3093853b7893e497adf6315051c4fd0b00046d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:34 +0000

    Add package bojs/0.0.0/bojs-0.0.0.tar.gz

---
+++ packages/bojs/bojs-0.0.0.tar.gz/bojs-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bojs',
+    version='0.0.0',
+    description='bojs: A Sandbox for JavaScript in Python',
+    url='http://github.com/neogeny/bojs',
+    author='Apalala',
+    author_email='apalala@gmail.com',
+    license='MIT',
+    packages=['bojs'],
+    zip_safe=True,
+)
