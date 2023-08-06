bbb565085a2444f9fe6cee864693145d0635397b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:08 +0000

    Add package jfscripts/0.3.0/jfscripts-0.3.0.tar.gz

---
+++ packages/jfscripts/jfscripts-0.3.0.tar.gz/jfscripts-0.3.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+import os
+import versioneer
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='jfscripts',
+    author='Josef Friedrich',
+    author_email='josef@friedrich.rocks',
+    description=('A collection of my Python scripts. Maybe they are useful for someone else.'),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='MIT',
+    project_urls={
+        'Documentation': 'http://jfscripts.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/Josef-Friedrich/python-scripts',
+        'Tracker': 'https://github.com/Josef-Friedrich/python-scripts/issues',
+    },
+    packages=['jfscripts'],
+    url='https://github.com/Josef-Friedrich/python-scripts',
+    install_requires=[
+        'PyPDF2',
+        'sphinx-argparse',
+        'termcolor', 
+    ],
+    entry_points = {
+        'console_scripts': [
+            'dns-ipv6-prefix.py = jfscripts.dns_ipv6_prefix:main',
+            'extract-pdftext.py = jfscripts.extract_pdftext:main',
+            'find-dupes-by-size.py = jfscripts.find_dupes_by_size:main',
+            'list-files.py = jfscripts.list_files:main',
+            'mac-to-eui64.py = jfscripts.mac_to_eui64:main',
+            'magick-imslp.py = jfscripts.magick_imslp:main',
+            'image-into-pdf.py = jfscripts.image_into_pdf:main',
+        ],
+    },
+    python_requires='>=3.6',
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
