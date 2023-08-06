5dfd8a9a65c641229f6d3dfda111077ec7bc2bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:51 +0000

    Add package hsgconv/1.0.9/hsgconv-1.0.9.tar.gz

---
+++ packages/hsgconv/hsgconv-1.0.9.tar.gz/hsgconv-1.0.9/hsgconv/__init__.py
@@ -0,0 +1,4 @@
+__title__ = 'hsgconv'
+__version__ = '1.0.9'
+
+from .hsgconv import GridParams,CreateParamList,ConvertToOSBG,ConvertToLocalGrid
---
+++ packages/hsgconv/hsgconv-1.0.9.tar.gz/hsgconv-1.0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(name='hsgconv',
+	packages=find_packages(exclude=('docs', 'tests', 'env', 'index.py')),
+    version='1.0.9',
+    description='Pure Python converter for Highways England Local Grid to British National Grid - OSBG36, EPSG:27700',
+    long_description=open('README.md').read(),
+    author='Claudio Campanile',
+    author_email='ccampanile@brydenwood.co.uk',
+    download_url='https://github.com/ccampanile/hsgconv/',
+	url='https://github.com/ccampanile/hsgconv/',
+    py_modules=['hsgconv'],
+	include_package_data=True,
+    license='MIT',
+    zip_safe=False,
+    keywords=['geographic', 'coordinates', 'highwaysengland', 'localgrid', 'osbg36'],
+    classifiers=[])
