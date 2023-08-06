871499d7859e131cf240063f367f406c1a95920f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:26 +0000

    Add package read-video-py/1.0.0.dev1/read-video-py-1.0.0.dev1.tar.gz

---
+++ packages/read-video-py/read-video-py-1.0.0.dev1.tar.gz/read-video-py-1.0.0.dev1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='read-video-py',
+    version='1.0.0.dev1',
+    description='A test for unpacking python project',
+    url='',
+    author='',
+    author_email='',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='read video python',
+#    packages=['video'],
+)
