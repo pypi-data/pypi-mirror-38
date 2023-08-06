0ba58e55e425613d0a05f500381a7538687f4260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:47 +0000

    Add package trytoncalidae-authentication-dummy/4.6.0/trytoncalidae_authentication_dummy-4.6.0-py3-none-any.whl

---
+++ packages/trytoncalidae-authentication-dummy/trytoncalidae_authentication_dummy-4.6.0-py3-none-any.whl/trytond/modules/authentication_dummy/user.py
@@ -0,0 +1,16 @@
+from future.utils import with_metaclass
+
+from trytond.pool import PoolMeta
+
+__all__ = ['User']
+
+
+class User(with_metaclass(PoolMeta)):
+    __name__ = 'res.user'
+
+    @classmethod
+    def _login_dummy(cls, login, parameters):
+        user_id, _ = cls._get_login(login)
+        if user_id:
+            return user_id
+        return
