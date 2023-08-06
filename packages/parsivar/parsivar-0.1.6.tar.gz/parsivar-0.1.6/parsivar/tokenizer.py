878d895f1f0bf488871bb23d6cd1e60fc78e551e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:50 +0000

    Add package parsivar/0.1.6/parsivar-0.1.6.tar.gz

---
+++ packages/parsivar/parsivar-0.1.6.tar.gz/parsivar-0.1.6/parsivar/tokenizer.py
@@ -0,0 +1,18 @@
+import re
+
+
+class Tokenizer():
+    def __init__(self):
+        pass
+
+    def tokenize_words(self, doc_string):
+        token_list = doc_string.strip().split()
+        token_list = [x.strip("\u200c") for x in token_list if len(x.strip("\u200c")) != 0]
+        return token_list
+
+    def tokenize_sentences(self, doc_string):
+        pattern = r'([!\.\?؟:]+)[ \n]+'
+        rep = r'\1\t\t'
+        doc_string = re.sub(pattern, rep, doc_string)
+        doc_string = doc_string.split('\t\t')
+        return doc_string
