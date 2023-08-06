43dae7f98c0f0f21f57e36ddf8e342fa07b6475e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:16 +0000

    Add package ElectronBonder/0.5/ElectronBonder-0.5.tar.gz

---
+++ packages/ElectronBonder/ElectronBonder-0.5.tar.gz/ElectronBonder-0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ElectronBonder",
+    url="https://github.com/RockefellerArchiveCenter/ElectronBonder",
+    description="Project Electron Client Library",
+    long_description="""A client library for interacting Project Electron applications via their REST APIs.""",
+    author="Rockefeller Archive Center",
+    author_email="archive@rockarch.org",
+    version="0.5",
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires=">=2.7",
+    install_requires=[
+        "requests",
+        "six",
+    ],
+)
