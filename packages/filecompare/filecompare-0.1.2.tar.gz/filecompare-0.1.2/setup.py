23496154cef4f606c4b17342fa86bc2325fe8752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:33 +0000

    Add package filecompare/0.1.2/filecompare-0.1.2.tar.gz

---
+++ packages/filecompare/filecompare-0.1.2.tar.gz/filecompare-0.1.2/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name = 'filecompare',
+    version = '0.1.2',
+    author="rascoro1",
+    author_email="andcope1995@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rascoro1/filecompare",
+    packages = ['filecompare'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'filecompare = filecompare.__main__:main'
+        ]
+    })
