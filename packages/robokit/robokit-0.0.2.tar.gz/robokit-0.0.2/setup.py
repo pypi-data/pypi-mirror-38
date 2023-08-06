5a62b56391b3a353d0fb094747fb46d0fda7936f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:13 +0000

    Add package robokit/0.0.2/robokit-0.0.2.tar.gz

---
+++ packages/robokit/robokit-0.0.2.tar.gz/robokit-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='robokit',
+    version='0.0.2',
+    description='Minimal implementation of common robotics maths',
+    url='https://github.com/Thomascountz/robokit',
+    author='Thomas Countz',
+    author_email='thomascountz@gmail.com',
+    license='MIT',
+    packages=['robokit'],
+    zip_safe=False)
