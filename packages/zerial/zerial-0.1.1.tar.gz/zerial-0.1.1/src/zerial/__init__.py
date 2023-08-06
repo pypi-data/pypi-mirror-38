b2c04a00b05bf96f9f9dc3358cf5cb1efe283b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:26 +0000

    Add package zerial/0.1.1/zerial-0.1.1.tar.gz

---
+++ packages/zerial/zerial-0.1.1.tar.gz/zerial-0.1.1/src/zerial/__init__.py
@@ -0,0 +1,20 @@
+from ._data import zdata, Zequence, Zariant
+from ._core import Ztructurer
+
+__author__ = 'Josh Reed'
+__email__ = 'jriddy@gmail.com'
+__version__ = '0.1.1'
+
+
+_the_ztructurer = Ztructurer()
+destructure = _the_ztructurer.destructure
+restructure = _the_ztructurer.restructure
+
+
+__all__ = [
+    'destructure',
+    'restructure',
+    'zdata',
+    'Zequence',
+    'Zariant',
+]
