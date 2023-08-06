082ba4b039cc9282c55524fdfd49b51d1fc8da2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:57 +0000

    Add package pyfy/1.1.3/pyfy-1.1.3.tar.gz

---
+++ packages/pyfy/pyfy-1.1.3.tar.gz/pyfy-1.1.3/tests/run_tests.py
@@ -0,0 +1,39 @@
+import os
+import pytest
+
+def run():
+    client_id = os.getenv('SPOTIFY_CLIENT_ID')
+    client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+    access_token = os.getenv('SPOTIFY_ACCESS_TOKEN')
+    redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI')
+    test_integration_sync = os.getenv('PYFY_TEST_INTEGRATION_SYNC')
+    test_integration_async = os.getenv('PYFY_TEST_INTEGRATION_ASYNC')
+
+    if client_id and client_secret and access_token and redirect_uri and test_integration_sync == 'true' and test_integration_async =='true':  # Run unit tests then integration tests
+        print('Running unit tests followed by integration tests')
+        pytest.main(
+            [
+                '-v',
+                '-s',
+                #'--maxfail=2',
+                #'--fulltrace',
+                '--cov',
+                'pyfy/',
+                'tests/test_units/',
+                'tests/test_integration/test_async/',
+                'tests/test_integration/test_sync/',
+            ]
+        )
+    elif client_id and client_secret and access_token and redirect_uri and test_integration_sync == 'true' and test_integration_async !='true':  # Run unit tests then integration tests
+        print('Running unit tests followed by synchronous integration tests')
+        pytest.main(['-v', '-s', '--cov', 'pyfy/', 'tests/test_units/', 'tests/test_integration/test_sync/'])
+    elif client_id and client_secret and access_token and redirect_uri and test_integration_sync != 'true' and test_integration_async =='true':  # Run unit tests then integration tests
+        print('Running unit tests followed by asynchronous integration tests')
+        pytest.main(['-v', '-s', '--cov', 'pyfy/', 'tests/test_units/', 'tests/test_integration/test_async/'])
+    else:
+        print('Running unit tests')
+        pytest.main(['-v', '-s', '--cov', 'pyfy/', 'tests/test_units/'])
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/pyfy/pyfy-1.1.3.tar.gz/pyfy-1.1.3/tests/test_units/test_auth.py
@@ -0,0 +1,2 @@
+def test_unit_tests_discovered():
+    assert True
