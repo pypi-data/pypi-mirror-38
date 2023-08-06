b067566d50ad14171b86259631195765261143e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:14 +0000

    Add package docs/0.1.0/docs-0.1.0.tar.gz

---
+++ packages/docs/docs-0.1.0.tar.gz/docs-0.1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='docs',
+    version='0.1.0',
+    description='Turn your Python script into a report with inline figures.',
+    url='http://github.com/danijar/docs',
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
