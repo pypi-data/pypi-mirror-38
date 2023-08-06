a7f296af3d06341b5f1bb8cf420b212ea0d2eed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package lunasync/0.1.4/lunasync-0.1.4.tar.gz

---
+++ packages/lunasync/lunasync-0.1.4.tar.gz/lunasync-0.1.4/lunasync/__about__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 miruka
+# This file is part of lunasync, licensed under LGPLv3.
+
+"Keep downloaded booru searches up-to-date, similar to Danbooru subscriptions."
+
+__project_name__ = "lunakit"
+__pkg_name__     = "lunasync"
+__version__      = "0.1.4"
+__status__       = "Development"
+# __status__     = "Production"
+
+__author__ = "miruka"
+__email__  = "miruka@disroot.org"
+
+__license__ = "LGPLv3"
