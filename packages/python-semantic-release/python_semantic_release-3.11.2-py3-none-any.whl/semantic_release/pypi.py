f88c2017058340205af9b335139494eb3a3a58cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:02 +0000

    Add package python-semantic-release/3.11.2/python_semantic_release-3.11.2-py3-none-any.whl

---
+++ packages/python-semantic-release/python_semantic_release-3.11.2-py3-none-any.whl/semantic_release/pypi.py
@@ -0,0 +1,27 @@
+"""PyPI
+"""
+from invoke import run
+
+
+def upload_to_pypi(
+        dists: str = 'sdist bdist_wheel',
+        username: str = None,
+        password: str = None,
+        skip_existing: bool = False
+):
+    """Creates the wheel and uploads to pypi with twine.
+
+    :param dists: The dists string passed to setup.py. Default: 'bdist_wheel'
+    :param username: PyPI account username string
+    :param password: PyPI account password string
+    """
+    run('python setup.py {}'.format(dists))
+    run(
+        'twine upload -u {} -p {} {} {}'.format(
+            username,
+            password,
+            '--skip-existing' if skip_existing else '',
+            'dist/*'
+        )
+    )
+    run('rm -rf build dist')
