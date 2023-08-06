a2ab1b0bb5049a7fc8ef338ca5dfa60721a507d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:57 +0000

    Add package packy-agent/2.0.1/packy-agent-2.0.1.tar.gz

---
+++ packages/packy-agent/packy-agent-2.0.1.tar.gz/packy-agent-2.0.1/packy_agent/exceptions.py
@@ -0,0 +1,22 @@
+class Skip(Exception):
+    pass
+
+
+class ImproperlyConfiguredError(Exception):
+    pass
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class ValidationError(Exception):
+    pass
+
+
+class CoolDown(Exception):
+    pass
+
+
+class NoAccessTokenError(ImproperlyConfiguredError):
+    pass
