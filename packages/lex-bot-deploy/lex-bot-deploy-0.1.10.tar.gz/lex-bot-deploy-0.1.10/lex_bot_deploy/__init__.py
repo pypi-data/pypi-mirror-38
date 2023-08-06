c3019cdf6ab3224d0a2cf9ff4db8c1242659001d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:55 +0000

    Add package lex-bot-deploy/0.1.10/lex-bot-deploy-0.1.10.tar.gz

---
+++ packages/lex-bot-deploy/lex-bot-deploy-0.1.10.tar.gz/lex-bot-deploy-0.1.10/lex_bot_deploy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for lex-bot-deploy."""
+
+__author__ = """Martin Schade"""
+__email__ = 'git.schade@gmail.com'
+__version__ = '0.1.8'
---
+++ packages/lex-bot-deploy/lex-bot-deploy-0.1.10.tar.gz/lex-bot-deploy-0.1.10/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The setup script."""
+
+import sys
+import os
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+requirements = ['boto3', 'botocore', 'tenacity']
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="Martin Schade",
+    author_email='git.schade@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Lex Bot deployment script and function",
+    install_requires=requirements,
+    scripts=['bin/lex-bot-deploy', 'bin/lex-bot-get-schema'],
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='lex-bot-deploy Amazon Lex Deployment automation Schema',
+    name='lex-bot-deploy',
+    packages=find_packages(include=['lex_bot_deploy']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Schadix/lex_bot_deploy',
+    version='0.1.10',
+    zip_safe=False,
+)
