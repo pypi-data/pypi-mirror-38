8ebec870f8c4bf0d2a624e7299b499cc00aa6823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:43 +0000

    Add package SimfPythonGUI/0.0.5a4/SimfPythonGUI-0.0.5a4-py3-none-any.whl

---
+++ packages/SimfPythonGUI/SimfPythonGUI-0.0.5a4-py3-none-any.whl/SimfPythonGUI/about.py
@@ -0,0 +1,15 @@
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog
+import os
+
+
+class AboutDialog(QDialog):
+    def __init__(self):
+        super().__init__(flags=Qt.WindowStaysOnTopHint)
+        version_file = open(os.path.join('.', 'VERSION'))
+        version = version_file.read().strip()
+        ui_file = os.path.join('.', 'AboutDialog.ui')
+        uic.loadUi(ui_file, self)
+        self.show()
+        self.versionLabel.setText("Version: " + version)
