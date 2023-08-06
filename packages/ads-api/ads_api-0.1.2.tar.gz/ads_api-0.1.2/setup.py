c823b5295732de4eb2f0c6cd0962b6c42f4d5ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:40 +0000

    Add package ads-api/0.1.2/ads_api-0.1.2.tar.gz

---
+++ packages/ads-api/ads_api-0.1.2.tar.gz/ads_api-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ads_api",
+    version="0.1.2",
+    description="major marketing platform python api",
+    packages=find_packages()
+)
