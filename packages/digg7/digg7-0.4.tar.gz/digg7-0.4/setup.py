da6fff81a15578f1660615ab14ebb8acdc22899d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:52 +0000

    Add package digg7/0.4/digg7-0.4.tar.gz

---
+++ packages/digg7/digg7-0.4.tar.gz/digg7-0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+  name = 'digg7',         # How you named your package folder (MyLib)
+  packages = ['digg7'],   # Chose the same as "name"
+  version = '0.4',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Digital simulation library!',   # Give a short description about your library
+  author = 'Kaif Khan,Pulak Sahoo,Tushar,Gaurav Singh',                   # Type in your name
+  author_email = 'kaifkhan.khan0@gmail.com',      # Type in your E-Mail
+  url = 'https://khanstark.github.io/DigiSim/',   # Provide either the link to your github or to your website
+    # I explain this later on
+  keywords = ['digital','electronics','logic gates','flip flops'],   # Keywords that define your package best
+  install_requires=[],
+  long_desription='This is a minimalistic library for scalable simulation of complex combinational digital logic in python, using which you can simulate various complex operations in digital logic.',
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+         #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 2'
+  ],
+)
