39c0e8a496206c5eb076a728f8a1a99b3b446a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:43 +0000

    Add package import-expression/0.3.1/import_expression-0.3.1-py3-none-any.whl

---
+++ packages/import-expression/import_expression-0.3.1-py3-none-any.whl/import_expression/_syntax.py
@@ -0,0 +1,103 @@
+import collections
+import io
+from token import *
+import tokenize as tokenize_
+
+from .constants import *
+
+tokenize_.TokenInfo.value = property(lambda self: self.string)
+
+is_import = lambda token: token.type == tokenize_.ERRORTOKEN and token.string == IMPORT_OP
+
+NEWLINES = {NEWLINE, tokenize_.NL}
+
+def fix_syntax(s: str, filename=DEFAULT_FILENAME) -> bytes:
+	tokens = tokenize(s)  # TODO is there a better way than tokenizing and then untokenizing? don't think so?
+	untokenizer = Untokenizer()
+	try:
+		out = untokenizer.untokenize(tokens)
+	except tokenize_.TokenError as ex:
+		# TODO add lineno info from the rest of ex.args
+		message, (lineno, offset) = ex.args
+
+		try:
+			source_line = s.splitlines()[lineno-1]
+		except IndexError:
+			source_line = None
+
+		raise SyntaxError(message, (filename, lineno, offset, source_line)) from None
+
+	if untokenizer.encoding is not None:
+		out = out.encode(untokenizer.encoding)
+
+	return out
+
+# taken from Lib/tokenize_.py at 3.6
+# TODO find out if license is compat with Charity Public License
+class Untokenizer:
+	def __init__(self):
+		self.tokens = collections.deque()
+		self.indents = collections.deque()
+		self.prev_row = 1
+		self.prev_col = 0
+		self.startline = False
+		self.encoding = None
+
+	def add_whitespace(self, start):
+		row, col = start
+		if row < self.prev_row or row == self.prev_row and col < self.prev_col:
+			raise ValueError("start ({},{}) precedes previous end ({},{})"
+							 .format(row, col, self.prev_row, self.prev_col))
+
+		col_offset = col - self.prev_col
+		self.tokens.append(" " * col_offset)
+
+	def untokenize(self, iterable):
+		indents = []
+		startline = False
+		for token in iterable:
+			if token.type == tokenize_.ENCODING:
+				self.encoding = token.value
+				continue
+
+			if token.type == tokenize_.ENDMARKER:
+				break
+
+			# XXX this abomination comes from tokenize.py
+			# i tried to move it to a separate method but failed
+
+			if token.type == tokenize_.INDENT:
+				indents.append(token.value)
+				continue
+			elif token.type == tokenize_.DEDENT:
+				indents.pop()
+				self.prev_row, self.prev_col = token.end
+				continue
+			elif token.type in NEWLINES:
+				startline = True
+			elif startline and indents:
+				indent = indents[-1]
+				start_row, start_col = token.start
+				if start_col >= len(indent):
+					self.tokens.append(indent)
+					self.prev_col = len(indent)
+				startline = False
+
+			# end abomination
+
+			self.add_whitespace(token.start)
+
+			if is_import(token):
+				self.tokens.append(MARKER)
+			else:
+				self.tokens.append(token.value)
+
+			self.prev_row, self.prev_col = token.end
+			if token.type in NEWLINES:
+				self.prev_row += 1
+				self.prev_col = 0
+
+		return "".join(self.tokens)
+
+def tokenize(string):
+	return tokenize_.tokenize(io.BytesIO(string.encode('utf-8')).readline)
---
+++ packages/import-expression/import_expression-0.3.1-py3-none-any.whl/import_expression/patch.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import builtins
+import contextlib
+import inspect
+import os
+import sys
+
+from . import eval as ie_eval, exec as ie_exec
+
+def patch(globals=sys.modules['__main__'].__dict__):
+	"""monkey patch sys.excepthook so that import expressions work at the repl
+
+	If a line has a syntax error, import_expression.eval is attempted on it.
+	If this also results in a syntax error, import_expression.exec will be run instead.
+	Both cases will run in the context of the given globals dict, or if None, globals produced by statements will be saved to the __main__ module
+	"""
+	if not _is_tty():
+		raise RuntimeError(f'patch() only works at the REPL, where stdin is a TTY.')
+
+	sys.excepthook = _make_excepthook(globals)
+
+def _make_excepthook(globals):
+	def excepthook(_, error, __):
+		if (
+			type(error) is not SyntaxError
+			or error.lineno != 1  # we don't have all the code
+		):
+			return sys.__excepthook__(type(error), error, error.__traceback__)
+
+		try:
+			result = ie_eval(error.text, globals)
+		except SyntaxError:
+			try:
+				ie_exec(error.text, globals)
+			except BaseException as error:
+				return sys.__excepthook__(type(error), error, error.__traceback__)
+		except BaseException as error:
+			return sys.__excepthook__(type(error), error, error.__traceback__)
+		else:
+			if result is None:
+				return
+
+			builtins._ = result
+			print(repr(result))
+
+	return excepthook
+
+def _is_tty():
+	return os.isatty(sys.stdin.fileno())
