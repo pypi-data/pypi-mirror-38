b2848722ba2d248ec6b8a2ffec9f160c9409db4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:33 +0000

    Add package baybars/0.0.8/baybars-0.0.8-py3-none-any.whl

---
+++ packages/baybars/baybars-0.0.8-py3-none-any.whl/baybars/data_types.py
@@ -0,0 +1,36 @@
+from collections import Mapping
+from enum import Enum
+from types import MappingProxyType
+from typing import List
+
+
+FloatVector = List[float]
+IntegerVector = List[int]
+StringVector = List[str]
+Float2DVector = List[List[float]]
+
+
+class DistanceMetric(str, Enum):
+  CityBlock = 'cityblock'
+  Cosine = 'cosine'
+  Euclidean = 'euclidean'
+  L1 = 'l1'
+  L2 = 'l2'
+  Manhattan = 'manhattan'
+  BrayCurtis = 'braycurtis'
+  Canberra = 'canberra'
+  Chebyshev = 'chebyshev'
+  Correlation = 'correlation'
+  Dice = 'dice'
+  Hamming = 'hamming'
+  Jaccard = 'jaccard'
+  Kulsinki = 'kulsinki'
+  Mahalanobis = 'mahalanobis'
+  Minkowski = 'minkowski'
+  Rogerstanimoto = 'rogerstanimoto'
+  Russellrao = 'russellrao'
+  Seuclidean = 'seuclidean'
+  Sokalmichener = 'sokalmichener'
+  Sokalsneath = 'sokalsneath'
+  SqEuclidean = 'sqeuclidean'
+  Yule = 'yule'
