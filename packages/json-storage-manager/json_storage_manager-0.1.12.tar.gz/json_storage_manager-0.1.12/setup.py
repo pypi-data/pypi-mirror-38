e79b73b5ea62fc89bc814fc8fbe5e2f9ecd0d8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:38 +0000

    Add package json-storage-manager/0.1.12/json_storage_manager-0.1.12.tar.gz

---
+++ packages/json-storage-manager/json_storage_manager-0.1.12.tar.gz/json_storage_manager-0.1.12/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="json_storage_manager",
+    version="0.1.12",
+    author="Ahmed Hefnawi",
+    author_email="me@ahmedhefnawi.com",
+    description="JSON Storage Manager for JSON data stored in text-files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hefnawi/json-storage-manager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
