bc8854717519173a5f88361dde64136ec1121e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:48 +0000

    Add package tap-urban-airship/0.9.0/tap-urban-airship-0.9.0.tar.gz

---
+++ packages/tap-urban-airship/tap-urban-airship-0.9.0.tar.gz/tap-urban-airship-0.9.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-urban-airship',
+      version='0.9.0',
+      description='Singer.io tap for extracting data from the Urban Airship API',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_urban_airship'],
+      install_requires=[
+          'singer-python==1.2.0',
+          'requests==2.20.0',
+          'backoff==1.3.2'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-urban-airship=tap_urban_airship:main
+      ''',
+      packages=['tap_urban_airship'],
+      package_data = {
+          'tap_urban_airship/schemas': [
+            "channels.json",
+            "lists.json",
+            "named_users.json",
+            "segments.json",
+          ],
+      },
+      include_package_data=True,
+)
---
+++ packages/tap-urban-airship/tap-urban-airship-0.9.0.tar.gz/tap-urban-airship-0.9.0/tap_urban_airship/transform.py
@@ -0,0 +1,85 @@
+class InvalidData(Exception):
+    """Raise when data doesn't validate the schema"""
+
+
+def transform_row(row, schema):
+    return _transform_field(row, schema)
+
+
+def _transform_datetime(value):
+    return value + "Z"
+
+
+def _any_of(data, schema_list):
+    for schema in schema_list:
+        try:
+            return _transform_field(data, schema)
+        except Exception:
+            pass
+
+    raise InvalidData("{} doesn't match any of {}".format(data, schema_list))
+
+
+def _array(data, items_schema):
+    return [_transform_field(value, items_schema) for value in data]
+
+
+def _object(data, properties_schema):
+    return {field: _transform_field(data[field], field_schema)
+            for field, field_schema in properties_schema.items()
+            if field in data}
+
+
+def _type_transform(value, type_schema):
+    if isinstance(type_schema, list):
+        for typ in type_schema:
+            try:
+                return _type_transform(value, typ)
+            except Exception:
+                pass
+
+        raise InvalidData("{} doesn't match any of {}".format(value, type_schema))
+
+    if value is None:
+        if type_schema != "null":
+            raise InvalidData("Null is not allowed")
+        else:
+            return None
+
+    if type_schema == "string":
+        return str(value)
+
+    if type_schema == "integer":
+        return int(value)
+
+    if type_schema == "number":
+        return float(value)
+
+    if type_schema == "boolean":
+        return bool(value)
+
+    raise InvalidData("Unknown type {}".format(type_schema))
+
+
+def _format_transform(value, format_schema):
+    if format_schema == "date-time":
+        return _transform_datetime(value)
+
+    raise InvalidData("Unknown format {}".format(format_schema))
+
+
+def _transform_field(value, field_schema):
+    if "anyOf" in field_schema:
+        return _any_of(value, field_schema["anyOf"])
+
+    if field_schema["type"] == "array":
+        return _array(value, field_schema["items"])
+
+    if field_schema["type"] == "object":
+        return _object(value, field_schema["properties"])
+
+    value = _type_transform(value, field_schema["type"])
+    if "format" in field_schema:
+        value = _format_transform(value, field_schema["format"])
+
+    return value
