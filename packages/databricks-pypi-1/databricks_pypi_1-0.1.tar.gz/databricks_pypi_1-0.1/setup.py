e209eef832831b3200e5103a7e0dbe1f7f3bcf47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package databricks-pypi-1/0.1/databricks_pypi_1-0.1.tar.gz

---
+++ packages/databricks-pypi-1/databricks_pypi_1-0.1.tar.gz/databricks_pypi_1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name = "databricks_pypi_1",
+    version = "0.1",
+    decription = 'databricks whl library',
+    packages = find_packages())
