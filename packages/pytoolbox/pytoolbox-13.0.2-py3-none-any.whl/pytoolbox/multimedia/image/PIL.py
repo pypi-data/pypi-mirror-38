23785d6da83b431b9280b795fa155d5553f93af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:57 +0000

    Add package pytoolbox/13.0.2/pytoolbox-13.0.2-py3-none-any.whl

---
+++ packages/pytoolbox/pytoolbox-13.0.2-py3-none-any.whl/pytoolbox/multimedia/image/PIL.py
@@ -0,0 +1,79 @@
+# -*- encoding: utf-8 -*-
+
+
+
+import functools
+
+from pytoolbox import module
+
+_all = module.All(globals())
+
+from PIL import Image  # noqa
+
+TRANSPOSE_SEQUENCES = {
+    None: [],
+    1: [],
+    2: [Image.FLIP_LEFT_RIGHT],
+    3: [Image.ROTATE_180],
+    4: [Image.FLIP_TOP_BOTTOM],
+    5: [Image.FLIP_LEFT_RIGHT, Image.ROTATE_90],
+    6: [Image.ROTATE_270],
+    7: [Image.FLIP_TOP_BOTTOM, Image.ROTATE_90],
+    8: [Image.ROTATE_90]
+}
+
+
+def get_orientation(image, orientation_tag=0x0112, no_exif_default=None, no_key_default=None):
+    exif = getattr(image, '_getexif', lambda: None)()
+    try:
+        return no_exif_default if exif is None else exif[orientation_tag]
+    except KeyError:
+        return no_key_default
+
+
+def apply_orientation(image, get_orientation=get_orientation, sequences=TRANSPOSE_SEQUENCES):
+    """Credits: https://stackoverflow.com/questions/4228530/pil-thumbnail-is-rotating-my-image."""
+    orientation = get_orientation(image)
+    return functools.reduce(lambda i, op: i.transpose(op), sequences.get(orientation, []), image)
+
+
+def open(file_or_path):
+    image = Image.open(file_or_path)
+    try:
+        image.load()
+    except IOError as e:
+        if 'truncated' not in str(e):
+            raise
+    return image
+
+
+def remove_metadata(image, keys=('exif', ), inplace=False):
+    image = image if inplace else image.copy()
+    for key in keys:
+        if key in image.info:
+            image.info[key] = b''
+    return image
+
+
+def remove_transparency(image, background=(255, 255, 255), force_rgb=False):
+    """
+    Return a RGB image with an alpha mask applied to picture + background.
+    If alpha not found, then convert to RGB if forced else do nothing.
+    """
+    try:
+        alpha = image.getchannel('A')
+    except ValueError as e:
+        if 'has no channel' in str(e):
+            return image.convert('RGB') if force_rgb else image
+        raise
+    new_image = Image.new('RGB', image.size, background)
+    new_image.paste(image, mask=alpha)
+    return new_image
+
+
+def save(image, *args, **kwargs):
+    kwargs.setdefault('exif', image.info.get('exif', b''))
+    return image.save(*args, **kwargs)
+
+
+__all__ = _all.diff(globals())
