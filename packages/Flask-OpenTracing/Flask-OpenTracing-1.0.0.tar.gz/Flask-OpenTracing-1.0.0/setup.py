a0e1db6f068d33ac7739e15e5819fc8b8c52bfdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:26 +0000

    Add package Flask-OpenTracing/1.0.0/Flask-OpenTracing-1.0.0.tar.gz

---
+++ packages/Flask-OpenTracing/Flask-OpenTracing-1.0.0.tar.gz/Flask-OpenTracing-1.0.0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+'''
+Flask-OpenTracing
+-----------------
+
+This extension provides simple integration of OpenTracing in Flask applications.
+'''
+version = open('VERSION').read()
+setup(
+    name='Flask-OpenTracing',
+    version=version,
+    url='http://github.com/opentracing-contrib/python-flask',
+    download_url='https://github.com/opentracing-contrib/python-flask/tarball/'+version,
+    license='BSD',
+    author='Kathy Camenzind',
+    author_email='kcamenzind@lightstep.com',
+    description='OpenTracing support for Flask applications',
+    long_description=open('README.rst').read(),
+    packages=['flask_opentracing', 'tests'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'opentracing>=2.0,<2.1',
+    ],
+    extras_require={
+        'tests': [
+            'flake8<3',  # see https://github.com/zheller/flake8-quotes/issues/29
+            'flake8-quotes',
+            'mock',
+            'pytest>=2.7,<3',
+            'pytest-cov',
+        ],
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
