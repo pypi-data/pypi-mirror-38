9871b1ed6908a34bd3b26d0336349d3f92fcee3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:23 +0000

    Add package pip-compile-multi/1.2.2/pip-compile-multi-1.2.2.tar.gz

---
+++ packages/pip-compile-multi/pip-compile-multi-1.2.2.tar.gz/pip-compile-multi-1.2.2/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = "1.2.2"
+
+
+with open('README.rst') as fp:
+    readme = fp.read()
+
+
+with open('HISTORY.rst') as fp:
+    history = fp.read().replace('.. :changelog:', '')
+
+
+with open(os.path.join('requirements', 'base.in')) as fp:
+    requirements = list(fp)
+
+
+setup(
+    name='pip-compile-multi',
+    version=version,
+    description="""Compile multiple requirements files to lock dependency versions""",
+    long_description=readme + '\n\n' + history,
+    author='Peter Demin',
+    author_email='peterdemin@gmail.com',
+    url='https://github.com/peterdemin/pip-compile-multi',
+    include_package_data=True,
+    py_modules=['pipcompilemulti'],
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='pip-compile-multi',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pip-compile-multi = pipcompilemulti:cli',
+        ]
+    },
+)
