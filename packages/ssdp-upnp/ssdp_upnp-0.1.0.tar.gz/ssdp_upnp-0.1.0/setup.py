852576b9ece167e1a19e32c062160c51f4b3cc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:05 +0000

    Add package ssdp-upnp/0.1.0/ssdp_upnp-0.1.0.tar.gz

---
+++ packages/ssdp-upnp/ssdp_upnp-0.1.0.tar.gz/ssdp_upnp-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ssdp_upnp",
+    version="0.1.0",
+    author="tw7613781",
+    author_email="tw7613781@gmail.com",
+    description="ssdp server and client of upnp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tw7613781/ssdp_upnp",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'colorlog>=3.1.4',
+        'miniupnpc>=2.0.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
