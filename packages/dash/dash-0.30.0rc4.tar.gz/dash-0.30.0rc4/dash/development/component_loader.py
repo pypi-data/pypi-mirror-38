a398088d5a63e4fbbb68901d306d1f6dcf16df3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:27 +0000

    Add package dash/0.30.0rc4/dash-0.30.0rc4.tar.gz

---
+++ packages/dash/dash-0.30.0rc4.tar.gz/dash-0.30.0rc4/dash/development/component_loader.py
@@ -0,0 +1,124 @@
+import collections
+import json
+import os
+import textwrap
+
+from .base_component import generate_class
+from .base_component import generate_class_file
+from .base_component import ComponentRegistry
+
+
+def _get_metadata(metadata_path):
+    # Start processing
+    with open(metadata_path) as data_file:
+        json_string = data_file.read()
+        data = json\
+            .JSONDecoder(object_pairs_hook=collections.OrderedDict)\
+            .decode(json_string)
+    return data
+
+
+def generate_imports(project_shortname, components):
+    with open(os.path.join(project_shortname, '_imports_.py'), 'w') as f:
+        f.write(textwrap.dedent(
+            '''
+            {}
+
+            __all__ = [
+            {}
+            ]
+            '''.format(
+                '\n'.join(
+                    'from .{0} import {0}'.format(x) for x in components),
+                ',\n'.join('    "{}"'.format(x) for x in components)
+            )
+        ).lstrip())
+
+
+def load_components(metadata_path,
+                    namespace='default_namespace'):
+    """Load React component metadata into a format Dash can parse.
+
+    Usage: load_components('../../component-suites/lib/metadata.json')
+
+    Keyword arguments:
+    metadata_path -- a path to a JSON file created by
+    [`react-docgen`](https://github.com/reactjs/react-docgen).
+
+    Returns:
+    components -- a list of component objects with keys
+    `type`, `valid_kwargs`, and `setup`.
+    """
+
+    # Register the component lib for index include.
+    ComponentRegistry.registry.add(namespace)
+    components = []
+
+    data = _get_metadata(metadata_path)
+
+    # Iterate over each property name (which is a path to the component)
+    for componentPath in data:
+        componentData = data[componentPath]
+
+        # Extract component name from path
+        # e.g. src/components/MyControl.react.js
+        # TODO Make more robust - some folks will write .jsx and others
+        # will be on windows. Unfortunately react-docgen doesn't include
+        # the name of the component atm.
+        name = componentPath.split('/').pop().split('.')[0]
+        component = generate_class(
+            name,
+            componentData['props'],
+            componentData['description'],
+            namespace
+        )
+
+        components.append(component)
+
+    return components
+
+
+def generate_classes(namespace, metadata_path='lib/metadata.json'):
+    """Load React component metadata into a format Dash can parse,
+    then create python class files.
+
+    Usage: generate_classes()
+
+    Keyword arguments:
+    namespace -- name of the generated python package (also output dir)
+
+    metadata_path -- a path to a JSON file created by
+    [`react-docgen`](https://github.com/reactjs/react-docgen).
+
+    Returns:
+    """
+
+    data = _get_metadata(metadata_path)
+    imports_path = os.path.join(namespace, '_imports_.py')
+
+    # Make sure the file doesn't exist, as we use append write
+    if os.path.exists(imports_path):
+        os.remove(imports_path)
+
+    components = []
+
+    # Iterate over each property name (which is a path to the component)
+    for componentPath in data:
+        componentData = data[componentPath]
+
+        # Extract component name from path
+        # e.g. src/components/MyControl.react.js
+        # TODO Make more robust - some folks will write .jsx and others
+        # will be on windows. Unfortunately react-docgen doesn't include
+        # the name of the component atm.
+        name = componentPath.split('/').pop().split('.')[0]
+        generate_class_file(
+            name,
+            componentData['props'],
+            componentData['description'],
+            namespace
+        )
+        components.append(name)
+
+    # Add the __all__ value so we can import * from _imports_
+    generate_imports(namespace, components)
