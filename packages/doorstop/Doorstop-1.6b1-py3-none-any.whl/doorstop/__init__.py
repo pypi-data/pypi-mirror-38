5fae928281cbd5833e9b0ee8f253a205a630fb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:18 +0000

    Add package doorstop/1.6b1/Doorstop-1.6b1-py3-none-any.whl

---
+++ packages/doorstop/Doorstop-1.6b1-py3-none-any.whl/doorstop/__init__.py
@@ -0,0 +1,15 @@
+"""Package for doorstop."""
+
+from doorstop.common import DoorstopError, DoorstopWarning, DoorstopInfo
+from doorstop.core import Item, Document, Tree
+from doorstop.core import build, find_document, find_item
+from doorstop.core import importer, exporter, builder, editor, publisher
+
+__project__ = 'Doorstop'
+__version__ = '1.6b1'
+
+CLI = 'doorstop'
+GUI = 'doorstop-gui'
+SERVER = 'doorstop-server'
+VERSION = "{0} v{1}".format(__project__, __version__)
+DESCRIPTION = "Requirements management using version control."
