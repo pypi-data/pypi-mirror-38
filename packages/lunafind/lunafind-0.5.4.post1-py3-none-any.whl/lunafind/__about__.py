eb6e5df3746eae1634384e08aea9b748d175fed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package lunafind/0.5.4.post1/lunafind-0.5.4.post1-py3-none-any.whl

---
+++ packages/lunafind/lunafind-0.5.4.post1-py3-none-any.whl/lunafind/__about__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 miruka
+# This file is part of lunafind, licensed under LGPLv3.
+
+"""Search, filter, inspect, download posts from Danbooru/Safebooru and find
+them back from the command line without setting up any service or database."""
+
+__project_name__ = "lunakit"
+__pkg_name__     = "lunafind"
+__version__      = "0.5.4-1"
+__status__       = "Development"
+# __status__     = "Production"
+
+__author__ = "miruka"
+__email__  = "miruka@disroot.org"
+
+__license__ = "LGPLv3"
