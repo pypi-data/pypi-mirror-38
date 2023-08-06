08ea10a5ee81647c96b02e9b358197cff2d80bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:10 +0000

    Add package formation/0.1.7/formation-0.1.7.tar.gz

---
+++ packages/formation/formation-0.1.7.tar.gz/formation-0.1.7/formation/for_requests.py
@@ -0,0 +1,31 @@
+import requests
+from .formation import wrap
+
+__all__ = ["sender"]
+
+
+def sender(middleware=[]):
+    wrapped = wrap(requests_adapter, middleware=middleware)
+
+    def send(method, url, **kwargs):
+        ctx = {"req": {"method": method, "url": url}}
+        ctx["req"].update(kwargs)
+        ctx = wrapped(ctx)
+        return ctx["res"]
+
+    return send
+
+
+def requests_adapter(ctx):
+    req = ctx["req"]
+    meth = getattr(requests, req.get("method", "get").lower())
+    # TODO ship var as kwargs and not explicitly
+    res = meth(
+        req["url"],
+        headers=req.get("headers", {}),
+        params=req.get("params", {}),
+        auth=req.get("auth", None),
+        data=req.get("data", None),
+    )
+    ctx["res"] = res
+    return ctx
