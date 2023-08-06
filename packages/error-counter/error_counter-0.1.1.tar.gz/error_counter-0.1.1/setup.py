b6a85ab22376bcc695f1dbf89ab12d95616e16e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:22 +0000

    Add package error-counter/0.1.1/error_counter-0.1.1.tar.gz

---
+++ packages/error-counter/error_counter-0.1.1.tar.gz/error_counter-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#from distutils.core import setup
+from setuptools import setup
+
+with open("README.md") as f:
+  long_description = f.read()
+
+setup(
+  name = 'error_counter',
+  packages = ['error_counter'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'Error Counter: Count error (e.g. network error) beyond process boundary, then issue fix command when exceeded threshold.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Takeyuki UEDA',
+  author_email = 'gde00107@nifty.com',
+  license='MIT',
+  url = 'https://github.com/UedaTakeyuki/error_counter', # use the URL to the github repo
+  keywords = ['Counter', 'Error', 'Reset'], # arbitrary keywords
+  classifiers = ['Development Status :: 4 - Beta',
+                 'Programming Language :: Python',
+                 'Topic :: Terminals'
+  ],
+  install_requires=[
+    'incremental_counter'
+  ]
+)
