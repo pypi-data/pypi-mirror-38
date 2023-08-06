7efebaae57ade5a9840ef884289ff89f0b2332f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:23 +0000

    Add package donn/1.1.3/donn-1.1.3.tar.gz

---
+++ packages/donn/donn-1.1.3.tar.gz/donn-1.1.3/donn/__init__.py
@@ -0,0 +1,6 @@
+from . import main
+from .main import Optimizer
+from .main import predict
+from .main import allowed_layers
+from .base_model import run_base_model
+from .main import *
---
+++ packages/donn/donn-1.1.3.tar.gz/donn-1.1.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+long_description = '''
+DONN (Deep Optimized Neural Networks) is a library that can be used by people new to deep learning, to build optimized neural networks.
+
+Read the documentation at: https://github.com/sharan-amutharasu/donn
+
+DONN is compatible with Python 3
+and is distributed under the MIT license.
+'''
+
+setup(name='donn',
+      version='1.1.3',
+      description='Deep Optimized Neural Networks',
+      long_description=long_description,
+      author='Sharan Amutharasu',
+      author_email='sharan.amutharasu@gmail.com',
+      url='https://github.com/sharan-amutharasu/donn',
+      download_url='https://github.com/sharan-amutharasu/donn/tarball/1.1.1',
+      license='MIT',
+      install_requires=['keras>=2.2.0',
+                        'numpy>=1.9.1',
+                        'scipy>=0.14',
+                        'six>=1.9.0',
+                        'pyyaml',
+                        'h5py',
+                        'gc',
+                        'pickle',
+                        'scikit-learn>=0.19.0',
+                        'time',
+                        'string',
+                        'random'],
+      extras_require={
+          'tests': ['pytest'],
+      },
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=['donn'])
