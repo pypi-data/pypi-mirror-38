bafedf8552fc313aaec7a768f5d7254cb830812d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:10 +0000

    Add package pygraphml/2.3/pygraphml-2.3.tar.gz

---
+++ packages/pygraphml/pygraphml-2.3.tar.gz/pygraphml-2.3/setup.py
@@ -0,0 +1,36 @@
+import os
+import io
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+setup(name='pygraphml',
+      version='2.3',
+      author='Hadrien Mary',
+      author_email='hadrien.mary@gmail.com',
+      description='Library to parse GraphML files in Python',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
+      url='https://github.com/hadim/pygraphml/',
+      packages=find_packages(),
+      classifiers=[
+              'Development Status :: 5 - Production/Stable',
+              'Intended Audience :: Developers',
+              'Natural Language :: English',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 2',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.4',
+              ])
