10a50478c5309c429b7031f34f9fe9f2fbe2deab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:12 +0000

    Add package seneca/0.1.169/seneca-0.1.169.tar.gz

---
+++ packages/seneca/seneca-0.1.169.tar.gz/seneca-0.1.169/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+import os
+
+major = 0
+
+def get_version_number():
+    if os.getenv('CIRCLECI'):
+        minor, patch = divmod(int(os.getenv('CIRCLE_BUILD_NUM')), 360)
+        ver = '{}.{}.{}'.format(major, minor, patch)
+        return ver
+    else:
+        return '{}.1.166'.format(major)
+
+__version__ = get_version_number()
+
+print('#' * 128)
+print('\n    VERSION = {}\n'.format(__version__))
+print('#' * 128)
+
+requirements = [
+    'redis==2.10.6',
+    'python-dotenv==0.9.1',
+    'ujson==1.35'
+]
+
+setup(
+    name='seneca',
+    version=__version__,
+    description='Python-based smart contract language and interpreter.',
+    entry_points={
+        'console_scripts': ['seneca=seneca.cli:main'],
+    },
+    packages=find_packages(),
+    install_requires=requirements,
+    url='https://github.com/Lamden/seneca',
+    author='Lamden',
+    author_email='team@lamden.io',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    ext_modules=[
+        Extension('seneca.libs.metering.tracer', sources = ['seneca/libs/metering/tracer.c'])
+    ]
+)
