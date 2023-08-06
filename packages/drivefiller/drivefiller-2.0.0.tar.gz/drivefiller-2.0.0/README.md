51273b60484b56028642e15e085ba78e47aada33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:46 +0000

    Add package drivefiller/2.0.0/drivefiller-2.0.0.tar.gz

---
+++ packages/drivefiller/drivefiller-2.0.0.tar.gz/drivefiller-2.0.0/README.md
@@ -0,0 +1,5 @@
+# DriveFiller(dot)py
+A python library that creates very large files very quickly.
+
+## Examples
+I can't be bothered to update this so figure it out yourself
---
+++ packages/drivefiller/drivefiller-2.0.0.tar.gz/drivefiller-2.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drivefiller",
+    version="2.0.0",
+    author="Conor Matthews",
+    author_email="conor.matthews15@gmail.com",
+    description="A Python 3 library that makes very large files very quickly. Can also return file size.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ConorMatthews/drivefiller",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
