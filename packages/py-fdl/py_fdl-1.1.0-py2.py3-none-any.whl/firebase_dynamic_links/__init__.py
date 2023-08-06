3bcdbac6d77f009a248f0365daf5e62c13e89954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package py-fdl/1.1.0/py_fdl-1.1.0-py2.py3-none-any.whl

---
+++ packages/py-fdl/py_fdl-1.1.0-py2.py3-none-any.whl/firebase_dynamic_links/__init__.py
@@ -0,0 +1,5 @@
+from .builder import generate_long_link
+from .builder import generate_short_link
+from .builder import dynamic_link_builder
+from .builder import DynamicLinkBuilder
+from .client import FirebaseClient
---
+++ packages/py-fdl/py_fdl-1.1.0-py2.py3-none-any.whl/firebase_dynamic_links/dynamic_links.py
@@ -0,0 +1,48 @@
+import requests
+from urllib.parse import urlencode
+
+from .errors import FirebaseServerError
+
+
+def generate_short_link(client, app_code, query_params):
+    long_dynamic_link = generate_long_link(app_code=app_code, query_params=query_params)
+
+    return client.shorten_link(long_link=long_dynamic_link)
+
+
+def generate_long_link(app_code, query_params):
+    query_string = urlencode(query_params)
+    link = 'https://{app_code}.app.goo.gl/?{query_string}'.format(app_code=app_code, query_string=query_string)
+
+    return link
+
+
+class FirebaseClient:
+    FIREBASE_API_URL = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key={api_key}'
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.url = self.FIREBASE_API_URL.format(api_key=self.api_key)
+
+    def shorten_link(self, long_link):
+        payload = {
+            "longDynamicLink": long_link
+        }
+        response = requests.post(self.url, json=payload)
+        data = response.json()
+
+        if not response.ok:
+            raise FirebaseServerError('Error from Firebase: {data}'.format(data=data))
+
+        return data.get('shortLink')
+
+
+class DynamicLinkBuilder:
+    def __init__(self, client):
+        self.client = client
+
+    def generate_long_link(self, app_code, **kwargs):
+        return generate_long_link(app_code=app_code, query_params=kwargs)
+
+    def generate_short_link(self, app_code, **kwargs):
+        return generate_short_link(client=self.client, app_code=app_code, query_params=kwargs)
