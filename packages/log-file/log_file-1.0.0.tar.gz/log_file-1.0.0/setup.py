e91afbbd20fc579e14a78c7bc05fccfe8d7b3199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:50 +0000

    Add package log-file/1.0.0/log_file-1.0.0.tar.gz

---
+++ packages/log-file/log_file-1.0.0.tar.gz/log_file-1.0.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 19:11:53 2018
+
+@author: 017253
+"""
+
+from setuptools import setup
+
+setup(
+    name='log_file',
+    version='1.0.0',
+    description='A log python project',
+    url='https://github.com/fyingzhu/Python_logging',
+    author='fyingzhu',
+    author_email='fyingzhu@126.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='log logging python',
+    packages=['log_file'],
+)
