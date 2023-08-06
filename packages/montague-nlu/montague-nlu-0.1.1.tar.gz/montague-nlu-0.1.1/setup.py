d39e1e75155f1bb073d6c91078c439f0ceb98fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:04 +0000

    Add package montague-nlu/0.1.1/montague-nlu-0.1.1.tar.gz

---
+++ packages/montague-nlu/montague-nlu-0.1.1.tar.gz/montague-nlu-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='montague-nlu',
+    version='0.1.1',
+    description='Natural language understanding system',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Ian Fisher',
+    author_email='iafisher@protonmail.com',
+    entry_points={
+        'console_scripts': [
+            'montague = montague.montague:run_shell',
+        ],
+    },
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'lark-parser==0.6.4',
+    ],
+    project_urls={
+        'Source': 'https://github.com/iafisher/montague',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
