ad6dfd2bd1d5d75db0d9483f3a7de3714065a730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:35 +0000

    Add package tinyscript/1.1.7/tinyscript-1.1.7.tar.gz

---
+++ packages/tinyscript/tinyscript-1.1.7.tar.gz/tinyscript-1.1.7/tinyscript/parser.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Module for defining argument parser-related functions and objects.
+
+"""
+import argparse
+import atexit
+import os
+import random
+import re
+import shlex
+import sys
+from os.path import basename, splitext
+
+from .__info__ import __author__, __copyright__, __version__
+from .handlers import *
+from .loglib import *
+
+
+__all__ = __features__ = ["parser", "initialize", "validate"]
+
+
+def __descr_format(g):
+    """
+    Help description formatting function to add global documentation variables.
+
+    :param g: globals() dictionary
+    :return: the formatted help description
+    """
+    p, _ = splitext(basename(g['__file__']))
+    s = ''.join([x.capitalize() for x in p.split('-')])
+    if '__version__' in g:
+        s += " v" + g['__version__']
+    for v in ['__author__', '__reference__', '__source__', '__training__']:
+        if v in g:
+            s += "\n%s: %s" % (v.strip('_').capitalize(), g[v])
+            if v == '__author__' and '__email__' in g:
+                s += " ({})".format(g['__email__'])
+    d = g.get('__doc__')
+    return s + "\n\n" + d if d is not None else s
+
+
+def __get_calls_from_parser(proxy_parser, real_parser):
+    """
+    This actually executes the calls registered in the ProxyArgumentParser.
+
+    :param parser: ProxyArgumentParser instance
+    """
+    __parsers[proxy_parser] = real_parser
+    for method, safe, args, kwargs, proxy_subparser in proxy_parser.calls:
+        args = (__proxy_to_real_parser(v) for v in args)
+        kwargs = {k: __proxy_to_real_parser(v) for k, v in kwargs.items()}
+        real_subparser = getattr(real_parser, method)(*args, **kwargs)
+        if real_subparser is not None:
+            __get_calls_from_parser(proxy_subparser, real_subparser)
+
+
+def __proxy_to_real_parser(value):
+    """
+    This recursively converts ProxyArgumentParser instances to actual parsers.
+
+    Use case: defining subparsers with a parent
+      >>> [...]
+      >>> parser.add_argument(...)  # argument common to all subparsers
+      >>> subparsers = parser.add_subparsers()
+      >>> subparsers.add_parser(..., parents=[parent])
+                                                ^
+                              this is an instance of ProxyArgumentParser
+                              and must be converted to an actual parser instance
+
+    :param value: a value coming from args or kwargs aimed to an actual parser
+    """
+    if isinstance(value, ProxyArgumentParser):
+        return __parsers[value]
+    elif any(isinstance(value, t) for t in [list, tuple]):
+        new_value = []
+        for subvalue in iter(value):
+            new_value.append(__proxy_to_real_parser(subvalue))
+        return new_value
+    return value
+
+
+def initialize(glob, sudo=False, multi_debug_level=False,
+                     add_help=True, add_demo=False, add_wizard=False):
+    """
+    Initialization function ; sets up the arguments for the parser and creates a
+     logger to be inserted in the input dictionary of global variables from the
+     calling script.
+
+    :param glob:              globals() instance from the calling script
+    :param sudo:              if True, require sudo credentials and re-run
+                               script with sudo
+    :param multi_debug_level: allow to use -v, -vv, -vvv (adjust logging level)
+                               instead of just -v (only debug on/off)
+    :param add_help:          set add_help in ArgumentParser
+    :param add_demo:          add an option to re-run the process using a random
+                               entry from the __examples__ (only works if this
+                               variable is populated)
+    :param add_wizard:        add an option to run a wizard, asking for each
+                               input argument
+    """
+    global parser, __parsers
+    # 1) handle the demo if relevant
+    if add_demo and "--play-demo" in sys.argv:
+        argv = random.choice(glob['__examples__']).replace("--play-demo", "")
+        sys.argv[1:] = shlex.split(argv)
+    # 2) if sudo required, restart the script
+    if sudo:
+        # if not root, restart the script in another process and jump to this
+        if os.geteuid() != 0:
+            python = [] if glob['__file__'].startswith("./") else ["python"]
+            os.execvp("sudo", ["sudo"] + python + sys.argv)
+    # 3) format help message's variables and create the real argument parser
+    p = basename(glob['__file__'])
+    pn, _ = splitext(p)
+    e = None if '__examples__' not in glob or len(glob['__examples__']) == 0 \
+        else glob['__examples__']
+    e = "Usage examples:\n" + '\n'.join(["  python {0} {1}".format(p, x) \
+        for x in e]) if e is not None else e
+    glob['parser'] = argparse.ArgumentParser(prog=pn, epilog=e,
+        description=__descr_format(glob), add_help=add_help,
+        formatter_class=argparse.RawTextHelpFormatter,
+        conflict_handler="resolve")
+    # 4) populate the real parser and add default options (if not used yet)
+    __parsers = {}
+    __get_calls_from_parser(parser, glob['parser'])
+    try:
+        glob['parser'].add_argument("-v", dest="verbose", default=0,
+            action="count" if multi_debug_level else "store_true",
+            help="debug verbose level (default: {})"
+                 .format(["false", "error"][multi_debug_level]))
+    except argparse.ArgumentError:
+        pass  # if this argument was already passed, just ignore
+    if e is not None and add_demo:
+        try:
+            glob['parser'].add_argument("--play-demo", action="store_true",
+                                        help="play a demo (default: false)\n  "
+                                             "NB: this has the precedence on "
+                                             "any other option")
+        except argparse.ArgumentError:
+            pass # if this argument was already passed, just ignore
+    if add_wizard:
+        try:
+            glob['parser'].add_argument("--start-wizard", action="store_true",
+                                        help="start a wizard (default: false)")
+        except argparse.ArgumentError:
+            pass # if this argument was already passed, just ignore
+    glob['args'] = glob['parser'].parse_args()
+    # then, handle the wizard if relevant
+    #TODO: parse each possible argument, using its default value if not set in
+    #       user input, using os.execvp once all new arguments have been entered
+    #       by the user
+    # 5) configure logging and get the main logger
+    configure_logger(glob, multi_debug_level)
+    # 6) finally, bind the global exit handler
+    def __at_exit():
+        if _hooks.state == "INTERRUPTED":
+            glob['at_interrupt']()
+        elif _hooks.state == "TERMINATED":
+            glob['at_terminate']()
+        else:
+            glob['at_graceful_exit']()
+        glob['at_exit']()
+        logging.shutdown()
+    atexit.register(__at_exit)
+
+
+def validate(glob, *arg_checks):
+    """
+    Function for validating group of arguments ; each argument is represented as
+     a 4-tuple like follows:
+
+        (argument_name, fail_condition, error_message, default)
+
+        - argument_name: the name of the argument like entered in add_argument()
+        - fail_condition: condition in Python code with the argument name
+                          replaced by ' ? ' (e.g. " ? > 0")
+        - error_message: message describing what failed with the validation ofs
+                         the current argument
+        - default [optional]: value to be given if the validation fails ; this
+                              implies that the script will not exit after the
+                              validation (if no other blocking argument present)
+
+    :param glob: globals() instance from the calling script
+    :param arg_checks: list of 3/4-tuples
+    """
+    locals().update(glob)
+    if glob['args'] is None or glob['logger'] is None:
+        return
+    exit_app = False
+    for check in arg_checks:
+        check = check + (None, ) * (4 - len(check))
+        param, condition, message, default = check
+        assert re.match(r'^_?[a-zA-Z][a-zA-Z0-9_]*$', param) is not None, \
+               "Illegal argument name"
+        try:
+            result = eval(condition.replace(" ? ", " glob['args'].{} "
+                                                   .format(param)))
+        except (AssertionError, TypeError) as e:
+            result = True
+            message = str(e)
+        if result:
+            if default is None:
+                glob['logger'].error(message or "Validation failed")
+                exit_app = True
+            else:
+                glob['logger'].warn(message or "Validation failed")
+                setattr(glob['args'], param, default)
+    if exit_app:
+        sys.exit(2)
+
+
+class ProxyArgumentParser(object):
+    """
+    Proxy class for collecting added arguments before initialization.
+    """
+    def __init__(self):
+        self.calls = []
+        self.__parser = argparse.ArgumentParser()
+
+    def __getattr__(self, name):
+        self.__current_call = name
+        self.__call_exists = hasattr(self.__parser, name) and \
+                             callable(getattr(self.__parser, name))
+        return self.__collect
+
+    def __collect(self, *args, **kwargs):
+        subparser = ProxyArgumentParser()
+        self.calls.append((self.__current_call, self.__call_exists,
+                           args, kwargs, subparser))
+        del self.__current_call
+        del self.__call_exists
+        return subparser
+
+
+parser = ProxyArgumentParser()
