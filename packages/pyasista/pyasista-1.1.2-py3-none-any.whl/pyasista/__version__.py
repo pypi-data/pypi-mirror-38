1ff6d0b7723edc88b70c87e792ca1bf89fc53bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:30 +0000

    Add package pyasista/1.1.2/pyasista-1.1.2-py3-none-any.whl

---
+++ packages/pyasista/pyasista-1.1.2-py3-none-any.whl/pyasista/__version__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+__title__ = 'pyasista'
+__description__ = 'Arcelik Asista Smart Assistant AWS Lambda Python Library'
+__url__ = 'https://github.com/Arcelik/pyasista'
+__version__ = '1.1.2'
+__author__ = 'Arcelik Asista Team'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 Arcelik A.S.'
