795cd2c8cc7d54749ecb16356ddebc35b58297a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:24 +0000

    Add package cryptofeed/0.17.3/cryptofeed-0.17.3-py3-none-any.whl

---
+++ packages/cryptofeed/cryptofeed-0.17.3-py3-none-any.whl/cryptofeed/exchanges.py
@@ -0,0 +1,15 @@
+'''
+Copyright (C) 2017-2018  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+BITSTAMP = 'BITSTAMP'
+BITFINEX = 'BITFINEX'
+COINBASE = 'COINBASE'
+GEMINI = 'GEMINI'
+HITBTC = 'HITBTC'
+POLONIEX = 'POLONIEX'
+BITMEX = 'BITMEX'
+KRAKEN = 'KRAKEN'
+BINANCE = 'BINANCE'
