b7bedb5b0bdc98f179f1484bac5a8ed35d3e3f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:41 +0000

    Add package limix-plot/0.0.10/limix-plot-0.0.10.tar.gz

---
+++ packages/limix-plot/limix-plot-0.0.10.tar.gz/limix-plot-0.0.10/limix_plot/_plt.py
@@ -0,0 +1,16 @@
+def get_pyplot():
+    from os import environ as _environ
+
+    if get_pyplot.pyplot is not None:
+        return get_pyplot.pyplot
+
+    if "DISPLAY" not in _environ:
+        from matplotlib import use as _backend_use
+
+        _backend_use("Agg")
+    from matplotlib import pyplot
+
+    return pyplot
+
+
+get_pyplot.pyplot = None
