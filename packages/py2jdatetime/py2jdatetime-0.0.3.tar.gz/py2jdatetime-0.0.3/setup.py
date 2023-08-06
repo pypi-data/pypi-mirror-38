304c1c8b1d59024fe44cbc609640719f0d80ba0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:12 +0000

    Add package py2jdatetime/0.0.3/py2jdatetime-0.0.3.tar.gz

---
+++ packages/py2jdatetime/py2jdatetime-0.0.3.tar.gz/py2jdatetime-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py2jdatetime",
+    version="0.0.3",
+    author="Iddo Yadlin",
+    author_email="iddoyadlin@gmail.com",
+    description="A package for converting python datetime patterns to java patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Iddoyadlin/Py2JDatetime",
+    packages=['py2jdatetime'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
