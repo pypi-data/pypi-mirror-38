592bcbac488ee86d88536ec5fedfbc678000b588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:57 +0000

    Add package pysmlib/2.0.0rc2/pysmlib-2.0.0rc2.tar.gz

---
+++ packages/pysmlib/pysmlib-2.0.0rc2.tar.gz/pysmlib-2.0.0rc2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='pysmlib',
+      version=versioneer.get_version(),
+      description='Python Finite State Machines for EPICS',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://darcato.github.io/pysmlib/docs/html/',
+      download_url='https://github.com/darcato/pysmlib',
+      author='Damiano Bortolato - Davide Marcato',
+      author_email='davide.marcato@lnl.infn.it',
+      license='GPLv3',
+      packages=['smlib'],
+      install_requires=['pyepics', 'numpy'],
+      zip_safe=False)
