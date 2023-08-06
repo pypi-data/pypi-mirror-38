a9f4f1c2fbbd9c4c9487549b23bdcf0c4c7af15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:05 +0000

    Add package django-globee/1.3.0/django_globee-1.3.0-py3-none-any.whl

---
+++ packages/django-globee/django_globee-1.3.0-py3-none-any.whl/globee/views.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from logging import getLogger
+from json import loads as json_loads, dumps as json_dumps
+
+from pytz import utc as pytz_utc
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+from globee.models import GlobeeIPN
+
+
+logger = getLogger(__name__)
+
+
+@require_POST
+@csrf_exempt
+def globee_ipn_view(request):
+    payment_data = json_loads(request.body.decode("utf-8"))
+
+    pretty_data = json_dumps(payment_data, indent=4, sort_keys=True)
+    logger.debug('Globee POST data: %s' % pretty_data)
+
+    try:
+        created_at = datetime.strptime(payment_data['created_at'], '%Y-%m-%d %H:%M:%S')
+        expires_at = datetime.strptime(payment_data['expires_at'], '%Y-%m-%d %H:%M:%S')
+
+        defaults = {
+            'payment_status': payment_data['status'],
+            'total': float(payment_data['total']),
+            'currency': payment_data['currency'],
+            'custom_payment_id': payment_data['custom_payment_id'],
+            'callback_data': payment_data['callback_data'],
+            'customer_email': payment_data['customer']['email'],
+            'customer_name': payment_data['customer']['name'],
+            'created_at': pytz_utc.localize(created_at),
+            'expires_at': pytz_utc.localize(expires_at),
+        }
+
+        payment, created = GlobeeIPN.objects.update_or_create(
+            payment_id=payment_data['id'],
+            defaults=defaults
+        )
+        payment.send_valid_signal()
+    except KeyError as e:
+        logger.error('Key %s not found in payment data.' % e)
+        return HttpResponse(status=400)
+    except ValueError as e:
+        logger.error(e)
+        return HttpResponse(status=400)
+
+    return HttpResponse(status=200)
+
