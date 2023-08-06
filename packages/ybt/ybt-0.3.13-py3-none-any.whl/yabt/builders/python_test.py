86808bff5e13371e999d107efbb076f17fdec423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:31 +0000

    Add package ybt/0.3.13/ybt-0.3.13-py3-none-any.whl

---
+++ packages/ybt/ybt-0.3.13-py3-none-any.whl/yabt/builders/python_test.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 Resonai Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+
+:author: Itamar Ostricher
+"""
+
+import random
+import pytest
+
+from ..buildcontext import BuildContext
+from ..graph import populate_targets_graph
+
+
+slow = pytest.mark.skipif(not pytest.config.getoption('--with-slow'),
+                          reason='need --with-slow option to run')
+
+
+@slow
+@pytest.mark.usefixtures('in_tests_project')
+def test_python_tester_success(basic_conf):
+    build_context = BuildContext(basic_conf)
+    basic_conf.targets = ['hello_pytest:greet-test']
+    populate_targets_graph(build_context, basic_conf)
+    build_context.build_graph(run_tests=True)
+
+
+@slow
+@pytest.mark.usefixtures('in_tests_project')
+def test_python_tester_fail(basic_conf):
+    build_context = BuildContext(basic_conf)
+    basic_conf.targets = ['hello_pytest:greet-failing-test']
+    populate_targets_graph(build_context, basic_conf)
+    with pytest.raises(SystemExit):
+        build_context.build_graph(run_tests=True)
+
+
+@slow
+@pytest.mark.usefixtures('in_tests_project')
+def test_python_tester_fail_with_retry(basic_conf):
+    build_context = BuildContext(basic_conf)
+    target_name = 'hello_pytest:greet-failing-test'
+    basic_conf.targets = [target_name]
+    populate_targets_graph(build_context, basic_conf)
+    target = build_context.targets[target_name]
+    target.props.attempts = 5
+    with pytest.raises(SystemExit):
+        build_context.build_graph(run_tests=True)
+    assert target.info['fail_count'] == 5
+
+
+@slow
+@pytest.mark.usefixtures('in_tests_project')
+def test_python_tester_flaky(basic_conf):
+    build_context = BuildContext(basic_conf)
+    target_name = 'hello_pytest:flaky-test'
+    basic_conf.targets = [target_name]
+    populate_targets_graph(build_context, basic_conf)
+    target = build_context.targets[target_name]
+    target.props.test_env['RANDOM_FILE'] = str(random.randint(0, 20000))
+    build_context.build_graph(run_tests=True)
+    assert target.info['fail_count'] == 1
