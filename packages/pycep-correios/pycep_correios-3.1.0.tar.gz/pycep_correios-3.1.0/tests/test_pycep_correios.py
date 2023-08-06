3ca787e7f0c7c86d590df40f1f6f696b10198c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:30 +0000

    Add package pycep-correios/3.1.0/pycep_correios-3.1.0.tar.gz

---
+++ packages/pycep-correios/pycep_correios-3.1.0.tar.gz/pycep_correios-3.1.0/tests/test_pycep_correios.py
@@ -0,0 +1,55 @@
+import unittest
+
+from pycep_correios import consultar_cep, formatar_cep, validar_cep
+
+
+class TestPyCEPCorreios(unittest.TestCase):
+
+    def setUp(self):
+        self.expected_address = {
+            'bairro': 'Santo Antônio',
+            'cep': '37503130',
+            'cidade': 'Itajubá',
+            'end': 'Rua Geraldino Campista',
+            'uf': 'MG',
+            'complemento2': '- até 214/215',
+            'unidadesPostagem': [],
+        }
+
+    def test_consultar_cep(self):
+
+        # Realizamos a consulta de CEP
+        endereco = consultar_cep('37.503-130')
+
+        self.assertEqual(endereco['bairro'], 'Santo Antônio')
+        self.assertEqual(endereco['cep'], '37503130')
+        self.assertEqual(endereco['cidade'], 'Itajubá')
+        self.assertEqual(endereco['complemento2'], '- até 214/215')
+        self.assertEqual(endereco['end'], 'Rua Geraldino Campista')
+        self.assertEqual(endereco['uf'], 'MG')
+        self.assertEqual(endereco['unidadesPostagem'], [])
+
+    def test_formatar_cep(self):
+        self.assertRaises(ValueError, formatar_cep, 37503003)
+        self.assertRaises(ValueError, formatar_cep, '')
+        self.assertRaises(ValueError, formatar_cep, None)
+        self.assertRaises(ValueError, formatar_cep, False)
+        self.assertRaises(ValueError, formatar_cep, True)
+        self.assertEqual(formatar_cep('37.503-003'), '37503003')
+        self.assertEqual(formatar_cep('   37.503-003'), '37503003')
+        self.assertEqual(formatar_cep('37 503-003'), '37503003')
+        self.assertEqual(formatar_cep('37.503&003saasd'), '37503003')
+        self.assertEqual(formatar_cep('\n \r 37.503-003'), '37503003')
+        self.assertEqual(formatar_cep('\n \r 37.503-003'), '37503003')
+        # ponto e virgula
+        self.assertEqual(formatar_cep('37.503-003;'), '37503003')
+        # Unicode Greek Question Mark
+        self.assertEqual(formatar_cep(u'37.503-003;'), '37503003')
+
+    def test_validar_cep(self):
+        self.assertRaises(ValueError, validar_cep, 37503003)
+        self.assertRaises(ValueError, validar_cep, '')
+        self.assertIs(validar_cep('37.503-003'), True)
+        self.assertIs(validar_cep('37.503-00'), False)
+        self.assertIs(validar_cep('   37.503-003'), True)
+        self.assertIs(validar_cep('37.503&003saasd'), True)
