a6488f7ac0d3ca2effbc17b2844f04dcbe6f54a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:44 +0000

    Add package handout/0.2.1/handout-0.2.1.tar.gz

---
+++ packages/handout/handout-0.2.1.tar.gz/handout-0.2.1/setup.py
@@ -0,0 +1,19 @@
+import glob
+import setuptools
+
+
+setuptools.setup(
+    name='handout',
+    version='0.2.1',
+    description='Add Markdown text and inline figures to your Python script.',
+    url='http://github.com/danijar/handout',
+    install_requires=[],
+    packages=['handout'],
+    data_files=[('handout', glob.glob('handout/data/*'))],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
