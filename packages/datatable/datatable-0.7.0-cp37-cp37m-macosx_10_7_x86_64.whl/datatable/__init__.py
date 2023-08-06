e4d4c7a4e4e20394d289e948557c4bef15d8eae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:35 +0000

    Add package datatable/0.7.0/datatable-0.7.0-cp37-cp37m-macosx_10_7_x86_64.whl

---
+++ packages/datatable/datatable-0.7.0-cp37-cp37m-macosx_10_7_x86_64.whl/datatable/__init__.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# © H2O.ai 2018; -*- encoding: utf-8 -*-
+#   This Source Code Form is subject to the terms of the Mozilla Public
+#   License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#-------------------------------------------------------------------------------
+from .__version__ import version as __version__
+from .dt_append import rbind, cbind
+from .frame import Frame
+from .expr import mean, min, max, sd, isna, sum, count, first, abs, exp
+from .fread import fread, GenericReader, FreadWarning
+from .graph import f, g, join, by
+from .lib import core as _core
+from .nff import save, open
+from .options import options
+from .str import split_into_nhot
+from .types import stype, ltype
+from .utils.typechecks import TTypeError as TypeError
+from .utils.typechecks import TValueError as ValueError
+from .utils.typechecks import DatatableWarning
+try:
+    from .__git__ import __git_revision__
+except:
+    __git_revision__ = ""
+
+
+__all__ = ("__version__", "__git_revision__",
+           "Frame", "max", "mean", "min", "open", "sd", "sum", "count", "first",
+           "isna", "fread", "GenericReader", "save", "stype", "ltype", "f", "g",
+           "join", "by", "abs", "exp",
+           "TypeError", "ValueError", "DatatableWarning", "FreadWarning",
+           "DataTable", "options",
+           "bool8", "int8", "int16", "int32", "int64",
+           "float32", "float64", "str32", "str64", "obj64",
+           "cbind", "rbind",
+           "unique", "union", "intersect", "setdiff", "symdiff",
+           "split_into_nhot")
+
+bool8 = stype.bool8
+int8 = stype.int8
+int16 = stype.int16
+int32 = stype.int32
+int64 = stype.int64
+float32 = stype.float32
+float64 = stype.float64
+str32 = stype.str32
+str64 = stype.str64
+obj64 = stype.obj64
+DataTable = Frame
+unique = _core.unique
+union = _core.union
+intersect = _core.intersect
+setdiff = _core.setdiff
+symdiff = _core.symdiff
+
+Frame.__module__ = "datatable"
---
+++ packages/datatable/datatable-0.7.0-cp37-cp37m-macosx_10_7_x86_64.whl/datatable/expr/mean_expr.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# © H2O.ai 2018; -*- encoding: utf-8 -*-
+#   This Source Code Form is subject to the terms of the Mozilla Public
+#   License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#-------------------------------------------------------------------------------
+
+from .base_expr import BaseExpr
+from .consts import ops_rules, ctypes_map, nas_map, reduce_opcodes
+from datatable.lib import core
+
+
+def mean(expr, skipna=True):
+    return MeanReducer(expr, skipna)
+
+
+class MeanReducer(BaseExpr):
+    __slots__ = ["expr", "skipna"]
+
+    def __init__(self, expr, skipna=True):
+        super().__init__()
+        self.expr = expr
+        self.skipna = skipna
+
+    def is_reduce_expr(self, ee):
+        return True
+
+
+    def resolve(self):
+        self.expr.resolve()
+        expr_stype = self.expr.stype
+        self._stype = ops_rules.get(("mean", expr_stype), None)
+        if self._stype is None:
+            raise ValueError(
+                "Cannot compute mean of a variable of type %s" % expr_stype)
+
+
+    def evaluate_eager(self, ee):
+        col = self.expr.evaluate_eager(ee)
+        opcode = reduce_opcodes["mean"]
+        return core.expr_reduceop(opcode, col, ee.groupby)
+
+
+    def __str__(self):
+        return "mean%d(%s)" % (self.skipna, self.expr)
+
+
+    def _value(self, key, inode):
+        pf = inode.previous_function
+        vsum, vcnt, vres = pf.make_variable("sum", "cnt", "res")
+        visna = vsum + "_isna"
+        v = inode.make_variable("mean")
+        i = inode.add_stack_variable(str(self))
+        arg_isna = self.expr.isna(pf)
+        arg_notna = self.expr.notna(pf)
+        res_ctype = ctypes_map[self.stype]
+        if self.skipna:
+            pf.add_prologue_expr("long double %s = 0;" % vsum)
+            pf.add_prologue_expr("int64_t %s = 0;" % vcnt)
+            pf.add_mainloop_expr("%s += %s? 0 : %s;"
+                                 % (vsum, arg_isna, arg_notna))
+            pf.add_mainloop_expr("%s += %s;" % (vcnt, arg_isna))
+            pf.add_epilogue_expr("%s %s = %s / %s;"
+                                 % (res_ctype, vres, vsum, vcnt))
+        else:
+            pf.add_prologue_expr("long double %s = 0;" % vsum)
+            pf.add_prologue_expr("int %s = 0;" % visna)
+            pf.add_mainloop_expr("%s |= %s;" % (visna, arg_isna))
+            pf.add_mainloop_expr("%s += %s? 0 : (%s);"
+                                 % (vsum, visna, arg_notna))
+            pf.add_epilogue_expr("%s %s = %s? %s : (%s / nrows);"
+                                 % (res_ctype, vres, visna,
+                                    nas_map[self.stype], vsum))
+        pf.add_epilogue_expr("stack[%d].f8 = %s;" % (i, vres))
+        inode.add_prologue_expr("%s %s = stack[%d].f8;"
+                                % (res_ctype, v, i))
+        return v
+
+
+    def _isna(self, key, inode):
+        if self.skipna:
+            return "0"
+        else:
+            return "ISNA_F8(%s)" % self.value(inode)
+
+
+    def _notna(self, key, inode):
+        return self.value(inode)
