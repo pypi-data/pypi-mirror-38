17a61b8dc3ae314649e92bdaca6ca9f1ab18d5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:17 +0000

    Add package py-pgtest/0.1.10/py-pgtest-0.1.10.tar.gz

---
+++ packages/py-pgtest/py-pgtest-0.1.10.tar.gz/py-pgtest-0.1.10/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='py-pgtest',
+    version='0.1.10',
+    packages=['py_pgtest'],
+    url='https://github.com/zh217/py-pgtest',
+    license='MIT',
+    author='Ziyang Hu',
+    author_email='hu.ziyang@cantab.net',
+    description='testing pg made easy',
+    entry_points={
+        'console_scripts': ['pgtest=py_pgtest.cli:main'],
+    },
+    install_requires=['pytest-tap', 'watchdog', 'colorama'],
+    include_package_data=True
+)
