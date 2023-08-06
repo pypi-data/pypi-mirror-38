320fb229f928ccb4ab6702b9e7d3bd3b5a06e3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:37 +0000

    Add package draco/0.0.3/draco-0.0.3.tar.gz

---
+++ packages/draco/draco-0.0.3.tar.gz/draco-0.0.3/tests/test_run.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+from jsonschema import validate
+
+from draco.run import run
+from draco.spec import Task
+
+EXAMPLES_DIR = os.path.join("examples")
+
+
+class TestFull:
+    def test_output_schema(self):
+        json_files = [
+            os.path.join(EXAMPLES_DIR, fname)
+            for fname in os.listdir(EXAMPLES_DIR)
+            if fname.endswith(".json") and not fname.endswith(".vl.json")
+        ]
+
+        with open("node_modules/vega-lite/build/vega-lite-schema.json") as sf:
+            schema = json.load(sf)
+
+            for fname in json_files:
+                with open(fname, "r") as f:
+                    query_spec = json.load(f)
+                    input_task = Task.from_cql(query_spec, os.path.dirname(f.name))
+                    program = input_task.to_asp_list()
+                    result = run(program)
+                    validate(result.as_vl(), schema)
