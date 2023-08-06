536aa206d135db4e7b89a9e98af437c2b6b89cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:19 +0000

    Add package plp.Java/0.0.0a1/plp.Java-0.0.0a1.zip

---
+++ packages/plp.Java/plp.Java-0.0.0a1.zip/plp.Java-0.0.0a1/docs/index.rst
@@ -0,0 +1,15 @@
+plp.Java documentation
+======================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   README
+   CHANGES
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
---
+++ packages/plp.Java/plp.Java-0.0.0a1.zip/plp.Java-0.0.0a1/src/plp/Java/__about__.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2018 Adam Karpierz
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+__all__ = ('__title__', '__summary__', '__uri__', '__version_info__',
+           '__version__', '__author__', '__maintainer__', '__email__',
+           '__copyright__', '__license__')
+
+__title__        = "plp.Java"
+__summary__      = "Parser for Java programming language."
+__uri__          = "http://pypi.python.org/pypi/plp.Java/"
+__version_info__ = type("version_info", (), dict(serial=1,
+                        major=0, minor=0, micro=0, releaselevel="alpha"))
+__version__      = "{0.major}.{0.minor}.{0.micro}{1}{2}".format(__version_info__,
+                   dict(final="", alpha="a", beta="b", rc="rc")[__version_info__.releaselevel],
+                   "" if __version_info__.releaselevel == "final" else __version_info__.serial)
+__author__       = "Adam Karpierz"
+__maintainer__   = "Adam Karpierz"
+__email__        = "adam@karpierz.net"
+__copyright__    = "Copyright 2003-2018 {0}".format(__author__)
+__license__      = "Apache License, Version 2.0 ; {0}".format(
+                   "http://www.apache.org/licenses/LICENSE-2.0")
