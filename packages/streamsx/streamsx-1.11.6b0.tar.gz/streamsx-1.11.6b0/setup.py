9e6380aea6b9421472312b916d015ecdbe049ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:14 +0000

    Add package streamsx/1.11.6b0/streamsx-1.11.6b0.tar.gz

---
+++ packages/streamsx/streamsx-1.11.6b0.tar.gz/streamsx-1.11.6b0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+setup(
+  name = 'streamsx',
+  packages = ['streamsx', 'streamsx.spl', 'streamsx.topology', 'streamsx.scripts', 'streamsx._streams'],
+  include_package_data=True,
+  version = '1.11.6b',
+  description = 'IBM Streams Python Support',
+  long_description = open('DESC.txt').read(),
+  author = 'IBM Streams @ github.com',
+  author_email = 'debrunne@us.ibm.com',
+  license='Apache License - Version 2.0',
+  url = 'https://github.com/IBMStreams/pypi.streamsx',
+  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/1.11.6b',
+  keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  install_requires=['requests', 'future', 'dill', 'enum34'],
+  entry_points = {
+        'console_scripts': [
+            'streamsx-runner=streamsx.scripts.runner:main',
+            'streams-service=streamsx.scripts.service:main',
+            'spl-python-extract=streamsx.scripts.extract:main'
+        ],
+  },
+)
