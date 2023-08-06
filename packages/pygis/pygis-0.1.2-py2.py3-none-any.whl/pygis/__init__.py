d1b04d2cfdf14c15189da58bb383aaa60526f414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package pygis/0.1.2/pygis-0.1.2-py2.py3-none-any.whl

---
+++ packages/pygis/pygis-0.1.2-py2.py3-none-any.whl/pygis/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pygis."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = 'giswqs@gmail.com'
+__version__ = '0.1.2'
+
+from .zipfile import *
+from .download import download_from_gdrive, download_from_url
