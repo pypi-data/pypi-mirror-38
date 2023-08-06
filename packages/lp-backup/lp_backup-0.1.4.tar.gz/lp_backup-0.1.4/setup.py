2fc7f91dd8d6650d41fc5891fca44b72c2e4a267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:20 +0000

    Add package lp-backup/0.1.4/lp_backup-0.1.4.tar.gz

---
+++ packages/lp-backup/lp_backup-0.1.4.tar.gz/lp_backup-0.1.4/setup.py
@@ -0,0 +1,38 @@
+from os import path
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+VERS = '0.1.4'
+DESC = 'Script to create local backups from Lastpass'
+
+with open(path.join(HERE, 'README.md'), 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='lp_backup',
+    version=VERS,
+
+    description=DESC,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/rickh94/lp_backup',
+    author='Rick Henry',
+    author_email='fredericmhenry@gmail.com',
+
+    license='MIT',
+    python_requires='>=3.6',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'boto3',
+        'fs==2.1.2',
+        'fs.s3fs',
+        'ruamel.yaml',
+        'cryptography',
+    ],
+    tests_require=['pytest', 'pytest-cov', 'freezegun'],
+    # setup_requires=['pytest-runner'],
+
+)
