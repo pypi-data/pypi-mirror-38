0b18393fc068583d90fcb917356e62ced6028a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:59 +0000

    Add package gitanalysis/1.0.0/gitanalysis-1.0.0.tar.gz

---
+++ packages/gitanalysis/gitanalysis-1.0.0.tar.gz/gitanalysis-1.0.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='gitanalysis',
+    version='1.0.0',
+    url='https://github.com/FabienArcellier/gitanalysis',
+    author='Fabien Arcellier',
+    author_email='fabien.arcellier@gmail.com',
+    packages=find_packages(exclude=["*_tests"]),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires = [
+        'click',
+        'pandas'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'gitanalysis = gitanalysis.app.cli:cli',
+        ],
+    },
+    extras_require={
+        'dev': [
+            'pylint',
+            'coverage',
+            'tox',
+            'twine'
+        ]
+    }
+)
