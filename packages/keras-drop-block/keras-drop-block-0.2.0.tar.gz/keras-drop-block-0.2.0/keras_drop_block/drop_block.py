d3d0cdeedc1a71c513e5dc337100069d98d7654f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:42 +0000

    Add package keras-drop-block/0.2.0/keras-drop-block-0.2.0.tar.gz

---
+++ packages/keras-drop-block/keras-drop-block-0.2.0.tar.gz/keras-drop-block-0.2.0/keras_drop_block/drop_block.py
@@ -0,0 +1,153 @@
+import keras
+import keras.backend as K
+
+
+class DropBlock1D(keras.layers.Layer):
+    """See: https://arxiv.org/pdf/1810.12890.pdf"""
+
+    def __init__(self,
+                 block_size,
+                 keep_prob,
+                 sync_channels=False,
+                 data_format=None,
+                 **kwargs):
+        """Initialize the layer.
+
+        :param block_size: Size for each mask block.
+        :param keep_prob: Probability of keeping the original feature.
+        :param sync_channels: Whether to use the same dropout for all channels.
+        :param data_format: 'channels_first' or 'channels_last' (default).
+        :param kwargs: Arguments for parent class.
+        """
+        super(DropBlock1D, self).__init__(**kwargs)
+        self.block_size = block_size
+        self.keep_prob = keep_prob
+        self.sync_channels = sync_channels
+        self.data_format = K.normalize_data_format(data_format)
+        self.input_spec = keras.engine.base_layer.InputSpec(ndim=3)
+        self.supports_masking = True
+
+    def get_config(self):
+        config = {'block_size': self.block_size,
+                  'keep_prob': self.keep_prob,
+                  'sync_channels': self.sync_channels,
+                  'data_format': self.data_format}
+        base_config = super(DropBlock1D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_mask(self, inputs, mask=None):
+        return mask
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def _get_gamma(self, feature_dim):
+        """Get the number of activation units to drop"""
+        feature_dim = K.cast(feature_dim, K.floatx())
+        block_size = K.constant(self.block_size, dtype=K.floatx())
+        return ((1.0 - self.keep_prob) / block_size) * (feature_dim / (feature_dim - block_size + 1.0))
+
+    def _compute_drop_mask(self, shape):
+        mask = K.random_binomial(shape, p=self._get_gamma(shape[1]))
+        mask = keras.layers.MaxPool1D(
+            pool_size=self.block_size,
+            padding='same',
+            strides=1,
+            data_format='channels_last',
+        )(mask)
+        return 1.0 - mask
+
+    def call(self, inputs, training=None):
+
+        def dropped_inputs():
+            outputs = inputs
+            if self.data_format == 'channels_first':
+                outputs = K.permute_dimensions(outputs, [0, 2, 1])
+            shape = K.shape(outputs)
+            if self.sync_channels:
+                mask = self._compute_drop_mask([shape[0], shape[1], 1])
+            else:
+                mask = self._compute_drop_mask(shape)
+            outputs = outputs * mask *\
+                (K.cast(K.prod(shape), dtype=K.floatx()) / K.sum(mask))
+            if self.data_format == 'channels_first':
+                outputs = K.permute_dimensions(outputs, [0, 2, 1])
+            return outputs
+
+        return K.in_train_phase(dropped_inputs, inputs, training=training)
+
+
+class DropBlock2D(keras.layers.Layer):
+    """See: https://arxiv.org/pdf/1810.12890.pdf"""
+
+    def __init__(self,
+                 block_size,
+                 keep_prob,
+                 sync_channels=False,
+                 data_format=None,
+                 **kwargs):
+        """Initialize the layer.
+
+        :param block_size: Size for each mask block.
+        :param keep_prob: Probability of keeping the original feature.
+        :param sync_channels: Whether to use the same dropout for all channels.
+        :param data_format: 'channels_first' or 'channels_last' (default).
+        :param kwargs: Arguments for parent class.
+        """
+        super(DropBlock2D, self).__init__(**kwargs)
+        self.block_size = block_size
+        self.keep_prob = keep_prob
+        self.sync_channels = sync_channels
+        self.data_format = K.normalize_data_format(data_format)
+        self.input_spec = keras.engine.base_layer.InputSpec(ndim=4)
+        self.supports_masking = True
+
+    def get_config(self):
+        config = {'block_size': self.block_size,
+                  'keep_prob': self.keep_prob,
+                  'sync_channels': self.sync_channels,
+                  'data_format': self.data_format}
+        base_config = super(DropBlock2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_mask(self, inputs, mask=None):
+        return mask
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def _get_gamma(self, height, width):
+        """Get the number of activation units to drop"""
+        height, width = K.cast(height, K.floatx()), K.cast(width, K.floatx())
+        block_size = K.constant(self.block_size, dtype=K.floatx())
+        return ((1.0 - self.keep_prob) / (block_size ** 2)) *\
+               (height * width / ((height - block_size + 1.0) * (width - block_size + 1.0)))
+
+    def _compute_drop_mask(self, shape):
+        mask = K.random_binomial(shape, p=self._get_gamma(shape[1], shape[2]))
+        mask = keras.layers.MaxPool2D(
+            pool_size=(self.block_size, self.block_size),
+            padding='same',
+            strides=1,
+            data_format='channels_last',
+        )(mask)
+        return 1.0 - mask
+
+    def call(self, inputs, training=None):
+
+        def dropped_inputs():
+            outputs = inputs
+            if self.data_format == 'channels_first':
+                outputs = K.permute_dimensions(outputs, [0, 2, 3, 1])
+            shape = K.shape(outputs)
+            if self.sync_channels:
+                mask = self._compute_drop_mask([shape[0], shape[1], shape[2], 1])
+            else:
+                mask = self._compute_drop_mask(shape)
+            outputs = outputs * mask *\
+                (K.cast(K.prod(shape), dtype=K.floatx()) / K.sum(mask))
+            if self.data_format == 'channels_first':
+                outputs = K.permute_dimensions(outputs, [0, 3, 1, 2])
+            return outputs
+
+        return K.in_train_phase(dropped_inputs, inputs, training=training)
