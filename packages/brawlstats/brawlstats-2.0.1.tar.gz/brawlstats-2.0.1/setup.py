0df2b34642e52e6706308f074f1d5bfcec679bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:03 +0000

    Add package brawlstats/2.0.1/brawlstats-2.0.1.tar.gz

---
+++ packages/brawlstats/brawlstats-2.0.1.tar.gz/brawlstats-2.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brawlstats',
+    version='2.0.1',
+    description='An async Python API wrapper for the unofficial Brawl Stars API',
+    long_description='This package include an easy to use async Client to get Brawl Stars player and band statistics, and the leaderboard. Github: https://github.com/SharpBit/brawlstats',
+    url='https://github.com/SharpBit/brawlstats',
+    author='SharpBit',
+    author_email='sharpbit3618@gmail.com',
+    license='MIT',
+    keywords=['brawl stars, brawlstats, api-wrapper, async'],
+    packages=find_packages(),
+    install_requires=['aiohttp', 'python-box']
+)
