eebe0ec5c41eb12838687ca14e8c4808193c716b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:09 +0000

    Add package emannotationschemas/1.0.9/emannotationschemas-1.0.9.tar.gz

---
+++ packages/emannotationschemas/emannotationschemas-1.0.9.tar.gz/emannotationschemas-1.0.9/test/test_models.py
@@ -0,0 +1,33 @@
+from emannotationschemas.models import make_all_models, InvalidSchemaField
+from emannotationschemas.models import make_annotation_model_from_schema
+from emannotationschemas.models import Base
+import pytest
+import marshmallow as mm
+
+
+def test_model_creation():
+    model_dict = make_all_models(['test', 'test2'], [('synapse', 'synapse')], include_contacts=True)
+    model = model_dict['test']['synapse']
+    assert(model.__name__ == "TestSynapse")
+    model = model_dict['test2']['synapse']
+    assert(model.__name__ == "Test2Synapse")
+    model = model_dict['test']['cellsegment']
+    assert(model.__name__ == "TestCellSegment")
+    model = model_dict['test']['contact']
+    assert(model.__name__ == "TestContact")
+
+    assert(issubclass(model, Base))
+    # TODO better tests here
+
+
+def test_model_failure():
+    class NestSchema(mm.Schema):
+        a = mm.fields.Email()
+
+    class BadSchema(mm.Schema):
+        nest = mm.fields.Nested(NestSchema, many=True)
+
+    with pytest.raises(InvalidSchemaField):
+        make_annotation_model_from_schema('test', 'badschema', BadSchema)
+    with pytest.raises(InvalidSchemaField):
+        make_annotation_model_from_schema('test', 'nestschema', NestSchema)
