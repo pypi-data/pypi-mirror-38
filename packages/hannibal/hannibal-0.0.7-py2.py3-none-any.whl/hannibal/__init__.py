1ffcfef75ccf0b2cf1fb3a434224c2eb057f6295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:25 +0000

    Add package hannibal/0.0.7/hannibal-0.0.7-py2.py3-none-any.whl

---
+++ packages/hannibal/hannibal-0.0.7-py2.py3-none-any.whl/hannibal/__init__.py
@@ -0,0 +1,3 @@
+from hannibal.parser import IncreasingParser, BaseParser
+from hannibal.spider import DistributeCollector, LocalCollector
+from hannibal.util import RedisQueue, RedisHrefPool, MemPool, MemQueue, TrickHelper, extract_json, extract_html, Mission
