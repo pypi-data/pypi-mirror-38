96bdaf6b022f3e5a27f281b5e12af3d81c43c34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:47 +0000

    Add package plpy-ext/0.0.7/plpy_ext-0.0.7.tar.gz

---
+++ packages/plpy-ext/plpy_ext-0.0.7.tar.gz/plpy_ext-0.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+description = 'Some extensions for plpython3u.'
+
+setuptools.setup(
+    name='plpy_ext',
+    version='0.0.7',
+    author='Filantus',
+    author_email='filantus@mail.ru',
+    description=description,
+    long_description=description,
+    py_modules=['plpy_ext'],
+    packages=setuptools.find_packages(),
+    license='GPL',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+    ],
+)
