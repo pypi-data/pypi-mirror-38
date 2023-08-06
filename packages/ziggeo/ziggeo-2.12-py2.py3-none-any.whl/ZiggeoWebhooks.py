6e0220e33032704c43bfb5bb2ddb9ee1e012412a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:42 +0000

    Add package ziggeo/2.12/ziggeo-2.12-py2.py3-none-any.whl

---
+++ packages/ziggeo/ziggeo-2.12-py2.py3-none-any.whl/ZiggeoWebhooks.py
@@ -0,0 +1,11 @@
+class ZiggeoWebhooks:
+
+    def __init__(self, application):
+        self.__application = application
+
+    def create(self, data = None):
+        return self.__application.connect.post('/v1/api/hook', data)
+
+    def delete(self, data = None):
+        return self.__application.connect.post('/v1/api/removehook', data)
+
