32e8dde91d5681fabd1b26a95c94faf0bb99d4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:08 +0000

    Add package django-admitad/0.0.1/django-admitad-0.0.1.tar.gz

---
+++ packages/django-admitad/django-admitad-0.0.1.tar.gz/django-admitad-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-admitad',
+    version='0.0.1',
+    packages=['admitad'],
+    url='https://github.com/k0t3n/django-admitad',
+    license='MIT License',
+    author='k0t3n',
+    author_email='k0t3n.mail@gmail.com',
+    description='Django application that integrates Admitad CPA using postback requests'
+)
