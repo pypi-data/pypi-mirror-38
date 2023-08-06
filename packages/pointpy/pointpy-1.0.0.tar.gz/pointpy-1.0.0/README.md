7e51dbaf05d804019aae37f7186934aac3eea9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:35 +0000

    Add package pointpy/1.0.0/pointpy-1.0.0.tar.gz

---
+++ packages/pointpy/pointpy-1.0.0.tar.gz/pointpy-1.0.0/README.md
@@ -0,0 +1,4 @@
+# pointpy
+Small api client for point. More info at https://api.minut.com/v1/docs/
+
+Code inspired from https://github.com/plamere/spotipy
---
+++ packages/pointpy/pointpy-1.0.0.tar.gz/pointpy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pointpy',
+    version='1.0.0',
+    description='point client',
+    author="@gyran",
+    author_email="gustav.ahlberg@gmail.com",
+    license='LICENSE.txt',
+    packages=['pointpy']
+)
