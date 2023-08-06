c261a0b1db7cac3584ba1e5b5ce0b4e23190b81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:33 +0000

    Add package manyMathFunctions/0.0.7.9/manyMathFunctions-0.0.7.9.tar.gz

---
+++ packages/manyMathFunctions/manyMathFunctions-0.0.7.9.tar.gz/manyMathFunctions-0.0.7.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+desc = open('readme.md', 'r')
+
+setup(
+    name="manyMathFunctions",
+    version="0.0.7.9",
+    author="TheOnlyWalrus",
+    description="See long description / README.md",
+    long_description=desc.read(),
+    long_description_content_type="text/markdown",
+    url="https://manymathfunctions.readthedocs.io/en/latest/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Education",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Education",
+        "Natural Language :: English",
+    ],
+    install_requires=[
+        "matplotlib>=3.0.0",
+    ],
+)
