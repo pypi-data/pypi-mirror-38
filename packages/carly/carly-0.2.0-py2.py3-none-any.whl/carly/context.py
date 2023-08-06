8bfd8bfdcf7ace4d118c34bc673e93adf00eab18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:42 +0000

    Add package carly/0.2.0/carly-0.2.0-py2.py3-none-any.whl

---
+++ packages/carly/carly-0.2.0-py2.py3-none-any.whl/carly/context.py
@@ -0,0 +1,76 @@
+from functools import partial
+
+from collections import namedtuple
+
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks, gatherResults, maybeDeferred
+from twisted.internet.protocol import Factory, ClientFactory
+
+from .hook import hook
+from .timeout import resolveTimeout
+
+TCPServer = namedtuple('TCPServer', ['protocolClass', 'port'])
+TCPClient = namedtuple('TCPClient', ['protocolClass', 'connection', 'protocol'])
+
+
+class Context(object):
+
+    def __init__(self):
+        self.cleanups = {
+            'connections': [],
+            'listens': [],
+        }
+
+    def _cleanup(self, cleanups, timeout):
+        deferreds = []
+        for p in cleanups:
+            d = p()
+            deferreds.append(d)
+            d.addTimeout(timeout, reactor)
+        return gatherResults(deferreds)
+
+    @inlineCallbacks
+    def cleanup(self, timeout=None):
+        timeout = resolveTimeout(timeout)
+        yield self._cleanup(self.cleanups['connections'], timeout)
+        yield self._cleanup(self.cleanups['listens'], timeout)
+
+    def makeTCPServer(self, protocol, factory=None):
+        protocolClass = hook(protocol, 'connectionLost')
+        if factory is None:
+            factory = Factory()
+        factory.protocol = protocolClass
+        port = reactor.listenTCP(0, factory)
+        server = TCPServer(protocolClass, port)
+        self.cleanupTCPServer(server)
+        return server
+
+    def cleanupTCPServer(self, server, timeout=None):
+        timeout = resolveTimeout(timeout)
+        self.cleanups['connections'].append(
+            partial(server.protocolClass.connectionLost.called, timeout=timeout)
+        )
+        self.cleanups['listens'].append(
+            partial(maybeDeferred, server.port.stopListening)
+        )
+
+    def makeTCPClient(self, protocol, port, factory=None):
+        protocolClass = hook(protocol, 'connectionMade', 'connectionLost')
+        if factory is None:
+            factory = ClientFactory()
+        factory.protocol = protocolClass
+        connection = reactor.connectTCP('localhost', port.getHost().port, factory)
+        client = TCPClient(
+            protocolClass, connection, protocolClass.connectionMade.protocol()
+        )
+        self.cleanupTCPClient(client)
+        return client
+
+    def cleanupTCPClient(self, client, timeout=None):
+        timeout = resolveTimeout(timeout)
+        self.cleanups['connections'].extend((
+            partial(maybeDeferred, client.connection.disconnect),
+            partial(client.protocolClass.connectionLost.called, timeout=timeout),
+        ))
+
+
