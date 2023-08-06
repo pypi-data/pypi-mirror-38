324043c0f28f5198d329e7283779c91268cd65b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:07 +0000

    Add package flask-react-simple/0.1.2/flask-react-simple-0.1.2.tar.gz

---
+++ packages/flask-react-simple/flask-react-simple-0.1.2.tar.gz/flask-react-simple-0.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+
+from setuptools import setup
+
+setup(
+    name='flask-react-simple',
+    version='0.1.2',
+    packages=['flask_reactjs'],
+    license='LICENSE.txt',
+    long_description=open('README.rst', encoding='utf-8').read(),
+    author='aweffr',
+    author_email='aweffr@foxmail.com',
+    url='https://github.com/aweffr/flask-react-simple',
+    python_requires='>=3.6',
+    include_package_data=True,  # specify data files to be included in your packages
+    install_requires=[
+        "flask>=1.0.2",
+        "click>=7.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'cra = flask_reactjs:create'
+        ]
+    }
+)
