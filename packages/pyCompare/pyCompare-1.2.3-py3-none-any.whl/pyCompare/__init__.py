5e1102b813bc76734752e2b943e232df0637bb48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:34 +0000

    Add package pyCompare/1.2.3/pyCompare-1.2.3-py3-none-any.whl

---
+++ packages/pyCompare/pyCompare-1.2.3-py3-none-any.whl/pyCompare/__init__.py
@@ -0,0 +1,9 @@
+"""
+`pyCompare` implements functions for generating Bland-Altman plots of agreement between measurements.
+"""
+
+from ._plotBlandAltman import blandAltman
+
+__version__ = '1.2.0'
+
+__all__ = ['blandAltman']
