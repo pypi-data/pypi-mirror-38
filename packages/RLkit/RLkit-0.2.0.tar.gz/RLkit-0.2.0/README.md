095cefbb4f4fdc7a2f85a5976f112271cc4c4f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:40 +0000

    Add package RLkit/0.2.0/RLkit-0.2.0.tar.gz

---
+++ packages/RLkit/RLkit-0.2.0.tar.gz/RLkit-0.2.0/README.md
@@ -0,0 +1,32 @@
+# RLkit: A simple Reinforcement Learning library
+
+This project is still a work in progress. More algorithms and detailed documentation coming soon :)
+
+Currently supported agents-
+
+1. Random agent
+2. REINFORCE (Policy Gradients)
+3. DQN
+4. DQN with baseline
+5. Actor-Critic
+
+See examples for details on how to use the library.
+
+Requirements-
+```
+tensorflow==1.11.0
+gym==0.10.8
+numpy==1.15.4
+```
+
+## New in v0.2
+- Added DQN and DQN with baseline agents
+- Added ActorCritic agent
+- Added support for various activation functions
+
+
+## Upcoming
+- Duelling DQN
+- Support for logging and plotting
+- Support for adding seeds
+- Support for custom environments
---
+++ packages/RLkit/RLkit-0.2.0.tar.gz/RLkit-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='RLkit',
+      version='0.2.0',
+      description='A simple RL library.',
+      url='http://github.com/shubhamjha97/RLkit',
+      author='Shubham Jha',
+      author_email='jha1shubham@gmail.com',
+      license='MIT',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      install_requires=[
+        'tensorflow==1.11.0',
+		    'gym==0.10.8',
+		    'ipdb==0.11',
+		    'numpy==1.15.4'
+    	],
+      packages=find_packages(),
+      classifiers = ('Intended Audience :: Science/Research', 'Natural Language :: English', 'Programming Language :: Python :: 3.6'),
+      zip_safe=False)
