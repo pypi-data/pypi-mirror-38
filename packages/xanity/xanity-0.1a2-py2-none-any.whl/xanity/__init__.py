801f80d605d52527db11584059b36af7c9180637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:16 +0000

    Add package xanity/0.1a2/xanity-0.1a2-py2-none-any.whl

---
+++ packages/xanity/xanity-0.1a2-py2-none-any.whl/xanity/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from . import common
+
+if 'xanity' not in locals():
+    xanity = common.Xanity()
+
+def log(message):
+    xanity.log(message)
+
+def save_variable(name, value):
+    xanity.save_variable(name, value)
+
+def run_hook():
+    xanity.run_all_exps()
+    
+#def run():
+#    # check whether you're in a xanity path
+#    inxanity=True if os.path.exists('.xanity') else False
+#        
+#    # look for .xanity/.runlock
+#    xanity_running=True if os.path.exists('.xanity/.runlock') else False
+#    
+#    if xanity_running:
+#        #load metadata
+#        with open('.xanity/.runlock','rb') as mdfile:
+#            metadata = dill.load(mdfile)
+#        pass
+#            
+#    else:
+#        sys.path.append('.xanity')
+#        import run_experiments
+#        run_experiments(__name__)
