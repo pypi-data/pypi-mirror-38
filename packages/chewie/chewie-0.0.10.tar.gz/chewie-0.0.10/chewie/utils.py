7914a5758896d7a1a569a66ce0e6b8a210aa5119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:44 +0000

    Add package chewie/0.0.10/chewie-0.0.10.tar.gz

---
+++ packages/chewie/chewie-0.0.10.tar.gz/chewie-0.0.10/chewie/utils.py
@@ -0,0 +1,22 @@
+"""Utility Functions"""
+import logging
+
+
+def get_logger(logname):
+    """Create and return a logger object."""
+    logger = logging.getLogger(logname)
+    return logger
+
+
+def log_method(method):
+    """Generate method for logging"""
+    def wrapped(self, *args, **kwargs):
+        """Method that gets called for logging"""
+        self.logger.info('Entering %s' % method.__name__)
+        return method(self, *args, **kwargs)
+    return wrapped
+
+
+class MessageParseError(Exception):
+    """Error for when parsing cannot be successfully completed."""
+    pass
