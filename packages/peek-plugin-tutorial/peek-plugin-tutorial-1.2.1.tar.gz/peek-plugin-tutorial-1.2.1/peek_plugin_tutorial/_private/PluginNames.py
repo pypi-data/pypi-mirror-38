3bf11b89225f3384b693f6c5b6f087f87870c1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:15 +0000

    Add package peek-plugin-tutorial/1.2.1/peek-plugin-tutorial-1.2.1.tar.gz

---
+++ packages/peek-plugin-tutorial/peek-plugin-tutorial-1.2.1.tar.gz/peek-plugin-tutorial-1.2.1/peek_plugin_tutorial/_private/PluginNames.py
@@ -0,0 +1,5 @@
+tutorialFilt = {"plugin": "peek_plugin_tutorial"}
+tutorialTuplePrefix = "peek_plugin_tutorial."
+tutorialObservableName = "peek_plugin_tutorial"
+tutorialActionProcessorName = "peek_plugin_tutorial"
+tutorialTupleOfflineServiceName = "peek_plugin_tutorial"
