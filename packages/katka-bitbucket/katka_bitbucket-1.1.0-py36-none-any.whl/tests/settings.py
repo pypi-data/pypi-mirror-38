f453ff07452965a4438adf993400335e9b0d3f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:35 +0000

    Add package katka-bitbucket/1.1.0/katka_bitbucket-1.1.0-py36-none-any.whl

---
+++ packages/katka-bitbucket/katka_bitbucket-1.1.0-py36-none-any.whl/tests/settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 't)c0g-tbhgoe2ybv4(iyj%!07*si)co@rg21f&ejm3v=)u^_-8'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'tests.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'bitbucket',
+]
+
+MIDDLEWARE = ()
