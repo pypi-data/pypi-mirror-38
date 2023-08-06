4f4402f519debe154162c25f5342e954a69c5116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:13 +0000

    Add package docsend/0.1.3/docsend-0.1.3-py3-none-any.whl

---
+++ packages/docsend/docsend-0.1.3-py3-none-any.whl/docsend/cli.py
@@ -0,0 +1,27 @@
+import click
+
+from docsend import DocSend
+
+
+@click.command()
+@click.argument('doc_id')
+@click.option('-e', '--email', default=None, help='Email to authorize with')
+@click.option('-f', '--format', type=click.Choice(['pdf', 'png']), default='pdf', help='Save as PDF (default) or image sequence')
+@click.option('-o', '--output', default=None, help='Output file name')
+def main(doc_id, email, format, output):
+    ds = DocSend(doc_id)
+    ds.fetch_meta()
+    if email:
+        ds.authorize(email)
+    ds.fetch_images()
+    if output is None:
+        output = f'docsend_{doc_id}.pdf' if format == 'pdf' else f'docsend_{doc_id}'
+    if format == 'pdf':
+        ds.save_pdf(output)
+    elif format == 'png':
+        ds.save_images(output)
+    print(f'saved to {output}')
+
+
+if __name__ == '__main__':
+    main()
