869e7956513e8a80d5443d9a1dad0b9560cac3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:17 +0000

    Add package BUPT/10.0.1/BUPT-10.0.1.tar.gz

---
+++ packages/BUPT/BUPT-10.0.1.tar.gz/BUPT-10.0.1/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='BUPT',
+    version='10.0.1',
+    author='BUPTSICE',
+    author_email='sice@bupt.edu.cn',
+    url='https://sice.bupt.edu.cn/',
+    description='信息与通信工程学院',
+    packages=['BUPT'],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
