bf97c959a9a717b356b4e6c7653139cda386ee62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:34 +0000

    Add package pyCompare/1.2.3/pyCompare-1.2.3.tar.gz

---
+++ packages/pyCompare/pyCompare-1.2.3.tar.gz/pyCompare-1.2.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(name='pyCompare',
+	version='1.2.3',
+	description='Bland-Altman plots for Python',
+	url='https://github.com/jaketmp/pyCompare',
+	author='Jake TM Pearce',
+	license='MIT',
+	packages=find_packages(),
+	install_requires=[
+		'numpy>=1.14.2',
+		'scipy>=1.0.1',
+		'matplotlib>=2.2.2',
+	],
+	classifiers = [
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering :: Bio-Informatics",
+		"Topic :: Scientific/Engineering :: Visualization",
+		],
+	long_description = """\
+		.. image:: https://travis-ci.org/jaketmp/pyCompare.svg?branch=master
+		   :target: https://travis-ci.org/jaketmp/pyCompare
+		   :alt: Travis CI build status
+
+		.. image:: https://codecov.io/gh/jaketmp/pyCompare/branch/master/graph/badge.svg
+		   :target: https://codecov.io/gh/jaketmp/pyCompare
+		   :alt: Test coverage
+
+		.. image:: https://zenodo.org/badge/DOI/10.5281/zenodo.1238915.svg
+		   :target: https://doi.org/10.5281/zenodo.1238915
+		   :alt: Zenodo DOI
+
+		A Python module for generating `Bland-Altman <https://en.wikipedia.org/wiki/Bland–Altman_plot>`_ plots to compare two sets of measurements.
+
+		For documentation see the `project page <https://github.com/jaketmp/pyCompare>`_ on GitHub.
+		""",
+	documentation='https://github.com/jaketmp/pyCompare',
+	include_package_data=True,
+	zip_safe=True
+	)
