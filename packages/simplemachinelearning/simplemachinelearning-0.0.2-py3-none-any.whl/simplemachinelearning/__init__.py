e49bf1d3d364250e67535d111a8218ef63d2fae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:33 +0000

    Add package simplemachinelearning/0.0.2/simplemachinelearning-0.0.2-py3-none-any.whl

---
+++ packages/simplemachinelearning/simplemachinelearning-0.0.2-py3-none-any.whl/simplemachinelearning/__init__.py
@@ -0,0 +1,15 @@
+from simplemachinelearning.Class_Data_Modeler import DataModeler
+
+'''
+import pandas as pd
+from scipy.special import boxcox
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+# from xgboost import XGBClassifier
+'''
