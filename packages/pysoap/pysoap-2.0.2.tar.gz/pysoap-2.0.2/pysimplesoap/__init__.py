eed83144cde8b8c81c519fd29cb8aae445815b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:44 +0000

    Add package pysoap/2.0.2/pysoap-2.0.2.tar.gz

---
+++ packages/pysoap/pysoap-2.0.2.tar.gz/pysoap-2.0.2/pysimplesoap/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""PySimpleSOAP"""
+
+
+__author__ = "Vcancy"
+__author_email__ = "happiness1019@gmail.com"
+__copyright__ = "Copyright (C) 2018 Vcancy"
+__license__ = "LGPL 3.0"
+__version__ = "2.0.2"
+
+TIMEOUT = 60
+
+
+from . import client, server, simplexml, transport, helpers
