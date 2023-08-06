840872099309acb8a37f6333214085d99b9a765e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:11 +0000

    Add package pyprobables/0.2.5/pyprobables-0.2.5-py2.py3-none-any.whl

---
+++ packages/pyprobables/pyprobables-0.2.5-py2.py3-none-any.whl/probables/__init__.py
@@ -0,0 +1,25 @@
+''' pyprobables module '''
+from __future__ import (unicode_literals, absolute_import, print_function)
+from . blooms import (BloomFilter, BloomFilterOnDisk, CountingBloomFilter,
+                      ExpandingBloomFilter)
+from . countminsketch import (CountMinSketch, HeavyHitters, StreamThreshold,
+                              CountMeanSketch, CountMeanMinSketch)
+from . cuckoo import (CuckooFilter, CountingCuckooFilter)
+from . exceptions import (InitializationError, NotSupportedError,
+                          ProbablesBaseException, CuckooFilterFullError)
+
+__author__ = 'Tyler Barrus'
+__maintainer__ = 'Tyler Barrus'
+__email__ = 'barrust@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.2.5'
+__credits__ = []
+__url__ = 'https://github.com/barrust/pyprobables'
+__bugtrack_url__ = 'https://github.com/barrust/pyprobables/issues'
+
+__all__ = ['BloomFilter', 'BloomFilterOnDisk', 'CountingBloomFilter',
+           'CountMinSketch', 'CountMeanSketch', 'CountMeanMinSketch',
+           'HeavyHitters', 'StreamThreshold', 'CuckooFilter',
+           'CountingCuckooFilter', 'InitializationError', 'NotSupportedError',
+           'ProbablesBaseException', 'CuckooFilterFullError',
+           'ExpandingBloomFilter']
