6f9eb61949620fe81537ca8f23103b1614b5e7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:56 +0000

    Add package suggestsoda/0.1.3/suggestsoda-0.1.3.tar.gz

---
+++ packages/suggestsoda/suggestsoda-0.1.3.tar.gz/suggestsoda-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='suggestsoda',
+      version='0.1.3',
+      author='Daniel J Brooks',
+      author_email='daniel.brooks@alumni.caltech.edu',
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      packages=['suggestsoda',],
+      long_description=open("README.rst").read(),
+     )
