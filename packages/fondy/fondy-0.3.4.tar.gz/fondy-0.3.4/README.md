f6aa2c36b88a9de48048d15e7686d630bd28d7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:06 +0000

    Add package fondy/0.3.4/fondy-0.3.4.tar.gz

---
+++ packages/fondy/fondy-0.3.4.tar.gz/fondy-0.3.4/README.md
@@ -0,0 +1,30 @@
+# fondy
+
+Fondy.eu Python SDK
+
+## Install
+
+```shell
+pip install fondy
+```
+
+## Usage example
+
+```python
+from fondy import API
+api = API(merchant_id, merchant_key, server_callback_url)
+# generate Fondy payment url to checkout 200 UAH for "123456" order id
+api.checkout('123456', 200 * 100)
+```
+
+## Supported API endpoints
+
+|Endpoint        |Explanation           |
+|----------------|----------------------|
+|order_status    |Check order status    |
+|p2pcredit       |Pay to credit card    |
+|checkout        |Generate checkout URL |
+
+## Requirements
+
+- requests
---
+++ packages/fondy/fondy-0.3.4.tar.gz/fondy-0.3.4/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fondy',
+    packages=['fondy'],
+    version='0.3.4',
+    description='Python library for the Fondy API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Olexandr Shalakhin',
+    author_email='olexandr@shalakhin.com',
+    url='https://github.com/shalakhin/fondy',
+    download_url='https://github.com/shalakhin/fondy/archive/0.3.4.tar.gz',
+    keywords=['testing', 'logging', 'example'],
+    requires=['requests'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
