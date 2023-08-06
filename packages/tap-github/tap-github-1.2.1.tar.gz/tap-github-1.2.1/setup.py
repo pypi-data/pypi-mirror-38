bdb814cbb274b853ca4096c2fc0c6c582498c26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:49 +0000

    Add package tap-github/1.2.1/tap-github-1.2.1.tar.gz

---
+++ packages/tap-github/tap-github-1.2.1.tar.gz/tap-github-1.2.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-github',
+      version='1.2.1',
+      description='Singer.io tap for extracting data from the GitHub API',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_github'],
+      install_requires=[
+          'singer-python==5.1.0',
+          'requests==2.20.0'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-github=tap_github:main
+      ''',
+      packages=['tap_github'],
+      package_data = {
+          'tap_github': ['tap_github/*.json']
+      },
+      include_package_data=True
+)
