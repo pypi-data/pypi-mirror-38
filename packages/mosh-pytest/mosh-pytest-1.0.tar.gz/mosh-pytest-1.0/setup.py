f6b1eb485647798f8d77f40a433769ca45c59c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:52 +0000

    Add package mosh-pytest/1.0/mosh-pytest-1.0.tar.gz

---
+++ packages/mosh-pytest/mosh-pytest-1.0.tar.gz/mosh-pytest-1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="mosh-pytest",
+    version="1.0",
+    long_description=long_description,
+    packages=[]
+)
