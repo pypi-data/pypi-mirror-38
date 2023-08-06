628c1c0e320f239742992ad18f14ab9c7bcca8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:49 +0000

    Add package spikeforest/0.3.4/spikeforest-0.3.4.tar.gz

---
+++ packages/spikeforest/spikeforest-0.3.4.tar.gz/spikeforest-0.3.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+pkg_name="spikeforest"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.3.4",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="",
+    url="https://github.com/magland/spikeforest",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pybind11',
+        'ml_ms4alg',
+        'numpy',
+        'matplotlib',
+        'spikeinterface',
+        'spikewidgets',
+        'spiketoolkit',
+        'kbucket',
+        'pillow',
+        'mlprocessors',
+        'pandas'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    )
+)
---
+++ packages/spikeforest/spikeforest-0.3.4.tar.gz/spikeforest-0.3.4/spikeforest/__init__.py
@@ -0,0 +1,10 @@
+#from .sfstudy import SFStudy
+#from . import studies
+from .sortdataset import sortDataset, MountainSort4
+from .summarizesorting import summarizeSorting
+from .comparewithtruth import compareWithTruth
+from .kbucketconfig import kbucketConfigLocal, kbucketConfigRemote
+from .sfdata import SFData
+from . import batch
+from . import sorters
+from . import sf_batch
