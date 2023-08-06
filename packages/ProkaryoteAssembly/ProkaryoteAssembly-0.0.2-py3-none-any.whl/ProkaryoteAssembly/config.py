69bea2d4dee735063c689e9a93405eb5cd18004e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:57 +0000

    Add package ProkaryoteAssembly/0.0.2/ProkaryoteAssembly-0.0.2-py3-none-any.whl

---
+++ packages/ProkaryoteAssembly/ProkaryoteAssembly-0.0.2-py3-none-any.whl/ProkaryoteAssembly/config.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.2"
+__author__ = "Forest Dussault"
+__email__ = "forest.dussault@canada.ca"
+
+DEPENDENCIES = [
+    'skesa',
+    'bbduk.sh',
+    'bbmap.sh',
+    'tadpole.sh',
+    'pilon',
+    'samtools'
+]
