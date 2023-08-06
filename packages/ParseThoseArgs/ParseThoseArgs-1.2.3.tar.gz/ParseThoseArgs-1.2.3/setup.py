1894de1b85d1d7d5f2682d019bfbac216ef6e650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:46 +0000

    Add package ParseThoseArgs/1.2.3/ParseThoseArgs-1.2.3.tar.gz

---
+++ packages/ParseThoseArgs/ParseThoseArgs-1.2.3.tar.gz/ParseThoseArgs-1.2.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Extension, find_packages
+
+setup(
+    name="ParseThoseArgs",
+    version="1.2.3",
+    ext_modules=[
+        Extension("_parsethoseargs_c_parser", ['./parsethoseargs/python_wrapper.c'], include_dirs=['./parsethoseargs/']),
+    ],
+    packages=find_packages(exclude=('tests',)),
+    author="Jake Gealer",
+    author_email="jake@auttaja.io",
+    url="https://github.com/JakeMakesStuff/ParseThoseArgs"
+)
+
