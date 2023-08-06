10340796c2a81628713c019888c69c5c4b353d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:35 +0000

    Add package volcano-test/0.3/volcano-test-0.3.tar.gz

---
+++ packages/volcano-test/volcano-test-0.3.tar.gz/volcano-test-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='volcano-test',
+    version='0.3',
+    description='Volcano test kit',
+    author='Vinogradov D',
+    author_email='dgrapes@gmail.com',
+    license='MIT',
+    packages=['volcano.test'],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
