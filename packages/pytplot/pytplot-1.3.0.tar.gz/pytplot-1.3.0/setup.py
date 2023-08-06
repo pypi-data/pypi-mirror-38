bccd6041a45626edcec51b962a13ad098392786d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:22 +0000

    Add package pytplot/1.3.0/pytplot-1.3.0.tar.gz

---
+++ packages/pytplot/pytplot-1.3.0.tar.gz/pytplot-1.3.0/setup.py
@@ -0,0 +1,11 @@
+#
+#To upload the latest version, change "version=0.X.X+1" and type:
+#	python setup.py sdist upload
+#
+#
+#
+
+from setuptools import setup
+
+from setuptools import setup
+setup()
