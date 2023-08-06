033b05c09df9fc988018f028435eb4b5bfc5932a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:12 +0000

    Add package vps/0.0.6/vps-0.0.6.tar.gz

---
+++ packages/vps/vps-0.0.6.tar.gz/vps-0.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vps",
+    version="0.0.6",
+    author="Andrew Schultz",
+    author_email="andrew.schultz@protonmail.com",
+    description="Create and Manage VPS Instances",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/aschult5/vps",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "ovh"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
