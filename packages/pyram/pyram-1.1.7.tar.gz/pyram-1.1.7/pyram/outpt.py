c07c73285688e3cec3c48ee2b2b30d9552262e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:48 +0000

    Add package pyram/1.1.7/pyram-1.1.7.tar.gz

---
+++ packages/pyram/pyram-1.1.7.tar.gz/pyram-1.1.7/pyram/outpt.py
@@ -0,0 +1,30 @@
+'''outpt function definition'''
+
+import numpy
+from numba import jit, int64, float64, complex128
+
+
+@jit(int64[:](float64, int64, int64, int64, int64, float64[:], complex128[:],
+              float64, int64, float64[:], float64[:, :]), nopython=True)
+def outpt(r, mdr, ndr, ndz, tlc, f3, u, _dir, ir, tll, tlg):
+
+    '''Output transmission loss'''
+
+    eps = 1e-20
+
+    mdr += 1
+    if mdr == ndr:
+        mdr = 0
+        tlc += 1
+        ur = (1 - _dir) * f3[ir] * u[ir] + \
+            _dir * f3[ir + 1] * u[ir + 1]
+        temp = 10 * numpy.log10(r + eps)
+        tll[tlc] = -20 * numpy.log10(numpy.abs(ur) + eps) + temp
+
+        for i in range(tlg.shape[0]):
+            j = (i + 1) * ndz
+            ur = u[j] * f3[j]
+            tlg[i, tlc] = \
+                -20 * numpy.log10(numpy.abs(ur) + eps) + temp
+
+    return numpy.array([mdr, tlc], dtype=numpy.int64)
