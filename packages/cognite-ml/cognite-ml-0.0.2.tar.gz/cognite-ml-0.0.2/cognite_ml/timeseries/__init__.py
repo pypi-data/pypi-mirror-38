d32dd13402ff686d67b4a9d1322978b6d4a10925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:26 +0000

    Add package cognite-ml/0.0.2/cognite-ml-0.0.2.tar.gz

---
+++ packages/cognite-ml/cognite-ml-0.0.2.tar.gz/cognite-ml-0.0.2/cognite_ml/timeseries/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+@author: quetran
+"""
+__version__ = "0.0.2"
+
+from .windows import SlidingWindows
+
+from . import windows
+
+__all__ = ["SlidingWindows"]
