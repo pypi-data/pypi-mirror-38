6988af28916ccdd2c724e72fdcc5705d6f8ded17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:48 +0000

    Add package model-picker/0.0.2/model_picker-0.0.2.tar.gz

---
+++ packages/model-picker/model_picker-0.0.2.tar.gz/model_picker-0.0.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name="model_picker",
+    version='0.0.2',
+    author = "Alexandre Clement",
+    author_email = "alexandre.clement@etu.unice.fr",
+    url = "https://github.com/clement-alexandre/TotemBionet",
+    description = "Pick a model",
+    long_description = open("README.rst").read(),
+    install_requires = [],
+    license="WTFPL",
+    include_package_data = True,
+    packages = find_packages(),
+    py_modules = ["model_picker"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords="jupyter, computational systems biology",
+)
