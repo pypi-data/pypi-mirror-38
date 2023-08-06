e0708249d12549de120bfea8fda7b00e93c2c3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:36 +0000

    Add package penpal/0.4.2/penpal-0.4.2-py2.py3-none-any.whl

---
+++ packages/penpal/penpal-0.4.2-py2.py3-none-any.whl/pen/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+pen: terminal notes
+"""
+
+__title__ = 'pen'
+__author__ = 'cwoebker'
+__version__ = '0.4.2'
+__license__ = 'BSD'
+__copyright__ = '© 2013-2018 Cecil Wöbker'
