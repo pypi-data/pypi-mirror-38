8ed17e83254fddc1e969c0f612d4201c3cd3eceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:32 +0000

    Add package xanity/0.1b15/xanity-0.1b15-py2-none-any.whl

---
+++ packages/xanity/xanity-0.1b15-py2-none-any.whl/xanity/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+from .constants import COMMANDS
+
+parser = argparse.ArgumentParser(description='Manage scientific experiments, parameters, and analyses')
+parser.add_argument('action', type=str, nargs=1, help='action for xanity to do. ( {} )'.format(COMMANDS.INIT+COMMANDS.SETUP+COMMANDS.RUN+COMMANDS.ANAL))
+args, remaining_args = parser.parse_known_args()
+
+# print('args: {}   rem_args:{}'.format(args,remaining_args))
+# print('args.action: {}'.format(args.action))
+
+if args.action[0] in COMMANDS.INIT:
+    from . import initialize
+    initialize.main(remaining_args)
+  
+elif args.action[0] in COMMANDS.SETUP:
+    from . import setup
+    setup.main(remaining_args)
+
+elif args.action[0] in COMMANDS.RUN+COMMANDS.ANAL:
+    from . import xanity
+    xanity.run_hook()
+
+else:
+    raise NotImplementedError('xanity did not understand that action')
