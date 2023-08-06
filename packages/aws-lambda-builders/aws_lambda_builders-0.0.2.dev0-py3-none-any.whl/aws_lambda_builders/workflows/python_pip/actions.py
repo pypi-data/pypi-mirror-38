79abee6753e6d32869031e1a129f28591cd69149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:01 +0000

    Add package aws-lambda-builders/0.0.2.dev0/aws_lambda_builders-0.0.2.dev0-py3-none-any.whl

---
+++ packages/aws-lambda-builders/aws_lambda_builders-0.0.2.dev0-py3-none-any.whl/aws_lambda_builders/workflows/python_pip/actions.py
@@ -0,0 +1,29 @@
+"""
+Action to resolve Python dependencies using PIP
+"""
+
+from aws_lambda_builders.actions import BaseAction, Purpose, ActionFailedError
+from .packager import PythonPipDependencyBuilder, PackagerError
+
+
+class PythonPipBuildAction(BaseAction):
+
+    NAME = 'ResolveDependencies'
+    DESCRIPTION = "Installing dependencies from PIP"
+    PURPOSE = Purpose.RESOLVE_DEPENDENCIES
+
+    def __init__(self, artifacts_dir, manifest_path, runtime):
+        self.artifacts_dir = artifacts_dir
+        self.manifest_path = manifest_path
+        self.runtime = runtime
+        self.package_builder = PythonPipDependencyBuilder()
+
+    def execute(self):
+        try:
+            self.package_builder.build_dependencies(
+                self.artifacts_dir,
+                self.manifest_path,
+                self.runtime,
+            )
+        except PackagerError as ex:
+            raise ActionFailedError(str(ex))
