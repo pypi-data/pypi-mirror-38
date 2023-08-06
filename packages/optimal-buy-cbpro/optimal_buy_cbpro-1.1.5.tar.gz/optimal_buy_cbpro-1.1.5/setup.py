d22650bd71560e59d832d48587b6fec9d4c24d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:33 +0000

    Add package optimal-buy-cbpro/1.1.5/optimal_buy_cbpro-1.1.5.tar.gz

---
+++ packages/optimal-buy-cbpro/optimal_buy_cbpro-1.1.5.tar.gz/optimal_buy_cbpro-1.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='optimal_buy_cbpro',
+    version='1.1.5',
+    description='Buy the coins, optimally!',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Brenden Matthews',
+    author_email='brenden@diddyinc.com',
+    url='https://github.com/brndnmtthws/optimal-buy-cbpro',
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts':
+            ['optimal-buy-cbpro=optimal_buy_cbpro.optimal_buy_cbpro:main'],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ),
+)
