db191e1459539a6027f51db29f1783c741fbb62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:41 +0000

    Add package github_watcher/3.0/github_watcher-3.0.tar.gz

---
+++ packages/github_watcher/github_watcher-3.0.tar.gz/github_watcher-3.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+try:
+    from disutils.core import setup
+except ImportError:
+    from setuptools import setup
+
+
+setup(name='github_watcher',
+        version='3.0',
+        description='Monitors files/directories on github and alerts you when someone submits a PR with changes',
+        author='Andrew Kelleher',
+        author_email='keats.kelleher@gmail.com',
+        packages=['github_watcher'],
+        scripts=['github_watcher/github-watcher'],
+        url='https://github.com/akellehe/github-watcher',
+        install_requires=[
+           'unidiff==0.5.4',
+           'requests==2.18.4',
+           'pync==1.6.1',
+           'PyYAML==3.11',
+           'PyGithub==1.43',
+        ])
