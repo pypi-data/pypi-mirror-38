ec14aa393d5fc1829ab8bdd35e1a9a9f8ecc8def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package Docker-Compose-Templer/1.0.2/Docker%20Compose%20Templer-1.0.2.tar.gz

---
+++ packages/Docker-Compose-Templer/Docker%20Compose%20Templer-1.0.2.tar.gz/Docker Compose Templer-1.0.2/docker_compose_templer/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+__author__ = "Andre Lehmann"
+__email__ = "aisberg@posteo.de"
+__version__ = '1.0.2'
+
+from docker_compose_templer.cli import cli
+
+if __name__ == '__main__':
+    cli()
