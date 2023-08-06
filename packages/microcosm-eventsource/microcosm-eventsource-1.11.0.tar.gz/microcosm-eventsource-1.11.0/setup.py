dc821c49267920c7c1bcf97410326a3a06f19fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:30 +0000

    Add package microcosm-eventsource/1.11.0/microcosm-eventsource-1.11.0.tar.gz

---
+++ packages/microcosm-eventsource/microcosm-eventsource-1.11.0.tar.gz/microcosm-eventsource-1.11.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+project = "microcosm-eventsource"
+version = "1.11.0"
+
+setup(
+    name=project,
+    version=version,
+    description="Event-sourced state machines using microcosm",
+    author="Globality Engineering",
+    author_email="engineering@globality.com",
+    url="https://github.com/globality-corp/microcosm-eventsource",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.6",
+    keywords="microcosm",
+    install_requires=[
+        "microcosm>=2.0.0",
+        "microcosm-flask>=1.0.1",
+        "microcosm-logging>=1.0.0",
+        "microcosm-postgres>=1.0.0",
+        "microcosm-pubsub>=1.15.0",
+        "urllib3>=1.23,!=1.24",
+    ],
+    setup_requires=[
+        "nose>=1.3.6",
+    ],
+    dependency_links=[
+    ],
+    entry_points={
+        "microcosm.factories": [
+        ],
+    },
+    tests_require=[
+        "coverage>=3.7.1",
+        "PyHamcrest>=1.9.0",
+    ],
+)
