4a35d7a7f431beb5f6f21a156bb6320df0826bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:09 +0000

    Add package cocotask/0.2.1.1/cocotask-0.2.1.1.tar.gz

---
+++ packages/cocotask/cocotask-0.2.1.1.tar.gz/cocotask-0.2.1.1/cocotask/base_consumer.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+import logging
+default_logger = logging.getLogger(__name__)
+
+class CocoBaseConsumer(ABC):
+    def __init__(self, conf, worker_class, logger = None):
+        self._config = conf
+        self._worker_class = worker_class
+        self._logger = logger if logger else default_logger
+        super().__init__()
+
+
+    @abstractmethod
+    def connect(self):
+        pass
