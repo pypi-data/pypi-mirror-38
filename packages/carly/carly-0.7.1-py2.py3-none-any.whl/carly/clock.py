21c1693a37166400fa9bfc66e02b95df9a81f791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:04 +0000

    Add package carly/0.7.1/carly-0.7.1-py2.py3-none-any.whl

---
+++ packages/carly/carly-0.7.1-py2.py3-none-any.whl/carly/clock.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+
+from sys import stderr
+
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+
+DEFAULT_TIMEOUT = 0.2
+
+
+def withTimeout(deferred, timeout=None):
+    if timeout is None:
+        timeout = DEFAULT_TIMEOUT
+    return deferred.addTimeout(timeout, reactor)
+
+
+def cancelDelayedCalls(expected=2):
+    """
+    :param expected:
+      The number of calls to cancel. If the number found does not match this,
+      none of them will be cancelled so that trial's cleanup can tell you
+      more about them.
+
+      Why the default of 2? Hopefully you're only testing one delayed calll
+      generator at a time, and there's one for trial's 2 minute timeout.
+    """
+    calls = reactor.getDelayedCalls()
+    if len(calls) == expected:
+        for call in calls:
+            call.cancel()
+    else:
+        print('\n\nExpected {} delayed calls, found {}'.format(expected, len(calls)),
+              file=stderr)
+
+
+def advanceTime(seconds):
+    """
+    Advance the reactor time by the number of seconds or partial seconds
+    specified.
+    """
+    now = reactor.seconds()
+    for call in reactor.getDelayedCalls():
+        currentSecondsFromNow = call.getTime() - now
+        newSecondsFromNow = max(0, currentSecondsFromNow - seconds)
+        call.reset(newSecondsFromNow)
+    # give the reactor a chance to run calls we're brought forward:
+    d = Deferred()
+    reactor.callLater(0, lambda: d.callback(None))
+    return d
