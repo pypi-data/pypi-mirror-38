71b136803a78d9684ca669527bf7f8c63d32d4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:19 +0000

    Add package pyort/0.1.7.5.1/pyort-0.1.7.5.1.tar.gz

---
+++ packages/pyort/pyort-0.1.7.5.1.tar.gz/pyort-0.1.7.5.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='pyort',
+    url='https://github.com/Gananath/Pyort',
+    author='Gananath R',
+    author_email='nomail@nomail.com',
+    # Needed to actually package something
+    packages=['pyort'],
+    # Needed for dependencies
+    install_requires=['psutil','argparse','pysqlite ;python_version<"3.0"','pysqlite3 ;python_version>"3.0"'],
+    # *strongly* suggested for sharing
+    version='0.1.7.5.1',
+    
+    entry_points = {
+        'console_scripts': [
+            'pyort=pyort.pyort:main',
+        ],
+    },
+    # The license can be anything you like
+    license='MIT',
+    description='Command line tool for monitoring all network connections',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
