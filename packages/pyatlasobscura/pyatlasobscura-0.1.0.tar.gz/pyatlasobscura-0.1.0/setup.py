02c38d7f74131d78044c963e105ce15f154670e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:11 +0000

    Add package pyatlasobscura/0.1.0/pyatlasobscura-0.1.0.tar.gz

---
+++ packages/pyatlasobscura/pyatlasobscura-0.1.0.tar.gz/pyatlasobscura-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyatlasobscura',
+    version='0.1.0',
+    packages=find_packages(),
+    python_requires='>3.5.0',
+    url='https://github.com/drewsonne/pyatlasobscura',
+    license='LGPLv3',
+    author='Drew J. Sonne',
+    author_email='drew.sonne@gmail.com',
+    description='',
+    install_requires=['requests', 'beautifulsoup4','latlon'],
+    extras_require={
+        'pandas': ['pandas']
+    }
+)
