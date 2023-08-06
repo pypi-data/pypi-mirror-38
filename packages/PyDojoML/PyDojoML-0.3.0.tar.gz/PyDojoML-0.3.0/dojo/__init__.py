d7e3dca2db1311647a528773ed87540e2cc8618a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:35 +0000

    Add package PyDojoML/0.3.0/PyDojoML-0.3.0.tar.gz

---
+++ packages/PyDojoML/PyDojoML-0.3.0.tar.gz/PyDojoML-0.3.0/dojo/__init__.py
@@ -0,0 +1,37 @@
+from . import (
+    anomaly,
+    base,
+    bayes,
+    cluster,
+    dimred,
+    ensemble,
+    linear,
+    metrics,
+    preprocessing,
+    recommender,
+    split,
+    svm,
+    tree,
+    activations,
+    exceptions,
+)
+
+__all__ = [
+    "anomaly",
+    "base",
+    "bayes",
+    "cluster",
+    "dimred",
+    "ensemble",
+    "linear",
+    "metrics",
+    "preprocessing",
+    "recommender",
+    "split",
+    "svm",
+    "tree",
+    "activations",
+    "exceptions",
+]
+
+__version__ = "0.3.0"
