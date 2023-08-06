dd9d48e548538c2aed822cb85344aeb193830380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:41 +0000

    Add package torch-sparse/0.2.2/torch_sparse-0.2.2.tar.gz

---
+++ packages/torch-sparse/torch_sparse-0.2.2.tar.gz/torch_sparse-0.2.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import torch
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+__version__ = '0.2.2'
+url = 'https://github.com/rusty1s/pytorch_sparse'
+
+install_requires = ['scipy']
+setup_requires = ['pytest-runner']
+tests_require = ['pytest', 'pytest-cov']
+ext_modules = []
+cmdclass = {}
+
+if torch.cuda.is_available():
+    ext_modules += [
+        CUDAExtension(
+            'spspmm_cuda',
+            ['cuda/spspmm.cpp', 'cuda/spspmm_kernel.cu'],
+            extra_link_args=['-lcusparse', '-l', 'cusparse'],
+        ),
+        CUDAExtension('unique_cuda',
+                      ['cuda/unique.cpp', 'cuda/unique_kernel.cu']),
+    ]
+    cmdclass['build_ext'] = BuildExtension
+
+setup(
+    name='torch_sparse',
+    version=__version__,
+    description='PyTorch Extension Library of Optimized Autograd Sparse '
+    'Matrix Operations',
+    author='Matthias Fey',
+    author_email='matthias.fey@tu-dortmund.de',
+    url=url,
+    download_url='{}/archive/{}.tar.gz'.format(url, __version__),
+    keywords=['pytorch', 'sparse', 'autograd', 'deep-learning'],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+    packages=find_packages(),
+)
