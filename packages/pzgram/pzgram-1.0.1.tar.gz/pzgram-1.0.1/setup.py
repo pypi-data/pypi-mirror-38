eba26262b45b9dbdeb9c49d91e90ff5902ce4eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:06 +0000

    Add package pzgram/1.0.1/pzgram-1.0.1.tar.gz

---
+++ packages/pzgram/pzgram-1.0.1.tar.gz/pzgram-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'pzgram',
+  packages = ['pzgram'],
+  version = '1.0.1',
+  description = 'A pz-way to create your telegram bot',
+  author = 'Giovani Casari',
+  author_email = 'casari.giovanni@gmail.com',
+  url = 'https://github.com/infopz/pzgram',
+  download_url = 'https://github.com/infopz/pzgram/archive/1.0.1.tar.gz',
+  license = 'Apache2',
+
+  #install_requires = ["requests >= 2.18.4"],
+  keywords = ['telegram', 'telegram-bot', 'telegram-api'],
+  classifiers = [],
+)
