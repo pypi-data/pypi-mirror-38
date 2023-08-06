83ddfa5ee69757288af6c3e275fe462723a2442d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package funniest-dev-11-12-18/0.1/funniest_dev_11_12_18-0.1.tar.gz

---
+++ packages/funniest-dev-11-12-18/funniest_dev_11_12_18-0.1.tar.gz/funniest_dev_11_12_18-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_dev_11_12_18',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
