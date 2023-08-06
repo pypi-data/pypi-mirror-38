43bb0cf484325e307e63394117477d4779046c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:36 +0000

    Add package hellowebapp-deploy/1.0.4/hellowebapp-deploy-1.0.4.tar.gz

---
+++ packages/hellowebapp-deploy/hellowebapp-deploy-1.0.4.tar.gz/hellowebapp-deploy-1.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hellowebapp-deploy',
+    version='1.0.4',
+    description="Packages needed to deploy to Heroku using Hello Web App's tutorial.",
+    url='https://github.com/hellowebapp/hellowebapp-deploy',
+    download_url='https://github.com/hellowebbooks/hellowebapp-deploy/archive/v1.0.4.tar.gz',
+    author='Tracy Osborn',
+    author_email='tracy@limedaring.com',
+    license='MIT',
+    classifiers=[
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='deployment heroku hellowebapp',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires = [
+        'waitress==1.1.0',
+        'dj-database-url==0.5.0',
+        'whitenoise==3.3.1',
+    ],
+)
