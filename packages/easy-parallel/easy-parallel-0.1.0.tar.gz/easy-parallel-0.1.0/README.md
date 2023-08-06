152f8dc13f5cc12c726c4464c75845ce0186abc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:37 +0000

    Add package easy-parallel/0.1.0/easy-parallel-0.1.0.tar.gz

---
+++ packages/easy-parallel/easy-parallel-0.1.0.tar.gz/easy-parallel-0.1.0/README.md
@@ -0,0 +1,19 @@
+# easy-parallel
+
+## Installation
+
+`make requirements`
+
+## Usage
+
+```
+def foo(x, y):
+  return x + y
+
+num_threads = 10
+inputs = [(x * x, x) for x in range(10)]
+
+import parallel as par
+results = par.submit_jobs(foo, inputs, num_threads)
+print results
+```
---
+++ packages/easy-parallel/easy-parallel-0.1.0.tar.gz/easy-parallel-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='easy-parallel',
+    py_modules=['parallel'],
+    version='0.1.0',
+    description='Parallel wrapper for easy multi-threading.',
+    author='Raphael Townshend',
+    license='MIT',
+    install_requires=['pathos'],
+)
