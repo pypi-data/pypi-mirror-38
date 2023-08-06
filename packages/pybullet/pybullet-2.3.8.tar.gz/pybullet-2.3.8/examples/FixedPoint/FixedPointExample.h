34d5a6547b35587cc577e4ce2b7d49b19e325a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:24 +0000

    Add package pybullet/2.3.8/pybullet-2.3.8.tar.gz

---
+++ packages/pybullet/pybullet-2.3.8.tar.gz/pybullet-2.3.8/examples/FixedPoint/FixedPointExample.h
@@ -0,0 +1,7 @@
+#ifndef FIXED_POINT_EXAMPLE_H
+#define FIXED_POINT_EXAMPLE_H
+
+
+class	CommonExampleInterface*    FixedPointExampleCreateFunc(struct CommonExampleOptions& options);
+
+#endif //FIXED_POINT_EXAMPLE_H
---
+++ packages/pybullet/pybullet-2.3.8.tar.gz/pybullet-2.3.8/examples/pybullet/gym/pybullet_envs/ARS/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+x = np.arange((2,3))
+np.save("file", x)
+
