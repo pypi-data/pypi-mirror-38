2a8c361decad4818e2016ae60a3ff8a135bb22d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:47 +0000

    Add package pyutm/0.2/pyutm-0.2.tar.gz

---
+++ packages/pyutm/pyutm-0.2.tar.gz/pyutm-0.2/setup.py
@@ -0,0 +1,34 @@
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'pyutm', '__version__.py')) as fp:
+    exec(fp.read(), version)
+
+setuptools.setup(
+    name="pyutm",
+    version=version['__version__'],
+    author="Philip Griffith",
+    author_email="philip.griffith@fsu.edu",
+    description="Creates standardized grid references and unique identifiers for point data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FREAC/pyutm",
+    packages=setuptools.find_packages(exclude=['docs', 'tests']),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pyshp',
+        'pyproj'
+    ]
+)
