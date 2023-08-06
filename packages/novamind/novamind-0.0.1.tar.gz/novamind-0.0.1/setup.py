f35fc3f105fef177f2f0aa55042647888705620e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:43 +0000

    Add package novamind/0.0.1/novamind-0.0.1.tar.gz

---
+++ packages/novamind/novamind-0.0.1.tar.gz/novamind-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="novamind",
+    version="0.0.1",
+    author="Nova Mind",
+    author_email="duanyzhi@outlook.com",
+    description="Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/duanyzhi/novamind",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
