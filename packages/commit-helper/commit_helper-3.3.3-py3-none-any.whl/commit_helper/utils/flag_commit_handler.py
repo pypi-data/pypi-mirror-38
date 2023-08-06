65632b1271a84af2f6c477752f007cdb99898ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:38 +0000

    Add package commit-helper/3.3.3/commit_helper-3.3.3-py3-none-any.whl

---
+++ packages/commit-helper/commit_helper-3.3.3-py3-none-any.whl/commit_helper/utils/flag_commit_handler.py
@@ -0,0 +1,33 @@
+from os import system
+# utils imports
+from .utils import create_file
+from .utils import gen_co_author
+from .utils import handle_conventioned_commit
+from .text_utils import debug
+from .text_utils import get_text
+from .text_utils import get_context
+# conventions imports
+from commit_helper.conventions.karma_angular import angular_convention
+from commit_helper.conventions.changelog import changelog_convention
+from commit_helper.conventions.symphony_cmf import symphony_convention
+from commit_helper.conventions.no_convention import just_message
+
+
+def convention_flag_handler(args, debug_mode):
+    convention = str(args.convention)
+    debug('convention flag', convention, debug_mode)
+
+    if convention == 'message':
+        if args.message is not '':
+            commit_message = just_message(msg=args.message)
+        else:
+            commit_message = just_message()
+        convention = 'none'
+
+    else:
+        commit_message = handle_conventioned_commit(convention, args)
+
+    create_file(convention, args.no_file)
+    commit_message += gen_co_author(args.co_author)
+    debug('commit message', commit_message, debug_mode)
+    system('git commit -m "%s"' % commit_message)
---
+++ packages/commit-helper/commit_helper-3.3.3-py3-none-any.whl/test/test_conventions.py
@@ -0,0 +1,64 @@
+import yaml
+import commit_helper.conventions.karma_angular as angular
+import commit_helper.conventions.changelog as changelog
+import commit_helper.conventions.symphony_cmf as symphony
+import commit_helper.conventions.no_convention as no_convention
+import commit_helper.conventions.custom_convention as custom
+
+
+def test_angular_convention_with_context():
+    message = angular.angular_convention('TAG', 'message', 'context')
+    if not (message == 'tag(context): message\n'):
+        raise AssertionError()
+
+
+def test_angular_convention_without_context():
+    message = angular.angular_convention('tag', 'message', '')
+    if not (message == 'tag: message\n'):
+        raise AssertionError()
+
+
+def test_changelog_convention():
+    message = changelog.changelog_convention('tag', 'message')
+    if not (message == 'TAG: message\n'):
+        raise AssertionError()
+
+
+def test_symphony_convention():
+    message = symphony.symphony_convention('tag', 'message')
+    if not (message == '[Tag] message\n'):
+        raise AssertionError()
+
+
+def test_custom_convention():
+    fyle = yaml.safe_load("""
+    convention: custom
+    commit_pattern: tag..:..message
+    context: false
+    """)
+
+    msg = "a nice function"
+    tag = "add"
+    message = custom.custom_convention(tag, msg, fyle, True)
+    if not message == "add..:..a nice function\n":
+        raise AssertionError()
+
+
+def test_no_convention_without_args():
+    inputs = [
+        "message",
+    ]
+
+    def mock_input(s):
+        return inputs.pop(0)
+
+    no_convention.input = mock_input
+    message = no_convention.just_message()
+    if not message == 'Message\n':
+        raise AssertionError()
+
+
+def test_no_convention_with_args():
+    message = no_convention.just_message('Message')
+    if not message == 'Message\n':
+        raise AssertionError()
