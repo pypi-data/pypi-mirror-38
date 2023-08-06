a42f26f1f31c81ac4a5c354d71e115edc60e6a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:35 +0000

    Add package tap-listrak/1.0.6/tap-listrak-1.0.6.tar.gz

---
+++ packages/tap-listrak/tap-listrak-1.0.6.tar.gz/tap-listrak-1.0.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-listrak",
+    version="1.0.6",
+    description="Singer.io tap for extracting data from the Listrak API",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_listrak"],
+    install_requires=[
+        "singer-python==5.0.3",
+        "requests==2.20.0",
+        "zeep",
+        'backoff==1.3.2',
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-listrak=tap_listrak:main
+    """,
+    packages=["tap_listrak"],
+    package_data = {
+        "schemas": ["tap_listrak/schemas/*.json"]
+    },
+    include_package_data=True,
+)
