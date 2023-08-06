75eb8e7323e6909ccac9607ec7217ce142517b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:13 +0000

    Add package libconfig/0.8.0/libconfig-0.8.0.tar.gz

---
+++ packages/libconfig/libconfig-0.8.0.tar.gz/libconfig-0.8.0/libconfig/__init__.py
@@ -0,0 +1,17 @@
+# pylint: disable-msg=W0614,W0401,W0611,W0622
+
+# flake8: noqa
+
+"""A small library for global parameter configuration.
+
+.. moduleauthor:: Jaume Bonet <jaume.bonet@gmail.com>
+
+"""
+
+
+from .config import *
+from .util import AlreadyRegisteredError, NotRegisteredError
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
