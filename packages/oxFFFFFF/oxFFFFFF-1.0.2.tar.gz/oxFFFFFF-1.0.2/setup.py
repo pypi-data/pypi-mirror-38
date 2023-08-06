189fb65575e67dacccd22f0c5015003e63636936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:22 +0000

    Add package oxFFFFFF/1.0.2/oxFFFFFF-1.0.2.tar.gz

---
+++ packages/oxFFFFFF/oxFFFFFF-1.0.2.tar.gz/oxFFFFFF-1.0.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='oxFFFFFF',
+    version='1.0.2',
+    description=(
+        'hello oxFFFFFF'
+    ),
+#    long_description=open('README.rst').read(),
+    author='oxFFFFFF',
+    author_email='tomorrow_is_better@yeah.net',
+    maintainer='oxFFFFFF',
+    maintainer_email='tomorrow_is_better@yeah.net',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='http://www.google.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
