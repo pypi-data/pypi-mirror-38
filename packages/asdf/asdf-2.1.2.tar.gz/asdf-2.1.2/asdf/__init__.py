3d0873f638540c9e6f947d32fdf20648d4faef84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:06 +0000

    Add package asdf/2.1.2/asdf-2.1.2.tar.gz

---
+++ packages/asdf/asdf-2.1.2.tar.gz/asdf-2.1.2/asdf/__init__.py
@@ -0,0 +1,45 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+
+"""
+asdf: Python library for reading and writing Advanced Scientific
+Data Format (ASDF) files
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._internal_init import *
+# ----------------------------------------------------------------------------
+
+__all__ = [
+    'AsdfFile', 'CustomType', 'AsdfExtension', 'Stream', 'open', 'test',
+    'commands', 'ExternalArrayReference'
+]
+
+try:
+    import yaml as _
+except ImportError:
+    raise ImportError("asdf requires pyyaml")
+
+try:
+    import jsonschema as _
+except ImportError:
+    raise ImportError("asdf requires jsonschema")
+
+try:
+    import numpy as _
+except ImportError:
+    raise ImportError("asdf requires numpy")
+
+from .asdf import AsdfFile
+from .asdftypes import CustomType
+from .extension import AsdfExtension
+from .stream import Stream
+from . import commands
+from .tags.core.external_reference import ExternalArrayReference
+
+from jsonschema import ValidationError
+
+open = AsdfFile.open
