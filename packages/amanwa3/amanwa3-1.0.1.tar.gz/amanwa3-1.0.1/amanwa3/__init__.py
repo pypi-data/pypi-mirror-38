8d3f612106c39f7d124c62ef5977a97f4b6427aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:49 +0000

    Add package amanwa3/1.0.1/amanwa3-1.0.1.tar.gz

---
+++ packages/amanwa3/amanwa3-1.0.1.tar.gz/amanwa3-1.0.1/amanwa3/__init__.py
@@ -0,0 +1,1 @@
+name = "amanwa3"
---
+++ packages/amanwa3/amanwa3-1.0.1.tar.gz/amanwa3-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='amanwa3',
+    version='1.0.1',
+    packages=['amanwa3'],
+    url='https://github.com/checkitout/a1',
+    license='',
+    author='aman',
+    author_email='angrish_aman@yahoo.com',
+    description='testing pypi'
+)
+
