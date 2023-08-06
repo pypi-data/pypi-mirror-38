8cdb5a6d3a5eff58dcd4f6d9e920876318211cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:08 +0000

    Add package compath-resources/0.0.7/compath_resources-0.0.7.tar.gz

---
+++ packages/compath-resources/compath_resources-0.0.7.tar.gz/compath_resources-0.0.7/src/compath_resources/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""Utilities for ComPath's resources."""
+
+from compath_resources.manager import Manager, get_bel
+from compath_resources.parser import get_df
---
+++ packages/compath-resources/compath_resources-0.0.7.tar.gz/compath_resources-0.0.7/src/compath_resources/parser.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""Parsers for ComPath's resources."""
+
+import pandas as pd
+
+from compath_resources.constants import (
+    KEGG_REACTOME_URL, KEGG_WIKIPATHWAYS_URL, REACTOME_HIERARCHICAL_MAPPINGS_URL,
+    SPECIAL_MAPPINGS_URL, WIKIPATHWAYS_REACTOME_URL,
+)
+
+__all__ = [
+    'get_df',
+]
+
+
+def get_df(reactome: bool = False) -> pd.DataFrame:
+    """Get all dataframes.
+
+    :param reactome: includes reactome hiearchy
+    :return: dataframe with ComPath dataset
+    """
+    dfs = [
+        pd.read_csv(KEGG_WIKIPATHWAYS_URL),
+        pd.read_csv(KEGG_REACTOME_URL),
+        pd.read_csv(WIKIPATHWAYS_REACTOME_URL),
+        pd.read_csv(SPECIAL_MAPPINGS_URL, usecols=list(range(7))),
+    ]
+
+    if reactome:
+        dfs.append(pd.read_csv(REACTOME_HIERARCHICAL_MAPPINGS_URL))
+
+    return pd.concat(dfs)
